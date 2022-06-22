0c619153bf7c4b67cc0d833a8c59bf48e9c251d9
Anthony Buisset <abuisset@gmail.com>
2022-06-22 22:56:26 +0200

    :sparkles: create REST API to add a new project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,58 @@
+use deathnote_contributions_feeder::{
+    connection::{self, DbConn},
+    database, github,
+    model::repository,
+    traits::{fetcher::Fetcher, logger::Logger},
+};
+
+use dotenv::dotenv;
+use futures::stream::StreamExt;
+use rocket::serde::{json::Json, Deserialize};
+use rocket::{response::status, Responder};
+
 #[macro_use]
 extern crate rocket;
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Project<'r> {
+    owner: &'r str,
+    name: &'r str,
+}
+
+#[derive(Responder)]
+enum Failure {
+    #[response(status = 500, content_type = "json")]
+    InternalServerError(String),
+}
+
+#[post("/projects", format = "application/json", data = "<project>")]
+async fn new(
+    project: Json<Project<'_>>,
+    connection: DbConn,
+) -> Result<status::Accepted<()>, Failure> {
+    let filter = repository::Filter {
+        owner: Some(String::from(project.owner)),
+        name: Some(String::from(project.name)),
+    };
+
+    let database = database::API::new(connection);
+    let github = github::API::new();
+
+    github
+        .fetch(filter)
+        .await
+        .map_err(|error| Failure::InternalServerError(error.to_string()))?
+        .for_each(|repo| async {
+            database
+                .log(repo)
+                .await
+                .expect("Unable to log repository in database");
+        })
+        .await;
+
+    Ok(status::Accepted(Some(())))
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -9,5 +62,10 @@ fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     env_logger::init();
-    rocket::build().mount("/", routes![index,])
+    dotenv().ok();
+    octocrab::initialise(octocrab::Octocrab::builder()).expect("Unable to initialize octocrab");
+
+    rocket::build()
+        .manage(connection::init_pool())
+        .mount("/", routes![index, new])
 }
