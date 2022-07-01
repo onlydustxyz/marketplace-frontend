061e31c51976234c66c88b3bd62ecf093b0465d2
Anthony Buisset <abuisset@gmail.com>
2022-07-01 18:38:26 +0200

    :recycle: move Result out of Logger return type

--- src/traits/logger.rs
+++ src/traits/logger.rs
@@ -1,6 +1,7 @@
+use anyhow::Result;
 use async_trait::async_trait;
 
 #[async_trait]
 pub trait Logger<Item, Report> {
-    async fn log(&self, item: Item) -> Report;
+    async fn log(&self, item: Item) -> Result<Report>;
 }
