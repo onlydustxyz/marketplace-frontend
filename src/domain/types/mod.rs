1d2867706b4838d88a7650a8dd15b3614b0f1664
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-07-06 15:02:59 +0200

    feat(app): write skeleton of our application functions

--- src/domain/types/mod.rs
+++ src/domain/types/mod.rs
@@ -1,3 +1,4 @@
 pub mod contracts_update_status;
 pub mod contribution;
+pub mod contributor;
 pub mod project;
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 extern crate diesel;
 extern crate dotenv;
 
+pub mod application;
 pub mod database;
 pub mod domain;
 pub mod github;
