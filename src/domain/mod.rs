ce42eda3b15a551f0444939c098332094a246d8e
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-07-06 15:02:59 +0200

    feat(app): write skeleton of our application functions

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -9,4 +9,5 @@ pub use types::contracts_update_status::*;
 pub use types::contribution::{
     Contribution, Filter as ContributionFilter, Id as ContributionId, Status as ContributionStatus,
 };
+pub use types::contributor::ContributorId;
 pub use types::project::{Filter as ProjectFilter, Id as ProjectId, IndexingStatus, Project};
