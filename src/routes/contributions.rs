fd6c6debf7b5e2282aa01c10006a8555f9499784
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-07-08 14:08:53 +0200

    feat(WebApi): POST at /contribution push action in queue

--- src/routes/contributions.rs
+++ src/routes/contributions.rs
@@ -1,7 +1,7 @@
 use std::collections::VecDeque;
 use std::sync::{Arc, RwLock};
 
-use deathnote_contributions_feeder::domain::{ContributionId, ProjectId};
+use deathnote_contributions_feeder::domain::{Action, ContributionId, ProjectId};
 use rocket::response::status;
 use rocket::serde::{json::Json, Deserialize};
 use rocket::State;
@@ -11,17 +11,26 @@ use super::Failure;
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Body {
-    _contribution_id: ContributionId,
-    _project_id: ProjectId,
+    contribution_id: ContributionId,
+    project_id: ProjectId,
+    gate: u8,
 }
 
-#[post("/contribution", format = "application/json", data = "<_body>")]
+#[post("/contribution", format = "application/json", data = "<body>")]
 pub async fn create_contribution(
-    _body: Json<Body>,
-    queue: &State<Arc<RwLock<VecDeque<String>>>>,
+    body: Json<Body>,
+    queue: &State<Arc<RwLock<VecDeque<Action>>>>,
 ) -> Result<status::Accepted<()>, Failure> {
+    let body = body.into_inner();
+
+    let create_contribution_action = Action::CreateContribution {
+        contribution_id: body.contribution_id,
+        project_id: body.project_id,
+        gate: body.gate,
+    };
+
     match queue.write() {
-        Ok(mut queue) => queue.push_front("value".to_string()),
+        Ok(mut queue) => queue.push_front(create_contribution_action),
         Err(_) => {
             return Err(Failure::InternalServerError(
                 "queue is busy, try again later".to_string(),
