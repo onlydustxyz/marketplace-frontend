15a8d3237112432c4b72f0dbb48d88030d4d343c
Anthony Buisset <abuisset@gmail.com>
2022-07-11 16:47:43 +0200

    :recycle: create api dedicated object to build REST response

--- src/routes/projects.rs
+++ src/routes/projects.rs
@@ -49,12 +49,60 @@ pub async fn new_project(
 #[get("/projects")]
 pub async fn list_projects(
     connection: DbConn,
-) -> Result<Json<Vec<database::models::ProjectWithContributions>>, status::NotFound<String>> {
+) -> Result<Json<Vec<api::Project>>, status::NotFound<String>> {
     let database = database::API::new(connection);
     let results = database
         .list_projects_with_contributions()
-        .map_err(|error| status::NotFound(error.to_string()))?;
-    let projects: Vec<database::models::ProjectWithContributions> = results.collect();
+        .map_err(|error| status::NotFound(error.to_string()))?
+        .map(build_project);
+    let projects: Vec<_> = results.collect();
 
     Ok(Json(projects))
+}
+
+fn build_project(project: database::models::ProjectWithContributions) -> api::Project {
+    api::Project {
+        id: project.id,
+        title: project.name,
+        contributions: project
+            .contributions
+            .into_iter()
+            .map(build_contribution)
+            .collect(),
+    }
+}
+
+fn build_contribution(contribution: database::models::Contribution) -> api::Contribution {
+    api::Contribution {
+        id: contribution.id,
+        status: contribution.status.to_string(),
+        gate: contribution.gate as u8,
+        metadata: api::Metadata {
+            assignee: contribution.author.to_string(),
+        },
+    }
+}
+
+mod api {
+    use serde::Serialize;
+
+    #[derive(Serialize)]
+    pub struct Project {
+        pub id: String,
+        pub title: String,
+        pub contributions: Vec<Contribution>,
+    }
+
+    #[derive(Serialize)]
+    pub struct Contribution {
+        pub id: String,
+        pub status: String,
+        pub gate: u8,
+        pub metadata: Metadata,
+    }
+
+    #[derive(Serialize)]
+    pub struct Metadata {
+        pub assignee: String,
+    }
 }
