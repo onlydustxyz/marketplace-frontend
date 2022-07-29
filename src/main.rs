64ae70950144575bbb3ac8165eff33227d52fcd3
Stanislas Bernard <bernardstanislas@gmail.com>
2022-07-29 18:25:08 +0200

    feat(logger): use std_log to format log as JSON

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod routes;
 use diesel_migrations::*;
 use log::info;
 use rocket_okapi::{openapi_get_routes, swagger_ui::make_swagger_ui};
+use slog::{o, Drain, Logger};
 use std::{
 	sync::{Arc, RwLock},
 	thread,
@@ -33,10 +34,19 @@ extern crate diesel_migrations;
 
 embed_migrations!("migrations");
 
+fn get_root_logger() -> Logger {
+	let drain = slog_async::Async::default(slog_envlogger::new(
+		slog_json::Json::new(std::io::stdout()).add_default_keys().build().fuse(),
+	));
+	slog_stdlog::init().unwrap();
+	slog::Logger::root(drain.fuse(), o!("version" => env!("CARGO_PKG_VERSION")))
+}
+
 #[tokio::main]
 async fn main() {
 	dotenv().ok();
-	env_logger::init();
+	let root_logger = get_root_logger();
+	let _global_logger_guard = slog_scope::set_global_logger(root_logger);
 	github::API::initialize();
 
 	let action_queue = Arc::new(RwLock::new(ActionQueue::new()));
