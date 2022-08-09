d96df0ca2a2fd5b13b1edc74dc048129b77c05a0
tekkac <tarik.kaced@protonmail.com>
2022-08-09 11:08:20 +0200

    use dto types in e2e testing
    Co-authored-by: Timothée Delabrouille <tdelabro@users.noreply.github.com>

--- src/bin/e2e_tests.rs
+++ src/bin/e2e_tests.rs
@@ -1,3 +1,4 @@
+use log::{debug, info};
 use reqwest::StatusCode;
 use std::{fs::read_to_string, str::FromStr, thread, time::Duration};
 
@@ -14,25 +15,23 @@ use contribution::*;
 async fn main() {
 	add_all_projects().await;
 
-	let starkonquest = find_project_by_title(&list_all_projects().await, "starkonquest");
-	let starkonquest_id: u64 = starkonquest["id"]
-		.as_str()
-		.expect("starkonquest id is not a string")
-		.parse()
-		.expect("starkonquest id is not a number");
+	let starkonquest = find_project_by_title(&list_all_projects().await, "starkonquest")
+		.expect("Project not found in list of all projects");
+	let starkonquest_id: u64 = starkonquest.id.parse().expect("starkonquest id is not a number");
 
 	const ISSUE_NUMBER: u64 = 51;
 	add_contribution(ISSUE_NUMBER, starkonquest_id, 0, "0x123").await;
 
 	wait_for_result("src/data/contributions_open.json").await;
 
-	let starkonquest = find_project_by_title(&list_all_projects().await, "starkonquest");
+	let starkonquest = find_project_by_title(&list_all_projects().await, "starkonquest")
+		.expect("Project not found in list of all projects");
 	let contribution =
-		find_contribution_by_onchain_id(&starkonquest, starkonquest_id * 1_000_000 + ISSUE_NUMBER);
+		find_contribution_by_onchain_id(&starkonquest, starkonquest_id * 1_000_000 + ISSUE_NUMBER)
+			.expect("Contribution not found in project");
 
 	let contribution_id =
-		uuid::Uuid::from_str(contribution["id"].as_str().expect("contribution id is not a str"))
-			.expect("contribution id is not a valid uuid");
+		uuid::Uuid::from_str(&contribution.id).expect("contribution id is not a valid uuid");
 
 	const CONTRIBUTOR_ID: u128 = 123;
 	assign_contribution(contribution_id, CONTRIBUTOR_ID).await;
@@ -50,16 +49,20 @@ async fn add_all_projects() {
 }
 
 async fn wait_for_result(result_file_path: &'static str) {
-	for _ in 0..10 {
+	for i in 0..10 {
+		debug!("LOOP {i}");
 		let handle = tokio::spawn(async move {
-			compare_jsons(
+			compare_projects_to_expected(
 				list_all_projects().await,
 				serde_json::from_str(&read_to_string(result_file_path).unwrap()).unwrap(),
 			);
 		});
 
 		match tokio::join!(handle).0 {
-			Ok(_) => return,
+			Ok(_) => {
+				info!("Success");
+				return;
+			},
 			Err(_) => {
 				thread::sleep(Duration::from_secs(3));
 			},
--- src/bin/projects/list.rs
+++ src/bin/projects/list.rs
@@ -1,9 +1,9 @@
 use super::*;
+use deathnote_contributions_feeder::dto::Project;
 
-pub async fn list_all_projects() -> serde_json::Value {
+pub async fn list_all_projects() -> Vec<Project> {
 	let response = get(format!("{BACKEND_BASE_URI}/projects")).await;
 
-	assert_eq!(StatusCode::OK, response.status());
 	let projects = response.text().await.unwrap();
 	serde_json::from_str(&projects).unwrap()
 }
