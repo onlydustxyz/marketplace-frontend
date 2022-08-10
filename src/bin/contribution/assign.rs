d9efe740b1a45c248c689062de309c52df4ad20e
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-08-10 16:48:28 +0200

    fix: e2e tests use 0x prefixed contributor_id

--- src/bin/contribution/assign.rs
+++ src/bin/contribution/assign.rs
@@ -4,7 +4,9 @@ use serde_json::json;
 pub async fn assign_contribution(contribution_id: uuid::Uuid, contributor_id: u128) {
 	let response = post(
 		format!("{BACKEND_BASE_URI}/contributions/{contribution_id}/contributor"),
-		Some(json!({ "contributor_id": contributor_id })),
+		Some(json!({
+			"contributor_id": format!("{:#x}", contributor_id)
+		})),
 	)
 	.await;
 
