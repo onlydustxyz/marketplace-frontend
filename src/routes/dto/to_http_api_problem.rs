16e19b7069f215aaa1670e2e20a3f0a0549bd1bc
Stanislas Bernard <bernardstanislas@gmail.com>
2022-08-12 18:40:50 +0200

    chore: update format

--- src/routes/dto/to_http_api_problem.rs
+++ src/routes/dto/to_http_api_problem.rs
@@ -12,24 +12,20 @@ pub(crate) trait ToHttpApiProblem {
 impl ToHttpApiProblem for ApplicationRepositoryError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			ApplicationRepositoryError::NotFound => {
-				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string())
-			},
-			ApplicationRepositoryError::AlreadyExist(e) => {
+			ApplicationRepositoryError::NotFound =>
+				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string()),
+			ApplicationRepositoryError::AlreadyExist(e) =>
 				HttpApiProblem::new(StatusCode::CONFLICT)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ApplicationRepositoryError::InvalidEntity(e) => {
+					.detail(e.to_string()),
+			ApplicationRepositoryError::InvalidEntity(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ApplicationRepositoryError::Infrastructure(e) => {
+					.detail(e.to_string()),
+			ApplicationRepositoryError::Infrastructure(e) =>
 				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -37,24 +33,20 @@ impl ToHttpApiProblem for ApplicationRepositoryError {
 impl ToHttpApiProblem for ContributionRepositoryError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			ContributionRepositoryError::NotFound => {
-				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string())
-			},
-			ContributionRepositoryError::AlreadyExist(e) => {
+			ContributionRepositoryError::NotFound =>
+				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string()),
+			ContributionRepositoryError::AlreadyExist(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ContributionRepositoryError::InvalidEntity(e) => {
+					.detail(e.to_string()),
+			ContributionRepositoryError::InvalidEntity(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ContributionRepositoryError::Infrastructure(e) => {
+					.detail(e.to_string()),
+			ContributionRepositoryError::Infrastructure(e) =>
 				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -62,11 +54,10 @@ impl ToHttpApiProblem for ContributionRepositoryError {
 impl ToHttpApiProblem for ContributorRepositoryError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			ContributorRepositoryError::Infrastructure(e) => {
+			ContributorRepositoryError::Infrastructure(e) =>
 				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -74,11 +65,10 @@ impl ToHttpApiProblem for ContributorRepositoryError {
 impl ToHttpApiProblem for OnchainContributionServiceError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			OnchainContributionServiceError::Infrastructure(e) => {
+			OnchainContributionServiceError::Infrastructure(e) =>
 				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -86,11 +76,10 @@ impl ToHttpApiProblem for OnchainContributionServiceError {
 impl ToHttpApiProblem for ContributionServiceError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			ContributionServiceError::CannotApply(e) => {
+			ContributionServiceError::CannotApply(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -98,29 +87,24 @@ impl ToHttpApiProblem for ContributionServiceError {
 impl ToHttpApiProblem for ApplicationServiceError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			ApplicationServiceError::NotFound => {
-				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string())
-			},
-			ApplicationServiceError::AlreadyExist(e) => {
+			ApplicationServiceError::NotFound =>
+				HttpApiProblem::new(StatusCode::NOT_FOUND).title(self.to_string()),
+			ApplicationServiceError::AlreadyExist(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ApplicationServiceError::InvalidEntity(e) => {
+					.detail(e.to_string()),
+			ApplicationServiceError::InvalidEntity(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ApplicationServiceError::Infrastructure(e) => {
+					.detail(e.to_string()),
+			ApplicationServiceError::Infrastructure(e) =>
 				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
-			ApplicationServiceError::InvalidContributionStatus(e) => {
+					.detail(e.to_string()),
+			ApplicationServiceError::InvalidContributionStatus(e) =>
 				HttpApiProblem::new(StatusCode::BAD_REQUEST)
 					.title(self.to_string())
-					.detail(e.to_string())
-			},
+					.detail(e.to_string()),
 		}
 	}
 }
@@ -128,27 +112,20 @@ impl ToHttpApiProblem for ApplicationServiceError {
 impl ToHttpApiProblem for DomainError {
 	fn to_http_api_problem(&self) -> HttpApiProblem {
 		match self {
-			DomainError::ApplicationRepository(application_repository_error) => {
-				application_repository_error.to_http_api_problem()
-			},
-			DomainError::ContributionRepository(contribution_reopsitory_error) => {
-				contribution_reopsitory_error.to_http_api_problem()
-			},
-			DomainError::ContributorRepository(contributor_repository_error) => {
-				contributor_repository_error.to_http_api_problem()
-			},
-			DomainError::OnchainContributionService(onchain_contribution_service_error) => {
-				onchain_contribution_service_error.to_http_api_problem()
-			},
-			DomainError::ContributionService(contribution_service_error) => {
-				contribution_service_error.to_http_api_problem()
-			},
-			DomainError::ApplicationService(application_service_error) => {
-				application_service_error.to_http_api_problem()
-			},
-			DomainError::Lock => {
-				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR).title(self.to_string())
-			},
+			DomainError::ApplicationRepository(application_repository_error) =>
+				application_repository_error.to_http_api_problem(),
+			DomainError::ContributionRepository(contribution_reopsitory_error) =>
+				contribution_reopsitory_error.to_http_api_problem(),
+			DomainError::ContributorRepository(contributor_repository_error) =>
+				contributor_repository_error.to_http_api_problem(),
+			DomainError::OnchainContributionService(onchain_contribution_service_error) =>
+				onchain_contribution_service_error.to_http_api_problem(),
+			DomainError::ContributionService(contribution_service_error) =>
+				contribution_service_error.to_http_api_problem(),
+			DomainError::ApplicationService(application_service_error) =>
+				application_service_error.to_http_api_problem(),
+			DomainError::Lock =>
+				HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR).title(self.to_string()),
 		}
 	}
 }
