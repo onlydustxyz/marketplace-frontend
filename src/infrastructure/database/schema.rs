747dde43a97f9134e9b5ead3c3470e101c0afbff
tekkac <tarik.kaced@protonmail.com>
2022-08-16 15:00:42 +0200

    fix: formatting issues

--- src/infrastructure/database/schema.rs
+++ src/infrastructure/database/schema.rs
@@ -1,58 +1,53 @@
 table! {
-    applications (id) {
-        id -> Uuid,
-        contribution_id -> Uuid,
-        contributor_id -> Varchar,
-        status -> Varchar,
-    }
+	applications (id) {
+		id -> Uuid,
+		contribution_id -> Uuid,
+		contributor_id -> Varchar,
+		status -> Varchar,
+	}
 }
 
 table! {
-    contributions (id) {
-        onchain_id -> Varchar,
-        project_id -> Varchar,
-        status -> Varchar,
-        transaction_hash -> Nullable<Varchar>,
-        contributor_id -> Varchar,
-        gate -> Int2,
-        title -> Nullable<Text>,
-        description -> Nullable<Text>,
-        external_link -> Nullable<Text>,
-        difficulty -> Nullable<Text>,
-        technology -> Nullable<Text>,
-        duration -> Nullable<Text>,
-        context -> Nullable<Text>,
-        #[sql_name = "type"]
-        type_ -> Nullable<Text>,
-        validator -> Varchar,
-        id -> Uuid,
-    }
+	contributions (id) {
+		onchain_id -> Varchar,
+		project_id -> Varchar,
+		status -> Varchar,
+		transaction_hash -> Nullable<Varchar>,
+		contributor_id -> Varchar,
+		gate -> Int2,
+		title -> Nullable<Text>,
+		description -> Nullable<Text>,
+		external_link -> Nullable<Text>,
+		difficulty -> Nullable<Text>,
+		technology -> Nullable<Text>,
+		duration -> Nullable<Text>,
+		context -> Nullable<Text>,
+		#[sql_name = "type"]
+		type_ -> Nullable<Text>,
+		validator -> Varchar,
+		id -> Uuid,
+	}
 }
 
 table! {
-    contributors (id) {
-        id -> Varchar,
-        discord_handle -> Nullable<Varchar>,
-        github_handle -> Nullable<Varchar>,
-        github_username -> Nullable<Varchar>,
-    }
+	contributors (id) {
+		id -> Varchar,
+		discord_handle -> Nullable<Varchar>,
+		github_handle -> Nullable<Varchar>,
+		github_username -> Nullable<Varchar>,
+	}
 }
 
 table! {
-    projects (id) {
-        id -> Varchar,
-        owner -> Varchar,
-        name -> Varchar,
-        last_indexed_time -> Nullable<Timestamp>,
-    }
+	projects (id) {
+		id -> Varchar,
+		owner -> Varchar,
+		name -> Varchar,
+		last_indexed_time -> Nullable<Timestamp>,
+	}
 }
 
 joinable!(applications -> contributions (contribution_id));
 joinable!(contributions -> projects (project_id));
 
-allow_tables_to_appear_in_same_query!(
-    applications,
-    contributions,
-    contributors,
-    projects,
-);
+allow_tables_to_appear_in_same_query!(applications, contributions, contributors, projects,);
