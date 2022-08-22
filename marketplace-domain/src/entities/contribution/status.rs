4a2d930c03346b18cedf03edb736aaec8d431ae5
Anthony Buisset <abuisset@gmail.com>
2022-08-22 14:13:15 +0200

    :sparkles: create Contribution aggregate

--- marketplace-domain/src/entities/contribution/status.rs
+++ marketplace-domain/src/entities/contribution/status.rs
@@ -4,15 +4,17 @@ use thiserror::Error;
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub enum Status {
 	#[default]
-	Open = 0,
-	Assigned = 1,
-	Completed = 2,
-	Abandoned = 3,
+	None = 0,
+	Open = 1,
+	Assigned = 2,
+	Completed = 3,
+	Abandoned = 4,
 }
 
 impl std::fmt::Display for Status {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
 		match self {
+			Status::None => write!(f, "NONE"),
 			Status::Open => write!(f, "OPEN"),
 			Status::Assigned => write!(f, "ASSIGNED"),
 			Status::Completed => write!(f, "COMPLETED"),
@@ -30,6 +32,7 @@ impl FromStr for Status {
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		match s {
+			"NONE" => Ok(Status::None),
 			"OPEN" => Ok(Status::Open),
 			"ASSIGNED" => Ok(Status::Assigned),
 			"COMPLETED" => Ok(Status::Completed),
@@ -45,6 +48,7 @@ mod test {
 	use rstest::*;
 
 	#[rstest]
+	#[case(Status::None, "NONE")]
 	#[case(Status::Open, "OPEN")]
 	#[case(Status::Assigned, "ASSIGNED")]
 	#[case(Status::Completed, "COMPLETED")]
