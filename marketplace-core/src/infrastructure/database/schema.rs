ee4b837e7e3c63104d332c66cb782d6ef4211622
Anthony Buisset <abuisset@gmail.com>
2022-08-22 16:43:29 +0200

    :card_file_box: add events table

--- marketplace-core/src/infrastructure/database/schema.rs
+++ marketplace-core/src/infrastructure/database/schema.rs
@@ -1,52 +1,68 @@
 table! {
-	applications (id) {
-		id -> Uuid,
-		contribution_id -> Uuid,
-		contributor_id -> Varchar,
-		status -> Varchar,
-	}
+    applications (id) {
+        id -> Uuid,
+        contribution_id -> Uuid,
+        contributor_id -> Varchar,
+        status -> Varchar,
+    }
 }
 
 table! {
-	contact_information (id) {
-		id -> Uuid,
-		contributor_id -> Varchar,
-		discord_handle -> Nullable<Varchar>,
-	}
+    contact_information (id) {
+        id -> Uuid,
+        contributor_id -> Varchar,
+        discord_handle -> Nullable<Varchar>,
+    }
 }
 
 table! {
-	contributions (id) {
-		onchain_id -> Varchar,
-		project_id -> Varchar,
-		status -> Varchar,
-		transaction_hash -> Nullable<Varchar>,
-		contributor_id -> Varchar,
-		gate -> Int2,
-		title -> Nullable<Text>,
-		description -> Nullable<Text>,
-		external_link -> Nullable<Text>,
-		difficulty -> Nullable<Text>,
-		technology -> Nullable<Text>,
-		duration -> Nullable<Text>,
-		context -> Nullable<Text>,
-		#[sql_name = "type"]
-		type_ -> Nullable<Text>,
-		validator -> Varchar,
-		id -> Uuid,
-	}
+    contributions (id) {
+        onchain_id -> Varchar,
+        project_id -> Varchar,
+        status -> Varchar,
+        transaction_hash -> Nullable<Varchar>,
+        contributor_id -> Varchar,
+        gate -> Int2,
+        title -> Nullable<Text>,
+        description -> Nullable<Text>,
+        external_link -> Nullable<Text>,
+        difficulty -> Nullable<Text>,
+        technology -> Nullable<Text>,
+        duration -> Nullable<Text>,
+        context -> Nullable<Text>,
+        #[sql_name = "type"]
+        type_ -> Nullable<Text>,
+        validator -> Varchar,
+        id -> Uuid,
+    }
 }
 
 table! {
-	projects (id) {
-		id -> Varchar,
-		owner -> Varchar,
-		name -> Varchar,
-		last_indexed_time -> Nullable<Timestamp>,
-	}
+    events (index) {
+        index -> Int4,
+        timestamp -> Timestamp,
+        aggregate_name -> Varchar,
+        aggregate_id -> Uuid,
+        payload -> Json,
+    }
+}
+
+table! {
+    projects (id) {
+        id -> Varchar,
+        owner -> Varchar,
+        name -> Varchar,
+        last_indexed_time -> Nullable<Timestamp>,
+    }
 }
 
 joinable!(applications -> contributions (contribution_id));
 joinable!(contributions -> projects (project_id));
 
-allow_tables_to_appear_in_same_query!(applications, contact_information, contributions, projects,);
+allow_tables_to_appear_in_same_query!(
+    applications,
+    contact_information,
+    contributions,
+    events,
+    projects,
+);
