cdde0a913c2ccea6eec2146ac2e9b8983ebb0de2
Anthony Buisset <abuisset@gmail.com>
2022-08-23 15:57:20 +0200

    :recycle: remove checks in aggregate::apply function

--- marketplace-domain/src/contribution/aggregate.rs
+++ marketplace-domain/src/contribution/aggregate.rs
@@ -4,54 +4,46 @@ use crypto_bigint::U256;
 use marketplace_wrappers::HexStringWrapper;
 use serde::{Deserialize, Serialize};
 use std::{fmt::Display, str::FromStr};
-use thiserror::Error;
-
-#[derive(Debug, Error, PartialEq, Eq)]
-pub enum Error {
-	#[error("Invalid state transition")]
-	InvalidStateTransition,
-}
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Hash, Default, HexStringWrapper)]
 pub struct Id(HexPrefixedString);
+
 pub struct Aggregate;
 
 impl EventAggregate for Aggregate {
-	type Error = Error;
 	type Event = Event;
 	type Id = Id;
 	type State = State;
 
-	fn apply(state: Self::State, event: Self::Event) -> Result<Self::State, Self::Error> {
+	fn apply(state: Self::State, event: Self::Event) -> Self::State {
 		match event {
 			Event::Created {
 				id: _,
 				project_id,
 				gate,
-			} if state.status == Status::None => Ok(Self::State {
+			} => Self::State {
 				project_id,
 				gate,
 				status: Status::Open,
 				..state
-			}),
+			},
 			Event::Assigned {
 				id: _,
 				contributor_id,
-			} if state.status == Status::Open => Ok(Self::State {
+			} => Self::State {
 				status: Status::Assigned,
 				contributor_id: Some(contributor_id),
 				..state
-			}),
-			Event::Unassigned { id: _ } if state.status == Status::Assigned => Ok(Self::State {
+			},
+			Event::Unassigned { id: _ } => Self::State {
 				status: Status::Open,
 				contributor_id: None,
 				..state
-			}),
-			Event::Validated { id: _ } if state.status == Status::Assigned => Ok(Self::State {
+			},
+			Event::Validated { id: _ } => Self::State {
 				status: Status::Completed,
 				..state
-			}),
-			_ => Err(Self::Error::InvalidStateTransition),
+			},
 		}
 	}
 }
@@ -123,53 +115,33 @@ mod test {
 		}
 	}
 
-	fn apply(state: State, event: Event) -> Result<State, Error> {
+	fn apply(state: State, event: Event) -> State {
 		<Aggregate as EventAggregate>::apply(state, event)
 	}
 
 	#[rstest]
 	fn create_contribution(no_contribution: State, contribution_created_event: Event) {
-		let state = apply(no_contribution, contribution_created_event).unwrap();
+		let state = apply(no_contribution, contribution_created_event);
 		assert_eq!(Status::Open, state.status);
 	}
 
 	#[rstest]
 	fn assign_contribution(open_contribution: State, contribution_assigned_event: Event) {
-		let state = apply(open_contribution, contribution_assigned_event).unwrap();
+		let state = apply(open_contribution, contribution_assigned_event);
 		assert_eq!(Status::Assigned, state.status);
 		assert!(state.contributor_id.is_some());
 	}
 
 	#[rstest]
 	fn unassign_contribution(assigned_contribution: State, contribution_unassigned_event: Event) {
-		let state = apply(assigned_contribution, contribution_unassigned_event).unwrap();
+		let state = apply(assigned_contribution, contribution_unassigned_event);
 		assert_eq!(Status::Open, state.status);
 		assert!(state.contributor_id.is_none());
 	}
 
 	#[rstest]
 	fn validate_contribution(assigned_contribution: State, contribution_validated_event: Event) {
-		let state = apply(assigned_contribution, contribution_validated_event).unwrap();
+		let state = apply(assigned_contribution, contribution_validated_event);
 		assert_eq!(Status::Completed, state.status);
-	}
-
-	#[rstest]
-	#[case(no_contribution(), contribution_assigned_event())]
-	#[case(no_contribution(), contribution_unassigned_event())]
-	#[case(no_contribution(), contribution_validated_event())]
-	#[case(open_contribution(), contribution_created_event())]
-	#[case(open_contribution(), contribution_unassigned_event())]
-	#[case(open_contribution(), contribution_validated_event())]
-	#[case(assigned_contribution(), contribution_created_event())]
-	#[case(assigned_contribution(), contribution_assigned_event())]
-	#[case(completed_contribution(), contribution_created_event())]
-	#[case(completed_contribution(), contribution_assigned_event())]
-	#[case(completed_contribution(), contribution_unassigned_event())]
-	#[case(completed_contribution(), contribution_validated_event())]
-	fn invalid_transition(#[case] state: State, #[case] event: Event) {
-		assert_eq!(
-			Error::InvalidStateTransition,
-			apply(state, event).unwrap_err()
-		);
 	}
 }
--- marketplace-domain/src/event_aggregate.rs
+++ marketplace-domain/src/event_aggregate.rs
@@ -2,7 +2,6 @@ pub trait Aggregate {
 	type Id;
 	type State;
 	type Event;
-	type Error;
 
-	fn apply(state: Self::State, event: Self::Event) -> Result<Self::State, Self::Error>;
+	fn apply(state: Self::State, event: Self::Event) -> Self::State;
 }
