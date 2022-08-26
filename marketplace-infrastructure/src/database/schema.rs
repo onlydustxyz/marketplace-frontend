ad8c9908103c1b1a4c5e1be0a3170b6fb0f03856
Anthony Buisset <abuisset@gmail.com>
2022-08-26 22:52:06 +0200

    :art: ignore schema.rs in cargo fmt

--- marketplace-infrastructure/src/database/schema.rs
+++ marketplace-infrastructure/src/database/schema.rs
@@ -1,86 +1,86 @@
 table! {
-	applications (id) {
-		id -> Uuid,
-		contribution_id -> Text,
-		contributor_id -> Text,
-		status -> Text,
-	}
+    applications (id) {
+        id -> Uuid,
+        contribution_id -> Text,
+        contributor_id -> Text,
+        status -> Text,
+    }
 }
 
 table! {
-	applications_backup (id) {
-		id -> Uuid,
-		contribution_id -> Uuid,
-		contributor_id -> Varchar,
-		status -> Varchar,
-	}
+    applications_backup (id) {
+        id -> Uuid,
+        contribution_id -> Uuid,
+        contributor_id -> Varchar,
+        status -> Varchar,
+    }
 }
 
 table! {
-	contact_information (id) {
-		id -> Uuid,
-		contributor_id -> Varchar,
-		discord_handle -> Nullable<Varchar>,
-	}
+    contact_information (id) {
+        id -> Uuid,
+        contributor_id -> Varchar,
+        discord_handle -> Nullable<Varchar>,
+    }
 }
 
 table! {
-	contributions (id) {
-		id -> Text,
-		project_id -> Text,
-		status -> Text,
-		gate -> Int4,
-		contributor_id -> Nullable<Text>,
-		title -> Nullable<Text>,
-		description -> Nullable<Text>,
-		external_link -> Nullable<Text>,
-		difficulty -> Nullable<Text>,
-		technology -> Nullable<Text>,
-		duration -> Nullable<Text>,
-		context -> Nullable<Text>,
-		#[sql_name = "type"]
-		type_ -> Nullable<Text>,
-	}
+    contributions (id) {
+        id -> Text,
+        project_id -> Text,
+        status -> Text,
+        gate -> Int4,
+        contributor_id -> Nullable<Text>,
+        title -> Nullable<Text>,
+        description -> Nullable<Text>,
+        external_link -> Nullable<Text>,
+        difficulty -> Nullable<Text>,
+        technology -> Nullable<Text>,
+        duration -> Nullable<Text>,
+        context -> Nullable<Text>,
+        #[sql_name = "type"]
+        type_ -> Nullable<Text>,
+    }
 }
 
 table! {
-	contributions_backup (id) {
-		onchain_id -> Varchar,
-		project_id -> Varchar,
-		status -> Varchar,
-		transaction_hash -> Nullable<Varchar>,
-		contributor_id -> Varchar,
-		gate -> Int2,
-		title -> Nullable<Text>,
-		description -> Nullable<Text>,
-		external_link -> Nullable<Text>,
-		difficulty -> Nullable<Text>,
-		technology -> Nullable<Text>,
-		duration -> Nullable<Text>,
-		context -> Nullable<Text>,
-		#[sql_name = "type"]
-		type_ -> Nullable<Text>,
-		id -> Uuid,
-	}
+    contributions_backup (id) {
+        onchain_id -> Varchar,
+        project_id -> Varchar,
+        status -> Varchar,
+        transaction_hash -> Nullable<Varchar>,
+        contributor_id -> Varchar,
+        gate -> Int2,
+        title -> Nullable<Text>,
+        description -> Nullable<Text>,
+        external_link -> Nullable<Text>,
+        difficulty -> Nullable<Text>,
+        technology -> Nullable<Text>,
+        duration -> Nullable<Text>,
+        context -> Nullable<Text>,
+        #[sql_name = "type"]
+        type_ -> Nullable<Text>,
+        id -> Uuid,
+    }
 }
 
 table! {
-	events (index) {
-		index -> Int4,
-		timestamp -> Timestamp,
-		aggregate_name -> Varchar,
-		aggregate_id -> Varchar,
-		payload -> Jsonb,
-	}
+    events (index) {
+        index -> Int4,
+        timestamp -> Timestamp,
+        aggregate_name -> Varchar,
+        aggregate_id -> Varchar,
+        payload -> Jsonb,
+    }
 }
 
 table! {
-	projects (id) {
-		id -> Varchar,
-		owner -> Varchar,
-		name -> Varchar,
-		last_indexed_time -> Nullable<Timestamp>,
-	}
+    projects (id) {
+        id -> Varchar,
+        owner -> Varchar,
+        name -> Varchar,
+        last_indexed_time -> Nullable<Timestamp>,
+    }
 }
 
 joinable!(applications -> contributions (contribution_id));
@@ -89,11 +89,11 @@ joinable!(contributions -> projects (project_id));
 joinable!(contributions_backup -> projects (project_id));
 
 allow_tables_to_appear_in_same_query!(
-	applications,
-	applications_backup,
-	contact_information,
-	contributions,
-	contributions_backup,
-	events,
-	projects,
+    applications,
+    applications_backup,
+    contact_information,
+    contributions,
+    contributions_backup,
+    events,
+    projects,
 );
