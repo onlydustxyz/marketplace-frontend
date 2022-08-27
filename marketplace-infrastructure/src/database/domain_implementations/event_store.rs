ecfb18599f0c086168ad84dca9740ac416ae7753
Stanislas Bernard <bernardstanislas@gmail.com>
2022-08-27 17:02:41 +0200

    :sparkles: Add list_by_id method in the event store

--- marketplace-infrastructure/src/database/domain_implementations/event_store.rs
+++ marketplace-infrastructure/src/database/domain_implementations/event_store.rs
@@ -1,6 +1,7 @@
 use crate::database::{models, schema::events, Client};
 use diesel::prelude::*;
 use marketplace_domain::*;
+use serde_json::Value;
 
 const CONTRIBUTION_AGGREGATE: &str = "CONTRIBUTION";
 
@@ -8,7 +9,7 @@ impl EventStore<ContributionAggregateRoot> for Client {
 	fn append(
 		&self,
 		aggregate_id: &<ContributionAggregateRoot as AggregateRoot>::Id,
-		events: &[<ContributionAggregateRoot as AggregateRoot>::Event],
+		events: Vec<<ContributionAggregateRoot as AggregateRoot>::Event>,
 	) -> Result<(), EventStoreError> {
 		let connection = self.connection().map_err(|_| EventStoreError::Connection)?;
 
@@ -30,5 +31,78 @@ impl EventStore<ContributionAggregateRoot> for Client {
 			.map_err(|_| EventStoreError::Append)?;
 
 		Ok(())
+	}
+
+	fn list_by_id(
+		&self,
+		aggregate_id: &<ContributionAggregateRoot as AggregateRoot>::Id,
+	) -> Result<Vec<<ContributionAggregateRoot as AggregateRoot>::Event>, EventStoreError> {
+		let connection = self.connection().map_err(|_| EventStoreError::Connection)?;
+
+		let query = events::dsl::events
+			.select(events::payload)
+			.filter(events::aggregate_id.eq(aggregate_id.to_string()))
+			.filter(events::aggregate_name.eq_all(CONTRIBUTION_AGGREGATE))
+			.order_by(events::index)
+			.into_boxed();
+		let serialized_events =
+			query.load::<Value>(&*connection).map_err(|_| EventStoreError::List)?;
+		let deserialized_events: Result<
+			Vec<<ContributionAggregateRoot as AggregateRoot>::Event>,
+			_,
+		> = serialized_events
+			.iter()
+			.map(|event_value| {
+				serde_json::from_value::<<ContributionAggregateRoot as AggregateRoot>::Event>(
+					event_value.to_owned(),
+				)
+			})
+			.collect();
+		deserialized_events.map_err(|_| EventStoreError::List)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::database::{init_pool, Client};
+	use rstest::{fixture, rstest};
+	use std::str::FromStr;
+
+	#[fixture]
+	fn event_store() -> Box<dyn marketplace_domain::EventStore<ContributionAggregateRoot>> {
+		let event_store = Client::new(init_pool());
+		Box::new(event_store) as Box<dyn EventStore<ContributionAggregateRoot>>
+	}
+
+	#[rstest]
+	#[ignore = "require a database"]
+	fn test_append_and_list(event_store: Box<dyn EventStore<ContributionAggregateRoot>>) {
+		let contribution_id: ContributionId = HexPrefixedString::from_str("0x123").unwrap().into();
+		let contributor_id: ContributorId = HexPrefixedString::from_str("0x456").unwrap().into();
+		let creation_event = ContributionEvent::Created {
+			id: contribution_id.clone(),
+			project_id: Default::default(),
+			issue_number: Default::default(),
+			gate: Default::default(),
+		};
+		let assigned_event = ContributionEvent::Assigned {
+			id: contribution_id.clone(),
+			contributor_id,
+		};
+
+		assert!(
+			event_store
+				.append(
+					&contribution_id,
+					vec![creation_event.clone(), assigned_event.clone()]
+				)
+				.is_ok()
+		);
+
+		let contribution_events = event_store.list_by_id(&contribution_id).unwrap();
+		assert_eq!(contribution_events.len(), 2);
+		assert_eq!(*contribution_events.first().unwrap(), creation_event);
+		assert_eq!(*contribution_events.last().unwrap(), assigned_event);
 	}
 }
