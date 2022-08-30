ae7252eb8f46b3ef9fa22fba723fc1c334e3b097
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-08-30 18:21:42 +0200

    ✨ deduplicate events in event_store

--- marketplace-infrastructure/src/database/domain_implementations/event_store.rs
+++ marketplace-infrastructure/src/database/domain_implementations/event_store.rs
@@ -1,4 +1,8 @@
-use crate::database::{models, schema::events, Client};
+use crate::database::{
+	models,
+	schema::{event_deduplications, events},
+	Client,
+};
 use diesel::prelude::*;
 use marketplace_domain::*;
 use serde_json::Value;
@@ -13,6 +17,15 @@ impl EventStore<Contribution> for Client {
 	) -> Result<(), EventStoreError> {
 		let connection = self.connection().map_err(|_| EventStoreError::Connection)?;
 
+		let deduplications = events
+			.iter()
+			.map(|event| {
+				Ok(models::EventDeduplication {
+					deduplication_id: event.deduplication_id.to_owned(),
+				})
+			})
+			.collect::<Result<Vec<_>, EventStoreError>>()?;
+
 		let events = events
 			.iter()
 			.map(|event| {
@@ -25,9 +38,13 @@ impl EventStore<Contribution> for Client {
 			})
 			.collect::<Result<Vec<_>, EventStoreError>>()?;
 
-		diesel::insert_into(events::table)
-			.values(&events)
-			.execute(&*connection)
+		connection
+			.transaction(|| {
+				diesel::insert_into(event_deduplications::table)
+					.values(&deduplications)
+					.execute(&*connection)?;
+				diesel::insert_into(events::table).values(&events).execute(&*connection)
+			})
 			.map_err(|_| EventStoreError::Append)?;
 
 		Ok(())
@@ -73,38 +90,94 @@ mod tests {
 		Box::new(event_store) as Box<dyn EventStore<Contribution>>
 	}
 
-	#[rstest]
-	#[ignore = "require a database"]
-	fn test_append_and_list(event_store: Box<dyn EventStore<Contribution>>) {
-		let contribution_id: ContributionId = HexPrefixedString::from_str("0x123").unwrap().into();
-		let contributor_id: ContributorId = HexPrefixedString::from_str("0x456").unwrap().into();
-		let creation_event = StorableEvent {
+	#[fixture]
+	fn contribution_id() -> ContributionId {
+		HexPrefixedString::from_str("0x123").unwrap().into()
+	}
+
+	#[fixture]
+	fn contributor_id() -> ContributorId {
+		HexPrefixedString::from_str("0x456").unwrap().into()
+	}
+
+	#[fixture]
+	fn creation_event(contribution_id: ContributionId) -> StorableEvent<Contribution> {
+		StorableEvent {
 			event: ContributionEvent::Created {
 				id: contribution_id.clone(),
 				project_id: Default::default(),
 				issue_number: Default::default(),
 				gate: Default::default(),
 			},
-			deduplication_id: "dedup".to_string(),
-		};
-		let assigned_event = StorableEvent {
+			deduplication_id: "dedup1".to_string(),
+		}
+	}
+
+	#[fixture]
+	fn assigned_event(
+		contribution_id: ContributionId,
+		contributor_id: ContributorId,
+	) -> StorableEvent<Contribution> {
+		StorableEvent {
 			event: ContributionEvent::Assigned {
 				id: contribution_id.clone(),
 				contributor_id,
 			},
-			deduplication_id: "dedup".to_string(),
-		};
+			deduplication_id: "dedup2".to_string(),
+		}
+	}
 
-		assert!(event_store
-			.append(
-				&contribution_id,
-				vec![creation_event.clone(), assigned_event.clone()]
-			)
-			.is_ok());
+	#[rstest]
+	#[ignore = "require a database"]
+	fn test_append_and_list(
+		event_store: Box<dyn EventStore<Contribution>>,
+		contribution_id: ContributionId,
+		creation_event: StorableEvent<Contribution>,
+		assigned_event: StorableEvent<Contribution>,
+	) {
+		assert!(
+			event_store
+				.append(
+					&contribution_id,
+					vec![creation_event.clone(), assigned_event.clone()]
+				)
+				.is_ok()
+		);
 
 		let contribution_events = event_store.list_by_id(&contribution_id).unwrap();
 		assert_eq!(contribution_events.len(), 2);
 		assert_eq!(*contribution_events.first().unwrap(), creation_event.event);
 		assert_eq!(*contribution_events.last().unwrap(), assigned_event.event);
+	}
+
+	#[rstest]
+	#[ignore = "require a database"]
+	fn test_cannot_append_duplicate_event_in_same_batch(
+		event_store: Box<dyn EventStore<Contribution>>,
+		contribution_id: ContributionId,
+		creation_event: StorableEvent<Contribution>,
+	) {
+		assert!(
+			event_store
+				.append(
+					&contribution_id,
+					vec![creation_event.clone(), creation_event.clone()]
+				)
+				.is_err()
+		);
+
+		let contribution_events = event_store.list_by_id(&contribution_id).unwrap();
+		assert_eq!(contribution_events.len(), 0);
+	}
+
+	#[rstest]
+	#[ignore = "require a database"]
+	fn test_cannot_append_duplicate_event_in_different_batches(
+		event_store: Box<dyn EventStore<Contribution>>,
+		contribution_id: ContributionId,
+		creation_event: StorableEvent<Contribution>,
+	) {
+		assert!(event_store.append(&contribution_id, vec![creation_event.clone()]).is_ok());
+		assert!(event_store.append(&contribution_id, vec![creation_event.clone()]).is_err());
 	}
 }
