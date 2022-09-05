75138030aa91df753d654b8cd430e8b96a9ad3e4
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-05 16:46:58 +0200

    logs(starknet): improve logs

--- marketplace-domain/src/actions.rs
+++ marketplace-domain/src/actions.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::*;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -17,4 +19,32 @@ pub enum Action {
 	ValidateContribution {
 		contribution_id: ContributionId,
 	},
+}
+
+impl Display for Action {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			f,
+			"{}",
+			match self {
+				Action::CreateContribution {
+					project_id,
+					issue_number,
+					gate,
+				} => format!(
+					"Create contribution for issue {issue_number} of project {project_id} with gate {gate}."
+				),
+				Action::AssignContributor {
+					contribution_id,
+					contributor_id,
+				} => format!(
+					"Assign contributor {contributor_id} to contribution {contribution_id}."
+				),
+				Action::UnassignContributor { contribution_id } =>
+					format!("Unassign contributor from contribution {contribution_id}."),
+				Action::ValidateContribution { contribution_id } =>
+					format!("Validate contribution {contribution_id}."),
+			}
+		)
+	}
 }
