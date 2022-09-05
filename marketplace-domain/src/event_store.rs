e88c5775497439cc3ded605d56634219d3b25bf6
Anthony Buisset <abuisset@gmail.com>
2022-09-05 15:07:53 +0200

    ✨ event_store can list all events

--- marketplace-domain/src/event_store.rs
+++ marketplace-domain/src/event_store.rs
@@ -18,4 +18,5 @@ pub enum Error {
 pub trait Store<A: Aggregate>: Send + Sync {
 	fn append(&self, aggregate_id: &A::Id, events: Vec<StorableEvent<A>>) -> Result<(), Error>;
 	fn list_by_id(&self, aggregate_id: &A::Id) -> Result<Vec<A::Event>, Error>;
+	fn list(&self) -> Result<Vec<A::Event>, Error>;
 }
