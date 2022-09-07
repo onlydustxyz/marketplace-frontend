2afea3a3a1081d5ffb42c9102fa383a74e1221cf
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-07 11:29:33 +0200

    feat: add location to indexer logs

--- marketplace-infrastructure/src/github/errors.rs
+++ marketplace-infrastructure/src/github/errors.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
 	#[error("Failed to interact with Github")]
-	Octocrab(#[from] octocrab::Error),
+	Octocrab(#[from] anyhow::Error),
 	#[error("Timeout sending request to GitHub API")]
 	Timeout,
 }
