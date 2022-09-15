d1b5e1084173cc2c2c58ba62fcb9dc85e473242e
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-15 15:36:22 +0200

    refactor: Move EventWebHook logic in a separated function

--- marketplace-infrastructure/src/event_webhook/tests.rs
+++ marketplace-infrastructure/src/event_webhook/tests.rs
@@ -1,65 +1,48 @@
 use std::ffi::OsString;
 
 use super::*;
+use assert_matches::assert_matches;
 use envtestkit::{
 	lock::{lock_read, lock_test},
 	set_env,
 };
-use log::Level;
 use marketplace_domain::ContributionEvent;
 use mockito;
 
 #[allow(clippy::await_holding_lock)]
 #[tokio::test]
 async fn env_variable_not_set() {
-	testing_logger::setup();
 	let _lock = lock_read();
-	let webhook = EventWebHook::new(reqwest::Client::new());
 
 	let event = Event::Contribution(ContributionEvent::Validated {
 		id: Default::default(),
 	});
-	webhook.on_event(&event).await;
 
-	testing_logger::validate(|captured_logs| {
-		assert_eq!(
-			captured_logs[0].body,
-			format!(
-				"Event webhook ignored: environment variable '{WEBHOOK_TARGET_ENV_VAR}' is not set"
-			)
-		);
-		assert_eq!(captured_logs[0].level, Level::Info);
-	});
+	assert_matches!(
+		send_event_to_webhook(&reqwest::Client::new(), &event).await,
+		Err(Error::EnvVarNotSet(_))
+	);
 }
 
 #[allow(clippy::await_holding_lock)]
 #[tokio::test]
 async fn env_variable_invalid() {
-	testing_logger::setup();
 	let _lock = lock_test();
 	let _test = set_env(OsString::from(WEBHOOK_TARGET_ENV_VAR), "Some random junk");
-	let webhook = EventWebHook::new(reqwest::Client::new());
 
 	let event = Event::Contribution(ContributionEvent::Validated {
 		id: Default::default(),
 	});
-	webhook.on_event(&event).await;
 
-	testing_logger::validate(|captured_logs| {
-		assert_eq!(
-			captured_logs[0].body,
-			format!(
-				"Failed to parse environment variable '{WEBHOOK_TARGET_ENV_VAR}' content to Url: relative URL without a base"
-			)
-		);
-		assert_eq!(captured_logs[0].level, Level::Error);
-	});
+	assert_matches!(
+		send_event_to_webhook(&reqwest::Client::new(), &event).await,
+		Err(Error::InvalidEnvVar(_))
+	);
 }
 
 #[allow(clippy::await_holding_lock)]
 #[tokio::test]
 async fn http_call_fail() {
-	testing_logger::setup();
 	let server_url = mockito::server_url();
 	let _m = mockito::mock("POST", "/webhook").with_status(400).expect(1).create();
 
@@ -69,23 +52,14 @@ async fn http_call_fail() {
 	let _lock = lock_test();
 	let _test = set_env(OsString::from(WEBHOOK_TARGET_ENV_VAR), &target_url);
 
-	let webhook = EventWebHook::new(reqwest::Client::new());
-
 	let event = Event::Contribution(ContributionEvent::Validated {
 		id: Default::default(),
 	});
-	webhook.on_event(&event).await;
+
+	assert_matches!(
+		send_event_to_webhook(&reqwest::Client::new(), &event).await,
+		Err(Error::RespondWithErrorStatusCode(_))
+	);
 
 	assert!(_m.matched());
-
-	testing_logger::validate(|captured_logs| {
-		assert!(
-			captured_logs
-				.last()
-				.unwrap()
-				.body
-				.starts_with("WebHook target failed to process event:")
-		);
-		assert_eq!(captured_logs.last().unwrap().level, Level::Error);
-	});
 }
