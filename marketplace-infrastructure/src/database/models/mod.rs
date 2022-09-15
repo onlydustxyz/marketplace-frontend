db6516a5e3a093fa107dd9fd09ad81db9800f31d
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-15 16:43:12 +0200

    CI: run actions with stable pined rust version

--- marketplace-infrastructure/src/database/models/mod.rs
+++ marketplace-infrastructure/src/database/models/mod.rs
@@ -1,20 +1,27 @@
+#[allow(clippy::extra_unused_lifetimes)]
 mod contributions;
 pub use contributions::*;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod projects;
 pub use projects::*;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod applications;
 pub use applications::*;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod contact_informations;
 pub use contact_informations::*;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod events;
 pub use events::*;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod project_members;
 pub use project_members::ProjectMember;
 
+#[allow(clippy::extra_unused_lifetimes)]
 mod contributors;
 pub use contributors::*;
