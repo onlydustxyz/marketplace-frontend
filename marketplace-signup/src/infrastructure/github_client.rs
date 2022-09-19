5f632a7a808ea640faf2e85462b1ecbbf930d568
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-09-19 11:27:59 +0200

    🎨 format code with cargo nightly

--- marketplace-signup/src/infrastructure/github_client.rs
+++ marketplace-signup/src/infrastructure/github_client.rs
@@ -1,201 +1,200 @@
 use rocket::serde::{Deserialize, Serialize};
 
 use crate::domain::{
-    errors::AuthenticationError,
-    errors::IdentificationError,
-    services::identity_provider::IdentityProvider,
-    value_objects::{AccessToken, Identity},
+	errors::{AuthenticationError, IdentificationError},
+	services::identity_provider::IdentityProvider,
+	value_objects::{AccessToken, Identity},
 };
 
 const USER_AGENT: &str = "od-marketplace-signup";
 
 pub struct GitHubClient {
-    http_client: reqwest::Client,
+	http_client: reqwest::Client,
 
-    access_token_url: String,
-    user_api_url: String,
+	access_token_url: String,
+	user_api_url: String,
 
-    github_id: String,
-    github_secret: String,
+	github_id: String,
+	github_secret: String,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct AccessTokenRequestBody<'r> {
-    client_id: &'r str,
-    client_secret: &'r str,
-    code: &'r str,
+	client_id: &'r str,
+	client_secret: &'r str,
+	code: &'r str,
 }
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct AccessTokenResponseBody {
-    access_token: String,
+	access_token: String,
 }
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct UserResponseBody {
-    id: u64,
+	id: u64,
 }
 
 impl GitHubClient {
-    pub fn new(
-        github_id: String,
-        github_secret: String,
-        access_token_url: String,
-        user_api_url: String,
-    ) -> Self {
-        GitHubClient {
-            http_client: reqwest::Client::new(),
-            access_token_url,
-            user_api_url,
-            github_id,
-            github_secret,
-        }
-    }
+	pub fn new(
+		github_id: String,
+		github_secret: String,
+		access_token_url: String,
+		user_api_url: String,
+	) -> Self {
+		GitHubClient {
+			http_client: reqwest::Client::new(),
+			access_token_url,
+			user_api_url,
+			github_id,
+			github_secret,
+		}
+	}
 }
 
 #[rocket::async_trait]
 impl IdentityProvider for GitHubClient {
-    async fn new_access_token(
-        &self,
-        authorization_code: &str,
-    ) -> Result<AccessToken, AuthenticationError> {
-        let request_body = AccessTokenRequestBody {
-            client_id: &self.github_id,
-            client_secret: &self.github_secret,
-            code: authorization_code,
-        };
+	async fn new_access_token(
+		&self,
+		authorization_code: &str,
+	) -> Result<AccessToken, AuthenticationError> {
+		let request_body = AccessTokenRequestBody {
+			client_id: &self.github_id,
+			client_secret: &self.github_secret,
+			code: authorization_code,
+		};
 
-        let response = self
-            .http_client
-            .post(&self.access_token_url)
-            .json(&request_body)
-            .header(reqwest::header::ACCEPT, "application/json")
-            .header(reqwest::header::USER_AGENT, USER_AGENT)
-            .send()
-            .await
-            .map_err(|e| AuthenticationError::Http(Box::new(e)))?
-            .error_for_status()
-            .map_err(|e| AuthenticationError::Http(Box::new(e)))?;
+		let response = self
+			.http_client
+			.post(&self.access_token_url)
+			.json(&request_body)
+			.header(reqwest::header::ACCEPT, "application/json")
+			.header(reqwest::header::USER_AGENT, USER_AGENT)
+			.send()
+			.await
+			.map_err(|e| AuthenticationError::Http(Box::new(e)))?
+			.error_for_status()
+			.map_err(|e| AuthenticationError::Http(Box::new(e)))?;
 
-        let response = response
-            .json::<AccessTokenResponseBody>()
-            .await
-            .map_err(|e| AuthenticationError::Serde(Box::new(e)))?;
+		let response = response
+			.json::<AccessTokenResponseBody>()
+			.await
+			.map_err(|e| AuthenticationError::Serde(Box::new(e)))?;
 
-        Ok(AccessToken::from(response.access_token))
-    }
+		Ok(AccessToken::from(response.access_token))
+	}
 
-    async fn get_user_id(
-        &self,
-        access_token: &AccessToken,
-    ) -> Result<Identity, IdentificationError> {
-        let response = self
-            .http_client
-            .get(&self.user_api_url)
-            .header(reqwest::header::ACCEPT, "application/json")
-            .header(reqwest::header::USER_AGENT, USER_AGENT)
-            .header(
-                reqwest::header::AUTHORIZATION,
-                format!("token {}", access_token),
-            )
-            .send()
-            .await
-            .map_err(|e| IdentificationError::Http(Box::new(e)))?
-            .error_for_status()
-            .map_err(|e| IdentificationError::Http(Box::new(e)))?;
+	async fn get_user_id(
+		&self,
+		access_token: &AccessToken,
+	) -> Result<Identity, IdentificationError> {
+		let response = self
+			.http_client
+			.get(&self.user_api_url)
+			.header(reqwest::header::ACCEPT, "application/json")
+			.header(reqwest::header::USER_AGENT, USER_AGENT)
+			.header(
+				reqwest::header::AUTHORIZATION,
+				format!("token {}", access_token),
+			)
+			.send()
+			.await
+			.map_err(|e| IdentificationError::Http(Box::new(e)))?
+			.error_for_status()
+			.map_err(|e| IdentificationError::Http(Box::new(e)))?;
 
-        let response = response
-            .json::<UserResponseBody>()
-            .await
-            .map_err(|e| IdentificationError::Serde(Box::new(e)))?;
+		let response = response
+			.json::<UserResponseBody>()
+			.await
+			.map_err(|e| IdentificationError::Serde(Box::new(e)))?;
 
-        Ok(Identity::GitHubId(response.id.into()))
-    }
+		Ok(Identity::GitHubId(response.id.into()))
+	}
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::domain::{
-        services::identity_provider::IdentityProvider,
-        value_objects::{AccessToken, Identity},
-    };
+	use crate::domain::{
+		services::identity_provider::IdentityProvider,
+		value_objects::{AccessToken, Identity},
+	};
 
-    use super::GitHubClient;
-    use claim::*;
-    use httpmock::prelude::*;
-    use rocket::{serde::json::serde_json, tokio};
-    use serde_json::json;
+	use super::GitHubClient;
+	use claim::*;
+	use httpmock::prelude::*;
+	use rocket::{serde::json::serde_json, tokio};
+	use serde_json::json;
 
-    #[tokio::test]
-    async fn new_access_token() {
-        // Start a server running on a local ephemeral port.
-        let server = MockServer::start();
+	#[tokio::test]
+	async fn new_access_token() {
+		// Start a server running on a local ephemeral port.
+		let server = MockServer::start();
 
-        let github_client = GitHubClient::new(
-            "foo-github-id".into(),
-            "foo-github-secret".into(),
-            server.url("/login/oauth/access_token"),
-            "".into(),
-        );
+		let github_client = GitHubClient::new(
+			"foo-github-id".into(),
+			"foo-github-secret".into(),
+			server.url("/login/oauth/access_token"),
+			"".into(),
+		);
 
-        let github_mock = server.mock(|when, then| {
-            when.method(POST)
-                .path("/login/oauth/access_token")
-                .header("Accept", "application/json")
-                .json_body(json!({
-                    "client_id": "foo-github-id",
-                    "client_secret": "foo-github-secret",
-                    "code": "foo-code",
-                }));
-            then.status(200).json_body(json!({
-                "access_token":"gho_16C7e42F292c6912E7710c838347Ae178B4a",
-                "scope":"repo,gist",
-                "token_type":"bearer",
-            }));
-        });
+		let github_mock = server.mock(|when, then| {
+			when.method(POST)
+				.path("/login/oauth/access_token")
+				.header("Accept", "application/json")
+				.json_body(json!({
+					"client_id": "foo-github-id",
+					"client_secret": "foo-github-secret",
+					"code": "foo-code",
+				}));
+			then.status(200).json_body(json!({
+				"access_token":"gho_16C7e42F292c6912E7710c838347Ae178B4a",
+				"scope":"repo,gist",
+				"token_type":"bearer",
+			}));
+		});
 
-        let result = github_client.new_access_token("foo-code").await;
+		let result = github_client.new_access_token("foo-code").await;
 
-        github_mock.assert();
-        assert!(result.is_ok());
-        assert_eq!(
-            "gho_16C7e42F292c6912E7710c838347Ae178B4a".to_string(),
-            result.unwrap().to_string()
-        );
-    }
+		github_mock.assert();
+		assert!(result.is_ok());
+		assert_eq!(
+			"gho_16C7e42F292c6912E7710c838347Ae178B4a".to_string(),
+			result.unwrap().to_string()
+		);
+	}
 
-    #[tokio::test]
-    async fn get_user_id() {
-        // Start a server running on a local ephemeral port.
-        let server = MockServer::start();
+	#[tokio::test]
+	async fn get_user_id() {
+		// Start a server running on a local ephemeral port.
+		let server = MockServer::start();
 
-        let github_client = GitHubClient::new(
-            "foo-github-id".into(),
-            "foo-github-secret".into(),
-            "".into(),
-            server.url("/user"),
-        );
+		let github_client = GitHubClient::new(
+			"foo-github-id".into(),
+			"foo-github-secret".into(),
+			"".into(),
+			server.url("/user"),
+		);
 
-        let github_mock = server.mock(|when, then| {
-            when.method(GET)
-                .path("/user")
-                .header("Accept", "application/json")
-                .header("Authorization", "token foo-access-token");
-            then.status(200).json_body(json!({
-                "login": "octocat",
-                "id": 42,
-                "node_id": "MDQ6VXNlcjE=",
-            }));
-        });
+		let github_mock = server.mock(|when, then| {
+			when.method(GET)
+				.path("/user")
+				.header("Accept", "application/json")
+				.header("Authorization", "token foo-access-token");
+			then.status(200).json_body(json!({
+				"login": "octocat",
+				"id": 42,
+				"node_id": "MDQ6VXNlcjE=",
+			}));
+		});
 
-        let access_token = AccessToken::from("foo-access-token".to_string());
-        let result = github_client.get_user_id(&access_token).await;
+		let access_token = AccessToken::from("foo-access-token".to_string());
+		let result = github_client.get_user_id(&access_token).await;
 
-        github_mock.assert();
-        assert_ok_eq!(result, Identity::GitHubId(42.into()));
-    }
+		github_mock.assert();
+		assert_ok_eq!(result, Identity::GitHubId(42.into()));
+	}
 }
--- marketplace-signup/src/routes/dto/hex_felt.rs
+++ marketplace-signup/src/routes/dto/hex_felt.rs
@@ -9,57 +9,57 @@ use starknet::core::types::FieldElement;
 pub struct HexFieldElement(FieldElement);
 
 impl schemars::JsonSchema for HexFieldElement {
-    fn schema_name() -> String {
-        "HexFieldElement".to_string()
-    }
+	fn schema_name() -> String {
+		"HexFieldElement".to_string()
+	}
 
-    fn json_schema(_gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
-        let schema = SchemaObject {
-            instance_type: Some(InstanceType::String.into()),
-            format: Some("hexadecimal".to_string()),
+	fn json_schema(_gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+		let schema = SchemaObject {
+			instance_type: Some(InstanceType::String.into()),
+			format: Some("hexadecimal".to_string()),
 
-            ..Default::default()
-        };
-        schema.into()
-    }
+			..Default::default()
+		};
+		schema.into()
+	}
 }
 
 impl Serialize for HexFieldElement {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&format!("{:#x}", &self.0))
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		serializer.serialize_str(&format!("{:#x}", &self.0))
+	}
 }
 
 impl<'de> Deserialize<'de> for HexFieldElement {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: rocket::serde::Deserializer<'de>,
-    {
-        let value = String::deserialize(deserializer)?;
-        match FieldElement::from_hex_be(&value) {
-            Ok(value) => Ok(HexFieldElement(value)),
-            Err(err) => Err(de::Error::custom(format!("invalid hex string: {}", err))),
-        }
-    }
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: rocket::serde::Deserializer<'de>,
+	{
+		let value = String::deserialize(deserializer)?;
+		match FieldElement::from_hex_be(&value) {
+			Ok(value) => Ok(HexFieldElement(value)),
+			Err(err) => Err(de::Error::custom(format!("invalid hex string: {}", err))),
+		}
+	}
 }
 
 impl From<FieldElement> for HexFieldElement {
-    fn from(value: FieldElement) -> Self {
-        HexFieldElement(value)
-    }
+	fn from(value: FieldElement) -> Self {
+		HexFieldElement(value)
+	}
 }
 
 impl From<HexFieldElement> for FieldElement {
-    fn from(value: HexFieldElement) -> Self {
-        value.0
-    }
+	fn from(value: HexFieldElement) -> Self {
+		value.0
+	}
 }
 
 impl Display for HexFieldElement {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:#x}", self.0)
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:#x}", self.0)
+	}
 }
