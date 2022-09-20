23f6d3371e8fff0df01480d20de55d40f39232f8
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-20 12:17:51 +0200

    feat(DB): add LeadContributor model

---
+++ marketplace-infrastructure/src/database/models/lead_contributors.rs
@@ -0,0 +1,9 @@
+use crate::database::schema::*;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Serialize, Deserialize, Insertable)]
+#[serde(crate = "rocket::serde")]
+pub struct LeadContributor {
+	pub project_id: String,
+	pub contributor_account: String,
+}
