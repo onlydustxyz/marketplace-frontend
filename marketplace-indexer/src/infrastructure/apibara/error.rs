e2749ec5b78b3e920d867c18baae85529f8a959f
Anthony Buisset <abuisset@gmail.com>
2022-09-20 15:15:16 +0200

    🚚 move apibara client into dedicated module

--- marketplace-indexer/src/infrastructure/apibara/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum Error {
-	#[error(transparent)]
-	Connect(#[from] tonic::transport::Error),
-	#[error(transparent)]
-	Stream(#[from] tonic::Status),
-}
