b31d63d7e98c3ab066ccd2fa4f9b64deb2ae1a0c
Anthony Buisset <abuisset@gmail.com>
2022-09-20 17:18:11 +0200

    ♻️ move message processing logic into a callback to avoid breaking stream

--- marketplace-indexer/src/infrastructure/apibara/starknet/indexing/service.rs
+++ marketplace-indexer/src/infrastructure/apibara/starknet/indexing/service.rs
@@ -6,7 +6,7 @@ use super::{
 use crate::{
 	domain::{BlockchainObserver, EventFilterRepository, IndexingService, IndexingServiceError},
 	infrastructure::apibara::{
-		proto::{connect_response::Message as ResponseMessage, Data},
+		proto::{stream_messages_response::Message as ResponseMessage, Data},
 		ConnectedClient as ApibaraClient,
 	},
 };
@@ -19,28 +19,35 @@ impl<O: BlockchainObserver> IndexingService for ApibaraClient<O> {
 		&self,
 		event_filter_repository: Arc<dyn EventFilterRepository>,
 	) -> Result<(), IndexingServiceError> {
-		loop {
-			let data = self
-				.stream_messages()
-				.await
-				.map_err(anyhow::Error::msg)
-				.map_err(IndexingServiceError::Stream)?;
+		let starting_sequence = 2_000; // TODO: persist indexing state
 
-			match data {
-				ResponseMessage::Data(Data { data, .. }) if data.is_some() => {
-					let block = data.unwrap().try_into_block()?;
-					block.observed(&self.observer).await?;
+		self.stream_messages(starting_sequence, move |data| {
+			let cloned_event_filter_repository = event_filter_repository.clone();
+			async move {
+				match data {
+					ResponseMessage::Data(Data { data, .. }) if data.is_some() => {
+						let block = data.unwrap().try_into_block()?;
+						block.observed(&self.observer).await?;
 
-					for event in block.as_events()?.filtered(event_filter_repository.deref())? {
-						event.observed(&self.observer).await?;
-					}
-				},
+						for event in
+							block.as_events()?.filtered(cloned_event_filter_repository.deref())?
+						{
+							event.observed(&self.observer).await?;
+						}
+					},
 
-				ResponseMessage::Invalidate(invalidate) =>
-					invalidate.observed(&self.observer).await?,
+					ResponseMessage::Invalidate(invalidate) =>
+						invalidate.observed(&self.observer).await?,
 
-				_ => (),
-			};
-		}
+					_ => (),
+				};
+				Ok(())
+			}
+		})
+		.await
+		.map_err(anyhow::Error::msg)
+		.map_err(IndexingServiceError::Stream)?;
+
+		Ok(())
 	}
 }
