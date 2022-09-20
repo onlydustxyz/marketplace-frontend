8e8825b619dc1e60f8937b59e1423c473ac2c70b
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-20 11:00:25 +0200

    feat: add migration to give lead contributor their own table

---
+++ migrations/2022-09-20-084727_give-lead-contributors-their-own-projection-table/down.sql
@@ -0,0 +1,9 @@
+ALTER TABLE project_members
+ADD COLUMN is_lead_contributor BOOLEAN DEFAULT FALSE;
+
+INSERT INTO project_members (project_id, contributor_account, is_lead_contributor)
+(
+    SELECT project_id, contributor_account, TRUE FROM lead_contributors
+);
+
+DROP TABLE lead_contributors;
