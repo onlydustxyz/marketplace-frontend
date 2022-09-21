84afba4bcef1b69877242dce5434646d71a03c8e
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-09-21 15:53:39 +0200

    ✨ implement authenticate_user for Github Client

--- marketplace-infrastructure/src/github/github_service.rs
+++ marketplace-infrastructure/src/github/github_service.rs
@@ -1,6 +1,8 @@
 use super::{extract_metadata, Client, OctocrabIssue};
+use ::serde::{Deserialize, Serialize};
 use anyhow::anyhow;
 use async_trait::async_trait;
+use log::error;
 use mapinto::ResultMapInto;
 use marketplace_domain::{
 	GithubClient, GithubClientError, GithubIssue, GithubIssueNumber, GithubProjectId, GithubRepo,
@@ -55,6 +57,100 @@ impl GithubClient for Client {
 			name: user.login,
 		})
 	}
+
+	async fn authenticate_user(
+		&self,
+		authorization_code: String,
+	) -> Result<GithubUserId, GithubClientError> {
+		let access_token = self
+			.new_access_token(&authorization_code)
+			.await
+			.map_err(|e| GithubClientError::Infrastructure(e))?;
+
+		self.get_user_id(access_token)
+			.await
+			.map_err(|e| GithubClientError::Infrastructure(e))
+	}
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct AccessTokenRequestBody<'r> {
+	client_id: &'r str,
+	client_secret: &'r str,
+	code: &'r str,
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct AccessTokenResponseBody {
+	access_token: String,
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct UserResponseBody {
+	id: u64,
+}
+
+const USER_AGENT: &str = "onlydust-marketplace";
+
+impl Client {
+	async fn new_access_token(&self, authorization_code: &str) -> Result<String, anyhow::Error> {
+		let request_body = AccessTokenRequestBody {
+			client_id: &self.github_id,
+			client_secret: &self.github_secret,
+			code: authorization_code,
+		};
+
+		let response = self
+			.http_client
+			.post(&self.access_token_url)
+			.json(&request_body)
+			.header(reqwest::header::ACCEPT, "application/json")
+			.header(reqwest::header::USER_AGENT, USER_AGENT)
+			.send()
+			.await
+			.map_err(anyhow::Error::from)?
+			.error_for_status()
+			.map_err(anyhow::Error::from)?;
+
+		let body_string = response.text().await.map_err(anyhow::Error::from)?;
+
+		let decoded_body: AccessTokenResponseBody =
+			serde_json::from_str(&body_string).map_err(|e| {
+				error!("Failed to deserialize github response {body_string} with error {e}");
+				anyhow::Error::from(e)
+			})?;
+
+		Ok(decoded_body.access_token)
+	}
+
+	async fn get_user_id(&self, access_token: String) -> Result<GithubUserId, anyhow::Error> {
+		let response = self
+			.http_client
+			.get(&self.user_api_url)
+			.header(reqwest::header::ACCEPT, "application/json")
+			.header(reqwest::header::USER_AGENT, USER_AGENT)
+			.header(
+				reqwest::header::AUTHORIZATION,
+				format!("token {}", access_token),
+			)
+			.send()
+			.await
+			.map_err(anyhow::Error::from)?
+			.error_for_status()
+			.map_err(anyhow::Error::from)?;
+
+		let body_string = response.text().await.map_err(anyhow::Error::from)?;
+
+		let decoded_body: UserResponseBody = serde_json::from_str(&body_string).map_err(|e| {
+			error!("Failed to deserialize github response {body_string} with error {e}");
+			anyhow::Error::from(e)
+		})?;
+
+		Ok(decoded_body.id)
+	}
 }
 
 impl From<OctocrabIssue> for GithubIssue {
@@ -75,5 +171,72 @@ impl From<OctocrabIssue> for GithubIssue {
 			context: metadata.context,
 			r#type: metadata.r#type,
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+
+	use super::Client;
+	use claim::*;
+	use marketplace_domain::GithubClient;
+	use rocket::serde::json::serde_json;
+	use serde_json::json;
+	use wiremock::{matchers::*, Mock, MockServer, ResponseTemplate};
+
+	// The following test is performed as component test (component as it uses a "real" ephemeral
+	// local server) instead of end to end tests because doing the real authentication flow would
+	// require a web browser. This test aims to check to requests sent to github are the one
+	// expected by github and responses given by github are well decoded.
+	#[tokio::test]
+	async fn authenticate_user() {
+		// Start a server running on a local ephemeral port.
+		let server = MockServer::start().await;
+
+		std::env::set_var(
+			"GITHUB_ACCESS_TOKEN_URL",
+			format!("{}/login/oauth/access_token", &server.uri()),
+		);
+		std::env::set_var("GITHUB_USER_API_URL", format!("{}/user", &server.uri()));
+		std::env::set_var("GITHUB_ID", "foo-github-id".to_string());
+		std::env::set_var("GITHUB_SECRET", "foo-github-secret".to_string());
+
+		let github_client = Client::new();
+
+		Mock::given(method("POST"))
+			.and(path("/login/oauth/access_token"))
+			.and(header("Accept", "application/json"))
+			.and(body_json(json!({
+				"client_id": "foo-github-id",
+				"client_secret": "foo-github-secret",
+				"code": "foo-code",
+			})))
+			.respond_with(ResponseTemplate::new(200).set_body_json(json!({
+				"access_token":"gho_16C7e42F292c6912E7710c838347Ae178B4a",
+				"scope":"repo,gist",
+				"token_type":"bearer",
+			})))
+			// Mounting the mock on the mock server - it's now effective!
+			.mount(&server)
+			.await;
+
+		Mock::given(method("GET"))
+			.and(path("/user"))
+			.and(header("Accept", "application/json"))
+			.and(header(
+				"Authorization",
+				"token gho_16C7e42F292c6912E7710c838347Ae178B4a",
+			))
+			.respond_with(ResponseTemplate::new(200).set_body_json(json!({
+				"login": "octocat",
+				"id": 42,
+				"node_id": "MDQ6VXNlcjE=",
+			})))
+			// Mounting the mock on the mock server - it's now effective!
+			.mount(&server)
+			.await;
+
+		let result = github_client.authenticate_user("foo-code".to_string()).await;
+		assert_ok_eq!(result, 42u64);
 	}
 }
--- marketplace-infrastructure/src/github/mod.rs
+++ marketplace-infrastructure/src/github/mod.rs
@@ -12,11 +12,35 @@ use errors::Error;
 
 pub struct Client {
 	octo: Arc<octocrab::Octocrab>,
+
+	http_client: reqwest::Client,
+	access_token_url: String,
+	user_api_url: String,
+	github_id: String,
+	github_secret: String,
 }
 
 pub struct OctocrabIssue {
 	pub issue: octocrab::models::issues::Issue,
 	pub project_id: GithubProjectId,
+}
+
+fn access_token_url() -> String {
+	std::env::var("GITHUB_ACCESS_TOKEN_URL")
+		.unwrap_or_else(|_| "https://github.com/login/oauth/access_token".to_string())
+}
+
+fn user_api_url() -> String {
+	std::env::var("GITHUB_USER_API_URL")
+		.unwrap_or_else(|_| "https://api.github.com/user".to_string())
+}
+
+fn github_client_id() -> String {
+	std::env::var("GITHUB_ID").expect("GITHUB_ID environment variable must be set")
+}
+
+fn github_client_secret() -> String {
+	std::env::var("GITHUB_SECRET").expect("GITHUB_SECRET environment variable must be set")
 }
 
 impl Client {
@@ -33,6 +57,11 @@ impl Client {
 	pub fn new() -> Self {
 		Client {
 			octo: octocrab::instance(),
+			http_client: reqwest::Client::new(),
+			access_token_url: access_token_url(),
+			user_api_url: user_api_url(),
+			github_id: github_client_id(),
+			github_secret: github_client_secret(),
 		}
 	}
 
