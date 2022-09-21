7514ba379af5d5f2d7ef78f83dbadc5424500d75
Anthony Buisset <abuisset@gmail.com>
2022-09-21 17:11:50 +0200

    🐛 do not re-process a block after a crash

--- marketplace-infrastructure/src/database/schema.rs
+++ marketplace-infrastructure/src/database/schema.rs
@@ -1,109 +1,109 @@
 table! {
-    applications (id) {
-        id -> Uuid,
-        contribution_id -> Text,
-        contributor_id -> Text,
-        status -> Text,
-        applied_at -> Timestamp,
-    }
+	applications (id) {
+		id -> Uuid,
+		contribution_id -> Text,
+		contributor_id -> Text,
+		status -> Text,
+		applied_at -> Timestamp,
+	}
 }
 
 table! {
-    contact_information (id) {
-        id -> Uuid,
-        contributor_id -> Varchar,
-        discord_handle -> Nullable<Varchar>,
-    }
+	contact_information (id) {
+		id -> Uuid,
+		contributor_id -> Varchar,
+		discord_handle -> Nullable<Varchar>,
+	}
 }
 
 table! {
-    contributions (id) {
-        id -> Text,
-        project_id -> Text,
-        issue_number -> Text,
-        status -> Text,
-        gate -> Int4,
-        contributor_id -> Nullable<Text>,
-        title -> Nullable<Text>,
-        description -> Nullable<Text>,
-        external_link -> Nullable<Text>,
-        difficulty -> Nullable<Text>,
-        technology -> Nullable<Text>,
-        duration -> Nullable<Text>,
-        context -> Nullable<Text>,
-        #[sql_name = "type"]
-        type_ -> Nullable<Text>,
-    }
+	contributions (id) {
+		id -> Text,
+		project_id -> Text,
+		issue_number -> Text,
+		status -> Text,
+		gate -> Int4,
+		contributor_id -> Nullable<Text>,
+		title -> Nullable<Text>,
+		description -> Nullable<Text>,
+		external_link -> Nullable<Text>,
+		difficulty -> Nullable<Text>,
+		technology -> Nullable<Text>,
+		duration -> Nullable<Text>,
+		context -> Nullable<Text>,
+		#[sql_name = "type"]
+		type_ -> Nullable<Text>,
+	}
 }
 
 table! {
-    contributors (id) {
-        id -> Text,
-        account -> Text,
-        github_identifier -> Text,
-        github_username -> Text,
-    }
+	contributors (id) {
+		id -> Text,
+		account -> Text,
+		github_identifier -> Text,
+		github_username -> Text,
+	}
 }
 
 table! {
-    event_deduplications (deduplication_id) {
-        deduplication_id -> Text,
-        event_index -> Int4,
-    }
+	event_deduplications (deduplication_id) {
+		deduplication_id -> Text,
+		event_index -> Int4,
+	}
 }
 
 table! {
-    events (index) {
-        index -> Int4,
-        timestamp -> Timestamp,
-        aggregate_name -> Varchar,
-        aggregate_id -> Varchar,
-        payload -> Jsonb,
-        payload_backup -> Nullable<Jsonb>,
-    }
+	events (index) {
+		index -> Int4,
+		timestamp -> Timestamp,
+		aggregate_name -> Varchar,
+		aggregate_id -> Varchar,
+		payload -> Jsonb,
+		payload_backup -> Nullable<Jsonb>,
+	}
 }
 
 table! {
-    indexers (id) {
-        id -> Text,
-        index_head -> Int8,
-    }
+	indexers (id) {
+		id -> Text,
+		index_head -> Int8,
+	}
 }
 
 table! {
-    lead_contributors (project_id, account) {
-        project_id -> Text,
-        account -> Text,
-    }
+	lead_contributors (project_id, account) {
+		project_id -> Text,
+		account -> Text,
+	}
 }
 
 table! {
-    project_members (project_id, contributor_account) {
-        project_id -> Text,
-        contributor_account -> Text,
-    }
+	project_members (project_id, contributor_account) {
+		project_id -> Text,
+		contributor_account -> Text,
+	}
 }
 
 table! {
-    projects (id) {
-        id -> Varchar,
-        owner -> Varchar,
-        name -> Varchar,
-        url -> Nullable<Varchar>,
-        description -> Nullable<Varchar>,
-        logo_url -> Nullable<Varchar>,
-    }
+	projects (id) {
+		id -> Varchar,
+		owner -> Varchar,
+		name -> Varchar,
+		url -> Nullable<Varchar>,
+		description -> Nullable<Varchar>,
+		logo_url -> Nullable<Varchar>,
+	}
 }
 
 allow_tables_to_appear_in_same_query!(
-    applications,
-    contact_information,
-    contributions,
-    contributors,
-    event_deduplications,
-    events,
-    indexers,
-    lead_contributors,
-    project_members,
-    projects,
+	applications,
+	contact_information,
+	contributions,
+	contributors,
+	event_deduplications,
+	events,
+	indexers,
+	lead_contributors,
+	project_members,
+	projects,
 );
