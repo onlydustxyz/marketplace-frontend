0d1dfc02fb27c1c86aede1435243cdd22c15fb98
Stanislas Bernard <bernardstanislas@gmail.com>
2022-09-22 19:02:22 +0200

    :recycle: remove json logs and parse them in datadog

--- marketplace-infrastructure/src/logger/terminal.rs
+++ marketplace-infrastructure/src/logger/terminal.rs
@@ -1,6 +1,9 @@
 use slog::Drain;
-use slog_term::{CompactFormat, TermDecorator};
+use slog_term::{FullFormat, TermDecorator};
 
 pub fn drain() -> impl Drain<Ok = (), Err = slog::Never> {
-	CompactFormat::new(TermDecorator::new().stderr().build()).build().fuse()
+	FullFormat::new(TermDecorator::new().stdout().build())
+		.use_utc_timestamp()
+		.build()
+		.fuse()
 }
