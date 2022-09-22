b4abc83ccd354e017366db909424b5e702cbf19d
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-09-22 17:04:23 +0200

    ⚰️ remove signup crate

--- marketplace-signup/src/infrastructure/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod github_client;
-mod registry_client;
-pub mod starknet_client;
-
-pub use registry_client::{Signature as StarknetSignature, SignedData as StarknetSignedData};
--- marketplace-signup/src/routes/mod.rs
+++
@@ -1,4 +0,0 @@
-mod registrations;
-pub use registrations::{okapi_add_operation_for_register_github_user_, register_github_user};
-
-mod dto;
