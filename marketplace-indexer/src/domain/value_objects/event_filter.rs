831b517dde6a8f881efe73656e2a533ae2c9645b
Anthony Buisset <abuisset@gmail.com>
2022-09-29 16:33:13 +0200

    ✨ create event filter observer

--- marketplace-indexer/src/domain/value_objects/event_filter.rs
+++ marketplace-indexer/src/domain/value_objects/event_filter.rs
@@ -1,6 +1,7 @@
 use crate::domain::IndexerId;
 use marketplace_domain::ContractAddress;
 
+#[derive(PartialEq, Eq, Debug)]
 pub struct EventFilter {
 	pub indexer_id: IndexerId,
 	pub source_contract: ContractAddress,
