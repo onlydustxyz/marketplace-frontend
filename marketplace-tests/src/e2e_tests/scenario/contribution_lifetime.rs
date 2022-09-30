0964d3b7bde4bbdbf4ffdfc7af33acfcaca7b92e
Anthony Buisset <abuisset@gmail.com>
2022-09-30 18:21:52 +0200

    ⬆️ use latest smart contract dump with new contribution API

--- marketplace-tests/src/e2e_tests/scenario/contribution_lifetime.rs
+++ marketplace-tests/src/e2e_tests/scenario/contribution_lifetime.rs
@@ -30,7 +30,7 @@ async fn contribution_lifetime(
 	add_lead_contributor(&accounts[0], STARKONQUEST_ID, lead_contributor.address()).await;
 	// Create a new contribution
 	contributions::create(lead_contributor, STARKONQUEST_ID, issue_number, 0).await;
-	wait_for_events(events_count + 2).await;
+	wait_for_events(events_count + 3).await;
 
 	let contribution =
 		contributions::get_project_contribution_by_issue_number(STARKONQUEST_TITLE, issue_number)
