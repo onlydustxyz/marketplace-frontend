6f7ed31e8467064d3e7cc923fb061624b8c08fda
Anthony Buisset <abuisset@gmail.com>
2022-10-05 14:16:09 +0200

    🚚 move project projectors into projectors module

--- marketplace-domain/src/project/mod.rs
+++ marketplace-domain/src/project/mod.rs
@@ -4,8 +4,5 @@ pub use projections::{LeadContributorProjection, MemberProjection};
 mod aggregate;
 pub use aggregate::Project as Aggregate;
 
-mod projectors;
-pub use projectors::{LeadContributorProjector, MemberProjector};
-
 mod events;
 pub use events::Event;
