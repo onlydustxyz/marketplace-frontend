31c5e0a23c2971c053194b9c1ab90f89586bfaed
Anthony Buisset <abuisset@gmail.com>
2022-10-11 17:22:55 +0200

    ✨ create event-store event

---
+++ marketplace-event-store/src/event.rs
@@ -0,0 +1,13 @@
+use chrono::NaiveDateTime;
+use marketplace_domain::{Event as DomainEvent, EventOrigin};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Event {
+	pub event: DomainEvent,
+	pub deduplication_id: String,
+	pub timestamp: NaiveDateTime,
+	pub metadata: Value,
+	pub origin: EventOrigin,
+}
