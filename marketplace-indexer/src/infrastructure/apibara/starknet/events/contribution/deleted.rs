0fdc077513cc34d69fdf9b4d14db2177df3c5506
nicolas.ngomai <nicolasn@theodo.fr>
2022-10-11 10:07:07 +0200

    Use contract like id for contributor id in tests

--- marketplace-indexer/src/infrastructure/apibara/starknet/events/contribution/deleted.rs
+++ marketplace-indexer/src/infrastructure/apibara/starknet/events/contribution/deleted.rs
@@ -25,12 +25,14 @@ impl EventTranslator for Deleted {
 mod test {
 	use super::*;
 	use rstest::*;
+	use std::str::FromStr;
 
 	#[fixture]
 	fn apibara_event_data() -> Topics {
 		vec![vec![
 			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-			0, 0, 12,
+			6, 149, 119, 230, 117, 106, 153, 181, 132, 181, 209, 206, 142, 96, 101, 10, 227, 59,
+			110, 43, 19, 84, 23, 131, 69, 130, 104, 240, 125, 166, 179, 138,
 		]]
 		.into()
 	}
@@ -48,7 +50,13 @@ mod test {
 		let result = <Deleted as EventTranslator>::to_domain_event(apibara_event_data);
 		assert!(result.is_ok(), "{}", result.err().unwrap());
 		assert_eq!(
-			Event::Contribution(ContributionEvent::Closed { id: 12.into() },),
+			Event::Contribution(ContributionEvent::Closed {
+				id: HexPrefixedString::from_str(
+					"0x069577e6756a99b584b5d1ce8e60650ae33b6e2b13541783458268f07da6b38a"
+				)
+				.unwrap()
+				.into()
+			},),
 			result.unwrap()
 		);
 	}
