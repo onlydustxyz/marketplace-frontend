b024c6da20829c706f4472f410a1542e9b877f3f
Anthony Buisset <abuisset@gmail.com>
2022-10-15 23:44:59 +0200

    ✅ add unit tests

--- marketplace-domain/src/messaging/destination.rs
+++ marketplace-domain/src/messaging/destination.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 pub enum Destination {
 	Queue(String),
 	Exchange { name: String, topic: String },
@@ -13,5 +14,29 @@ impl Destination {
 			name: name.into(),
 			topic: topic.into(),
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn create_queue_destination() {
+		assert_eq!(
+			Destination::queue("name"),
+			Destination::Queue(String::from("name"))
+		);
+	}
+
+	#[test]
+	fn create_exchange_destination() {
+		assert_eq!(
+			Destination::exchange("name", "topic"),
+			Destination::Exchange {
+				name: String::from("name"),
+				topic: String::from("topic")
+			}
+		);
 	}
 }
