d17adadb3042fe1b517bc614e3f5646bedc34d1b
Anthony Buisset <abuisset@gmail.com>
2022-10-18 11:59:10 +0200

    ✨ return discord_handle in GET /contributor

--- marketplace-core/src/dto/contributor.rs
+++ marketplace-core/src/dto/contributor.rs
@@ -6,18 +6,20 @@ use marketplace_domain as domain;
 #[derive(Serialize, JsonSchema, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Contributor {
 	pub id: String,
+	pub account: String,
 	pub github_identifier: Option<String>,
 	pub github_username: Option<String>,
-	pub account: String,
+	pub discord_handle: Option<String>,
 }
 
 impl From<domain::ContributorProfile> for Contributor {
 	fn from(contributor: domain::ContributorProfile) -> Self {
 		Self {
 			id: contributor.id.to_string(),
+			account: contributor.account.to_string(),
 			github_identifier: contributor.github_identifier.map(|id| id.to_string()),
 			github_username: contributor.github_username,
-			account: contributor.account.to_string(),
+			discord_handle: contributor.discord_handle,
 		}
 	}
 }
--- marketplace-tests/src/e2e_tests/contributors/mod.rs
+++ marketplace-tests/src/e2e_tests/contributors/mod.rs
@@ -3,3 +3,6 @@ pub use get::{get, get_by_account};
 
 mod signup;
 pub use signup::signup;
+
+mod discord;
+pub use discord::register_discord_handle;
