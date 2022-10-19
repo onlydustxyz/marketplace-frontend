2939c94b3d26f3408a4b495fce68f98c1be4fc6f
Anthony Buisset <abuisset@gmail.com>
2022-10-19 12:23:05 +0200

    🚚 rename contributor_id to contributor_account_address in apply route

--- marketplace-tests/src/e2e_tests/contributions/apply.rs
+++ marketplace-tests/src/e2e_tests/contributions/apply.rs
@@ -2,12 +2,14 @@ use crate::e2e_tests::http::*;
 use reqwest::StatusCode;
 use serde_json::json;
 
-pub async fn apply(contribution_id: &str, contributor_id: &str) {
-	println!("Applying to contribution {contribution_id} for contributor {contributor_id}");
+pub async fn apply(contribution_id: &str, contributor_account_address: &str) {
+	println!(
+		"Applying to contribution {contribution_id} for contributor {contributor_account_address}"
+	);
 	let response = post(
 		format!("{BACKEND_BASE_URI}/contributions/{contribution_id}/applications"),
 		Some(json!({
-		  "contributor_id": contributor_id,
+		  "contributor_account_address": contributor_account_address,
 		})),
 	)
 	.await;
