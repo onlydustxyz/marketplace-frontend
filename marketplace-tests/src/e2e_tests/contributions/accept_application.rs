2cc51fe7da99c6ef5aefd5d88649e55253b81ab4
Anthony Buisset <abuisset@gmail.com>
2022-10-19 12:20:13 +0200

    🚚 rename contributor_id to contributor_account_address in accept_application

--- marketplace-tests/src/e2e_tests/contributions/accept_application.rs
+++ marketplace-tests/src/e2e_tests/contributions/accept_application.rs
@@ -3,14 +3,14 @@ use crate::e2e_tests::starknet::{Account, ContractAdministrator, ContributionsCo
 pub async fn assign_contributor_to_contribution(
 	account: &Account,
 	contribution_id: &str,
-	contributor_id: &str,
+	contributor_account_address: &str,
 ) {
 	println!(
-		"Accepting application to contribution {contribution_id} for contributor {contributor_id}"
+		"Accepting application to contribution {contribution_id} for contributor {contributor_account_address}"
 	);
 	let contributions_contract = ContractAdministrator::new(account);
 	contributions_contract
-		.assign_contributor_to_contribution(contribution_id, contributor_id)
+		.assign_contributor_to_contribution(contribution_id, contributor_account_address)
 		.await
 		.expect("Unable to create a contribution");
 }
