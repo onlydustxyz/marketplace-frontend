7289515ffe03568630d57642f2ff384cf05c0d9e
Stanislas Bernard <bernardstanislas@gmail.com>
2022-10-23 16:46:34 +0200

    :recycle: Remove unused code

--- marketplace-core/src/datadog_event_format.rs
+++ marketplace-core/src/datadog_event_format.rs
@@ -4,7 +4,7 @@ use tracing_opentelemetry::OtelData;
 use tracing_subscriber::{
 	fmt::{
 		format::{self, FormatEvent, FormatFields},
-		FmtContext, FormattedFields,
+		FmtContext,
 	},
 	registry::{LookupSpan, SpanRef},
 };
@@ -25,27 +25,6 @@ where
 		// Format values from the event's's metadata:
 		let metadata = event.metadata();
 		write!(&mut writer, "{} {}: ", metadata.level(), metadata.target())?;
-
-		// Format all the spans in the event's span context.
-		if let Some(scope) = ctx.event_scope() {
-			for span in scope.from_root() {
-				write!(writer, "{}", span.name())?;
-
-				// `FormattedFields` is a formatted representation of the span's
-				// fields, which is stored in its extensions by the `fmt` layer's
-				// `new_span` method. The fields will have been formatted
-				// by the same field formatter that's provided to the event
-				// formatter in the `FmtContext`.
-				let ext = span.extensions();
-				let fields = &ext.get::<FormattedFields<N>>().expect("will never be `None`");
-
-				// Skip formatting the fields if the span had no fields.
-				if !fields.is_empty() {
-					write!(writer, "{{{}}}", fields)?;
-				}
-				write!(writer, ": ")?;
-			}
-		}
 
 		// Write fields on the event
 		ctx.field_format().format_fields(writer.by_ref(), event)?;
--- marketplace-core/src/main.rs
+++ marketplace-core/src/main.rs
@@ -2,25 +2,8 @@ mod datadog_event_format;
 mod marketplace_tracing;
 use anyhow::Result;
 use dotenv::dotenv;
-use marketplace_infrastructure::logger;
-use slog::{Drain, Level, Logger};
-use std::sync::Arc;
 
-use crate::marketplace_tracing::{get_span_id, get_trace_id, get_tracing_id, get_tracing_span_id};
-
-#[macro_use]
 extern crate rocket;
-
-fn create_root_logger() -> Logger {
-	let drain = logger::default_drain()
-		.filter(|record| {
-			!(record.level() == Level::Error
-				&& record.msg().to_string().starts_with("No matching routes for"))
-		})
-		.fuse();
-
-	logger::create_root_logger(drain)
-}
 
 #[tracing::instrument]
 fn foo(bar: String) -> String {
