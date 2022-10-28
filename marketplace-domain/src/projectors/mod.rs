f3afe8d4d102277b843b8ebb4761985b259c0ef2
Anthony Buisset <abuisset@gmail.com>
2022-10-28 15:19:59 +0100

    🚚 move contribution projectors in domain root

--- marketplace-domain/src/projectors/mod.rs
+++ marketplace-domain/src/projectors/mod.rs
@@ -6,3 +6,12 @@ pub use project_lead_contributors::LeadContributorProjector;
 
 mod project_members;
 pub use project_members::MemberProjector;
+
+mod github_contribution;
+pub use github_contribution::GithubContributionProjector;
+
+mod application;
+pub use application::ApplicationProjector;
+
+mod github_project;
+pub use github_project::GithubProjectProjector;
