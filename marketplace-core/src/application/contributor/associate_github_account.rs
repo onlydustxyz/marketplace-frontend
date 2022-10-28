0697a047c24a726f0ce582e9a6aa2cc364caef54
Anthony Buisset <abuisset@gmail.com>
2022-10-28 18:50:52 +0100

    ♻ publish events in associate github account usecase

--- marketplace-core/src/application/contributor/associate_github_account.rs
+++ marketplace-core/src/application/contributor/associate_github_account.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 use chrono::Utc;
 use marketplace_domain::{Error as DomainError, *};
+use marketplace_event_store::{bus::QUEUE_NAME as EVENT_STORE_QUEUE, Event as StorableEvent};
 use std::sync::Arc;
 
 // Usecase must be `Send` and `Sync` as it is managed in a rocket State<T> that requires T to be
@@ -16,7 +17,7 @@ pub trait Usecase<S: Clone + Send + Sync>: Send + Sync {
 }
 
 pub struct AssociateGithubAccount<S: Clone + Send + Sync> {
-	event_store: Arc<dyn EventStore<Contributor>>,
+	event_publisher: Arc<dyn Publisher<StorableEvent>>,
 	account_verifier: Arc<dyn OnChainAccountVerifier<SignedData = S>>,
 	github_client: Arc<dyn GithubClient>,
 	contributor_projector: Arc<ContributorWithGithubDataProjector>,
@@ -25,14 +26,14 @@ pub struct AssociateGithubAccount<S: Clone + Send + Sync> {
 
 impl<S: Clone + Send + Sync> AssociateGithubAccount<S> {
 	pub fn new(
-		event_store: Arc<dyn EventStore<Contributor>>,
+		event_publisher: Arc<dyn Publisher<StorableEvent>>,
 		account_verifier: Arc<dyn OnChainAccountVerifier<SignedData = S>>,
 		github_client: Arc<dyn GithubClient>,
 		contributor_projector: Arc<ContributorWithGithubDataProjector>,
 		uuid_generator: Arc<dyn UuidGenerator>,
 	) -> Self {
 		Self {
-			event_store,
+			event_publisher,
 			account_verifier,
 			github_client,
 			contributor_projector,
@@ -43,14 +44,14 @@ impl<S: Clone + Send + Sync> AssociateGithubAccount<S> {
 
 impl<S: Clone + Send + Sync + 'static> AssociateGithubAccount<S> {
 	pub fn new_usecase_boxed(
-		event_store: Arc<dyn EventStore<Contributor>>,
+		event_publisher: Arc<dyn Publisher<StorableEvent>>,
 		account_verifier: Arc<dyn OnChainAccountVerifier<SignedData = S>>,
 		github_client: Arc<dyn GithubClient>,
 		contributor_projector: Arc<ContributorWithGithubDataProjector>,
 		uuid_generator: Arc<dyn UuidGenerator>,
 	) -> Box<dyn Usecase<S>> {
 		Box::new(Self::new(
-			event_store,
+			event_publisher,
 			account_verifier,
 			github_client,
 			contributor_projector,
@@ -75,13 +76,13 @@ impl<S: Clone + Send + Sync> Usecase<S> for AssociateGithubAccount<S> {
 			signed_data,
 		)
 		.await?;
-		let storable_events: Vec<StorableEvent<Contributor>> = events
+		let storable_events: Vec<StorableEvent> = events
 			.iter()
 			.map(|event| {
 				if let Event::Contributor(contributor_event) = event {
 					StorableEvent {
 						deduplication_id: self.uuid_generator.new_uuid().to_string(),
-						event: contributor_event.clone(),
+						event: contributor_event.clone().into(),
 						timestamp: Utc::now().naive_utc(),
 						origin: EventOrigin::BACKEND,
 						metadata: Default::default(),
@@ -91,7 +92,12 @@ impl<S: Clone + Send + Sync> Usecase<S> for AssociateGithubAccount<S> {
 				}
 			})
 			.collect();
-		self.event_store.append(&contributor_account_address, storable_events)?;
+		self.event_publisher
+			.publish_many(
+				Destination::Queue(EVENT_STORE_QUEUE.into()),
+				&storable_events,
+			)
+			.await?;
 		// TODO: the usecase shouldn't know about the projectors, it should just push the events to
 		// a bus
 		for event in &events {
