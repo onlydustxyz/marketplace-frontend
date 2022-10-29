83a703f295acc71b962aa92b86e0b927f56d1faa
Anthony Buisset <abuisset@gmail.com>
2022-10-29 20:01:19 +0100

    ♻️ move applications refresh tests to e2e

--- marketplace-core/src/application/contribution/refresh.rs
+++ marketplace-core/src/application/contribution/refresh.rs
@@ -4,6 +4,3 @@ use marketplace_domain::*;
 pub type RefreshContributions = Refresh<ContributionProjection>;
 pub type RefreshApplications = Refresh<ApplicationProjection>;
 pub type RefreshProjects = Refresh<ProjectProjection>;
-
-#[cfg(test)]
-mod tests;
--- marketplace-tests/src/e2e_tests/applications/mod.rs
+++ marketplace-tests/src/e2e_tests/applications/mod.rs
@@ -1,2 +1,5 @@
 mod list_for_contributor;
 pub use list_for_contributor::list_for_contributor;
+
+mod refresh;
+pub use refresh::refresh;
