f8eeaef009d4379353f344f92451b101aff29b43
Anthony Buisset <abuisset@gmail.com>
2022-11-03 15:58:32 +0100

    ✨ create module to subscribe a projector

--- marketplace-core/src/event_listeners/mod.rs
+++ marketplace-core/src/event_listeners/mod.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use marketplace_infrastructure::event_bus;
 
 mod logger;
+mod projector;
 
 pub async fn main() -> Result<()> {
 	logger::spawn(event_bus::consumer().await?).await?;
