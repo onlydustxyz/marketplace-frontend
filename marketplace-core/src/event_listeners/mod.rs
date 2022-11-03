339ece74cb34425605761224b3ad3880f6331c96
Anthony Buisset <abuisset@gmail.com>
2022-11-03 16:57:56 +0100

    ✨ subscribe all other projectors

--- marketplace-core/src/event_listeners/mod.rs
+++ marketplace-core/src/event_listeners/mod.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
 use futures::future::join_all;
-use marketplace_domain::ProjectMemberProjector;
+use marketplace_domain::{
+	ApplicationProjector, ContributorWithGithubDataProjector, EventListener,
+	GithubContributionProjector, GithubProjectProjector, LeadContributorProjector,
+	ProjectMemberProjector,
+};
 use marketplace_infrastructure::{
+	amqp::ConsumableBus,
 	database::{self, init_pool},
 	event_bus,
+	event_webhook::EventWebHook,
+	github,
 };
 use std::sync::Arc;
 use tokio::task::JoinHandle;
@@ -12,9 +19,7 @@ mod logger;
 mod projector;
 
 pub async fn main() -> Result<()> {
-	let database = Arc::new(database::Client::new(init_pool()?));
-
-	join_all(spawn_listeners(database).await?)
+	join_all(spawn_listeners().await?)
 		.await
 		.into_iter()
 		.collect::<Result<Vec<()>, _>>()?;
@@ -22,11 +27,25 @@ pub async fn main() -> Result<()> {
 	Ok(())
 }
 
-async fn spawn_listeners(database: Arc<database::Client>) -> Result<Vec<JoinHandle<()>>> {
+async fn spawn_listeners() -> Result<Vec<JoinHandle<()>>> {
+	let database = Arc::new(database::Client::new(init_pool()?));
+	let github = Arc::new(github::Client::new());
+	let reqwest_client = reqwest::Client::new();
+
 	let handles = [
 		logger::spawn(event_bus::consumer().await?),
-		ProjectMemberProjector::new(database).spawn(event_bus::consumer().await?),
+		ProjectMemberProjector::new(database.clone()).spawn(event_bus::consumer().await?),
+		GithubContributionProjector::new(database.clone(), github.clone())
+			.spawn(event_bus::consumer().await?),
+		ApplicationProjector::new(database.clone()).spawn(event_bus::consumer().await?),
+		GithubProjectProjector::new(github.clone(), database.clone())
+			.spawn(event_bus::consumer().await?),
+		ContributorWithGithubDataProjector::new(github, database.clone())
+			.spawn(event_bus::consumer().await?),
+		LeadContributorProjector::new(database.clone()).spawn(event_bus::consumer().await?),
+		EventWebHook::new(reqwest_client).spawn(event_bus::consumer().await?),
 	];
+
 	Ok(handles.into())
 }
 
