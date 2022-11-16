2d7cc89ed785bb416f9a76b5855d556842ec0eba
Anthony Buisset <abuisset@gmail.com>
2022-11-16 13:56:30 +0100

    🐛 make sure logger does not make the process fail

--- event-listeners/src/presentation/listeners/logger.rs
+++ event-listeners/src/presentation/listeners/logger.rs
@@ -1,15 +1,19 @@
 use async_trait::async_trait;
 use marketplace_domain::{Event, EventListener};
-use tracing::info;
+use tracing::{error, info};
 
 pub struct Logger;
 
 #[async_trait]
 impl EventListener for Logger {
 	async fn on_event(&self, event: &Event) {
-		info!(
-			"[events] 📨 Received event: {}",
-			&serde_json::to_string_pretty(&event).expect("Unable to format event")
-		);
+		match serde_json::to_string_pretty(&event) {
+			Ok(event) => info!("[events] 📨 Received event: {event}",),
+			Err(error) => error!(
+				error = error.to_string(),
+				event = format!("{:?}", event),
+				"Unable to format event"
+			),
+		}
 	}
 }
