db02b7b5a650aeb9038da9b7c62b550db803f82d
Stanislas Bernard <bernardstanislas@gmail.com>
2022-11-17 17:50:45 +0100

    :card_file_box: Remove foreign keys to auth schema

--- migrations/2022-11-16-164651_remove_our_users/up.sql
+++ migrations/2022-11-16-164651_remove_our_users/up.sql
@@ -3,20 +3,11 @@ BEGIN;
 ALTER TABLE project_leads
     DROP CONSTRAINT project_leads_user_id_fkey;
 
-ALTER TABLE project_leads
-    ADD CONSTRAINT project_leads_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id);
-
 ALTER TABLE payment_requests
     DROP CONSTRAINT payment_requests_requestor_id_fkey;
 
 ALTER TABLE payment_requests
     DROP CONSTRAINT payment_requests_recipient_id_fkey;
-
-ALTER TABLE payment_requests
-    ADD CONSTRAINT payment_requests_requestor_id_fkey FOREIGN KEY (requestor_id) REFERENCES auth.users(id);
-
-ALTER TABLE payment_requests
-    ADD CONSTRAINT payment_requests_recipient_id_fkey FOREIGN KEY (recipient_id) REFERENCES auth.users(id);
 
 DROP TABLE users;
 
