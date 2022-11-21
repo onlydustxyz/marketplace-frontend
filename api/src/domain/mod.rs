2265326eea3dfb833fa0c60302373ecaa2ca0fff
Anthony Buisset <abuisset@gmail.com>
2022-11-21 14:00:03 +0100

    💡 comment unused aggregate root code until needed

--- api/src/domain/mod.rs
+++ api/src/domain/mod.rs
@@ -1,7 +1,8 @@
 mod event_store;
 pub use self::event_store::{Error as EventStoreError, Store as EventStore};
 
-mod aggregate_root;
-pub use aggregate_root::{
-	AggregateRoot, Error as AggregateRootRepositoryError, Repository as AggregateRootRepository,
-};
+// TODO: Uncomment when needed
+// mod aggregate_root;
+// pub use aggregate_root::{
+// 	AggregateRoot, Error as AggregateRootRepositoryError, Repository as AggregateRootRepository,
+// };
