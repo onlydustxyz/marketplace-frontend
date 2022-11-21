012edfeb4e1a1f014b7becec544e9a128f5d9473
Anthony Buisset <abuisset@gmail.com>
2022-11-21 23:46:28 +0100

    ⚡️ make payment id copyable

--- domain/src/payment/id.rs
+++ domain/src/payment/id.rs
@@ -2,5 +2,7 @@ use derive_more::{Display, From, Into};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize, Display, From, Into)]
+#[derive(
+	Debug, Default, Copy, Clone, PartialEq, Eq, Serialize, Deserialize, Display, From, Into,
+)]
 pub struct Id(Uuid);
