5ad8b66f3d4e0a29629b489cc81d12d52255b512
Anthony Buisset <abuisset@gmail.com>
2022-11-21 09:10:25 +0100

    ⚰️ remove unused github service

--- api/src/routes/dto/to_http_api_problem.rs
+++ api/src/routes/dto/to_http_api_problem.rs
@@ -1,6 +1,5 @@
 use domain::*;
 use http_api_problem::{HttpApiProblem, StatusCode};
-use infrastructure::github::GithubError;
 
 pub(crate) trait ToHttpApiProblem {
 	fn to_http_api_problem(&self) -> HttpApiProblem;
@@ -24,17 +23,5 @@ impl ToHttpApiProblem for EventStoreError {
 		HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
 			.title("Internal error")
 			.detail(self.to_string())
-	}
-}
-
-impl ToHttpApiProblem for GithubError {
-	fn to_http_api_problem(&self) -> HttpApiProblem {
-		match self {
-			GithubError::Octocrab(e) => HttpApiProblem::new(StatusCode::INTERNAL_SERVER_ERROR)
-				.title(self.to_string())
-				.detail(e.to_string()),
-			GithubError::Timeout =>
-				HttpApiProblem::new(StatusCode::REQUEST_TIMEOUT).title(self.to_string()),
-		}
 	}
 }
--- infrastructure/src/github/errors.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum Error {
-	#[error("Failed to interact with Github")]
-	Octocrab(#[from] anyhow::Error),
-	#[error("Timeout sending request to GitHub API")]
-	Timeout,
-}
