786d5161eaa929598f8fe01561e4df0e12704fa4
nicolas.ngomai <nicolasn@theodo.fr>
2022-11-22 13:36:25 +0100

    Add heroku_ext in search path

--- scripts/initdb.d/add-heroku-ext-schema.sql
+++ scripts/initdb.d/add-heroku-ext-schema.sql
@@ -1,4 +1,6 @@
 -- Needed to synchronize data dumped from Heroku
 
-CREATE SCHEMA IF NOT EXISTS heroku_ext;
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+CREATE SCHEMA IF NOT EXISTS heroku_ext;
+
+ALTER DATABASE marketplace_db SET search_path = "$user", public, heroku_ext, auth;
