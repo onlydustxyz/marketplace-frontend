01409fa4697d7f2321d5313eda0eb161ee037e8f
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-11-22 16:29:56 +0100

    ✅ add tests for payment request aggregate

--- domain/src/payment_request/aggregate.rs
+++ domain/src/payment_request/aggregate.rs
@@ -51,3 +51,125 @@ impl PaymentRequest {
 		}])
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{specifications::tests::MockSpecifications, PaymentRequestId, ProjectId, UserId};
+	use mockall::predicate::*;
+	use rstest::{fixture, rstest};
+	use std::{str::FromStr, sync::Arc};
+	use uuid::Uuid;
+	use assert_matches::assert_matches;
+
+	#[fixture]
+	fn payment_request_id() -> PaymentRequestId {
+		Uuid::from_str("00000000-aaaa-495e-9f4c-038ec0ebecb1").unwrap().into()
+	}
+
+	#[fixture]
+	fn project_id() -> ProjectId {
+		Uuid::from_str("11111111-aaaa-495e-9f4c-038ec0ebecb1").unwrap().into()
+	}
+
+	#[fixture]
+	fn wrong_project_id() -> ProjectId {
+		Uuid::from_str("11111111-bbbb-495e-9f4c-038ec0ebecb1").unwrap().into()
+	}
+
+	#[fixture]
+	fn requestor_id() -> UserId {
+		Uuid::from_str("22222222-aaaa-495e-9f4c-038ec0ebecb1").unwrap().into()
+	}
+
+	#[fixture]
+	fn recipient_id() -> UserId {
+		Uuid::from_str("33333333-aaaa-495e-9f4c-038ec0ebecb1").unwrap().into()
+	}
+
+	#[fixture]
+	fn amount_in_usd() -> u32 {
+		420
+	}
+
+	#[fixture]
+	fn reason() -> Value {
+		serde_json::to_value("{}").unwrap()
+	}
+
+	#[rstest]
+	async fn test_create(
+		payment_request_id: PaymentRequestId,
+		project_id: ProjectId,
+		requestor_id: UserId,
+		recipient_id: UserId,
+		amount_in_usd: u32,
+		reason: Value,
+	) {
+		let mut specifications = MockSpecifications::new();
+		specifications
+			.expect_project_exists()
+			.with(eq(project_id))
+			.times(1)
+			.returning(|_| Ok(true));
+
+		let events = PaymentRequest::create(
+			Arc::new(specifications),
+			payment_request_id,
+			project_id,
+			requestor_id,
+			recipient_id,
+			amount_in_usd,
+			reason.clone(),
+		)
+		.await
+		.unwrap();
+
+		assert_eq!(events.len(), 1);
+		assert_eq!(
+			events[0],
+			PaymentRequestEvent::Created {
+				id: payment_request_id,
+				project_id,
+				requestor_id,
+				recipient_id,
+				amount_in_usd,
+				reason,
+			}
+		);
+	}
+
+	#[rstest]
+	async fn test_create_with_wrong_project_id(
+		payment_request_id: PaymentRequestId,
+		wrong_project_id: ProjectId,
+		requestor_id: UserId,
+		recipient_id: UserId,
+		amount_in_usd: u32,
+		reason: Value,
+	) {
+		let mut specifications = MockSpecifications::new();
+		specifications
+			.expect_project_exists()
+			.with(eq(wrong_project_id))
+			.times(1)
+			.returning(|_| Ok(false));
+
+		let result = PaymentRequest::create(
+			Arc::new(specifications),
+			payment_request_id,
+			wrong_project_id,
+			requestor_id,
+			recipient_id,
+			amount_in_usd,
+			reason,
+		)
+		.await;
+
+		assert!(result.is_err());
+		assert_matches!(
+			result,
+			Err(Error::ProjectNotFound)
+		);
+	}
+}
