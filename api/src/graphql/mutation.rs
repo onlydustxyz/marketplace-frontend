3219e65d0432644e299dbc030dd5eca4f155a7f3
Anthony Buisset <abuisset@gmail.com>
2022-11-22 14:47:42 +0100

    ♻️ remove useless structure in mutation output

--- api/src/graphql/mutation.rs
+++ api/src/graphql/mutation.rs
@@ -1,16 +1,11 @@
 use super::Context;
 use anyhow::anyhow;
 use domain::{Amount, BlockchainNetwork, Currency, PaymentReceipt};
-use juniper::{graphql_object, FieldResult, GraphQLObject};
+use juniper::{graphql_object, FieldResult};
 use rusty_money::{crypto, Money};
 use uuid::Uuid;
 
 pub struct Mutation;
-
-#[derive(GraphQLObject)]
-struct PaymentId {
-	id: Uuid,
-}
 
 #[graphql_object(context=Context)]
 impl Mutation {
@@ -25,7 +20,7 @@ impl Mutation {
 		currency_code: String,
 		recipient_address: String,
 		transaction_hash: String,
-	) -> FieldResult<PaymentId> {
+	) -> FieldResult<Uuid> {
 		let currency = crypto::find(&currency_code)
 			.ok_or_else(|| anyhow!("Unknown currency code: {currency_code}"))?;
 		let amount = Money::from_str(&amount, currency)?;
@@ -37,15 +32,13 @@ impl Mutation {
 				Amount::new(*amount.amount(), Currency::Crypto(currency_code)),
 				PaymentReceipt::OnChainPayment {
 					network: BlockchainNetwork::Ethereum,
-					recipient_address: recipient_address.parse()?,
-					transaction_hash: transaction_hash.parse()?,
+					recipient_address,
+					transaction_hash,
 				},
 			)
 			.await?;
 
-		Ok(PaymentId {
-			id: payment_id.into(),
-		})
+		Ok(payment_id.into())
 	}
 
 	pub async fn create_project(context: &Context, name: String) -> FieldResult<Uuid> {
