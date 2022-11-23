c646b5177c5a631e45b010422ffb00c2f5226259
Anthony Buisset <abuisset@gmail.com>
2022-11-23 11:41:25 +0100

    ♻️ make aggregate exists spec generic

--- domain/src/specifications/aggregate_exists.rs
+++ domain/src/specifications/aggregate_exists.rs
@@ -1,23 +1,25 @@
 use crate::{
-	specifications::Error, AggregateRootRepository, AggregateRootRepositoryError, Project,
-	ProjectId,
+	specifications::Error, AggregateRoot, AggregateRootRepository, AggregateRootRepositoryError,
+	Project,
 };
 #[cfg(test)]
 use mockall::automock;
 use std::sync::Arc;
 
-pub struct Specification {
-	project_repository: Arc<AggregateRootRepository<Project>>,
+pub struct Specification<A: AggregateRoot> {
+	aggregate_repository: Arc<AggregateRootRepository<A>>,
 }
 
 #[cfg_attr(test, automock)]
-impl Specification {
-	pub fn new(project_repository: Arc<AggregateRootRepository<Project>>) -> Self {
-		Self { project_repository }
+impl<A: AggregateRoot + 'static> Specification<A> {
+	pub fn new(aggregate_repository: Arc<AggregateRootRepository<A>>) -> Self {
+		Self {
+			aggregate_repository,
+		}
 	}
 
-	pub fn is_satisfied_by(&self, project_id: &ProjectId) -> Result<bool, Error> {
-		match self.project_repository.find_by_id(project_id) {
+	pub fn is_satisfied_by(&self, aggregate_id: &A::Id) -> Result<bool, Error> {
+		match self.aggregate_repository.find_by_id(aggregate_id) {
 			Ok(_) => Ok(true),
 			Err(e) => match e {
 				AggregateRootRepositoryError::NotFound => Ok(false),
@@ -26,3 +28,8 @@ impl Specification {
 		}
 	}
 }
+
+pub type ProjectExists = Specification<Project>;
+
+#[cfg(test)]
+pub type MockProjectExists = MockSpecification<Project>;
--- domain/src/specifications/mod.rs
+++ domain/src/specifications/mod.rs
@@ -2,10 +2,10 @@ use crate::aggregate_root;
 use thiserror::Error;
 
 mod aggregate_exists;
-pub use aggregate_exists::Specification as ProjectExistsSpecification;
+pub use aggregate_exists::ProjectExists;
 
 #[cfg(test)]
-pub use aggregate_exists::MockSpecification as MockProjectExistsSpecification;
+pub use aggregate_exists::MockProjectExists;
 
 #[derive(Debug, Error)]
 pub enum Error {
