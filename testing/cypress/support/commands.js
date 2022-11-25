581a765d855d167ea1804e72bfcfc78c476fccf3
Anthony Buisset <abuisset@gmail.com>
2022-11-25 14:50:37 +0100

    ✅ add cypress command to create a user

--- testing/cypress/support/commands.js
+++ testing/cypress/support/commands.js
@@ -26,3 +26,39 @@ Cypress.Commands.add('createProject', (projectName) => {
         .its("body.data.createProject")
         .should("be.a", "string");
 });
+
+Cypress.Commands.add('createUser', () => {
+    const email = `cypress-${Date.now()}@onlydust.xyz`;
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:4000/signup/email-password',
+        body: {
+            "email": email,
+            "options": {
+                "allowedRoles": [
+                    "me",
+                    "public",
+                    "user"
+                ],
+                "defaultRole": "public",
+                "displayName": "John Smith",
+                "locale": "en",
+            },
+            "password": "Str0ngPassw#ord-94|%"
+        },
+        failOnStatusCode: false
+    }).then(() => {
+        return cy.graphqlAsAdmin({
+            query: `{
+                users(where: {email: {_eq: "${email}"}}) {
+                    id
+                }
+            }`
+        })
+            .its('body.data.users')
+            .its(0)
+            .its('id')
+            .should('be.a', 'string');
+    })
+});
