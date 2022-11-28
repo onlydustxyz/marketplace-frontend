4f4f12dfa51f484e39f0fcd619ab0c202ad6b793
Anthony Buisset <abuisset@gmail.com>
2022-11-28 10:13:45 +0100

    ✨ create user trait in api domain

---
+++ api/src/domain/user/mod.rs
@@ -0,0 +1,21 @@
+use domain::ProjectId;
+
+mod admin;
+mod project_leader;
+mod public;
+
+pub trait User: Send + Sync {
+	fn is_leader_on_project(&self, project_id: &ProjectId) -> bool;
+}
+
+pub fn admin() -> Box<dyn User> {
+	Box::new(admin::Admin)
+}
+
+pub fn project_leader(projects: Vec<ProjectId>) -> Box<dyn User> {
+	Box::new(project_leader::ProjectLeader::new(projects))
+}
+
+pub fn public() -> Box<dyn User> {
+	Box::new(public::Public)
+}
