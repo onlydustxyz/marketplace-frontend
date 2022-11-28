1f38a12d2655dd2b4817bef9ab9e9610691e194c
Anthony Buisset <abuisset@gmail.com>
2022-11-28 12:05:32 +0100

    🔒️ check project leader role when requesting payment

--- api/src/graphql/mutation.rs
+++ api/src/graphql/mutation.rs
@@ -1,18 +1,35 @@
 use super::Context;
 use anyhow::anyhow;
 use domain::{Amount, BlockchainNetwork, Currency, PaymentReceipt};
-use juniper::{graphql_object, FieldResult};
+use juniper::{graphql_object, graphql_value, DefaultScalarValue, FieldError, IntoFieldError};
 use rusty_money::{crypto, Money};
+use thiserror::Error;
 use uuid::Uuid;
+
+#[derive(Debug, Error)]
+enum Error {
+	#[error("User is not authorized to perform the action")]
+	NotAuthorized(String),
+	#[error("Invalid GraphQL request")]
+	InvalidRequest(#[from] anyhow::Error),
+}
+
+impl IntoFieldError for Error {
+	fn into_field_error(self) -> FieldError<DefaultScalarValue> {
+		let (msg, reason) = match &self {
+			Self::NotAuthorized(reason) => (self.to_string(), reason.clone()),
+			Self::InvalidRequest(source) => (self.to_string(), source.to_string()),
+		};
+		FieldError::new(msg, graphql_value!({ "reason": reason }))
+	}
+}
+
+type Result<T> = std::result::Result<T, Error>;
 
 pub struct Mutation;
 
-#[graphql_object(context=Context)]
+#[graphql_object(context=Context, Scalar = DefaultScalarValue)]
 impl Mutation {
-	pub fn new() -> Self {
-		Self {}
-	}
-
 	pub async fn add_eth_payment_receipt(
 		context: &Context,
 		payment_id: Uuid,
@@ -20,10 +37,10 @@ impl Mutation {
 		currency_code: String,
 		recipient_address: String,
 		transaction_hash: String,
-	) -> FieldResult<Uuid> {
+	) -> Result<Uuid> {
 		let currency = crypto::find(&currency_code)
 			.ok_or_else(|| anyhow!("Unknown currency code: {currency_code}"))?;
-		let amount = Money::from_str(&amount, currency)?;
+		let amount = Money::from_str(&amount, currency).map_err(anyhow::Error::msg)?;
 
 		let payment_id = context
 			.process_payment_usecase
@@ -41,7 +58,7 @@ impl Mutation {
 		Ok(payment_id.into())
 	}
 
-	pub async fn create_project(context: &Context, name: String) -> FieldResult<Uuid> {
+	pub async fn create_project(context: &Context, name: String) -> Result<Uuid> {
 		let project_id = context.create_project_usecase.create(name).await?;
 
 		Ok(project_id.into())
@@ -51,7 +68,7 @@ impl Mutation {
 		context: &Context,
 		project_id: Uuid,
 		leader_id: Uuid,
-	) -> FieldResult<Uuid> {
+	) -> Result<Uuid> {
 		context
 			.assign_project_lead_usecase
 			.assign_leader(project_id.into(), leader_id.into())
@@ -67,7 +84,13 @@ impl Mutation {
 		recipient_id: Uuid,
 		amount_in_usd: i32,
 		reason: String,
-	) -> FieldResult<Uuid> {
+	) -> Result<Uuid> {
+		if !context.user.is_leader_on_project(&project_id.into()) {
+			return Err(Error::NotAuthorized(
+				"Project leader role required".to_string(),
+			));
+		}
+
 		let payment_request_id = context
 			.request_payment_usecase
 			.request(
