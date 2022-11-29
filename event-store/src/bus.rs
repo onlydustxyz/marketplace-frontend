ceabcf97c1e272d6b5090cc0540dc494c1c549ff
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-11-29 10:17:48 +0100

    🐛 fix AMQP config to avoid dropping queues on consumers shutdown

--- event-store/src/bus.rs
+++ event-store/src/bus.rs
@@ -10,9 +10,15 @@ pub async fn consumer() -> Result<ConsumableBus, BusError> {
 		.with_queue(
 			QUEUE_NAME,
 			QueueDeclareOptions {
-				durable: true,      // persist messages during restart
-				exclusive: true,    // only one consumer on this queue
-				auto_delete: false, // do not delete the queue at shutdown
+				// allows multiple connections to this queue, and do not delete the queue when
+				// connection is closed
+				exclusive: false,
+
+				// the queue will survive a broker restart
+				durable: true,
+
+				// do not delete the queue when the last consumer unsubscribes
+				auto_delete: false,
 				..Default::default()
 			},
 		)
