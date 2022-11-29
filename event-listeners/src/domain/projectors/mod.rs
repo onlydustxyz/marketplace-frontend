6fd8a0d1c0fcde078523ea5cb7c50057a0634a76
Anthony Buisset <abuisset@gmail.com>
2022-11-29 22:04:04 +0100

    🎨 run cargo fmt after rebase

--- event-listeners/src/domain/projectors/mod.rs
+++ event-listeners/src/domain/projectors/mod.rs
@@ -1,9 +1,9 @@
+mod budget;
 mod payment;
 mod payment_request;
 mod project;
-mod budget;
 
+pub use budget::Projector as BudgetProjector;
 pub use payment::Projector as PaymentProjector;
 pub use payment_request::Projector as PaymentRequestProjector;
 pub use project::Projector as ProjectProjector;
-pub use budget::Projector as BudgetProjector;
