d29a655b677c264f5793aa2065eb1a3e34aa7109
Anthony Buisset <abuisset@gmail.com>
2022-11-30 17:32:34 +0100

    ✨ create budget spender repository

--- event-listeners/src/infrastructure/database/mod.rs
+++ event-listeners/src/infrastructure/database/mod.rs
@@ -1,10 +1,12 @@
 mod budget;
+mod budget_spender;
 mod payment;
 mod payment_request;
 mod project;
 mod project_lead;
 
 pub use budget::Repository as BudgetRepository;
+pub use budget_spender::Repository as BudgetSpenderRepository;
 pub use payment::Repository as PaymentRepository;
 pub use payment_request::Repository as PaymentRequestRepository;
 pub use project::Repository as ProjectRepository;
