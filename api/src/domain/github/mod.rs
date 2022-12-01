d1e5b3c6cb32af06c1e3edb7e7e880928cd64d85
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-01 10:16:42 +0100

    ♻️ replace ToSql implementation with NewtypeToSql macro

--- api/src/domain/github/mod.rs
+++ api/src/domain/github/mod.rs
@@ -1,11 +1,4 @@
-use std::io::Write;
-
 use derive_more::{AsRef, Display, From, Into};
-use diesel::{
-	backend::Backend,
-	serialize::{self, Output},
-	types::ToSql,
-};
 use serde::{Deserialize, Serialize};
 
 #[derive(
@@ -23,16 +16,7 @@ use serde::{Deserialize, Serialize};
 	AsRef,
 	Hash,
 	AsExpression,
+	NewtypeToSql,
 )]
 #[sql_type = "diesel::sql_types::BigInt"]
 pub struct GithubRepositoryId(i64);
-
-impl<DB> ToSql<diesel::sql_types::BigInt, DB> for GithubRepositoryId
-where
-	DB: Backend,
-	i64: ToSql<diesel::sql_types::BigInt, DB>,
-{
-	fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> serialize::Result {
-		self.0.to_sql(out)
-	}
-}
