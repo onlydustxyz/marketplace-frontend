884d508f563b052722e97fdce27b005e10bdd100
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-01 18:21:39 +0100

    ✨ add other info! declinations

--- olog/src/info.rs
+++ olog/src/info.rs
@@ -1,5 +1,172 @@
 #[macro_export]
 macro_rules! info {
+	(target: $target:expr, parent: $parent:expr, { $($field:tt)* }, $($arg:tt)* ) => (
+        tracing::event!(target: $target, parent: $parent, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($field)* }, $($arg)*)
+    );
+    (target: $target:expr, parent: $parent:expr, $($k:ident).+ $($field:tt)+ ) => (
+        tracing::event!(target: $target, parent: $parent, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ $($field)+ })
+    );
+    (target: $target:expr, parent: $parent:expr, ?$($k:ident).+ $($field:tt)+ ) => (
+        tracing::event!(target: $target, parent: $parent, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ $($field)+ })
+    );
+    (target: $target:expr, parent: $parent:expr, %$($k:ident).+ $($field:tt)+ ) => (
+        tracing::event!(target: $target, parent: $parent, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ $($field)+ })
+    );
+    (target: $target:expr, parent: $parent:expr, $($arg:tt)+ ) => (
+        tracing::event!(target: $target, parent: $parent, tracing::Level::INFO, {trace_id = $crate::trace_id!(), span_id = $crate::span_id!() }, $($arg)+)
+    );
+    (parent: $parent:expr, { $($field:tt)+ }, $($arg:tt)+ ) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($field)+ },
+            $($arg)+
+        )
+    );
+    (parent: $parent:expr, $($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ = $($field)*}
+        )
+    );
+    (parent: $parent:expr, ?$($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+ = $($field)*}
+        )
+    );
+    (parent: $parent:expr, %$($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), %$($k).+ = $($field)*}
+        )
+    );
+    (parent: $parent:expr, $($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+, $($field)*}
+        )
+    );
+    (parent: $parent:expr, ?$($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+, $($field)*}
+        )
+    );
+    (parent: $parent:expr, %$($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), %$($k).+, $($field)*}
+        )
+    );
+    (parent: $parent:expr, $($arg:tt)+) => (
+        tracing::event!(
+            target: module_path!(),
+            parent: $parent,
+            tracing::Level::INFO,
+            {trace_id = $crate::trace_id!(), span_id = $crate::span_id!() },
+            $($arg)+
+        )
+    );
+    (target: $target:expr, { $($field:tt)* }, $($arg:tt)* ) => (
+        tracing::event!(target: $target, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($field)* }, $($arg)*)
+    );
+    (target: $target:expr, $($k:ident).+ $($field:tt)* ) => (
+        tracing::event!(target: $target, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ $($field)* })
+    );
+    (target: $target:expr, ?$($k:ident).+ $($field:tt)* ) => (
+        tracing::event!(target: $target, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+ $($field)* })
+    );
+    (target: $target:expr, %$($k:ident).+ $($field:tt)* ) => (
+        tracing::event!(target: $target, tracing::Level::INFO, { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ $($field)* })
+    );
+    (target: $target:expr, $($arg:tt)+ ) => (
+        tracing::event!(target: $target, tracing::Level::INFO, {trace_id = $crate::trace_id!(), span_id = $crate::span_id!() }, $($arg)+)
+    );
+    ({ $($field:tt)+ }, $($arg:tt)+ ) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($field)+ },
+            $($arg)+
+        )
+    );
+    ($($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ = $($field)*}
+        )
+    );
+    (?$($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+ = $($field)*}
+        )
+    );
+    (%$($k:ident).+ = $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), %$($k).+ = $($field)*}
+        )
+    );
+    ($($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+, $($field)*}
+        )
+    );
+    (?$($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+, $($field)*}
+        )
+    );
+    (%$($k:ident).+, $($field:tt)*) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), %$($k).+, $($field)*}
+        )
+    );
+    (?$($k:ident).+) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), ?$($k).+ }
+        )
+    );
+    (%$($k:ident).+) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), %$($k).+ }
+        )
+    );
+    ($($k:ident).+) => (
+        tracing::event!(
+            target: module_path!(),
+            tracing::Level::INFO,
+            { trace_id = $crate::trace_id!(), span_id = $crate::span_id!(), $($k).+ }
+        )
+    );
 	($($arg:tt)+) => (
         tracing::event!(
             target: module_path!(),
@@ -32,74 +199,44 @@ mod tests {
 
 	#[test]
 	fn info() {
-		// info!(foo = ?3, bar.baz = %2, quux = false);
-		// info!(foo = 3, bar.baz = 2, quux = false);
-		// info!(foo = 3, bar.baz = 3,);
+		info!(foo = ?3, bar.baz = %2, quux = false);
+		info!(foo = 3, bar.baz = 2, quux = false);
+		info!(foo = 3, bar.baz = 3,);
 		info!("foo");
 		info!("foo: {}", 3);
-		// info!(foo = ?3, bar.baz = %2, quux = false, "hello world {:?}", 42);
-		// info!(foo = 3, bar.baz = 2, quux = false, "hello world {:?}", 42);
-		// info!(foo = 3, bar.baz = 3, "hello world {:?}", 42,);
-		// info!({ foo = 3, bar.baz = 80 }, "quux");
-		// info!({ foo = 2, bar.baz = 79 }, "quux {:?}", true);
-		// info!({ foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux = false);
-		// info!({ foo = 2, bar.baz = 78 }, "quux");
-		// info!({ foo = ?2, bar.baz = %78 }, "quux");
-		// info!(target: "foo_events", foo = 3, bar.baz = 2, quux = false);
-		// info!(target: "foo_events", foo = 3, bar.baz = 3,);
-		// info!(target: "foo_events", "foo");
-		// info!(target: "foo_events", "foo: {}", 3);
-		// info!(target: "foo_events", { foo = 3, bar.baz = 80 }, "quux");
-		// info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}", true);
-		// info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux =
-		// false); info!(target: "foo_events", { foo = 2, bar.baz = 78, }, "quux");
-		// let foo = 1;
-		// info!(?foo);
-		// info!(%foo);
-		// info!(foo);
-		// info!(target: "foo_events", ?foo);
-		// info!(target: "foo_events", %foo);
-		// info!(target: "foo_events", foo);
-		// info!(target: "foo_events", ?foo, true, "message");
-		// info!(target: "foo_events", %foo, true, "message");
-		// info!(target: "foo_events", foo, true, "message");
+		info!(foo = ?3, bar.baz = %2, quux = false, "hello world {:?}", 42);
+		info!(foo = 3, bar.baz = 2, quux = false, "hello world {:?}", 42);
+		info!(foo = 3, bar.baz = 3, "hello world {:?}", 42,);
+		info!({ foo = 3, bar.baz = 80 }, "quux");
+		info!({ foo = 2, bar.baz = 79 }, "quux {:?}", true);
+		info!({ foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux = false);
+		info!({ foo = 2, bar.baz = 78 }, "quux");
+		info!({ foo = ?2, bar.baz = %78 }, "quux");
+		info!(target: "foo_events", foo = 3, bar.baz = 2, quux = false);
+		info!(target: "foo_events", foo = 3, bar.baz = 3,);
+		info!(target: "foo_events", "foo");
+		info!(target: "foo_events", "foo: {}", 3);
+		info!(target: "foo_events", { foo = 3, bar.baz = 80 }, "quux");
+		info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}", true);
+		info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux =
+		false);
+		info!(target: "foo_events", { foo = 2, bar.baz = 78, }, "quux");
+		let foo = 1;
+		info!(?foo);
+		info!(%foo);
+		info!(foo);
+		info!(target: "foo_events", ?foo);
+		info!(target: "foo_events", %foo);
+		info!(target: "foo_events", foo);
+		info!(target: "foo_events", ?foo, true, "message");
+		info!(target: "foo_events", %foo, true, "message");
+		info!(target: "foo_events", foo, true, "message");
 	}
 
 	#[test]
 	fn info_inside_span() {
 		let span = tracing::span!(Level::INFO, "my span");
 		let _enter = span.enter();
-
-		// info!(foo = ?3, bar.baz = %2, quux = false);
-		// info!(foo = 3, bar.baz = 2, quux = false);
-		// info!(foo = 3, bar.baz = 3,);
-		info!("foo");
-		info!("foo: {}", 3);
-		// info!(foo = ?3, bar.baz = %2, quux = false, "hello world {:?}", 42);
-		// info!(foo = 3, bar.baz = 2, quux = false, "hello world {:?}", 42);
-		// info!(foo = 3, bar.baz = 3, "hello world {:?}", 42,);
-		// info!({ foo = 3, bar.baz = 80 }, "quux");
-		// info!({ foo = 2, bar.baz = 79 }, "quux {:?}", true);
-		// info!({ foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux = false);
-		// info!({ foo = 2, bar.baz = 78 }, "quux");
-		// info!({ foo = ?2, bar.baz = %78 }, "quux");
-		// info!(target: "foo_events", foo = 3, bar.baz = 2, quux = false);
-		// info!(target: "foo_events", foo = 3, bar.baz = 3,);
-		// info!(target: "foo_events", "foo");
-		// info!(target: "foo_events", "foo: {}", 3);
-		// info!(target: "foo_events", { foo = 3, bar.baz = 80 }, "quux");
-		// info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}", true);
-		// info!(target: "foo_events", { foo = 2, bar.baz = 79 }, "quux {:?}, {quux}", true, quux =
-		// false); info!(target: "foo_events", { foo = 2, bar.baz = 78, }, "quux");
-		// let foo = 1;
-		// info!(?foo);
-		// info!(%foo);
-		// info!(foo);
-		// info!(target: "foo_events", ?foo);
-		// info!(target: "foo_events", %foo);
-		// info!(target: "foo_events", foo);
-		// info!(target: "foo_events", ?foo, true, "message");
-		// info!(target: "foo_events", %foo, true, "message");
-		// info!(target: "foo_events", foo, true, "message");
+		info();
 	}
 }
