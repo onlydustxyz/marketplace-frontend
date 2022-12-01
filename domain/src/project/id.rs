dd82de4b9f8d675465168dbe59c6a4595e78bd6b
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-01 10:12:28 +0100

    ♻️ introduce NewtypeToSql macro

--- domain/src/project/id.rs
+++ domain/src/project/id.rs
@@ -1,11 +1,4 @@
-use std::io::Write;
-
 use derive_more::{AsRef, Display, From, Into};
-use diesel::{
-	backend::Backend,
-	serialize::{self, Output},
-	types::ToSql,
-};
 use serde::{Deserialize, Serialize};
 
 #[derive(
@@ -23,16 +16,7 @@ use serde::{Deserialize, Serialize};
 	AsRef,
 	Hash,
 	AsExpression,
+	NewtypeToSql,
 )]
 #[sql_type = "diesel::sql_types::Uuid"]
 pub struct Id(uuid::Uuid);
-
-impl<DB> ToSql<diesel::sql_types::Uuid, DB> for Id
-where
-	DB: Backend,
-	uuid::Uuid: ToSql<diesel::sql_types::Uuid, DB>,
-{
-	fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> serialize::Result {
-		self.0.to_sql(out)
-	}
-}
