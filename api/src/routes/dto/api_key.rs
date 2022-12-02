3eb01fb9003ebb0d7730acc7491cc6081dddded2
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-02 17:54:28 +0100

    🔒️ require Api-Key to call backend graphql endpoint

---
+++ api/src/routes/dto/api_key.rs
@@ -0,0 +1,42 @@
+use rocket::{
+	http::Status,
+	outcome::Outcome,
+	request::{self, FromRequest},
+	Request,
+};
+#[derive(Default)]
+pub struct ApiKey(String);
+
+fn is_valid_key(key: &str) -> bool {
+	key == match std::env::var("BACKEND_GRAPHQL_API_KEY") {
+		Ok(v) => v,
+		Err(_) => return false,
+	}
+}
+
+#[derive(Debug)]
+pub enum ApiKeyError {
+	BadCount,
+	Missing,
+	Invalid,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+	type Error = ApiKeyError;
+
+	async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+		println!(
+			"Api-Key = {} : {}",
+			request.headers().get_one("Api-Key").unwrap_or_default(),
+			is_valid_key(request.headers().get_one("Api-Key").unwrap_or_default())
+		);
+		let keys: Vec<_> = request.headers().get("Api-Key").collect();
+		match keys.len() {
+			0 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Missing)),
+			1 if is_valid_key(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
+			1 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Invalid)),
+			_ => Outcome::Failure((Status::Unauthorized, ApiKeyError::BadCount)),
+		}
+	}
+}
