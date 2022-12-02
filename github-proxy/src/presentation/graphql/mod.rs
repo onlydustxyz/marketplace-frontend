45adf87b85d77224349d06a4d4ab3c6b405e6539
Anthony Buisset <abuisset@gmail.com>
2022-12-02 16:45:49 +0100

    ✨ expose graphql endpoint

---
+++ github-proxy/src/presentation/graphql/mod.rs
@@ -0,0 +1,13 @@
+mod query;
+use query::Query;
+
+mod context;
+pub use context::Context;
+
+use juniper::{EmptyMutation, EmptySubscription, RootNode};
+
+pub type Schema = RootNode<'static, Query, EmptyMutation<Context>, EmptySubscription<Context>>;
+
+pub fn create_schema() -> Schema {
+	Schema::new(Query, EmptyMutation::new(), EmptySubscription::new())
+}
