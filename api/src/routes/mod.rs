eaaa8a7c5a5401ef2d619f4a47e4fce069d9580e
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-02 17:56:32 +0100

    🔥 remove openapi

--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -4,12 +4,3 @@ pub mod graphql;
 pub mod health;
 
 pub use dto::*;
-
-use rocket_okapi::swagger_ui::SwaggerUIConfig;
-
-pub(crate) fn get_docs() -> SwaggerUIConfig {
-	SwaggerUIConfig {
-		url: "/openapi.json".to_string(),
-		..Default::default()
-	}
-}
