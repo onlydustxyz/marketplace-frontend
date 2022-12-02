c560ea1498aaca3f46be8e0a79dfd7b3b511245f
Anthony Buisset <abuisset@gmail.com>
2022-12-02 16:32:38 +0100

    ✨ create web server using rocket

---
+++ github-proxy/src/presentation/http/config.rs
@@ -0,0 +1,10 @@
+use figment::{
+	providers::{Env, Format, Toml},
+	Figment,
+};
+
+pub fn get() -> Figment {
+	Figment::from(rocket::Config::default())
+		.merge(Toml::file("github-proxy/Rocket.toml").nested())
+		.merge(Env::prefixed("ROCKET_"))
+}
