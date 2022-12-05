681a7fd1e5c5024f1c784672714bcd932b3b2f4e
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-12-05 15:47:37 +0100

    🚧 testing datadog

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use dotenv::dotenv;
 use infrastructure::tracing::Tracer;
+use olog::{opentelemetry::trace::TraceContextExt, tracing_opentelemetry::OpenTelemetrySpanExt};
 
 extern crate rocket;
 
@@ -9,5 +10,15 @@ async fn main() -> Result<()> {
 	dotenv().ok();
 
 	let _tracer = Tracer::init("api")?;
+	{
+		let span = tracing::span!(tracing::Level::INFO, "my span");
+		let _enter = span.enter();
+		let the_id =
+			u128::from_be_bytes(span.context().span().span_context().trace_id().to_bytes()) as u64;
+		olog::info!(the_id = the_id, "foooooooo");
+	}
+
+	olog::info!("baaaaaaar");
+
 	api::main().await
 }
