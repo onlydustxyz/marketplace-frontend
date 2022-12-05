13cc7548d2968299c12a25d3735d79ee1bceaffc
Anthony Buisset <abuisset@gmail.com>
2022-12-05 23:03:59 +0100

    🔧 move CYPRESS_BASE_URL to config file

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -1,13 +1,10 @@
-require("dotenv").config({
-    path: "../.env",
-});
 const fs = require('fs');
 const config = require('toml-js').parse(fs.readFileSync('../App.toml'));
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: process.env.CYPRESS_BASE_URL,
+        baseUrl: config.cypress.base_url,
     },
     env: {
         hasuraAdminSecret: config.hasura.graphql_admin_secret,
