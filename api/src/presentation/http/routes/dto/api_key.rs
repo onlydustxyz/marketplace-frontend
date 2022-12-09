ec5b13076306d63d77996c9f75e4f803ed390494
Anthony Buisset <abuisset@gmail.com>
2022-12-09 10:12:11 +0100

    ♻️ move api_key to config

--- api/src/presentation/http/routes/dto/api_key.rs
+++ api/src/presentation/http/routes/dto/api_key.rs
@@ -4,15 +4,10 @@ use rocket::{
 	request::{self, FromRequest},
 	Request,
 };
+
+use crate::presentation::http::Config;
 #[derive(Default)]
 pub struct ApiKey(String);
-
-fn is_valid_key(key: &str) -> bool {
-	key == match std::env::var("BACKEND_GRAPHQL_API_KEY") {
-		Ok(v) => v,
-		Err(_) => return false,
-	}
-}
 
 #[derive(Debug)]
 pub enum ApiKeyError {
@@ -27,11 +22,16 @@ impl<'r> FromRequest<'r> for ApiKey {
 
 	async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
 		let keys: Vec<_> = request.headers().get("Api-Key").collect();
-		match keys.len() {
-			0 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Missing)),
-			1 if is_valid_key(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
-			1 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Invalid)),
-			_ => Outcome::Failure((Status::Unauthorized, ApiKeyError::BadCount)),
+
+		if let Some(config) = request.rocket().state::<Config>() {
+			match keys.len() {
+				0 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Missing)),
+				1 if keys[0] == config.api_key() => Outcome::Success(ApiKey(keys[0].to_string())),
+				1 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Invalid)),
+				_ => Outcome::Failure((Status::Unauthorized, ApiKeyError::BadCount)),
+			}
+		} else {
+			Outcome::Forward(())
 		}
 	}
 }
