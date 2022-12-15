09406851ec7fff1a1fa08914a2104772d64b646d
Anthony Buisset <abuisset@gmail.com>
2022-12-15 16:54:14 +0100

    ♻️ remove id, owner and name from github repo live content

--- backend/github-proxy/src/domain/github/repository.rs
+++ backend/github-proxy/src/domain/github/repository.rs
@@ -5,9 +5,6 @@ use crate::domain::{GithubFile, GithubUser};
 
 #[derive(Constructor, GraphQLObject)]
 pub struct Repository {
-	id: i32,
-	owner: String,
-	name: String,
 	contributors: Vec<GithubUser>,
 	readme: GithubFile,
 }
