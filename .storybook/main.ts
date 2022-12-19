54ce3e87b75c4993186f25949b9a0cadc1cccd23
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-12-19 12:14:05 +0100

    :wrench: added react router pluging for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookViteConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-react-router-v6",
     {
       name: "@storybook/addon-postcss",
       options: {
