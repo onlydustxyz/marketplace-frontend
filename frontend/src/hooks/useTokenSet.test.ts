100f458f44bac9de42e398158666086985f52748
Stanislas Bernard <bernardstanislas@gmail.com>
2022-12-20 10:35:06 +0100

    :white_check_mark: Test token consumption

--- frontend/src/hooks/useTokenSet.test.ts
+++ frontend/src/hooks/useTokenSet.test.ts
@@ -1,9 +1,12 @@
 import { act, renderHook } from "@testing-library/react-hooks";
 import { LOCAL_STORAGE_TOKEN_SET_KEY, TokenSetProvider, useTokenSet } from "src/hooks/useTokenSet";
-import { TokenSet } from "src/types";
-import { describe, expect, it, beforeEach } from "vitest";
+import { RefreshToken, TokenSet } from "src/types";
+import { describe, expect, it, beforeEach, afterEach, vi, Mock } from "vitest";
+import axios from "axios";
+import config from "src/config";
 
 const renderWithProvider = () => renderHook(() => useTokenSet(), { wrapper: TokenSetProvider });
+vi.mock("axios");
 
 describe("useTokenSet", () => {
   beforeEach(() => {
@@ -56,6 +59,36 @@ describe("useTokenSet", () => {
         result.current.clearTokenSet();
       });
       expect(window.localStorage.getItem(LOCAL_STORAGE_TOKEN_SET_KEY)).toBe("null");
+    });
+  });
+
+  describe("setFromRefreshToken", () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+    });
+
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+
+    it("should exchange the refresh token with a token set", async () => {
+      const refreshToken = Symbol("refreshToken") as unknown as RefreshToken;
+      const accessToken = "accessToken";
+      const tokenSet = { accessToken } as TokenSet;
+      const date = new Date(2000, 1, 1, 13);
+      vi.setSystemTime(date);
+
+      (axios.post as Mock).mockResolvedValue({ data: tokenSet });
+      const { result, waitForValueToChange } = renderWithProvider();
+
+      act(async () => {
+        await result.current.setFromRefreshToken(refreshToken);
+      });
+
+      expect(axios.post).toHaveBeenCalledWith(`${config.HASURA_AUTH_BASE_URL}/token`, { refreshToken });
+
+      await waitForValueToChange(() => result.current.tokenSet);
+      expect(result.current.tokenSet).toEqual({ ...tokenSet, creationDate: date.getTime() });
     });
   });
 });
