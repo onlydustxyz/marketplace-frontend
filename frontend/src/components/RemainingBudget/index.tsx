4833b6123ec984ca7c854636f975cdafe6566a32
Anthony Buisset <abuisset@gmail.com>
2022-12-21 16:20:24 +0100

    ✅ add e2e test

--- frontend/src/components/RemainingBudget/index.tsx
+++ frontend/src/components/RemainingBudget/index.tsx
@@ -13,7 +13,9 @@ export default function RemainingBudget({ remainingAmount, initialAmount }: Rema
         <div className="flex flex-col">
           <div>{T("project.remainingBudget")}</div>
           <div className="flex flex-row items-center space-x-3 md:flex-nowrap flex-wrap">
-            <div className="text-xl font-black">${remainingAmount}</div>
+            <div id="remainingBudget" className="text-xl font-black">
+              ${remainingAmount}
+            </div>
             <div className="w-full bg-gray-700 rounded-full h-2.5">
               <div
                 className="bg-blue-600 h-2.5 rounded-full"
