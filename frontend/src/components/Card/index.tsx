ca0f35dbcc6f1414db5432dc0e789cd56d2b99bb
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-12-23 00:02:50 +0100

    :lipstick: card with custom background

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -2,12 +2,26 @@ import headerElementBackground from "assets/img/grain-background.png";
 
 interface ProjectCardProps extends React.PropsWithChildren {
   selectable?: boolean;
+  backgroundImageUrl?: string;
+  backgroundImageClassName?: string;
 }
 
-export default function Card({ selectable = false, children }: ProjectCardProps) {
+export default function Card({
+  selectable = false,
+  backgroundImageUrl = headerElementBackground,
+  backgroundImageClassName = "",
+  children,
+}: ProjectCardProps) {
   return (
-    <div className="w-full rounded-xl" style={{ backgroundImage: `url(${headerElementBackground})` }}>
-      <div className="w-full h-full bg-chineseBlack p-6 border border-neutral-600/70 rounded-xl font-walsheim bg-opacity-90 hover:bg-opacity-95">
+    <div
+      className={`w-full rounded-xl ${backgroundImageClassName}`}
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+    >
+      <div
+        className={`w-full h-full bg-chineseBlack p-6 border border-neutral-600/70 rounded-xl font-walsheim bg-opacity-90 ${
+          selectable ? "hover:bg-opacity-95" : ""
+        }`}
+      >
         {children}
       </div>
     </div>
