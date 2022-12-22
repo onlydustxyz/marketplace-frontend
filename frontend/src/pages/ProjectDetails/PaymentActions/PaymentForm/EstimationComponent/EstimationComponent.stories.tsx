7a3615a1a85139761cfe1361f1f186058f127460
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-12-23 00:03:53 +0100

    :lipstick: new estimation component

---
+++ frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/EstimationComponent/EstimationComponent.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import EstimationComponent from ".";
+
+export default {
+  title: "EstimationComponent",
+  component: EstimationComponent,
+} as ComponentMeta<typeof EstimationComponent>;
+
+const Template: ComponentStory<typeof EstimationComponent> = args => <EstimationComponent {...args} />;
+
+export const Default = Template.bind({});
+
+let numberOfDays = 2;
+
+Default.args = {
+  numberOfDays,
+  subtractNumberOfDays: () => numberOfDays--,
+  addNumberOfDays: () => numberOfDays++,
+  budget: { total: 50000, remaining: 30000 },
+};
