49cf01cffba13e976d72685b6d0e3708539d26a8
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2022-12-27 09:59:02 +0100

    :bug: fix invalid jwt

--- frontend/src/hooks/useAuth/useGithubProfile.test.tsx
+++ frontend/src/hooks/useAuth/useGithubProfile.test.tsx
@@ -5,36 +5,37 @@ import { GET_USER_GITHUB_ID, useGithubProfile } from "src/hooks/useAuth/useGithu
 import { HasuraUserRole, UserRole } from "src/types";
 import { describe, expect, it } from "vitest";
 
+const roles: UserRole[] = [HasuraUserRole.Public];
+const userId = "test-user-id";
+const githubUserId = Symbol("githubUserId");
+const mocks = [
+  {
+    request: {
+      query: GET_USER_GITHUB_ID,
+      variables: {
+        userId,
+      },
+    },
+    result: {
+      data: {
+        authGithubUsers: [
+          {
+            githubUserId,
+          },
+        ],
+      },
+    },
+  },
+];
+
 describe("useGithubProfile", () => {
   it("should not fetch the profile when the user is not registered, then fetch the github profile", async () => {
-    const roles: UserRole[] = [HasuraUserRole.Public];
-    const userId = "test-user-id";
-    const githubUserId = Symbol("githubUserId");
-    const mocks = [
-      {
-        request: {
-          query: GET_USER_GITHUB_ID,
-          variables: {
-            userId,
-          },
-        },
-        result: {
-          data: {
-            authGithubUsers: [
-              {
-                githubUserId,
-              },
-            ],
-          },
-        },
-      },
-    ];
     const wrapper = ({ children }: PropsWithChildren) => <MockedProvider mocks={mocks}>{children}</MockedProvider>;
 
     const { result, rerender, waitForValueToChange } = renderHook<
       PropsWithChildren & { roles: UserRole[] },
       ReturnType<typeof useGithubProfile>
-    >(({ roles }) => useGithubProfile(roles, userId), {
+    >(({ roles }) => useGithubProfile(roles, userId, true), {
       wrapper,
       initialProps: { roles },
     });
@@ -45,5 +46,19 @@ describe("useGithubProfile", () => {
     await waitForValueToChange(() => result.current.githubUserId);
 
     expect(result.current.githubUserId).toBe(githubUserId);
+  });
+
+  it("should not fetch the profile when the token is not refreshed", async () => {
+    const wrapper = ({ children }: PropsWithChildren) => <MockedProvider mocks={mocks}>{children}</MockedProvider>;
+
+    const { result } = renderHook<PropsWithChildren & { roles: UserRole[] }, ReturnType<typeof useGithubProfile>>(
+      ({ roles }) => useGithubProfile(roles, userId, false),
+      {
+        wrapper,
+        initialProps: { roles },
+      }
+    );
+
+    expect(result.current.githubUserId).toBeUndefined();
   });
 });
--- frontend/src/hooks/useAuth/useGithubProfile.ts
+++ frontend/src/hooks/useAuth/useGithubProfile.ts
@@ -3,9 +3,9 @@ import { useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { HasuraUserRole, UserRole } from "src/types";
 import { GetGithubUserIdQuery } from "src/__generated/graphql";
 
-export const useGithubProfile = (roles: UserRole[], userId?: string) => {
+export const useGithubProfile = (roles: UserRole[], userId?: string, isTokenRefreshed?: boolean) => {
   const { data, loading } = useHasuraQuery<GetGithubUserIdQuery>(GET_USER_GITHUB_ID, HasuraUserRole.RegisteredUser, {
-    skip: !roles.includes(HasuraUserRole.RegisteredUser) || !userId,
+    skip: !roles.includes(HasuraUserRole.RegisteredUser) || !userId || !isTokenRefreshed,
     variables: {
       userId,
     },
