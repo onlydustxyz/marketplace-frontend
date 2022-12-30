dd3fdfb2057a96401d3f88dfcc8fa95fef5399fe
Anthony Buisset <abuisset@gmail.com>
2022-12-30 17:21:03 +0100

    ✨ decode github user id from hasura headers

--- backend/common/presentation/src/http/guards/option_user_id.rs
+++ backend/common/presentation/src/http/guards/option_user_id.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use async_trait::async_trait;
-use domain::UserId;
+use domain::{GithubUserId, UserId};
 use rocket::{
 	http::{HeaderMap, Status},
 	outcome::IntoOutcome,
@@ -11,6 +11,7 @@ use rocket::{
 use thiserror::Error;
 
 const HASURA_USER_ID_HEADER_KEY: &str = "x-hasura-user-id";
+const HASURA_GITHUB_USER_ID_HEADER_KEY: &str = "x-hasura-githubUserId";
 
 #[derive(Debug, Error, PartialEq, Eq)]
 pub enum Error {
@@ -23,15 +24,26 @@ pub enum Error {
 #[derive(Debug, PartialEq, Eq)]
 pub struct OptionUserId {
 	user_id: Option<UserId>,
+	github_user_id: Option<GithubUserId>,
 }
 
 impl OptionUserId {
 	pub fn user_id(&self) -> Result<UserId, Error> {
-		self.user_id.clone().ok_or(Error::Missing(HASURA_USER_ID_HEADER_KEY))
+		self.user_id.ok_or(Error::Missing(HASURA_USER_ID_HEADER_KEY))
 	}
 
-	fn build<E: std::error::Error>(user_id: Result<Option<UserId>, E>) -> Result<Self, E> {
-		Ok(Self { user_id: user_id? })
+	pub fn github_user_id(&self) -> Result<GithubUserId, Error> {
+		self.github_user_id.ok_or(Error::Missing(HASURA_GITHUB_USER_ID_HEADER_KEY))
+	}
+
+	fn build<E: std::error::Error>(
+		user_id: Result<Option<UserId>, E>,
+		github_user_id: Result<Option<GithubUserId>, E>,
+	) -> Result<Self, E> {
+		Ok(Self {
+			user_id: user_id?,
+			github_user_id: github_user_id?,
+		})
 	}
 }
 
@@ -40,8 +52,9 @@ impl OptionUserId {
 		headers: &HeaderMap<'r>,
 	) -> Outcome<Self, <Self as FromRequest<'r>>::Error> {
 		let user_id = headers.try_parse_header_as(HASURA_USER_ID_HEADER_KEY);
+		let github_user_id = headers.try_parse_header_as(HASURA_GITHUB_USER_ID_HEADER_KEY);
 
-		OptionUserId::build(user_id).into_outcome(Status::BadRequest)
+		OptionUserId::build(user_id, github_user_id).into_outcome(Status::BadRequest)
 	}
 }
 
@@ -49,7 +62,7 @@ trait TryParseHeaderAs<R> {
 	fn try_parse_header_as(&self, header_name: &'static str) -> Result<Option<R>, Error>;
 }
 
-impl<R: FromStr> TryParseHeaderAs<R> for HeaderMap<'_> {
+impl<R: FromStr + Default> TryParseHeaderAs<R> for HeaderMap<'_> {
 	fn try_parse_header_as(&self, header_name: &'static str) -> Result<Option<R>, Error> {
 		let value: Option<R> = match self.get_one(header_name) {
 			Some(value) => Some(value.parse().map_err(|_| Error::Invalid(header_name))?),
@@ -77,15 +90,20 @@ mod test {
 	use super::*;
 
 	#[rstest]
-	#[case(Uuid::default())]
-	#[case(Uuid::from_u128(42))]
-	fn from_valid_headers(#[case] user_id: Uuid) {
+	#[case(Uuid::default(), Default::default())]
+	#[case(Uuid::from_u128(42), 42)]
+	fn from_valid_headers(#[case] user_id: Uuid, #[case] github_user_id: i64) {
 		let mut header_map = HeaderMap::new();
 		header_map.add(Header::new(HASURA_USER_ID_HEADER_KEY, user_id.to_string()));
+		header_map.add(Header::new(
+			HASURA_GITHUB_USER_ID_HEADER_KEY,
+			github_user_id.to_string(),
+		));
 
 		match OptionUserId::from_request_headers(&header_map) {
 			Outcome::Success(user) => {
 				assert_eq!(user.user_id(), Ok(user_id.into()));
+				assert_eq!(user.github_user_id(), Ok(github_user_id.into()));
 			},
 			_ => panic!(),
 		}
@@ -100,6 +118,10 @@ mod test {
 				assert_eq!(
 					user.user_id(),
 					Err(Error::Missing(HASURA_USER_ID_HEADER_KEY))
+				);
+				assert_eq!(
+					user.github_user_id(),
+					Err(Error::Missing(HASURA_GITHUB_USER_ID_HEADER_KEY))
 				);
 			},
 			_ => panic!(),
@@ -116,6 +138,23 @@ mod test {
 			Outcome::Failure((
 				Status::BadRequest,
 				Error::Invalid(HASURA_USER_ID_HEADER_KEY)
+			))
+		)
+	}
+
+	#[rstest]
+	fn from_invalid_github_user_id() {
+		let mut header_map = HeaderMap::new();
+		header_map.add(Header::new(
+			HASURA_GITHUB_USER_ID_HEADER_KEY,
+			"some random string",
+		));
+
+		assert_eq!(
+			OptionUserId::from_request_headers(&header_map),
+			Outcome::Failure((
+				Status::BadRequest,
+				Error::Invalid(HASURA_GITHUB_USER_ID_HEADER_KEY)
 			))
 		)
 	}
