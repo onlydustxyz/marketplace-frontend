541743605d5e8ca2b50f74f8c769aa28193c6ea7
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-01-04 19:09:08 +0100

    :lipstick: new project lead invitation component

---
+++ frontend/src/components/ProjectLeadInvitation/ProjectLeadInvitation.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import ProjectLeadInvitation from ".";
+
+export default {
+  title: "ProjectLeadInvitation",
+  component: ProjectLeadInvitation,
+} as ComponentMeta<typeof ProjectLeadInvitation>;
+
+const Template: ComponentStory<typeof ProjectLeadInvitation> = args => <ProjectLeadInvitation {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {}; // put your component's args for the Default story here
