e702193e1778755523bb37b1fbe45dcde5a69984
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-01-05 16:57:39 +0100

    feat(GithubProxy): add query to fetch a repository pull requests

--- backend/github-proxy/src/domain/github/service.rs
+++ backend/github-proxy/src/domain/github/service.rs
@@ -1,5 +1,7 @@
+use domain::GithubRepositoryId;
 use thiserror::Error;
 
+use super::PullRequest;
 use crate::domain::{GithubRepository, GithubUser};
 
 #[derive(Debug, Error)]
@@ -14,8 +16,13 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+#[allow(non_snake_case)]
 #[async_trait]
 pub trait Service: Send + Sync {
 	async fn fetch_repository_by_id(&self, id: u64) -> Result<GithubRepository>;
 	async fn fetch_user_by_name(&self, username: &str) -> Result<GithubUser>;
+	async fn fetch_repository_PRs(
+		&self,
+		repository_id: &GithubRepositoryId,
+	) -> Result<Vec<PullRequest>>;
 }
--- backend/github-proxy/src/domain/mod.rs
+++ backend/github-proxy/src/domain/mod.rs
@@ -1,6 +1,7 @@
 mod github;
 pub use github::{
-	File as GithubFile, FileEncoding as GithubFileEncoding, Repository as GithubRepository,
+	File as GithubFile, FileEncoding as GithubFileEncoding, PullRequest as GithubPullRequest,
+	PullRequestStatus as GithubPullRequestStatus, Repository as GithubRepository,
 	Service as GithubService, ServiceError as GithubServiceError,
 	ServiceResult as GithubServiceResult, User as GithubUser,
 };
