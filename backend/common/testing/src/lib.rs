906660d72abec70b0ea5dcaf79b8827dbc0f0e97
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-01-05 11:03:40 +0100

    🔊 instrument Postgres connections

--- backend/common/testing/src/lib.rs
+++ backend/common/testing/src/lib.rs
@@ -1,13 +1,14 @@
 pub mod fixtures;
 
-use diesel::{Connection, PgConnection};
+use diesel::Connection;
+use diesel_tracing::pg::InstrumentedPgConnection;
 use r2d2_diesel::ConnectionManager;
-type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+type Pool = r2d2::Pool<ConnectionManager<InstrumentedPgConnection>>;
 
 use infrastructure::database;
 
 pub fn init_pool(config: &database::Config) -> Pool {
-	let manager = ConnectionManager::<PgConnection>::new(config.url());
+	let manager = ConnectionManager::<InstrumentedPgConnection>::new(config.url());
 	let pool = Pool::builder().max_size(1).build(manager).unwrap();
 	pool.get().unwrap().begin_test_transaction().unwrap();
 	pool
