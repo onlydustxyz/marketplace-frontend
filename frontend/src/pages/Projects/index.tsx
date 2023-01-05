e33da0f5ab24c9f6f1dcf1c51e7391638b88f776
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-01-05 12:27:43 +0100

    :sparkles: project lead invite in projects list

--- frontend/src/pages/Projects/index.tsx
+++ frontend/src/pages/Projects/index.tsx
@@ -5,44 +5,68 @@ import Card from "src/components/Card";
 import ProjectInformation from "src/components/ProjectInformation";
 import QueryWrapper from "src/components/QueryWrapper";
 import { GITHUB_REPO_FIELDS_FOR_PROJECT_CARD_FRAGMENT } from "src/graphql/fragments";
+import { PENDING_PROJECT_LEADER_INVITATIONS_QUERY } from "src/graphql/queries";
+import { useAuth } from "src/hooks/useAuth";
 import { useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { HasuraUserRole } from "src/types";
-import { GetProjectsQuery } from "src/__generated/graphql";
+import hasProjectInvitation from "src/utils/hasProjectInvitation";
+import { GetProjectsQuery, PendingProjectLeaderInvitationsQuery } from "src/__generated/graphql";
 import { useT } from "talkr";
 
 export default function Projects() {
   const { T } = useT();
-  const query = useHasuraQuery<GetProjectsQuery>(GET_PROJECTS_QUERY, HasuraUserRole.Public);
-  const { data } = query;
+  const { isLoggedIn } = useAuth();
+
+  const getProjectsQuery = useHasuraQuery<GetProjectsQuery>(GET_PROJECTS_QUERY, HasuraUserRole.Public);
+  const pendingProjectLeaderInvitationsQuery = useHasuraQuery<PendingProjectLeaderInvitationsQuery>(
+    PENDING_PROJECT_LEADER_INVITATIONS_QUERY,
+    HasuraUserRole.RegisteredUser,
+    { skip: !isLoggedIn }
+  );
 
   return (
     <div className="container mx-auto pb-10 mt-10">
       <div className="text-3xl font-alfreda">{T("navbar.projects")}</div>
-      <QueryWrapper<GetProjectsQuery> query={query}>
+      <QueryWrapper<GetProjectsQuery> query={getProjectsQuery}>
         <div className="px-10 flex flex-col align-center items-center gap-5 mt-10">
-          {data &&
-            data.projects.map(project => (
+          {getProjectsQuery.data &&
+            getProjectsQuery.data.projects.map(project => (
               <Link
                 key={project.id}
                 className="flex w-11/12 my-3"
                 to={generatePath(RoutePaths.ProjectDetails, { projectId: project.id })}
               >
-                <Card selectable={true}>
-                  <ProjectInformation
-                    name={project.name}
-                    details={{
-                      description: project?.projectDetails?.description,
-                      telegramLink: project?.projectDetails?.telegramLink,
-                      logoUrl: project.projectDetails?.logoUrl || project.githubRepo?.content.logoUrl,
-                    }}
-                    lead={project?.projectLeads?.[0]?.user}
-                    githubRepoInfo={{
-                      owner: project?.githubRepo?.owner,
-                      name: project?.githubRepo?.name,
-                      contributors: project?.githubRepo?.content?.contributors,
-                      languages: project?.githubRepo?.languages,
-                    }}
-                  />
+                <Card
+                  selectable={true}
+                  className={
+                    hasProjectInvitation(pendingProjectLeaderInvitationsQuery, project.id) ? "bg-amber-700/20" : ""
+                  }
+                >
+                  <div className="flex flex-col gap-5">
+                    <ProjectInformation
+                      name={project.name}
+                      details={{
+                        description: project?.projectDetails?.description,
+                        telegramLink: project?.projectDetails?.telegramLink,
+                        logoUrl: project.projectDetails?.logoUrl || project.githubRepo?.content.logoUrl,
+                      }}
+                      lead={project?.projectLeads?.[0]?.user}
+                      githubRepoInfo={{
+                        owner: project?.githubRepo?.owner,
+                        name: project?.githubRepo?.name,
+                        contributors: project?.githubRepo?.content?.contributors,
+                        languages: project?.githubRepo?.languages,
+                      }}
+                    />
+                    {hasProjectInvitation(pendingProjectLeaderInvitationsQuery, project.id) && (
+                      <div className="flex flex-row justify-between items-center font-medium p-5 text-lg rounded-xl bg-amber-700/30">
+                        <div>You’ve been promoted to Project Lead</div>
+                        <div className="w-fit rounded-xl bg-neutral-100 shadow-inner shadow-neutral-100 py-2 px-4 text-chineseBlack">
+                          View invite
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </Card>
               </Link>
             ))}
