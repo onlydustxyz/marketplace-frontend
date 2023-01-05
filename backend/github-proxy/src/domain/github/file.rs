27e59aea92077cc794cb0333710655d9c614b02a
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-01-05 14:41:09 +0100

    refactor: remove unused and unexported struct

--- backend/github-proxy/src/domain/github/file.rs
+++ backend/github-proxy/src/domain/github/file.rs
@@ -1,17 +1,6 @@
 use derive_more::Constructor;
 use juniper::{GraphQLEnum, GraphQLObject};
 
-use crate::domain::GithubUser;
-
-#[derive(Constructor, GraphQLObject)]
-pub struct Repository {
-	id: i32,
-	owner: String,
-	name: String,
-	contributors: Vec<GithubUser>,
-	readme: File,
-}
-
 #[derive(Constructor, GraphQLObject)]
 pub struct File {
 	encoding: Encoding,
