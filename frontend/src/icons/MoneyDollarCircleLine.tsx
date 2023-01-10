6e7a1322bd5724964fbdbebe167328c36867d31b
Anthony Buisset <abuisset@gmail.com>
2023-01-10 13:43:23 +0100

    ♻️ use Dollar icom from remix-icons

---
+++ frontend/src/icons/MoneyDollarCircleLine.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  className?: string;
+}
+
+export default function MoneyDollarCircleLine({ className }: Props) {
+  return <i className={`ri-money-dollar-circle-line ${className}`} />;
+}
