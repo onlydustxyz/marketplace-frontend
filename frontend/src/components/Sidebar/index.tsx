7408c39add21bf342acf5663b51ad64d7114d067
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-11 18:51:26 +0100

    :lipstick: Add noisy background in the sidebar

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -2,7 +2,11 @@ type SidebarProps = React.PropsWithChildren;
 
 export default function Sidebar({ children }: SidebarProps) {
   return (
-    <div className={"flex flex-col shrink-0 w-96 gap-8 bg-white/[0.04] p-6 font-walsheim rounded-l-2xl"}>
+    <div
+      className={
+        "flex flex-col min-h-full shrink-0 w-96 gap-8 bg-noise-medium bg-white/[0.04] p-6 font-walsheim rounded-l-2xl"
+      }
+    >
       {children}
     </div>
   );
