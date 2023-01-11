69dad18b40c49dd1ce5043d1ae057e309127bf13
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-11 14:27:01 +0100

    :lipstick: Fix invitation design

--- frontend/src/components/ProjectLeadInvitation/index.tsx
+++ frontend/src/components/ProjectLeadInvitation/index.tsx
@@ -16,11 +16,11 @@ export default function ProjectLeadInvitation({ projectName, onClick }: ProjectL
       className="backdrop-blur-none bg-opacity-10 border-none"
       backgroundImageClassName="bg-no-repeat bg-cover"
     >
-      <div className="flex flex-row justify-between items-center font-medium px-3 py-1 text-lg">
-        <div>{T("projectLeadInvitation.prompt", { projectName })}</div>
+      <div className="flex flex-row justify-between items-center font-medium px-3 py-1">
+        <div className="text-lg">{T("projectLeadInvitation.prompt", { projectName })}</div>
         <div
           onClick={onClick}
-          className="flex flex-row justify-between items-center gap-5 w-fit rounded-xl bg-neutral-100 shadow-inner shadow-neutral-400 px-4 py-3 text-black hover:cursor-pointer"
+          className="flex flex-row justify-between items-center gap-2 w-fit rounded-xl bg-neutral-100 shadow-inner shadow-neutral-400 px-4 py-3 text-black hover:cursor-pointer"
         >
           <CheckLine className="text-black font-normal text-xl" />
           <div>{T("projectLeadInvitation.accept")}</div>
