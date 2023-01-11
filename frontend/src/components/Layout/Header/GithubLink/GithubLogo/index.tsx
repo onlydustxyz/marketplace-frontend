303691996fb541d8d85a05089517f32aa9bccece
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-11 01:01:47 +0100

    :lipstick: Header

--- frontend/src/components/Layout/Header/GithubLink/GithubLogo/index.tsx
+++ frontend/src/components/Layout/Header/GithubLink/GithubLogo/index.tsx
@@ -3,5 +3,5 @@ import { useIntl } from "src/hooks/useIntl";
 
 export default function GithubLogo() {
   const { T } = useIntl();
-  return <img className="w-7 hover:opacity-90" src={githubLogo} alt={T("images.githubLogo")} />;
+  return <img className="h-6 hover:opacity-90" src={githubLogo} alt={T("images.githubLogo")} />;
 }
