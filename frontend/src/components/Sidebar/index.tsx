288241983fd476c150cc47e350696d141d740c88
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-11 12:19:50 +0100

    :lipstick: Right pane in project details

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,5 +1,9 @@
 type SidebarProps = React.PropsWithChildren;
 
 export default function Sidebar({ children }: SidebarProps) {
-  return <div className={"flex flex-col gap-8 bg-white/[0.04] p-6 font-walsheim rounded-l-2xl"}>{children}</div>;
+  return (
+    <div className={"flex flex-col shrink-0 w-96 gap-8 bg-white/[0.04] p-6 font-walsheim rounded-l-2xl"}>
+      {children}
+    </div>
+  );
 }
