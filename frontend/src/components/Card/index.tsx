4415ef5ae2ab2a46293673edb8ed6e2b9bbb5d7d
Anthony Buisset <abuisset@gmail.com>
2023-01-12 12:03:50 +0100

    ✅ add e2e test

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -3,6 +3,7 @@ interface ProjectCardProps extends React.PropsWithChildren {
   backgroundImageUrl?: string;
   backgroundImageClassName?: string;
   className?: string;
+  dataTestId?: string;
 }
 
 export default function Card({
@@ -10,6 +11,7 @@ export default function Card({
   backgroundImageUrl,
   backgroundImageClassName = "",
   className = "",
+  dataTestId,
   children,
 }: ProjectCardProps) {
   return (
@@ -18,6 +20,7 @@ export default function Card({
         <div
           className={`w-full rounded-xl ${backgroundImageClassName}`}
           style={{ backgroundImage: `url(${backgroundImageUrl})`, backgroundPosition: "center" }}
+          data-testid={dataTestId}
         >
           <div
             className={`w-full bg-chineseBlack p-6 border-2 border-neutral-600/90 rounded-xl font-walsheim bg-opacity-50 backdrop-blur-xl ${
@@ -33,6 +36,7 @@ export default function Card({
           className={`w-full h-full bg-white/[0.02] backdrop-blur-4xl p-6 border border-stone-100/[0.08] rounded-xl font-walsheim ${
             selectable ? "transition hover:bg-opacity-80 hover:border-neutral-300/70 duration-400" : ""
           } ${className}`}
+          data-testid={dataTestId}
         >
           {children}
         </div>
