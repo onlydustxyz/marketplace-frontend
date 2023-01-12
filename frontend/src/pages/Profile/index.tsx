b0805249ec181fe2870504396c8608b6f23f9c8b
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-01-12 18:07:03 +0100

    :sparkles: showing profile info missing component

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,14 +1,15 @@
-import { gql } from "@apollo/client";
+import { gql, QueryResult } from "@apollo/client";
 import { useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { useAuth } from "src/hooks/useAuth";
 import { HasuraUserRole } from "src/types";
 import QueryWrapper from "src/components/QueryWrapper";
 import ProfileForm from "./components/ProfileForm";
 import { ProfileQuery } from "src/__generated/graphql";
+import InfoMissingBanner from "./components/InfoMissingBanner";
 
 const Profile: React.FC = () => {
   const { isLoggedIn } = useAuth();
-  const query = useHasuraQuery<ProfileQuery>(GET_PROFILE_QUERY, HasuraUserRole.RegisteredUser, {
+  const getProfileQuery = useHasuraQuery<ProfileQuery>(GET_PROFILE_QUERY, HasuraUserRole.RegisteredUser, {
     skip: !isLoggedIn,
     fetchPolicy: "network-only",
   });
@@ -16,12 +17,31 @@ const Profile: React.FC = () => {
   return (
     <div className="bg-space h-full">
       <div className="container mx-auto pt-16 h-full">
-        <div className="text-4xl font-alfreda mb-6">Edit profile</div>
-        <QueryWrapper query={query}>{query.data && <ProfileForm user={query.data.userInfo[0]} />}</QueryWrapper>
+        <div className="flex flex-col gap-6">
+          <div className="text-4xl font-alfreda mb-6">Edit profile</div>
+          {getProfileQuery.data && (
+            <QueryWrapper query={getProfileQuery}>
+              {isPaymentInfoMissing(getProfileQuery) && <InfoMissingBanner />}
+              {getProfileQuery.data && <ProfileForm user={getProfileQuery.data.userInfo[0]} />}
+            </QueryWrapper>
+          )}
+        </div>
       </div>
     </div>
   );
 };
+
+function isPaymentInfoMissing(queryResult: QueryResult<ProfileQuery>) {
+  const payoutSettings = queryResult?.data?.userInfo?.[0].payoutSettings;
+  return (
+    queryResult.data &&
+    !(
+      payoutSettings?.EthTransfer?.Address ||
+      payoutSettings?.EthTransfer?.Name ||
+      (payoutSettings?.WireTransfer?.IBAN && payoutSettings?.WireTransfer?.BIC)
+    )
+  );
+}
 
 export const GET_PROFILE_QUERY = gql`
   query Profile {
