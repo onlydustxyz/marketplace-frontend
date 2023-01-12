d40812c623b3f270dec358b9101ecfa6c5e96f82
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-12 20:15:24 +0100

    :bug: Fix (you) in project lead

--- frontend/src/components/LeadContributor/index.tsx
+++ frontend/src/components/LeadContributor/index.tsx
@@ -11,7 +11,7 @@ export default function ProjectLead({ displayName, avatarUrl }: ProjectLeadProps
     <div className="text-md text-neutral-300 font-bold flex flex-row gap-2 items-center">
       <img src={avatarUrl} className="w-3 md:w-4 h-3 md:h-4 rounded-full" />
       <div className="text-purple-700 truncate">{displayName}</div>
-      {user?.displayName && <div>(you)</div>}
+      {user?.displayName === displayName && <div>(you)</div>}
     </div>
   );
 }
