bc8308fdecbca5d5dae14a8958475cc2eeeb26f9
Anthony Buisset <abuisset@gmail.com>
2023-01-14 16:06:40 +0100

    💄 dropdown covers the sidebar content

--- frontend/src/pages/ProjectDetails/index.tsx
+++ frontend/src/pages/ProjectDetails/index.tsx
@@ -122,68 +122,70 @@ export default function ProjectDetails() {
               </BackLink>
             )}
             <div className="flex flex-col gap-6 divide-y divide-neutral-700 w-full">
-              <Listbox value={project} onChange={onChangeProjectFromDropdown} disabled={!currentProjectIsMine}>
-                <div className="flex flex-col w-full border-2 rounded-2xl border-neutral-700 divide-y divide-neutral-700 bg-white/[0.02]">
-                  <Listbox.Button
-                    className={`p-4 font-medium text-2xl ${currentProjectIsMine ? "hover:cursor-pointer" : ""}`}
-                  >
-                    <div className="flex flex-row gap-3 items-center">
-                      <RoundedImage src={logoUrl} alt="Project Logo" className="object-cover w-8 h-8" />
-                      <div className="truncate grow font-belwe text-left">{project.name}</div>
-                      {currentProjectIsMine && <UpDownChevrons className="h-5 w-5 fill-gray-400" />}
-                    </div>
-                  </Listbox.Button>
-                  <Listbox.Options className="flex flex-col divide-y">
-                    {projects.map(projectFromDropdown => (
-                      <Listbox.Option
-                        key={projectFromDropdown.id}
-                        value={projectFromDropdown}
-                        className={`hover:cursor-pointer p-4 hover:bg-white/10 border-neutral-600 duration-300 last:rounded-b-2xl ${
-                          getInvitationForProject(projectFromDropdown.id) && "bg-orange-400/10  hover:bg-amber-700/30"
-                        } `}
-                      >
-                        <div className="flex flex-col gap-5">
-                          <div className="flex flex-row gap-5 items-center">
-                            <RoundedImage
-                              src={
-                                projectFromDropdown?.projectDetails?.logoUrl ||
-                                projectFromDropdown?.githubRepo?.content?.logoUrl ||
-                                onlyDustLogo
-                              }
-                              alt="Project Logo"
-                              className="object-cover w-10 h-10"
-                            />
-                            <div className="flex flex-col flex-1 justify-self-start truncate">
-                              <div className="truncate text-base font-medium">{projectFromDropdown.name}</div>
-                              <div className="truncate text-sm font-regular text-slate-400">
-                                {projectFromDropdown.githubRepo?.content?.contributors?.length ?? 0}{" "}
-                                {T("project.details.sidebar.contributors")}
-                              </div>
-                            </div>
-                            <>
-                              {getInvitationForProject(projectFromDropdown.id) ? (
-                                <div className="flex flex-row px-2 py-1 rounded-2xl bg-orange-400 items-center gap-1 text-xs text-black">
-                                  <ShootingStar />
-                                  <div>{T("project.details.sidebar.newInvite")}</div>
+              <div className="relative h-16">
+                <Listbox value={project} onChange={onChangeProjectFromDropdown} disabled={!currentProjectIsMine}>
+                  <div className="flex flex-col w-full border-2 rounded-2xl border-neutral-700 divide-y divide-neutral-700 bg-white/[0.02] absolute backdrop-blur-4xl z-10">
+                    <Listbox.Button
+                      className={`p-4 font-medium text-2xl ${currentProjectIsMine ? "hover:cursor-pointer" : ""}`}
+                    >
+                      <div className="flex flex-row gap-3 items-center">
+                        <RoundedImage src={logoUrl} alt="Project Logo" className="object-cover w-8 h-8" />
+                        <div className="truncate grow font-belwe text-left">{project.name}</div>
+                        {currentProjectIsMine && <UpDownChevrons className="h-5 w-5 fill-gray-400" />}
+                      </div>
+                    </Listbox.Button>
+                    <Listbox.Options className="flex flex-col divide-y">
+                      {projects.map(projectFromDropdown => (
+                        <Listbox.Option
+                          key={projectFromDropdown.id}
+                          value={projectFromDropdown}
+                          className={`hover:cursor-pointer p-4 hover:bg-white/10 border-neutral-600 duration-300 last:rounded-b-2xl ${
+                            getInvitationForProject(projectFromDropdown.id) && "bg-orange-400/10  hover:bg-amber-700/30"
+                          } `}
+                        >
+                          <div className="flex flex-col gap-5">
+                            <div className="flex flex-row gap-5 items-center">
+                              <RoundedImage
+                                src={
+                                  projectFromDropdown?.projectDetails?.logoUrl ||
+                                  projectFromDropdown?.githubRepo?.content?.logoUrl ||
+                                  onlyDustLogo
+                                }
+                                alt="Project Logo"
+                                className="object-cover w-10 h-10"
+                              />
+                              <div className="flex flex-col flex-1 justify-self-start truncate">
+                                <div className="truncate text-base font-medium">{projectFromDropdown.name}</div>
+                                <div className="truncate text-sm font-regular text-slate-400">
+                                  {projectFromDropdown.githubRepo?.content?.contributors?.length ?? 0}{" "}
+                                  {T("project.details.sidebar.contributors")}
                                 </div>
-                              ) : (
-                                projectFromDropdown.id === projectId && (
-                                  <CheckLine className="text-gray-200 text-lg font-normal" />
-                                )
-                              )}
-                            </>
-                          </div>
-                          {getInvitationForProject(projectFromDropdown.id) && (
-                            <div className="bg-neutral-100 rounded-xl w-full text-black text-sm text-center p-2">
-                              View invite
+                              </div>
+                              <>
+                                {getInvitationForProject(projectFromDropdown.id) ? (
+                                  <div className="flex flex-row px-2 py-1 rounded-2xl bg-orange-400 items-center gap-1 text-xs text-black">
+                                    <ShootingStar />
+                                    <div>{T("project.details.sidebar.newInvite")}</div>
+                                  </div>
+                                ) : (
+                                  projectFromDropdown.id === projectId && (
+                                    <CheckLine className="text-gray-200 text-lg font-normal" />
+                                  )
+                                )}
+                              </>
                             </div>
-                          )}
-                        </div>
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </div>
-              </Listbox>
+                            {getInvitationForProject(projectFromDropdown.id) && (
+                              <div className="bg-neutral-100 rounded-xl w-full text-black text-sm text-center p-2">
+                                View invite
+                              </div>
+                            )}
+                          </div>
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </div>
+                </Listbox>
+              </div>
               <div className="flex flex-col align-start font-medium text-xl pt-3 pb-2 gap-2">
                 {availableTabs.map((tab: ProjectDetailsTab) => (
                   <div
