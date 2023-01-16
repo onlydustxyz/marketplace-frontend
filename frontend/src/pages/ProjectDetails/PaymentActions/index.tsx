57e5e8b593922bf8e3dbcaac584dbb99077634d3
Anthony Buisset <abuisset@gmail.com>
2023-01-16 22:44:56 +0100

    ♻️ move payment logic in payment component

--- frontend/src/pages/ProjectDetails/PaymentActions/index.tsx
+++ frontend/src/pages/ProjectDetails/PaymentActions/index.tsx
@@ -1,14 +1,14 @@
 import { gql } from "@apollo/client";
 import { useState } from "react";
 import Card from "src/components/Card";
-import PaymentTable, { mapApiPaymentsToProps } from "src/components/PaymentTable";
+import PaymentTable from "src/components/PaymentTable";
 import ProjectPaymentTableFallback from "src/components/ProjectPaymentTableFallback";
 import QueryWrapper from "src/components/QueryWrapper";
 import RemainingBudget from "src/components/RemainingBudget";
 import { useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { useIntl } from "src/hooks/useIntl";
-import { HasuraUserRole } from "src/types";
-import { GetPaymentRequestsForBudgetIdQuery } from "src/__generated/graphql";
+import { Currency, HasuraUserRole, PaymentStatus } from "src/types";
+import { GetPaymentRequestsForProjectQuery } from "src/__generated/graphql";
 import PaymentForm from "./PaymentForm";
 
 enum Action {
@@ -18,99 +18,102 @@ enum Action {
 
 interface PaymentsProps {
   projectId: string;
-  budget: {
-    remainingAmount: number;
-    initialAmount: number;
-    id: string;
-  };
 }
-export default function PaymentActions({ projectId, budget }: PaymentsProps) {
+
+export default function PaymentActions({ projectId }: PaymentsProps) {
   const { T } = useIntl();
 
   const [action, setAction] = useState<Action>(Action.List);
 
-  return (
-    <div className="flex flex-col gap-8 mt-3">
-      <div className="text-3xl font-belwe">{T("project.details.payments.title")}</div>
-      <div className="flex flex-row items-start gap-5">
-        <div className="flex basis-2/3">
-          {action === Action.Submit && <PaymentForm project={{ id: projectId, budget }} />}
-          {action === Action.List && (
-            <PaymentTableQueryContainer budgetId={budget.id} onClickSendPayment={() => setAction(Action.Submit)} />
-          )}
-        </div>
-        <div className="flex basis-1/3">
-          <Card>
-            <div className="flex flex-col gap-10 items-stretch w-full">
-              <RemainingBudget remainingAmount={budget.remainingAmount} initialAmount={budget.initialAmount} />
-              {budget.remainingAmount > 0 && (
-                <div
-                  className="bg-neutral-50 rounded-xl w-fit p-3 hover:cursor-pointer text-black"
-                  onClick={() => setAction(action === Action.List ? Action.Submit : Action.List)}
-                >
-                  {T(action === Action.List ? "payment.form.submit" : "payment.list")}
-                </div>
-              )}
-            </div>
-          </Card>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-interface PaymentTableQueryContainerProps {
-  budgetId: string;
-  onClickSendPayment: () => void;
-}
-
-function PaymentTableQueryContainer({ budgetId, onClickSendPayment }: PaymentTableQueryContainerProps) {
-  const query = useHasuraQuery<GetPaymentRequestsForBudgetIdQuery>(
-    GET_BUDGET_PAYMENTS_QUERY,
+  const query = useHasuraQuery<GetPaymentRequestsForProjectQuery>(
+    GET_PAYMENT_REQUESTS_FOR_PROJECT,
     HasuraUserRole.RegisteredUser,
     {
-      variables: { budgetId },
+      variables: { projectId },
     }
   );
-  const { data } = query;
-  const payments = data?.paymentRequests?.map(mapApiPaymentsToProps) ?? null;
-  const hasPayments = payments && payments.length > 0;
+
+  const budget = query.data?.projectsByPk?.budgets.reduce(
+    (acc, b) => ({
+      remainingAmount: acc.remainingAmount + b.remainingAmount,
+      initialAmount: acc.initialAmount + b.initialAmount,
+    }),
+    { initialAmount: 0, remainingAmount: 0 }
+  ) || { initialAmount: 0, remainingAmount: 0 };
+
+  const payments = query.data?.projectsByPk?.budgets.map(b => b.paymentRequests).flat() || [];
 
   return (
     <QueryWrapper query={query}>
-      <Card>
-        {hasPayments ? (
-          <PaymentTable payments={payments} />
-        ) : (
-          <ProjectPaymentTableFallback onClick={onClickSendPayment} />
-        )}
-      </Card>
+      <div className="flex flex-col gap-8 mt-3">
+        <div className="text-3xl font-belwe">{T("project.details.payments.title")}</div>
+        <div className="flex flex-row items-start gap-5">
+          <div className="flex basis-2/3">
+            {action === Action.Submit && <PaymentForm {...{ projectId, budget }} />}
+            {action === Action.List && (
+              <Card>
+                {payments.length > 0 ? (
+                  <PaymentTable payments={payments.map(mapPaymentRequestsFromQuery)} />
+                ) : (
+                  <ProjectPaymentTableFallback onClick={() => setAction(Action.Submit)} />
+                )}
+              </Card>
+            )}
+          </div>
+          <div className="flex basis-1/3">
+            <Card>
+              <div className="flex flex-col gap-10 items-stretch w-full">
+                <RemainingBudget {...budget} />
+                {budget.remainingAmount > 0 && (
+                  <div
+                    className="bg-neutral-50 rounded-xl w-fit p-3 hover:cursor-pointer text-black"
+                    onClick={() => setAction(action === Action.List ? Action.Submit : Action.List)}
+                  >
+                    {T(action === Action.List ? "payment.form.submit" : "payment.list")}
+                  </div>
+                )}
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
     </QueryWrapper>
   );
 }
 
-export const GET_BUDGET_PAYMENTS_QUERY = gql`
-  query GetPaymentRequestsForBudgetId($budgetId: uuid!) {
-    paymentRequests(where: { budgetId: { _eq: $budgetId } }) {
-      id
-      payments {
-        amount
-        currencyCode
-      }
-      amountInUsd
-      reason
-      budget {
-        project {
+const mapPaymentRequestsFromQuery = (paymentRequest: any) => {
+  const getPaidAmount = (payments: { amount: number }[]) =>
+    payments.reduce((total: number, payment: { amount: number }) => total + payment.amount, 0);
+
+  return {
+    id: paymentRequest.id,
+    amount: { value: paymentRequest.amountInUsd, currency: Currency.USD },
+    recipient: paymentRequest.githubRecipient,
+    reason: paymentRequest.reason?.work_items?.at(0),
+    status:
+      getPaidAmount(paymentRequest.payments) === paymentRequest.amountInUsd
+        ? PaymentStatus.ACCEPTED
+        : PaymentStatus.WAITING_PAYMENT,
+  };
+};
+
+export const GET_PAYMENT_REQUESTS_FOR_PROJECT = gql`
+  query GetPaymentRequestsForProject($projectId: uuid!) {
+    projectsByPk(id: $projectId) {
+      budgets {
+        initialAmount
+        remainingAmount
+        paymentRequests {
           id
-          name
-          projectDetails {
-            description
-            logoUrl
+          githubRecipient {
+            login
+            avatarUrl
           }
-          githubRepo {
-            content {
-              logoUrl
-            }
+          amountInUsd
+          reason
+          payments {
+            amount
+            currencyCode
           }
         }
       }
--- frontend/src/pages/ProjectDetails/Sidebar/index.tsx
+++ frontend/src/pages/ProjectDetails/Sidebar/index.tsx
@@ -1,126 +1,90 @@
-import { Listbox } from "@headlessui/react";
-import Sidebar from "src/components/Sidebar";
-import UpDownChevrons from "src/assets/icons/UpDownChevrons";
-import GithubLink from "src/components/GithubLink";
-import TelegramLink from "src/components/TelegramLink";
-import ShootingStar from "src/assets/icons/ShootingStar";
-import { RoutePaths } from "src/App";
-import BackLink from "src/components/BackLink";
-import CheckLine from "src/icons/CheckLine";
-import RoundedImage from "src/components/RoundedImage";
 import { ProjectDetailsTab, ProjectDetails } from "..";
-import { useIntl } from "src/hooks/useIntl";
-import { User } from "src/types";
-import { buildGithubLink } from "src/utils/stringUtils";
+import { HasuraUserRole } from "src/types";
+import View from "./View";
+import { useHasuraQuery } from "src/hooks/useHasuraQuery";
+import { GetProjectsForSidebarQuery } from "src/__generated/graphql";
+import QueryWrapper from "src/components/QueryWrapper";
+import { gql } from "@apollo/client";
+import { useAuth } from "src/hooks/useAuth";
+import { useMemo } from "react";
+import { sortBy } from "lodash";
+import { useProjectLeadInvitations } from "src/hooks/useProjectLeadInvitations";
+import onlyDustLogo from "assets/img/onlydust-logo.png";
 
 interface Props {
-  user: User | null;
   currentProject: ProjectDetails;
-  allProjects: ProjectDetails[];
-  onChangeProjectFromDropdown: (project: unknown) => void;
+  onProjectSelected: (projectId: string) => void;
   selectedTab: ProjectDetailsTab;
   availableTabs: ProjectDetailsTab[];
-  onTabClicked: (tab: ProjectDetailsTab) => void;
+  onTabSelected: (tab: ProjectDetailsTab) => void;
 }
 
 export default function ProjectsSidebar({
-  user,
   currentProject,
-  allProjects,
-  onChangeProjectFromDropdown,
+  onProjectSelected,
   selectedTab,
   availableTabs,
-  onTabClicked,
+  onTabSelected,
 }: Props) {
-  const { T } = useIntl();
+  const { isLoggedIn, ledProjectIds } = useAuth();
+  const { amIInvitedForProject, allInvitations } = useProjectLeadInvitations();
 
-  const isExpandable = currentProject.leads.find(lead => lead.id === user?.id) || !!currentProject.invitationId;
+  const isProjectMine = (projectId: string) => ledProjectIds.includes(projectId) || !!amIInvitedForProject(projectId);
+
+  const getProjectsForSidebarQuery = useHasuraQuery<GetProjectsForSidebarQuery>(
+    GET_PROJECTS_FOR_SIDEBAR_QUERY,
+    HasuraUserRole.Public,
+    {
+      skip: !isLoggedIn,
+    }
+  );
+
+  const projects = useMemo(() => {
+    const projects = getProjectsForSidebarQuery?.data?.projects.filter(({ id }) => isProjectMine(id));
+    return sortBy(projects, project => !amIInvitedForProject(project.id));
+  }, [getProjectsForSidebarQuery?.data?.projects, ledProjectIds, allInvitations]);
+
+  const projectFromQuery = (project: any) => ({
+    id: project.id,
+    name: project.name,
+    logoUrl: project.projectDetails?.logoUrl || project.githubRepo?.content?.logoUrl || onlyDustLogo,
+    nbContributors: project.githubRepo?.content?.contributors?.length || 0,
+    withInvitation: amIInvitedForProject(project.id),
+  });
 
   return (
-    <Sidebar>
-      {!isExpandable && (
-        <BackLink to={RoutePaths.Projects} className="divide-none">
-          {T("project.details.sidebar.backToProjects")}
-        </BackLink>
-      )}
-      <div className="flex flex-col gap-6 divide-y divide-neutral-700 w-full">
-        <div className="relative h-16">
-          <Listbox value={currentProject} onChange={onChangeProjectFromDropdown} disabled={!isExpandable}>
-            <div className="flex flex-col w-full border-2 rounded-2xl border-neutral-700 divide-y divide-neutral-700 bg-white/[0.02] absolute backdrop-blur-4xl z-10">
-              <Listbox.Button className={`p-4 font-medium text-2xl ${isExpandable ? "hover:cursor-pointer" : ""}`}>
-                <div className="flex flex-row gap-3 items-center">
-                  <RoundedImage src={currentProject.logoUrl} alt="Project Logo" className="object-cover w-8 h-8" />
-                  <div className="truncate grow font-belwe text-left">{currentProject.name}</div>
-                  {isExpandable && <UpDownChevrons className="h-5 w-5 fill-gray-400" />}
-                </div>
-              </Listbox.Button>
-              <Listbox.Options className="flex flex-col divide-y">
-                {allProjects.map(project => (
-                  <Listbox.Option
-                    key={project.id}
-                    value={project}
-                    className={`hover:cursor-pointer p-4 hover:bg-white/10 border-neutral-600 duration-300 last:rounded-b-2xl ${
-                      project.invitationId && "bg-orange-400/10  hover:bg-amber-700/30"
-                    } `}
-                  >
-                    <div className="flex flex-col gap-5">
-                      <div className="flex flex-row gap-5 items-center">
-                        <RoundedImage src={project.logoUrl} alt="Project Logo" className="object-cover w-10 h-10" />
-                        <div className="flex flex-col flex-1 justify-self-start truncate">
-                          <div className="truncate text-base font-medium">{project.name}</div>
-                          <div className="truncate text-sm font-regular text-slate-400">
-                            {project.githubRepoInfo?.contributors?.length ?? 0}{" "}
-                            {T("project.details.sidebar.contributors")}
-                          </div>
-                        </div>
-                        <>
-                          {project.invitationId ? (
-                            <div className="flex flex-row px-2 py-1 rounded-2xl bg-orange-400 items-center gap-1 text-xs text-black">
-                              <ShootingStar />
-                              <div>{T("project.details.sidebar.newInvite")}</div>
-                            </div>
-                          ) : (
-                            project.id === currentProject.id && (
-                              <CheckLine className="text-gray-200 text-lg font-normal" />
-                            )
-                          )}
-                        </>
-                      </div>
-                      {project.invitationId && (
-                        <div className="bg-neutral-100 rounded-xl w-full text-black text-sm text-center p-2">
-                          View invite
-                        </div>
-                      )}
-                    </div>
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </div>
-          </Listbox>
-        </div>
-        <div className="flex flex-col align-start font-medium text-xl pt-3 pb-2 gap-2">
-          {availableTabs.map(tab => (
-            <div
-              key={tab}
-              className={`rounded-xl hover:cursor-pointer text-white text-base px-4 py-2.5 ${
-                selectedTab === tab ? "bg-white/[0.08]" : "text-neutral-400"
-              }`}
-              onClick={() => onTabClicked(tab)}
-              data-testid={`${tab}-tab`}
-            >
-              {tab}
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-row gap-2 pt-8">
-          {currentProject.telegramLink && <TelegramLink link={currentProject.telegramLink} />}
-          {currentProject.githubRepoInfo?.name && currentProject.githubRepoInfo.owner && (
-            <GithubLink
-              link={buildGithubLink(currentProject.githubRepoInfo?.name, currentProject.githubRepoInfo.owner)}
-            />
-          )}
-        </div>
-      </div>
-    </Sidebar>
+    <QueryWrapper query={getProjectsForSidebarQuery}>
+      <View
+        {...{
+          currentProject,
+          onProjectSelected,
+          selectedTab,
+          availableTabs,
+          onTabSelected,
+        }}
+        allProjects={projects.map(project => projectFromQuery(project))}
+        expandable={isProjectMine(currentProject.id)}
+      />
+    </QueryWrapper>
   );
 }
+
+export const GET_PROJECTS_FOR_SIDEBAR_QUERY = gql`
+  query GetProjectsForSidebar {
+    projects {
+      id
+      name
+      projectDetails {
+        logoUrl
+      }
+      githubRepo {
+        content {
+          contributors {
+            login
+          }
+          logoUrl
+        }
+      }
+    }
+  }
+`;
