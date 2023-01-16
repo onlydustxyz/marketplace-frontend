e67ccf80e4c581a2b067429c6eed78914a542409
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-01-16 18:40:50 +0100

    ✅ add e2e tests

--- cypress/support/commands/payment.js
+++ cypress/support/commands/payment.js
@@ -16,3 +16,45 @@ Cypress.Commands.add(
         };
     }
 );
+
+Cypress.Commands.add(
+    "cancelPaymentRequest",
+    (paymentId) => {
+        return {
+            query: `mutation($paymentId: Uuid!) {
+                cancelPaymentRequest(paymentId: $paymentId)
+            }`,
+            variables: {
+                paymentId
+            }
+        };
+    }
+);
+
+Cypress.Commands.add(
+    "paymentRequestShouldExist",
+    (paymentId) => {
+        return cy.graphql({query: `{
+            paymentRequestsByPk(id: "${paymentId}") {
+                id
+            }
+        }`})
+        .asAdmin()
+        .data("paymentRequestsByPk.id")
+        .should("be.a", "string");
+    }
+);
+
+Cypress.Commands.add(
+    "paymentRequestShouldNotExist",
+    (paymentId) => {
+        return cy.graphql({query: `{
+            paymentRequestsByPk(id: "${paymentId}") {
+                id
+            }
+        }`})
+        .asAdmin()
+        .should("have.nested.property", "body.data.paymentRequestsByPk")
+        .should("be.null");
+    }
+);
