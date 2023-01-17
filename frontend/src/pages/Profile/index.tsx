5c42e9ecf0416876861a950267c986944ea20001
Anthony Buisset <abuisset@gmail.com>
2023-01-17 14:02:41 +0100

    🐛 link submit button to profile form

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -41,6 +41,7 @@ const Profile: React.FC = () => {
               </button>
               <button
                 type="submit"
+                form="profile-form"
                 className="bg-neutral-50 text-base text-slate-900 whitespace-nowrap border-2 px-6 py-4 rounded-xl font-semibold shadow-inner"
               >
                 {T("profile.form.send")}
