487e5347a73da6602a6fd3189f8b618fc50c3df6
Anthony Buisset <abuisset@gmail.com>
2023-01-18 15:41:52 +0100

    💄 badass toaster border

--- frontend/src/components/Toaster/View.tsx
+++ frontend/src/components/Toaster/View.tsx
@@ -8,14 +8,12 @@ interface Props {
 export default function View({ message, className }: Props) {
   return (
     <div
-      className={`${
-        message ? "visible" : "invisible"
-      } flex items-center p-6 gap-2 backdrop-blur-xl rounded-2xl border-2 w-fit ${className}`}
+      className={`${message ? "visible" : "invisible"} rounded-2xl p-0.5 bg-multi-color-gradient w-fit ${className}`}
     >
-      <>
+      <div className={"flex items-center p-6 gap-2 bg-chineseBlack backdrop-blur-xl rounded-2xl"}>
         <CheckLine className="font-semibold text-2xl" />
         <span className="font-walsheim font-semibold text-lg">{message}</span>
-      </>
+      </div>
     </div>
   );
 }
