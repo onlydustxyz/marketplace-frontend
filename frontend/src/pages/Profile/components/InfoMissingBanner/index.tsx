bf4749768a45a7ff9bcfc3d2f5d8e6b198153441
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-01-19 22:37:33 +0100

    :lipstick: change background + minor style fixes

--- frontend/src/pages/Profile/components/InfoMissingBanner/index.tsx
+++ frontend/src/pages/Profile/components/InfoMissingBanner/index.tsx
@@ -1,5 +1,5 @@
-import { FC, PropsWithChildren } from "react";
-import headerElementBackground from "src/assets/img/header-element-background.png";
+import { PropsWithChildren } from "react";
+import headerElementBackground from "src/assets/img/payout-information-missing-banner-background.svg";
 import Card from "src/components/Card";
 import RiErrorWarningLine from "src/icons/RiErrorWarningLine";
 import { useT } from "talkr";
@@ -9,15 +9,15 @@ export default function InfoMissingBanner({ children }: PropsWithChildren) {
   return (
     <Card
       backgroundImageUrl={headerElementBackground}
-      className="bg-opacity-10 border-none backdrop-blur-2xl"
-      backgroundImageClassName="bg-no-repeat bg-cover top-500"
+      className="bg-opacity-0 backdrop-blur-0 border-none py-5"
+      backgroundImageClassName="bg-no-repeat bg-cover rounded-2xl"
     >
       <div className="flex flex-row justify-between">
-        <div className="flex flex-row justify-start items-center font-medium gap-5">
-          <RiErrorWarningLine className="p-3 px-4 text-4xl rounded-2xl bg-white/10" />
+        <div className="flex flex-row justify-start items-center font-medium gap-4">
+          <RiErrorWarningLine className="px-3 py-2.5 text-3xl rounded-2xl bg-white/10" />
           <div className="flex flex-col ">
-            <div className="text-xl font-medium">{T("profile.missing.title")}</div>
-            <div className="text-lg font-normal">{T("profile.missing.subtitle")}</div>
+            <div className="text-lg font-medium">{T("profile.missing.title")}</div>
+            <div className="text-sm font-normal">{T("profile.missing.subtitle")}</div>
           </div>
         </div>
         {children}
