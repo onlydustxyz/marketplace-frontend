feebfac284332043a5c646c0214c5eb5d0114e01
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-19 15:50:50 +0100

    :lipstick: Unify rounded images

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -23,7 +23,7 @@ export default function Card({
           data-testid={dataTestId}
         >
           <div
-            className={`w-full bg-chineseBlack p-6 border-2 border-neutral-600/90 rounded-xl font-walsheim bg-opacity-50 backdrop-blur-xl ${
+            className={`w-full bg-chineseBlack p-6 border-2 border-neutral-600/90 rounded-2xl font-walsheim bg-opacity-50 backdrop-blur-xl ${
               selectable ? "transition hover:bg-opacity-80 hover:border-neutral-300/90 duration-400" : ""
             } ${className}`}
           >
@@ -33,7 +33,7 @@ export default function Card({
       )}
       {!backgroundImageUrl && (
         <div
-          className={`w-full bg-white/[0.02] backdrop-blur-4xl p-6 border border-stone-100/[0.08] rounded-xl font-walsheim ${
+          className={`w-full bg-white/[0.02] backdrop-blur-4xl p-6 border border-stone-100/[0.08] rounded-2xl font-walsheim ${
             selectable ? "transition hover:bg-opacity-80 hover:border-neutral-300/70 duration-400" : ""
           } ${className}`}
           data-testid={dataTestId}
