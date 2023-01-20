d1b8589a4d68c3ed6b65fb3e50264031f88b54e9
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-01-20 17:25:38 +0100

    :lipstick: style button with box-shadow and hover

--- frontend/src/pages/MyContributions/CompletePaymentInformationButton/index.tsx
+++ frontend/src/pages/MyContributions/CompletePaymentInformationButton/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function CompletePaymentInformationButton({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-row justify-between items-center gap-2 w-fit rounded-xl bg-neutral-100 shadow-inner-bottom-sm hover:shadow-inner-bottom-md shadow-neutral-400 px-6 py-4 text-black text-base font-medium hover:cursor-pointer">
+    <div className="flex flex-row justify-between items-center gap-2 w-fit px-6 py-4 rounded-xl text-spaceBlue-900 text-base font-medium bg-greyscale-50 shadow-bottom-sm hover:cursor-pointer hover:text-spacePurple-900 hover:shadow-none hover:outline hover:outline-4 hover:outline-spacePurple-800  hover:bg-spacePurple-50 ">
       {children}
     </div>
   );
