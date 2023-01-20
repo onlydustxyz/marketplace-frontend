2449e2a0b3ce2eb7f4500c59559ff3d7092923af
Anthony Buisset <abuisset@gmail.com>
2023-01-20 16:55:50 +0100

    ♻️ refactor input component and create stories
    💄 style input on focus
    
    💄 display spinner inside input
    
    💄 style input label
    
    💄 style input error

--- frontend/src/components/FormInput/index.tsx
+++ frontend/src/components/FormInput/index.tsx
@@ -1,48 +1,44 @@
-import { memo, forwardRef } from "react";
+import { ChangeEventHandler } from "react";
 import { useFormContext, useFormState, RegisterOptions } from "react-hook-form";
-import LoaderIcon from "src/assets/icons/Loader";
+import View from "./View";
 
 type PropsType = {
-  label?: string;
+  label: string;
   type?: string;
   placeholder?: string;
   name: string;
   loading?: boolean;
   options?: RegisterOptions;
   value?: string | number;
-  onChange?: (value: any) => void;
+  onChange?: ChangeEventHandler<any>;
 };
 
-const Input: React.FC<PropsType> = ({
+export default function Input({
   label,
   type = "text",
   placeholder,
   name,
   value,
-  onChange,
   loading,
-  options = {},
-}) => {
+  options,
+  onChange,
+}: PropsType) {
   const { register } = useFormContext();
   const { errors } = useFormState({ name });
-  const error = errors[name];
 
   return (
-    <label html-for={name} className="flex flex-col flex-grow gap-3">
-      <div className="font-medium text-neutral-300">{label}</div>
-      <input
-        id={name}
-        placeholder={placeholder}
-        type={type}
-        {...register(name, options)}
-        className={error ? "border-2 border-rose-600" : "p-3"}
-        value={value}
-        onChange={onChange}
-      />
-      {loading && <LoaderIcon className="flex animate-spin place-items-center" />}
-      <span className="text-red-600 text-sm">{error?.message ? error.message.toString() : "\u00A0"}</span>
-    </label>
+    <View
+      {...{
+        name,
+        label,
+        error: errors[name],
+        loading,
+        placeholder,
+        type,
+        value,
+        register: register(name, options),
+        onChange,
+      }}
+    />
   );
-};
-
-export default memo(Input);
+}
