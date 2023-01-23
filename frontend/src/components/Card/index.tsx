a610e7cd26ca7cf5c00fc6e161d13d57f55b8e45
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-23 19:05:32 +0100

    :lipstick: Fix project card style

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -18,9 +18,9 @@ export default function Card({
 }: CardProps) {
   return (
     <div
-      className={`${className} w-full bg-white/2 backdrop-blur-lg p-6 border rounded-2xl font-walsheim ${
-        selectable ? "transition duration-300 hover:bg-white/4" : ""
-      } ${border === CardBorder.Light ? "border-greyscale-50/8" : "border-greyscale-50/12"} `}
+      className={`${className} w-full bg-white/2 backdrop-blur-lg p-6 outline outline-1 rounded-2xl font-walsheim ${
+        selectable ? "transition duration-300 hover:bg-white/4 outline-offset-0 hover:outline-2" : ""
+      } ${border === CardBorder.Light ? "outline-greyscale-50/8" : "outline-greyscale-50/12"} `}
       data-testid={dataTestId}
     >
       {children}
