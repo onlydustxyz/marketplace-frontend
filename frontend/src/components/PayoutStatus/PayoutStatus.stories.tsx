072955b66ac40d50821bd00016f73f30ef2030e5
Anthony Buisset <abuisset@gmail.com>
2023-01-23 18:59:07 +0100

    ✨ create PayoutStatus component

---
+++ frontend/src/components/PayoutStatus/PayoutStatus.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentStory } from "@storybook/react";
+import { JSXElementConstructor } from "react";
+import { PaymentStatus } from "src/types";
+import PayoutStatus from ".";
+
+export default {
+  title: "PayoutStatus",
+  argTypes: {
+    status: {
+      control: {
+        type: "select",
+        options: [PaymentStatus.WAITING_PAYMENT, PaymentStatus.ACCEPTED],
+      },
+    },
+  },
+};
+
+const Template: ComponentStory<JSXElementConstructor<typeof args>> = args => <PayoutStatus {...args} />;
+
+const args = {
+  status: PaymentStatus.WAITING_PAYMENT,
+  payoutInfoMissing: false,
+};
+
+export const Default = Template.bind({});
+Default.args = args;
+Default.parameters = {
+  backgrounds: {
+    default: "space",
+  },
+};
+
+export const PayoutInfoMissing = Template.bind({});
+PayoutInfoMissing.args = { ...args, payoutInfoMissing: true };
+PayoutInfoMissing.parameters = {
+  backgrounds: {
+    default: "space",
+  },
+};
