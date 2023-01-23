7d960cfb2be270d3cb21e9a2ac4dc868cec14aa4
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-23 18:16:34 +0100

    :lipstick: Connect with github button

--- frontend/src/components/Layout/Header/GithubLink/index.tsx
+++ frontend/src/components/Layout/Header/GithubLink/index.tsx
@@ -1,6 +1,6 @@
-import GithubLogo from "./GithubLogo";
 import config from "src/config";
 import { useIntl } from "src/hooks/useIntl";
+import GithubLogo from "src/icons/GithubLogo";
 
 const GITHUB_SIGN_IN_URL = `${config.HASURA_AUTH_BASE_URL}/signin/provider/github`;
 
@@ -8,9 +8,13 @@ export default function GithubLink() {
   const { T } = useIntl();
   return (
     <a href={GITHUB_SIGN_IN_URL}>
-      <div className="flex flex-row items-center gap-2.5 w-fit py-2 px-4 border-slate-400 border border-solid border-white rounded-3xl">
-        <div className="text-base md:flex hidden text-white font-bold">{T("navbar.signInWithGithub")}</div>
-        <GithubLogo />
+      <div className="absolute overflow-hidden transition duration-300 rounded-full m-px p-px hover:m-0  hover:p-0.5">
+        <div className="relative flex justify-center items-center bg-black hover:bg-spacePurple-900 rounded-full before:absolute before:-z-10 before:h-screen before:w-screen before:bg-multi-color-gradient hover:before:animate-spin-invert-slow">
+          <div className="flex flex-row items-center gap-2 w-fit py-1 px-2">
+            <GithubLogo className="text-3xl" />
+            <div className="md:flex hidden font-belwe mr-1">{T("navbar.signInWithGithub")}</div>
+          </div>
+        </div>
       </div>
     </a>
   );
