cc45005fb69989f57033fcfab97e807cdadaaf05
Anthony Buisset <abuisset@gmail.com>
2023-01-23 18:01:11 +0100

    ✨ new Tag component

---
+++ frontend/src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,59 @@
+import { ComponentStory } from "@storybook/react";
+import { JSXElementConstructor } from "react";
+import Tag, { TagBorderColor, TagIcon, TagIconColor, TagProps, TagSize } from ".";
+
+export default {
+  title: "Tag",
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+        options: [TagSize.Small, TagSize.Medium, TagSize.Large],
+      },
+    },
+    borderColor: {
+      control: {
+        type: "select",
+        options: [TagBorderColor.Grey, TagBorderColor.MultiColor],
+      },
+    },
+    icon: {
+      control: {
+        type: "select",
+        options: [TagIcon.Check, TagIcon.Time, TagIcon.Warning],
+      },
+    },
+    iconColor: {
+      control: {
+        type: "select",
+        options: [TagIconColor.Grey, TagIconColor.Orange, TagIconColor.Pink],
+      },
+    },
+  },
+};
+
+const Template: ComponentStory<JSXElementConstructor<TagProps>> = args => <Tag {...args} />;
+
+const args = {
+  size: TagSize.Medium,
+  label: "Processing",
+};
+
+export const Default = Template.bind({});
+Default.args = args;
+Default.parameters = {
+  backgrounds: {
+    default: "space",
+  },
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  ...args,
+  icon: TagIcon.Time,
+};
+WithIcon.parameters = {
+  backgrounds: {
+    default: "space",
+  },
+};
