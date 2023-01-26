ced954a1199daffa5d7f453fedf48394dcb87928
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-01-26 12:10:04 +0100

    refactor(front): use classNames utility and replace fushia color by spacePurple

--- frontend/src/components/ContributorsTable/SortingArrow.tsx
+++ frontend/src/components/ContributorsTable/SortingArrow.tsx
@@ -1,10 +1,12 @@
+import classNames from "classnames";
+
 type PropsType = {
   direction: "up" | "down";
   visible: boolean;
 };
 
 const SortingArrow: React.FC<PropsType> = ({ direction, visible }) => (
-  <i className={`ri-arrow-${direction}-line font-normal text-fuchsia-700 ${!visible && "invisible"}`} />
+  <i className={classNames(`ri-arrow-${direction}-line font-normal text-spacePurple-500`, { invisible: !visible })} />
 );
 
 export default SortingArrow;
--- frontend/src/components/Table/Line.tsx
+++ frontend/src/components/Table/Line.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {
@@ -9,12 +10,9 @@ interface Props extends PropsWithChildren {
 const Line: React.FC<Props> = ({ className, children, highlightOnHover }) => {
   return (
     <tr
-      className={`group/line border-b border-gray-800
-        ${
-          highlightOnHover &&
-          `transition duration-${highlightOnHover} hover:bg-white/4 outline-offset-0 hover:outline-2`
-        }
-        ${className}`}
+      className={classNames(`group/line border-b border-gray-800 ${className}`, {
+        highlightOnHover: `transition duration-${highlightOnHover} hover:bg-white/4 outline-offset-0 hover:outline-2`,
+      })}
     >
       {children}
     </tr>
