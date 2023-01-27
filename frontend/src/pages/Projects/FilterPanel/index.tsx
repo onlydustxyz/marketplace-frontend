5ed76fd6c08dd3a447c0ba95194ec851e1f839f9
Anthony Buisset <abuisset@gmail.com>
2023-01-27 15:22:19 +0100

    🐛 fix technology filter when no github repo

--- frontend/src/pages/Projects/FilterPanel/index.tsx
+++ frontend/src/pages/Projects/FilterPanel/index.tsx
@@ -12,8 +12,9 @@ export default function FilterPanel({ onTechnologiesChange }: Props) {
   const technologiesQuery = useHasuraQuery<GetAllTechnologiesQuery>(GET_ALL_TECHNOLOGIES_QUERY, HasuraUserRole.Public);
 
   const technologies = new Set(
-    technologiesQuery.data?.projects.map(p => Object.keys(p.githubRepo?.languages).slice(0, 2)).flat()
+    technologiesQuery.data?.projects.map(p => Object.keys(p.githubRepo?.languages || {}).slice(0, 2)).flat()
   );
+
   return (
     <View
       technologies={Array.from(technologies).sort((t1: string, t2: string) => t1.localeCompare(t2))}
