b04cb91b28817551eb5fd5c5ed2eaf198de7ad18
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-27 14:35:27 +0100

    :lipstick: Menu items style

--- frontend/src/components/Layout/Header/MenuItem/index.tsx
+++ frontend/src/components/Layout/Header/MenuItem/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
-import headerElementBackground from "assets/img/header-element-background.png";
+import underline from "assets/img/underline.png";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 interface MenuItemProps {
   path: string;
@@ -12,14 +13,18 @@ export default function MenuItem({ path, link, children, activeRegex }: MenuItem
   const active = (activeRegex || new RegExp(`^${link}.*`)).test(path);
   return (
     <div
-      className={`pb-0.5 flex justify-center align-center drop-shadow-lg saturate-200 outline-4 ${
-        active ? "text-white" : ""
-      }`}
-      style={active ? { backgroundImage: `url(${headerElementBackground})` } : {}}
+      className={classNames(
+        "pb-0.5 flex justify-center align-center drop-shadow-lg saturate-200 outline-4 text-base relative",
+        {
+          "text-white font-bold": active,
+          "text-greyscale-500": !active,
+        }
+      )}
     >
       <div className="bg-black py-1">
         <Link to={link}>{children}</Link>
       </div>
+      {active && <img className="absolute inset-x-0 bottom-0 mt-1 h-0.5 w-full" src={underline} alt="underline" />}
     </div>
   );
 }
