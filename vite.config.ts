9345d67d2048387d173649accaa7e7b4fadbdba4
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-01-27 11:28:50 +0100

    🔊 add Datadog RUM for frontend monitoring

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import istanbul from "vite-plugin-istanbul";
+import * as child from "child_process";
 
 export default defineConfig({
   plugins: [
@@ -19,6 +20,9 @@ export default defineConfig({
       src: path.resolve(__dirname, "./frontend/src"),
       assets: path.resolve(__dirname, "./frontend/src/assets"),
     },
+  },
+  define: {
+    APP_COMMIT_HASH: JSON.stringify(child.execSync("git rev-parse --short HEAD").toString()),
   },
   test: {
     globals: true,
