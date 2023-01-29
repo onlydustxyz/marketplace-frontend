e73e5c0c46ef4d78cae475e1b98fc6cc87d84350
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-29 14:06:57 +0100

    :sparkles: Add a reducer to handle days/hours logic

--- frontend/src/hooks/useWorkEstimation.test.ts
+++ frontend/src/hooks/useWorkEstimation.test.ts
@@ -1,14 +1,22 @@
 import { act, renderHook } from "@testing-library/react-hooks";
-import { BASE_RATE_USD, DEFAULT_NUMBER_OF_DAYS, Steps, useWorkEstimation } from "src/hooks/useWorkEstimation";
+import {
+  Action,
+  DAY_RATE_USD,
+  DEFAULT_NUMBER_OF_DAYS,
+  getReducer,
+  Steps,
+  useWorkEstimation,
+} from "src/hooks/useWorkEstimation";
 import { describe, expect, it, vi, beforeEach } from "vitest";
 
+const budget = {
+  initialAmount: 200,
+  remainingAmount: 10000,
+};
+
 describe("useWorkEstimation", () => {
-  const budget = {
-    initialAmount: 200,
-    remainingAmount: 10000,
-  };
   const onChange = vi.fn();
-  const initialAmountToPay = DEFAULT_NUMBER_OF_DAYS * BASE_RATE_USD;
+  const initialAmountToPay = DEFAULT_NUMBER_OF_DAYS * DAY_RATE_USD;
 
   beforeEach(() => {
     vi.resetAllMocks();
@@ -31,7 +39,7 @@ describe("useWorkEstimation", () => {
       result.current.tryIncreaseNumberOfDays();
     });
     expect(onChange).toHaveBeenCalledTimes(1);
-    expect(onChange).toHaveBeenCalledWith(initialAmountToPay + BASE_RATE_USD);
+    expect(onChange).toHaveBeenCalledWith(initialAmountToPay + DAY_RATE_USD);
   });
 
   it("should switch from days steps to hours steps", () => {
@@ -44,5 +52,113 @@ describe("useWorkEstimation", () => {
       result.current.tryDecreaseNumberOfDays();
     });
     expect(result.current.steps).toBe(Steps.Hours);
+  });
+});
+
+describe("reducer", () => {
+  const reducer = getReducer(budget);
+
+  it("increases days", () => {
+    const state = {
+      stepNumber: 1,
+      steps: Steps.Days,
+    };
+    const action = Action.Increase;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(2);
+  });
+
+  it("descreases days", () => {
+    const state = {
+      stepNumber: 2,
+      steps: Steps.Days,
+    };
+    const action = Action.Decrease;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(1);
+  });
+
+  it("switches from hours to days", () => {
+    const state = {
+      stepNumber: 7,
+      steps: Steps.Hours,
+    };
+    const action = Action.Increase;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(1);
+    expect(nextState.steps).toBe(Steps.Days);
+  });
+
+  it("switches from days to hours", () => {
+    const state = {
+      stepNumber: 1,
+      steps: Steps.Days,
+    };
+    const action = Action.Decrease;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(7);
+    expect(nextState.steps).toBe(Steps.Hours);
+  });
+
+  it("does not go over 20 days", () => {
+    const state = {
+      stepNumber: 20,
+      steps: Steps.Days,
+    };
+    const action = Action.Increase;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(20);
+    expect(nextState.steps).toBe(Steps.Days);
+  });
+
+  it("does not go below 1 hours", () => {
+    const state = {
+      stepNumber: 1,
+      steps: Steps.Hours,
+    };
+    const action = Action.Decrease;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(1);
+    expect(nextState.steps).toBe(Steps.Hours);
+  });
+
+  it("does not exceed budget in days", () => {
+    const lowBudget = {
+      initialAmount: 200,
+      remainingAmount: 5499,
+    };
+    const reducer = getReducer(lowBudget);
+
+    const state = {
+      stepNumber: 10,
+      steps: Steps.Days,
+    };
+    const action = Action.Increase;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(10);
+  });
+
+  it("does not exceed budget in days", () => {
+    const lowBudget = {
+      initialAmount: 110,
+      remainingAmount: 312,
+    };
+    const reducer = getReducer(lowBudget);
+
+    const state = {
+      stepNumber: 4,
+      steps: Steps.Hours,
+    };
+    const action = Action.Increase;
+    const nextState = reducer(state, action);
+
+    expect(nextState.stepNumber).toBe(4);
   });
 });
