2655cb283383dd9ff9a916317b5439d19185fca9
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-29 10:55:44 +0100

    :recycle: Separate work estimation view

---
+++ frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/WorkEstimation/View.tsx
@@ -0,0 +1,84 @@
+import Card from "src/components/Card";
+import { formatMoneyAmount } from "src/utils/money";
+import { useT } from "talkr";
+
+interface Props {
+  budget: { initialAmount: number; remainingAmount: number };
+  submitDisabled: boolean;
+  amountToPay: number;
+  numberOfDays: number;
+  tryIncreaseNumberOfDays: () => void;
+  tryDecreaseNumberOfDays: () => void;
+}
+
+export default function WorkEstimation({
+  tryIncreaseNumberOfDays,
+  tryDecreaseNumberOfDays,
+  budget,
+  amountToPay,
+  submitDisabled,
+  numberOfDays,
+}: Props) {
+  const { T } = useT();
+
+  return (
+    <Card>
+      <div className="flex flex-col gap-10 items-stretch justify-items-center w-full">
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-3xl">
+            <span className="font-walsheim font-black">{numberOfDays}</span> <span>{T("payment.form.days")}</span>
+          </div>
+          <div className="flex flex-row gap-3 text-white items-center">
+            <div className="border rounded-xl w-fit py-2 px-4 hover:cursor-pointer" onClick={tryDecreaseNumberOfDays}>
+              {T("payment.form.decrease")}
+            </div>
+            <div className="border rounded-xl w-fit py-2 px-4 hover:cursor-pointer" onClick={tryIncreaseNumberOfDays}>
+              {T("payment.form.increase")}
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-purple-400 rounded-full h-3">
+          <div
+            className="bg-purple-600 h-3 rounded-full"
+            style={{
+              width: `${Math.floor(
+                ((budget.initialAmount - budget.remainingAmount + amountToPay) * 100) / budget.initialAmount
+              )}%`,
+            }}
+          >
+            <div
+              className="bg-purple-800 h-3 rounded-full"
+              style={{
+                width: `${Math.floor(
+                  ((budget.initialAmount - budget.remainingAmount) * 100) /
+                    (budget.initialAmount - budget.remainingAmount + amountToPay)
+                )}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 font-medium">
+          <div className="flex flex-row justify-between">
+            <div>{T("payment.form.totalBudget")}</div>
+            <div>{formatMoneyAmount(budget.initialAmount)}</div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div>{T("payment.form.thisPayment")}</div>
+            <div className="text-purple-600">{formatMoneyAmount(amountToPay)}</div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div>{T("payment.form.leftToSpend")}</div>
+            <div>{formatMoneyAmount(budget.remainingAmount - amountToPay)}</div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          disabled={submitDisabled}
+          className=" border-white border-2 px-3 py-2 rounded-md bg-neutral-50 text-black"
+        >
+          {T("payment.form.confirm")}
+        </button>
+      </div>
+    </Card>
+  );
+}
--- frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/WorkEstimation/index.tsx
+++ frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/WorkEstimation/index.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "src/components/Card";
-import { formatMoneyAmount } from "src/utils/money";
-import { useT } from "talkr";
+import View from "./View";
 
 export const BASE_RATE_USD = 500;
 const DEFAULT_NUMBER_OF_DAYS = 2;
@@ -13,7 +11,6 @@ interface Props {
 }
 
 export default function WorkEstimation({ onChange, budget, submitDisabled }: Props) {
-  const { T } = useT();
   const [numberOfDays, setNumberOfDays] = useState(DEFAULT_NUMBER_OF_DAYS);
   const amountToPay = numberOfDays * BASE_RATE_USD;
 
@@ -35,63 +32,8 @@ export default function WorkEstimation({ onChange, budget, submitDisabled }: Pro
     }
   };
   return (
-    <Card>
-      <div className="flex flex-col gap-10 items-stretch justify-items-center w-full">
-        <div className="flex flex-row justify-between items-center">
-          <div className="text-3xl">
-            <span className="font-walsheim font-black">{numberOfDays}</span> <span>{T("payment.form.days")}</span>
-          </div>
-          <div className="flex flex-row gap-3 text-white items-center">
-            <div className="border rounded-xl w-fit py-2 px-4 hover:cursor-pointer" onClick={tryDecreaseNumberOfDays}>
-              {T("payment.form.decrease")}
-            </div>
-            <div className="border rounded-xl w-fit py-2 px-4 hover:cursor-pointer" onClick={tryIncreaseNumberOfDays}>
-              {T("payment.form.increase")}
-            </div>
-          </div>
-        </div>
-        <div className="w-full bg-purple-400 rounded-full h-3">
-          <div
-            className="bg-purple-600 h-3 rounded-full"
-            style={{
-              width: `${Math.floor(
-                ((budget.initialAmount - budget.remainingAmount + amountToPay) * 100) / budget.initialAmount
-              )}%`,
-            }}
-          >
-            <div
-              className="bg-purple-800 h-3 rounded-full"
-              style={{
-                width: `${Math.floor(
-                  ((budget.initialAmount - budget.remainingAmount) * 100) /
-                    (budget.initialAmount - budget.remainingAmount + amountToPay)
-                )}%`,
-              }}
-            ></div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-3 font-medium">
-          <div className="flex flex-row justify-between">
-            <div>{T("payment.form.totalBudget")}</div>
-            <div>{formatMoneyAmount(budget.initialAmount)}</div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div>{T("payment.form.thisPayment")}</div>
-            <div className="text-purple-600">{formatMoneyAmount(amountToPay)}</div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div>{T("payment.form.leftToSpend")}</div>
-            <div>{formatMoneyAmount(budget.remainingAmount - amountToPay)}</div>
-          </div>
-        </div>
-        <button
-          type="submit"
-          disabled={submitDisabled}
-          className=" border-white border-2 px-3 py-2 rounded-md bg-neutral-50 text-black"
-        >
-          {T("payment.form.confirm")}
-        </button>
-      </div>
-    </Card>
+    <View
+      {...{ amountToPay, numberOfDays, tryDecreaseNumberOfDays, tryIncreaseNumberOfDays, budget, submitDisabled }}
+    />
   );
 }
