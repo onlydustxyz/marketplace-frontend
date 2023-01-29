e5bb41f8aee6b0a614575c6884a7ccc5065b87b9
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-29 14:06:57 +0100

    :sparkles: Add a reducer to handle days/hours logic

--- frontend/src/hooks/useWorkEstimation.ts
+++ frontend/src/hooks/useWorkEstimation.ts
@@ -1,11 +1,77 @@
 import { useCallback, useEffect, useState } from "react";
 
-export const BASE_RATE_USD = 500;
+export const DAY_RATE_USD = 500;
 export const DEFAULT_NUMBER_OF_DAYS = 2;
 export enum Steps {
   Days = "days",
   Hours = "hours",
 }
+
+type Budget = {
+  initialAmount: number;
+  remainingAmount: number;
+};
+
+type State = {
+  stepNumber: number;
+  steps: Steps;
+};
+
+export enum Action {
+  Increase,
+  Decrease,
+}
+
+const maxSteps = {
+  [Steps.Days]: 20,
+  [Steps.Hours]: 7,
+};
+
+const rates = {
+  [Steps.Days]: DAY_RATE_USD,
+  [Steps.Hours]: DAY_RATE_USD / 8,
+};
+
+export const getReducer = (budget: Budget) => (state: State, action: Action) => {
+  switch (action) {
+    case Action.Increase: {
+      let nextState: State = {
+        stepNumber: state.stepNumber + 1,
+        steps: state.steps,
+      };
+      if (state.steps === Steps.Hours && state.stepNumber === maxSteps[state.steps]) {
+        nextState = {
+          stepNumber: 1,
+          steps: Steps.Days,
+        };
+      }
+      if (state.steps === Steps.Days && state.stepNumber === maxSteps[state.steps]) {
+        nextState = state;
+      }
+      if (budget.remainingAmount - nextState.stepNumber * rates[state.steps] < 0) {
+        nextState = state;
+      }
+
+      return nextState;
+    }
+    case Action.Decrease: {
+      let nextState: State = {
+        stepNumber: state.stepNumber - 1,
+        steps: state.steps,
+      };
+      if (state.steps === Steps.Days && state.stepNumber === 1) {
+        nextState = {
+          stepNumber: maxSteps[Steps.Hours],
+          steps: Steps.Hours,
+        };
+      }
+      if (state.steps === Steps.Hours && state.stepNumber === 1) {
+        nextState = state;
+      }
+      return nextState;
+    }
+  }
+};
 
 export const useWorkEstimation = (
   onChange: (amount: number) => void,
@@ -13,7 +79,7 @@ export const useWorkEstimation = (
 ) => {
   const [numberOfDays, setNumberOfDays] = useState(DEFAULT_NUMBER_OF_DAYS);
   const [steps, setSteps] = useState(Steps.Days);
-  const amountToPay = numberOfDays * BASE_RATE_USD;
+  const amountToPay = numberOfDays * DAY_RATE_USD;
 
   useEffect(() => {
     onChange(amountToPay);
@@ -24,7 +90,7 @@ export const useWorkEstimation = (
     if (numberOfDays == 0.5) {
       setSteps(Steps.Days);
     }
-    if (numberOfDays < 20 && budget.remainingAmount - (numberOfDays + increment) * BASE_RATE_USD >= 0) {
+    if (numberOfDays < 20 && budget.remainingAmount - (numberOfDays + increment) * DAY_RATE_USD >= 0) {
       setNumberOfDays(numberOfDays + increment);
     }
   }, [numberOfDays, budget]);
