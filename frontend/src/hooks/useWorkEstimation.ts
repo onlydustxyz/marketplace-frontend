b4217af0439ade9a2e59472be03e714d3931f5b9
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-29 11:23:22 +0100

    :sparkles: Add steps indicator

--- frontend/src/hooks/useWorkEstimation.ts
+++ frontend/src/hooks/useWorkEstimation.ts
@@ -2,12 +2,17 @@ import { useCallback, useEffect, useState } from "react";
 
 export const BASE_RATE_USD = 500;
 export const DEFAULT_NUMBER_OF_DAYS = 2;
+export enum Steps {
+  Days = "days",
+  Hours = "hours",
+}
 
 export const useWorkEstimation = (
   onChange: (amount: number) => void,
   budget: { initialAmount: number; remainingAmount: number }
 ) => {
   const [numberOfDays, setNumberOfDays] = useState(DEFAULT_NUMBER_OF_DAYS);
+  const [steps, setSteps] = useState(Steps.Days);
   const amountToPay = numberOfDays * BASE_RATE_USD;
 
   useEffect(() => {
@@ -16,6 +21,9 @@ export const useWorkEstimation = (
 
   const tryIncreaseNumberOfDays = useCallback(() => {
     const increment = numberOfDays < 1 ? 0.5 : 1;
+    if (numberOfDays == 0.5) {
+      setSteps(Steps.Days);
+    }
     if (numberOfDays < 20 && budget.remainingAmount - (numberOfDays + increment) * BASE_RATE_USD >= 0) {
       setNumberOfDays(numberOfDays + increment);
     }
@@ -24,6 +32,9 @@ export const useWorkEstimation = (
   const tryDecreaseNumberOfDays = useCallback(() => {
     if (numberOfDays > 0.5) {
       const decrement = numberOfDays == 1 ? 0.5 : 1;
+      if (numberOfDays == 1) {
+        setSteps(Steps.Hours);
+      }
       setNumberOfDays(numberOfDays - decrement);
     }
   }, [numberOfDays, budget]);
@@ -33,5 +44,6 @@ export const useWorkEstimation = (
     numberOfDays,
     tryIncreaseNumberOfDays,
     tryDecreaseNumberOfDays,
+    steps,
   };
 };
