0d6ebef969feb1bc0470f4d2c17caa2a9c05aa2e
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-01-30 15:29:57 +0100

    ♻️ refactor languages test

--- frontend/src/utils/languages.test.ts
+++ frontend/src/utils/languages.test.ts
@@ -1,48 +1,46 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect } from "vitest";
 import { getMostUsedLanguages } from "./languages";
+import { LanguageMap } from "src/types";
 
-describe("Languages", () => {
-  it("should return most used languages in descending order", () => {
-    const result = getMostUsedLanguages(
-      {
-        Cairo: 1000,
-        html: 100,
-        css: 10,
-        Rust: 10000,
-      },
-      2
-    );
-    expect(result).toStrictEqual(["Rust", "Cairo"]);
-  });
-
-  it("should return all languages in descending order when count is greater than language count", () => {
-    const result = getMostUsedLanguages(
-      {
-        Cairo: 1000,
-        html: 100,
-        css: 10,
-        Rust: 10000,
-      },
-      9
-    );
-    expect(result).toStrictEqual(["Rust", "Cairo", "html", "css"]);
-  });
-
-  it("should return languages in alphabetical order in case of equality", () => {
-    const result = getMostUsedLanguages(
-      {
-        Cairo: 100,
-        html: 100,
-        css: 100,
-        Rust: 100,
-      },
-      3
-    );
-    expect(result).toStrictEqual(["Cairo", "css", "html"]);
-  });
-
-  it("should return no language when there is nothing in language map", () => {
-    const result = getMostUsedLanguages({}, 2);
-    expect(result).toStrictEqual([]);
+describe.each([
+  {
+    languages: {
+      Cairo: 1000,
+      html: 100,
+      css: 10,
+      Rust: 10000,
+    },
+    count: 2,
+    expected: ["Rust", "Cairo"],
+  },
+  {
+    languages: {
+      Cairo: 1000,
+      html: 100,
+      css: 100,
+      Rust: 10000,
+    },
+    count: 9,
+    expected: ["Rust", "Cairo", "css", "html"],
+  },
+  {
+    languages: {
+      Cairo: 100,
+      html: 100,
+      css: 100,
+      Rust: 100,
+    },
+    count: 3,
+    expected: ["Cairo", "css", "html"],
+  },
+  {
+    languages: {},
+    count: 2,
+    expected: [],
+  },
+])("getMostUsedLanguages", ({ languages, count, expected }) => {
+  test("should return most used languages in descending order", async () => {
+    const result = getMostUsedLanguages(languages as LanguageMap, count);
+    expect(result).toStrictEqual(expected);
   });
 });
