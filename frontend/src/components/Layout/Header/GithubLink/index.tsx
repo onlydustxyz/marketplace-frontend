17b02332b235668ab5bb28f221957894d1457c7d
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-30 23:37:22 +0100

    :wrench: Add an env variable for login url

--- frontend/src/components/Layout/Header/GithubLink/index.tsx
+++ frontend/src/components/Layout/Header/GithubLink/index.tsx
@@ -2,12 +2,10 @@ import config from "src/config";
 import { useIntl } from "src/hooks/useIntl";
 import GithubLogo from "src/icons/GithubLogo";
 
-const GITHUB_SIGN_IN_URL = `${config.HASURA_AUTH_BASE_URL}/signin/provider/github`;
-
 export default function GithubLink() {
   const { T } = useIntl();
   return (
-    <a className="z-10" href={GITHUB_SIGN_IN_URL}>
+    <a className="z-10" href={config.LOGIN_URL}>
       <div className="overflow-hidden w-fit transition duration-300 rounded-full m-px p-px hover:m-0 hover:p-0.5">
         <div className="relative w-fit flex justify-center items-center bg-black hover:bg-spacePurple-900 rounded-full before:absolute before:-z-10 before:h-screen before:w-screen before:bg-multi-color-gradient hover:before:animate-spin-invert-slow">
           <div className="flex flex-row items-center gap-2 w-fit py-1 px-2">
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,7 @@ const config = {
   HASURA_AUTH_BASE_URL: import.meta.env.VITE_HASURA_AUTH_BASE_URL,
   HASURA_BASE_URL: import.meta.env.VITE_HASURA_BASE_URL,
   HASURA_ADMIN_SECRET: import.meta.env.VITE_HASURA_ADMIN_SECRET,
+  LOGIN_URL: import.meta.env.VITE_LOGIN_URL,
 };
 
 export default config;
