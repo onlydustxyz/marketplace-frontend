8a84ebebd62297601d9d3f26e8ee28becc9cd674
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-01-30 14:56:07 +0100

    🐛 sort languages to get the most used only

--- frontend/src/utils/stringUtils.ts
+++ frontend/src/utils/stringUtils.ts
@@ -3,9 +3,3 @@ import { LanguageMap } from "src/types";
 export const decodeBase64ToString = (x: string) => decodeURIComponent(escape(atob(x)));
 export const buildGithubLink = (repoOwner: string, repoName: string) =>
   `https://www.github.com/${repoOwner}/${repoName}`;
-export const buildLanguageString = (languageMap: LanguageMap) => {
-  return Object.keys(languageMap)
-    .slice(0, 2)
-    .map(str => str.toLowerCase())
-    .join(", ");
-};
