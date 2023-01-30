4744317cfe4e83c498c516203e544b173db763ad
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-30 10:27:51 +0100

    :lipstick: Spacings in the send payment card

--- frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/WorkEstimation/View.tsx
+++ frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/WorkEstimation/View.tsx
@@ -28,73 +28,75 @@ export default function WorkEstimation({
 
   return (
     <Card padded={false}>
-      <div
-        className={classNames(
-          "flex flex-col gap-5 items-stretch justify-items-center",
-          "p-8 w-full",
-          "bg-space-card bg-top bg-cover bg-no-repeat"
-        )}
-      >
-        <div className="flex flex-row justify-between items-center">
-          <div className="font-belwe">
-            <span className="text-5xl mr-2">{stepNumber}</span>
-            <span className="text-2xl">{T("payment.form.steps." + steps, { count: stepNumber })}</span>{" "}
-          </div>
-          <div className="flex flex-row gap-3 text-white items-center">
-            <div onClick={tryDecreaseNumberOfDays}>
-              <Button size={ButtonSize.Small} type={ButtonType.Secondary}>
-                {T("payment.form.decrease")}
-              </Button>
+      <div className="divide-y divide-greyscale-50/8">
+        <div
+          className={classNames(
+            "flex flex-col gap-5 items-stretch justify-items-center",
+            "p-8 pb-5 w-full",
+            "bg-space-card bg-top bg-cover bg-no-repeat"
+          )}
+        >
+          <div className="flex flex-row justify-between items-center">
+            <div className="font-belwe">
+              <span className="text-5xl mr-2">{stepNumber}</span>
+              <span className="text-2xl">{T("payment.form.steps." + steps, { count: stepNumber })}</span>{" "}
             </div>
-            <div onClick={tryIncreaseNumberOfDays}>
-              <Button size={ButtonSize.Small} type={ButtonType.Secondary}>
-                {T("payment.form.increase")}
-              </Button>
+            <div className="flex flex-row gap-3 text-white items-center">
+              <div onClick={tryDecreaseNumberOfDays}>
+                <Button size={ButtonSize.Small} type={ButtonType.Secondary}>
+                  {T("payment.form.decrease")}
+                </Button>
+              </div>
+              <div onClick={tryIncreaseNumberOfDays}>
+                <Button size={ButtonSize.Small} type={ButtonType.Secondary}>
+                  {T("payment.form.increase")}
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-full bg-purple-400 rounded-full h-3">
-          <div
-            className="bg-purple-600 h-3 rounded-full"
-            style={{
-              width: `${Math.floor(
-                ((budget.initialAmount - budget.remainingAmount + amountToPay) * 100) / budget.initialAmount
-              )}%`,
-            }}
-          >
+          <div className="w-full bg-purple-400 rounded-full h-3">
             <div
-              className="bg-purple-800 h-3 rounded-full"
+              className="bg-purple-600 h-3 rounded-full"
               style={{
                 width: `${Math.floor(
-                  ((budget.initialAmount - budget.remainingAmount) * 100) /
-                    (budget.initialAmount - budget.remainingAmount + amountToPay)
+                  ((budget.initialAmount - budget.remainingAmount + amountToPay) * 100) / budget.initialAmount
                 )}%`,
               }}
-            ></div>
+            >
+              <div
+                className="bg-purple-800 h-3 rounded-full"
+                style={{
+                  width: `${Math.floor(
+                    ((budget.initialAmount - budget.remainingAmount) * 100) /
+                      (budget.initialAmount - budget.remainingAmount + amountToPay)
+                  )}%`,
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="flex flex-col text-sm">
+            <div className="flex flex-row justify-between">
+              <div className="text-greyscale-300">{T("payment.form.totalBudget")}</div>
+              <div>{formatMoneyAmount(budget.initialAmount)}</div>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="text-greyscale-300">{T("payment.form.thisPayment")}</div>
+              <div className="text-purple-500">{formatMoneyAmount(amountToPay)}</div>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="text-greyscale-300">{T("payment.form.leftToSpend")}</div>
+              <div>{formatMoneyAmount(budget.remainingAmount - amountToPay)}</div>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col text-sm">
-          <div className="flex flex-row justify-between">
-            <div className="text-greyscale-300">{T("payment.form.totalBudget")}</div>
-            <div>{formatMoneyAmount(budget.initialAmount)}</div>
+        {!submitDisabled && (
+          <div className="p-6 pt-5 w-full">
+            <Button htmlType="submit" width={Width.Full}>
+              <span>{T("payment.form.confirm")}</span>
+            </Button>
           </div>
-          <div className="flex flex-row justify-between">
-            <div className="text-greyscale-300">{T("payment.form.thisPayment")}</div>
-            <div className="text-purple-500">{formatMoneyAmount(amountToPay)}</div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="text-greyscale-300">{T("payment.form.leftToSpend")}</div>
-            <div>{formatMoneyAmount(budget.remainingAmount - amountToPay)}</div>
-          </div>
-        </div>
+        )}
       </div>
-      {!submitDisabled && (
-        <div className="p-6 w-full">
-          <Button htmlType="submit" width={Width.Full}>
-            <span>{T("payment.form.confirm")}</span>
-          </Button>
-        </div>
-      )}
     </Card>
   );
 }
