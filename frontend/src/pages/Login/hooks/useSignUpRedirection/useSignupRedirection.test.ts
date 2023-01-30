ffc09096664fa532ac7f947313a1136ecb398fbd
Anthony Buisset <abuisset@gmail.com>
2023-01-30 17:53:56 +0100

    ✅ add unit tests

---
+++ frontend/src/pages/Login/hooks/useSignUpRedirection/useSignupRedirection.test.ts
@@ -0,0 +1,165 @@
+import { renderHook } from "@testing-library/react-hooks";
+import useSignupRedirection, {
+  PENDING_PROJECT_LEADER_INVITATIONS_QUERY,
+  PENDING_USER_PAYMENTS_AND_PAYOUT_SETTINGS,
+  User,
+} from ".";
+import { describe, expect, it, vi, beforeEach } from "vitest";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { RoutePaths } from "src/App";
+import { generatePath } from "react-router-dom";
+import { waitFor } from "@testing-library/react";
+
+const render = (user: User, mocks?: ReadonlyArray<MockedResponse>) =>
+  renderHook(() => useSignupRedirection(user), { wrapper: MockedProvider, initialProps: { mocks } });
+
+const githubUserId = 123456789;
+const userId = "user-id";
+const projectId = "project-id";
+
+const pendingProjectLeadInvitationMock = {
+  request: {
+    query: PENDING_PROJECT_LEADER_INVITATIONS_QUERY,
+    variables: { githubUserId },
+  },
+  result: {
+    data: {
+      pendingProjectLeaderInvitations: [
+        {
+          id: "invitation-id",
+          projectId,
+        },
+      ],
+    },
+  },
+};
+
+const pendingPaymentsMock = {
+  request: {
+    query: PENDING_USER_PAYMENTS_AND_PAYOUT_SETTINGS,
+    variables: { userId },
+  },
+  newData: vi.fn(() => ({})),
+};
+
+describe("useSignupRedirection", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("should return Projects url by default", () => {
+    const { result } = render({});
+    expect(result.current.loading).toBe(false);
+    expect(result.current.url).toBe(RoutePaths.Projects);
+  });
+
+  it("should return ProjectDetails if user is invited", async () => {
+    const { result } = render({ githubUserId }, [pendingProjectLeadInvitationMock]);
+    expect(result.current.loading).toBe(true);
+    await waitFor(() => expect(result.current.url).toBe(generatePath(RoutePaths.ProjectDetails, { projectId })));
+  });
+
+  it("should return ProfilePage if pending payments and no payout settings", async () => {
+    const mock = pendingPaymentsMock.newData.mockReturnValue({
+      data: {
+        user: {
+          userInfo: { payoutSettings: null },
+          githubUser: {
+            paymentRequests: [
+              {
+                amountInUsd: 100,
+                paymentsAggregate: { aggregate: { sum: { amount: null } } },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const { result } = render({ userId }, [pendingPaymentsMock]);
+    expect(result.current.loading).toBe(true);
+
+    await waitFor(() => {
+      expect(mock).toBeCalledTimes(1);
+      expect(result.current.url).toBe(RoutePaths.Profile);
+    });
+  });
+
+  it("should return Projects if pending payments but payout settings are filled", async () => {
+    const mock = pendingPaymentsMock.newData.mockReturnValue({
+      data: {
+        user: {
+          userInfo: { payoutSettings: {} },
+          githubUser: {
+            paymentRequests: [
+              {
+                amountInUsd: 100,
+                paymentsAggregate: { aggregate: { sum: { amount: null } } },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const { result } = render({ userId }, [pendingPaymentsMock]);
+    expect(result.current.loading).toBe(true);
+
+    await waitFor(() => {
+      expect(mock).toBeCalledTimes(1);
+      expect(result.current.url).toBe(RoutePaths.Projects);
+    });
+  });
+
+  it("should return Projects if no pending payments and no payout settings", async () => {
+    const mock = pendingPaymentsMock.newData.mockReturnValue({
+      data: {
+        user: {
+          userInfo: { payoutSettings: null },
+          githubUser: {
+            paymentRequests: [
+              {
+                amountInUsd: 100,
+                paymentsAggregate: { aggregate: { sum: { amount: 100 } } },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const { result } = render({ userId }, [pendingPaymentsMock]);
+    expect(result.current.loading).toBe(true);
+
+    await waitFor(() => {
+      expect(mock).toBeCalledTimes(1);
+      expect(result.current.url).toBe(RoutePaths.Projects);
+    });
+  });
+
+  it("should return Profile if both invited and pending payments with no payout settings", async () => {
+    const mock = pendingPaymentsMock.newData.mockReturnValue({
+      data: {
+        user: {
+          userInfo: { payoutSettings: null },
+          githubUser: {
+            paymentRequests: [
+              {
+                amountInUsd: 100,
+                paymentsAggregate: { aggregate: { sum: { amount: null } } },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const { result } = render({ userId, githubUserId }, [pendingPaymentsMock, pendingProjectLeadInvitationMock]);
+    expect(result.current.loading).toBe(true);
+
+    await waitFor(() => {
+      expect(mock).toBeCalledTimes(1);
+      expect(result.current.url).toBe(RoutePaths.Profile);
+    });
+  });
+});
