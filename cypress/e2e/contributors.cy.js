24f4fe9ca0ed6eb178952fba4f31a0328243c23c
Anthony Buisset <abuisset@gmail.com>
2023-01-31 13:03:31 +0100

    ✅ fix e2e tests

--- cypress/e2e/contributors.cy.js
+++ cypress/e2e/contributors.cy.js
@@ -1,15 +1,17 @@
 describe("As a public user, I", () => {
-    const OFUX = 595505;
-    const ANTHO = 990474;
+    const ANTHO = 43467246;
 
   beforeEach(function () {
     cy.createGithubUser(98735558)
       .then(user => {
-        cy.createProjectWithLeader(user, "Project with budget", 1000, 493591124)
+        cy.createProjectWithLeader(user, "Project with budget", 100000, 493591124)
           .as("projectId").then(projectId => {
-            cy.requestPayment(projectId, 100, OFUX, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68", "https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
-            cy.requestPayment(projectId, 100, OFUX, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
+            cy.requestPayment(projectId, 100, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68", "https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
+            cy.requestPayment(projectId, 100, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
             cy.requestPayment(projectId, 500, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
+            cy.requestPayment(projectId, 500, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
+            cy.requestPayment(projectId, 2000, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
+            cy.requestPayment(projectId, 10000, ANTHO, { workItems: ["https://github.com/onlydustxyz/starkonquest/pull/68"] }).asRegisteredUser(user).data("requestPayment");
           })
       });
   });
@@ -20,36 +22,24 @@ describe("As a public user, I", () => {
     cy.contains("Contributors").click();
 
     cy.get("#contributors_table tbody tr:nth-child(1)").within(() => {
-        cy.get("td:nth-child(1)").should("have.text", "abuisset");
-        cy.get("td:nth-child(2)").should("have.text", "500 $");
-        cy.get("td:nth-child(3)").should("have.text", "1");
+        cy.get("td:nth-child(1)").should(div => expect(div.text()).to.include("AnthonyBuisset"))
+        cy.get("td:nth-child(2)").should("have.text", "13200 $");
+        cy.get("td:nth-child(3)").should("have.text", "7");
     });
 
-    cy.get("#contributors_table tbody tr:nth-child(2)").within(() => {
-          cy.get("td:nth-child(1)").should("have.text", "ofux");
-          cy.get("td:nth-child(2)").should("have.text", "200 $");
-          cy.get("td:nth-child(3)").should("have.text", "3");
-      });
+    cy.get("#contributors_table tbody tr").its("length").should("be.greaterThan", 5);
   });
-
 
   it("can sort the contributors of a project", function () {
     cy.visit(`http://localhost:5173/projects/${this.projectId}`);
 
     cy.contains("Contributors").click();
-    cy.get("#contributors_table thead tr th:nth-child(3)").click();
-    cy.get("#contributors_table thead tr th:nth-child(3)").click(); // sort by paid contributions DESC
+    cy.get("#contributors_table thead tr th:nth-child(1)").click();  // sort by contributor name ASC
 
     cy.get("#contributors_table tbody tr:nth-child(1)").within(() => {
-        cy.get("td:nth-child(1)").should("have.text", "ofux");
-        cy.get("td:nth-child(2)").should("have.text", "200 $");
-        cy.get("td:nth-child(3)").should("have.text", "3");
-      });
-
-      cy.get("#contributors_table tbody tr:nth-child(2)").within(() => {
-        cy.get("td:nth-child(1)").should("have.text", "abuisset");
-        cy.get("td:nth-child(2)").should("have.text", "500 $");
-        cy.get("td:nth-child(3)").should("have.text", "1");
-      });
+        cy.get("td:nth-child(1)").should("have.text", "abdelhamidbakhta");
+        cy.get("td:nth-child(2)").should("have.text", "- $");
+        cy.get("td:nth-child(3)").should("have.text", "-");
+    });
   });
 });
