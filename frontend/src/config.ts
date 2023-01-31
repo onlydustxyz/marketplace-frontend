6a046152d9ab854aabfb35523591de0ce7a22af7
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-31 10:26:23 +0100

    :sparkles: Enable review app login from anywhere in the app

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ const config = {
   HASURA_AUTH_BASE_URL: import.meta.env.VITE_HASURA_AUTH_BASE_URL,
   HASURA_BASE_URL: import.meta.env.VITE_HASURA_BASE_URL,
   HASURA_ADMIN_SECRET: import.meta.env.VITE_HASURA_ADMIN_SECRET,
-  LOGIN_URL: import.meta.env.VITE_LOGIN_URL,
+  LOGIN_URL: import.meta.env.VITE_LOGIN_URL ?? `${import.meta.env.VITE_HASURA_AUTH_BASE_URL}/signin/provider/github`,
 };
 
 export default config;
