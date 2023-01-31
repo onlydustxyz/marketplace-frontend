5d2ee4ac74ab3b5b3739e03e286b7a233e503c7f
Stanislas Bernard <bernardstanislas@gmail.com>
2023-01-31 10:26:23 +0100

    :sparkles: Enable review app login from anywhere in the app

--- frontend/src/components/Layout/Header/GithubLink/index.tsx
+++ frontend/src/components/Layout/Header/GithubLink/index.tsx
@@ -2,10 +2,12 @@ import config from "src/config";
 import { useIntl } from "src/hooks/useIntl";
 import GithubLogo from "src/icons/GithubLogo";
 
+const loginUrl = `${config.LOGIN_URL}?redirect_url=${encodeURI(window.location.origin)}`;
+
 export default function GithubLink() {
   const { T } = useIntl();
   return (
-    <a className="z-10" href={config.LOGIN_URL}>
+    <a className="z-10" href={loginUrl}>
       <div className="overflow-hidden w-fit transition duration-300 rounded-full m-px p-px hover:m-0 hover:p-0.5">
         <div className="relative w-fit flex justify-center items-center bg-black hover:bg-spacePurple-900 rounded-full before:absolute before:-z-10 before:h-screen before:w-screen before:bg-multi-color-gradient hover:before:animate-spin-invert-slow">
           <div className="flex flex-row items-center gap-2 w-fit py-1 px-2">
