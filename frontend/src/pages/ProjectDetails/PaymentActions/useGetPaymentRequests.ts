361252c67f762e76ea195226d980fd470a4e0870
Anthony Buisset <abuisset@gmail.com>
2023-02-01 11:52:15 +0100

    ✨ e-321: use graphql subscription for payment requests listing
    ♻️ refactor useGetPaymentRequests in dedicated hook
    
    ➕ add graphql-ws
    
    🔧 add HASURA base websocket URL in config
    
    ✨ add Apollo graphql subscription support over WS
    
    ✨ use subscription for payment request listing
    
    ✨ use subscription for payment request listing

---
+++ frontend/src/pages/ProjectDetails/PaymentActions/useGetPaymentRequests.ts
@@ -0,0 +1,121 @@
+import { gql } from "@apollo/client";
+import { useState } from "react";
+import { useHasuraLazyQuery, useHasuraSubscription } from "src/hooks/useHasuraQuery";
+import { Currency, HasuraUserRole, PaymentStatus } from "src/types";
+import {
+  GetGithubUserQuery,
+  OnNewPaymentRequestsSubscription,
+  GithubUserFragment,
+  PaymentRequestFragment,
+} from "src/__generated/graphql";
+
+export default function useGetPaymentRequests(projectId: string) {
+  const fetchAllGithubRecipients = (query: OnNewPaymentRequestsSubscription) => {
+    const allRecipientIds = new Set(
+      query.projectsByPk?.budgets.map(b => b.paymentRequests.map(r => r.recipientId)).flat() || []
+    );
+    allRecipientIds.forEach(id => getGithubUser({ variables: { githubUserId: id } }));
+  };
+
+  const getPaymentRequestsQuery = useHasuraSubscription<OnNewPaymentRequestsSubscription>(
+    PAYMENT_REQUESTS_FOR_PROJECT_SUBSCRIPTION,
+    HasuraUserRole.RegisteredUser,
+    {
+      variables: { projectId },
+      onData: ({ data }) => data.data && fetchAllGithubRecipients(data.data),
+    }
+  );
+
+  const [githubRecipients, setGithubRecipients] = useState<Record<number, GithubUserFragment>>({});
+
+  const addGithubRecipient = (query: GetGithubUserQuery) =>
+    setGithubRecipients(recipients => ({
+      ...recipients,
+      [query.fetchUserDetailsById.id]: query.fetchUserDetailsById,
+    }));
+
+  const [getGithubUser] = useHasuraLazyQuery<GetGithubUserQuery>(GET_GITHUB_USER_QUERY, HasuraUserRole.RegisteredUser, {
+    onCompleted: addGithubRecipient,
+  });
+
+  const toPaymentRequest = (paymentRequest: PaymentRequestFragment) => {
+    const paidAmount = paymentRequest.payments.reduce((total, payment) => total + payment.amount, 0);
+    const recipient = githubRecipients[paymentRequest.recipientId];
+
+    return {
+      id: paymentRequest.id,
+      amount: { value: paymentRequest.amountInUsd, currency: Currency.USD },
+      recipient: recipient && {
+        id: recipient.id,
+        login: recipient.login,
+        avatarUrl: recipient.avatarUrl,
+      },
+      reason: paymentRequest.reason?.work_items?.at(0),
+      status: paidAmount === paymentRequest.amountInUsd ? PaymentStatus.ACCEPTED : PaymentStatus.WAITING_PAYMENT,
+    };
+  };
+
+  return {
+    ...getPaymentRequestsQuery,
+    data: getPaymentRequestsQuery.data && {
+      budget: getPaymentRequestsQuery.data.projectsByPk?.budgets.reduce(
+        (acc, b) => ({
+          remainingAmount: acc.remainingAmount + b.remainingAmount,
+          initialAmount: acc.initialAmount + b.initialAmount,
+        }),
+        { initialAmount: 0, remainingAmount: 0 }
+      ),
+      paymentRequests: getPaymentRequestsQuery.data.projectsByPk?.budgets
+        .map(b => b.paymentRequests)
+        .flat()
+        .map(toPaymentRequest),
+    },
+  };
+}
+
+const PAYMENT_REQUEST_FRAGMENT = gql`
+  fragment PaymentRequest on PaymentRequests {
+    id
+    recipientId
+    amountInUsd
+    reason
+    payments {
+      amount
+      currencyCode
+    }
+  }
+`;
+
+export const PAYMENT_REQUESTS_FOR_PROJECT_SUBSCRIPTION = gql`
+  ${PAYMENT_REQUEST_FRAGMENT}
+  subscription OnNewPaymentRequests($projectId: uuid!) {
+    projectsByPk(id: $projectId) {
+      id
+      budgets {
+        id
+        initialAmount
+        remainingAmount
+        paymentRequests {
+          ...PaymentRequest
+        }
+      }
+    }
+  }
+`;
+
+const GITHUB_USER_FRAGMENT = gql`
+  fragment GithubUser on User {
+    id
+    login
+    avatarUrl
+  }
+`;
+
+export const GET_GITHUB_USER_QUERY = gql`
+  ${GITHUB_USER_FRAGMENT}
+  query GetGithubUser($githubUserId: Int!) {
+    fetchUserDetailsById(userId: $githubUserId) {
+      ...GithubUser
+    }
+  }
+`;
