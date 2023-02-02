3a738e6ed0ab31755bd920b8e4c4726a6f3681f5
Anthony Buisset <abuisset@gmail.com>
2023-02-02 16:18:26 +0100

    ✨ create and register refreshers

--- backend/event-listeners/src/bin/refresh/main.rs
+++ backend/event-listeners/src/bin/refresh/main.rs
@@ -1,14 +1,28 @@
+use std::sync::Arc;
+
 use ::infrastructure::config;
 use anyhow::Result;
 use dotenv::dotenv;
 use event_listeners::Config;
-use infrastructure::tracing::Tracer;
+use infrastructure::{database, github, tracing::Tracer};
+
+mod refresher;
+use refresher::{Registrable, Registry};
 
 #[tokio::main]
 async fn main() -> Result<()> {
 	dotenv().ok();
 	let config: Config = config::load("backend/event-listeners/app.yaml")?;
 	let _tracer = Tracer::init(config.tracer(), "refresh")?;
+	let database = Arc::new(database::Client::new(database::init_pool(
+		config.database(),
+	)?));
+	let github = Arc::new(github::Client::new(config.github())?);
+
+	let mut registry = Registry::new();
+
+	refresher::project::create(database.clone(), github).register(&mut registry, "Project")?;
+	refresher::payment::create(database).register(&mut registry, "Payment")?;
 
 	Ok(())
 }
