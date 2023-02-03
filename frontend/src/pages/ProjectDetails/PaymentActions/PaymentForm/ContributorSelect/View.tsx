3a17cdfee0f11a7e87f744a2088e90ba8e5f25e0
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-02-03 14:58:00 +0100

    :children_crossing: e-336 add scrolling to dropdown

--- frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/ContributorSelect/View.tsx
+++ frontend/src/pages/ProjectDetails/PaymentActions/PaymentForm/ContributorSelect/View.tsx
@@ -87,22 +87,24 @@ const View = ({
           leaveFrom="transform translate-y-0 opacity-100"
           leaveTo="transform -translate-y-1/3 opacity-0"
         >
-          <Card className="bg-spaceBlue-900" padded={false}>
-            {contributors.map(contributor => (
-              <div
-                key={contributor.id}
-                className="px-4 py-3 hover:bg-white/2 cursor-pointer"
-                onMouseDown={() => onContributorChange(contributor)}
-              >
-                <Contributor
-                  contributor={{
-                    avatarUrl: contributor.avatarUrl,
-                    login: contributor.login,
-                    isRegistered: !!contributor.user?.userId,
-                  }}
-                />
-              </div>
-            ))}
+          <Card className="bg-spaceBlue-900 h-60 overflow-hidden" padded={false}>
+            <div className="overflow-scroll h-60">
+              {contributors.map(contributor => (
+                <div
+                  key={contributor.id}
+                  className="px-4 py-3 hover:bg-white/2 cursor-pointer"
+                  onMouseDown={() => onContributorChange(contributor)}
+                >
+                  <Contributor
+                    contributor={{
+                      avatarUrl: contributor.avatarUrl,
+                      login: contributor.login,
+                      isRegistered: !!contributor.user?.userId,
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
           </Card>
         </Transition>
       </div>
