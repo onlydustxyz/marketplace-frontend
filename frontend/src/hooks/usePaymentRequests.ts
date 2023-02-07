51ea3a3c3d20a6007698afa3abbaa78bf2e2f5b6
Anthony Buisset <abuisset@gmail.com>
2023-02-07 16:57:13 +0100

    ✨ update query cache after mutation success

--- frontend/src/hooks/usePaymentRequests.ts
+++ frontend/src/hooks/usePaymentRequests.ts
@@ -1,7 +1,13 @@
 import { gql } from "@apollo/client";
+import { cloneDeep } from "lodash";
 import { useHasuraMutation, useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { HasuraUserRole } from "src/types";
-import { GetPaymentRequestsForProjectQuery } from "src/__generated/graphql";
+import {
+  GetPaymentRequestsForProjectQuery,
+  PaymentRequestFragment,
+  RequestPaymentMutationResult,
+  RequestPaymentMutationVariables,
+} from "src/__generated/graphql";
 
 type Params = {
   projectId: string;
@@ -19,8 +25,37 @@ export default function usePaymentRequests({ projectId, onNewPaymentRequested }:
 
   const [requestNewPayment] = useHasuraMutation(REQUEST_PAYMENT_MUTATION, HasuraUserRole.RegisteredUser, {
     variables: { projectId },
-    onCompleted: () => {
-      onNewPaymentRequested && onNewPaymentRequested();
+    onCompleted: () => onNewPaymentRequested && onNewPaymentRequested(),
+    update: (cache, result, { variables }) => {
+      const { data } = result as RequestPaymentMutationResult;
+      const { amount, contributorId, projectId, reason } = variables as RequestPaymentMutationVariables;
+      const cachedQuery: GetPaymentRequestsForProjectQuery | null = cache.readQuery({
+        query: PAYMENT_REQUESTS_FOR_PROJECT_QUERY,
+        variables: { projectId },
+      });
+
+      const newPaymentRequest: PaymentRequestFragment = {
+        __typename: "PaymentRequests",
+        id: data?.requestPayment,
+        amountInUsd: amount,
+        recipientId: contributorId,
+        reason: { work_items: reason.workItems },
+        payments: [],
+        requestedAt: Date.now(),
+      };
+
+      const newQuery = cloneDeep(cachedQuery);
+      const budget = newQuery?.projectsByPk?.budgets.at(0);
+      if (budget) {
+        budget.paymentRequests.push(newPaymentRequest);
+        budget.remainingAmount -= amount;
+      }
+
+      cache.writeQuery({
+        query: PAYMENT_REQUESTS_FOR_PROJECT_QUERY,
+        data: newQuery,
+        variables: { projectId },
+      });
     },
   });
 
