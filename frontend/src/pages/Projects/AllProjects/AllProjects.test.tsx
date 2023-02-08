8673b9711dd8a89f58c5e3591717c46bcd43b60c
Oscar Roche <oscar.w.roche@gmail.com>
2023-02-08 14:54:47 +0100

    e-365 sort projects by descending money granted (#666)
    Co-authored-by: Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>

---
+++ frontend/src/pages/Projects/AllProjects/AllProjects.test.tsx
@@ -0,0 +1,95 @@
+import { describe, expect, it } from "vitest";
+import { screen } from "@testing-library/react";
+import matchers from "@testing-library/jest-dom/matchers";
+import { MemoryRouterProviderFactory, renderWithIntl } from "src/test/utils";
+import AllProjects, { buildGetProjectsQuery } from ".";
+
+expect.extend(matchers);
+
+const ALL_PROJECTS_RESULT_NO_INVITATIONS = {
+  data: {
+    projects: [
+      {
+        __typename: "Projects",
+        id: "1",
+        name: "project1",
+        totalSpentAmountInUsd: 1000,
+        pendingInvitations: [],
+      },
+      {
+        __typename: "Projects",
+        id: "2",
+        name: "project2",
+        totalSpentAmountInUsd: 999,
+        pendingInvitations: [],
+      },
+    ],
+  },
+};
+
+const ALL_PROJECTS_RESULT_WITH_INVITATION = {
+  data: {
+    projects: [
+      {
+        __typename: "Projects",
+        id: "1",
+        name: "project1",
+        totalSpentAmountInUsd: 1000,
+        pendingInvitations: [],
+      },
+      {
+        __typename: "Projects",
+        id: "2",
+        name: "project1",
+        totalSpentAmountInUsd: 999,
+        pendingInvitations: [],
+      },
+      {
+        __typename: "Projects",
+        id: "3",
+        name: "project2",
+        totalSpentAmountInUsd: 0,
+        pendingInvitations: ["test"],
+      },
+    ],
+  },
+};
+
+const buildGraphQlMocks = (projectsQueryResult: any) => [
+  {
+    request: {
+      query: buildGetProjectsQuery([]),
+      variables: { languages: [] },
+    },
+    result: projectsQueryResult,
+  },
+];
+
+describe("All projects", () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  it("should sort by money granted desc if no pending invitations", async () => {
+    renderWithIntl(<AllProjects technologies={[]} />, {
+      wrapper: MemoryRouterProviderFactory({
+        mocks: buildGraphQlMocks(ALL_PROJECTS_RESULT_NO_INVITATIONS),
+      }),
+    });
+    const moneyGrantedElementsInOrderOfAppearance = await screen.findAllByText("granted", { exact: false });
+    expect(moneyGrantedElementsInOrderOfAppearance[0]).toHaveTextContent("$1,000");
+    expect(moneyGrantedElementsInOrderOfAppearance[1]).toHaveTextContent("$999");
+  });
+
+  it("should sort by pending invitation, then money granted desc if pendign invitations", async () => {
+    renderWithIntl(<AllProjects technologies={[]} />, {
+      wrapper: MemoryRouterProviderFactory({
+        mocks: buildGraphQlMocks(ALL_PROJECTS_RESULT_WITH_INVITATION),
+      }),
+    });
+    const moneyGrantedElementsInOrderOfAppearance = await screen.findAllByText("granted", { exact: false });
+    expect(moneyGrantedElementsInOrderOfAppearance[0]).toHaveTextContent("$0");
+    expect(moneyGrantedElementsInOrderOfAppearance[1]).toHaveTextContent("$1,000");
+    expect(moneyGrantedElementsInOrderOfAppearance[2]).toHaveTextContent("$999");
+  });
+});
