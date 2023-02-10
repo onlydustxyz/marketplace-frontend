527c15b2ed8e6b9d4759259b4cacffbd938f1d5b
Anthony Buisset <abuisset@gmail.com>
2023-02-10 17:22:02 +0100

    🚑 fix budget projection upon payment cancelled

--- cypress/e2e/project_details_overview.cy.js
+++ cypress/e2e/project_details_overview.cy.js
@@ -1,6 +1,7 @@
 const GITHUB_REPO_ID_WITHOUT_README = 584840242;
 const GITHUB_REPO_ID_EMPTY_REPO = 584839416;
 const STARKONQUEST_ID = 481932781;
+const OFUX = 595505;
 
 describe("A project without readme", () => {
   beforeEach(() => {
@@ -24,7 +25,7 @@ describe("A project without readme", () => {
 
 describe("A project", () => {
   beforeEach(() => {
-    cy.createGithubUser(98735558).then(user => {
+    cy.createGithubUser(98735558).as("user").then(user => {
       cy.createProjectWithLeader(user, "Starkonquest", 1000, STARKONQUEST_ID).as("projectId");
     });
   });
@@ -32,6 +33,29 @@ describe("A project", () => {
   it("should render properly in public view", function () {
     cy.visit(`http://127.0.0.1:5173/projects/${this.projectId}`);
     cy.contains("CONTRIBUTORS");
+  });
+
+  it.only("should display total budget granted", function() {
+    cy.visit(`http://127.0.0.1:5173/projects/${this.projectId}`);
+    cy.get('[data-testid="money-granted-amount"]').should("have.text", "$0");
+
+    cy.requestPayment(this.projectId, 500, OFUX, { workItems: ["https://github.com/onlydustxyz/marketplace/pull/504"] })
+        .asRegisteredUser(this.user)
+        .data("requestPayment")
+        .as("paymentId");
+
+    cy.requestPayment(this.projectId, 200, OFUX, { workItems: ["https://github.com/onlydustxyz/marketplace/pull/525"] })
+        .asRegisteredUser(this.user)
+        .data("requestPayment");
+
+
+    cy.reload();
+    cy.get('[data-testid="money-granted-amount"]').should("have.text", "$700");
+
+    cy.get("@paymentId").then(paymentId =>cy.cancelPaymentRequest(this.projectId, paymentId).asAdmin())
+
+    cy.reload();
+    cy.get('[data-testid="money-granted-amount"]').should("have.text", "$200");
   });
 });
 
