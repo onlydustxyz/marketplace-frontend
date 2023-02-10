c30b1304986f01652d2cc1dd73794cfb1cbfecbb
Stanislas Bernard <bernardstanislas@gmail.com>
2023-02-10 20:44:39 +0100

    :green_heart: Fix sidebar story

--- frontend/src/pages/ProjectDetails/Sidebar/Sidebar.stories.tsx
+++ frontend/src/pages/ProjectDetails/Sidebar/Sidebar.stories.tsx
@@ -1,11 +1,13 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ProjectDetailsTab } from "src/pages/ProjectDetails/Sidebar";
+import { withRouter } from "storybook-addon-react-router-v6";
 import { ProjectDetailsTab__deprecated } from "../ProjectDetailsContext";
 import View from "./View";
 
 export default {
   title: "ProjectsSidebar",
   component: View,
+  decorators: [withRouter],
 } as ComponentMeta<typeof View>;
 
 const availableTabs__deprecated = [
@@ -16,7 +18,7 @@ const availableTabs__deprecated = [
 const availableTabs: ProjectDetailsTab[] = [];
 
 const currentProject = {
-  id: "test-projct-id",
+  id: "test-project-id",
   name: "Our project",
   logoUrl: "https://avatars.githubusercontent.com/u/98735558?v=4",
   leads: [
@@ -29,11 +31,26 @@ const currentProject = {
   nbContributors: 4,
   withInvitation: false,
 };
+const otherProject = {
+  id: "other-project-id",
+  name: "Other project",
+  logoUrl: "https://avatars.githubusercontent.com/u/98735558?v=4",
+  leads: [
+    {
+      id: "leader-id",
+      displayName: "Leader",
+      avatarUrl: "https://avatars.githubusercontent.com/u/98735558?v=4",
+    },
+  ],
+  nbContributors: 4,
+  withInvitation: false,
+};
+
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const empty = () => {};
 const selectedTab = ProjectDetailsTab__deprecated.Overview;
 const expandable = true;
-const allProjects = [currentProject, currentProject, currentProject];
+const allProjects = [currentProject, otherProject];
 
 const Template: ComponentStory<typeof View> = () => (
   <View
