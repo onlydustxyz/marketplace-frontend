27f3435ac76c6bc7cf87b50391eb599c2e7e4e57
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-02-13 17:47:55 +0100

    ✅ fix Header story

--- frontend/src/components/Layout/Header/Header.stories.tsx
+++ frontend/src/components/Layout/Header/Header.stories.tsx
@@ -3,6 +3,9 @@ import { RoutePaths } from "src/App";
 import { AuthContext, AuthContextType } from "src/hooks/useAuth";
 import { User } from "src/types";
 import { withRouter } from "storybook-addon-react-router-v6";
+import { GET_USER_IDENTITY_QUERY } from "./FeedbackButton";
+import { SuspenseCache } from "@apollo/client";
+import { MockedProvider } from "@apollo/client/testing";
 
 import Header from "./View";
 
@@ -11,6 +14,8 @@ export default {
   component: Header,
   decorators: [withRouter],
 } as ComponentMeta<typeof Header>;
+
+const USER_ID = "e2ee731a-2697-4306-bf4b-c807f6fda0d7";
 
 const MockAuthProvider = ({ children }: { children: React.ReactNode }) => {
   const mockedValue: AuthContextType = {
@@ -22,6 +27,8 @@ const MockAuthProvider = ({ children }: { children: React.ReactNode }) => {
     logout: () => Promise.resolve(),
     roles: [],
     user: {
+      id: USER_ID,
+      email: "le@chinoix.fr",
       displayName: "lechinoix",
       avatarUrl: "https://avatars.githubusercontent.com/u/10167015?v=4",
     } as unknown as User,
@@ -30,10 +37,37 @@ const MockAuthProvider = ({ children }: { children: React.ReactNode }) => {
   return <AuthContext.Provider value={mockedValue}>{children}</AuthContext.Provider>;
 };
 
+const mocks = [
+  {
+    request: {
+      query: GET_USER_IDENTITY_QUERY,
+      variables: { userId: USER_ID },
+    },
+    result: {
+      data: {
+        userInfo: [
+          {
+            identity: {
+              Person: {
+                lastname: "Bar",
+                firstname: "Foo",
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+];
+
+const suspenseCache = new SuspenseCache();
+
 const Template: ComponentStory<typeof Header> = args => (
-  <MockAuthProvider>
-    <Header {...args} />
-  </MockAuthProvider>
+  <MockedProvider mocks={mocks} suspenseCache={suspenseCache}>
+    <MockAuthProvider>
+      <Header {...args} />
+    </MockAuthProvider>
+  </MockedProvider>
 );
 
 export const Default = Template.bind({});
