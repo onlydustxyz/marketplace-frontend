83329946ee21d35c769b11ff866ae1d028788521
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-02-13 19:15:36 +0100

    :recycle: add feature flag

--- frontend/src/components/ApolloWrapper/index.tsx
+++ frontend/src/components/ApolloWrapper/index.tsx
@@ -13,9 +13,10 @@ import { onError } from "@apollo/client/link/error";
 
 import config from "src/config";
 import ErrorFallback from "../ErrorFallback";
-import { LOCAL_STORAGE_TOKEN_SET_KEY } from "src/hooks/useTokenSet";
+import { LOCAL_STORAGE_TOKEN_SET_KEY, useTokenSet } from "src/hooks/useTokenSet";
 import { useIntl } from "src/hooks/useIntl";
 import { useShowToaster } from "src/hooks/useToaster";
+import { FeatureFlags, isFeatureEnabled } from "src/utils/featureFlags";
 
 type ErrorDisplay = "screen" | "toaster" | "none";
 
@@ -23,7 +24,7 @@ const DEFAULT_ERROR_DISPLAY: ErrorDisplay = "screen";
 
 disableFragmentWarnings();
 
-const ApolloWrapper: React.FC<PropsWithChildren> = ({ children }) => {
+const ApolloWrapperWithoutTokenHook: React.FC<PropsWithChildren> = ({ children }) => {
   const [displayError, setDisplayError] = useState(false);
   const showToaster = useShowToaster();
   const { T } = useIntl();
@@ -96,5 +97,77 @@ const ApolloWrapper: React.FC<PropsWithChildren> = ({ children }) => {
     </>
   );
 };
+
+const ApolloWrapperWithTokenHook: React.FC<PropsWithChildren> = ({ children }) => {
+  const [displayError, setDisplayError] = useState(false);
+  const showToaster = useShowToaster();
+  const { T } = useIntl();
+  const { tokenSet } = useTokenSet();
+
+  const AuthenticationLink = setContext((_, { headers }) => {
+    const authorizationHeaders = tokenSet?.accessToken ? { Authorization: `Bearer ${tokenSet?.accessToken}` } : {};
+    return {
+      headers: {
+        ...headers,
+        ...authorizationHeaders,
+      },
+    };
+  });
+
+  const HttpLink = createHttpLink({
+    uri: `${config.HASURA_BASE_URL}/v1/graphql`,
+  });
+
+  const ErrorLink = onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+      );
+
+      switch ((operation.getContext().graphqlErrorDisplay || DEFAULT_ERROR_DISPLAY) as ErrorDisplay) {
+        case "screen":
+          setDisplayError(true);
+          break;
+        case "toaster":
+          showToaster(T("state.errorOccured"), { isError: true });
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (networkError) {
+      console.error(`[Network error]: ${networkError}`);
+    }
+  });
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([ErrorLink, AuthenticationLink, HttpLink]),
+    cache: new InMemoryCache({
+      typePolicies: {
+        ProjectDetails: {
+          keyFields: ["projectId"],
+        },
+      },
+    }),
+  });
+
+  const suspenseCache = new SuspenseCache();
+
+  return (
+    <>
+      {displayError && <ErrorFallback />}
+      {!displayError && (
+        <ApolloProvider client={client} suspenseCache={suspenseCache}>
+          {children}
+        </ApolloProvider>
+      )}
+    </>
+  );
+};
+
+const ApolloWrapper = isFeatureEnabled(FeatureFlags.FIX_TOKEN_RELOAD)
+  ? ApolloWrapperWithoutTokenHook
+  : ApolloWrapperWithTokenHook;
 
 export default ApolloWrapper;
--- frontend/src/utils/featureFlags.ts
+++ frontend/src/utils/featureFlags.ts
@@ -1,8 +1,17 @@
-export enum FeatureFlags {}
+export enum FeatureFlags {
+  /**
+   * @owner @oscarwroche
+   * @feature https://linear.app/onlydust/issue/B-358/fix-unwanted-component-reloads
+   * @expiration 2023-02-21
+   */
+  FIX_TOKEN_RELOAD = "FIX_TOKEN_RELOAD",
+}
 
 type FeatureFlagsConfig = Record<FeatureFlags, boolean>;
 
-const featureFlags: FeatureFlagsConfig = {};
+const featureFlags: FeatureFlagsConfig = {
+  FIX_TOKEN_RELOAD: import.meta.env.VITE_FF_FIX_TOKEN_RELOAD === "true",
+};
 
 export const isFeatureEnabled = (feature: keyof FeatureFlagsConfig) => {
   return featureFlags[feature];
