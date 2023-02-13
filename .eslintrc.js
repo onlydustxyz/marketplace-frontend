ab45821c84df5419d557c6c197fa5bf6106886c3
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-02-13 18:13:15 +0100

    :wrench: update eslint with react and hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:storybook/recommended",
     "plugin:storybook/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
@@ -43,6 +45,8 @@ module.exports = {
       },
     ],
     "@typescript-eslint/no-namespace": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
   },
   ignorePatterns: [
     "frontend/dist/*",
