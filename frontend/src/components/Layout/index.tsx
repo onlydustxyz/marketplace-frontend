74ea0beb497c8b3dd07b88a43b0929c45350a724
Stanislas Bernard <bernardstanislas@gmail.com>
2023-02-15 18:21:48 +0100

    :iphone: Make the header responsive

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import { Outlet } from "react-router-dom";
+import { RESPONSIVE } from "src/utils/featureFlags";
 import ResponsivityFallback from "../ResponsivityFallback";
 import { Toaster } from "../Toaster";
 
@@ -7,10 +9,16 @@ import Header from "./Header";
 export default function Layout() {
   return (
     <div>
-      <div className="md:invisible visible md:h-0">
-        <ResponsivityFallback />
-      </div>
-      <div className="md:visible invisible h-screen flex flex-col">
+      {!RESPONSIVE && (
+        <div className="md:invisible visible md:h-0">
+          <ResponsivityFallback />
+        </div>
+      )}
+      <div
+        className={classNames("h-screen flex flex-col", {
+          "md:visible invisible ": !RESPONSIVE,
+        })}
+      >
         <Header />
         <div className="px-6 pb-6 flex-1">
           <Outlet />
