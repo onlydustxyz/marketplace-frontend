8165bd93ff5bf85657e2d8d64e6e4f8cbfe70626
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-02-16 10:05:27 +0100

    :triangular_flag_on_post: remove fix token refresh feature flag

--- frontend/src/utils/featureFlags.ts
+++ frontend/src/utils/featureFlags.ts
@@ -1,17 +1,8 @@
-export enum FeatureFlags {
-  /**
-   * @owner @oscarwroche
-   * @feature https://linear.app/onlydust/issue/B-358/fix-unwanted-component-reloads
-   * @expiration 2023-02-21
-   */
-  FIX_TOKEN_RELOAD = "FIX_TOKEN_RELOAD",
-}
+export enum FeatureFlags {}
 
 type FeatureFlagsConfig = Record<FeatureFlags, boolean>;
 
-const featureFlags: FeatureFlagsConfig = {
-  FIX_TOKEN_RELOAD: import.meta.env.VITE_FF_FIX_TOKEN_RELOAD === "true",
-};
+const featureFlags: FeatureFlagsConfig = {};
 
 export const isFeatureEnabled = (feature: keyof FeatureFlagsConfig) => {
   return featureFlags[feature];
