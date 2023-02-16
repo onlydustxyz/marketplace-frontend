43e6d42a3d0ab4ccb41f12b519974965f67be00b
Stanislas Bernard <bernardstanislas@gmail.com>
2023-02-16 11:19:48 +0100

    :iphone: Make card mobile responsive

--- frontend/src/components/ProjectCard/index.tsx
+++ frontend/src/components/ProjectCard/index.tsx
@@ -37,14 +37,14 @@ export default function ProjectCard({
       dataTestId="project-card"
     >
       <div className="flex flex-col gap-5">
-        <div className="flex flex-row w-full divide-x divide-stone-100/8 gap-6 justify-items-center font-walsheim">
-          <div className="flex flex-col basis-4/12 gap-y-5">
-            <div className="flex flex-row gap-4 items-start">
+        <div className="flex flex-col sm:flex-row w-full sm:divide-x divide-stone-100/8 gap-4 sm:gap-6 justify-items-center font-walsheim">
+          <div className="sm:flex flex-col basis-4/12 gap-y-5">
+            <div className="flex gap-4 items-start">
               <RoundedImage src={logoUrl} alt="Project Logo" size={ImageSize.ExtraLarge} className="mt-1" />
-              <div className="flex flex-col gap-0.5">
-                <div className="text-2xl font-medium font-belwe">{name}</div>
+              <div className="min-w-0">
+                <div className="text-2xl font-medium font-belwe truncate">{name}</div>
                 {lead && (
-                  <div className="text-sm flex flex-row text-spaceBlue-200 gap-1 items-center">
+                  <div className="text-sm flex flex-row text-spaceBlue-200 gap-1 items-center pt-0.5">
                     <div className="whitespace-nowrap">{T("project.ledBy")}</div>
                     <div className="truncate">{lead?.displayName}</div>{" "}
                     <img src={lead?.avatarUrl} className="w-3 md:w-4 h-3 md:h-4 rounded-full" />
@@ -53,7 +53,7 @@ export default function ProjectCard({
               </div>
             </div>
             {githubRepo?.languages && Object.keys(githubRepo?.languages).length > 0 && (
-              <div className="flex flex-row border border-neutral-600 w-fit px-3 py-1 rounded-2xl gap-2 text-md">
+              <div className="hidden sm:flex flex-row border border-neutral-600 w-fit px-3 py-1 rounded-2xl gap-2 text-md">
                 <div className="flex items-center justify-center">
                   <CodeSSlashLine className="text-gray-400" />
                 </div>
@@ -61,16 +61,16 @@ export default function ProjectCard({
               </div>
             )}
           </div>
-          <div className="flex flex-col basis-8/12 pl-6 gap-6">
+          <div className="flex flex-col basis-8/12 sm:pl-6 gap-4 sm:gap-6">
             <div className="line-clamp-2">{projectDetails?.description}</div>
-            <div className="flex flex-row divide-x divide-stone-100/8">
-              <div className="flex flex-row gap-2 pr-6">
+            <div className="flex flex-row sm:divide-x divide-stone-100/8">
+              <div className="hidden sm:flex flex-row gap-2 pr-6">
                 {projectDetails?.telegramLink && <TelegramLink link={projectDetails?.telegramLink} />}
                 {githubRepo?.owner && githubRepo?.name && (
                   <GithubLink link={buildGithubLink(githubRepo?.owner, githubRepo?.name)} />
                 )}
               </div>
-              <div className="flex text-sm gap-4 items-center pl-6">
+              <div className="flex text-sm gap-4 items-center sm:pl-6">
                 {!!githubRepo?.content.contributors?.length && (
                   <span>
                     {T("project.details.contributors.count", { count: githubRepo?.content.contributors?.length })}
