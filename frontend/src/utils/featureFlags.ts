166a86d98c460e468117358e791fca7c500b2cbd
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-02-20 11:41:54 +0100

    :triangular_flag_on_post: add multiple repos feature flag

--- frontend/src/utils/featureFlags.ts
+++ frontend/src/utils/featureFlags.ts
@@ -5,12 +5,14 @@ export enum FeatureFlags {
    * @expiration 2023-02-30
    */
   REMOVE_TIMER_BASED_TOKEN_RELOAD = "REMOVE_TIMER_BASED_TOKEN_RELOAD",
+  MULTIPLE_REPOSITORIES_PER_PROJECT = "MULTIPLE_REPOSITORIES_PER_PROJECT",
 }
 
 type FeatureFlagsConfig = Record<FeatureFlags, boolean>;
 
 const featureFlags: FeatureFlagsConfig = {
   REMOVE_TIMER_BASED_TOKEN_RELOAD: import.meta.env.VITE_FF_REMOVE_TIMER_BASED_TOKEN_RELOAD === "true",
+  MULTIPLE_REPOSITORIES_PER_PROJECT: import.meta.env.VITE_FF_MULTIPLE_REPOSITORIES_PER_PROJECT === "true",
 };
 
 export const isFeatureEnabled = (feature: keyof FeatureFlagsConfig) => {
