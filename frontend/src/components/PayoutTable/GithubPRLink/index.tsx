ccc9c8201313cf93ce5fd7f6fccd09437996baae
Anthony Buisset <abuisset@gmail.com>
2023-02-20 16:46:54 +0100

    ♻️ extract ExternalLink from GithubPRLink

--- frontend/src/components/PayoutTable/GithubPRLink/index.tsx
+++ frontend/src/components/PayoutTable/GithubPRLink/index.tsx
@@ -1,19 +1,13 @@
+import ExternalLink from "src/components/ExternalLink";
 import { useIntl } from "src/hooks/useIntl";
-import ExternalLinkLine from "src/icons/ExternalLinkLine";
-import { linkClickHandlerFactory } from "src/utils/clickHandler";
 
 export const REGEX_GITHUB_PULL_REQUEST_URL = /^https:\/\/github\.com\/[\w.-]+\/[\w.-]+\/pull\/(\d+)$/i;
 
 interface LinkProps {
   link: string;
-  linkColor?: LinkColor;
 }
 
-export enum LinkColor {
-  Grey = "text-spaceBlue-200",
-}
-
-export default function GithubPRLink({ link, linkColor }: LinkProps) {
+export default function GithubPRLink({ link }: LinkProps) {
   const { T } = useIntl();
   let linkText = link;
   const matches = REGEX_GITHUB_PULL_REQUEST_URL.exec(link);
@@ -21,11 +15,8 @@ export default function GithubPRLink({ link, linkColor }: LinkProps) {
     linkText = T("payment.table.githubLink", { pullRequestNumber: matches[1] });
   }
   return (
-    <div className="group/github-pr-link flex flex-row gap-1 w-fit items-center hover:cursor-pointer">
-      <div className={`${linkColor} flex group-hover/github-pr-link:underline`} onClick={linkClickHandlerFactory(link)}>
-        {linkText}
-      </div>
-      <ExternalLinkLine className="flex text-spacePurple-500 invisible group-hover/github-pr-link:visible" />
+    <div className="text-spaceBlue-200">
+      <ExternalLink text={linkText} url={link} />
     </div>
   );
 }
--- frontend/src/components/PayoutTable/Line.tsx
+++ frontend/src/components/PayoutTable/Line.tsx
@@ -3,7 +3,7 @@ import Cell from "../Table/Cell";
 import onlyDustLogo from "assets/img/onlydust-logo.png";
 import RoundedImage from "src/components/RoundedImage";
 import PayoutStatus from "../PayoutStatus";
-import GithubPRLink, { LinkColor } from "./GithubPRLink";
+import GithubPRLink from "./GithubPRLink";
 import { formatMoneyAmount } from "src/utils/money";
 import displayRelativeDate from "src/utils/displayRelativeDate";
 import { getPaymentStatusOrder, Payment } from "src/types";
@@ -36,7 +36,7 @@ export default function PaymentLine({ payment, payoutInfoMissing, setSortingFiel
         <RoundedImage src={payment?.project?.logoUrl || onlyDustLogo} alt={payment?.project?.title || ""} />
         <div className="flex flex-col truncate justify-center">
           <div className="font-normal text-base font-belwe">{payment?.project?.title}</div>
-          {payment.reason && <GithubPRLink link={payment.reason} linkColor={LinkColor.Grey}></GithubPRLink>}
+          {payment.reason && <GithubPRLink link={payment.reason}></GithubPRLink>}
         </div>
       </Cell>
       <Cell>{formatMoneyAmount(payment.amount.value, payment.amount.currency)}</Cell>
