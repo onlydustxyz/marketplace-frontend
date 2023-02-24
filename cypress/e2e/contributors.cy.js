8ef899a2dd3691b7e7688b2de375f0535b2997c2
Anthony Buisset <abuisset@gmail.com>
2023-02-24 18:07:39 +0100

    ✅ fix e2e tests

--- cypress/e2e/contributors.cy.js
+++ cypress/e2e/contributors.cy.js
@@ -55,7 +55,17 @@ describe("As a public user, I", () => {
       cy.get("td:nth-child(3)").should("have.text", "7");
     });
 
-    cy.get("#contributors_table tbody tr").its("length").should("equal", 2);
+    cy.get("#contributors_table tbody tr:nth-child(2)").within(() => {
+      cy.get("td:nth-child(2)").should("have.text", "-");
+      cy.get("td:nth-child(3)").should("have.text", "-");
+    });
+
+    cy.get("#contributors_table tbody tr:nth-child(3)").within(() => {
+      cy.get("td:nth-child(2)").should("have.text", "-");
+      cy.get("td:nth-child(3)").should("have.text", "-");
+    });
+
+    cy.get("#contributors_table tbody tr").its("length").should("equal", 3);
   });
 
   it("can sort the contributors of a project", function () {
@@ -65,7 +75,7 @@ describe("As a public user, I", () => {
     cy.get("#contributors_table thead tr th:nth-child(1)").click(); // sort by contributor name ASC
 
     cy.get("#contributors_table tbody tr:nth-child(1)").within(() => {
-      cy.get("td:nth-child(1)").should("have.text", "AnthonyBuisset");
+      cy.get("td:nth-child(1)").should(div => expect(div.text()).to.include("AnthonyBuisset"));
       cy.get("td:nth-child(2)").should("have.text", "-");
       cy.get("td:nth-child(3)").should("have.text", "-");
     });
