83e7ef09af48403bf8139ce7ca3b6bd9d82460a4
Anthony Buisset <abuisset@gmail.com>
2023-02-27 10:14:27 +0100

    ✨ display several project leads

--- frontend/src/components/ProjectCard/ProjectTitle.tsx
+++ frontend/src/components/ProjectCard/ProjectTitle.tsx
@@ -1,6 +1,6 @@
 import { useIntl } from "src/hooks/useIntl";
 import { ProjectLeadFragment } from "src/__generated/graphql";
-import RoundedImage, { ImageSize } from "../RoundedImage";
+import RoundedImage, { ImageSize, Rounding } from "../RoundedImage";
 
 type Props = {
   projectName: string;
@@ -8,13 +8,25 @@ type Props = {
   logoUrl: string;
 };
 
-const ProjectLead = ({ lead }: { lead: ProjectLeadFragment }) => {
+const ProjectLeads = ({ leads }: { leads: ProjectLeadFragment[] }) => {
   const { T } = useIntl();
 
   return (
     <div className="text-sm flex flex-row text-spaceBlue-200 gap-1 items-center pt-0.5">
-      <div className="whitespace-nowrap">{T("project.ledBy")}</div>
-      <div className="truncate">{lead.displayName}</div> <img src={lead.avatarUrl} className="w-4 h-4 rounded-full" />
+      <div className="whitespace-nowrap truncate">
+        {T("project.ledBy", { name: leads[0]?.displayName, count: leads.length })}
+      </div>
+      <div className="flex flex-row -space-x-1" id="projectLeads">
+        {leads.map(lead => (
+          <RoundedImage
+            rounding={Rounding.Circle}
+            alt={lead.displayName}
+            size={ImageSize.Xxs}
+            key={lead.displayName}
+            src={lead.avatarUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 };
@@ -25,7 +37,7 @@ export default function ProjectTitle({ projectName, projectLeads, logoUrl }: Pro
       <RoundedImage src={logoUrl} alt="Project Logo" size={ImageSize.Xl} className="mt-1" />
       <div className="min-w-0">
         <div className="text-2xl font-medium font-belwe truncate">{projectName}</div>
-        {projectLeads.length === 1 && <ProjectLead lead={projectLeads[0]} />}
+        <ProjectLeads leads={projectLeads} />
       </div>
     </div>
   );
