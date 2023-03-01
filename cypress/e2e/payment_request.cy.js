fc899b7f2e1a71e6c0689a8e4eafef6692545a6b
Oscar Roche <oscar.w.roche@gmail.com>
2023-03-01 13:26:39 +0100

    b-265-add-e2e-test-with-2-leads-interacting-on
    Co-authored-by: Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>

--- cypress/e2e/payment_request.cy.js
+++ cypress/e2e/payment_request.cy.js
@@ -1,5 +1,7 @@
 import { WAIT_LONG } from "../support/commands/common";
 
+const TEST_ETH_ADDRESS = "0x3cd05ab88fbf996c0784e54f74195815bfa866de";
+
 describe("As a project lead, I", () => {
   beforeEach(function () {
     cy.createGithubUser(98735558)
@@ -10,6 +12,24 @@ describe("As a project lead, I", () => {
           .then(user => JSON.stringify(user.session))
           .as("token");
       });
+
+    cy.createGithubUser(21149076)
+      .as("otherUser")
+      .then(user =>
+        cy
+          .signinUser(user)
+          .then(user => JSON.stringify(user.session))
+          .as("otherToken")
+      );
+
+    cy.createGithubUser(43467246)
+      .as("paymentRecipient")
+      .then(user =>
+        cy
+          .signinUser(user)
+          .then(user => JSON.stringify(user.session))
+          .as("paymentRecipientToken")
+      );
   });
 
   it("can request a payment", function () {
@@ -30,6 +50,84 @@ describe("As a project lead, I", () => {
     cy.contains("Confirm payment").click();
 
     cy.get("#remainingBudget").should("have.text", "$0");
+  });
+
+  it("can request a payment and other project leads can see the update", function () {
+    cy.visit(`http://localhost:5173/profile`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("hasura_token", this.paymentRecipientToken);
+      },
+    });
+
+    cy.get("[name=ethIdentity]").clear().type(TEST_ETH_ADDRESS);
+    cy.contains("Save profile").click();
+
+    cy.visit(`http://localhost:5173/projects/${this.projectId}`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("hasura_token", this.token);
+      },
+    });
+
+    cy.contains("Payments").click();
+    cy.get("#remainingBudget").should("have.text", "$1,000");
+
+    cy.contains("New payment").click();
+    cy.get("[name=contributorHandle]").type("AnthonyBuisset");
+    cy.get("[name=contributorHandle]").blur();
+    cy.get("[name=linkToIssue]").type("https://github.com/od-mocks/cool-repo-A/pull/2");
+    cy.wait(WAIT_LONG);
+    cy.contains("Confirm payment").click();
+
+    cy.get("#remainingBudget").should("have.text", "$0");
+
+    cy.graphql({
+      query: `{ paymentRequests(where: {budget: {projectId: {_eq: "${this.projectId}"}}, _and: {recipientId: {_eq: 43467246}}})
+	    {
+	  id
+  }
+          }`,
+    })
+      .asAdmin()
+      .data("paymentRequests")
+      .its(0)
+      .its("id")
+      .then(paymentId =>
+        cy
+          .addEthPaymentReceipt(
+            this.projectId,
+            paymentId,
+            "1000",
+            "USDC",
+            {
+              type: "ETHEREUM_ADDRESS",
+              optEthAddress: TEST_ETH_ADDRESS,
+            },
+            "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+          )
+          .asAdmin()
+          .data("addEthPaymentReceipt")
+          .then(receipt_id => {
+            cy.paymentRequestShouldBePaid(paymentId, receipt_id);
+          })
+      );
+
+    cy.inviteProjectLeader(this.projectId, this.otherUser.githubUserId)
+      .asAdmin()
+      .data("inviteProjectLeader")
+      .should("be.a", "string")
+      .then(invitationId => cy.acceptProjectLeaderInvitation(invitationId))
+      .asRegisteredUser(this.otherUser)
+      .then(() => this.projectId);
+
+    cy.visit(`http://localhost:5173/projects/${this.projectId}`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("hasura_token", this.otherToken);
+      },
+    });
+
+    cy.contains("Payments").click();
+    cy.get("#remainingBudget").should("have.text", "$0");
+    cy.get("#payment_table").contains("Complete");
   });
 
   it("can accept an invitation to become project lead", function () {
