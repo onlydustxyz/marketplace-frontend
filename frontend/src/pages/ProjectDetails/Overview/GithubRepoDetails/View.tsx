d2a81882558dfc028e9dca3f444132fc490a77c6
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-03-01 15:42:59 +0100

    :lipstick: change repo card layout

--- frontend/src/pages/ProjectDetails/Overview/GithubRepoDetails/View.tsx
+++ frontend/src/pages/ProjectDetails/Overview/GithubRepoDetails/View.tsx
@@ -13,32 +13,30 @@ type Props = Omit<GithubRepoStaticDetailsFragment & GithubRepoDynamicDetailsFrag
 
 export default function View({ owner, name, description, languages, stars, forksCount }: Props) {
   return (
-    <Card className="flex flex-col gap-3 font-walsheim text-greyscale-50 font-normal relative p-6" padded={false}>
-      {owner && name && (
-        <div className="absolute top-3 right-3">
-          <GithubLink link={buildGithubLink(owner, name)} />
+    <Card className="flex flex-row justify-between p-3" padded={false}>
+      <div className="flex flex-col justify-between gap-3 font-walsheim text-greyscale-50 font-normal p-3">
+        {Object.keys(languages).length > 0 && (
+          <Tag size={TagSize.Small}>
+            <CodeSSlashLine />
+            {getMostUsedLanguages(languages, 1)[0]}
+          </Tag>
+        )}
+        <div className="flex flex-col">
+          <span className="font-medium text-base">{name}</span>
+          <span className="my-2 text-greyscale-200 text-sm">{description}</span>
         </div>
-      )}
-      {Object.keys(languages).length > 0 && (
-        <Tag size={TagSize.Small}>
-          <CodeSSlashLine />
-          {getMostUsedLanguages(languages, 1)[0]}
-        </Tag>
-      )}
-      <div className="flex flex-col">
-        <span className="font-medium text-base">{name}</span>
-        <span className="my-2 text-greyscale-200 text-sm">{description}</span>
-      </div>
-      <div className="flex flex-row gap-5 text-greyscale-200 text-sm font-medium">
-        <div className="flex flex-row gap-1">
-          <StarLine />
-          {stars}
-        </div>
-        <div className="flex flex-row gap-1 items-center">
-          <ForkLine className="fill-greyscale-200" />
-          {forksCount}
+        <div className="flex flex-row gap-5 text-greyscale-200 text-sm font-medium">
+          <div className="flex flex-row gap-1">
+            <StarLine />
+            {stars}
+          </div>
+          <div className="flex flex-row gap-1 items-center">
+            <ForkLine className="fill-greyscale-200" />
+            {forksCount}
+          </div>
         </div>
       </div>
+      <div>{owner && name && <GithubLink link={buildGithubLink(owner, name)} />}</div>
     </Card>
   );
 }
