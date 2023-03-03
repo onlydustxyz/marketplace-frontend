6add0d26d724ea1310296aceb156faef8b1aad09
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-03-03 14:45:16 +0100

    :white_check_mark: fix tests

--- frontend/src/pages/Projects/FilterPanel/FilterPanel.test.tsx
+++ frontend/src/pages/Projects/FilterPanel/FilterPanel.test.tsx
@@ -5,53 +5,93 @@ import userEvent from "@testing-library/user-event";
 import FilterPanel, { GET_ALL_TECHNOLOGIES_QUERY } from ".";
 import { renderWithIntl, MemoryRouterProviderFactory } from "src/test/utils";
 import { ProjectOwnershipType } from "..";
-import { GithubRepoLanguagesFieldsFragment } from "src/__generated/graphql";
+import { GetAllTechnologiesQuery } from "src/__generated/graphql";
 
 expect.extend(matchers);
 
-const githubRepo1: GithubRepoLanguagesFieldsFragment = {
-  __typename: "ProjectGithubRepos",
-  githubRepoId: 1,
-  githubRepoDetails: {
-    id: 1,
-    languages: { "C++": 1234, Shell: 123, Makefile: 10 },
-  },
+const projects1: GetAllTechnologiesQuery["projects"][number] = {
+  id: "project-1",
+  githubRepos: [
+    {
+      __typename: "ProjectGithubRepos",
+      githubRepoId: "github-repo-1",
+      githubRepoDetails: {
+        id: 1,
+        languages: { "C++": 1234, Shell: 123, Makefile: 10 },
+      },
+    },
+  ],
+  budgets: [{ id: "budget-1" }],
+  projectLeads: [{ userId: "user-1" }],
+  pendingInvitations: [],
 };
 
-const githubRepo2: GithubRepoLanguagesFieldsFragment = {
-  __typename: "ProjectGithubRepos",
-  githubRepoId: 2,
-  githubRepoDetails: {
-    id: 2,
-    languages: { Rust: 1234, Shell: 123 },
-  },
+const projects2: GetAllTechnologiesQuery["projects"][number] = {
+  id: "project-1",
+  githubRepos: [
+    {
+      __typename: "ProjectGithubRepos",
+      githubRepoId: 2,
+      githubRepoDetails: {
+        id: 2,
+        languages: { Rust: 1234, Shell: 123 },
+      },
+    },
+  ],
+  budgets: [{ id: "budget-1" }],
+  projectLeads: [{ userId: "user-1" }],
+  pendingInvitations: [],
 };
 
-const githubRepo3: GithubRepoLanguagesFieldsFragment = {
-  __typename: "ProjectGithubRepos",
-  githubRepoId: 3,
-  githubRepoDetails: {
-    id: 3,
-    languages: { TypeScript: 1234 },
-  },
+const projects3: GetAllTechnologiesQuery["projects"][number] = {
+  id: "project-1",
+  githubRepos: [
+    {
+      __typename: "ProjectGithubRepos",
+      githubRepoId: 3,
+      githubRepoDetails: {
+        id: 3,
+        languages: { TypeScript: 1234 },
+      },
+    },
+  ],
+  budgets: [{ id: "budget-1" }],
+  projectLeads: [{ userId: "user-1" }],
+  pendingInvitations: [],
 };
 
-const githubRepo4: GithubRepoLanguagesFieldsFragment = {
-  __typename: "ProjectGithubRepos",
-  githubRepoId: 4,
-  githubRepoDetails: {
-    id: 4,
-    languages: { Go: 5555, C: 123 },
-  },
+const projects4: GetAllTechnologiesQuery["projects"][number] = {
+  id: "project-1",
+  githubRepos: [
+    {
+      __typename: "ProjectGithubRepos",
+      githubRepoId: 4,
+      githubRepoDetails: {
+        id: 4,
+        languages: { Go: 5555, C: 123 },
+      },
+    },
+  ],
+  budgets: [{ id: "budget-1" }],
+  projectLeads: [{ userId: "user-1" }],
+  pendingInvitations: [],
 };
 
-const githubRepo5: GithubRepoLanguagesFieldsFragment = {
-  __typename: "ProjectGithubRepos",
-  githubRepoId: 5,
-  githubRepoDetails: {
-    id: 5,
-    languages: {},
-  },
+const projects5: GetAllTechnologiesQuery["projects"][number] = {
+  id: "project-1",
+  githubRepos: [
+    {
+      __typename: "ProjectGithubRepos",
+      githubRepoId: 5,
+      githubRepoDetails: {
+        id: 5,
+        languages: { Elisp: 666 },
+      },
+    },
+  ],
+  budgets: [],
+  projectLeads: [{ userId: "user-1" }],
+  pendingInvitations: [],
 };
 
 const graphQlMocks = [
@@ -61,24 +101,7 @@ const graphQlMocks = [
     },
     result: {
       data: {
-        projects: [
-          {
-            id: "1",
-            githubRepos: [githubRepo1],
-          },
-          {
-            id: "2",
-            githubRepos: [githubRepo2],
-          },
-          {
-            id: "3",
-            githubRepos: [githubRepo3, githubRepo4],
-          },
-          {
-            id: "4",
-            githubRepos: [githubRepo5],
-          },
-        ],
+        projects: [projects1, projects2, projects3, projects4, projects5],
       },
     },
   },
@@ -108,12 +131,13 @@ describe("FilterPanel", () => {
     userEvent.click(await screen.findByRole("button"));
 
     const allOptions = await screen.findAllByRole("option");
-    expect(allOptions.length).toBe(5);
-    expect(allOptions[0]).toHaveTextContent("C++");
-    expect(allOptions[1]).toHaveTextContent("Go");
-    expect(allOptions[2]).toHaveTextContent("Rust");
-    expect(allOptions[3]).toHaveTextContent("Shell");
-    expect(allOptions[4]).toHaveTextContent("TypeScript");
+    expect(allOptions.length).toBe(6);
+    expect(allOptions[0]).toHaveTextContent("C");
+    expect(allOptions[1]).toHaveTextContent("C++");
+    expect(allOptions[2]).toHaveTextContent("Go");
+    expect(allOptions[3]).toHaveTextContent("Rust");
+    expect(allOptions[4]).toHaveTextContent("Shell");
+    expect(allOptions[5]).toHaveTextContent("TypeScript");
   });
 
   it("should display 'Mine only' when user is leader'", async () => {
@@ -134,5 +158,24 @@ describe("FilterPanel", () => {
 
     await screen.findByText(/all projects/i);
     await screen.findByText(/mine only/i);
+  });
+
+  it("should not display technologies from projects that aren't visible", async () => {
+    renderWithIntl(
+      <FilterPanel
+        projectFilter={{ ownershipType: ProjectOwnershipType.All, technologies: [] }}
+        setProjectFilter={() => {
+          return;
+        }}
+        isProjectLeader={true}
+      />,
+      {
+        wrapper: MemoryRouterProviderFactory({
+          mocks: graphQlMocks,
+        }),
+      }
+    );
+    await screen.findByText(/go/i);
+    expect(screen.queryByText(/elisp/i)).toBeNull();
   });
 });
