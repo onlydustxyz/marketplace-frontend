075d827de10aa052b605e7fbcb5db83f8595238f
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-03-08 15:03:20 +0100

    b-745-add-new-alerting-system-when-user
    Co-authored-by: Olivier Fuxet <ofux@users.noreply.github.com>

--- frontend/src/components/Layout/Header/ProfileButton/ProfileButton.stories.tsx
+++ frontend/src/components/Layout/Header/ProfileButton/ProfileButton.stories.tsx
@@ -1,4 +1,3 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { withRouter } from "storybook-addon-react-router-v6";
 
 import ProfileButton from "./View";
@@ -7,17 +6,39 @@ export default {
   title: "ProfileButton",
   component: ProfileButton,
   decorators: [withRouter],
-} as ComponentMeta<typeof ProfileButton>;
+  argTypes: {
+    payoutSettingsInvalid: { type: "boolean" },
+  },
+};
 
-const Template: ComponentStory<typeof ProfileButton> = args => (
-  <div className="ml-32">
-    <ProfileButton {...args} />
-  </div>
-);
+type Props = {
+  avatarUrl: string;
+  displayName: string;
+  payoutSettingsInvalid: boolean;
+  logout: () => void;
+};
 
-export const Default = Template.bind({});
-
-Default.args = {
+const props: Props = {
   avatarUrl: "https://avatars.githubusercontent.com/u/10167015?v=4",
   displayName: "lechinoix",
+  payoutSettingsInvalid: false,
+  logout: () => {
+    return;
+  },
+};
+
+export const Default = {
+  render: (args: Props) => (
+    <div className="ml-32">
+      <ProfileButton {...props} {...args} />
+    </div>
+  ),
+};
+
+export const MissingPayoutSettings = {
+  render: (args: Props) => (
+    <div className="ml-32">
+      <ProfileButton {...props} {...{ payoutSettingsInvalid: true }} {...args} />
+    </div>
+  ),
 };
