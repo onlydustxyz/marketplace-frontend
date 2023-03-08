e691a7c353e9a1270b2427a067f0e9bb348965f3
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-03-08 17:14:06 +0100

    [B-793] Remove payout info missing banner (#782)

--- frontend/src/pages/Payments/index.tsx
+++ frontend/src/pages/Payments/index.tsx
@@ -1,5 +1,5 @@
-import { gql, QueryResult } from "@apollo/client";
-import { Link, Navigate } from "react-router-dom";
+import { gql } from "@apollo/client";
+import { Navigate } from "react-router-dom";
 import { RoutePaths } from "src/App";
 import Card from "src/components/Card";
 import PayoutTable from "src/components/PayoutTable";
@@ -9,8 +9,6 @@ import { useHasuraQuery } from "src/hooks/useHasuraQuery";
 import { Currency, HasuraUserRole, PaymentStatus } from "src/types";
 import { GetPaymentRequestsQuery, UserPaymentRequestFragment } from "src/__generated/graphql";
 import { useT } from "talkr";
-import InfoMissingBanner from "src/components/InfoMissingBanner";
-import Button from "src/components/Button";
 import TotalEarnings from "./TotalEarnings";
 import Background, { BackgroundRoundedBorders } from "src/components/Background";
 import usePayoutSettings from "src/hooks/usePayoutSettings";
@@ -46,17 +44,6 @@ const Payments = () => {
       <div className="container mx-auto pt-16 h-full">
         <div className="text-5xl font-belwe">{T("navbar.payments")}</div>
         <QueryWrapper query={getPaymentRequestsQuery}>
-          <div className="my-10">
-            {!payoutSettingsValid && hasPendingPaymentsRequests(getPaymentRequestsQuery) && (
-              <InfoMissingBanner>
-                <Link to={RoutePaths.Profile}>
-                  <Button>
-                    <div>{T("profile.missing.button")}</div>
-                  </Button>
-                </Link>
-              </InfoMissingBanner>
-            )}
-          </div>
           <div className="flex gap-4 mb-10">
             <Card>{payments && <PayoutTable payments={payments} payoutInfoMissing={!payoutSettingsValid} />}</Card>
             {totalEarnings && <TotalEarnings amount={totalEarnings} />}
