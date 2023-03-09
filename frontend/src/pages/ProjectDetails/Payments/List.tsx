6935e2d872596fb124a36920cb69ab25072e5f01
Anthony Buisset <abuisset@gmail.com>
2023-03-09 10:49:55 +0100

    💬 update send payment page title

--- frontend/src/pages/ProjectDetails/Payments/List.tsx
+++ frontend/src/pages/ProjectDetails/Payments/List.tsx
@@ -4,6 +4,7 @@ import Card from "src/components/Card";
 import Loader from "src/components/Loader";
 import PaymentTable from "src/components/PaymentTable";
 import ProjectPaymentTableFallback from "src/components/ProjectPaymentTableFallback";
+import { useIntl } from "src/hooks/useIntl";
 import RemainingBudget from "src/pages/ProjectDetails/Payments/RemainingBudget";
 import { Sortable } from "src/types";
 import { PaymentRequestFragment } from "src/__generated/graphql";
@@ -14,25 +15,30 @@ const PaymentList: React.FC = () => {
     budget: { initialAmount: number; remainingAmount: number };
   }>();
 
+  const { T } = useIntl();
+
   return (
-    <div className="flex flex-row items-start gap-5 h-full">
-      <div className="flex basis-2/3">
-        {payments.length > 0 ? (
-          <Card>
-            <Suspense fallback={<Loader />}>
-              <PaymentTable payments={payments} />
-            </Suspense>
-          </Card>
-        ) : (
-          <Card className="p-16">
-            <ProjectPaymentTableFallback disabled={budget.initialAmount === 0 || budget.remainingAmount === 0} />
-          </Card>
-        )}
+    <>
+      <div className="text-3xl font-belwe">{T("project.details.payments.title")}</div>
+      <div className="flex flex-row items-start gap-5 h-full">
+        <div className="flex basis-2/3">
+          {payments.length > 0 ? (
+            <Card>
+              <Suspense fallback={<Loader />}>
+                <PaymentTable payments={payments} />
+              </Suspense>
+            </Card>
+          ) : (
+            <Card className="p-16">
+              <ProjectPaymentTableFallback disabled={budget.initialAmount === 0 || budget.remainingAmount === 0} />
+            </Card>
+          )}
+        </div>
+        <div className="flex basis-1/3">
+          <RemainingBudget budget={budget} disabled={budget.remainingAmount === 0 || payments.length === 0} />
+        </div>
       </div>
-      <div className="flex basis-1/3">
-        <RemainingBudget budget={budget} disabled={budget.remainingAmount === 0 || payments.length === 0} />
-      </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/ProjectDetails/Payments/PaymentForm/View.tsx
+++ frontend/src/pages/ProjectDetails/Payments/PaymentForm/View.tsx
@@ -17,43 +17,46 @@ const View: React.FC<Props> = ({ budget, onWorkEstimationChange, projectId }) =>
   const { T } = useIntl();
 
   return (
-    <div className="flex flex-row items-start gap-5 h-full">
-      <div className="basis-3/5 self-stretch">
-        <div className="flex flex-col gap-6 w-full">
-          <Card className="px-8 pb-3 z-10">
-            <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
-              <div className="font-medium text-lg">{T("payment.form.contributor.title")}</div>
-              <div className="flex flex-row pt-3">
-                <ContributorSelect projectId={projectId} />
+    <>
+      <div className="text-3xl font-belwe">{T("project.details.payments.new.title")}</div>
+      <div className="flex flex-row items-start gap-5 h-full">
+        <div className="basis-3/5 self-stretch">
+          <div className="flex flex-col gap-6 w-full">
+            <Card className="px-8 pb-3 z-10">
+              <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
+                <div className="font-medium text-lg">{T("payment.form.contributor.title")}</div>
+                <div className="flex flex-row pt-3">
+                  <ContributorSelect projectId={projectId} />
+                </div>
               </div>
-            </div>
-          </Card>
-          <Card className="px-8 pb-8 z-0">
-            <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
-              <div className="font-medium text-lg">{T("payment.form.issueLink.title")}</div>
-              <div className="flex flex-row pt-3">
-                <Input
-                  label={T("payment.form.issueLink.inputLabel")}
-                  name="linkToIssue"
-                  placeholder={T("payment.form.issueLink.placeholder")}
-                  errorType={InputErrorType.Banner}
-                  options={{
-                    pattern: {
-                      value: REGEX_VALID_GITHUB_PULL_REQUEST_URL,
-                      message: T("payment.form.issueLink.error"),
-                    },
-                  }}
-                  showValidationErrors={false}
-                />
+            </Card>
+            <Card className="px-8 pb-8 z-0">
+              <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
+                <div className="font-medium text-lg">{T("payment.form.issueLink.title")}</div>
+                <div className="flex flex-row pt-3">
+                  <Input
+                    label={T("payment.form.issueLink.inputLabel")}
+                    name="linkToIssue"
+                    placeholder={T("payment.form.issueLink.placeholder")}
+                    errorType={InputErrorType.Banner}
+                    options={{
+                      pattern: {
+                        value: REGEX_VALID_GITHUB_PULL_REQUEST_URL,
+                        message: T("payment.form.issueLink.error"),
+                      },
+                    }}
+                    showValidationErrors={false}
+                  />
+                </div>
               </div>
-            </div>
-          </Card>
+            </Card>
+          </div>
+        </div>
+        <div className="basis-2/5">
+          <WorkEstimation onChange={onWorkEstimationChange} budget={budget} />
         </div>
       </div>
-      <div className="basis-2/5">
-        <WorkEstimation onChange={onWorkEstimationChange} budget={budget} />
-      </div>
-    </div>
+    </>
   );
 };
 
