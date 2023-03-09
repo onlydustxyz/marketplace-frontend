b18131842b88b712edd8e05c166938f04b5ecadd
Anthony Buisset <abuisset@gmail.com>
2023-03-09 11:23:37 +0100

    ♻️ extract common code in payment submission form

--- frontend/src/pages/ProjectDetails/Payments/PaymentForm/View.tsx
+++ frontend/src/pages/ProjectDetails/Payments/PaymentForm/View.tsx
@@ -11,11 +11,25 @@ import Button, { ButtonSize, ButtonType } from "src/components/Button";
 import { useNavigate } from "react-router-dom";
 import CloseLine from "src/icons/CloseLine";
 import Title from "../../Title";
+import { PropsWithChildren } from "react";
 
 interface Props {
   projectId: string;
   budget: Budget;
   onWorkEstimationChange: (workEstimation: number) => void;
+}
+
+type SectionProps = {
+  title: string;
+} & PropsWithChildren;
+
+function Section({ title, children }: SectionProps) {
+  return (
+    <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
+      <div className="font-medium text-lg">{title}</div>
+      <div className="flex flex-row pt-3">{children}</div>
+    </div>
+  );
 }
 
 const View: React.FC<Props> = ({ budget, onWorkEstimationChange, projectId }) => {
@@ -40,31 +54,25 @@ const View: React.FC<Props> = ({ budget, onWorkEstimationChange, projectId }) =>
         <div className="basis-3/5 self-stretch">
           <div className="flex flex-col gap-6 w-full">
             <Card className="px-8 pb-3 z-10">
-              <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
-                <div className="font-medium text-lg">{T("payment.form.contributor.title")}</div>
-                <div className="flex flex-row pt-3">
-                  <ContributorSelect projectId={projectId} />
-                </div>
-              </div>
+              <Section title={T("payment.form.contributor.title")}>
+                <ContributorSelect projectId={projectId} />
+              </Section>
               {contributor && (
-                <div className="flex flex-col gap-2 divide-y divide-solid divide-greyscale-50/8 ">
-                  <div className="font-medium text-lg">{T("payment.form.issueLink.title")}</div>
-                  <div className="flex flex-row pt-3">
-                    <Input
-                      label={T("payment.form.issueLink.inputLabel")}
-                      name="linkToIssue"
-                      placeholder={T("payment.form.issueLink.placeholder")}
-                      errorDisplay={InputErrorDisplay.Banner}
-                      options={{
-                        pattern: {
-                          value: REGEX_VALID_GITHUB_PULL_REQUEST_URL,
-                          message: T("payment.form.issueLink.error"),
-                        },
-                      }}
-                      showValidationErrors={false}
-                    />
-                  </div>
-                </div>
+                <Section title={T("payment.form.issueLink.title")}>
+                  <Input
+                    label={T("payment.form.issueLink.inputLabel")}
+                    name="linkToIssue"
+                    placeholder={T("payment.form.issueLink.placeholder")}
+                    errorDisplay={InputErrorDisplay.Banner}
+                    options={{
+                      pattern: {
+                        value: REGEX_VALID_GITHUB_PULL_REQUEST_URL,
+                        message: T("payment.form.issueLink.error"),
+                      },
+                    }}
+                    showValidationErrors={false}
+                  />
+                </Section>
               )}
             </Card>
           </div>
