2c02e9852c1136463e0a798fa57f53794caf75be
Oscar Roche <oscar.w.roche@gmail.com>
2023-03-13 11:37:14 +0100

    b-798-visual-fixes
    Co-authored-by: Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>

--- frontend/src/pages/Payments/index.tsx
+++ frontend/src/pages/Payments/index.tsx
@@ -45,8 +45,8 @@ const Payments = () => {
 
   return (
     <Background roundedBorders={BackgroundRoundedBorders.Full}>
-      <div className="container flex flex-col mx-auto py-6 px-8 gap-6 h-full">
-        <div className="text-3xl font-belwe">{T("navbar.payments")}</div>
+      <div className="container flex flex-col mx-auto pt-16 pb-8 gap-6 h-full px-8">
+        <div className="text-5xl font-belwe">{T("navbar.payments")}</div>
         <QueryWrapper query={getPaymentRequestsQuery}>
           <div className="flex gap-4 mb-10">
             <Card>
