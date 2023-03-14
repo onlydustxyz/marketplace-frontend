d00e6c1f204c5e46faa179764380c4e12477cf16
Anthony Buisset <abuisset@gmail.com>
2023-03-14 18:19:20 +0100

    ✨ add repo link parsing

--- frontend/src/utils/github.ts
+++ frontend/src/utils/github.ts
@@ -5,3 +5,11 @@ export const parsePullRequestLink = (link?: string) => {
   const [_, repoOwner, repoName, prNumber] = link?.match(REGEX_VALID_GITHUB_PULL_REQUEST_URL) || [];
   return { repoOwner, repoName, prNumber: parseInt(prNumber) };
 };
+
+export const REGEX_VALID_GITHUB_REPOSITORY_URL = /^https:\/\/api.github\.com\/repos\/([\w.-]+)\/([\w.-]+)$/;
+
+export const parseApiRepositoryLink = (link?: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, owner, name] = link?.match(REGEX_VALID_GITHUB_REPOSITORY_URL) || [];
+  return { owner, name };
+};
