bf9e1603240f6d86ad8788fa247a5ce09fcf461c
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-16 13:54:34 +0100

    ✅ populate sponsors
    Co-authored-by: Antho <AnthonyBuisset@users.noreply.github.com>

--- cypress/support/commands/populate/all.ts
+++ cypress/support/commands/populate/all.ts
@@ -10,11 +10,12 @@ declare global {
 
 Cypress.Commands.add("populateAll", function () {
   cy.populateUsers().then(() => {
-    cy.wait(1000); // wait events
-    cy.populateProjects().then(() => {
-      cy.wait(1000); // wait events
-      cy.populatePayments().then(() => {
+    cy.populateSponsors().then(() => {
+      cy.populateProjects().then(() => {
         cy.wait(1000); // wait events
+        cy.populatePayments().then(() => {
+          cy.wait(1000); // wait events
+        });
       });
     });
   });
