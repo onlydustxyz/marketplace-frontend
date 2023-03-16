0216c5032a94271a862307a3acab19da53940743
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-16 14:30:01 +0100

    ✅ migrate user_registration e2e to ts + fixtures

--- cypress/e2e/user_registration.cy.ts
+++ cypress/e2e/user_registration.cy.ts
@@ -1,26 +1,22 @@
 import { WAIT_LONG } from "../support/commands/common";
-import IBAN from "iban";
+import { printFormat } from "iban";
+import { User } from "../support/commands/populate/users";
 
 describe("The user", () => {
-  before(() => {
-    cy.createGithubUser(12345).then(user => {
-      cy.signinUser(user)
-        .then(user => JSON.stringify(user.session))
-        .as("token");
-    });
-  });
+  let user: User;
 
-  beforeEach(() => {
+  beforeEach(function () {
+    user = this.users["Oscar"];
     cy.fixture("profiles/james_bond").as("profile");
   });
 
   it("can fill their personal info", function () {
-    cy.fillPayoutSettings(this.token);
+    cy.fillPayoutSettings(user.token);
     cy.contains("Changes saved");
   });
 
   it("can see their personal info pre-filled", function () {
-    cy.visitApp({ path: "profile", token: this.token });
+    cy.visitApp({ path: "profile", token: user.token });
 
     cy.get("[name=firstname]").should("have.value", this.profile.firstname);
     cy.get("[name=lastname]").should("have.value", this.profile.lastname);
@@ -37,23 +33,17 @@ describe("The user", () => {
 });
 
 describe("The company", () => {
-  before(() => {
-    cy.createGithubUser(54321).then(user => {
-      cy.signinUser(user)
-        .then(user => JSON.stringify(user.session))
-        .as("token");
-    });
-  });
+  let user: User;
 
-  beforeEach(() => {
+  beforeEach(function () {
+    user = this.users["Oscar"];
     cy.fixture("profiles/mi6").as("profile");
   });
 
   it("can fill their personal info", function () {
-    cy.visitApp({ path: "profile", token: this.token });
+    cy.visitApp({ path: "profile", token: user.token });
 
-    cy.wait(1000);
-    cy.get("[data-testid=COMPANY]").click().wait(100);
+    cy.get("[data-testid=COMPANY]").click();
     cy.get("[name=companyName]").clear().type(this.profile.name);
     cy.get("[name=identificationNumber]").clear().type(this.profile.identificationNumber);
     cy.get("[name=firstname]").clear().type(this.profile.companyOwnerFirstName);
@@ -77,7 +67,7 @@ describe("The company", () => {
   });
 
   it("can see their personal info pre-filled", function () {
-    cy.visitApp({ path: "profile", token: this.token });
+    cy.visitApp({ path: "profile", token: user.token });
 
     cy.get("[name=companyName]").should("have.value", this.profile.name);
     cy.get("[name=identificationNumber]").should("have.value", this.profile.identificationNumber);
@@ -92,7 +82,7 @@ describe("The company", () => {
     cy.get("[name=city]").should("have.value", this.profile.city);
     cy.get("[name=country]").should("have.value", this.profile.country);
     cy.get("[id=BANK_ADDRESS]").should("be.checked");
-    cy.get("[name=IBAN]").should("have.value", IBAN.printFormat(this.profile.IBAN));
+    cy.get("[name=IBAN]").should("have.value", printFormat(this.profile.IBAN));
     cy.get("[name=BIC]").should("have.value", this.profile.BIC);
   });
 });
