0231888b0caec748914681a00dd495423602d523
Anthony Buisset <abuisset@gmail.com>
2023-03-16 18:53:54 +0100

    🐛 fix invoice rejected projection

--- backend/event-listeners/src/domain/projections/payment_request.rs
+++ backend/event-listeners/src/domain/projections/payment_request.rs
@@ -6,6 +6,7 @@ use infrastructure::database::schema::payment_requests;
 use serde_json::Value;
 
 #[derive(Debug, Insertable, Identifiable, Queryable, AsChangeset, Constructor, Getters)]
+#[changeset_options(treat_none_as_null = "true")]
 pub struct PaymentRequest {
 	id: PaymentId,
 	budget_id: BudgetId,
