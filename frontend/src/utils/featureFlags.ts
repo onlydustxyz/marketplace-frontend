7d080ecc026b76529a70e3fe2b06e2b30b6a99a3
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-03-20 15:31:08 +0100

    :fire: remove feature flags file

--- frontend/src/utils/featureFlags.ts
+++
@@ -1,9 +0,0 @@
-export enum FeatureFlags {}
-
-type FeatureFlagsConfig = Record<FeatureFlags, boolean>;
-
-const featureFlags: FeatureFlagsConfig = {};
-
-export const isFeatureEnabled = (feature: keyof FeatureFlagsConfig) => {
-  return featureFlags[feature];
-};
