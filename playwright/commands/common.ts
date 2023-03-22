857898d4c8e3de496d953e79fb6c75e1a789eeac
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-22 14:51:00 +0100

    ✨ add common helper functions

---
+++ playwright/commands/common.ts
@@ -0,0 +1,153 @@
+import {
+  ApolloClient,
+  DefaultContext,
+  HttpLink,
+  InMemoryCache,
+  MutationOptions,
+  NormalizedCacheObject,
+  OperationVariables,
+  QueryOptions,
+} from "@apollo/client/core";
+import { expect } from "@playwright/test";
+
+export type Url = string;
+export type Uuid = string;
+
+export const getEnv = (key: string) => {
+  const value = process.env[key];
+  if (value !== undefined) {
+    return value;
+  }
+  throw new Error(`Environment variable '${key}' is not defined`);
+};
+
+const apolloClientAnonymous = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:8080/v1/graphql",
+    fetch,
+    credentials: "same-origin",
+  }),
+  cache: new InMemoryCache(),
+});
+
+const apolloClientAdmin = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:8080/v1/graphql",
+    fetch,
+    headers: {
+      "X-Hasura-Admin-Secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET || "",
+    },
+    credentials: "same-origin",
+  }),
+  cache: new InMemoryCache(),
+});
+
+const apolloClientRegisteredUser = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:8080/v1/graphql",
+    fetch,
+    headers: {
+      "X-Hasura-Role": "registered_user",
+    },
+    credentials: "same-origin",
+  }),
+  cache: new InMemoryCache(),
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mutateAsAnonymous = async <TData = any, TVariables = OperationVariables, TContext = DefaultContext>(
+  options: MutationOptions<TData, TVariables, TContext>,
+  expectError = false
+) => mutateAs(apolloClientAnonymous, options, expectError);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mutateAsAdmin = async <TData = any, TVariables = OperationVariables, TContext = DefaultContext>(
+  options: MutationOptions<TData, TVariables, TContext>,
+  expectError = false
+) => mutateAs(apolloClientAdmin, options, expectError);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mutateAsRegisteredUser = async <TData = any, TVariables = OperationVariables, TContext = DefaultContext>(
+  accessToken: string,
+  options: MutationOptions<TData, TVariables, TContext>,
+  expectError = false
+) => {
+  return mutateAs(
+    apolloClientRegisteredUser,
+    {
+      mutation: options.mutation,
+      variables: options.variables,
+      context: { ...options.context, headers: { Authorization: `Bearer ${accessToken}` } },
+    },
+    expectError
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mutateAs = async <TData = any, TVariables = OperationVariables, TEContext = DefaultContext>(
+  client: ApolloClient<NormalizedCacheObject>,
+  options: MutationOptions<TData, TVariables, TEContext>,
+  expectError: boolean
+) => {
+  const response = await client.mutate({
+    mutation: options.mutation,
+    variables: options.variables,
+    context: options.context,
+    fetchPolicy: "network-only",
+  });
+  if (expectError) {
+    expect(response.errors).toBeDefined();
+  } else {
+    expect(response.errors).toBeUndefined();
+  }
+  return response;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const queryAsAnonymous = async <T = any, TVariables = OperationVariables>(
+  options: QueryOptions<TVariables, T>,
+  expectError = false
+) => queryAs(apolloClientAnonymous, options, expectError);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const queryAsAdmin = async <T = any, TVariables = OperationVariables>(
+  options: QueryOptions<TVariables, T>,
+  expectError = false
+) => queryAs(apolloClientAdmin, options, expectError);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const queryAsRegisteredUser = async <T = any, TVariables = OperationVariables>(
+  accessToken: string,
+  options: QueryOptions<TVariables, T>,
+  expectError = false
+) => {
+  return queryAs(
+    apolloClientRegisteredUser,
+    {
+      query: options.query,
+      variables: options.variables,
+      context: { ...options.context, headers: { Authorization: `Bearer ${accessToken}` } },
+    },
+    expectError
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const queryAs = async <T = any, TVariables = OperationVariables>(
+  client: ApolloClient<NormalizedCacheObject>,
+  options: QueryOptions<TVariables, T>,
+  expectError: boolean
+) => {
+  const response = await client.query({
+    query: options.query,
+    variables: options.variables,
+    context: options.context,
+    fetchPolicy: "network-only",
+  });
+  if (expectError) {
+    expect(response.errors).toBeDefined();
+  } else {
+    expect(response.errors).toBeUndefined();
+  }
+  return response;
+};
