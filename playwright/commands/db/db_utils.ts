7336ec55577ee22f184d42816af4f7251fdcd5dc
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-23 15:40:01 +0100

    ♻️ improve playwright db utils

--- playwright/commands/db/db_utils.ts
+++ playwright/commands/db/db_utils.ts
@@ -3,18 +3,22 @@ import { getEnv } from "../common";
 
 export const dumpDB = () => {
   const DATABASE_URL = getEnv("DATABASE_URL");
-  execSync(`pg_dump --clean --exclude-schema=hdb_catalog ${DATABASE_URL} > "playwright/marketplace_db_dump"`);
+  execSync(`pg_dump --clean --exclude-schema=hdb_catalog ${DATABASE_URL} > "playwright/marketplace_db_dump"`, {
+    stdio: "pipe",
+  });
 };
 
 export const restoreDB = () => {
   const DATABASE_URL = getEnv("DATABASE_URL");
   execSync(
-    `if [ -f "playwright/marketplace_db_dump" ]; then psql ${DATABASE_URL} < "playwright/marketplace_db_dump"; fi`
+    `if [ -f "playwright/marketplace_db_dump" ]; then psql ${DATABASE_URL} < "playwright/marketplace_db_dump"; fi`,
+    { stdio: "pipe" }
   );
 };
 
 export const cleanupDB = () => {
   const DATABASE_URL = getEnv("DATABASE_URL");
-  execSync('rm -f "playwright/marketplace_db_dump"');
-  execSync(`psql ${DATABASE_URL} -f "playwright/commands/db/truncate.sql"`);
+  execSync(`rm -f "playwright/marketplace_db_dump" && psql ${DATABASE_URL} -f "playwright/commands/db/truncate.sql"`, {
+    stdio: "pipe",
+  });
 };
