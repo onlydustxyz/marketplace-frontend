0456587e6004d5c2440ed71ce5878403d2323863
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-23 17:56:01 +0100

    ♻️ log elapsed time in populate

--- playwright/__populate.spec.ts
+++ playwright/__populate.spec.ts
@@ -7,8 +7,10 @@ test.describe("Once and before all test suites, ", () => {
   test("cleanup and populate some data", async ({ request }) => {
     test.skip(fs.existsSync(DUMP_PATH), `DB dump already exists at ${DUMP_PATH}`);
 
+    console.time("Fixtures populated in");
     cleanupDB();
     await populate(request);
     dumpDB();
+    console.timeEnd("Fixtures populated in");
   });
 });
