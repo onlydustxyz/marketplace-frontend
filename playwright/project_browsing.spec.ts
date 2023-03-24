6f9bd356610e93ac97a558681dffd545011beb1d
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-03-24 10:13:24 +0100

    ♻️ add Page object model for browse projects page

--- playwright/project_browsing.spec.ts
+++ playwright/project_browsing.spec.ts
@@ -1,6 +1,7 @@
 import { test } from "./fixtures";
 import { expect } from "@playwright/test";
 import { restoreDB } from "./commands/db/db_utils";
+import { BrowseProjectsPage } from "./pages/browse_projects_page";
 
 test.describe("As a visitor, I", () => {
   test.beforeAll(async () => {
@@ -8,65 +9,44 @@ test.describe("As a visitor, I", () => {
   });
 
   test("can list and filter projects", async ({ page, projects }) => {
-    await page.goto("/");
+    const browseProjectsPage = new BrowseProjectsPage(page);
+    await browseProjectsPage.goto();
 
     // Projects
-    const projectALocator = page.locator(`a[href='/projects/${projects.ProjectA.id}']`);
-    const projectBLocator = page.locator(`a[href='/projects/${projects.ProjectB.id}']`);
-    const kakarotLocator = page.locator(`a[href='/projects/${projects.Kakarot.id}']`);
-    const emptyProjectLocator = page.locator(`a[href='/projects/${projects.Empty.id}']`);
+    await browseProjectsPage.expectProjectToBeVisible(projects.ProjectA, {
+      logoSrc: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/14623987721662397761.png",
+      contributors: "3 contributors",
+      repositories: "2 repositories",
+    });
+    await browseProjectsPage.expectProjectToBeVisible(projects.ProjectB, {
+      contributors: "2 contributors",
+      repositories: "1 repository",
+    });
 
-    await expect(projectALocator.getByText(projects.ProjectA.name, { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText(projects.ProjectB.name, { exact: true })).toBeVisible();
-    await expect(kakarotLocator.getByText(projects.Kakarot.name, { exact: true })).toBeVisible();
-    await expect(emptyProjectLocator.getByText(projects.Empty.name, { exact: true })).toBeVisible();
+    await browseProjectsPage.expectProjectsToBeVisible(projects.Kakarot, projects.Empty);
+    await browseProjectsPage.expectProjectsNotToBeVisible(projects.RepoLess);
 
-    await expect(projectALocator.locator("img[alt='Project Logo']")).toHaveAttribute(
-      "src",
-      "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/14623987721662397761.png"
+    // Filtering
+    await browseProjectsPage.expectFiltersToBeVisible("Rust", "HTML", "StarkNet", "Ether Foundation");
+
+    // Test filter
+    await browseProjectsPage.filterBy("Rust");
+    await browseProjectsPage.expectProjectsToBeVisible(projects.ProjectA);
+    await browseProjectsPage.expectProjectsNotToBeVisible(projects.ProjectB, projects.Empty);
+
+    // Clear filters
+    await browseProjectsPage.clearFilters();
+    await browseProjectsPage.expectProjectsToBeVisible(
+      projects.ProjectA,
+      projects.ProjectB,
+      projects.Kakarot,
+      projects.Empty
     );
-    await expect(projectALocator.getByText("3 contributors", { exact: true })).toBeVisible();
-    await expect(projectALocator.getByText("2 repositories", { exact: true })).toBeVisible();
-
-    await expect(projectBLocator.getByText("2 contributors", { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText("1 repository", { exact: true })).toBeVisible();
-
-    // Filtering
-    const rustFilterLocator = page.getByText("Rust", { exact: true });
-    await expect(rustFilterLocator).toBeVisible();
-    await expect(page.getByText("HTML", { exact: true })).toBeVisible();
 
     // Test filter
-    await rustFilterLocator.click();
-    await expect(projectALocator.getByText(projects.ProjectA.name, { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText(projects.ProjectB.name, { exact: true })).not.toBeVisible();
-    await expect(emptyProjectLocator.getByText(projects.Empty.name, { exact: true })).not.toBeVisible();
-
-    // Clear filters
-    await page.getByText("Clear all", { exact: true }).click();
-    await expect(projectALocator.getByText(projects.ProjectA.name, { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText(projects.ProjectB.name, { exact: true })).toBeVisible();
-    await expect(kakarotLocator.getByText(projects.Kakarot.name, { exact: true })).toBeVisible();
-    await expect(emptyProjectLocator.getByText(projects.Empty.name, { exact: true })).toBeVisible();
-
-    // Filtering
-    const starknetFilterLocator = page.getByText("StarkNet", { exact: true });
-    await expect(starknetFilterLocator).toBeVisible();
-    await expect(page.getByText("Ether Foundation", { exact: true })).toBeVisible();
-
-    // Test filter
-    await starknetFilterLocator.click();
-    await expect(projectALocator.getByText(projects.ProjectA.name, { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText(projects.ProjectB.name, { exact: true })).toBeVisible();
-    await expect(kakarotLocator.getByText(projects.Kakarot.name, { exact: true })).toBeVisible();
-    await expect(emptyProjectLocator.getByText(projects.Empty.name, { exact: true })).not.toBeVisible();
-
-    // Clear filters
-    await page.getByText("Clear all", { exact: true }).click();
-    await expect(projectALocator.getByText(projects.ProjectA.name, { exact: true })).toBeVisible();
-    await expect(projectBLocator.getByText(projects.ProjectB.name, { exact: true })).toBeVisible();
-    await expect(kakarotLocator.getByText(projects.Kakarot.name, { exact: true })).toBeVisible();
-    await expect(emptyProjectLocator.getByText(projects.Empty.name, { exact: true })).toBeVisible();
+    await browseProjectsPage.filterBy("StarkNet");
+    await browseProjectsPage.expectProjectsToBeVisible(projects.ProjectA, projects.ProjectB, projects.Kakarot);
+    await browseProjectsPage.expectProjectsNotToBeVisible(projects.Empty);
   });
 
   test("cannot access restricted projects page", async ({ page, projects }) => {
