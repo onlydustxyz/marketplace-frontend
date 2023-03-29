e269c5e5d44eb0b4a1c23c7edaf50d29bff6873e
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-03-29 18:23:44 +0200

    :lipstick: fix full screen display

--- frontend/src/components/Background/index.tsx
+++ frontend/src/components/Background/index.tsx
@@ -14,12 +14,15 @@ type Props = {
 export default function Background({ roundedBorders, withSidebar = false, children }: Props) {
   return (
     <div
-      className={classNames("overflow-y-auto mx-6 scrollbar-none h-full w-full mb-6", {
+      className={classNames("overflow-y-auto scrollbar-none h-full w-full mb-6", {
         "mr-6 ml-2": withSidebar,
+        "mx-6 pr-6": !withSidebar,
       })}
     >
       <div
-        className={classNames("bg-space bg-no-repeat bg-fixed bg-clip-content min-h-full min-w-full", roundedBorders)}
+        className={classNames("bg-space bg-no-repeat bg-fixed bg-clip-content min-h-full", roundedBorders, {
+          "mr-6": !withSidebar,
+        })}
       >
         {children}
       </div>
