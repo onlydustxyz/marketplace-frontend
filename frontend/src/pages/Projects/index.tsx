fb583a92b641477abb5f46d4024580831ff38e57
Oscar Roche <oscarroche@Oscars-MacBook-Pro.local>
2023-03-29 17:50:28 +0200

    :lipstick: make filter panel sticky

--- frontend/src/pages/Projects/index.tsx
+++ frontend/src/pages/Projects/index.tsx
@@ -12,10 +12,10 @@ export default function Projects() {
   return (
     <ProjectFilterProvider>
       <Background roundedBorders={BackgroundRoundedBorders.Full}>
-        <div className="md:container md:mx-auto pt-8 xl:pt-16 pb-8 px-4 md:px-12 h-full">
+        <div className="md:container md:mx-auto pt-8 xl:pt-16 pb-8 px-4 md:px-12">
           <div className="hidden xl:block text-5xl font-belwe">{T("navbar.projects")}</div>
           <div className="flex xl:mt-8 gap-6 h-full">
-            <div className="hidden xl:block basis-80 shrink-0">
+            <div className="hidden xl:block basis-80 shrink-0 sticky top-0">
               <FilterPanel isProjectLeader={!!ledProjectIds.length} />
             </div>
             <AllProjects />
