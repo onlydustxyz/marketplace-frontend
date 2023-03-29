f7dcb72741057d32045b5789e47735cc33e9a7dc
Anthony Buisset <abuisset@gmail.com>
2023-03-29 13:18:37 +0200

    ⚰️ remove attachments

--- frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/OtherWorkForm/index.tsx
+++ frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/OtherWorkForm/index.tsx
@@ -19,7 +19,6 @@ import {
   GetProjectReposQuery,
   RepositoryOwnerAndNameFragment,
 } from "src/__generated/graphql";
-import Attachments from "./Attachments";
 import Description from "./Description";
 import RepoSelect from "./RepoSelect";
 import Title from "./Title";
@@ -99,7 +98,6 @@ export default function OtherWorkForm({ projectId, contributorHandle, onWorkItem
       <WorkKinds workKind={selectedWorkKind} setWorkKind={setSelectedWorkKind} />
       <Title title={title} setTitle={setTitle} defaultTitle={defaultTitle} />
       <Description description={description} setDescription={setDescription} />
-      <Attachments />
       <Callout>{T("payment.form.workItems.other.callout")}</Callout>
       <div className="fixed bottom-0 inset-x-0 flex flex-row gap-8 px-6 py-6 bg-white/2 border-t border-greyscale-50/8">
         {selectedRepo ? (
