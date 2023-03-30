a39d9b8b1ef77ce1584925654f1e3181286d21f4
Stanislas Bernard <bernardstanislas@gmail.com>
2023-03-30 15:41:33 +0200

    :recycle: Use own label in other work description

--- frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/OtherWorkForm/Description.tsx
+++ frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/OtherWorkForm/Description.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import Input from "src/components/FormInput";
 import { useIntl } from "src/hooks/useIntl";
 
@@ -8,18 +9,21 @@ type Props = {
 
 export default function Description({ description, setDescription }: Props) {
   const { T } = useIntl();
+  const name = "other-work-description";
 
   return (
-    <Input
-      name="other-work-description"
-      label={T("payment.form.workItems.other.inputs.description")}
-      placeholder={T("payment.form.workItems.other.issue.descriptionPlaceholder")}
-      value={description}
-      onChange={({ target }) => setDescription(target.value)}
-      as="textarea"
-      inputProps={{
-        rows: 5,
-      }}
-    />
+    <label htmlFor={name} className={classNames("flex flex-col flex-grow gap-2 text-greyscale-300 font-walsheim")}>
+      <div className="font-medium text-sm tracking-tight">{T("payment.form.workItems.other.inputs.description")}</div>
+      <Input
+        name="other-work-description"
+        placeholder={T("payment.form.workItems.other.issue.descriptionPlaceholder")}
+        value={description}
+        onChange={({ target }) => setDescription(target.value)}
+        as="textarea"
+        inputProps={{
+          rows: 5,
+        }}
+      />
+    </label>
   );
 }
