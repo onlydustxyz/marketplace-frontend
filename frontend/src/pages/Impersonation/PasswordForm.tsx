d993dea88c2e95221a71045144ab8be687dcaeba
Stanislas Bernard <bernardstanislas@gmail.com>
2023-04-07 16:18:38 +0200

    :sparkles: Impersonation password form

---
+++ frontend/src/pages/Impersonation/PasswordForm.tsx
@@ -0,0 +1,36 @@
+import { FormProvider, useForm } from "react-hook-form";
+import Card from "src/components/Card";
+import FormInput from "src/components/FormInput";
+import { useIntl } from "src/hooks/useIntl";
+
+type Props = {
+  onSubmit: (password: string) => void;
+};
+
+const PasswordForm: React.FC<Props> = ({ onSubmit }) => {
+  const methods = useForm({ defaultValues: { password: "" } });
+  const password = methods.watch("password");
+  const { T } = useIntl();
+
+  return (
+    <div className="w-96">
+      <Card>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(({ password }) => onSubmit(password))} className="flex flex-col gap-4">
+            <h1 className="text-center font-bold text-orange-500 text-lg">{T("impersonation.title")}</h1>
+            <i className="text-sm text-spaceBlue-200">{T("impersonation.passwordNotice")}</i>
+            <FormInput
+              name="password"
+              label={T("impersonation.passwordLabel")}
+              type="password"
+              options={{ required: { value: password.length === 0, message: T("impersonation.missingPassword") } }}
+              withMargin={false}
+            />
+          </form>
+        </FormProvider>
+      </Card>
+    </div>
+  );
+};
+
+export default PasswordForm;
--- frontend/src/pages/Impersonation/index.tsx
+++ frontend/src/pages/Impersonation/index.tsx
@@ -2,23 +2,30 @@ import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { RoutePaths } from "src/App";
 import { useImpersonationClaims } from "src/hooks/useImpersonationClaims";
-import { useIntl } from "src/hooks/useIntl";
+import PasswordForm from "src/pages/Impersonation/PasswordForm";
 
 const ImpersonationPage = () => {
   const { userId } = useParams();
-  const { T } = useIntl();
   const navigate = useNavigate();
   const { setImpersonationSet } = useImpersonationClaims();
-  useEffect(() => {
-    const password = window.prompt(T("impersonation.passwordPrompt"));
-    if (password && password !== "" && userId) {
+
+  const onPasswordSubmit = (password: string) => {
+    if (userId) {
       setImpersonationSet({ password, userId });
     }
     navigate(RoutePaths.Projects);
-  });
+  };
+
+  useEffect(() => {
+    if (!userId) {
+      navigate(RoutePaths.Projects);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId]);
+
   return (
-    <div>
-      <h1>Impersonation</h1>
+    <div className="h-screen flex items-center justify-center">
+      <PasswordForm onSubmit={onPasswordSubmit} />
     </div>
   );
 };
