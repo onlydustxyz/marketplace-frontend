760686e729ecf113b485d2450b13115c6c3a5404
Anthony Buisset <abuisset@gmail.com>
2023-04-14 12:13:27 +0200

    🔨 update install-datadog.sh

--- scripts/install-datadog.sh
+++ scripts/install-datadog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage
-# ./install-datadog.sh <staging|production> <my-api-key-*******>
+# ./install-datadog.sh <develop|staging|production> <my-api-key-*******>
 
 ENV=$1
 DD_API_KEY=$2
@@ -13,6 +13,8 @@ add_log_drain() {
     HOST=""
     if [ $ENV = "staging" ]; then
         HOST="staging.${SUBDOMAIN}.onlydust.xyz"
+    elif [ $ENV = "develop" ]; then
+        HOST="develop.${SUBDOMAIN}.onlydust.xyz"
     elif [ $ENV = "production" ]; then
         HOST="${SUBDOMAIN}.onlydust.xyz"
     else
@@ -37,6 +39,8 @@ add_log_drain hasura-auth auth
 HOST=""
 if [ $ENV = "staging" ]; then
     HOST='staging.api.onlydust.xyz'
+elif [ $ENV = "develop" ]; then
+    HOST='develop.api.onlydust.xyz'
 elif [ $ENV = "production" ]; then
     HOST='api.onlydust.xyz'
 else
@@ -49,24 +53,14 @@ do
 
     echo "Installing Datadog agent for application $APPNAME with {SERVICE=$SERVICE, ENV=$ENV, HOST=$HOST}"
 
-    # Use the latest major Agent version
-    heroku config:add DD_AGENT_MAJOR_VERSION=7 -a $APPNAME
-
-    # Enable tracer startup logs
-    heroku config:add DD_TRACE_STARTUP_LOGS=true -a $APPNAME
-
-    # We use Datadog EU site - this is important to have a site that matches our API Keys
-    heroku config:add DD_SITE=datadoghq.eu -a $APPNAME
+    # Add env variables
+    heroku config:add -a $APPNAME DD_AGENT_MAJOR_VERSION=7 DD_SITE=datadoghq.eu DD_DYNO_HOST=true DD_API_KEY=$DD_API_KEY DD_LOG_TO_CONSOLE=false
 
     # Enable Heroku Labs Dyno Metadata to set HEROKU_APP_NAME env variable automatically
     heroku labs:enable runtime-dyno-metadata -a $APPNAME
 
-    # Set hostname in Datadog as appname.dynotype.dynonumber for metrics continuity
-    heroku config:add DD_DYNO_HOST=true -a $APPNAME
-
     # Add this buildpack and set your Datadog API key
     heroku buildpacks:add --index 1 https://github.com/DataDog/heroku-buildpack-datadog.git#2.6 -a $APPNAME
-    heroku config:add DD_API_KEY=$DD_API_KEY -a $APPNAME
 
     # Add drain for logs
     heroku drains:add "https://http-intake.logs.datadoghq.eu/api/v2/logs/?dd-api-key=${DD_API_KEY}&ddsource=heroku&env=${ENV}&service=${SERVICE}&host=${HOST}" -a $APPNAME
