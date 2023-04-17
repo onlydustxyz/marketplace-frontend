e93bc259d303001028688c6fcc6c78582ee9595a
Anthony Buisset <abuisset@gmail.com>
2023-04-17 15:04:46 +0200

    🚚 rename github events indexer binary

--- backend/event-listeners/src/bin/github-events-indexer/main.rs
+++ backend/event-listeners/src/bin/github-events-indexer/main.rs
@@ -11,7 +11,8 @@ mod indexer;
 #[tokio::main]
 async fn main() -> Result<()> {
 	dotenv().ok();
-	let config: Config = config::load("backend/event-listeners/src/bin/github/app.yaml")?;
+	let config: Config =
+		config::load("backend/event-listeners/src/bin/github-events-indexer/app.yaml")?;
 	let _tracer = Tracer::init(config.tracer(), "github")?;
 	let github = Arc::<github::Client>::new(github::RoundRobinClient::new(config.github())?.into());
 	let database = Arc::new(database::Client::new(database::init_pool(
