4ed96b2e502b37843e3d55fda8a1c76754bbc37f
Anthony Buisset <abuisset@gmail.com>
2023-04-25 15:51:35 +0200

    ♻️ add mailto link in error screen

--- frontend/src/components/ErrorFallback/index.tsx
+++ frontend/src/components/ErrorFallback/index.tsx
@@ -11,13 +11,19 @@ export default function ErrorFallback() {
   const { T } = useIntl();
   const { resetBoundary } = useErrorBoundary();
 
+  const [begin, link, end] = T("state.error.description").split("_");
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center gap-12">
       <OnlyDustCrashedLogo />
       <div className="flex flex-col gap-6 w-72">
         <div className="font-normal font-belwe text-3xl text-greyscale-50">{T("state.error.title")}</div>
         <div className="font-normal font-walsheim text-lg text-spaceBlue-200 px-3.5">
-          {T("state.error.description")}
+          {begin}
+          <a className="underline" href={"mailto:contact@onlydust.xyz"}>
+            {link}
+          </a>
+          {end}
         </div>
       </div>
       <div className="flex flex-row gap-4 items-center">
