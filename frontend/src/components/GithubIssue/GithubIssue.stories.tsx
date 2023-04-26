e0a777eb9b749a604cf19e57463bf9ee8b3c40bf
Oscar Roche <oscarroche@oscarroche.home>
2023-04-26 12:02:39 +0200

    :lisptick: added long link story

--- frontend/src/components/GithubIssue/GithubIssue.stories.tsx
+++ frontend/src/components/GithubIssue/GithubIssue.stories.tsx
@@ -15,6 +15,19 @@ const issues = {
     closedAt: daysFromNow(5),
     mergedAt: null,
   },
+  closedWithLongLink: {
+    id: 1268051991,
+    repoId: 123456,
+    number: 541,
+    type: Type.Issue,
+    status: Status.Closed,
+    htmlUrl: "https://github.com/sayajin-labs/kakarot/pull/541",
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    createdAt: daysFromNow(6),
+    closedAt: daysFromNow(5),
+    mergedAt: null,
+  },
   prOpen: {
     id: 1268051991,
     repoId: 123456,
@@ -93,8 +106,13 @@ export default {
   },
 };
 
-const props: Props = { workItem: issues.closed };
+const defaultProps: Props = { workItem: issues.closed };
 
 export const Default = {
-  render: (args: Props) => <GithubIssue {...props} {...args} />,
+  render: (args: Props) => <GithubIssue {...defaultProps} {...args} />,
+};
+const longLinkProps: Props = { workItem: issues.closedWithLongLink };
+
+export const LongLink = {
+  render: (args: Props) => <GithubIssue {...longLinkProps} {...args} />,
 };
