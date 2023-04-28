174ba81dbce1a158661803f9745946fbe2f1243f
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-04-28 10:07:31 +0200

    💄 restore white button on maintenance page (#929)
    Co-authored-by: Olivier Fuxet <ofux@users.noreply.github.com>

--- frontend/src/Maintenance/index.tsx
+++ frontend/src/Maintenance/index.tsx
@@ -1,12 +1,13 @@
 import { useIntl } from "src/hooks/useIntl";
 import MaintenanceAnimation from "src/assets/animations/Maintenance";
+import Button from "src/components/Button";
 
 export default function Maintenance() {
   const { T } = useIntl();
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center">
-      <div className="w-72 -mb-8 stroke-white">
+    <div className="min-h-screen flex flex-col items-center justify-center text-center gap-12">
+      <div className="w-72 -mb-20 stroke-white -mt-20">
         <MaintenanceAnimation />
       </div>
       <div className="flex flex-col gap-6 w-110">
@@ -15,6 +16,9 @@ export default function Maintenance() {
           {T("state.maintenance.description")}
         </div>
       </div>
+      <a href="https://onlydust.xyz" rel="noreferrer">
+        <Button>{T("state.maintenance.visitButton")}</Button>
+      </a>
     </div>
   );
 }
