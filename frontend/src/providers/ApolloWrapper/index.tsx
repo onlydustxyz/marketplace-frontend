5aa117ec6406c3755fecb021cd2b9aa6475f3082
Anthony Buisset <abuisset@gmail.com>
2023-05-01 23:16:51 +0200

    ♻️ put apollo client in a state
    🐛 use only one instance of apollo client and cache

--- frontend/src/providers/ApolloWrapper/index.tsx
+++ frontend/src/providers/ApolloWrapper/index.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { ApolloClient, ApolloProvider, SuspenseCache, disableFragmentWarnings } from "@apollo/client";
 import useApolloLink from "./links";
 import useApolloCache from "./cache";
@@ -8,13 +8,12 @@ disableFragmentWarnings();
 export default function ApolloWrapper({ children }: PropsWithChildren) {
   const link = useApolloLink();
   const cache = useApolloCache();
-
-  const client = new ApolloClient({
-    link,
-    cache,
-  });
-
   const suspenseCache = new SuspenseCache();
+
+  const [client] = useState(() => new ApolloClient({ link, cache }));
+
+  // Update the link whenever the accessToken changes
+  client.setLink(link);
 
   return (
     <ApolloProvider client={client} suspenseCache={suspenseCache}>
