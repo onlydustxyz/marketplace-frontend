e10da53594bd8a2b136e701862850e4f0f3bbb56
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-05-02 12:43:42 +0200

    [B-889] Make Apollo DevTools work (#939)
    Co-authored-by: Olivier Fuxet <ofux@users.noreply.github.com>

--- frontend/src/providers/ApolloWrapper/index.tsx
+++ frontend/src/providers/ApolloWrapper/index.tsx
@@ -1,20 +1,25 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { ApolloClient, ApolloProvider, SuspenseCache, disableFragmentWarnings } from "@apollo/client";
 import useApolloLink from "./links";
 import useApolloCache from "./cache";
+import { useRoles } from "src/hooks/useAuth/useRoles";
 
 disableFragmentWarnings();
 
 export default function ApolloWrapper({ children }: PropsWithChildren) {
   const link = useApolloLink();
   const cache = useApolloCache();
-
-  const client = new ApolloClient({
-    link,
-    cache,
-  });
-
   const suspenseCache = new SuspenseCache();
+  const { isLoggedIn } = useRoles();
+
+  const [client] = useState(() => new ApolloClient({ link, cache }));
+
+  // Update the link whenever the accessToken changes
+  client.setLink(link);
+
+  useEffect(() => {
+    client.resetStore();
+  }, [isLoggedIn]);
 
   return (
     <ApolloProvider client={client} suspenseCache={suspenseCache}>
