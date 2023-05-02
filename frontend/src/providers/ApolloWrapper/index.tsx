e11738b3034408877fb30593dfb1bd6df95de115
Anthony Buisset <abuisset@gmail.com>
2023-05-02 11:26:46 +0200

    🐛 reset apollo store once at login logout time
    Co-authored-by: Olivier Fuxet <ofux@users.noreply.github.com>

--- frontend/src/providers/ApolloWrapper/index.tsx
+++ frontend/src/providers/ApolloWrapper/index.tsx
@@ -1,7 +1,8 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { ApolloClient, ApolloProvider, SuspenseCache, disableFragmentWarnings } from "@apollo/client";
 import useApolloLink from "./links";
 import useApolloCache from "./cache";
+import { useRoles } from "src/hooks/useAuth/useRoles";
 
 disableFragmentWarnings();
 
@@ -9,11 +10,16 @@ export default function ApolloWrapper({ children }: PropsWithChildren) {
   const link = useApolloLink();
   const cache = useApolloCache();
   const suspenseCache = new SuspenseCache();
+  const { isLoggedIn } = useRoles();
 
   const [client] = useState(() => new ApolloClient({ link, cache }));
 
   // Update the link whenever the accessToken changes
   client.setLink(link);
+
+  useEffect(() => {
+    client.resetStore();
+  }, [isLoggedIn]);
 
   return (
     <ApolloProvider client={client} suspenseCache={suspenseCache}>
