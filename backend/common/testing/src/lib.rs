f47e7aaf76aa640c905af89e2f87fc50469a4175
Anthony Buisset <abuisset@gmail.com>
2023-05-05 13:49:35 +0200

    🔇 remove diesel instrumentation

--- backend/common/testing/src/lib.rs
+++ backend/common/testing/src/lib.rs
@@ -1,14 +1,13 @@
 pub mod fixtures;
 
-use diesel::Connection;
-use diesel_tracing::pg::InstrumentedPgConnection;
+use diesel::{pg::PgConnection, Connection};
 use r2d2_diesel::ConnectionManager;
-type Pool = r2d2::Pool<ConnectionManager<InstrumentedPgConnection>>;
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 use infrastructure::database;
 
 pub fn init_pool(config: &database::Config) -> Pool {
-	let manager = ConnectionManager::<InstrumentedPgConnection>::new(config.url());
+	let manager = ConnectionManager::<PgConnection>::new(config.url());
 	let pool = Pool::builder().max_size(1).build(manager).unwrap();
 	pool.get().unwrap().begin_test_transaction().unwrap();
 	pool
