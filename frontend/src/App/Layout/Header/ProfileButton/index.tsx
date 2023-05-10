7a7fe16bde24b0fcebf3c1de4efd14ee2bb34d9b
Anthony Buisset <abuisset@gmail.com>
2023-05-10 18:32:54 +0200

    🐛 display orange profile button only when user has pending payments

--- frontend/src/App/Layout/Header/ProfileButton/index.tsx
+++ frontend/src/App/Layout/Header/ProfileButton/index.tsx
@@ -1,14 +1,22 @@
 import { useAuth } from "src/hooks/useAuth";
 import View from "src/App/Layout/Header/ProfileButton/View";
 import usePayoutSettings from "src/hooks/usePayoutSettings";
+import { usePendingUserPaymentsQuery } from "src/__generated/graphql";
 
 const ProfileButton = () => {
   const { user, logout, githubUserId } = useAuth();
   const { avatarUrl, displayName } = user ?? { avatarUrl: null, displayName: "My Account" };
 
   const { valid } = usePayoutSettings(githubUserId);
+  const { data } = usePendingUserPaymentsQuery({ variables: { userId: user?.id }, skip: (valid ?? true) || !user?.id });
 
-  return <View {...{ avatarUrl, displayName, logout, payoutSettingsInvalid: valid === false }} />;
+  const pendingPaymentRequestsCount =
+    data?.user?.githubUser?.paymentRequests.filter(p => p.paymentsAggregate.aggregate?.sum?.amount || 0 < p.amountInUsd)
+      .length || 0;
+
+  const payoutSettingsInvalid = valid === false && pendingPaymentRequestsCount > 0;
+
+  return <View {...{ avatarUrl, displayName, logout, payoutSettingsInvalid }} />;
 };
 
 export default ProfileButton;
