9c554568d14eb4acf7d585e403e590e222a8e067
Anthony Buisset <abuisset@gmail.com>
2023-05-10 14:38:48 +0200

    ✨ implement github user indexer repository

--- backend/event-listeners/src/infrastructure/database/indexer.rs
+++ backend/event-listeners/src/infrastructure/database/indexer.rs
@@ -1,13 +1,51 @@
 use diesel::{QueryDsl, RunQueryDsl};
-use domain::GithubRepoId;
-use infrastructure::database::{schema::github_repo_indexes::dsl, Client};
+use domain::{GithubRepoId, GithubUserId};
+use infrastructure::database::{
+	schema::{github_repo_indexes, github_user_indexes},
+	Client,
+};
 
 use crate::domain::{IndexerRepository, RepositoryResult};
 
 impl IndexerRepository<GithubRepoId> for Client {
 	fn list_items_to_index(&self) -> RepositoryResult<Vec<GithubRepoId>> {
 		let connection = self.connection()?;
-		let ids = dsl::github_repo_indexes.select(dsl::repo_id).load(&*connection)?;
+		let ids = github_repo_indexes::table
+			.select(github_repo_indexes::repo_id)
+			.load(&*connection)?;
+		Ok(ids)
+	}
+}
+
+#[derive(Debug, QueryableByName)]
+#[table_name = "github_user_indexes"]
+struct GithubUserIndex {
+	user_id: GithubUserId,
+}
+
+impl IndexerRepository<GithubUserId> for Client {
+	fn list_items_to_index(&self) -> RepositoryResult<Vec<GithubUserId>> {
+		let connection = self.connection()?;
+
+		const QUERY: &str = r#"
+			SELECT
+				indexes.user_id
+			FROM
+				github_user_indexes indexes
+				LEFT JOIN auth.github_users users ON users.github_user_id = indexes.user_id
+			WHERE
+				user_indexer_state IS NULL OR (
+					users.id IS NOT NULL AND
+					(user_indexer_state ->> 'last_updated_time')::TIMESTAMP < NOW() - 1
+				)
+			"#;
+
+		let ids = diesel::sql_query(QUERY)
+			.load::<GithubUserIndex>(&*connection)?
+			.into_iter()
+			.map(|res| res.user_id)
+			.collect();
+
 		Ok(ids)
 	}
 }
