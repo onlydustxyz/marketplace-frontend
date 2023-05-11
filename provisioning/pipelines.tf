8d533578a4b728ab14829d299de350b3cc27c58e
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-11 17:44:32 +0200

    :recycle: Remove hasura_auth_config var

--- provisioning/pipelines.tf
+++ provisioning/pipelines.tf
@@ -33,3 +33,13 @@ resource "heroku_pipeline" "hasura_auth" {
     type = "team"
   }
 }
+
+variable "stage" {
+  type        = string
+  description = "The pipeline stage the workflow corresponds to"
+}
+
+variable "heroku_team_id" {
+  type        = string
+  description = "The Heroku team id"
+}
--- provisioning/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "stage" {
-  type        = string
-  description = "The pipeline stage the workflow corresponds to"
-}
-
-variable "heroku_team_id" {
-  type        = string
-  description = "The Heroku team id"
-}
