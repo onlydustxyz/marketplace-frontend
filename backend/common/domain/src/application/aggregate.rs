e17354513cb3d4afeda71b9a2d60bbc821078a6f
Anthony Buisset <abuisset@gmail.com>
2023-05-22 10:47:36 +0200

    ✨ new application aggregate and event

---
+++ backend/common/domain/src/application/aggregate.rs
@@ -0,0 +1,42 @@
+use derive_getters::Getters;
+use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
+
+use crate::{Aggregate, ApplicationEvent, ApplicationId, Entity, EventSourcable};
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Status {
+	#[default]
+	Active,
+	Cancelled,
+}
+
+#[serde_as]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Getters)]
+pub struct Application {
+	id: ApplicationId,
+}
+
+impl Default for Application {
+	fn default() -> Self {
+		Self {
+			id: Default::default(),
+		}
+	}
+}
+
+impl Entity for Application {
+	type Id = ApplicationId;
+}
+
+impl Aggregate for Application {
+	type Event = ApplicationEvent;
+}
+
+impl EventSourcable for Application {
+	fn apply_event(self, event: &Self::Event) -> Self {
+		match event {
+			ApplicationEvent::Received { id, .. } => Self { id: *id },
+		}
+	}
+}
