22a32d77acf2c90e1cad04d0671d0a7c62c62f3e
Anthony Buisset <abuisset@gmail.com>
2023-05-22 14:00:19 +0200

    ✅ fix tests

--- frontend/src/components/ProjectCard/ProjectCard.test.tsx
+++ frontend/src/components/ProjectCard/ProjectCard.test.tsx
@@ -29,6 +29,7 @@ const PROJECT: Project = {
     telegramLink: "https://app.onlydust.xyz/projects/92f022a9-dbd8-446f-a2a5-b161ccb4541c",
     shortDescription: "A short description",
     logoUrl: "https://avatars.githubusercontent.com/u/115809607?v=4",
+    hiring: false,
   },
   projectLeads: [
     {
