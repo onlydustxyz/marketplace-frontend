adfa853765e2642e544682d606caf9a9634b9032
Anthony Buisset <abuisset@gmail.com>
2023-05-23 10:16:47 +0200

    ♻️ throttle requests

--- scripts/docGPT/gpt.ts
+++ scripts/docGPT/gpt.ts
@@ -1,5 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 import { OpenAI, PromptTemplate } from "langchain";
+import { size } from "./file.ts";
 
 const MODEL = new OpenAI({
   modelName: "gpt-3.5-turbo",
@@ -22,4 +23,30 @@ export const decorateFile = async (path: string) => {
   writeFileSync(path, decorated);
 };
 
-export const decorateFiles = (paths: string[]) => Promise.all(paths.map(decorateFile));
+export const decorateFiles = async (paths: string[]) => {
+  const requests = [];
+  let sentTokens = 0;
+  let startTime = Date.now();
+
+  const MAX_TOKENS_PER_MIN = 85000;
+
+  for (const [index, path] of paths.entries()) {
+    const fileTokens = tokens(path);
+    if (sentTokens + fileTokens > MAX_TOKENS_PER_MIN) {
+      const elapsedTime = Date.now() - startTime;
+      const waitTime = Math.max(0, 60000 - elapsedTime); // Wait until the minute is over
+      await sleep(waitTime);
+      sentTokens = 0;
+      startTime = Date.now();
+    }
+
+    requests.push(decorateFile(path));
+    sentTokens += fileTokens;
+  }
+
+  await Promise.all(requests);
+};
+
+const tokens = (path: string) => size(path) / 4;
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
