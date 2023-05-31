d27b8fbbb298a1c3002161d16c9bc0d44da03f75
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-05-31 14:30:09 +0200

    ♻️ improve story

--- frontend/src/components/ContributorProfileSidePanel/ContributorProfileSidePanel.stories.tsx
+++ frontend/src/components/ContributorProfileSidePanel/ContributorProfileSidePanel.stories.tsx
@@ -1,4 +1,4 @@
-import { ContributionsFragment, ProfileProjectFragment, UserProfileFragment } from "src/__generated/graphql";
+import { ProfileProjectFragment, UserProfileFragment } from "src/__generated/graphql";
 import ContributorProfileSidePanel, { HeaderColor } from "./View";
 import { Project } from "./ProjectCard";
 import { daysFromNow, minutesFromNow } from "src/utils/date";
@@ -18,7 +18,7 @@ export default {
   },
 };
 
-const profile: UserProfileFragment = {
+const profileFull: UserProfileFragment = {
   __typename: "UserProfiles",
   githubUserId: 43467246,
   login: "AnthonyBuisset",
@@ -44,7 +44,7 @@ const profile: UserProfileFragment = {
     Cairo: 100,
     Solidity: 20,
   },
-  projects: [{} as ContributionsFragment, {} as ContributionsFragment, {} as ContributionsFragment],
+  projects: [],
   projectsLeaded: [{ projectId: "", project: {} as ProfileProjectFragment }],
   projectsAggregate: {
     aggregate: {
@@ -54,9 +54,75 @@ const profile: UserProfileFragment = {
       min: {
         minContributionDate: "2023-02-31T11:31:09.674+00:00",
       },
+      count: 3,
+    },
+  },
+};
+
+const profileNotSignedUp: UserProfileFragment = {
+  __typename: "UserProfiles",
+  githubUserId: 43467246,
+  login: "AnthonyBuisset",
+  avatarUrl: "https://avatars.githubusercontent.com/u/43467246?v=4",
+  htmlUrl: "https://github.com/AnthonyBuisset",
+  location: null,
+  bio: "Anthony Buisset est né le 17 décembre 1991 au Mans. Il commence la pétanque à l'âge de trois ans. Il pratique d'abord ce sport au sein de sa famille, avec son grand-père et son père.",
+  createdAt: null,
+  lastSeen: null,
+  email: "anthony@foobar.org",
+  twitter: "https://twitter.com/antho",
+  telegram: null,
+  linkedin: null,
+  discord: "ANTHO123",
+  website: null,
+  languages: {
+    Rust: 123,
+  },
+  projects: [],
+  projectsLeaded: [],
+  projectsAggregate: {
+    aggregate: {
+      sum: {
+        moneyGranted: 12000,
+      },
+      min: {
+        minContributionDate: "2023-02-31T11:31:09.674+00:00",
+      },
+      count: 1,
+    },
+  },
+};
+
+const profileMinimalist: UserProfileFragment = {
+  __typename: "UserProfiles",
+  githubUserId: 595505,
+  login: "ofux",
+  avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+  htmlUrl: "https://github.com/ofux",
+  location: null,
+  bio: null,
+  languages: {},
+  createdAt: null,
+  lastSeen: null,
+  email: null,
+  twitter: null,
+  telegram: null,
+  linkedin: null,
+  discord: null,
+  website: null,
+  projects: [],
+  projectsAggregate: {
+    aggregate: {
+      sum: {
+        moneyGranted: null,
+      },
+      min: {
+        minContributionDate: null,
+      },
       count: 0,
     },
   },
+  projectsLeaded: [],
 };
 
 const kakarot: Project = {
@@ -111,8 +177,36 @@ export const Default = {
       setOpen={() => {
         return;
       }}
-      profile={profile}
+      profile={profileFull}
       projects={[kakarot, wtf, checkpoint, poseidon]}
+      headerColor={headerColor}
+    />
+  ),
+};
+
+export const NotSignedUp = {
+  render: ({ headerColor = HeaderColor.Blue }: Args) => (
+    <ContributorProfileSidePanel
+      open={true}
+      setOpen={() => {
+        return;
+      }}
+      profile={profileNotSignedUp}
+      projects={[wtf]}
+      headerColor={headerColor}
+    />
+  ),
+};
+
+export const Minimalist = {
+  render: ({ headerColor = HeaderColor.Yellow }: Args) => (
+    <ContributorProfileSidePanel
+      open={true}
+      setOpen={() => {
+        return;
+      }}
+      profile={profileMinimalist}
+      projects={[]}
       headerColor={headerColor}
     />
   ),
