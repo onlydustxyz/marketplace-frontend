d2f6d6af340a99a07da9420b99333995e89de5e0
Anthony Buisset <abuisset@gmail.com>
2023-06-01 22:14:01 +0200

    🐛 hide non visible projects in profile

--- frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/index.tsx
+++ frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/index.tsx
@@ -4,6 +4,8 @@ import { contextWithCacheHeaders } from "src/utils/headers";
 import { Project } from "./ProjectCard";
 import { unionBy } from "lodash";
 import onlyDustLogo from "assets/img/onlydust-logo-space.jpg";
+import { isProjectVisibleToUser } from "src/hooks/useProjectVisibility";
+import { useAuth } from "src/hooks/useAuth";
 
 type Props = {
   githubUserId: number;
@@ -12,29 +14,47 @@ type Props = {
 };
 
 export default function ContributorProfileSidePanel({ githubUserId, ...rest }: Props) {
+  const { user: currentUser, githubUserId: currentUserGithubId } = useAuth();
   const { data } = useUserProfileQuery({ variables: { githubUserId }, ...contextWithCacheHeaders });
   const userProfile = data?.userProfiles.at(0);
+
   const projects: Project[] = unionBy(
-    userProfile?.projectsLeaded.map(
-      project =>
-        ({
-          id: project.projectId,
-          name: project.project?.projectDetails?.name || "",
-          logoUrl: project.project?.projectDetails?.logoUrl || onlyDustLogo,
-          leadSince: new Date(project.assignedAt + "Z"),
-          contributorCount: project.project?.contributorsAggregate.aggregate?.count || 0,
-          totalGranted: project.project?.budgetsAggregate.aggregate?.sum?.spentAmount || 0,
-        } as Project)
-    ),
-    userProfile?.projects.map(project => ({
-      id: project.projectId,
-      name: project.project?.projectDetails?.name || "",
-      logoUrl: project.project?.projectDetails?.logoUrl || onlyDustLogo,
-      contributionCount: project.contributionCount,
-      lastContribution: project.maxContributionDate,
-      contributorCount: project.project?.contributorsAggregate.aggregate?.count || 0,
-      totalGranted: project.project?.budgetsAggregate.aggregate?.sum?.spentAmount || 0,
-    })),
+    userProfile?.projectsLeaded
+      .filter(project =>
+        isProjectVisibleToUser({
+          project: project.project,
+          user: { userId: currentUser?.id, githubUserId: currentUserGithubId },
+        })
+      )
+      .map(
+        project =>
+          ({
+            ...project,
+            id: project.projectId,
+            name: project.project?.projectDetails?.name || "",
+            logoUrl: project.project?.projectDetails?.logoUrl || onlyDustLogo,
+            leadSince: new Date(project.assignedAt + "Z"),
+            contributorCount: project.project?.contributorsAggregate.aggregate?.count || 0,
+            totalGranted: project.project?.budgetsAggregate.aggregate?.sum?.spentAmount || 0,
+          } as Project)
+      ),
+    userProfile?.projects
+      .filter(project =>
+        isProjectVisibleToUser({
+          project: project.project,
+          user: { userId: currentUser?.id, githubUserId: currentUserGithubId },
+        })
+      )
+      .map(project => ({
+        ...project,
+        id: project.projectId,
+        name: project.project?.projectDetails?.name || "",
+        logoUrl: project.project?.projectDetails?.logoUrl || onlyDustLogo,
+        contributionCount: project.contributionCount,
+        lastContribution: project.maxContributionDate,
+        contributorCount: project.project?.contributorsAggregate.aggregate?.count || 0,
+        totalGranted: project.project?.budgetsAggregate.aggregate?.sum?.spentAmount || 0,
+      })),
     "id"
   );
 
