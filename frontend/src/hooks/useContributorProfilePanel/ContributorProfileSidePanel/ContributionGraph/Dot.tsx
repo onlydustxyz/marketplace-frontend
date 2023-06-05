6274d47f4a05418415cde24c44752922cad513bf
Anthony Buisset <abuisset@gmail.com>
2023-06-05 15:29:43 +0200

    💄 change secondary graph color

--- frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/ContributionGraph/Dot.tsx
+++ frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/ContributionGraph/Dot.tsx
@@ -1,10 +1,11 @@
 import { DotProps } from "recharts";
+import { Color } from ".";
 
 type Props = {
-  secondary?: boolean;
+  color?: Color;
 } & DotProps;
 
-export default function Dot({ cx = 0, cy = 0, secondary }: Props) {
+export default function Dot({ cx = 0, cy = 0, color = Color.Primary }: Props) {
   return (
     <svg
       x={cx - 5}
@@ -15,14 +16,7 @@ export default function Dot({ cx = 0, cy = 0, secondary }: Props) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle
-        cx={5}
-        cy={5}
-        r="3.5"
-        fill={secondary ? "#BABDCE" : "#EFCCFF"}
-        stroke={secondary ? "#262D5B" : "#AE00FF"}
-        strokeWidth="3"
-      />
+      <circle cx={5} cy={5} r="3.5" fill={"#F3F0EE"} stroke={color} strokeWidth="3" />
     </svg>
   );
 }
