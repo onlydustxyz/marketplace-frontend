fabd70bb2301d17d26169b866a7d6ff260bc0b97
Anthony Buisset <abuisset@gmail.com>
2023-06-07 23:17:12 +0200

    ♻️ use new hook in contributors page

--- frontend/src/pages/ProjectDetails/Contributors/index.tsx
+++ frontend/src/pages/ProjectDetails/Contributors/index.tsx
@@ -2,41 +2,24 @@ import Card from "src/components/Card";
 import ContributorsTableFallback from "src/components/ContributorsTableFallback";
 import ContributorsTable from "src/pages/ProjectDetails/Contributors/ContributorsTable";
 import { useIntl } from "src/hooks/useIntl";
-import {
-  GetProjectContributorsAsLeaderDocument,
-  GetProjectContributorsDocument,
-  useGetProjectRemainingBudgetQuery,
-} from "src/__generated/graphql";
 import { useAuth } from "src/hooks/useAuth";
 import { useOutletContext } from "react-router-dom";
-import { getContributors } from "src/utils/project";
 import Title from "src/pages/ProjectDetails/Title";
+import useProjectContributors from "src/hooks/useProjectContributors";
+import { useGetProjectDetailsQuery } from "src/__generated/graphql";
 import { contextWithCacheHeaders } from "src/utils/headers";
-import { useSuspenseQuery_experimental } from "@apollo/client";
 
 export default function Contributors() {
   const { T } = useIntl();
   const { ledProjectIds } = useAuth();
   const { projectId } = useOutletContext<{ projectId: string }>();
 
-  const isProjectLeader = !!ledProjectIds.find(element => element === projectId);
+  const isProjectLeader = ledProjectIds.some(element => element === projectId);
 
-  const getProjectContributorsQuery = useSuspenseQuery_experimental(
-    isProjectLeader ? GetProjectContributorsAsLeaderDocument : GetProjectContributorsDocument,
-    {
-      variables: { projectId },
-      ...contextWithCacheHeaders,
-    }
-  );
+  const { contributors } = useProjectContributors(projectId);
+  const { data: projectDetails } = useGetProjectDetailsQuery({ variables: { projectId }, ...contextWithCacheHeaders });
 
-  const getProjectRemainingBudget = useGetProjectRemainingBudgetQuery({
-    variables: { projectId },
-    skip: !isProjectLeader,
-  });
-
-  const contributors = getContributors(getProjectContributorsQuery.data?.projectsByPk);
-
-  const remainingBudget = getProjectRemainingBudget.data?.projectsByPk?.budgets.at(0)?.remainingAmount;
+  const remainingBudget = projectDetails?.projectsByPk?.budgets.at(0)?.remainingAmount;
 
   return (
     <>
@@ -46,7 +29,7 @@ export default function Contributors() {
           <ContributorsTable {...{ contributors, isProjectLeader, remainingBudget, projectId }} />
         </Card>
       ) : (
-        <ContributorsTableFallback projectName={getProjectContributorsQuery.data?.projectsByPk?.projectDetails?.name} />
+        <ContributorsTableFallback projectName={projectDetails?.projectsByPk?.projectDetails?.name} />
       )}
     </>
   );
