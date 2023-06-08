863435010de733a1466688ab7e53b92950235181
Anthony Buisset <abuisset@gmail.com>
2023-06-08 12:41:16 +0200

    🐛 useUnpaidIssues filters by recipient

--- frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/Issues/useUnpaidIssues.ts
+++ frontend/src/pages/ProjectDetails/Payments/PaymentForm/WorkItemSidePanel/Issues/useUnpaidIssues.ts
@@ -11,7 +11,8 @@ type Props = {
 
 export default function useUnpaidIssues({ projectId, authorId, type }: Props) {
   const getPaidItemsQuery = useGetPaidWorkItemsQuery({
-    variables: { projectId },
+    variables: { projectId, githubUserId: authorId },
+    skip: !authorId || !projectId,
   });
 
   const searchIssuesQuery = useSearchIssuesQuery({
@@ -24,8 +25,8 @@ export default function useUnpaidIssues({ projectId, authorId, type }: Props) {
   });
 
   const paidItems = useMemo(
-    () => getPaidItemsQuery.data?.projectsByPk?.budgets.flatMap(b => b.paymentRequests).flatMap(p => p.workItems),
-    [getPaidItemsQuery.data?.projectsByPk?.budgets]
+    () => getPaidItemsQuery.data?.paymentRequests.flatMap(p => p.workItems),
+    [getPaidItemsQuery.data?.paymentRequests]
   );
 
   const eligibleIssues: WorkItem[] | undefined | null = useMemo(
