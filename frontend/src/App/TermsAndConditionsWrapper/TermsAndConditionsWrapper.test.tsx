37fb150608a4ce09d1d234d31ced96d928a81c9c
Oscar Roche <oscarroche@oscarwroche.local>
2023-06-13 10:04:51 +0200

    :fire: remove unused import

--- frontend/src/App/TermsAndConditionsWrapper/TermsAndConditionsWrapper.test.tsx
+++ frontend/src/App/TermsAndConditionsWrapper/TermsAndConditionsWrapper.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, it, vi } from "vitest";
 import { screen, waitForElementToBeRemoved } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
