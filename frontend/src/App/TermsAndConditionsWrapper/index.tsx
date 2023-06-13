1f6512e2487b3c23bf10601bf46610cd975b37a4
Oscar Roche <oscarroche@oscarwroche.local>
2023-06-13 02:04:19 +0200

    :passport_control: disable profile menu items when t&c not accepted

--- frontend/src/App/TermsAndConditionsWrapper/index.tsx
+++ frontend/src/App/TermsAndConditionsWrapper/index.tsx
@@ -4,7 +4,7 @@ import { useAuth } from "src/hooks/useAuth";
 import { generatePath, Navigate, useLocation } from "react-router-dom";
 import { RoutePaths } from "src/App";
 
-const TERMS_AND_CONDITIONS_LAST_REDACTION_DATE = "2023-06-01";
+export const TERMS_AND_CONDITIONS_LAST_REDACTION_DATE = "2023-06-01";
 
 export default function TermsAndConditionsAcceptanceDateProvider({ children }: PropsWithChildren) {
   const { user, impersonating } = useAuth();
