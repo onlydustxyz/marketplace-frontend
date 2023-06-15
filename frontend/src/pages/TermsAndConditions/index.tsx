cb7b52aba170662f24e6d8a1561900ecfed93112
Oscar Roche <oscarroche@oscarwroche.local>
2023-06-15 10:35:38 +0200

    :rotating_light: change click to change for console error

--- frontend/src/pages/TermsAndConditions/index.tsx
+++ frontend/src/pages/TermsAndConditions/index.tsx
@@ -166,7 +166,7 @@ const TermsAndConditionsMainCard = ({ handleAcceptTermsAndConditions }: TermsAnd
               <input
                 type="checkbox"
                 checked={checked}
-                onClick={() => setChecked(!checked)}
+                onChange={() => setChecked(!checked)}
                 className="w-5 h-5 bg-white/8 hover:bg-white/2 checked:bg-spacePurple-500 rounded border border-greyscale-50/20 checked:border-spacePurple-700 focus:ring-0 focus:ring-offset-0 cursor-pointer enabled:ring-0 checked:focus:bg-spacePurple-500 checked:hover:bg-spacePurple-500/90  checked:hover:border-spacePurple-700"
               />
               <p>
