75746e643a982341d274aa42413a7c7e4690dead
Oscar Roche <oscar.w.roche@gmail.com>
2023-06-15 15:26:15 +0200

    :bug: Do not display menu items without T&C accepted (#1050)
    Co-authored-by: Oscar Roche <oscarroche@oscarwroche.local>

--- frontend/src/App/Layout/Header/index.tsx
+++ frontend/src/App/Layout/Header/index.tsx
@@ -4,13 +4,14 @@ import { RoutePaths } from "src/App";
 import { useAuth } from "src/hooks/useAuth";
 import { useIntl } from "src/hooks/useIntl";
 import { SessionMethod, useSessionDispatch } from "src/hooks/useSession";
-import { useGetPaymentRequestIdsQuery } from "src/__generated/graphql";
+import { useGetPaymentRequestIdsQuery, useGetTermsAndConditionsAcceptancesQuery } from "src/__generated/graphql";
 import View from "./View";
 import { useImpersonationClaims } from "src/hooks/useImpersonationClaims";
+import { TERMS_AND_CONDITIONS_LAST_REDACTION_DATE } from "src/App/TermsAndConditionsWrapper";
 
 export default function Header() {
   const location = useLocation();
-  const { isLoggedIn, githubUserId } = useAuth();
+  const { user, isLoggedIn, githubUserId } = useAuth();
   const { T } = useIntl();
   const dispatchSession = useSessionDispatch();
   const { impersonationSet } = useImpersonationClaims();
@@ -21,10 +22,24 @@ export default function Header() {
     skip: !githubUserId,
   });
 
+  const termsAndConditionsAcceptanceQuery = useGetTermsAndConditionsAcceptancesQuery({
+    variables: { userId: user?.id },
+    skip: !user?.id,
+  });
+
+  const hideMenuItems = !!(
+    user?.id &&
+    !termsAndConditionsAcceptanceQuery.loading &&
+    !impersonating &&
+    (!termsAndConditionsAcceptanceQuery?.data?.termsAndConditionsAcceptancesByPk?.acceptanceDate ||
+      new Date(termsAndConditionsAcceptanceQuery?.data?.termsAndConditionsAcceptancesByPk?.acceptanceDate) <
+        new Date(TERMS_AND_CONDITIONS_LAST_REDACTION_DATE))
+  );
+
   const hasPayments = paymentRequestIdsQueryData?.githubUsersByPk?.paymentRequests.length || 0 > 0;
 
-  const myContributionsMenuItem = hasPayments ? T("navbar.payments") : undefined;
-  const projectsMenuItem = myContributionsMenuItem ? T("navbar.projects") : undefined;
+  const myContributionsMenuItem = hasPayments && !hideMenuItems ? T("navbar.payments") : undefined;
+  const projectsMenuItem = myContributionsMenuItem && !hideMenuItems ? T("navbar.projects") : undefined;
 
   return (
     <View
