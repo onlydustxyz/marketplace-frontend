6edbc9048b57ed641b76337eaa5cb67834791658
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-06-20 18:25:03 +0200

    💚 fix dump restore errors

--- playwright/commands/db/db_utils.ts
+++ playwright/commands/db/db_utils.ts
@@ -5,9 +5,12 @@ export const DUMP_PATH = "playwright/marketplace_db_dump";
 
 export const dumpDB = () => {
   const DATABASE_URL = getEnv("DATABASE_URL");
-  execSync(`pg_dump --data-only --exclude-schema=hdb_catalog ${DATABASE_URL} > "${DUMP_PATH}"`, {
-    stdio: "pipe",
-  });
+  execSync(
+    `pg_dump --disable-triggers --data-only --exclude-table=auth.migrations --exclude-table=auth.providers --exclude-table=auth.roles --exclude-schema=hdb_catalog ${DATABASE_URL} > "${DUMP_PATH}"`,
+    {
+      stdio: "pipe",
+    }
+  );
 };
 
 export const restoreDB = () => {
