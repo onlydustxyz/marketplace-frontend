a147d27bda3a1c3e99a02f3441887b269c7cb83e
Oscar Roche <oscarroche@oscarwroche.local>
2023-06-21 12:57:50 +0200

    :lipstick: fix checkbox css

--- frontend/src/pages/TermsAndConditions/Checkbox.tsx
+++ frontend/src/pages/TermsAndConditions/Checkbox.tsx
@@ -5,11 +5,14 @@ interface TermsAndConditionsCheckBoxProps {
 
 export default function TermsAndConditionsCheckBox({ checked, setChecked }: TermsAndConditionsCheckBoxProps) {
   return (
-    <input
-      type="checkbox"
-      checked={checked}
-      onChange={() => setChecked(!checked)}
-      className="w-5 h-5 bg-white/8 hover:bg-white/2 checked:bg-spacePurple-500 rounded border border-greyscale-50/20 checked:border-spacePurple-700 focus:ring-0 focus:ring-offset-0 cursor-pointer enabled:ring-0 checked:focus:bg-spacePurple-500 checked:hover:bg-spacePurple-500/90  checked:hover:border-spacePurple-700"
-    />
+    <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+        className="w-5 h-5 bg-white/8 hover:bg-white/2 rounded border border-greyscale-50/20 cursor-pointer accent-spacePurple-700 checked:focus:spacePurple-600"
+      />
+    </>
   );
 }
