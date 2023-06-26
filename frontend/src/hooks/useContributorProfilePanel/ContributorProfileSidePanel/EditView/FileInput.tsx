8b0061ae1b0a964d9b8a59928d44ac3cf2bf4afe
Anthony Buisset <abuisset@gmail.com>
2023-06-26 15:04:44 +0200

    accept only image files

--- frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/EditView/FileInput.tsx
+++ frontend/src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/EditView/FileInput.tsx
@@ -12,7 +12,7 @@ const FileInput = React.forwardRef<HTMLInputElement, Props>(function FileInput({
     }
   };
 
-  return <input type="file" ref={ref} style={{ display: "none" }} onChange={onChange} />;
+  return <input type="file" ref={ref} style={{ display: "none" }} onChange={onChange} accept="image/*" />;
 });
 
 export default FileInput;
