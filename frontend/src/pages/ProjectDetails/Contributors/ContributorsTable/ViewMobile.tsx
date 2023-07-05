75300f97a95996fb9dc98878571124e698998efb
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-07-05 22:28:50 +0200

    Add contributors list icons

--- frontend/src/pages/ProjectDetails/Contributors/ContributorsTable/ViewMobile.tsx
+++ frontend/src/pages/ProjectDetails/Contributors/ContributorsTable/ViewMobile.tsx
@@ -2,6 +2,8 @@ import Card from "src/components/Card";
 import Contributor from "src/components/Contributor";
 import { formatMoneyAmount } from "src/utils/money";
 import { Contributor as ContributorType } from "./View";
+import MoneyDollarCircleLine from "src/icons/MoneyDollarCircleLine";
+import CheckLine from "src/icons/CheckLine";
 
 export function ViewMobile({ contributors }: { contributors: ContributorType[] }) {
   return (
@@ -9,9 +11,15 @@ export function ViewMobile({ contributors }: { contributors: ContributorType[] }
       {contributors.map(contributor => (
         <div className="flex items-center justify-between gap-4 p-3" key={contributor.login}>
           <Contributor contributor={contributor} clickable />
-          <div className="flex gap-3">
-            <div>{`${contributor?.totalEarned ? formatMoneyAmount({ amount: contributor.totalEarned }) : "-"}`}</div>
-            <div>{contributor.paidContributionsCount || "-"}</div>
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-1 text-sm">
+              <CheckLine className="text-base font-medium text-spaceBlue-200" />{" "}
+              {contributor.paidContributionsCount || "-"}
+            </div>
+            <div className="flex items-center gap-1 text-sm">
+              <MoneyDollarCircleLine className="text-base font-medium text-spaceBlue-200" />
+              {`${contributor?.totalEarned ? formatMoneyAmount({ amount: contributor.totalEarned }) : "-"}`}
+            </div>
           </div>
         </div>
       ))}
