a3db4ba120579d7af8feb0179705ab99491241fc
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-07-10 10:56:33 +0200

    💄 improve search bar design

--- frontend/src/pages/Projects/SearchBar/index.tsx
+++ frontend/src/pages/Projects/SearchBar/index.tsx
@@ -23,40 +23,50 @@ export default function SearchBar({ searchQuery, setSearchQuery }: Props) {
   return (
     <div
       className={classNames("overflow-hidden rounded-full p-0.5", {
-        "bg-multi-color-gradient": !inputFocus,
         "bg-spacePurple-500 outline outline-4 outline-spacePurple-500/25": inputFocus,
       })}
     >
       <div
-        className={classNames("relative flex h-16 flex-row items-center gap-5 rounded-full px-6", {
-          "bg-spaceBlue-900": !inputFocus,
-          "bg-spacePurple-900": inputFocus,
+        className={classNames("relative z-10 flex items-center justify-center rounded-full", {
+          "before:absolute before:-z-10 before:h-screen before:w-screen before:scale-x-[20] before:bg-multi-color-gradient":
+            !inputFocus,
         })}
       >
-        <SearchLine
-          className={classNames("text-3xl", { "text-spaceBlue-200": !inputFocus }, { "text-greyscale-50": inputFocus })}
-        />
-        <input
-          placeholder={T("searchBar.placeholder")}
-          className="h-8 w-full bg-transparent font-walsheim text-lg font-medium text-greyscale-50 outline-none placeholder:text-spaceBlue-200"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-          onFocus={() => setInputFocus(true)}
-          onBlur={() => setInputFocus(false)}
-        />
-        <button>
-          <CloseLine
+        <div
+          className={classNames("flex h-16 w-full flex-row items-center gap-5 rounded-full px-6", {
+            "bg-spaceBlue-900": !inputFocus,
+            "bg-spacePurple-900": inputFocus,
+          })}
+        >
+          <SearchLine
             className={classNames(
               "text-3xl",
-              { hidden: !inputFocus && !searchQuery },
-              { "text-greyscale-50": inputFocus || searchQuery }
+              { "text-spaceBlue-200": !inputFocus },
+              { "text-greyscale-50": inputFocus }
             )}
-            onClick={e => {
-              e.preventDefault();
-              setSearch("");
-            }}
           />
-        </button>
+          <input
+            placeholder={T("searchBar.placeholder")}
+            className="h-8 w-full bg-transparent font-walsheim text-lg font-medium text-greyscale-50 outline-none placeholder:text-spaceBlue-200"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            onFocus={() => setInputFocus(true)}
+            onBlur={() => setInputFocus(false)}
+          />
+          <button>
+            <CloseLine
+              className={classNames(
+                "text-3xl",
+                { hidden: !inputFocus && !searchQuery },
+                { "text-greyscale-50": inputFocus || searchQuery }
+              )}
+              onClick={e => {
+                e.preventDefault();
+                setSearch("");
+              }}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
