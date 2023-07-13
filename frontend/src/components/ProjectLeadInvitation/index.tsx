62b1b30120406f1dc3258619c87246199f2bf8fc
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-07-13 18:03:03 +0200

    [E-591] Accept lead invite banner doesn't disappear upon acceptance (#1118)

--- frontend/src/components/ProjectLeadInvitation/index.tsx
+++ frontend/src/components/ProjectLeadInvitation/index.tsx
@@ -1,10 +1,13 @@
 import { useAcceptProjectLeaderInvitationMutation, useGetProjectLeadInvitationsQuery } from "src/__generated/graphql";
 import headerElementBackground from "src/assets/img/header-element-background.png";
 import ImageCard, { BackgroundSize, Height } from "src/components/ImageCard";
+import { viewportConfig } from "src/config";
 import { useAuth } from "src/hooks/useAuth";
 import CheckLine from "src/icons/CheckLine";
 import { contextWithCacheHeaders } from "src/utils/headers";
 import { useT } from "talkr";
+import { useMediaQuery } from "usehooks-ts";
+import Button from "src/components/Button";
 
 interface ProjectLeadInvitationProps {
   projectId: string;
@@ -13,6 +16,7 @@ interface ProjectLeadInvitationProps {
 export default function ProjectLeadInvitation({ projectId }: ProjectLeadInvitationProps) {
   const { githubUserId } = useAuth();
   const { T } = useT();
+  const isMd = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.md}px)`);
 
   const { data } = useGetProjectLeadInvitationsQuery({
     variables: { projectId },
@@ -32,15 +36,11 @@ export default function ProjectLeadInvitation({ projectId }: ProjectLeadInvitati
   return invitationId ? (
     <ImageCard backgroundImageUrl={headerElementBackground} backgroundSize={BackgroundSize.Cover} height={Height.Fit}>
       <div className="flex flex-row items-center justify-between px-6 py-5 font-medium">
-        <div className="text-lg">{T("projectLeadInvitation.prompt", { projectName })}</div>
-        <div
-          onClick={() => acceptInvitation()}
-          className="flex w-fit flex-row items-center justify-between gap-2 rounded-xl bg-neutral-100 px-4 py-3 text-black shadow-inner shadow-neutral-400 hover:cursor-pointer"
-          data-testid="accept-invite-button"
-        >
+        <div className="text-base md:text-lg">{T("projectLeadInvitation.prompt", { projectName })}</div>
+        <Button onClick={() => acceptInvitation()} data-testid="accept-invite-button">
           <CheckLine className="text-xl font-normal text-black" />
-          <div>{T("projectLeadInvitation.accept")}</div>
-        </div>
+          <div>{T(isMd ? "projectLeadInvitation.accept" : "projectLeadInvitation.acceptShort")}</div>
+        </Button>
       </div>
     </ImageCard>
   ) : (
