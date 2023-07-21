23d8960bdc985ac2d6d57f994f31595c93ae1c30
Anthony Buisset <abuisset@gmail.com>
2023-07-21 14:48:10 +0200

    update hasura action to match existing mutation

--- backend/common/domain/src/project/visibility.rs
+++ backend/common/domain/src/project/visibility.rs
@@ -2,6 +2,7 @@ use juniper::GraphQLEnum;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq, Eq, GraphQLEnum)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Visibility {
 	Private,
 	#[default]
