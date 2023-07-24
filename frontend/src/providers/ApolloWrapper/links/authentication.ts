4a5f5f2a62e0af44a0614b6e8bf3ed6b88ad978c
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-07-24 17:19:26 +0200

    ♻️ update impersonation to send X-Impersonation-Secret and X-Impersonation-Claims

--- frontend/src/providers/ApolloWrapper/links/authentication.ts
+++ frontend/src/providers/ApolloWrapper/links/authentication.ts
@@ -8,14 +8,26 @@ export default function useAuthenticationLink() {
 
   return setContext((_, { headers }) => {
     const authorizationHeaders = tokenSet?.accessToken ? { Authorization: `Bearer ${tokenSet?.accessToken}` } : {};
-    const impersonationHeaders = impersonationSet
+    const impersonationClaims = impersonationSet
       ? {
-          "X-Hasura-Admin-Secret": impersonationSet.password,
-          "X-Hasura-User-Id": impersonationSet.userId,
-          "X-Hasura-projectsLeaded": `{${customClaims.projectsLeaded?.map(id => `"${id}"`).join(",")}}`,
-          "X-Hasura-githubUserId": customClaims.githubUserId,
+          "x-hasura-user-id": impersonationSet.userId,
+          "x-hasura-projectsLeaded": `{${customClaims.projectsLeaded?.map(id => `"${id}"`).join(",")}}`,
+          "x-hasura-githubUserId": `${customClaims.githubUserId}`,
         }
-      : {};
+      : undefined;
+    const impersonationHeaders =
+      impersonationSet && impersonationClaims
+        ? {
+            // Impersonation for Hasura
+            "X-Hasura-Admin-Secret": impersonationSet.password,
+            "X-Hasura-User-Id": impersonationClaims["x-hasura-user-id"],
+            "X-Hasura-projectsLeaded": impersonationClaims["x-hasura-projectsLeaded"],
+            "X-Hasura-githubUserId": impersonationClaims["x-hasura-githubUserId"],
+            // Impersonation for OnlyDust API
+            "X-Impersonation-Secret": impersonationSet.password,
+            "X-Impersonation-Claims": JSON.stringify(impersonationClaims),
+          }
+        : {};
     return {
       headers: {
         ...headers,
