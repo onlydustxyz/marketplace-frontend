f513a5f3689675cb7e4ec55ac1febf61594d66f2
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-07-31 15:36:31 +0200

    Nicer scrollbars on public profile

--- frontend/src/pages/PublicProfile/Profile.tsx
+++ frontend/src/pages/PublicProfile/Profile.tsx
@@ -17,7 +17,7 @@ export default function Profile({
   return (
     <div className="flex h-full min-h-0 w-full bg-greyscale-900 px-4 md:rounded-3xl">
       <div className="flex min-h-0 w-full flex-col gap-4 lg:flex-row lg:divide-x lg:divide-greyscale-50/8">
-        <div className="flex flex-col py-4 lg:basis-1/2 lg:overflow-y-auto">
+        <div className="flex flex-col py-4 scrollbar-thin scrollbar-thumb-whiteFakeOpacity-12 scrollbar-thumb-rounded scrollbar-w-1.5 lg:basis-1/2 lg:overflow-y-auto">
           <Header
             profile={{ ...profile, cover: translateProfileCover(profile.cover) ?? ProfileCover.Blue }}
             rounded={true}
@@ -34,7 +34,7 @@ export default function Profile({
             {languages.length > 0 && <TechnologiesSection languages={languages} />}
           </div>
         </div>
-        <div className="flex flex-col gap-12 px-px py-4 lg:basis-1/2 lg:overflow-y-auto lg:pl-8 lg:pr-4">
+        <div className="flex flex-col gap-12 px-px py-4 scrollbar-thin scrollbar-thumb-whiteFakeOpacity-12 scrollbar-thumb-rounded scrollbar-w-1.5 lg:basis-1/2 lg:overflow-y-auto lg:pl-8 lg:pr-4">
           <StatsSection
             profile={profile}
             contributionCounts={contributionCounts}
