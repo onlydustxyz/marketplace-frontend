6cbfdd97905d6bda9964254618cdee895ff28c1c
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-08-02 14:45:22 +0200

    Keep background fixed on scroll

--- frontend/src/components/Background/index.tsx
+++ frontend/src/components/Background/index.tsx
@@ -21,7 +21,7 @@ const Background = forwardRef(function Background(
       className="h-full w-full overflow-y-auto px-2 pb-2 scrollbar-thin scrollbar-thumb-white/12 scrollbar-thumb-rounded scrollbar-w-1.5 sm:px-6 xl:mb-6 xl:pb-0"
     >
       <div
-        className={classNames("min-h-full bg-space xl:bg-fixed xl:bg-clip-content xl:bg-no-repeat", roundedBorders, {
+        className={classNames("min-h-full bg-fixed bg-clip-content bg-space bg-no-repeat", roundedBorders, {
           "flex flex-col justify-center": centeredContent,
         })}
       >
