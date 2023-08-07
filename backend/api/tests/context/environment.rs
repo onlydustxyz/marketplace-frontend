65360f4048392dd7183d9bda50988ba621797d5b
Anthony Buisset <abuisset@gmail.com>
2023-08-07 11:19:08 +0200

    encode jwt in test to have dynamic expiry date

--- backend/api/tests/context/environment.rs
+++ backend/api/tests/context/environment.rs
@@ -11,7 +11,7 @@ impl Context {
 		Self {
 			_guards: vec![set_env(
 				OsString::from("HASURA_GRAPHQL_JWT_SECRET"),
-				r#"{"type":"HS256","key":"some-fake-secret-for-unittest","issuer":"hasura-auth-unit-tests"}"#,
+				r#"{"type":"HS256","key":"secret","issuer":"hasura-auth-unit-tests"}"#,
 			)],
 		}
 	}
