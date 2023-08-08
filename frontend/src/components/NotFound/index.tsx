73cf9c596a691bd07a2fc73879c5de1474ea70bf
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2023-08-08 16:45:17 +0200

    New NotFound design

--- frontend/src/components/NotFound/index.tsx
+++ frontend/src/components/NotFound/index.tsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 import { RoutePaths } from "src/App";
-import foxImg from "src/assets/img/fox.png";
-import Background, { BackgroundRoundedBorders } from "src/components/Background";
 import Button, { ButtonSize } from "src/components/Button";
 import { useIntl } from "src/hooks/useIntl";
+import logo from "src/assets/img/onlydust-logo-white.svg";
+import ArrowLeftSLine from "src/icons/ArrowLeftSLine";
 
 export function NotFound() {
   const { T } = useIntl();
 
   return (
-    <Background roundedBorders={BackgroundRoundedBorders.Full} centeredContent>
-      <img src={foxImg} className="max-w-xs" />
-      <div className="mb-6 text-center font-belwe text-2xl xl:text-3xl">{T("notFound.text")}</div>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-12 bg-[url('src/assets/img/not-found.png')] bg-cover bg-center p-5">
+      <div className="flex max-w-md flex-col items-center gap-6 text-center">
+        <img src={logo} alt="OnlyDust logo" className="w-12" />
+        <div className="font-belwe text-2xl sm:text-3xl">
+          <div>{T("notFound.title1")}</div>
+          <div>{T("notFound.title2")}</div>
+        </div>
+        <div className="font-walsheim text-base text-spaceBlue-200 sm:text-lg">{T("notFound.text")}</div>
+      </div>
       <Link to={RoutePaths.Home}>
-        <Button size={ButtonSize.Md}>{T("notFound.button")}</Button>
+        <Button size={ButtonSize.Lg}>
+          <ArrowLeftSLine className="text-xl" /> {T("notFound.button")}
+        </Button>
       </Link>
-    </Background>
+    </div>
   );
 }
