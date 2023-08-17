c156b9df1ed2e5e49310a6c58a9e0e05d9384d77
PierreOucif <pierre.oucif@gadz.org>
2023-08-17 17:48:02 +0200

    Updating migration

--- migrations/2023-08-17-143042_2023-08-16_update-contributions-for-api-views/up.sql
+++ migrations/2023-08-17-143042_2023-08-16_update-contributions-for-api-views/up.sql
@@ -1,4 +1,12 @@
 CREATE TYPE contribution_status AS enum('in_progress', 'complete', 'canceled');
 
 ALTER TABLE contributions
-    ADD COLUMN status contribution_status NOT NULL DEFAULT ('in_progress');
+    ADD COLUMN status contribution_status;
+
+UPDATE contributions
+SET status = 'in_progress'
+WHERE status IS NULL;
+
+ALTER TABLE contributions
+    ALTER COLUMN status SET NOT NULL;
+
