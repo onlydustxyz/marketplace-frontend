18d930eefdd8f3c4d20dd9821c080ad0042c38f1
PierreOucif <pierre.oucif@gadz.org>
2023-08-22 16:26:04 +0200

    Optimize project lead avatar size

--- frontend/src/components/ProjectCard/index.tsx
+++ frontend/src/components/ProjectCard/index.tsx
@@ -43,7 +43,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
     shortDescription,
   } = project;
 
-  const projectUrl = logoUrl ? config.CLOUDFLARE_PROJECT_LOGO_RESIZE_PREFIX + logoUrl : logoUrl;
+  const projectUrl = logoUrl ? config.CLOUDFLARE_RESIZE_W_100_PREFIX + logoUrl : logoUrl;
 
   const { T } = useIntl();
   const isXl = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.xl}px)`);
