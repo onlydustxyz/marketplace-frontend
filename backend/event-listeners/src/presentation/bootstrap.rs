413839b1865df855e2133fe70e44710b31a5c4dd
Anthony Buisset <abuisset@gmail.com>
2023-08-30 18:53:59 +0200

    new index repo route

--- backend/event-listeners/src/presentation/bootstrap.rs
+++ backend/event-listeners/src/presentation/bootstrap.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use anyhow::Result;
+use infrastructure::{database, github};
 use olog::info;
 use rocket::{Build, Rocket};
 
@@ -7,6 +10,12 @@ use crate::{presentation::http, Config};
 pub async fn bootstrap(config: Config) -> Result<Rocket<Build>> {
 	info!("Bootstrapping backend event-listeners api");
 
-	let rocket_build = http::serve(config.http);
+	let database = Arc::new(database::Client::new(database::init_pool(
+		config.database.clone(),
+	)?));
+
+	let github: Arc<github::Client> = github::RoundRobinClient::new(config.github.clone())?.into();
+
+	let rocket_build = http::serve(config.http, database, github);
 	Ok(rocket_build)
 }
--- backend/event-listeners/src/presentation/http/mod.rs
+++ backend/event-listeners/src/presentation/http/mod.rs
@@ -1,9 +1,21 @@
+pub mod routes;
+
+use std::sync::Arc;
+
 pub use http::Config;
+use infrastructure::{database, github};
 use presentation::http;
 use rocket::{Build, Rocket};
 
-pub fn serve(config: Config) -> Rocket<Build> {
+pub fn serve(
+	config: Config,
+	database: Arc<database::Client>,
+	github: Arc<github::Client>,
+) -> Rocket<Build> {
 	rocket::custom(http::config::rocket("backend/event-listeners/Rocket.toml"))
 		.manage(config)
+		.manage(database)
+		.manage(github)
 		.attach(http::guards::Cors)
+		.mount("/indexer", routes![routes::repo::index])
 }
