f25908e8264d88008b7f6c00fbf1db6276ddd58b
Olivier Fuxet <ofux@users.noreply.github.com>
2023-09-01 17:38:03 +0200

    E 672 move old integration tests as real integration tests (#1210)

--- backend/common/infrastructure/src/amqp/mod.rs
+++ backend/common/infrastructure/src/amqp/mod.rs
@@ -14,19 +14,3 @@ mod command;
 pub use command::{
 	CommandMessage, CommandPublisher, CommandPublisherDecorator, CommandSubscriberDecorator,
 };
-
-#[cfg(test)]
-mod test {
-	use rstest::fixture;
-
-	use super::Config;
-
-	#[fixture]
-	pub fn config() -> Config {
-		Config {
-			url: "amqp://127.0.0.1:5672/%2f".to_string(),
-			connection_retry_interval_ms: 200,
-			connection_retry_count: 0,
-		}
-	}
-}
