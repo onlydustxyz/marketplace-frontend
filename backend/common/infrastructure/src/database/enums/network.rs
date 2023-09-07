4887b0e7d88a3513aa0c1747dc1384dac3b5c519
Anthony Buisset <abuisset@gmail.com>
2023-09-07 21:31:02 +0200

    normalize wallets and bank accounts

---
+++ backend/common/infrastructure/src/database/enums/network.rs
@@ -0,0 +1,8 @@
+use diesel_derive_enum::DbEnum;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Hash, Deserialize, DbEnum)]
+#[ExistingTypePath = "crate::database::schema::sql_types::Network"]
+pub enum Network {
+	Ethereum,
+}
