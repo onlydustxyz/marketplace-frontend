6c4093871440397bc4a77a5c32ce088835ebfeaf
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-09-08 18:29:20 +0200

    [E-705] create `payout_info` view and untrack table from hasura (#1228)

---
+++ backend/migrations/2023-09-08-160905_remove-unused-functions/up.sql
@@ -0,0 +1,13 @@
+DROP FUNCTION IF EXISTS is_not_null;
+
+
+DROP FUNCTION IF EXISTS is_person_identity_complete;
+
+
+DROP FUNCTION IF EXISTS is_identity_complete;
+
+
+DROP FUNCTION IF EXISTS is_location_complete;
+
+
+DROP FUNCTION IF EXISTS is_payout_settings_complete;
