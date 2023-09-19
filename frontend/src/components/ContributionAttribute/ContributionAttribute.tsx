b9ff01e2af936d615381f8af11a47a941490f3dd
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-09-19 10:47:27 +0200

    feat: contribution attribute component

---
+++ frontend/src/components/ContributionAttribute/ContributionAttribute.tsx
@@ -0,0 +1,3 @@
+export function ContributionAttribute({ children }: { children: React.ReactNode }) {
+  return <div className="inline-block rounded-full border border-greyscale-50/8 bg-white/2 px-2 py-1">{children}</div>;
+}
