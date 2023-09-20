eed24b5d95cb26ae85492681fa899178a2264a8f
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-09-20 11:55:20 +0200

    feat: desktop contributions tabs

--- frontend/src/pages/Contributions/index.tsx
+++ frontend/src/pages/Contributions/index.tsx
@@ -1,39 +1,89 @@
-import Background, { BackgroundRoundedBorders } from "src/components/Background";
-import SEO from "src/components/SEO";
+import { useState } from "react";
 
 import { Tabs } from "src/components/Tabs/Tabs";
+import { useIntl } from "src/hooks/useIntl";
+import Background, { BackgroundRoundedBorders } from "src/components/Background";
+import CancelCircleLine from "src/assets/icons/CancelCircleLine";
+import IssueDraft from "src/assets/icons/IssueDraft";
+import IssueMerged from "src/assets/icons/IssueMerged";
+import ProgressCircle from "src/assets/icons/ProgressCircle";
+import SEO from "src/components/SEO";
+
+const TAB_ALL = "allContributions";
+const TAB_APPLIED = "applied";
+const TAB_IN_PROGRESS = "inProgress";
+const TAB_COMPLETED = "completed";
+const TAB_CANCELED = "canceled";
+
+function TabContents({ children }: { children: React.ReactNode }) {
+  return <div className="flex items-center gap-1.5">{children}</div>;
+}
 
 export default function Contributions() {
+  const { T } = useIntl();
+
+  const [activeTab, setActiveTab] = useState(TAB_ALL);
+
   const tabs = [
     {
-      active: true,
-      onClick: () => {},
+      active: activeTab === TAB_ALL,
+      onClick: () => {
+        setActiveTab(TAB_ALL);
+      },
       testId: "contributions-all-contributions-tab",
-      children: "All contributions",
+      children: T("contributions.nav.allContributions"),
     },
     {
-      active: false,
-      onClick: () => {},
+      active: activeTab === TAB_APPLIED,
+      onClick: () => {
+        setActiveTab(TAB_APPLIED);
+      },
       testId: "contributions-applied-tab",
-      children: "Applied",
+      children: (
+        <TabContents>
+          <IssueDraft />
+          {T("contributions.nav.applied")}
+        </TabContents>
+      ),
     },
     {
-      active: false,
-      onClick: () => {},
+      active: activeTab === TAB_IN_PROGRESS,
+      onClick: () => {
+        setActiveTab(TAB_IN_PROGRESS);
+      },
       testId: "contributions-in-progress-tab",
-      children: "In progress",
+      children: (
+        <TabContents>
+          <ProgressCircle />
+          {T("contributions.nav.inProgress")}
+        </TabContents>
+      ),
     },
     {
-      active: false,
-      onClick: () => {},
+      active: activeTab === TAB_COMPLETED,
+      onClick: () => {
+        setActiveTab(TAB_COMPLETED);
+      },
       testId: "contributions-completed-tab",
-      children: "Completed",
+      children: (
+        <TabContents>
+          <IssueMerged />
+          {T("contributions.nav.completed")}
+        </TabContents>
+      ),
     },
     {
-      active: false,
-      onClick: () => {},
+      active: activeTab === TAB_CANCELED,
+      onClick: () => {
+        setActiveTab(TAB_CANCELED);
+      },
       testId: "contributions-canceled-tab",
-      children: "Canceled",
+      children: (
+        <TabContents>
+          <CancelCircleLine />
+          {T("contributions.nav.canceled")}
+        </TabContents>
+      ),
     },
   ];
 
@@ -44,8 +94,8 @@ export default function Contributions() {
         <Background roundedBorders={BackgroundRoundedBorders.Full}>
           <div className="absolute inset-0 bg-gradient-to-b from-[#000113]/[0] to-[#0E0D2E]" />
           <div className="relative z-10">
-            <header className="bg-white/8 px-8 pt-8 shadow-2xl backdrop-blur-3xl">
-              <Tabs tabs={tabs} />
+            <header className="border-b border-greyscale-50/20 bg-white/8 px-8 pt-8 shadow-2xl backdrop-blur-3xl">
+              <Tabs tabs={tabs} variant="blue" />
             </header>
             Contributions
           </div>
