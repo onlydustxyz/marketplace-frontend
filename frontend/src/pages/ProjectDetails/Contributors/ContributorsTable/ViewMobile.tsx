b948b5569f3d9706e689cdb4b486582da204f07b
Mickaël Chimansky <mickael@onlydust.xyz>
2023-09-20 11:22:36 +0200

    fix: e2e

--- frontend/src/pages/ProjectDetails/Contributors/ContributorsTable/ViewMobile.tsx
+++ frontend/src/pages/ProjectDetails/Contributors/ContributorsTable/ViewMobile.tsx
@@ -16,27 +16,38 @@ export function ViewMobile({ contributors }: { contributors: ContributorType[] }
             contributor.toRewardCount - contributor.unpaidCodeReviewCount - contributor.unpaidIssueCount;
           const contributionCount =
             contributor.contributionCount - contributor.unpaidCodeReviewCount - contributor.unpaidIssueCount;
+
+          const hasNothing = toRewardCount === 0 && contributionCount === 0 && contributor.rewardCount === 0;
           return (
             <div className="flex items-center justify-between gap-1 p-3" key={contributor.login}>
               <Contributor contributor={contributor} clickable />
-              {toRewardCount > 0 ? (
+              {!hasNothing ? (
                 <div className="flex items-center gap-3">
-                  <div className="flex items-center gap-1 text-sm">
-                    <StackLine className="text-base font-medium text-spaceBlue-200" />
-                    {contributionCount || "-"}
-                  </div>
-                  <div className="flex items-center gap-1 text-sm">
-                    <Medal2Fill className="text-base font-medium text-spaceBlue-200" />
-                    {contributor.rewardCount || "-"}
-                  </div>
-                  <div className="flex items-center gap-1 text-sm">
-                    <MoneyDollarCircleLine className="text-base font-medium text-spaceBlue-200" />
-                    {`${contributor?.totalEarned ? formatMoneyAmount({ amount: contributor.totalEarned }) : "-"}`}
-                  </div>
-                  <div className="flex items-center gap-1 rounded-full bg-spacePurple-900 px-1.5 py-0.5 text-sm font-medium text-spacePurple-400">
-                    <StackLine className="text-base" />
-                    {toRewardCount}
-                  </div>
+                  {contributionCount ? (
+                    <div className="flex items-center gap-1 text-sm">
+                      <StackLine className="text-base font-medium text-spaceBlue-200" />
+                      {contributionCount}
+                    </div>
+                  ) : null}
+                  {contributor.rewardCount ? (
+                    <>
+                      <div className="flex items-center gap-1 text-sm">
+                        <Medal2Fill className="text-base font-medium text-spaceBlue-200" />
+                        {contributor.rewardCount}
+                      </div>
+                      <div className="flex items-center gap-1 text-sm">
+                        <MoneyDollarCircleLine className="text-base font-medium text-spaceBlue-200" />
+                        {`${contributor?.totalEarned ? formatMoneyAmount({ amount: contributor.totalEarned }) : "-"}`}
+                      </div>
+                    </>
+                  ) : null}
+
+                  {toRewardCount ? (
+                    <div className="flex items-center gap-1 rounded-full bg-spacePurple-900 px-1.5 py-0.5 text-sm font-medium text-spacePurple-400">
+                      <StackLine className="text-base" />
+                      {toRewardCount}
+                    </div>
+                  ) : null}
                 </div>
               ) : (
                 "-"
