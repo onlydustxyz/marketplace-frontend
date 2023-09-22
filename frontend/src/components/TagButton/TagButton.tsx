fb14526e51ea757b0bc22f3ab96bfc8d1cd2b0da
Mickaël Chimansky <mickael@onlydust.xyz>
2023-09-22 18:10:40 +0200

    fix: code review check icon size

--- frontend/src/components/TagButton/TagButton.tsx
+++ frontend/src/components/TagButton/TagButton.tsx
@@ -11,7 +11,7 @@ export default function TagButton({ children, onClick, ...rest }: ButtonHTMLAttr
   return (
     <Tag
       as="button"
-      className="text-spacePurple-500 transition-all delay-150 duration-200 ease-out hover:border-spacePurple-500 hover:px-[10px] hover:text-purple-500"
+      className="text-spacePurple-500 transition-all duration-200 ease-out hover:scale-[98%] hover:border-spacePurple-500 hover:text-purple-500"
       onClick={handleClick}
       {...rest}
     >
