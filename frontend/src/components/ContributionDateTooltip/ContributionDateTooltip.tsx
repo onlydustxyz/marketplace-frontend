a4ed0a7b9838993d445c0cb4ffb32eb77b571a1f
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-09-25 21:26:45 +0200

    fix: date tooltip texts

--- frontend/src/components/ContributionDateTooltip/ContributionDateTooltip.tsx
+++ frontend/src/components/ContributionDateTooltip/ContributionDateTooltip.tsx
@@ -23,15 +23,41 @@ export function ContributionDateTooltip({
   const formattedDate = new Intl.DateTimeFormat("en-US", { dateStyle: "short" }).format(date);
   const formattedTime = new Intl.DateTimeFormat("en-US", { timeStyle: "short" }).format(date);
 
+  const tokens = {
+    [ContributionIconType.PullRequest]: {
+      [ContributionIconStatus.Cancelled]: "",
+      [ContributionIconStatus.Closed]: "contributions.tooltip.dateClosed",
+      [ContributionIconStatus.Completed]: "",
+      [ContributionIconStatus.Draft]: "contributions.tooltip.dateOpened",
+      [ContributionIconStatus.Merged]: "contributions.tooltip.dateMerged",
+      [ContributionIconStatus.Open]: "contributions.tooltip.dateOpened",
+      [ContributionIconStatus.Pending]: "",
+    },
+    [ContributionIconType.Issue]: {
+      [ContributionIconStatus.Cancelled]: "contributions.tooltip.dateClosed",
+      [ContributionIconStatus.Closed]: "",
+      [ContributionIconStatus.Completed]: "contributions.tooltip.dateClosed",
+      [ContributionIconStatus.Draft]: "contributions.tooltip.dateAssigned",
+      [ContributionIconStatus.Merged]: "",
+      [ContributionIconStatus.Open]: "contributions.tooltip.dateAssigned",
+      [ContributionIconStatus.Pending]: "",
+    },
+    [ContributionIconType.CodeReview]: {
+      [ContributionIconStatus.Cancelled]: "",
+      [ContributionIconStatus.Closed]: "",
+      [ContributionIconStatus.Completed]: "contributions.tooltip.dateClosed",
+      [ContributionIconStatus.Draft]: "",
+      [ContributionIconStatus.Merged]: "",
+      [ContributionIconStatus.Open]: "",
+      [ContributionIconStatus.Pending]: "contributions.tooltip.dateOpened",
+    },
+  };
+
   return (
     <Tooltip anchorId={id} clickable position={TooltipPosition.Top} variant={BackgroundVariant.Blue}>
       <div className="flex items-center gap-2 px-1 py-2">
         <ContributionIcon type={type} status={status} />
-        <p className="text-sm">
-          {status === ContributionIconStatus.Open || status === ContributionIconStatus.Draft
-            ? T("contributions.tooltip.dateOpen", { date: formattedDate, time: formattedTime })
-            : T("contributions.tooltip.dateClosed", { date: formattedDate, time: formattedTime })}
-        </p>
+        <p className="text-sm">{T(tokens[type][status], { date: formattedDate, time: formattedTime })}</p>
       </div>
     </Tooltip>
   );
