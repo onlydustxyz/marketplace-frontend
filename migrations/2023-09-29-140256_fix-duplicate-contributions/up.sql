802d9b3f33bd68d986c3072c3f959702c7091bf7
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-09-29 16:13:31 +0200

    fix duplicate contributions (#13)

---
+++ migrations/2023-09-29-140256_fix-duplicate-contributions/up.sql
@@ -0,0 +1,24 @@
+DELETE FROM contributions
+WHERE
+    id = encode(sha256(row ('pull_request', details_id, user_id)::text::bytea), 'hex');
+
+
+INSERT INTO
+    contributions (id, repo_id, user_id, "type", details_id, status, created_at, closed_at) (
+        SELECT
+            encode(sha256(row ('PULL_REQUEST', id, author_id)::text::bytea), 'hex') as id,
+            repo_id,
+            author_id,
+            'pull_request'::contribution_type as "type",
+            id as details_id,
+            CASE
+                WHEN status = 'open'::github_pull_request_status THEN 'in_progress'::contribution_status
+                WHEN status = 'merged'::github_pull_request_status THEN 'complete'::contribution_status
+                WHEN status = 'closed'::github_pull_request_status THEN 'canceled'::contribution_status
+            END as status,
+            created_at,
+            closed_at
+        from
+            github_pull_requests
+    ) ON CONFLICT
+DO NOTHING;
