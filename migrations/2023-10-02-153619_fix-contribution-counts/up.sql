3d6e2549d7a7ff6ca8f4860511fadd5c1f16df5e
Olivier Fuxet <ofux@users.noreply.github.com>
2023-10-02 17:43:41 +0200

    Fix contribution_counts view (#19)

---
+++ migrations/2023-10-02-153619_fix-contribution-counts/up.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW
+    api.contribution_counts AS
+SELECT
+    c.github_user_id,
+    DATE_PART('isoyear'::TEXT, c.created_at) AS "year",
+    DATE_PART('week'::TEXT, c.created_at) AS "week",
+    COUNT(DISTINCT c.details_id) FILTER (
+        WHERE
+            c.type = 'issue'::contribution_type
+    ) AS issue_count,
+    COUNT(DISTINCT c.details_id) FILTER (
+        WHERE
+            c.type = 'code_review'::contribution_type
+    ) AS code_review_count,
+    COUNT(DISTINCT c.details_id) FILTER (
+        WHERE
+            c.type = 'pull_request'::contribution_type
+    ) AS pull_request_count
+FROM
+    api.completed_contributions c
+GROUP BY
+    c.github_user_id,
+    "year",
+    "week";
