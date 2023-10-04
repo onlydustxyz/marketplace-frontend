0b2938ac5385d28842e25591062a3ec5496c10bf
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-10-04 14:50:54 +0200

    style: spinning logo dimensions

--- src/components/Loader/SpinningLogo.tsx
+++ src/components/Loader/SpinningLogo.tsx
@@ -4,5 +4,5 @@ import { useIntl } from "src/hooks/useIntl";
 export function SpinningLogo() {
   const { T } = useIntl();
 
-  return <img src={atomLogo} alt={T("state.loading")} className="animate-spin-medium" />;
+  return <img src={atomLogo} alt={T("state.loading")} className="h-24 w-24 animate-spin-medium" />;
 }
