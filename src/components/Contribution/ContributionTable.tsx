a0a59f97dfd3ee1b1428d9a671d34af0f01f3328
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-10-18 16:33:59 +0200

    refactor: empty contributions table state

--- src/components/Contribution/ContributionTable.tsx
+++ src/components/Contribution/ContributionTable.tsx
@@ -140,14 +140,7 @@ export function ContributionTable({
     if (memoizedContributions?.length === 0) {
       return (
         <div className="py-6">
-          <Message>
-            {T("contributions.table.empty", {
-              time: Intl.DateTimeFormat("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-              }).format(new Date(data?.githubRepos[0].indexedAt)),
-            })}
-          </Message>
+          <Message>{T("contributions.table.empty")}</Message>
         </div>
       );
     }
@@ -204,16 +197,7 @@ export function ContributionTable({
     }
 
     if (memoizedContributions?.length === 0) {
-      return (
-        <TableText>
-          {T("contributions.table.empty", {
-            time: Intl.DateTimeFormat("en-US", {
-              hour: "numeric",
-              minute: "numeric",
-            }).format(new Date(data?.githubRepos[0].indexedAt)),
-          })}
-        </TableText>
-      );
+      return <TableText>{T("contributions.table.empty")}</TableText>;
     }
 
     return memoizedContributions?.map(contribution => {
