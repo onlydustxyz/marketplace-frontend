c53074c287455af616b9a5d90964379f1f7fc6c1
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-10-19 15:12:32 +0200

    Revert "refactor: removed sync time from empty state"
    This reverts commit 8813b4da89a592a8bbdecfec8371c088a7e93247.

--- .storybook/stories/EmptyState.stories.tsx
+++ .storybook/stories/EmptyState.stories.tsx
@@ -1,10 +1,12 @@
 import EmptyState from "src/pages/ProjectDetails/Rewards/RewardForm/WorkItemSidePanel/EmptyState/index";
 
+const indexedAtMock = "2023-10-06T09:00:00.00000";
+
 export default {
   title: "EmptyState",
   component: EmptyState,
 };
 
 export const Default = {
-  render: () => <EmptyState />,
+  render: () => <EmptyState indexedAt={indexedAtMock} />,
 };
--- src/pages/ProjectDetails/Rewards/RewardForm/WorkItemSidePanel/EmptyState/index.tsx
+++ src/pages/ProjectDetails/Rewards/RewardForm/WorkItemSidePanel/EmptyState/index.tsx
@@ -1,10 +1,16 @@
-import emptyStateLogo from "assets/img/empty-state.png";
 import { useIntl } from "src/hooks/useIntl";
+import emptyStateLogo from "assets/img/empty-state.png";
+import { getFormattedTimeUS } from "src/utils/date";
 
-export default function EmptyState() {
+type EmptyStateProps = {
+  indexedAt: string;
+};
+
+export default function EmptyState({ indexedAt }: EmptyStateProps) {
   const { T } = useIntl();
+  const isoDate = new Date(indexedAt + "Z");
 
-  return (
+  return indexedAt ? (
     <div className="flex flex-col items-center gap-2 rounded-2xl bg-white/2 p-12">
       <div className="mb-6">
         <img src={emptyStateLogo}></img>
@@ -12,6 +18,11 @@ export default function EmptyState() {
       <div className="text-center font-belwe text-2xl font-normal text-greyscale-50">
         {T("reward.form.contributions.emptyState.title")}
       </div>
+      <div className="text-center font-walsheim text-base font-normal text-greyscale-50">
+        {T("reward.form.contributions.emptyState.subtitle", {
+          time: getFormattedTimeUS(isoDate),
+        })}
+      </div>
     </div>
-  );
+  ) : null;
 }
