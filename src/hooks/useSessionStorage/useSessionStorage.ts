6e651164b548ae14167b4fef5472a683da4388cb
pixelfact <pixelfact.company@gmail.com>
2023-10-24 18:29:41 +0200

    add useSessionStorage hook

---
+++ src/hooks/useSessionStorage/useSessionStorage.ts
@@ -0,0 +1,59 @@
+import { useCallback, useEffect } from "react";
+import { useState } from "react";
+
+export type UseSessionStorageOptions = {
+  /**
+   * Function for converting to string.
+   *
+   * @default JSON.stringify
+   */
+  serialize: (value: unknown) => string;
+
+  /**
+   * Function to convert stored string to object value.
+   *
+   * @default JSON.parse
+   */
+  deserialize: (value: string) => unknown;
+};
+
+/**
+ * Modified `useState` hook that syncs with useSessionStorage.
+ *
+ * @param key
+ * @param initialValue
+ * @param options
+ *
+ * @see https://react-hooks-library.vercel.app/core/useSessionStorage
+ */
+export function useSessionStorage<T>(
+  key: string,
+  initialValue: T,
+  options?: UseSessionStorageOptions
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState(initialValue);
+  const { deserialize = JSON.parse, serialize = JSON.stringify } = options || {};
+
+  useEffect(() => {
+    try {
+      const item = sessionStorage.getItem(key);
+      item && setStoredValue(deserialize(item));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const setValue = useCallback(
+    (value: T) => {
+      try {
+        setStoredValue(value);
+        sessionStorage.setItem(key, serialize(value));
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [key, serialize]
+  );
+
+  return [storedValue, setValue];
+}
