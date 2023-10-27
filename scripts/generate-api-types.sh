a19a9687b04d0c4829f0900e0098008528cc0811
PierreOucif <pierre.oucif@gadz.org>
2023-10-27 10:05:54 +0200

    Using git branch for code gen

--- scripts/generate-api-types.sh
+++ scripts/generate-api-types.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-if [[ -z $VERCEL_ENV ]]; then
-    echo "⚙️  Genereting types using default API BASEPATH"
-    yarn openapi-typescript https://develop-api.onlydust.com/v3/api-docs --output ./src/__generated/api.d.ts
+default_swagger_doc_url="https://develop-api.onlydust.com/v3/api-docs"
+staging_swagger_doc_url="https://staging-api.onlydust.com/v3/api-docs"
+production_swagger_doc_url="https://api.onlydust.com/v3/api-docs"
+
+
+function generate_code_from_swagger_doc_url() {
+    echo "⚙️  Genereting types using default API BASEPATH = ${1}"
+    yarn openapi-typescript $1 --output ./src/__generated/api.d.ts
+}
+
+if [[ -z $VERCEL_GIT_COMMIT_REF ]]; then
+    generate_code_from_swagger_doc_url $default_swagger_doc_url
 else
-    echo "⚙️  Generating types using following API BASEPATH -> $VERCEL_ENV"
-    if [[ $VERCEL_ENV = "preview"  ]]; then
-        yarn openapi-typescript https://develop-api.onlydust.com/v3/api-docs --output ./src/__generated/api.d.ts
-
-    elif [[ $VERCEL_ENV = "preview (staging)"  ]]; then
-        yarn openapi-typescript https://staging-api.onlydust.com/v3/api-docs --output ./src/__generated/api.d.ts
-
-    elif [[ $VERCEL_ENV = "production"  ]]; then
-        yarn openapi-typescript https://api.onlydust.com/v3/api-docs --output ./src/__generated/api.d.ts
+    if [[ $VERCEL_GIT_COMMIT_REF = "staging"  ]]; then
+        generate_code_from_swagger_doc_url $staging_swagger_doc_url
+    elif [[ $VERCEL_GIT_COMMIT_REF = "production"  ]]; then
+        generate_code_from_swagger_doc_url $production_swagger_doc_url
+    else
+        generate_code_from_swagger_doc_url $default_swagger_doc_url
     fi
 fi
+
