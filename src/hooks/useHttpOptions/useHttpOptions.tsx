95256481c017000418406c5ebf4714eeaf52203c
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-10-31 16:24:45 +0100

    feat: get impersonation headers for REST

--- src/hooks/useHttpOptions/useHttpOptions.tsx
+++ src/hooks/useHttpOptions/useHttpOptions.tsx
@@ -1,3 +1,4 @@
+import { useImpersonationClaims } from "src/hooks/useImpersonationClaims";
 import { useTokenSet } from "src/hooks/useTokenSet";
 
 export function useHttpOptions(method: "GET" | "POST" | "PUT" | "DELETE"): {
@@ -6,6 +7,11 @@ export function useHttpOptions(method: "GET" | "POST" | "PUT" | "DELETE"): {
     Authorization?: string;
   };
 } {
+  const { getImpersonationHeaders } = useImpersonationClaims();
+  const impersonationHeaders = getImpersonationHeaders();
+  // getImpersonationHeaders returns either an object with headers or an empty array
+  const parsedImpersonationHeaders = Array.isArray(impersonationHeaders) ? {} : impersonationHeaders;
+
   const { tokenSet } = useTokenSet();
   const token = tokenSet?.accessToken;
 
@@ -15,6 +21,7 @@ export function useHttpOptions(method: "GET" | "POST" | "PUT" | "DELETE"): {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
       "Content-Type": "application/json",
       accept: "application/json",
+      ...parsedImpersonationHeaders,
     },
   };
 
