d92538146a335f2ee5546b0705d7daf92469e0b7
Mehdi Hamri <pixelfact.company@gmail.com>
2023-10-31 11:22:15 +0100

    impersonate Form rework

--- src/pages/Impersonation/PasswordForm.tsx
+++ src/pages/Impersonation/PasswordForm.tsx
@@ -1,35 +1,39 @@
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import Card from "src/components/Card";
-import FormInput from "src/components/FormInput";
 import { useIntl } from "src/hooks/useIntl";
 
 type Props = {
   onSubmit: (password: string) => void;
 };
 
+type FormValues = {
+  password: string;
+};
+
 const PasswordForm: React.FC<Props> = ({ onSubmit }) => {
-  const methods = useForm({ defaultValues: { password: "" } });
-  const password = methods.watch("password");
+  const { register, handleSubmit, watch } = useForm<FormValues>();
   const { T } = useIntl();
+  const onSubmitting: SubmitHandler<FormValues> = (data) => onSubmit(data.password);
+
+  const password = watch("password");
+  const isPasswordEmpty = !password || password.length === 0;
 
   return (
     <div className="w-96">
       <Card>
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(({ password }) => onSubmit(password))} className="flex flex-col gap-4">
-            <h1 className="text-center text-lg font-bold text-orange-500">{T("impersonation.form.title")}</h1>
-            <i className="text-sm text-spaceBlue-200">{T("impersonation.form.notice")}</i>
-            <FormInput
-              name="password"
-              label={T("impersonation.form.password.label")}
-              type="password"
-              options={{
-                required: { value: password.length === 0, message: T("impersonation.form.password.missing") },
-              }}
-              withMargin={false}
-            />
-          </form>
-        </FormProvider>
+        <form onSubmit={handleSubmit(onSubmitting)} className="flex flex-col gap-4">
+          <h1 className="text-center text-lg font-bold text-orange-500">{T("impersonation.form.title")}</h1>
+          <i className="text-sm text-spaceBlue-200">{T("impersonation.form.notice")}</i>
+          <label htmlFor="password" className="font-walsheim text-greyscale-300 text-sm font-medium tracking-tight">{T("impersonation.form.password.label")}</label>
+          <input 
+            type="password" 
+            {...register("password", { required: true })} 
+            className="w-full bg-white/5 rounded-xl font-walsheim font-normal text-greyscale-50 border border-greyscale-50/[0.08] placeholder:text-spaceBlue-200 focus:placeholder:text-spacePurple-200/60 focus:border-spacePurple-500 focus:bg-spacePurple-900 h-11 px-4 py-3 text-base focus:outline-none focus:ring-none"
+          />
+          {isPasswordEmpty && (
+            <span className="text-red-500 text-sm">{T("impersonation.form.password.missing")}</span>
+          )}
+        </form>
       </Card>
     </div>
   );
