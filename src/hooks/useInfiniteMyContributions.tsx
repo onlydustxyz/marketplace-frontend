cc18cfba6a7a1c690ab6b162e241e9072bdd9948
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-03 17:17:40 +0100

    refactor: useInfiniteMyContributions

--- src/hooks/useInfiniteMyContributions.tsx
+++ src/hooks/useInfiniteMyContributions.tsx
@@ -1,50 +1,15 @@
-import { useInfiniteQuery, UseInfiniteQueryResult } from "@tanstack/react-query";
 import { components } from "src/__generated/api";
 import { ApiResourcePaths } from "src/hooks/useRestfulData/config";
-import { PageData, PagesData } from "src/types";
-import { getEndpointUrl, QueryParam } from "src/utils/getEndpointUrl";
-import { useHttpOptions } from "./useHttpOptions";
+import { useInfiniteRestfulData } from "src/hooks/useRestfulData/useRestfulData";
+import { QueryParams } from "src/utils/getEndpointUrl";
 
-type ContributionsPageData = PageData<{
-  contributions: components["schemas"]["ContributionPageResponse"]["contributions"];
-}>;
-type ContributionsPagesData = PagesData<ContributionsPageData>;
-
-type Props = {
-  queryParams?: QueryParam[];
-  enabled?: boolean;
-};
-
-export default function useInfiniteMyContributions({
-  queryParams,
-  enabled,
-}: Props): UseInfiniteQueryResult<ContributionsPagesData, unknown> {
-  const options = useHttpOptions("GET");
-
-  return useInfiniteQuery({
-    queryKey: ["my-contributions", queryParams],
-    queryFn: ({ pageParam }) =>
-      fetch(
-        getEndpointUrl({
-          resourcePath: ApiResourcePaths.MY_CONTRIBUTIONS,
-          pageParam,
-          pageSize: 10,
-          queryParams,
-        }),
-        options
-      )
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          }
-
-          throw new Error(res.statusText);
-        })
-        .catch(e => {
-          throw new Error(e);
-        }),
-    initialPageParam: 0,
-    getNextPageParam: lastPage => (lastPage?.hasMore ? lastPage.nextPageIndex : undefined),
-    enabled,
-  });
+export default function useInfiniteMyContributions({ queryParams }: { queryParams?: QueryParams }) {
+  return useInfiniteRestfulData<components["schemas"]["ContributionPageResponse"]>(
+    {
+      resourcePath: ApiResourcePaths.MY_CONTRIBUTIONS,
+      pageSize: 10,
+      queryParams,
+    },
+    { queryKey: ["my-contributions", queryParams] }
+  );
 }
