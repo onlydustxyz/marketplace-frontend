3337b11b33e315f7b4dc256a1f5625b9e9122609
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2023-11-07 10:03:21 +0100

    add invitedLeaders (#1382)

--- src/pages/ProjectDetails/Contributors/Contributors.test.tsx
+++ src/pages/ProjectDetails/Contributors/Contributors.test.tsx
@@ -122,6 +122,7 @@ const project: ComponentProps<typeof View>["project"] = {
     Python: 6840986,
   },
   remainingUsdBudget: 99250.0,
+  invitedLeaders: [],
 };
 
 const queryClient = new QueryClient();
