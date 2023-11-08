a4268a43b3fbe0c74bbf176a6467b768240b74a1
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-08 10:43:04 +0100

    refactor: Github exports

--- src/api/Github/index.ts
+++ src/api/Github/index.ts
@@ -1,9 +1,9 @@
-import { GITHUB_TAGS } from "./tags";
+import { GITHUB_TAGS as tags } from "./tags";
 import queries from "./queries";
 
 const GithubApi = {
-  tags: GITHUB_TAGS,
-  queries: queries,
+  tags,
+  queries,
 };
 
 export default GithubApi;
