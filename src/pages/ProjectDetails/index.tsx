073235e06d9af33368283dfa68df7a49ba9ddf8d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-08 21:41:35 +0100

    refactor: project details api usage

--- src/pages/ProjectDetails/index.tsx
+++ src/pages/ProjectDetails/index.tsx
@@ -1,27 +1,17 @@
 import { Navigate, matchPath, useLocation, useParams } from "react-router-dom";
 import { ProjectRoutePaths, RoutePaths } from "src/App";
 import ErrorFallback from "src/ErrorFallback";
-import { components } from "src/__generated/api";
+import ProjectApi from "src/api/Project";
 import Loader from "src/components/Loader";
 import SEO from "src/components/SEO";
-import { ApiResourcePaths } from "src/hooks/useRestfulData/config";
-import { useRestfulData } from "src/hooks/useRestfulData/useRestfulData";
 import View from "./View";
 
-type ProjectDetailsParams = {
-  projectKey: string;
-};
-
 export default function ProjectDetails() {
-  const { projectKey = "" } = useParams<ProjectDetailsParams>();
+  const { projectKey = "" } = useParams<{ projectKey: string }>();
   const { pathname } = useLocation();
   const isProjectEdition = !!matchPath(`${RoutePaths.ProjectDetails}/${ProjectRoutePaths.Edit}`, pathname);
 
-  const { data, isLoading, isError } = useRestfulData<components["schemas"]["ProjectResponse"]>({
-    resourcePath: ApiResourcePaths.GET_PROJECT_DETAILS_SLUG,
-    pathParam: projectKey,
-    method: "GET",
-  });
+  const { data, isLoading, isError } = ProjectApi.queries.useDetails({ params: { projectKey } });
 
   if (isLoading) {
     // TODO Replace with skeleton component
@@ -32,15 +22,13 @@ export default function ProjectDetails() {
     return <ErrorFallback />;
   }
 
-  const { name } = data as components["schemas"]["ProjectResponse"];
-
   if (!data) {
     return <Navigate to={RoutePaths.NotFound} />;
   }
 
   return (
     <>
-      <SEO title={`${name} — OnlyDust`} />
+      <SEO title={`${data.name} — OnlyDust`} />
       <View project={data} loading={isLoading} error={isError} padded={!isProjectEdition} />
     </>
   );
