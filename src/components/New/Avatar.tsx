fdc228b193ec1a41778700402d12046a0dd6842a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-08 21:40:48 +0100

    style: avatar

--- src/components/New/Avatar.tsx
+++ src/components/New/Avatar.tsx
@@ -7,7 +7,7 @@ const sizes = {
   "4": "w-4 h-4",
   "6": "w-6 h-6",
   "12": "w-12 h-12",
-};
+} as const;
 
 const shapes: { square: Record<keyof typeof sizes, string>; circle: string } = {
   square: {
@@ -18,7 +18,7 @@ const shapes: { square: Record<keyof typeof sizes, string>; circle: string } = {
     "12": "rounded-xl",
   },
   circle: "rounded-full",
-};
+} as const;
 
 const border: Record<keyof typeof sizes, string> = {
   "2": "border-1",
@@ -26,7 +26,7 @@ const border: Record<keyof typeof sizes, string> = {
   "4": "border-1",
   "6": "border-2",
   "12": "border-2",
-};
+} as const;
 
 export interface AvatarProps {
   src: string;
@@ -40,7 +40,7 @@ export function Avatar({ src, alt, shape = "circle", size = "6", className }: Av
   return (
     <div
       className={cn(
-        "relative flex items-center justify-center overflow-hidden border-greyscale-50/12 bg-greyscale-50/8 bg-noise-heavy",
+        "relative box-content flex items-center justify-center overflow-hidden border-greyscale-50/12 bg-greyscale-50/8 bg-noise-heavy",
         shape === "circle" ? shapes[shape] : shapes[shape][size],
         sizes[size],
         border[size],
