a2d0f349689b22c0d0e86be86ef3e44968330247
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-08 11:19:15 +0100

    refactor: translate you

--- src/pages/ProjectCreation/pages/ProjectInformations/components/ProjectLead/ProjectLeadItem.tsx
+++ src/pages/ProjectCreation/pages/ProjectInformations/components/ProjectLead/ProjectLeadItem.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Avatar } from "src/components/New/Avatar";
+import { useIntl } from "src/hooks/useIntl";
 import CloseLine from "src/icons/CloseLine";
 
 export interface FieldProjectLeadItemProps {
@@ -10,6 +11,8 @@ export interface FieldProjectLeadItemProps {
 }
 
 export const FieldProjectLeadItem: FC<FieldProjectLeadItemProps> = ({ isYou = false, avatar, label, onRemove }) => {
+  const { T } = useIntl();
+
   return (
     <div className="flex items-center">
       <div className="flex items-center gap-2">
@@ -17,7 +20,7 @@ export const FieldProjectLeadItem: FC<FieldProjectLeadItemProps> = ({ isYou = fa
         <span className=" text-sm leading-[14px] text-spacePurple-300">{label}</span>
       </div>
       {isYou ? (
-        <span className="ml-2 text-sm text-spaceBlue-200">(you)</span>
+        <span className="ml-2 text-sm text-spaceBlue-200">({T("common.you")})</span>
       ) : (
         <button type="button" onClick={onRemove} className="leading-none">
           <CloseLine className="h-4 w-4 text-spacePurple-300" />
