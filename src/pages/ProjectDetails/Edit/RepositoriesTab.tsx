c719d6c5524d71cfccea2b197def58780fb0f22b
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-09 12:21:09 +0100

    feat: repositories states and local removal

--- src/pages/ProjectDetails/Edit/RepositoriesTab.tsx
+++ src/pages/ProjectDetails/Edit/RepositoriesTab.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import ProjectApi from "src/api/Project";
+import InfoIcon from "src/assets/icons/InfoIcon";
 import Button, { ButtonSize, ButtonType } from "src/components/Button";
 import { Avatar } from "src/components/New/Avatar";
-import { Flex } from "src/components/New/Layout/Flex";
 import { useIntl } from "src/hooks/useIntl";
 import AddLine from "src/icons/AddLine";
 import ForkLine from "src/icons/ForkLine";
@@ -13,11 +14,49 @@ export default function RepositoriesTab() {
   const { projectKey = "" } = useParams<{ projectKey: string }>();
   const { T } = useIntl();
 
-  const { data, isLoading, isError } = ProjectApi.queries.useDetails({ params: { projectKey } });
+  // TODO move to parent
+  const { data, isLoading, isError } = ProjectApi.queries.useDetails({
+    params: { projectKey },
+    options: { refetchOnMount: false, refetchOnWindowFocus: false },
+  });
 
-  function handleRemoveRepo(id: number) {
-    // TODO
-    alert(id);
+  // TODO move to parent
+  const [repositoriesData, setRepositoriesData] = useState<typeof data>(data);
+
+  function handleRemoveRepo(orgId?: number, repoId?: number) {
+    if (orgId && repoId) {
+      const { organizations } = repositoriesData ?? {};
+
+      // Remove repo from org
+      const filteredOrgRepos = organizations?.find(({ id }) => id === orgId)?.repos?.filter(({ id }) => id !== repoId);
+
+      let filteredOrgs: typeof organizations;
+
+      // If org still has repos -> update orgs
+      if (filteredOrgRepos?.length) {
+        filteredOrgs = organizations?.map(org => {
+          if (org.id === orgId) {
+            return {
+              ...org,
+              repos: filteredOrgRepos,
+            };
+          }
+
+          return org;
+        });
+      } else {
+        // If org empty -> remove it
+        filteredOrgs = organizations?.filter(({ id }) => id !== orgId);
+      }
+
+      // Remove repo from repos
+      const filteredRepos = repositoriesData?.repos?.filter(({ id }) => id !== repoId);
+
+      setRepositoriesData(prevState =>
+        // Can't use spread operator due to Typescript mismatch
+        Object.assign({}, prevState, { organizations: filteredOrgs, repos: filteredRepos })
+      );
+    }
   }
 
   function renderRepositories() {
@@ -27,22 +66,27 @@ export default function RepositoriesTab() {
     }
 
     if (isError) {
-      // TODO
-      return <div>Error</div>;
+      return (
+        <div className="py-24">
+          <p className="text-center font-walsheim text-sm text-greyscale-50">
+            {T("project.details.edit.repositories.error")}
+          </p>
+        </div>
+      );
     }
 
-    if (data) {
+    if (repositoriesData?.organizations?.length) {
       return (
         <div>
-          {data.organizations?.map(organization => (
+          {repositoriesData.organizations.map(organization => (
             <div
               key={organization.name}
               className="flex w-full flex-col gap-3 rounded-2xl border border-card-border-light bg-card-background-light p-5"
             >
-              <Flex justify="start" item="center" gap={2}>
+              <div className="flex items-center gap-2">
                 <Avatar src={organization.avatarUrl ?? ""} alt={organization.name ?? ""} shape="square" />
                 <p className="font-walsheim text-sm font-medium uppercase text-spaceBlue-200">{organization.name}</p>
-              </Flex>
+              </div>
               <div className="grid grid-flow-row grid-cols-2 gap-x-5 gap-y-5">
                 {organization.repos?.map(repo => (
                   <div
@@ -57,11 +101,12 @@ export default function RepositoriesTab() {
                             size={ButtonSize.Sm}
                             type={ButtonType.Secondary}
                             onClick={() => {
-                              handleRemoveRepo(repo.id);
+                              handleRemoveRepo(organization.id, repo.id);
                             }}
                             iconOnly
                           >
                             <SubtractLine className="text-base" />
+                            <span className="sr-only">{T("project.details.edit.repositories.removeRepository")}</span>
                           </Button>
                         </div>
                         <p className={`text-body-s text-greyscale-200 ${!repo.description && "italic"}`}>
@@ -88,8 +133,15 @@ export default function RepositoriesTab() {
       );
     }
 
-    // TODO possible state ?
-    return <div>Empty</div>;
+    return (
+      <div className="flex flex-col gap-2 rounded-2xl border border-card-border-medium bg-card-background-light px-6 py-8 shadow-light">
+        <p className="text-title-s text-center text-white">{T("project.details.edit.repositories.empty.title")}</p>
+        <p className="text-body-s flex items-center justify-center gap-1 text-spaceBlue-200">
+          <InfoIcon />
+          {T("project.details.edit.repositories.empty.message")}
+        </p>
+      </div>
+    );
   }
 
   return (
