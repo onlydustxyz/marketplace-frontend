a450b54090ee34a78aad89b10073647ff6433357
Mehdi Hamri <pixelfact.company@gmail.com>
2023-11-09 17:05:24 +0100

    fix test

--- src/pages/ProjectDetails/Contributors/Contributors.test.tsx
+++ src/pages/ProjectDetails/Contributors/Contributors.test.tsx
@@ -10,9 +10,126 @@ import View from "src/pages/ProjectDetails/View";
 import ApolloWrapper from "src/providers/ApolloWrapper";
 import { renderWithIntl } from "src/test/utils";
 
+// TODO detele
+// const project: ComponentProps<typeof View>["project"] = {
+//   id: "cdb45d97-13a6-4f71-8c8c-78917fc02649",
+//   createdAt: "2023-11-09T14:30:01.193Z",
+//   slug: "performance-test-with-a-very-long-name",
+//   name: "Performance test with a very long name",
+//   shortDescription: "Do not create issue on this one as it is linked with real projects !",
+//   longDescription: "Do not create issue on this one as it is linked with real projects !",
+//   logoUrl: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/1409408835957028656.png",
+//   hiring: false,
+//   visibility: "PUBLIC",
+//   contributorCount: 3,
+//   moreInfoUrl: "www.onlydust.xyz",
+//   invitedLeaders: [],
+//   createdAt: "",
+//   leaders: [
+//     {
+//       githubUserId: 74653697,
+//       login: "AnthonyBuisset",
+//       htmlUrl: "",
+//       avatarUrl: "https://avatars.githubusercontent.com/u/43467246?v=4",
+//       id: "adcb11a6-92cf-4a1e-bace-79f7bdbc54e7",
+//     },
+//     {
+//       githubUserId: 8642470,
+//       login: "ofux",
+//       htmlUrl: "",
+//       avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+//       id: "bd365490-dd23-4a24-ad23-7853fbd853c2",
+//     },
+//   ],
+//   repos: [
+//     {
+//       id: 566371874,
+//       owner: "onlydustxyz",
+//       name: "hasura-auth",
+//       description: "Authentication for Hasura.",
+//       stars: 0,
+//       forkCount: 1,
+//       htmlUrl: "https://github.com/onlydustxyz/hasura-auth",
+//       hasIssues: false,
+//     },
+//     {
+//       id: 498695724,
+//       owner: "onlydustxyz",
+//       name: "marketplace-frontend",
+//       description: "Contributions marketplace backend services",
+//       stars: 15,
+//       forkCount: 10,
+//       htmlUrl: "https://github.com/onlydustxyz/marketplace-frontend",
+//       hasIssues: true,
+//     },
+//   ],
+//   topContributors: [
+//     {
+//       githubUserId: 698957,
+//       login: "ltoussaint",
+//       htmlUrl: "https://github.com/ltoussaint",
+//       avatarUrl: "https://avatars.githubusercontent.com/u/698957?v=4",
+//     },
+//     {
+//       githubUserId: 595505,
+//       login: "ofux",
+//       htmlUrl: "https://github.com/ofux",
+//       avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+//     },
+//     {
+//       githubUserId: 4435377,
+//       login: "Bernardstanislas",
+//       htmlUrl: "https://github.com/Bernardstanislas",
+//       avatarUrl: "https://avatars.githubusercontent.com/u/4435377?v=4",
+//     },
+//   ],
+//   sponsors: [
+//     {
+//       id: "ce038af0-9f8d-4948-bd5a-1c86cf983041",
+//       name: "OnlyDust",
+//       url: "",
+//       logoUrl: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/13878645251970159319.jpg",
+//     },
+//     {
+//       id: "ce038af0-9f8d-4948-bd5a-1c86cf983042",
+//       name: "OnlyFast",
+//       url: "",
+//       logoUrl: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/13878645251970159319.jpg",
+//     },
+//     {
+//       id: "ce038af0-9f8d-4948-bd5a-1c86cf983043",
+//       name: "OnlyRust",
+//       url: "",
+//       logoUrl: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/13878645251970159319.jpg",
+//     },
+//   ],
+//   technologies: {
+//     Java: 1082,
+//     CPlusPlus: 22688656,
+//     CSS: 128024,
+//     C: 3655312,
+//     Scheme: 50648,
+//     CMake: 115552,
+//     ObjectiveCPlusPlus: 10994,
+//     QMake: 876,
+//     Makefile: 298032,
+//     M4: 435994,
+//     HTML: 285238,
+//     Sage: 118798,
+//     TypeScript: 42942,
+//     Dockerfile: 9068,
+//     Shell: 367042,
+//     CoffeeScript: 34960,
+//     CapnProto: 2512,
+//     JavaScript: 8236948,
+//     Assembly: 56732,
+//     Python: 6840986,
+//   },
+//   remainingUsdBudget: 99250.0,
+// };
+
 const project: ComponentProps<typeof View>["project"] = {
-  id: "cdb45d97-13a6-4f71-8c8c-78917fc02649",
-  createdAt: "2023-11-09T14:30:01.193Z",
+  id: "test-project-id",
   slug: "performance-test-with-a-very-long-name",
   name: "Performance test with a very long name",
   shortDescription: "Do not create issue on this one as it is linked with real projects !",
@@ -22,18 +139,16 @@ const project: ComponentProps<typeof View>["project"] = {
   visibility: "PUBLIC",
   contributorCount: 3,
   moreInfoUrl: "www.onlydust.xyz",
-  invitedLeaders: [],
-  createdAt: "",
   leaders: [
     {
-      githubUserId: 74653697,
+      githubUserId: 498695724,
       login: "AnthonyBuisset",
       htmlUrl: "",
       avatarUrl: "https://avatars.githubusercontent.com/u/43467246?v=4",
       id: "adcb11a6-92cf-4a1e-bace-79f7bdbc54e7",
     },
     {
-      githubUserId: 8642470,
+      githubUserId: 498695724,
       login: "ofux",
       htmlUrl: "",
       avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
@@ -125,6 +240,8 @@ const project: ComponentProps<typeof View>["project"] = {
     Python: 6840986,
   },
   remainingUsdBudget: 99250.0,
+  invitedLeaders: [],
+  createdAt: "",
 };
 
 const queryClient = new QueryClient();
