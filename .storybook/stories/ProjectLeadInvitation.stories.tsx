21eeb46a5c28247b7a6e8b126d9bc5c09a3c1a71
Alexis Benoliel <alexbenoliel@gmail.com>
2023-11-10 10:32:05 +0100

    Fix stories

--- .storybook/stories/ProjectLeadInvitation.stories.tsx
+++ .storybook/stories/ProjectLeadInvitation.stories.tsx
@@ -4,11 +4,18 @@ import withAuthProvider from "../decorators/withAuthProvider";
 import withMockedProvider from "../decorators/withMockedProvider";
 import ProjectLeadInvitation from "src/components/ProjectLeadInvitation/ProjectLeadInvitation";
 import { CalloutSizes } from "src/components/ProjectLeadInvitation/ProjectLeadInvitationView";
+import withToasterProvider from "../decorators/withToasterProvider";
+import withImpersonationClaimsProvider from "../decorators/withImpersonationClaimsProvider";
+import withTokenSetProvider from "../decorators/withTokenSetProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { JSXElementConstructor } from "react";
 
 const PROJECT_ID = "project-id";
 const PROJECT_NAME = "project-name";
 const PROJECT_SLUG = "project-slug";
 const GITHUB_USER_ID = 123456;
+
+const queryClient = new QueryClient();
 
 const mocks: MockedResponse[] = [
   {
@@ -33,7 +40,18 @@ const mocks: MockedResponse[] = [
 export default {
   title: "ProjectLeadInvitation",
   component: ProjectLeadInvitation,
-  decorators: [withAuthProvider({ githubUserId: GITHUB_USER_ID }), withMockedProvider(mocks)],
+  decorators: [
+    withAuthProvider({ githubUserId: GITHUB_USER_ID }),
+    withToasterProvider,
+    withTokenSetProvider,
+    withImpersonationClaimsProvider,
+    withMockedProvider(mocks),
+    (Story: JSXElementConstructor<any>) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
 };
 
 export const Default = {
