a9c59f7ea607899b915b94dd677d4ef26825103e
Mehdi Hamri <pixelfact.company@gmail.com>
2023-11-14 15:44:16 +0100

    fix UserRewardTable table stories

--- .storybook/stories/UserRewardTable.stories.tsx
+++ .storybook/stories/UserRewardTable.stories.tsx
@@ -6,56 +6,20 @@ import { Fields } from "src/components/UserRewardTable/Headers";
 import { MyRewardType as Reward } from "src/components/UserRewardTable/Line";
 import { daysFromNow } from "src/utils/date";
 import withAuthProvider from "../decorators/withAuthProvider";
+import withImpersonationClaimsProvider from "../decorators/withImpersonationClaimsProvider";
+import withTokenSetProvider from "../decorators/withTokenSetProvider";
+import withQueryClientProvider from "../decorators/withQueryClientProvider";
 
 const USER_ID = "e2ee731a-2697-4306-bf4b-c807f6fda0d7";
-
-const mockPayments: Reward[] = [
-  {
-    id: "c0cfdf80-bbba-4512-b5ec-066dfa9529b1",
-    amount: {
-      total: 2000,
-      currency: Currency.USD,
-    },
-    numberOfRewardedContributions: 2,
-    projectId: "1ac6c2ce-cba0-4e14-837c-facf9c1f0258",
-    requestedAt: daysFromNow(700).toISOString(),
-    rewardedOnProjectLogoUrl: "",
-    rewardedOnProjectName: "Project 1",
-    status: PaymentStatus.COMPLETE,
-  },
-  {
-    id: "6397226d-0461-4451-962c-a61e36fd324b",
-    amount: {
-      total: 3000,
-      currency: Currency.ETH,
-    },
-    numberOfRewardedContributions: 3,
-    projectId: "1ac6c2ce-cba0-4e14-837c-facf9c1f0258",
-    requestedAt: daysFromNow(200).toISOString(),
-    rewardedOnProjectLogoUrl: "",
-    rewardedOnProjectName: "Project 2",
-    status: PaymentStatus.PENDING_INVOICE,
-  },
-];
 
 export default {
   title: "UserRewardTable",
   component: UserRewardTable,
-  decorators: [withAuthProvider({ userId: USER_ID })],
+  decorators: [withAuthProvider({ userId: USER_ID }), withImpersonationClaimsProvider, withTokenSetProvider,withQueryClientProvider],
 };
 
 export const Default = {
   render: () => (
-    <UserRewardTable
-      rewards={mockPayments}
-      fetchNextPage={(() => {}) as ComponentProps<typeof UserRewardTable>["fetchNextPage"]}
-      hasNextPage={false}
-      isFetchingNextPage={false}
-      sorting={{
-        field: Fields.Date,
-        isAscending: false,
-      }}
-      sortField={() => {}}
-    />
+    <UserRewardTable />
   ),
 };
