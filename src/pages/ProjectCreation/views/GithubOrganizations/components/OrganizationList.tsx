d5fabb5e3b3c50e8c9cf7e7a58f580260d99049a
Mehdi Hamri <pixelfact.company@gmail.com>
2023-11-16 17:30:43 +0100

    comment

--- src/pages/ProjectCreation/views/GithubOrganizations/components/OrganizationList.tsx
+++ src/pages/ProjectCreation/views/GithubOrganizations/components/OrganizationList.tsx
@@ -1,4 +1,7 @@
 import { UseOrganizationsByGithubUserIdResponse } from "src/api/Github/queries";
+import HorizontalListItemCard from "src/components/New/Cards/HorizontalListItemCard";
+import AddLine from "src/icons/AddLine";
+import PencilLine from "src/icons/PencilLine";
 
 interface OrganizationListProps {
   organizations: UseOrganizationsByGithubUserIdResponse[];
@@ -6,28 +9,28 @@ interface OrganizationListProps {
 }
 
 export default function OrganizationList({ organizations, emptyListFallBackText }: OrganizationListProps) {
-  // if (organizations.length) {
-  //   return (
-  //     <ul className="flex flex-col gap-2 py-4 pb-6">
-  //       {organizations.map((org, index) => {
-  //         const linkUrl = org.installed
-  //           ? `https://github.com/organizations/${org.login}/settings/installations/${org.id}`
-  //           : `${import.meta.env.VITE_GITHUB_INSTALLATION_URL}/permissions?target_id=${org.id}`;
+  if (organizations.length) {
+    return (
+      <ul className="flex flex-col gap-2 py-4 pb-6">
+        {organizations.map((org, index) => {
+          const linkUrl = org.installed
+            ? `https://github.com/organizations/${org.login}/settings/installations/${org.id}`
+            : `${import.meta.env.VITE_GITHUB_INSTALLATION_URL}/permissions?target_id=${org.id}`;
 
-  //         return (
-  //           <HorizontalListItemCard
-  //             key={`${org.login}+${index}`}
-  //             avatarUrl={org.avatarUrl ?? ""}
-  //             title={org.name || org.login || ""}
-  //             linkUrl={linkUrl}
-  //             linkIcon={org.installed ? <PencilLine /> : <AddLine />}
-  //             isExternalFlow={org.installed}
-  //           />
-  //         );
-  //       })}
-  //     </ul>
-  //   );
-  // }
+          return (
+            <HorizontalListItemCard
+              key={`${org.login}+${index}`}
+              avatarUrl={org.avatarUrl ?? ""}
+              title={org.name || org.login || ""}
+              linkUrl={linkUrl}
+              linkIcon={org.installed ? <PencilLine /> : <AddLine />}
+              isExternalFlow={org.installed}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
 
   return <p className="mt-3 text-gray-500">{emptyListFallBackText}</p>;
 }
