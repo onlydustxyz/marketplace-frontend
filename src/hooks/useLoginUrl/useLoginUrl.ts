156176027f4906f72dd200b7d02e4d6775d4c279
Alexis Benoliel <alexbenoliel@gmail.com>
2023-11-21 10:46:26 +0100

    Config

--- src/hooks/useLoginUrl/useLoginUrl.ts
+++ src/hooks/useLoginUrl/useLoginUrl.ts
@@ -12,12 +12,15 @@ export const useLoginUrlStorage = () => {
 export const useLoginUrl = () => {
   const storage = useLoginUrlStorage();
   return (redirect?: string) => {
+    console.log("CUSTOM REDIRECT", redirect);
     const LOGIN_URL = `${config.LOGIN_URL}?redirect_url=${encodeURI(redirect || window.location.href)}`;
     const permission = storage.getValue();
     if (permission) {
+      console.log("useLoginUrl - 1", `${LOGIN_URL}&scope=${permission}`);
       return `${LOGIN_URL}&scope=${permission}`;
     }
 
+    console.log("useLoginUrl - 2", `${LOGIN_URL}&scope=user:email`);
     return `${LOGIN_URL}&scope=user:email`;
   };
 };
