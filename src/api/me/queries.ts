2064f46426bfa0e031aaefb60de42d513ff1eae8
Alexis Benoliel <alexbenoliel@gmail.com>
2023-11-21 15:15:24 +0100

    Add enabled condition on Me request to not fetch when is not oggedIn

--- src/api/me/queries.ts
+++ src/api/me/queries.ts
@@ -3,14 +3,18 @@ import { API_PATH } from "../ApiPath";
 import { UseQueryProps, useBaseQuery } from "../useBaseQuery";
 import { useInfiniteBaseQuery } from "../useInfiniteBaseQuery";
 import { ME_TAGS } from "./tags";
+import { useAuth } from "src/hooks/useAuth";
 
 export type UseGetUserMeResponse = components["schemas"]["GetMeResponse"];
 
 const useGetMe = ({ options = {} }: UseQueryProps<UseGetUserMeResponse, undefined>) => {
+  const { isLoggedIn } = useAuth();
+
   return useBaseQuery<UseGetUserMeResponse>({
     resourcePath: API_PATH.ME,
     tags: ME_TAGS.all,
     ...options,
+    enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled),
   });
 };
 
@@ -20,6 +24,8 @@ const useMyRewardsInfiniteList = (
   params: Partial<Parameters<typeof useInfiniteBaseQuery<UseMyRewardsInfiniteListResponse>>[0]>,
   options: Parameters<typeof useInfiniteBaseQuery<UseMyRewardsInfiniteListResponse>>[1] = {}
 ) => {
+  const { isLoggedIn } = useAuth();
+
   return useInfiniteBaseQuery<UseMyRewardsInfiniteListResponse>(
     {
       ...params,
@@ -27,7 +33,7 @@ const useMyRewardsInfiniteList = (
       tags: ME_TAGS.rewards(),
       pageSize: 15,
     },
-    options
+    { ...options, enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled) }
   );
 };
 
@@ -37,13 +43,14 @@ const useMyContributions = (
   params: Partial<Parameters<typeof useInfiniteBaseQuery>[0]>,
   options: Parameters<typeof useInfiniteBaseQuery<UseMyContributionsResponse>>[1] = {}
 ) => {
+  const { isLoggedIn } = useAuth();
   return useInfiniteBaseQuery<UseMyContributionsResponse>(
     {
       ...params,
       resourcePath: API_PATH.MY_CONTRIBUTIONS,
       tags: ME_TAGS.contributions([params.queryParams]),
     },
-    options
+    { ...options, enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled) }
   );
 };
 
@@ -53,11 +60,13 @@ const useMyContributedProjects = ({
   params,
   options = {},
 }: UseQueryProps<UseMyContributedProjectsResponse, { repositories?: string }>) => {
+  const { isLoggedIn } = useAuth();
   return useBaseQuery<UseMyContributedProjectsResponse>({
     resourcePath: API_PATH.MY_CONTRIBUTED_PROJECTS,
     tags: ME_TAGS.contributedProjects(),
     queryParams: params,
     ...options,
+    enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled),
   });
 };
 
@@ -67,21 +76,25 @@ const useMyContributedRepos = ({
   params,
   options = {},
 }: UseQueryProps<UseMyContributedReposResponse, { projects?: string }>) => {
+  const { isLoggedIn } = useAuth();
   return useBaseQuery<UseMyContributedReposResponse>({
     resourcePath: API_PATH.MY_CONTRIBUTED_REPOS,
     queryParams: params,
     tags: ME_TAGS.contributedRepos(),
     ...options,
+    enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled),
   });
 };
 
 export type UseGithubOrganizationsResponse = components["schemas"]["GithubOrganizationResponse"];
 
 const useGithubOrganizations = ({ options = {} }: UseQueryProps<UseGithubOrganizationsResponse[], unknown>) => {
+  const { isLoggedIn } = useAuth();
   return useBaseQuery<UseGithubOrganizationsResponse[]>({
     resourcePath: API_PATH.ME_GITHUB_ORGANIZATIONS,
     tags: ME_TAGS.githubOrganization(),
     ...options,
+    enabled: isLoggedIn && (options.enabled === undefined ? true : options.enabled),
   });
 };
 
