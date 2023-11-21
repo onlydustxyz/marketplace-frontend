172f6c4511e3b8e2f7c429b0ff7fb5c209fa438d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-21 10:34:39 +0100

    fix: made banner cta a button to avoid anchor nesting

--- src/components/New/Project/MissingGithubAppInstall.tsx
+++ src/components/New/Project/MissingGithubAppInstall.tsx
@@ -1,20 +1,27 @@
-import { Link, generatePath } from "react-router-dom";
+import { generatePath, useNavigate } from "react-router-dom";
 import { RoutePaths } from "src/App";
 import Button, { ButtonSize } from "src/components/Button";
 import { useIntl } from "src/hooks/useIntl";
 
 export function MissingGithubAppInstall({ slug = "" }: { slug: string }) {
   const { T } = useIntl();
+  const navigate = useNavigate();
 
   return (
     <div className="bg-oritems-center flex items-center justify-between gap-3 rounded-2xl bg-orange-800 p-3">
       <p className="font-walsheim text-sm font-medium text-white">{T("project.missingGithubAppInstall.message")}</p>
 
-      <Link to={generatePath(RoutePaths.ProjectDetailsEditRepos, { projectKey: slug })}>
-        <Button size={ButtonSize.Sm} className="whitespace-nowrap">
-          {T("project.missingGithubAppInstall.button")}
-        </Button>
-      </Link>
+      <Button
+        size={ButtonSize.Sm}
+        className="whitespace-nowrap"
+        onClick={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          navigate(generatePath(RoutePaths.ProjectDetailsEditRepos, { projectKey: slug }));
+        }}
+      >
+        {T("project.missingGithubAppInstall.button")}
+      </Button>
     </div>
   );
 }
