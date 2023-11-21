6a1714bc5970f6aa40a5eaf0bf65cb9afcdec895
Mickaël Chimansky <mickael@onlydust.xyz>
2023-11-21 15:36:32 +0100

    fix: useGithubOrganizationMembership

--- src/hooks/useGithubUserMembership/useGithubUserMembership.ts
+++ src/hooks/useGithubUserMembership/useGithubUserMembership.ts
@@ -5,26 +5,25 @@ import { HasuraJWT } from "src/types";
 import MeApi from "src/api/me";
 import { UseGithubOrganizationsResponse } from "src/api/me/queries";
 
-const fetchGithubOrganizationMembership = async (token: string, organization: UseGithubOrganizationsResponse) => {
-  const { data: userInfo } = await MeApi.queries.useGetMe({});
-
-  const organizationLogin = organization.login;
-  const userLogin = userInfo?.login;
-
-  return fetch(`https://api.github.com/orgs/${organizationLogin}/memberships/${userLogin}`, {
+const fetchGithubOrganizationMembership = (
+  token: string,
+  organization: UseGithubOrganizationsResponse,
+  userLogin: string
+) => {
+  return fetch(`https://api.github.com/orgs/${organization.login}/memberships/${userLogin}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   }).then(data => data.json());
 };
 
-const getOrganizationMembership = async (token: string, organization: UseGithubOrganizationsResponse) => {
+const getOrganizationMembership = (token: string, organization: UseGithubOrganizationsResponse, userLogin: string) => {
   if (token) {
     try {
       const decodedToken = jwtDecode<HasuraJWT>(token);
       const claims = decodedToken["https://hasura.io/jwt/claims"] as { [key: string]: string };
       const githubAccessToken = claims["x-hasura-githubAccessToken"];
-      return fetchGithubOrganizationMembership(githubAccessToken, organization);
+      return fetchGithubOrganizationMembership(githubAccessToken, organization, userLogin);
     } catch {
       return undefined;
     }
@@ -36,13 +35,13 @@ export const useGithubOrganizationMembership = ({
 }: {
   organization: UseGithubOrganizationsResponse;
 }): boolean => {
+  const { data: userInfo } = MeApi.queries.useGetMe({});
   const { tokenSet } = useTokenSet();
   const [isMember, setIsMember] = useState<boolean>(false);
 
   const getMembership = useCallback(async () => {
-    if (tokenSet?.accessToken) {
-      const membership = await getOrganizationMembership(tokenSet?.accessToken, organization);
-      console.log(membership);
+    if (tokenSet?.accessToken && userInfo) {
+      const membership = await getOrganizationMembership(tokenSet?.accessToken, organization, userInfo.login);
       setIsMember(membership.role === "member");
     }
   }, [tokenSet]);
