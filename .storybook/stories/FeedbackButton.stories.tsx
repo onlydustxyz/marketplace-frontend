66e3dd90f5b81331e23e7e5c3ed1dca14692dad2
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-11-22 17:05:25 +0100

    fix: stories using UserIdentityDocument

--- .storybook/stories/FeedbackButton.stories.tsx
+++ .storybook/stories/FeedbackButton.stories.tsx
@@ -1,36 +1,22 @@
-import { withRouter } from "storybook-addon-react-router-v6";
-
 import FeedbackButton from "src/App/Layout/Header/FeedbackButton";
-import { UserIdentityDocument } from "src/__generated/graphql";
-import withMockedProvider from "../decorators/withMockedProvider";
+import { withRouter } from "storybook-addon-react-router-v6";
 import withAuthProvider from "../decorators/withAuthProvider";
+import withImpersonationClaimsProvider from "../decorators/withImpersonationClaimsProvider";
+import withQueryClientProvider from "../decorators/withQueryClientProvider";
+import withTokenSetProvider from "../decorators/withTokenSetProvider";
 
 const USER_ID = "e2ee731a-2697-4306-bf4b-c807f6fda0d7";
-
-const mocks = [
-  {
-    request: {
-      query: UserIdentityDocument,
-      variables: { userId: USER_ID },
-    },
-    result: {
-      data: {
-        userPayoutInfo: [
-          {
-            userId: USER_ID,
-            lastname: "Bar",
-            firstname: "Foo",
-          },
-        ],
-      },
-    },
-  },
-];
 
 export default {
   title: "FeedbackButton",
   component: FeedbackButton,
-  decorators: [withRouter, withMockedProvider(mocks), withAuthProvider({ userId: USER_ID })],
+  decorators: [
+    withRouter,
+    withQueryClientProvider,
+    withImpersonationClaimsProvider,
+    withTokenSetProvider,
+    withAuthProvider({ userId: USER_ID }),
+  ],
 };
 
 export const Default = {
