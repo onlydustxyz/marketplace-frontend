9223a6c00a85d74acec37da6c235c1daa522ac17
Mickaël Chimansky <mickael@onlydust.xyz>
2023-11-23 10:41:47 +0100

    feat: rm usePendingProjectLeaderInvitationsQuery

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,18 +4,17 @@ import { RoutePaths } from "src/App";
 import { useAuth } from "src/hooks/useAuth";
 import { SessionMethod, useSession, useSessionDispatch } from "src/hooks/useSession";
 import { RefreshToken } from "src/types";
-import useSignupRedirection from "./hooks/useSignUpRedirection";
 import Loader from "src/components/Loader";
 
 export const AUTH_CODE_QUERY_KEY = "refreshToken";
 
 export default function Login() {
-  const { login, isLoggedIn, user, githubUserId } = useAuth();
+  const { login, isLoggedIn } = useAuth();
   const [searchParams] = useSearchParams();
   const refreshToken = searchParams.get(AUTH_CODE_QUERY_KEY);
   const navigate = useNavigate();
-  const { loading, url } = useSignupRedirection({ userId: user?.id, githubUserId });
-  const { lastLoginTime, visitedPageBeforeLogin } = useSession();
+
+  const { visitedPageBeforeLogin } = useSession();
   const dispatchSession = useSessionDispatch();
 
   useEffect(() => {
@@ -26,15 +25,10 @@ export default function Login() {
 
   useEffect(() => {
     if (isLoggedIn) {
-      if (!lastLoginTime && !loading) {
-        dispatchSession({ method: SessionMethod.SetLastLoginTime, value: Date.now().toString() });
-        navigate(url);
-      } else if (lastLoginTime) {
-        dispatchSession({ method: SessionMethod.SetLastLoginTime, value: Date.now().toString() });
-        navigate(visitedPageBeforeLogin || RoutePaths.Projects);
-      }
+      dispatchSession({ method: SessionMethod.SetLastLoginTime, value: Date.now().toString() });
+      navigate(visitedPageBeforeLogin || RoutePaths.Projects);
     }
-  }, [isLoggedIn, loading]);
+  }, [isLoggedIn]);
 
   return <>{refreshToken ? <Loader /> : <Navigate to={RoutePaths.Projects} />}</>;
 }
