39fd07e4e4a748322830e57a9f379487f581b07d
Alexis Benoliel <alexbenoliel@gmail.com>
2023-11-23 17:53:13 +0100

    WIP add <a> instead of window.open

--- src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/ReadOnlyView/SocialLink.tsx
+++ src/hooks/useContributorProfilePanel/ContributorProfileSidePanel/ReadOnlyView/SocialLink.tsx
@@ -1,6 +1,6 @@
 import { useIntl } from "src/hooks/useIntl";
 import { useShowToaster } from "src/hooks/useToaster";
-import { copyClickHandlerFactory, linkClickHandlerFactory } from "src/utils/clickHandler";
+import { copyClickHandlerFactory } from "src/utils/clickHandler";
 
 interface SocialLinkProps extends React.PropsWithChildren {
   link?: string;
@@ -13,17 +13,23 @@ export default function SocialLink({ link, copyableValue, copyableValueName, tes
   const showToaster = useShowToaster();
   const { T } = useIntl();
 
+  if (link) {
+    return (
+      <a className="rounded-xl bg-white/4" data-testid={testId} href={link} target="_blank" rel="noreferrer">
+        <div className="flex h-10 w-10 flex-row items-center justify-center rounded-xl bg-noise-light text-2xl text-greyscale-200  hover:cursor-pointer hover:opacity-60">
+          {children}
+        </div>
+      </a>
+    );
+  }
+
   return (
     <div
       className="rounded-xl bg-white/4"
       data-testid={testId}
-      onClick={
-        link
-          ? linkClickHandlerFactory(link)
-          : copyClickHandlerFactory(copyableValue || "", () => {
-              showToaster(T("profile.valueCopiedToClipboard", { valueName: copyableValueName }));
-            })
-      }
+      onClick={copyClickHandlerFactory(copyableValue || "", () => {
+        showToaster(T("profile.valueCopiedToClipboard", { valueName: copyableValueName }));
+      })}
     >
       <div className="flex h-10 w-10 flex-row items-center justify-center rounded-xl bg-noise-light text-2xl text-greyscale-200  hover:cursor-pointer hover:opacity-60">
         {children}
