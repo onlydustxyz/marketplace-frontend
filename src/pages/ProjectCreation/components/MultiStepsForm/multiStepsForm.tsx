e75154b16d6ec1c3173fa65e39d04aefc4a9ed32
Mehdi Hamri <pixelfact.company@gmail.com>
2023-11-27 11:22:46 +0100

    fix mozaic border height using padding

--- src/pages/ProjectCreation/components/MultiStepsForm/multiStepsForm.tsx
+++ src/pages/ProjectCreation/components/MultiStepsForm/multiStepsForm.tsx
@@ -25,7 +25,7 @@ export const MultiStepsForm: FC<MultiStepsFormProps> = ({
 
   return (
     <div className="relative flex max-h-full w-full max-w-full flex-col overflow-hidden bg-card-background-base md:w-[688px] md:rounded-2xl">
-      <div className="hidden h-1.5 w-full bg-mosaic bg-cover md:block" />
+      <div className="hidden w-full bg-mosaic bg-cover pb-1.5 md:block" />
       <div className="flex flex-col gap-4 bg-card-background-base p-12 pb-5">
         <div className="font-walsheim text-base font-normal text-spaceBlue-100">{`${step}/${stepCount}`}</div>
         <div className="font-belwe text-2xl font-normal text-greyscale-50">{title}</div>
