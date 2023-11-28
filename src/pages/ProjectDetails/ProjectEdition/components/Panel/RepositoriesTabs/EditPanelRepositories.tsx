63d707e0875987455f58b7712ff94d4b7167c187
Alexis Benoliel <alexbenoliel@gmail.com>
2023-11-28 11:57:36 +0100

    Clean

--- src/pages/ProjectDetails/ProjectEdition/components/Panel/RepositoriesTabs/EditPanelRepositories.tsx
+++ src/pages/ProjectDetails/ProjectEdition/components/Panel/RepositoriesTabs/EditPanelRepositories.tsx
@@ -30,11 +30,9 @@ export const EditPanelRepositories = () => {
         onChange={e => setSearch(e.target.value)}
         startIcon={({ className }) => <SearchLine className={className} />}
       />
-      {/* <div className="w-full"> */}
       {filterOrganizationBySearch(installedOrganization).map(organization => (
         <Organization key={organization.id} organization={organization} />
       ))}
-      {/* </div> */}
     </Flex>
   );
 };
