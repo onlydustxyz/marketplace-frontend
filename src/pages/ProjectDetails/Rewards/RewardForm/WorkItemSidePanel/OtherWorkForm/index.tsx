45e90397ecc55971003db630428d022910bc447f
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2023-11-30 13:57:56 +0100

    E 880 graphql migration 1 (#1497)

--- src/pages/ProjectDetails/Rewards/RewardForm/WorkItemSidePanel/OtherWorkForm/index.tsx
+++ src/pages/ProjectDetails/Rewards/RewardForm/WorkItemSidePanel/OtherWorkForm/index.tsx
@@ -100,10 +100,10 @@ export default function OtherWorkForm({ projectId, contributorHandle, addWorkIte
   useMutationAlert({
     mutation: restcreateOtherWorkMutation,
     success: {
-      message: T("reward.form.contributions.other.success", { item: T("reward.form.contributions.other.tab") }),
+      message: T("reward.form.contributions.other.work.success"),
     },
     error: {
-      message: T("reward.form.contributions.other.error", { item: T("reward.form.contributions.other.tab") }),
+      message: T("reward.form.contributions.other.work.error"),
     },
   });
 
