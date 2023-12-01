adf84e386d54ee6152afef47a17b2286a4ffd692
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-01 17:53:36 +0100

    ➕ add profile mutation

--- src/api/ApiPath.ts
+++ src/api/ApiPath.ts
@@ -41,4 +41,5 @@ export const API_PATH = {
   MY_CLAIM: (projectId: string) => BASE_API_V1(`me/project-claims/${projectId}`),
   ME_APPLY_TO_PROJECT: BASE_API_V1("me/applications"),
   MY_PAYOUT_INFO: BASE_API_V1("me/payout-info"),
+  ME_PROFILE: BASE_API_V1("me/profile"),
 };
--- src/api/me/mutations.ts
+++ src/api/me/mutations.ts
@@ -69,4 +69,23 @@ const useApplyProject = ({
   });
 };
 
-export default { useAcceptProjectLeaderInvitation, useClaimProject, usePayoutInfo, useApplyProject, useUpdateMe };
+export type UseUpdateProfileBody = components["schemas"]["UserProfileRequest"];
+export type UseUpdateProfileResponse = components["schemas"]["PrivateUserProfileResponse"];
+
+const useUpdateProfile = ({ options = {} }: UseMutationProps<UseUpdateProfileResponse, UseUpdateProfileBody>) => {
+  return useBaseMutation<UseUpdateProfileBody, UseUpdateProfileResponse>({
+    resourcePath: API_PATH.ME_PROFILE,
+    invalidatesTags: [{ queryKey: MeApi.tags.all, exact: false }],
+    method: "PUT",
+    ...options,
+  });
+};
+
+export default {
+  useAcceptProjectLeaderInvitation,
+  useClaimProject,
+  usePayoutInfo,
+  useApplyProject,
+  useUpdateMe,
+  useUpdateProfile,
+};
