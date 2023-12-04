25d0a13c43f5d424f8ad2af1859c835c8cea7e0c
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-12-04 11:39:19 +0100

    feat: Github status request

--- src/App/Layout/Header/GithubStatusBanner/index.tsx
+++ src/App/Layout/Header/GithubStatusBanner/index.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router";
 import { RoutePaths } from "src/App";
+import ExternalApi from "src/api/External";
 import Button, { ButtonAccentColor, ButtonSize, ButtonType } from "src/components/Button";
 import { useIntl } from "src/hooks/useIntl";
 import GithubWarning from "src/icons/GithubWarning";
@@ -12,24 +13,30 @@ export function GithubStatusBanner() {
   const marginRoutes = [RoutePaths.Contributions];
   const addMargin = marginRoutes.some(route => route === location.pathname);
 
-  return (
-    <div className={cn("flex items-center justify-between gap-8 bg-orange-900 px-8 py-4", { "mb-4": addMargin })}>
-      <div className="flex items-center gap-2">
-        <div>
-          <GithubWarning />
+  const { data } = ExternalApi.queries.useGithubStatus();
+
+  if (data && (data.status.indicator === "major" || data.status.indicator === "critical")) {
+    return (
+      <div className={cn("flex items-center justify-between gap-8 bg-orange-900 px-8 py-4", { "mb-4": addMargin })}>
+        <div className="flex items-center gap-2">
+          <div>
+            <GithubWarning />
+          </div>
+          <p className="font-walsheim text-orange-500">{T("githubStatusBanner.message")}</p>
         </div>
-        <p className="font-walsheim text-orange-500">{T("githubStatusBanner.message")}</p>
+        <a href="https://www.githubstatus.com/" target="_blank" rel="noopener noreferrer">
+          <Button
+            type={ButtonType.Secondary}
+            accentColor={ButtonAccentColor.Orange}
+            size={ButtonSize.Sm}
+            className="whitespace-nowrap"
+          >
+            {T("githubStatusBanner.button")}
+          </Button>
+        </a>
       </div>
-      <a href="https://www.githubstatus.com/" target="_blank" rel="noopener noreferrer">
-        <Button
-          type={ButtonType.Secondary}
-          accentColor={ButtonAccentColor.Orange}
-          size={ButtonSize.Sm}
-          className="whitespace-nowrap"
-        >
-          {T("githubStatusBanner.button")}
-        </Button>
-      </a>
-    </div>
-  );
+    );
+  }
+
+  return null;
 }
