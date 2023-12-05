4942c61f4ad71aa7d9cc0578ebb00b89099fca09
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-05 16:49:25 +0100

    Pointer

--- src/libs/react-stack/ui/BackClick.tsx
+++ src/libs/react-stack/ui/BackClick.tsx
@@ -3,8 +3,8 @@ import { usePlacement } from "../hooks/usePlacement";
 export const BackClick = ({ onClick }: { onClick: () => void }) => {
   const { placement } = usePlacement();
   if (placement === "bottom") {
-    return <div onClick={onClick} className="absolute -top-[40px] left-0 z-20 h-[40px] w-screen"></div>;
+    return <div onClick={onClick} className="absolute -top-[40px] left-0 z-20 h-[40px] w-screen cursor-pointer"></div>;
   }
 
-  return <div onClick={onClick} className="absolute -left-[40px] top-0 z-20 h-screen w-[40px] "></div>;
+  return <div onClick={onClick} className="absolute -left-[40px] top-0 z-20 h-screen w-[40px] cursor-pointer "></div>;
 };
