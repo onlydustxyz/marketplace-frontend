09cba50b5aa66afeb75c2061638f3359c4343452
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-06 15:44:30 +0100

    Add feedback button to claim panel

--- src/components/New/Ui/GithubSyncSettings.tsx
+++ src/components/New/Ui/GithubSyncSettings.tsx
@@ -5,9 +5,9 @@ import { OAuthGithubConfigLink } from "src/utils/githubSetupLink";
 
 export interface GithubSyncSettingsProps {
   title: string;
-  message: string;
-  showButton: string;
-  settingsButton: string;
+  message?: string;
+  showButton?: string;
+  settingsButton?: string;
   PoolingFeedback?: React.ReactElement;
 }
 
@@ -23,24 +23,28 @@ export const GithubSyncSettings = ({
     <div className="card-light flex w-full flex-col items-start justify-start gap-6 rounded-2xl border bg-transparent p-5">
       <div className="flex w-full flex-col items-start justify-start gap-2">
         <p className="font-walsheim text-sm font-medium uppercase">{title}</p>
-        <p className="text-body-s font-walsheim font-normal text-spaceBlue-200">
-          {message}
-          &nbsp;
-          <button
-            className="text-body-s-bold cursor-pointer font-bold text-spacePurple-500"
-            onClick={openTutorialPanel}
-          >
-            {showButton}
-          </button>
-        </p>
+        {message ? (
+          <p className="text-body-s font-walsheim font-normal text-spaceBlue-200">
+            {message}
+            &nbsp;
+            <button
+              className="text-body-s-bold cursor-pointer font-bold text-spacePurple-500"
+              onClick={openTutorialPanel}
+            >
+              {showButton}
+            </button>
+          </p>
+        ) : null}
       </div>
       <div className="center flex w-full flex-col items-center gap-5 lg:flex-row">
-        <a href={OAuthGithubConfigLink} target="_blank" rel="noopener noreferrer" className="w-full">
-          <Button type={ButtonType.Secondary} size={ButtonSize.Sm} className="w-full">
-            <GithubLogo />
-            {settingsButton}
-          </Button>
-        </a>
+        {settingsButton ? (
+          <a href={OAuthGithubConfigLink} target="_blank" rel="noopener noreferrer" className="w-full">
+            <Button type={ButtonType.Secondary} size={ButtonSize.Sm} className="w-full">
+              <GithubLogo />
+              {settingsButton}
+            </Button>
+          </a>
+        ) : null}
         {PoolingFeedback ? PoolingFeedback : null}
       </div>
     </div>
