c2fbf4b26168a1bc45019344f481ca57d518261f
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-06 14:55:49 +0100

    ⬆️ extract logic into score dico

--- src/utils/calculateCompletionScore.ts
+++ src/utils/calculateCompletionScore.ts
@@ -3,28 +3,47 @@ import { UseGetMyProfileInfoResponse } from "src/api/me/queries";
 import { ArrayElement } from "src/types";
 
 type Channel = ArrayElement<NonNullable<UseGetMyProfileInfoResponse["contacts"]>>["channel"];
+type ScorableFields = "avatarUrl" | "login" | "location" | "bio" | "website" | "technologies";
+
+const userScoreDictionary: Record<ScorableFields | Channel, number> = {
+  avatarUrl: 5,
+  login: 15,
+  location: 10,
+  bio: 20,
+  website: 10,
+  technologies: 10,
+  EMAIL: 5,
+  TELEGRAM: 5,
+  WHATSAPP: 5,
+  TWITTER: 5,
+  DISCORD: 5,
+  LINKEDIN: 5,
+};
 
 function calculateUserCompletionScore(userProfile: UseGetMyProfileInfoResponse) {
-  const scoreByExistence = (fieldName: keyof typeof userProfile, score: number) => (userProfile[fieldName] ? score : 0);
+  const scoreByExistence = (fieldName: ScorableFields) => (userProfile[fieldName] ? userScoreDictionary[fieldName] : 0);
 
-  const scoreContact = (channel: Channel, score: number) =>
-    userProfile.contacts?.find(contact => contact.channel === channel && contact.contact) ? score : 0;
+  const scoreContact = (channel: Channel) =>
+    userProfile.contacts?.some(contact => contact.channel === channel && contact.contact)
+      ? userScoreDictionary[channel]
+      : 0;
 
-  const scoreTechnologies = (score: number) => (Object.keys(userProfile.technologies ?? {}).length ? score : 0);
+  const scoreTechnologies = () =>
+    Object.keys(userProfile.technologies ?? {}).length ? userScoreDictionary.technologies : 0;
 
   return (
-    scoreByExistence("avatarUrl", 5) +
-    scoreByExistence("login", 15) +
-    scoreByExistence("location", 10) +
-    scoreByExistence("bio", 20) +
-    scoreByExistence("website", 10) +
-    scoreContact("EMAIL", 5) +
-    scoreContact("TELEGRAM", 5) +
-    scoreContact("WHATSAPP", 5) +
-    scoreContact("TWITTER", 5) +
-    scoreContact("DISCORD", 5) +
-    scoreContact("LINKEDIN", 5) +
-    scoreTechnologies(10)
+    scoreByExistence("avatarUrl") +
+    scoreByExistence("login") +
+    scoreByExistence("location") +
+    scoreByExistence("bio") +
+    scoreByExistence("website") +
+    scoreContact("EMAIL") +
+    scoreContact("TELEGRAM") +
+    scoreContact("WHATSAPP") +
+    scoreContact("TWITTER") +
+    scoreContact("DISCORD") +
+    scoreContact("LINKEDIN") +
+    scoreTechnologies()
   );
 }
 
@@ -32,22 +51,54 @@ export type FormValuesProps = {
   avatarUrl: string;
 } & UserProfileInfo;
 
+type FormScorableFields =
+  | "avatarUrl"
+  | "githubHandle"
+  | "location"
+  | "bio"
+  | "website"
+  | "email"
+  | "telegram"
+  | "whatsapp"
+  | "twitter"
+  | "discord"
+  | "linkedin"
+  | "technologies";
+
+const formScoreDictionary: Record<FormScorableFields, number> = {
+  avatarUrl: 5,
+  githubHandle: 15,
+  location: 10,
+  bio: 20,
+  website: 10,
+  email: 5,
+  telegram: 5,
+  whatsapp: 5,
+  twitter: 5,
+  discord: 5,
+  linkedin: 5,
+  technologies: 10,
+};
+
 function calculateFormCompletionScore(formValues: FormValuesProps) {
-  const score = (value: string | number | null, score: number) => (value && value !== "" ? score : 0);
+  const score = (fieldName: FormScorableFields) =>
+    formValues[fieldName] && formValues[fieldName] !== "" ? formScoreDictionary[fieldName] : 0;
+  const scoreTechnologies = () =>
+    Object.keys(formValues.technologies ?? {}).length ? formScoreDictionary.technologies : 0;
 
   return (
-    score(formValues?.avatarUrl, 5) +
-    score(formValues?.githubHandle, 15) +
-    score(formValues?.location, 10) +
-    score(formValues?.bio, 20) +
-    score(formValues?.website, 10) +
-    score(formValues?.email, 5) +
-    score(formValues?.telegram, 5) +
-    score(formValues?.whatsapp, 5) +
-    score(formValues?.twitter, 5) +
-    score(formValues?.discord, 5) +
-    score(formValues?.linkedin, 5) +
-    score(Object.keys(formValues?.technologies).length, 10)
+    score("avatarUrl") +
+    score("githubHandle") +
+    score("location") +
+    score("bio") +
+    score("website") +
+    score("email") +
+    score("telegram") +
+    score("whatsapp") +
+    score("twitter") +
+    score("discord") +
+    score("linkedin") +
+    scoreTechnologies()
   );
 }
 
