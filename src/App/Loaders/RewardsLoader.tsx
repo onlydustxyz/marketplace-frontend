388203e8589539359e62959b282b55ba3cb2329d
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-08 17:16:41 +0100

    🔧 fix loader UI

--- src/App/Loaders/RewardsLoader.tsx
+++ src/App/Loaders/RewardsLoader.tsx
@@ -1,23 +1,28 @@
+import Background, { BackgroundRoundedBorders } from "src/components/Background";
 import Skeleton from "src/components/Skeleton";
+import useScrollRestoration from "src/pages/Projects/AllProjects/useScrollRestoration";
 
 export default function RewardLoader() {
+  const { ref } = useScrollRestoration();
   return (
-    <div className="h-full">
-      <div className="mx-auto flex max-w-7xl flex-col gap-6 px-4 py-4 xl:p-8">
-        <div className="w-32">
-          <Skeleton variant="title" />
+    <Background ref={ref} roundedBorders={BackgroundRoundedBorders.Full}>
+      <div className="h-full">
+        <div className="mx-auto flex max-w-7xl flex-col gap-6 px-4 py-4 xl:p-8">
+          <div className="w-32">
+            <Skeleton variant="title" />
+          </div>
+          <Skeleton variant="invoice" />
+          <div className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-6">
+            <Skeleton variant="earnedRewards" />
+            <Skeleton variant="earnedRewards" />
+            <Skeleton variant="earnedRewards" />
+            <Skeleton variant="earnedRewards" />
+            <Skeleton variant="earnedRewards" />
+            <Skeleton variant="earnedRewards" />
+          </div>
+          <Skeleton variant="rewards" />
         </div>
-        <Skeleton variant="invoice" />
-        <div className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-6">
-          <Skeleton variant="earnedRewards" />
-          <Skeleton variant="earnedRewards" />
-          <Skeleton variant="earnedRewards" />
-          <Skeleton variant="earnedRewards" />
-          <Skeleton variant="earnedRewards" />
-          <Skeleton variant="earnedRewards" />
-        </div>
-        <Skeleton variant="rewards" />
       </div>
-    </div>
+    </Background>
   );
 }
--- src/pages/Projects/AllProjects/AllProjectsLoading/index.tsx
+++ src/pages/Projects/AllProjects/AllProjectsLoading/index.tsx
@@ -2,7 +2,7 @@ import Skeleton from "src/components/Skeleton";
 
 export default function AllProjectLoading() {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex w-full flex-col space-y-4">
       <Skeleton variant="header" />
       <Skeleton variant="card" />
       <Skeleton variant="card" />
