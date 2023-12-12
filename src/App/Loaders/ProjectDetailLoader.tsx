45e37cc778963aeb19da08772eac934591358e17
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2023-12-12 11:20:17 +0100

    refacto Skeleton (#1555)

--- src/App/Loaders/ProjectDetailLoader.tsx
+++ src/App/Loaders/ProjectDetailLoader.tsx
@@ -1,15 +1,42 @@
-import Skeleton from "src/components/Skeleton";
+import Skeleton, { SkeletonVariant } from "src/components/Skeleton";
+import { viewportConfig } from "src/config";
+import { useMediaQuery } from "usehooks-ts";
+import { ProjectRewardsRoutePaths, ProjectRoutePaths, RoutePaths } from "..";
+import { useMatch } from "react-router-dom";
+import { ContributionTableSkeleton } from "src/components/Contribution/ContributionTableSkeleton";
 
 export default function ProjectDetailsLoader() {
+  const isXl = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.xl}px)`);
+  const matches = {
+    isProjectOverview: useMatch(`${RoutePaths.ProjectDetails}/${ProjectRoutePaths.Overview}`),
+    isProjectContributors: useMatch(`${RoutePaths.ProjectDetails}/${ProjectRoutePaths.Contributors}`),
+    isProjectRewards: useMatch(`${RoutePaths.ProjectDetails}/${ProjectRoutePaths.Rewards}`),
+    isProjectRewardForm: useMatch(`${RoutePaths.ProjectDetails}/${ProjectRewardsRoutePaths.New}`),
+    isProjectContributions: useMatch(`${RoutePaths.ProjectDetails}/${ProjectRoutePaths.Contributions}`),
+  };
+
+  const renderSkeleton = (variant: SkeletonVariant) => <Skeleton variant={variant} />;
+
   return (
     <div className="flex w-full flex-1 flex-col gap-4 overflow-hidden pt-4 xl:h-0 xl:flex-row xl:gap-2 xl:p-6 xl:pt-0">
-      <div className="flex w-full shrink-0 flex-col gap-6 bg-white/4 bg-noise-medium p-6 font-walsheim xl:w-80 xl:rounded-l-2xl">
-        <Skeleton variant="projectSidebar" />
-      </div>
+      {isXl && (
+        <div className="flex w-full shrink-0 flex-col gap-6 bg-white/4 bg-noise-medium p-6 font-walsheim xl:w-80 xl:rounded-l-2xl">
+          {renderSkeleton("projectSidebar")}
+        </div>
+      )}
       <div className="h-full w-full overflow-y-auto bg-space bg-no-repeat scrollbar-thin scrollbar-thumb-white/12 scrollbar-thumb-rounded scrollbar-w-1.5 lg:rounded-r-3xl">
         <div className="h-full">
           <div className="mx-auto flex h-full max-w-7xl flex-1 flex-col gap-6 px-4 py-6 xl:px-8">
-            <Skeleton variant="projectOverview" />
+            {matches.isProjectOverview ? renderSkeleton("projectOverview") : null}
+            {matches.isProjectContributors ? (
+              <>
+                <div className="max-w-[15%]">{renderSkeleton("counter")}</div>
+                {renderSkeleton("contributorList")}
+              </>
+            ) : null}
+            {matches.isProjectRewards ? renderSkeleton("projectRewards") : null}
+            {matches.isProjectRewardForm ? renderSkeleton("projectRewardForm") : null}
+            {matches.isProjectContributions ? <ContributionTableSkeleton /> : null}
           </div>
         </div>
       </div>
---
+++ src/components/Skeleton/Placeholders/ProjectOverviewMobilePlaceholder.tsx
@@ -0,0 +1,82 @@
+export default function ProjectOverviewMobilePlaceholder() {
+  return (
+    <svg className="relative w-full animate-pulse" viewBox="0 0 656 869" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_95_1122)">
+        <rect width="160" height="36" rx="18" fill="#171D44" />
+        <rect y="57" width="312" height="26" rx="13" fill="#171D44" />
+        <rect x="344" y="57" width="312" height="26" rx="13" fill="#171D44" />
+        <rect y="107" width="656" height="152" rx="16" fill="#0E0D2E" />
+        <rect x="24" y="123" width="72" height="72" rx="8" fill="#171D44" />
+        <rect x="112" y="129" width="200" height="28" rx="14" fill="#171D44" />
+        <rect x="112" y="165" width="130" height="24" rx="12" fill="#171D44" />
+        <rect x="24" y="211" width="608" height="12" rx="6" fill="#171D44" />
+        <rect x="24" y="231" width="140" height="12" rx="6" fill="#171D44" />
+        <g filter="url(#filter0_d_95_1122)">
+          <rect y="275" width="656" height="594" rx="16" fill="#0E0D2E" />
+          <path d="M632 330H24V332H632V330Z" fill="#F3F0EE" fillOpacity="0.08" mask="url(#path-11-inside-1_95_1122)" />
+          <rect x="24" y="299" width="24" height="24" rx="12" fill="#171D44" />
+          <rect x="60" y="303" width="220" height="16" rx="8" fill="#171D44" />
+          <rect x="611" y="301" width="21" height="20" rx="10" fill="#171D44" />
+          <path d="M632 512H24V514H632V512Z" fill="#171D44" mask="url(#path-16-inside-2_95_1122)" />
+          <rect x="48" y="373" width="32" height="32" rx="10" fill="#171D44" />
+          <rect x="88" y="371" width="120" height="16" rx="8" fill="#171D44" />
+          <rect x="516" y="371" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="572" y="371" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="88" y="395" width="80" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="423" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="441" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="459" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="477" width="140" height="12" rx="6" fill="#171D44" />
+          <path d="M632 678H24V680H632V678Z" fill="#171D44" mask="url(#path-27-inside-3_95_1122)" />
+          <rect x="48" y="539" width="32" height="32" rx="10" fill="#171D44" />
+          <rect x="88" y="537" width="120" height="16" rx="8" fill="#171D44" />
+          <rect x="516" y="537" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="572" y="537" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="88" y="561" width="80" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="589" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="607" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="625" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="643" width="140" height="12" rx="6" fill="#171D44" />
+          <path d="M632 844H24V846H632V844Z" fill="#171D44" mask="url(#path-38-inside-4_95_1122)" />
+          <rect x="48" y="705" width="32" height="32" rx="10" fill="#171D44" />
+          <rect x="88" y="703" width="120" height="16" rx="8" fill="#171D44" />
+          <rect x="516" y="703" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="572" y="703" width="36" height="16" rx="8" fill="#171D44" />
+          <rect x="88" y="727" width="80" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="755" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="773" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="791" width="560" height="12" rx="6" fill="#171D44" />
+          <rect x="48" y="809" width="140" height="12" rx="6" fill="#171D44" />
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_95_1122"
+          x="-64"
+          y="219"
+          width="784"
+          height="722"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="8" />
+          <feGaussianBlur stdDeviation="32" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.32 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_95_1122" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_95_1122" result="shape" />
+        </filter>
+        <clipPath id="clip0_95_1122">
+          <rect width="656" height="869" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
