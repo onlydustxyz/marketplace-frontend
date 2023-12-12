c616ad3f622ba389c142bc12b299a5404543c6d7
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-12 11:46:32 +0100

    ⚡️ handle cover onclick

--- src/App/Stacks/ContributorProfileSidePanel/Header.tsx
+++ src/App/Stacks/ContributorProfileSidePanel/Header.tsx
@@ -4,6 +4,7 @@ import HeaderCoverButton from "./EditView/HeaderCoverButton";
 import { UserProfile } from "src/api/Users/queries";
 import { UseGetMyProfileInfoResponse } from "src/api/me/queries";
 import ProfilePicture from "./ProfilePicture";
+import { useState } from "react";
 
 type Props = {
   profile: UserProfile | UseGetMyProfileInfoResponse;
@@ -16,8 +17,11 @@ type Props = {
 export default function Header({ profile, editable, onChange, onChangeProfilePicture, rounded }: Props) {
   const { avatarUrl, cover } = profile;
 
+  const [coverColor, setCoverColor] = useState(cover);
+
   const handleClick = (value: ProfileCover) => {
-    onChange && onChange(value);
+    onChange?.(value);
+    setCoverColor(value);
   };
 
   return (
@@ -26,10 +30,10 @@ export default function Header({ profile, editable, onChange, onChangeProfilePic
         className={cn(
           "h-24 w-full shrink-0 bg-cover",
           {
-            "bg-profile-blue": cover === ProfileCover.Blue,
-            "bg-profile-cyan": cover === ProfileCover.Cyan,
-            "bg-profile-magenta": cover === ProfileCover.Magenta,
-            "bg-profile-yellow": cover === ProfileCover.Yellow,
+            "bg-profile-blue": coverColor === ProfileCover.Blue,
+            "bg-profile-cyan": coverColor === ProfileCover.Cyan,
+            "bg-profile-magenta": coverColor === ProfileCover.Magenta,
+            "bg-profile-yellow": coverColor === ProfileCover.Yellow,
           },
           {
             "rounded-xl": rounded,
@@ -39,9 +43,13 @@ export default function Header({ profile, editable, onChange, onChangeProfilePic
         {editable && (
           <div className="flex h-full w-full items-center justify-center bg-black/30">
             <div className="flex h-12 w-fit flex-row items-center justify-center gap-3 rounded-full border border-greyscale-50/8 bg-white/8 px-5">
-              <HeaderCoverButton active={cover === ProfileCover.Cyan} cover={ProfileCover.Cyan} onClick={handleClick} />
               <HeaderCoverButton
-                active={cover === ProfileCover.Magenta}
+                active={coverColor === ProfileCover.Cyan}
+                cover={ProfileCover.Cyan}
+                onClick={handleClick}
+              />
+              <HeaderCoverButton
+                active={coverColor === ProfileCover.Magenta}
                 cover={ProfileCover.Magenta}
                 onClick={handleClick}
               />
@@ -50,7 +58,11 @@ export default function Header({ profile, editable, onChange, onChangeProfilePic
                 cover={ProfileCover.Yellow}
                 onClick={handleClick}
               />
-              <HeaderCoverButton active={cover === ProfileCover.Blue} cover={ProfileCover.Blue} onClick={handleClick} />
+              <HeaderCoverButton
+                active={coverColor === ProfileCover.Blue}
+                cover={ProfileCover.Blue}
+                onClick={handleClick}
+              />
             </div>
           </div>
         )}
