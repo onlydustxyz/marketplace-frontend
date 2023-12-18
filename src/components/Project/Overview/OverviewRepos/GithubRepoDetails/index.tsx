f8ed80ddbd7216bd673395e0463bc4d15b42b61b
Alexis Benoliel <alexbeno@users.noreply.github.com>
2023-12-18 10:20:14 +0100

    E 946 build profile panel for projects (#1578)

---
+++ src/components/Project/Overview/OverviewRepos/GithubRepoDetails/index.tsx
@@ -0,0 +1,9 @@
+import { ComponentProps } from "react";
+import View from "./View";
+
+export default function GithubRepoDetails({
+  githubRepo,
+  withBg = true,
+}: Partial<ComponentProps<typeof View>> & { withBg?: boolean }) {
+  return githubRepo ? <View githubRepo={githubRepo} withBg={withBg} /> : null;
+}
--- src/libs/react-stack/components/RegisterStack.tsx
+++ src/libs/react-stack/components/RegisterStack.tsx
@@ -1,10 +1,13 @@
 import useStackRegister from "../hooks/useStackRegister";
 
-import { RegisterStackProps, StacksParams } from "../types/Stack";
+import { Options, RegisterStackProps, StacksParams } from "../types/Stack";
 import { Stack } from "./Stack";
 
-export function RegisterStack<P extends StacksParams>(props: RegisterStackProps<P>) {
+export function RegisterStack<P extends StacksParams>({
+  option,
+  ...props
+}: RegisterStackProps<P> & { option?: Options }) {
   const ref = useStackRegister(props);
 
-  return <Stack stackRef={ref} />;
+  return <Stack stackRef={ref} option={option} />;
 }
