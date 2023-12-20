951d7737be0a6d9f6a0b49cad97ca75b99d3264b
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-12-20 16:52:48 +0100

    feat: next config

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //output: "export", // Outputs a Single-Page Application (SPA).
+  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  poweredByHeader: false,
+};
+
+export default nextConfig;
