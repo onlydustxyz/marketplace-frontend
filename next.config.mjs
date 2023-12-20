ea1acf3c2621945d6939a10af1464b94449cacb4
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-12-20 15:22:48 +0100

    refactor: removed powered by header

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   //output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
