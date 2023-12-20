1902c9e112ebeacd6c8c1782cddc584ee2287c40
Alexis Benoliel <alexbeno@users.noreply.github.com>
2023-12-20 15:21:31 +0100

    Remove e2e folder (#1595)

--- src/utils/clickHandler.ts
+++ src/utils/clickHandler.ts
@@ -1,10 +1,5 @@
 import { MouseEvent } from "react";
 
-export const linkClickHandlerFactory = (url: string) => (e: MouseEvent<HTMLElement>) => {
-  e.preventDefault();
-  window?.open(url, "_blank")?.focus();
-};
-
 export const copyClickHandlerFactory = (value: string, callback?: () => void) => (e: MouseEvent<HTMLElement>) => {
   e.preventDefault();
   navigator.clipboard.writeText(value);
