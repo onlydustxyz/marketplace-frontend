0487d95e93a6a0f648aa0c723ba9cb9e87fd9375
Alexis Benoliel <alexbeno@users.noreply.github.com>
2023-12-20 16:36:12 +0100

    Design update (#1610)

--- src/components/New/Buttons/ChoiceButton/ChoiceButton.tsx
+++ src/components/New/Buttons/ChoiceButton/ChoiceButton.tsx
@@ -16,6 +16,7 @@ export const ChoiceButton = ({ choices, defaultOption, icon, ...rest }: ChoiceBu
   }, [choices, defaultOption]);
 
   const availableChoice = useMemo(() => choices.filter(choice => !choice.disabled), [choices]);
+  const otherChoices = choices.filter(choice => choice.name !== defaultChoice?.name);
 
   const isChoiceButton = availableChoice.length > 1;
 
@@ -25,14 +26,10 @@ export const ChoiceButton = ({ choices, defaultOption, icon, ...rest }: ChoiceBu
 
   return (
     <Menu as="div" className="relative z-20 inline-block">
-      <div
-        className={cn("flex items-center justify-start gap-px drop-shadow-bottom-sm", {
-          "gap-[0.5px]": rest.size === ButtonSize.Sm || rest.size === ButtonSize.Xs,
-        })}
-      >
+      <div className={cn("flex items-center justify-start gap-px shadow-button")}>
         <Button
           {...rest}
-          className={cn("focus:outline-none", {
+          className={cn("shadow-none !outline-none drop-shadow-none focus:outline-none active:outline-none", {
             "gap-[0.5px]": rest.size === ButtonSize.Sm || rest.size === ButtonSize.Xs,
             "rounded-r-none filter-none": isChoiceButton,
           })}
@@ -45,7 +42,12 @@ export const ChoiceButton = ({ choices, defaultOption, icon, ...rest }: ChoiceBu
           <Menu.Button as="div">
             <Button
               {...rest}
-              className={cn("rounded-l-none filter-none focus:outline-none")}
+              className={cn(
+                "rounded-l-none px-2 shadow-none !outline-none filter-none focus:outline-none active:outline-none",
+                {
+                  "px-3": rest.size !== ButtonSize.Sm && rest.size !== ButtonSize.Xs,
+                }
+              )}
               disabled={rest.disabled || defaultChoice.disabled}
             >
               {icon || (
@@ -67,13 +69,13 @@ export const ChoiceButton = ({ choices, defaultOption, icon, ...rest }: ChoiceBu
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 top-full z-30 mt-2 w-56 origin-top-right overflow-hidden rounded-md border-card-background-light bg-card-background-base py-2 shadow-lg focus:outline-none">
-            {choices.map(choice => (
+          <Menu.Items className="absolute right-0 top-full z-30 mt-2 w-56 origin-top-right overflow-hidden rounded-xl border border-card-background-light bg-greyscale-900 py-2 shadow-light focus:outline-none">
+            {otherChoices.map(choice => (
               <Menu.Item
                 as="div"
                 key={choice.name}
                 className={cn(
-                  "flex cursor-pointer flex-row items-center gap-3 px-4 py-2 font-walsheim text-sm transition-colors hover:bg-spaceBlue-800/50",
+                  "text-body-s-bold flex cursor-pointer flex-row items-center gap-3 px-4 py-2 font-walsheim transition-colors hover:bg-greyscale-800/50",
                   {
                     "opacity-50 hover:bg-transparent": choice.disabled,
                   }
