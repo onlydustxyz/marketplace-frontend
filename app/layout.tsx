ea0110456e6a1fe1aa91c52c42edd2fca19d6002
haydencleary <5160414+haydencleary@users.noreply.github.com>
2023-12-22 10:26:52 +0100

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import config from "src/config";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://app.onlydust.com"),
   title: "Only Dust — Forge your developer legacy",
   description:
     "Contribute to innovative projects, refine your skills and create a lasting impact in the developer community. The stars are within your reach.",
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
       "Contribute to innovative projects, refine your skills and create a lasting impact in the developer community. The stars are within your reach.",
     type: "website",
     siteName: "OnlyDust",
-    url: "https://app.onlydust.xyz/",
+    url: "https://app.onlydust.com",
     images: [
       {
         url: "https://onlydust-app-images.s3.eu-west-1.amazonaws.com/thumbnail.png",
@@ -22,15 +23,15 @@ export const metadata: Metadata = {
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image",
+    site: "@OnlyDust_xyz",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@OnlyDust_xyz" />
-      </head>
       <body>
         <div id="root">{children}</div>
       </body>
