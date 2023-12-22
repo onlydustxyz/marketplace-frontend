596e763b6fa1feac6feb5fb2638ab619b1334f47
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2023-12-22 10:21:30 +0100

    fix: GTM server (#1619)

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,22 +1,14 @@
 "use client";
 
 import dynamic from "next/dynamic";
-// import TagManager from "react-gtm-module";
 import "remixicon/fonts/remixicon.css";
 import "src/assets/css/index.css";
 import "src/assets/fonts/Alfreda/stylesheet.css";
 import "src/assets/fonts/Belwe/stylesheet.css";
 import "src/assets/fonts/GTWalsheimPro/stylesheet.css";
-// import config from "src/config";
 import "src/datadog";
 
 const Providers = dynamic(() => import("./providers"), { ssr: false });
-
-// if (config.GTM_ID) {
-//   TagManager.initialize({
-//     gtmId: config.GTM_ID,
-//   });
-// }
 
 export default function Page() {
   return <Providers />;
