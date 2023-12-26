e94aa5584ceff464452d547120ea46444b7afeff
Sami Lafrance <sami.lepays@gmail.com>
2023-12-26 11:31:45 +0100

    Improve layout for contribution tooltip (#1580)

--- src/components/Contribution/ContributionBadge.tsx
+++ src/components/Contribution/ContributionBadge.tsx
@@ -1,18 +1,79 @@
-import { cn } from "src/utils/cn";
-
-import { ContributionIcon, variants as contributionIconVariants } from "src/components/Contribution/ContributionIcon";
-import Contributor from "src/components/Contributor";
 import ExternalLink from "src/components/ExternalLink";
-import Tooltip, { TooltipPosition, Variant } from "src/components/Tooltip";
+import Tooltip, { PaddingVariant, TooltipPosition, Variant } from "src/components/Tooltip";
 import { useAuth } from "src/hooks/useAuth";
 import { useIntl } from "src/hooks/useIntl";
+import { ContributionIcon, variants as contributionIconVariants } from "src/components/Contribution/ContributionIcon";
 import ArrowRightUpLine from "src/icons/ArrowRightUpLine";
+
 import { Contribution, GithubContributionType, GithubPullRequestStatus } from "src/types";
+import Contributor from "../Contributor";
+import { cn } from "src/utils/cn";
+
+interface ContributionBadgeProps {
+  contribution: Pick<
+    Contribution,
+    "githubNumber" | "githubTitle" | "githubBody" | "githubHtmlUrl" | "githubAuthor" | "githubStatus" | "type"
+  > & { status?: Contribution["status"] };
+  withTooltip?: boolean;
+  asLink?: boolean;
+  size?: ContributionBadgeSizes;
+  tooltipProps?: React.ComponentProps<typeof Tooltip>;
+  showExternal?: boolean;
+}
+
+interface ContributionBadgeContentProps extends Omit<ContributionBadgeProps, "tooltipProps"> {
+  isExternal: boolean;
+  tooltipId: string;
+}
 
 export enum ContributionBadgeSizes {
   Xs = "text-xs",
   Sm = "text-sm",
   Md = "text-base",
+}
+
+function ContributionBadgeContent({
+  contribution,
+  size,
+  withTooltip = false,
+  showExternal = false,
+  asLink = false,
+  isExternal = false,
+  tooltipId,
+}: ContributionBadgeContentProps) {
+  const { githubNumber, githubHtmlUrl, githubStatus, status, type } = contribution;
+
+  const Component = asLink ? "a" : "div";
+  const ComponentProps = asLink ? { href: githubHtmlUrl, target: "_blank", rel: "noopener noreferrer" } : {};
+
+  const statusClassnames =
+    status && githubStatus !== GithubPullRequestStatus.Draft
+      ? contributionIconVariants.contributionStatus[status]
+      : contributionIconVariants.status[type][
+          githubStatus as keyof typeof contributionIconVariants.status[GithubContributionType]
+        ];
+
+  return (
+    <Component
+      data-tooltip-id={withTooltip ? tooltipId : undefined}
+      className={cn(
+        "inline-flex w-auto items-center gap-1 rounded-full px-1 py-0.5 font-walsheim",
+        isExternal && showExternal ? "border border-dashed" : "border-0.5 border-solid",
+        {
+          "hover:bg-card-background-heavy": withTooltip || asLink,
+        },
+        statusClassnames
+      )}
+      {...ComponentProps}
+    >
+      <ContributionIcon type={type as GithubContributionType} status={githubStatus} contributionStatus={status} />
+
+      <div className="flex">
+        <span className={cn("leading-none", size)}>{githubNumber}</span>
+        {isExternal && showExternal ? <ArrowRightUpLine className="text-xs leading-none" /> : null}
+      </div>
+    </Component>
+  );
 }
 
 export function ContributionBadge({
@@ -25,24 +86,12 @@ export function ContributionBadge({
     variant: Variant.Default,
   },
   showExternal = false,
-}: {
-  contribution: Pick<
-    Contribution,
-    "githubNumber" | "githubTitle" | "githubBody" | "githubHtmlUrl" | "githubAuthor" | "githubStatus" | "type"
-  > & { status?: Contribution["status"] };
-  withTooltip?: boolean;
-  asLink?: boolean;
-  size?: ContributionBadgeSizes;
-  tooltipProps?: React.ComponentProps<typeof Tooltip>;
-  showExternal?: boolean;
-}) {
+}: ContributionBadgeProps) {
   const { T } = useIntl();
   const { githubUserId } = useAuth();
 
-  const { githubNumber, githubTitle, githubBody, githubHtmlUrl, githubAuthor, githubStatus, status, type } =
-    contribution;
-  const Component = asLink ? "a" : "div";
-  const ComponentProps = asLink ? { href: githubHtmlUrl, target: "_blank", rel: "noopener noreferrer" } : {};
+  const { githubNumber, githubTitle, githubBody, githubHtmlUrl, githubAuthor, githubStatus, type } = contribution;
+
   const isExternal = githubAuthor && githubUserId !== githubAuthor.githubUserId;
   const tooltipId = `${githubNumber}-${type}-${githubStatus}`;
 
@@ -52,63 +101,44 @@ export function ContributionBadge({
     [GithubContributionType.Issue]: T("contributions.tooltip.badgeIssue"),
   };
 
-  const statusClassnames =
-    status && githubStatus !== GithubPullRequestStatus.Draft
-      ? contributionIconVariants.contributionStatus[status]
-      : contributionIconVariants.status[type][
-          githubStatus as keyof typeof contributionIconVariants.status[GithubContributionType]
-        ];
+  const contentProps = {
+    contribution,
+    withTooltip,
+    asLink,
+    size,
+    showExternal,
+    isExternal,
+    tooltipId,
+  };
 
   return (
     <>
       {withTooltip ? (
-        <Tooltip id={tooltipId} clickable {...tooltipProps}>
-          <div className="flex flex-col gap-2 text-left">
-            {isExternal ? (
-              <div className="flex items-center text-sm">
-                <span className="text-spaceBlue-200">{tokens[type]}</span>
-                <Contributor className="ml-1 flex-row-reverse" contributor={githubAuthor} clickable />
-              </div>
-            ) : null}
-            <div className="flex gap-2">
-              <ContributionIcon
-                type={type as GithubContributionType}
-                status={githubStatus}
-                contributionStatus={status}
-              />
-              <div className="flex max-w-sm flex-col items-start justify-start gap-2">
-                <span className="line-clamp-2 text-sm font-medium leading-4">
-                  <ExternalLink
-                    url={githubHtmlUrl}
-                    text={`#${githubNumber} • ${githubTitle}`}
-                    anchorProps={{ className: "line-clamp-2 whitespace-pre-line" }}
-                  />
-                </span>
-                {githubBody ? <p className="line-clamp-2 break-all text-xs text-spaceBlue-200">{githubBody}</p> : null}
-              </div>
+        <Tooltip id={tooltipId} clickable padding={PaddingVariant.Large} {...tooltipProps}>
+          <div className="flex max-w-sm flex-col gap-2 text-left">
+            <div className="flex items-center justify-between gap-2">
+              <span className="line-clamp-2 text-sm font-medium leading-4">
+                <ExternalLink
+                  url={githubHtmlUrl}
+                  text={githubTitle}
+                  anchorProps={{ className: "line-clamp-2 whitespace-pre-line" }}
+                />
+              </span>
+
+              <ContributionBadgeContent {...contentProps} withTooltip={false} />
             </div>
+
+            <div className="flex items-center text-xs font-medium">
+              <span className="text-spaceBlue-200">{tokens[type]}</span>
+              <Contributor className="ml-1 flex-row-reverse" contributor={githubAuthor} clickable />
+            </div>
+
+            {githubBody ? <p className="line-clamp-2 break-all text-xs text-spaceBlue-200">{githubBody}</p> : null}
           </div>
         </Tooltip>
       ) : null}
 
-      <Component
-        data-tooltip-id={withTooltip ? tooltipId : undefined}
-        className={cn(
-          "inline-flex w-auto items-center gap-1 rounded-full px-1 py-0.5 font-walsheim",
-          isExternal && showExternal ? "border border-dashed" : "border-0.5 border-solid",
-          {
-            "hover:bg-card-background-heavy": withTooltip || asLink,
-          },
-          statusClassnames
-        )}
-        {...ComponentProps}
-      >
-        <ContributionIcon type={type as GithubContributionType} status={githubStatus} contributionStatus={status} />
-        <div className="flex">
-          <span className={cn("leading-none", size)}>{githubNumber}</span>
-          {isExternal && showExternal ? <ArrowRightUpLine className="text-xs leading-none" /> : null}
-        </div>
-      </Component>
+      <ContributionBadgeContent {...contentProps} />
     </>
   );
 }
