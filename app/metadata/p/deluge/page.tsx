f7c5f935bc6ac5593dae53459809212af82d60b6
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-28 17:35:14 +0100

    make v1 in not-found

--- app/metadata/p/deluge/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-/** temporary component to render custom image metada for projects */
-function ProjectImageMetadata() {
-  return <div></div>;
-}
-
-export default ProjectImageMetadata;
