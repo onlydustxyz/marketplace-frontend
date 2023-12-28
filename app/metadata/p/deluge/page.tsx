f1843e123f0fcb7f3f28de6c4ccd0a25e315394d
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-28 16:55:04 +0100

    Try vercel naming for metadata route

---
+++ app/metadata/p/deluge/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+/** temporary component to render custom image metada for projects */
+function ProjectImageMetadata() {
+  return <div></div>;
+}
+
+export default ProjectImageMetadata;
