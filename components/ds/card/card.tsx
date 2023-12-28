ecf93b256ce82f9f8c6f711dbec4e32c338a834b
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-28 17:23:57 +0100

    :recycle: refactor Card and Tag component using Tailwind variant

--- components/ds/card/card.tsx
+++ components/ds/card/card.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { ElementType } from "react";
 import { cn } from "src/utils/cn";
-import { getBorderClass } from "./card.utils";
 import { CardBorder } from "./card.types";
+import { tv } from "tailwind-variants";
+
+const cardVariant = tv({
+  base: "rounded-2xl font-walsheim",
+  variants: {
+    hasBackground: {
+      true: "bg-whiteFakeOpacity-2",
+      false: "",
+    },
+    isFullWidth: {
+      true: "w-full",
+      false: "",
+    },
+    hasPadding: {
+      true: "p-4 lg:p-6",
+      false: "",
+    },
+    cursor: {
+      true: "cursor-pointer",
+      false: "",
+    },
+    border: {
+      light: "border border-greyscale-50/8",
+      medium: "border border-greyscale-50/12",
+      multiColor: "border-multicolored before:rounded-2xl",
+    },
+  },
+});
 
 interface CardProps extends React.PropsWithChildren {
+  as?: ElementType;
   className?: string;
   dataTestId?: string;
   border?: CardBorder;
@@ -14,6 +42,7 @@ interface CardProps extends React.PropsWithChildren {
 }
 
 export default function Card({
+  as: Component = "div",
   className = "",
   dataTestId,
   border = "light",
@@ -24,22 +53,21 @@ export default function Card({
   children,
 }: CardProps) {
   return (
-    <div
+    <Component
       className={cn(
-        "rounded-2xl font-walsheim",
-        {
-          "bg-whiteFakeOpacity-2": hasBackground,
-          "w-full": isFullWidth,
-          "p-4 lg:p-6": hasPadding,
-          "cursor-pointer": !!onClick,
-        },
-        getBorderClass(border),
+        cardVariant({
+          hasBackground,
+          isFullWidth,
+          hasPadding,
+          cursor: !!onClick,
+          border,
+        }),
         className
       )}
       data-testid={dataTestId}
       onClick={onClick}
     >
       {children}
-    </div>
+    </Component>
   );
 }
--- components/ds/tag/tag.tsx
+++ components/ds/tag/tag.tsx
@@ -1,7 +1,32 @@
 import { cn } from "src/utils/cn";
 import React, { ElementType, PropsWithChildren } from "react";
 import { TagBorderColor, TagSize } from "./tag.types";
-import { getBorderColorClass, getSizeClass } from "./tag.utils";
+import { tv } from "tailwind-variants";
+
+const tagVariant = tv({
+  base: "w-fit shrink-0 overflow-hidden rounded-full p-px blur-0",
+  variants: {
+    size: {
+      small: "px-2 py-1 text-xs",
+      medium: "px-3 py-1.5 text-sm",
+      large: "px-4 py-1.5 text-sm",
+    },
+    borderColor: {
+      grey: "border border-greyscale-50/8 bg-white/2",
+      orange: "border border-orange-500 bg-white/2",
+      "multi-color":
+        "before:absolute before:-z-10 before:h-[calc(100dvh)] before:w-screen before:animate-spin-invert-slow before:rounded-full before:bg-multi-color-gradient",
+    },
+    isOpaque: {
+      true: "bg-spaceBlue-900",
+      false: "",
+    },
+  },
+  defaultVariants: {
+    size: "small",
+    borderColor: "grey",
+  },
+});
 
 type TagProps = PropsWithChildren<{
   as?: ElementType;
@@ -29,15 +54,12 @@ export default function Tag({
     <Component
       data-testId={testId}
       id={id}
-      className={cn("w-fit shrink-0 overflow-hidden rounded-full p-px blur-0", className)}
+      className={cn(tagVariant({ size, borderColor, isOpaque }), className)}
       onClick={onClick}
     >
       <div
         className={cn(
-          "xl:min-h-7 relative flex w-fit items-center justify-center gap-1 rounded-full font-walsheim font-normal text-white",
-          getSizeClass(size),
-          getBorderColorClass(borderColor, isOpaque),
-          { "bg-spaceBlue-900": borderColor === "multi-color" || isOpaque }
+          "xl:min-h-7 relative flex w-fit items-center justify-center gap-1 rounded-full font-walsheim font-normal text-white"
         )}
       >
         {children}
