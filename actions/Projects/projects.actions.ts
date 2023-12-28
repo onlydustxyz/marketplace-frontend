9a5382838a810ef130d91630804d092a2557887d
Alexis Benoliel <alexbeno@users.noreply.github.com>
2023-12-28 14:22:51 +0100

    Add migration folder and middleware.ts to redirect user when env var … (#1622)

---
+++ actions/Projects/projects.actions.ts
@@ -0,0 +1,8 @@
+import queries from "./projects-queries.actions";
+import { ProjectActionTags as tags } from "./projects-tags.actions.ts";
+
+export const ProjectsActions = {
+  queries,
+  tags,
+  mutations: {},
+};
---
+++ actions/Users/users-queries.actions.ts
@@ -0,0 +1,26 @@
+import { BaseQueriesOptions } from "../type.actions";
+import { BaseQueries } from "../base-queries.actions";
+import { components } from "../../src/__generated/api";
+import { ACTION_PATH } from "../path.actions";
+import { UsersActionTags } from "./users-tags.actions";
+
+export type UserProfile = components["schemas"]["PublicUserProfileResponse"];
+
+async function retrieveByGithubId(githubId: string, options?: BaseQueriesOptions) {
+  return BaseQueries<UserProfile>(ACTION_PATH.USER_PROFILE_BY_GITHUB_ID(githubId), {
+    provideTag: [UsersActionTags.all, UsersActionTags.details(githubId)],
+    ...(options || {}),
+  });
+}
+
+async function retrieveByGithubLogin(githubLogin: string, options?: BaseQueriesOptions) {
+  return BaseQueries<UserProfile>(ACTION_PATH.USER_PROFILE_BY_GITHUB_LOGIN(githubLogin), {
+    provideTag: [UsersActionTags.all, UsersActionTags.details(githubLogin)],
+    ...(options || {}),
+  });
+}
+
+export default {
+  retrieveByGithubId,
+  retrieveByGithubLogin,
+};
