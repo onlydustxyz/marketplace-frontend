a637d18d9b686255ce83b048009cad11196139a3
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-28 17:46:03 +0100

    make v1 in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import "remixicon/fonts/remixicon.css";
 import "src/assets/css/index.css";
 import "src/assets/fonts/Alfreda/stylesheet.css";
@@ -8,8 +7,6 @@ import "src/assets/fonts/Belwe/stylesheet.css";
 import "src/assets/fonts/GTWalsheimPro/stylesheet.css";
 import "src/datadog.ts";
 
-const Providers = dynamic(() => import("./v1/providers.tsx"), { ssr: false });
-
 export default function NotFoundPage() {
-  return <Providers />;
+  return <div>Notfound</div>;
 }
--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ export function middleware(request: NextRequest) {
   /** add a middleware to redirect to the root if the user tries to access the migration folder */
   if (request.nextUrl.pathname.startsWith("/migration")) {
     if (process.env.NEXT_PUBLIC_ALLOW_MIGRATION_FOLDER !== "true") {
+      console.log("REDIRECT USER TO HOME");
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
