eddb5922387bb84f07a166631b5fe2ac2db0f67e
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-28 17:57:40 +0100

    make v1 in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import "remixicon/fonts/remixicon.css";
 import "src/assets/css/index.css";
 import "src/assets/fonts/Alfreda/stylesheet.css";
@@ -7,6 +8,8 @@ import "src/assets/fonts/Belwe/stylesheet.css";
 import "src/assets/fonts/GTWalsheimPro/stylesheet.css";
 import "src/datadog.ts";
 
+const Providers = dynamic(() => import("./v1/providers.tsx"), { ssr: false });
+
 export default function NotFoundPage() {
-  return <div>Notfound</div>;
+  return <Providers />;
 }
