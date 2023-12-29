e293369db5c6f1f3c96e8913993d41661392c4a0
NeoxAzrot <sami.lafrance@akwai.co>
2023-12-29 11:54:37 +0100

    feat(ds): button

--- components/ds/button/button.tsx
+++ components/ds/button/button.tsx
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes, FC, PropsWithChildren } from "react";
 import { cn } from "src/utils/cn";
-import { ButtonVariants, buttonVariants } from "./variants/button.variants";
+import { ButtonVariants } from "./variants/button.variants";
 import { buttonPrimaryVariants } from "./variants/button-primary.variants";
 import { buttonSecondaryVariants } from "./variants/button-secondary.variants";
 import { buttonTertiaryVariants } from "./variants/button-tertiary.variants";
@@ -16,7 +16,6 @@ export const Button: FC<ButtonProps> = ({ htmlType = "button", className, childr
   return (
     <button
       className={cn(
-        buttonVariants({ ...props }),
         type === "primary" && buttonPrimaryVariants({ ...props }),
         type === "secondary" && buttonSecondaryVariants({ ...props }),
         type === "tertiary" && buttonTertiaryVariants({ ...props }),
--- components/ds/button/variants/button.variants.ts
+++ components/ds/button/variants/button.variants.ts
@@ -7,16 +7,19 @@ export const buttonVariants = tv({
   variants: {
     size: {
       xs: "h-6 gap-1 rounded-lg text-xs px-2 py-1",
-      sm: "h-8 gap-2 rounded-large text-sm px-4 py-2",
-      md: "h-12 gap-2 rounded-xl text-base px-4 py-3.5",
-      "md-rounded": "h-11 gap-2 rounded-full px-4 py-3",
-      "lg-low-height": "h-12 gap-3 rounded-xl px-4 py-3.5",
-      lg: "h-14 gap-3 rounded-xl px-6 py-4",
-      "lg-rounded": "h-11 gap-3 rounded-full px-6 py-3",
+      s: "h-8 gap-2 rounded-large text-sm px-4 py-2",
+      m: "h-12 gap-2 rounded-xl text-base px-4 py-3.5",
+      l: "h-14 gap-3 rounded-xl px-6 py-4",
     },
     width: {
       fit: "w-fit",
       full: "w-full",
+    },
+    rounded: {
+      lg: "rounded-lg",
+      large: "rounded-large",
+      xl: "rounded-xl",
+      full: "rounded-full",
     },
     type: {
       primary: "",
@@ -49,22 +52,22 @@ export const buttonVariants = tv({
     },
     {
       iconOnly: true,
-      size: "sm",
+      size: "s",
       class: "h-8 w-8 p-2",
     },
     {
       iconOnly: true,
-      size: ["md", "lg-low-height"],
+      size: "m",
       class: "h-12 w-12 p-3.5",
     },
     {
       iconOnly: true,
-      size: "lg",
+      size: "l",
       class: "h-14 w-14 p-4",
     },
   ],
   defaultVariants: {
-    size: "md",
+    size: "m",
     width: "fit",
     type: "primary",
     accentColor: "purple",
