6cf260d32542868de0c41f390e8aebb44e61d4a9
Mehdi Hamri <pixelfact.company@gmail.com>
2023-12-29 09:18:37 +0100

    testing some configs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ function getCommitHash() {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  //distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   poweredByHeader: false,
   env: {
