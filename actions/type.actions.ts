c9e823fdc3b936ba594f1cd9ea06328565032f51
Alexis Benoliel <alexbenoliel@gmail.com>
2023-12-29 16:27:45 +0100

    Add query params in base queries

--- actions/type.actions.ts
+++ actions/type.actions.ts
@@ -1,12 +1,16 @@
-export interface BaseQueriesOptions extends Omit<RequestInit, "next" | "body"> {
+export type BaseQueriesDefaultParams = Record<string, string>;
+export interface BaseQueriesOptions<PARAMS extends BaseQueriesDefaultParams = BaseQueriesDefaultParams>
+  extends Omit<RequestInit, "next" | "body"> {
   provideTag?: string[];
   revalidateTag?: string[];
   onSuccess?: () => void;
   onError?: () => void;
   revalidate?: number | false;
+  params?: PARAMS;
 }
 
-export interface BaseMutationOptions extends Omit<BaseQueriesOptions, "body"> {
+export interface BaseMutationOptions<PARAMS extends BaseQueriesDefaultParams = BaseQueriesDefaultParams>
+  extends Omit<BaseQueriesOptions<PARAMS>, "body"> {
   provideTag?: string[];
   revalidateTag?: string[];
   onSuccess?: () => void;
