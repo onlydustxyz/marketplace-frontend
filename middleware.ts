0030bf55e417a05599589e7677191aabb0966cfd
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-02 14:13:59 +0100

    hide secret in develop and comment migration middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  /** add a middleware to redirect to the root if the user tries to access the migration folder */
-  if (request.nextUrl.pathname.startsWith("/migration")) {
-    if (process.env.NEXT_PUBLIC_ALLOW_MIGRATION_FOLDER !== "true") {
-      console.log("REDIRECT USER TO HOME");
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-}
-// middleware.js
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
+// TODO enable this while migrationg pages
+// export function middleware(request: NextRequest) {
+//   /** add a middleware to redirect to the root if the user tries to access the migration folder */
+//   if (request.nextUrl.pathname.startsWith("/migration")) {
+//     if (process.env.NEXT_PUBLIC_ALLOW_MIGRATION_FOLDER !== "true") {
+//       console.log("REDIRECT USER TO HOME");
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
+//   }
+// }
 export default withMiddlewareAuthRequired();
 
 export const config = {
