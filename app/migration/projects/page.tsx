88f6f8a4dba5870615cfe67c3e48d1714857d9de
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-02 11:00:00 +0100

    Loadmore

--- app/migration/projects/page.tsx
+++ app/migration/projects/page.tsx
@@ -2,6 +2,7 @@ import { Flex } from "components/layout/flex/flex";
 import { Typography } from "components/layout/typography/typography";
 import { ListProjectsParams, ProjectActions_listProjects } from "../../../actions/Projects/projects-queries.actions.ts";
 import React from "react";
+import { LoadMore } from "@/components/layout/api/load-more/load-more.tsx";
 
 async function ProjectsPage() {
   const paramsObject = {
@@ -17,25 +18,22 @@ async function ProjectsPage() {
       params,
     });
 
-    return {
-      pagination: projects,
-      element: [
-        <></>,
-        // <LoadMore
-        //   key={projects.nextPageIndex}
-        //   onLoadMore={() => getProjects({ ...paramsObject, pageIndex: projects.nextPageIndex })}
-        //   loading={false}
-        // >
-        //   <>
-        //     {projects.projects.map(project => (
-        //       <p className="bg-red-300 p-8" key={project.name}>
-        //         {project.name}
-        //       </p>
-        //     ))}
-        //   </>
-        // </LoadMore>,
-      ],
-    };
+    return [
+      ...projects.projects.map(project => (
+        <p className="bg-green-300 p-8" key={project.name}>
+          {project.name}
+        </p>
+      )),
+      <LoadMore
+        key={params.pageIndex}
+        params={{
+          ...params,
+          pageIndex: projects.nextPageIndex,
+        }}
+        hasMore={projects.hasMore}
+        onFetchMore={getProjects}
+      />,
+    ];
   }
 
   const projects = await getProjects(paramsObject);
@@ -43,12 +41,7 @@ async function ProjectsPage() {
   return (
     <Flex>
       <Typography variant="title-xl">ProjectsPage</Typography>
-      <div className="flex grow flex-col gap-5">
-        {projects.element}
-        {/* <InfiniteScrollProvider {...projects.pagination} onFetchMore={getProjects} pageSize={10}>
-          <LabsPagination />
-        </InfiniteScrollProvider> */}
-      </div>
+      <div className="flex grow flex-col gap-5">{projects}</div>
     </Flex>
   );
 }
