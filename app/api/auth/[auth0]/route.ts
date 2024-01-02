165f4e618a08b2d4a4a87b147497374165e9ff09
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-02 12:47:57 +0100

    configure connection name and add protected routes to middleware matcher

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,8 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    // Get the connection name from the Auth0 Dashboard
+    authorizationParams: { connection: process.env.NEXT_AUTH0_DEFAULT_CONNECTION_NAME },
+  }),
+});
--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,13 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ["/auth0-sample/:path*", "/rewards/:path*"],
+  matcher: [
+    "/auth0-sample/:path*",
+    "/rewards",
+    "/contributions",
+    "/p/create",
+    "/p/:path*/rewards",
+    "/p/:path*/rewards/new",
+    "/p/:path*/insights",
+  ],
 };
