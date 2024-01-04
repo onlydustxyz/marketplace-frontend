ee9ffdda52c87b5e3b39c57bdb8585ec489a5af3
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-04 12:32:48 +0100

    Add cli

--- tools/create-components.ts
+++ tools/create-components.ts
@@ -1,19 +1,41 @@
-import fs from "fs/promises";
+const fs = require("fs/promises");
+const prettier = require("prettier");
+const i = require("@inquirer/prompts");
+// @ts-ignore
+// import * as prettier from "prettier";
 
 const readline = require("readline/promises").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+type Informations = {
+  folder: string;
+  name: string;
+  path: string;
+  PascalName: string;
+  camelName: string;
+  options: {
+    variant: boolean;
+    loading: boolean;
+  };
+};
+
 // function capitalizeFirstLetter(str: string) {
 //   return str.charAt(0).toUpperCase() + str.slice(1);
 // }
 //
-// function kebabToPascal(str: string) {
-//   const words = str.split("-");
-//   const pascalCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-//   return pascalCaseWords.join("");
-// }
+function kebabToPascal(str: string) {
+  const words = str.split("-");
+  const pascalCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return pascalCaseWords.join("");
+}
+
+function kebabToCamel(str: string) {
+  const words = str.split("-");
+  const pascalCaseWords = words.map(word => word.charAt(0).toLowerCase() + word.slice(1));
+  return pascalCaseWords.join("");
+}
 
 async function exists(path: string) {
   try {
@@ -24,14 +46,97 @@ async function exists(path: string) {
   }
 }
 
-async function createIndex(name: string, folder: string, path: string) {
-  const createIndex = await fs.appendFile(`${path}/index.ts`, "coucou");
+// components/features
+
+async function createIndex({ name, path, PascalName }: Informations) {
+  await fs.appendFile(
+    `${path}/index.ts`,
+    prettier.format(
+      `
+    export * from "./${name}.tsx";
+
+    import ${PascalName} from "./${name}.tsx";
+
+    export * from "./${name}.tsx";
+    export type * from "./${name}.type.ts";
+
+    export default ${PascalName};
+  `,
+      { parser: "typescript" }
+    )
+  );
 }
-async function createFiles(name: string, folder: string, path: string) {}
+
+async function createComponent({ name, path, PascalName }: Informations) {
+  await fs.appendFile(
+    `${path}/${name}.tsx`,
+    prettier.format(
+      `
+        import { ${PascalName}Props } from "./${name}.type.ts";
+
+        export function ${PascalName}(props: ${PascalName}Props) {
+          return <div>${PascalName}</div>;
+        }
+
+        export default ${PascalName};
+  `,
+      { parser: "typescript" }
+    )
+  );
+}
+
+async function createProps({ name, path, PascalName, options: { variant } }: Informations) {
+  await fs.appendFile(
+    `${path}/${name}.type.ts`,
+    prettier.format(
+      `
+        import { PropsWithChildren } from "react";
+        ${variant ? `import { ${PascalName}Variant } from "./${name}.variant.ts"` : ""};
+
+        export interface ${PascalName}Props extends PropsWithChildren${variant ? `, ${PascalName}Variant` : ""} {
+          sample?: string;
+        }
+
+
+  `,
+      { parser: "typescript" }
+    )
+  );
+}
+
+async function createVariant({ name, path, PascalName, camelName }: Informations) {
+  await fs.appendFile(
+    `${path}/${name}.variant.ts`,
+    prettier.format(
+      `
+        import { tv, VariantProps } from "tailwind-variants";
+
+        export type ${PascalName}Variant = VariantProps<typeof ${camelName}Variant>;
+
+        export const ${camelName}Variant = tv({
+          base: "",
+          variants: {},
+          defaultVariants: {},
+        });
+
+  `,
+      { parser: "typescript" }
+    )
+  );
+}
+async function createFiles(informations: Informations) {
+  await createIndex(informations);
+  await createComponent(informations);
+  await createProps(informations);
+  if (informations.options.variant) {
+    await createVariant(informations);
+  }
+}
 
 async function promptName() {
-  const name = await readline.question("\x1b[34m Components Name ? ");
-  const folder = await readline.question("\x1b[34m Folder path ? ");
+  const name = await i.input({ message: "Components Name" });
+  const folder = await i.input({ message: "Folder path" });
+  const variant = await i.confirm({ message: "Add variant?" });
   const path = `${folder}/${name}`;
   const isExist = await exists(path);
 
@@ -40,12 +145,20 @@ async function promptName() {
     return promptName();
   }
 
-  return { folder, name, path };
+  return { folder, name, path, loading: false, variant: variant as boolean };
 }
 
 async function createComponents() {
-  const { folder, name, path } = await promptName();
-  await createFiles(folder, name, path);
+  const { folder, name, path, loading, variant } = await promptName();
+  await fs.mkdir(path);
+  await createFiles({
+    folder,
+    name,
+    path,
+    PascalName: kebabToPascal(name),
+    camelName: kebabToCamel(name),
+    options: { loading, variant },
+  });
 
   console.log("folder", folder, "name", name, "path", path);
   readline.close();
