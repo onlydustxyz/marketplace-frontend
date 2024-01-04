8b553233bec17361b87af88d6f6f506d0a374816
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-04 12:35:54 +0100

    Clean old

--- tools/create-components.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createComponents = void 0;
-function createComponents() {
-    console.log("creattte");
-}
-exports.createComponents = createComponents;
-createComponents();
--- tools/generators/React-client.js
+++
@@ -1,3 +0,0 @@
-const create = require("./Component");
-
-create.CreateComponent("", false);
