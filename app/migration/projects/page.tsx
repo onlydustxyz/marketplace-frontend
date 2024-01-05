405e9a97c64a792be1d6c9a18c9ab9cbf0e72cc0
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-05 15:15:39 +0100

    WIP project page layout

--- app/migration/projects/page.tsx
+++ app/migration/projects/page.tsx
@@ -21,20 +21,40 @@ function ProjectsPage() {
 
   const projects = data?.pages?.flatMap(({ projects }) => projects) ?? [];
   return (
-    <Flex direction="col">
-      <Typography variant="title-xl">ProjectsPage</Typography>
-      <div className="flex w-[840px] grow flex-col gap-5">
-        <ProjectCardLoading />
-        {projects.map(project => (
-          <ProjectCard
-            key={project.id}
-            project={project}
-            isUserProjectLead={isUserProjectLead(project, githubUserId)}
-          />
-        ))}
+    <>
+      <div className="flex max-w-7xl flex-col gap-6 px-4 py-4 md:mx-auto md:px-12 xl:pb-8 xl:pt-12">
+        <Flex className="w-full gap-6" direction="col">
+          <div className="col-span-2 row-auto self-start bg-blue-500">SEARCH</div>
+          <Flex className="w-full gap-6" direction="row">
+            <Flex className="w-full  gap-4 bg-green-500" direction="col">
+              <div className="w-full self-start bg-green-300">CREATE</div>
+              <div className="w-full self-start bg-green-300">FILTERS</div>
+            </Flex>
+            <Flex className="w-full  gap-5 bg-red-500" direction="col">
+              <Flex className="w-fullgap-5 bg-red-500" direction="row">
+                <div className="w-full self-start bg-red-300">COUNT</div>
+                <div className="w-full self-start bg-red-300">SORT</div>
+              </Flex>
+              <div className="w-full self-start bg-red-300">PROJECT</div>
+            </Flex>
+          </Flex>
+        </Flex>
       </div>
-      {hasNextPage ? <ShowMore onClick={fetchNextPage} loading={isFetchingNextPage} /> : null}
-    </Flex>
+      <Flex direction="col">
+        <Typography variant="title-xl">ProjectsPage</Typography>
+        <div className="flex w-[840px] grow flex-col gap-5">
+          <ProjectCardLoading />
+          {projects.map(project => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              isUserProjectLead={isUserProjectLead(project, githubUserId)}
+            />
+          ))}
+        </div>
+        {hasNextPage ? <ShowMore onClick={fetchNextPage} loading={isFetchingNextPage} /> : null}
+      </Flex>
+    </>
   );
 }
 
