3922e4cbe6691d190e8bdddd30357ea3b900576f
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-08 10:04:58 +0100

    Start filters

--- app/migration/projects/context/project.context.tsx
+++ app/migration/projects/context/project.context.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { createContext, useMemo } from "react";
+import { createContext, useMemo, useState } from "react";
 import ProjectApi from "src/api/Project";
 import { ProjectContextReturn, ProjectsContextProps } from "./project.context.type.ts";
+import { useInfiniteBaseQueryProps } from "../../../../src/api/useInfiniteBaseQuery.ts";
+import { Button } from "@/components/ds/button/button.tsx";
 
 export const ProjectsContext = createContext<ProjectContextReturn>({
   projects: [],
@@ -11,12 +13,26 @@ export const ProjectsContext = createContext<ProjectContextReturn>({
 });
 
 export function ProjectsContextProvider({ children }: ProjectsContextProps) {
+  const [filters, setFilters] = useState<useInfiniteBaseQueryProps["queryParams"]>([
+    ["mine", "true"],
+    ["sort", "RANK"],
+  ]);
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = ProjectApi.queries.useInfiniteList({
-    queryParams: [
-      ["mine", "true"],
-      ["sort", "RANK"],
-    ],
+    queryParams: filters,
   });
+
+  // const queryParams = useMemo(() => {
+  //   const params: useInfiniteBaseQueryProps["queryParams"] = [
+  //     technologies.length > 0 ? ["technologies", technologies.join(",")] : null,
+  //     sponsors.length > 0 ? ["sponsorId", sponsors.map(({ id }) => id).join(",")] : null,
+  //     search ? ["search", search] : null,
+  //     sorting ? ["sort", sorting] : null,
+  //     ownership ? ["mine", String(ownership === "Mine")] : null,
+  //   ].filter((param): param is string[] => Boolean(param));
+  //
+  //   return params;
+  // }, [technologies, sponsors, search, sorting, ownership]);
 
   const projects = useMemo(() => data?.pages?.flatMap(({ projects }) => projects) ?? [], [data]);
 
@@ -29,6 +45,16 @@ export function ProjectsContextProvider({ children }: ProjectsContextProps) {
         hasNextPage,
       }}
     >
+      <Button
+        onClick={() => {
+          setFilters([
+            ["mine", "false"],
+            ["sort", "RANK"],
+          ]);
+        }}
+      >
+        Fake change filter
+      </Button>
       {children}
     </ProjectsContext.Provider>
   );
