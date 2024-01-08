3c6b5ab678ade275704f7c853442a8c6a96852ad
Alexis Benoliel <alexbenoliel@gmail.com>
2024-01-08 11:44:32 +0100

    Add filter context for all projects

--- app/migration/projects/context/project.context.tsx
+++ app/migration/projects/context/project.context.tsx
@@ -1,38 +1,59 @@
 "use client";
 import { createContext, useMemo, useState } from "react";
 import ProjectApi from "src/api/Project";
-import { ProjectContextReturn, ProjectsContextProps } from "./project.context.type.ts";
+import {
+  DEFAULT_PROJECTS_FILTER,
+  PROJECT_FILTER_KEY,
+  ProjectContextReturn,
+  ProjectFilter,
+  ProjectsContextProps,
+} from "./project.context.type.ts";
+import { useLocalStorage } from "react-use";
 import { useInfiniteBaseQueryProps } from "../../../../src/api/useInfiniteBaseQuery.ts";
-import { Button } from "@/components/ds/button/button.tsx";
 
 export const ProjectsContext = createContext<ProjectContextReturn>({
   projects: [],
   fetchNextPage: () => null,
   hasNextPage: false,
   isFetchingNextPage: false,
+  filters: {
+    values: DEFAULT_PROJECTS_FILTER,
+    isCleared: true,
+    set: () => null,
+    clear: () => null,
+  },
 });
 
 export function ProjectsContextProvider({ children }: ProjectsContextProps) {
-  const [filters, setFilters] = useState<useInfiniteBaseQueryProps["queryParams"]>([
-    ["mine", "true"],
-    ["sort", "RANK"],
-  ]);
+  const [storage, setStorage] = useLocalStorage(PROJECT_FILTER_KEY, DEFAULT_PROJECTS_FILTER);
+  const [filters, setFilters] = useState<ProjectFilter>({ ...DEFAULT_PROJECTS_FILTER, ...storage });
+  const isCleared = useMemo(() => JSON.stringify(filters) == JSON.stringify(DEFAULT_PROJECTS_FILTER), [filters]);
+
+  const queryParams = useMemo(() => {
+    const params: useInfiniteBaseQueryProps["queryParams"] = [
+      filters.technologies.length > 0 ? ["technologies", filters.technologies.join(",")] : null,
+      filters.sponsors.length > 0 ? ["sponsorId", filters.sponsors.map(({ id }) => id).join(",")] : null,
+      filters.search ? ["search", filters.search] : null,
+      filters.sorting ? ["sort", filters.sorting] : null,
+      filters.ownership ? ["mine", String(filters.ownership === "Mine")] : null,
+    ].filter((param): param is string[] => Boolean(param));
+
+    return params;
+  }, [filters]);
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = ProjectApi.queries.useInfiniteList({
-    queryParams: filters,
+    queryParams: queryParams,
   });
 
-  // const queryParams = useMemo(() => {
-  //   const params: useInfiniteBaseQueryProps["queryParams"] = [
-  //     technologies.length > 0 ? ["technologies", technologies.join(",")] : null,
-  //     sponsors.length > 0 ? ["sponsorId", sponsors.map(({ id }) => id).join(",")] : null,
-  //     search ? ["search", search] : null,
-  //     sorting ? ["sort", sorting] : null,
-  //     ownership ? ["mine", String(ownership === "Mine")] : null,
-  //   ].filter((param): param is string[] => Boolean(param));
-  //
-  //   return params;
-  // }, [technologies, sponsors, search, sorting, ownership]);
+  function onFilterChange(newFilter: Partial<ProjectFilter>) {
+    const filtersValues = { ...filters, ...newFilter };
+    setFilters(filtersValues);
+    setStorage(filtersValues);
+  }
+  function onClearFilter() {
+    setFilters(DEFAULT_PROJECTS_FILTER);
+    setStorage(DEFAULT_PROJECTS_FILTER);
+  }
 
   const projects = useMemo(() => data?.pages?.flatMap(({ projects }) => projects) ?? [], [data]);
 
@@ -43,18 +64,14 @@ export function ProjectsContextProvider({ children }: ProjectsContextProps) {
         fetchNextPage: fetchNextPage,
         isFetchingNextPage,
         hasNextPage,
+        filters: {
+          values: filters,
+          isCleared,
+          set: onFilterChange,
+          clear: onClearFilter,
+        },
       }}
     >
-      <Button
-        onClick={() => {
-          setFilters([
-            ["mine", "false"],
-            ["sort", "RANK"],
-          ]);
-        }}
-      >
-        Fake change filter
-      </Button>
       {children}
     </ProjectsContext.Provider>
   );
--- app/migration/projects/context/project.context.type.ts
+++ app/migration/projects/context/project.context.type.ts
@@ -1,5 +1,6 @@
 import { UseInfiniteListResponse } from "src/api/Project/queries.ts";
 import { ReactNode } from "react";
+import { Sponsor } from "../../../../src/types.ts";
 
 export interface ProjectsContextProps {
   children: ReactNode;
@@ -10,4 +11,39 @@ export type ProjectContextReturn = {
   fetchNextPage: () => void;
   hasNextPage: boolean;
   isFetchingNextPage: boolean;
+  filters: {
+    values: ProjectFilter;
+    isCleared: boolean;
+    set: (filter: Partial<ProjectFilter>) => void;
+    clear: () => void;
+  };
+};
+
+export enum Ownership {
+  All = "All",
+  Mine = "Mine",
+}
+
+export interface ProjectFilter {
+  ownership: Ownership;
+  technologies: string[];
+  sponsors: Sponsor[];
+  search?: string;
+  sorting?: Sorting;
+}
+
+export enum Sorting {
+  Trending = "RANK",
+  ProjectName = "NAME",
+  ReposCount = "REPO_COUNT",
+  ContributorsCount = "CONTRIBUTOR_COUNT",
+}
+export const PROJECT_FILTER_KEY = "project_filter";
+export const DEFAULT_PROJECT_SORTING = Sorting.Trending;
+
+export const DEFAULT_PROJECTS_FILTER: ProjectFilter = {
+  ownership: Ownership.All,
+  technologies: [],
+  sponsors: [],
+  sorting: DEFAULT_PROJECT_SORTING,
 };
