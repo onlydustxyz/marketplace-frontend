2e100fb0a4d5951f34bc6e3edada67d0d24ab7ed
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-09 18:51:01 +0100

    refactor updateLogo

--- components/features/auth0/handlers/handle-login.ts
+++ components/features/auth0/handlers/handle-login.ts
@@ -3,9 +3,10 @@ import { RedirectLoginOptions } from "@auth0/auth0-react";
 export const handleLoginWithRedirect = async (
   loginWithRedirectFunc: (options?: RedirectLoginOptions) => Promise<void>
 ) => {
-  await loginWithRedirectFunc({
-    appState: {
-      returnTo: process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URL || "/",
-    },
-  });
+  await loginWithRedirectFunc();
+  //   {
+  //   appState: {
+  //     returnTo: window.location.pathname,
+  //   },
+  // }
 };
