a44a21a6db9da06b5f8453a9c7c6de205728162e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-10 18:52:58 +0100

    refactor: ClaimButton props

--- src/_pages/ProjectDetails/Banners/ClaimBanner/components/ClaimButton.tsx
+++ src/_pages/ProjectDetails/Banners/ClaimBanner/components/ClaimButton.tsx
@@ -6,9 +6,9 @@ import MagicLine from "src/icons/MagicLine";
 import { useAuth0 } from "@auth0/auth0-react";
 import { handleLoginWithRedirect } from "components/features/auth0/handlers/handle-login";
 
-type ClaimButton = { projectKey: string; callback: () => void };
+type ClaimButton = { callback: () => void };
 
-export function ClaimButton({ projectKey, callback }: ClaimButton) {
+export function ClaimButton({ callback }: ClaimButton) {
   const { T } = useIntl();
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   const [getPermission] = useLazyGetUserPermissions();
