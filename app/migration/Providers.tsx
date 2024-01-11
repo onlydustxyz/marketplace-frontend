3f18baf5ba51b01d0efa249273d5b6b20a09538a
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-11 16:37:50 +0100

    add provider

--- app/migration/Providers.tsx
+++ app/migration/Providers.tsx
@@ -1,19 +1,37 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
-import { ImpersonationClaimsProvider } from "../../src/hooks/useImpersonationClaims.tsx";
-import { TokenSetProvider } from "../../src/hooks/useTokenSet.tsx";
-import { IntlProvider } from "../../src/hooks/useIntl.tsx";
+import { IntlProvider } from "../../src/hooks/useIntl";
+import { StackProvider } from "../../src/libs/react-stack";
+import { Stacks } from "../../src/App/Stacks/Stacks";
+import Tooltip from "../../src/components/Tooltip";
+import { ToasterProvider } from "../../src/hooks/useToaster";
+import { BrowserRouter } from "react-router-dom";
+import { useMediaQuery } from "usehooks-ts";
+import { viewportConfig } from "../../src/config";
+import { Auth0ProviderWithNavigate } from "components/features/auth0/providers/auth0-provider-with-navigate";
+import ImpersonationProvider from "components/features/impersonation/impersonation.provider";
 const queryClient = new QueryClient();
 
 export default function MigrationProviders({ children }: PropsWithChildren) {
+  const isSm = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.sm}px)`);
   return (
-    <IntlProvider>
-      <ImpersonationClaimsProvider>
-        <TokenSetProvider>
-          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </TokenSetProvider>
-      </ImpersonationClaimsProvider>
-    </IntlProvider>
+    <BrowserRouter>
+      <IntlProvider>
+        <Auth0ProviderWithNavigate>
+          <ImpersonationProvider>
+            <QueryClientProvider client={queryClient}>
+              <ToasterProvider>
+                <StackProvider>
+                  {children}
+                  <Stacks />
+                  {isSm && <Tooltip />}
+                </StackProvider>
+              </ToasterProvider>
+            </QueryClientProvider>
+          </ImpersonationProvider>
+        </Auth0ProviderWithNavigate>
+      </IntlProvider>
+    </BrowserRouter>
   );
 }
