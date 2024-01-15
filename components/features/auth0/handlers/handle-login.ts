c64ff52937612de5d49ba70611ca07fd93e9d607
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-01-15 16:40:52 +0100

    handle redirection to (#1681)

--- components/features/auth0/handlers/handle-login.ts
+++ components/features/auth0/handlers/handle-login.ts
@@ -3,10 +3,9 @@ import { RedirectLoginOptions } from "@auth0/auth0-react";
 export async function handleLoginWithRedirect(
   loginWithRedirectFunc: (options?: RedirectLoginOptions) => Promise<void>
 ) {
-  await loginWithRedirectFunc();
-  //   {
-  //   appState: {
-  //     returnTo: window.location.pathname,
-  //   },
-  // }
+  await loginWithRedirectFunc({
+    appState: {
+      returnTo: window.location.pathname,
+    },
+  });
 }
