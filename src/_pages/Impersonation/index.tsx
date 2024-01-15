47d5df634a50a13771463322c9b51b83cf4a70a5
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-15 15:24:31 +0100

    fix impersonation behavior

--- src/_pages/Impersonation/index.tsx
+++ src/_pages/Impersonation/index.tsx
@@ -2,51 +2,45 @@ import { useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { RoutePaths } from "src/App";
 import { useImpersonation } from "../../../components/features/impersonation/use-impersonation.tsx";
-import { useIntl } from "src/hooks/useIntl.tsx";
 import MeApi from "src/api/me/index.ts";
 import { getGithubUserIdFromSub } from "components/features/auth0/utils/getGithubUserIdFromSub.util.ts";
-import Loader from "src/components/Loader/index.tsx";
+import { useIntl } from "../../hooks/useIntl.tsx";
 
 const ImpersonationPage = () => {
+  const { T } = useIntl();
   const { userId } = useParams();
   const navigate = useNavigate();
-  const { T } = useIntl();
-  const { isImpersonating, getImpersonateClaim, setImpersonateClaim, clearImpersonateClaim } = useImpersonation();
+  const { getImpersonateClaim, setImpersonateClaim, clearImpersonateClaim } = useImpersonation();
   const impersonateClaims = getImpersonateClaim();
-  const { data: userInfo, isLoading, isError } = MeApi.queries.useGetMe({ options: { retry: 1 } });
+  const { refetch } = MeApi.queries.useGetMe({ options: { retry: 1 } });
 
   useEffect(() => {
     if (!userId) {
       navigate(RoutePaths.Projects);
     } else {
       setImpersonateClaim({ sub: `github|${userId}` });
-      const claimedGithubUserId = getGithubUserIdFromSub(impersonateClaims?.sub);
+      refetch().then(response => {
+        const { data: userInfo, isFetching, isError } = response;
+        const claimedGithubUserId = getGithubUserIdFromSub(impersonateClaims?.sub);
 
-      if (isError) {
-        clearImpersonateClaim();
-        navigate(RoutePaths.NotFound);
-      }
-
-      if (userInfo && !isLoading) {
-        if (isImpersonating && userInfo?.githubUserId === claimedGithubUserId) {
-          navigate(RoutePaths.Projects);
-        } else {
+        if (isError) {
           clearImpersonateClaim();
           navigate(RoutePaths.NotFound);
         }
-      }
+
+        if (userInfo && !isFetching && claimedGithubUserId) {
+          if (userInfo?.githubUserId === claimedGithubUserId) {
+            navigate(RoutePaths.Projects);
+          } else {
+            clearImpersonateClaim();
+            navigate(RoutePaths.NotFound);
+          }
+        }
+      });
     }
-  }, [userId, userInfo, isLoading, isImpersonating, impersonateClaims]);
+  }, [userId, impersonateClaims]);
 
-  if (isLoading) {
-    return (
-      <div className="item-center flex h-screen justify-center">
-        <Loader />
-      </div>
-    );
-  }
-
-  return <Link to={RoutePaths.Projects}>{T("impersonation.backToProjects")}</Link>;
+  return <Link to={RoutePaths.Projects}>{T("notFound.button")}</Link>;
 };
 
 export default ImpersonationPage;
