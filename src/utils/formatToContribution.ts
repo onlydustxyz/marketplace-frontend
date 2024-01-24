a62f08a7707996857ba3688cb783d29ef7a9ff89
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-24 11:26:30 +0100

    fix: file extensiosn

--- src/utils/formatToContribution.ts
+++ src/utils/formatToContribution.ts
@@ -1,5 +1,5 @@
-import { RewardableItem } from "src/api/Project/queries.ts";
-import { RewardItem } from "src/hooks/useInfiniteRewardItems.tsx";
+import { RewardableItem } from "src/api/Project/queries";
+import { RewardItem } from "src/hooks/useInfiniteRewardItems";
 
 /**
  * Converts a rewardable item to a contribution
