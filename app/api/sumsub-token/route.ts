0f679dce894a08806e987574b7d086f43d591fca
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-24 16:25:16 +0100

    fix: lint

--- app/api/sumsub-token/route.ts
+++ app/api/sumsub-token/route.ts
@@ -1,14 +1,14 @@
-const axios = require("axios");
+import axios from "axios";
 import * as crypto from "crypto";
-const fs = require("fs");
-const FormData = require("form-data");
+import FormData from "form-data";
+import fs from "fs";
 
 // These parameters should be used for all requests
 const SUMSUB_APP_TOKEN = process.env.SUMSUB_APP_TOKEN ?? "";
 const SUMSUB_SECRET_KEY = process.env.SUMSUB_SECRET_KEY ?? "";
 const SUMSUB_BASE_URL = "https://api.sumsub.com";
 
-var config = {};
+const config = {};
 config.baseURL = SUMSUB_BASE_URL;
 
 axios.interceptors.request.use(createSignature, function (error) {
@@ -22,7 +22,7 @@ axios.interceptors.request.use(createSignature, function (error) {
 function createSignature(config) {
   console.log("Creating a signature for the request...");
 
-  var ts = Math.floor(Date.now() / 1000);
+  const ts = Math.floor(Date.now() / 1000);
   const signature = crypto.createHmac("sha256", SUMSUB_SECRET_KEY);
   signature.update(ts + config.method.toUpperCase() + config.url);
 
@@ -41,18 +41,19 @@ function createSignature(config) {
 // These functions configure requests for specified method
 
 // https://developers.sumsub.com/api-reference/#creating-an-applicant
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function createApplicant(externalUserId, levelName) {
   console.log("Creating an applicant...");
 
-  var method = "post";
-  var url = "/resources/applicants?levelName=" + encodeURIComponent(levelName);
-  var ts = Math.floor(Date.now() / 1000);
+  const method = "post";
+  const url = "/resources/applicants?levelName=" + encodeURIComponent(levelName);
+  //const ts = Math.floor(Date.now() / 1000);
 
-  var body = {
-    externalUserId: externalUserId,
+  const body = {
+    externalUserId,
   };
 
-  var headers = {
+  const headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
     "X-App-Token": SUMSUB_APP_TOKEN,
@@ -67,22 +68,23 @@ function createApplicant(externalUserId, levelName) {
 }
 
 // https://developers.sumsub.com/api-reference/#adding-an-id-document
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function addDocument(applicantId) {
   console.log("Adding document to the applicant...");
 
-  var method = "post";
-  var url = `/resources/applicants/${applicantId}/info/idDoc`;
-  var filePath = "resources/sumsub-logo.png";
+  const method = "post";
+  const url = `/resources/applicants/${applicantId}/info/idDoc`;
+  const filePath = "resources/sumsub-logo.png";
 
-  var metadata = {
+  const metadata = {
     idDocType: "PASSPORT",
     country: "GBR",
   };
 
-  var form = new FormData();
+  const form = new FormData();
   form.append("metadata", JSON.stringify(metadata));
 
-  var content = fs.readFileSync(filePath);
+  const content = fs.readFileSync(filePath);
   form.append("content", content, filePath);
 
   /*
@@ -95,7 +97,7 @@ function addDocument(applicantId) {
     form.append('content', Buffer.from(base64Data, 'base64'), { fileName });
     */
 
-  var headers = {
+  const headers = {
     Accept: "application/json",
     "X-App-Token": SUMSUB_APP_TOKEN,
   };
@@ -109,13 +111,14 @@ function addDocument(applicantId) {
 }
 
 // https://developers.sumsub.com/api-reference/#getting-applicant-status-sdk
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getApplicantStatus(applicantId) {
   console.log("Getting the applicant status...");
 
-  var method = "get";
-  var url = `/resources/applicants/${applicantId}/status`;
+  const method = "get";
+  const url = `/resources/applicants/${applicantId}/status`;
 
-  var headers = {
+  const headers = {
     Accept: "application/json",
     "X-App-Token": SUMSUB_APP_TOKEN,
   };
@@ -132,8 +135,8 @@ function getApplicantStatus(applicantId) {
 function createAccessToken(externalUserId, levelName = "basic-kyc-level", ttlInSecs = 600) {
   console.log("Creating an access token for initializng SDK...");
 
-  var method = "post";
-  var url =
+  const method = "post";
+  const url =
     "/resources/accessTokens?userId=" +
     encodeURIComponent(externalUserId) +
     "&ttlInSecs=" +
@@ -141,7 +144,7 @@ function createAccessToken(externalUserId, levelName = "basic-kyc-level", ttlInS
     "&levelName=" +
     encodeURIComponent(levelName);
 
-  var headers = {
+  const headers = {
     Accept: "application/json",
     "X-App-Token": SUMSUB_APP_TOKEN,
   };
