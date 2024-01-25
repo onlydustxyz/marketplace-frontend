a277eaa5f0b6b6df41da584f733ca6e618f626ba
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-01-25 17:52:17 +0100

    E 1041 les technologies dans les profils nutilisent pas la 02 (#1722)

--- src/utils/technologies.ts
+++ src/utils/technologies.ts
@@ -2,6 +2,14 @@ export interface Technologies {
   [key: string]: number;
 }
 
+export function isBlackListedTechnology(technology: string): boolean {
+  const blackListedTech = process.env.NEXT_PUBLIC_LANGUAGES_FILTER?.toLowerCase();
+
+  const arrayOfTech = blackListedTech?.split(",");
+
+  return !!arrayOfTech?.find(b => b === technology.toLowerCase()) || false;
+}
+
 export function getFilteredTechnologies(technologies: Technologies): {
   filteredTechObject: Technologies;
   filteredTechArray: [string, number][];
