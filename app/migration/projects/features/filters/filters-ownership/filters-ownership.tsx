30402492379e10abe5ad7afaac4ee3990309dca4
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-01-29 11:00:20 +0100

    E-1055 - Improve layout on browse projects (#1736)
    Co-authored-by: Mehdi Hamri <pixelfact.company@gmail.com>

--- app/migration/projects/features/filters/filters-ownership/filters-ownership.tsx
+++ app/migration/projects/features/filters/filters-ownership/filters-ownership.tsx
@@ -1,32 +1,45 @@
 import { useContext } from "react";
 import { ProjectsContext } from "../../../context/project.context";
 import { Flex } from "components/layout/flex/flex";
-import { SelectableTag } from "components/ds/selectable-tag/selectable-tag";
-import { Translate } from "components/layout/translate/translate";
-import { Icon } from "components/layout/icon/icon";
+import { SelectableTag } from "components/ds/form/selectable-tag/selectable-tag";
 import { Ownership } from "../../../context/project.context.types";
+import { SelectableTagItem } from "components/ds/form/selectable-tag/selectable-tag-item/selectable-tag-item";
+import { Icon } from "components/layout/icon/icon";
 
 export function FiltersOwnership() {
   const { filters } = useContext(ProjectsContext);
 
-  const handleAllClick = () => {
-    filters.set({ ownership: Ownership.All });
-  };
-
-  const handleMineClick = () => {
-    filters.set({ ownership: Ownership.Mine });
+  const onFilterChange = (value: Ownership | null) => {
+    filters.set({ ownership: value || Ownership.All });
   };
 
   return (
     <Flex className="gap-2">
-      <SelectableTag selected={filters.values.ownership === Ownership.All} onClick={handleAllClick}>
-        <Translate token="filter.ownership.all" />
-      </SelectableTag>
+      <SelectableTag
+        mode="single"
+        value={filters.values.ownership}
+        onChange={onFilterChange}
+        options={[
+          {
+            value: Ownership.Mine,
+            children: "Mine",
+            icon: props => <Icon remixName="ri-seedling-line" {...props} />,
+          },
+          {
+            value: Ownership.All,
+            children: "All",
+          },
+          {
+            value: Ownership.All,
+            children: "All2",
+            disabled: true,
+          },
+        ]}
+      />
 
-      <SelectableTag selected={filters.values.ownership === Ownership.Mine} onClick={handleMineClick}>
-        <Icon remixName="ri-star-line" />
-        <Translate token="filter.ownership.mine" />
-      </SelectableTag>
+      <SelectableTagItem value={Ownership.Mine} checkboxProps={{ onChange: () => null }}>
+        Display tag
+      </SelectableTagItem>
     </Flex>
   );
 }
---
+++ components/ds/form/selectable-tag/selectable-tag.types.ts
@@ -0,0 +1,26 @@
+import { TSelectableTagItem } from "components/ds/form/selectable-tag/selectable-tag-item/selectable-tag-item.types";
+import { CheckboxGroupProps } from "@nextui-org/react";
+
+export namespace TSelectableTag {
+  export type Mode = "single" | "multiple";
+  export type BaseCheckboxGroupProps = Omit<CheckboxGroupProps, "value" | "onChange">;
+  export interface BaseProps<V extends string> extends Partial<BaseCheckboxGroupProps> {
+    className?: string;
+    mode: Mode;
+    options: TSelectableTagItem.Props<V>[];
+  }
+
+  export interface MultipleProps<V extends string> extends BaseProps<V> {
+    mode: "multiple";
+    value: V[];
+    onChange: (value: V[]) => void;
+  }
+
+  export interface SingleProps<V extends string> extends BaseProps<V> {
+    mode: "single";
+    value: V | null;
+    onChange: (value: V | null) => void;
+  }
+
+  export type Props<V extends string> = MultipleProps<V> | SingleProps<V>;
+}
