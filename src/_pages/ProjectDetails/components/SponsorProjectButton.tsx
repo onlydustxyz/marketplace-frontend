4c7afb30cee05378780b2de649c6fe3584b2ee79
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-29 14:29:50 +0100

    feat: sponsor params

--- src/_pages/ProjectDetails/components/SponsorProjectButton.tsx
+++ src/_pages/ProjectDetails/components/SponsorProjectButton.tsx
@@ -3,8 +3,6 @@ import { Icon } from "components/layout/icon/icon";
 import { useCurrentUser } from "hooks/users/useCurrentUser";
 import { useState } from "react";
 import { components } from "src/__generated/api";
-import MeApi from "src/api/me";
-import { Channel } from "src/App/Stacks/ContributorProfileSidePanel/EditView/types";
 import Button, { ButtonOnBackground, ButtonSize } from "src/components/Button";
 import SidePanel from "src/components/SidePanel";
 import { useIntl } from "src/hooks/useIntl";
@@ -16,36 +14,7 @@ interface Props {
 export function SponsorProjectButton({ project }: Props) {
   const { T } = useIntl();
   const { user } = useCurrentUser();
-  const { data: userInfo } = MeApi.queries.useGetMyPayoutInfo({});
-  const { data: userProfile } = MeApi.queries.useGetMyProfileInfo({});
   const [isOpen, setIsOpen] = useState(false);
-
-  const findContact = (channel: Channel) => {
-    const findContact = userProfile?.contacts?.find(contact => contact.channel === channel);
-    return findContact?.contact || undefined;
-  };
-
-  console.log({ project, user, userInfo, userProfile });
-
-  // Project
-  //
-  // Coordonnées de facturation
-  //
-  // Nom
-  //
-  // Addresse, Ville, Pays
-  //
-  // Currency
-  //
-  // Montant
-  //
-  // Origine des fonds: addresse from
-  //
-  // Network
-  //
-  // USD - Ethereum
-  //
-  // USDC - Ethereum
 
   return (
     <>
@@ -63,18 +32,13 @@ export function SponsorProjectButton({ project }: Props) {
         <FilloutStandardEmbed
           filloutId="1cTn46XDDVus"
           inheritParameters
-          parameters={
-            {
-              // lead_id: user?.id,
-              // lead_first_name: userInfo?.person?.firstname,
-              // lead_last_name: userInfo?.person?.lastname,
-              // lead_github: user?.login,
-              // lead_telegram: findContact(Channel.Telegram),
-              // lead_email: findContact(Channel.Email),
-              // project_id: project.id,
-              // project_name: project.name,
-            }
-          }
+          parameters={{
+            project_id: project.id,
+            project_name: project.name,
+            user_id: user?.id,
+            user_github: user?.login,
+            user_email: user?.email,
+          }}
         />
       </SidePanel>
     </>
