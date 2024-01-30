fab17564f3056058a8205059b9bf4a17d58c90d2
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-30 18:09:26 +0100

    fix: client provider loading

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import dynamic from "next/dynamic";
 import { PropsWithChildren } from "react";
 
 import { IntlProvider } from "src/hooks/useIntl";
-import { SidePanelProvider } from "src/hooks/useSidePanel";
-import { SidePanelStackProvider } from "src/hooks/useSidePanelStack";
-import { StackProvider } from "src/libs/react-stack";
 
 import { QueryProvider } from "components/features/api/providers/query-provider";
 import { Auth0ProviderWithNavigate } from "components/features/auth0/providers/auth0-provider-with-navigate";
 import { ImpersonationProvider } from "components/features/impersonation/impersonation.provider";
 import { PosthogProvider } from "components/features/posthog/providers/posthog.provider";
+
+const StackProvider = dynamic(() => import("src/libs/react-stack").then(mod => mod.StackProvider), { ssr: false });
+const SidePanelStackProvider = dynamic(
+  () => import("src/hooks/useSidePanelStack").then(mod => mod.SidePanelStackProvider),
+  { ssr: false }
+);
+const SidePanelProvider = dynamic(() => import("src/hooks/useSidePanel").then(mod => mod.SidePanelProvider), {
+  ssr: false,
+});
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
@@ -20,13 +27,13 @@ export default function Providers({ children }: PropsWithChildren) {
         <Auth0ProviderWithNavigate>
           <IntlProvider>
             <QueryProvider>
-              <StackProvider>
-                <SidePanelStackProvider>
-                  <SidePanelProvider>
-                    <NextUIProvider>{children}</NextUIProvider>
-                  </SidePanelProvider>
-                </SidePanelStackProvider>
-              </StackProvider>
+              <NextUIProvider>
+                <StackProvider>
+                  <SidePanelStackProvider>
+                    <SidePanelProvider>{children}</SidePanelProvider>
+                  </SidePanelStackProvider>
+                </StackProvider>
+              </NextUIProvider>
             </QueryProvider>
           </IntlProvider>
         </Auth0ProviderWithNavigate>
