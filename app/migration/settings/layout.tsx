621cf2c18726c8e0fa0f0ce115d55edc9d4bfb1b
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-01-30 17:45:48 +0100

    refactor: background component

--- app/migration/settings/layout.tsx
+++ app/migration/settings/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 import { Sidebar } from "app/migration/settings/components/sidebar";
 
-import { Background } from "components/layout/background/background";
+import { ScrollView } from "components/layout/pages/scroll-view/scroll-view";
 
 export default function SettingsLayout({ children }: PropsWithChildren) {
   return (
@@ -10,9 +10,10 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
     <div className="flex h-[calc(100dvh)] w-screen flex-col xl:fixed">
       <div className="flex w-full flex-1 flex-col gap-4 overflow-hidden pt-4 xl:h-0 xl:flex-row xl:gap-2 xl:p-6 xl:pt-0">
         <Sidebar />
-        <Background className="xl:rounded-r-2xl">
+
+        <ScrollView className="bg-space bg-no-repeat xl:rounded-r-2xl">
           <div className="mx-auto flex h-full max-w-7xl flex-1 flex-col gap-6 px-4 py-6 xl:px-8">{children}</div>
-        </Background>
+        </ScrollView>
       </div>
     </div>
   );
--- components/layout/background/background.types.ts
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export namespace TBackground {
-  export interface Props extends PropsWithChildren {
-    className?: string;
-  }
-}
