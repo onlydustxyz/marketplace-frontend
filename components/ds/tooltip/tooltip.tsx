59bd7d245547a95586d073ad37e2f438612b837c
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-30 14:18:59 +0100

    handle projects tag with a dedicated component that handle also tooltips

--- components/ds/tooltip/tooltip.tsx
+++ components/ds/tooltip/tooltip.tsx
@@ -1,7 +1,7 @@
 import { Tooltip as NextUiTooltip } from "@nextui-org/react";
 import { TTooltip } from "components/ds/tooltip/tooltip.types";
 
-export function Tooltip({ as: Component = "button", content, placement, children }: TTooltip.Props) {
+export function Tooltip({ as: Component = "div", content, placement, children }: TTooltip.Props) {
   return (
     <NextUiTooltip
       content={content}
