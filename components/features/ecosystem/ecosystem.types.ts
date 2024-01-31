c9bebc3789ea861004616ece0243b0034da59baa
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-31 10:48:47 +0100

    WIP ecosystem

---
+++ components/features/ecosystem/ecosystem.types.ts
@@ -0,0 +1,10 @@
+export namespace TEcosystem {
+  export interface Props {
+    id: string;
+    name: string;
+    logoUrl: string;
+    url: string;
+    clickable?: boolean;
+    className?: string;
+  }
+}
