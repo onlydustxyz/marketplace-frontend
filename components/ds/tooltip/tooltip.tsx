bb2f7b194d07698f85a2be0064aa350c2bd888d2
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-31 09:22:45 +0100

    change tooltip background and handle with flex-wrap avatars

--- components/ds/tooltip/tooltip.tsx
+++ components/ds/tooltip/tooltip.tsx
@@ -1,4 +1,5 @@
 import { Tooltip as NextUiTooltip } from "@nextui-org/react";
+
 import { TTooltip } from "components/ds/tooltip/tooltip.types";
 
 export function Tooltip({ as: Component = "div", content, placement, children }: TTooltip.Props) {
@@ -8,7 +9,7 @@ export function Tooltip({ as: Component = "div", content, placement, children }:
       placement={placement}
       showArrow={true}
       classNames={{
-        content: ["p-4", "od-text-body-s"],
+        content: ["p-4 bg-greyscale-800", "od-text-body-s"],
       }}
     >
       <Component>{children}</Component>
--- components/features/contributors-avatars/contributors-avatars.tsx
+++ components/features/contributors-avatars/contributors-avatars.tsx
@@ -1,22 +1,30 @@
-import { TContributorsAvatars } from "./contributors-avatars.types";
+import isDefined from "src/utils/isDefined";
+
 import { ThumbnailGroup } from "components/ds/thumbnail-group/thumbnail-group";
-import { Contributor } from "components/features/contributor/contributor";
 import { Tooltip } from "components/ds/tooltip/tooltip";
+import { Contributor } from "components/features/contributor/contributor";
+
+import { TContributorsAvatars } from "./contributors-avatars.types";
 
 export function ContributorsAvatars({ contributors, ...variant }: TContributorsAvatars.Props) {
   function contributorsContent() {
     return (
-      <div className="grid grid-cols-2 items-center gap-3 text-snow md:grid-cols-4">
-        {contributors.map(contributor => (
-          <Contributor
-            key={contributor.githubUserId}
-            githubUserId={contributor.githubUserId}
-            login={contributor.login}
-            avatarUrl={contributor.avatarUrl}
-            isRegistered={false}
-            clickable
-          />
-        ))}
+      <div className="flex flex-row flex-wrap items-center gap-4 text-snow">
+        {contributors
+          .filter(
+            contributor =>
+              isDefined(contributor.githubUserId) && isDefined(contributor.login) && isDefined(contributor.avatarUrl)
+          )
+          .map(contributor => (
+            <Contributor
+              key={contributor.githubUserId}
+              githubUserId={contributor.githubUserId}
+              login={contributor.login}
+              avatarUrl={contributor.avatarUrl}
+              isRegistered={false}
+              clickable
+            />
+          ))}
       </div>
     );
   }
