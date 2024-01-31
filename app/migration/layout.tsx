7c11c9a80717749d185ece15a2ced098ca447c30
Mehdi Hamri <pixelfact.company@gmail.com>
2024-01-31 18:38:29 +0100

    add header an improve background gradient

--- app/migration/layout.tsx
+++ app/migration/layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 
+import Header from "src/App/Layout/Header";
+
 import MigrationProviders from "./providers";
 
 export default function MigrationLayout({ children }: { children: ReactNode }) {
   return (
     <MigrationProviders>
-      <div className="fixed flex h-[calc(100dvh)] w-screen flex-col">{children}</div>
+      <div className="fixed flex h-[calc(100dvh)] w-screen flex-col">
+        <Header />
+        {children}
+      </div>
     </MigrationProviders>
   );
 }
