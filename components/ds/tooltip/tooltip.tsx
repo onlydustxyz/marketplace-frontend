a2a9d91d4ce185f7902bab39f8bc48d1eb704d49
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-01 15:59:24 +0100

    Prefer tooltipProps so we can send anything a tooltip needs

--- components/ds/tooltip/tooltip.tsx
+++ components/ds/tooltip/tooltip.tsx
@@ -2,11 +2,10 @@ import { Tooltip as NextUiTooltip } from "@nextui-org/react";
 
 import { TTooltip } from "components/ds/tooltip/tooltip.types";
 
-export function Tooltip({ as: Component = "div", content, placement, children }: TTooltip.Props) {
+export function Tooltip({ as: Component = "div", children, ...props }: TTooltip.Props) {
   return (
     <NextUiTooltip
-      content={content}
-      placement={placement}
+      {...props}
       showArrow={true}
       classNames={{
         content: ["p-4 bg-greyscale-800", "od-text-body-s"],
