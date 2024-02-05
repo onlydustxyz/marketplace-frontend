dc76cd39841ed417401cce55a0ee76258e128b18
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-02-05 17:58:29 +0100

    Add new projects tags (#1766)
    Co-authored-by: Mehdi Hamri <pixelfact.company@gmail.com>

--- app/migration/projects/features/filters/filters-tags/filters-tags.tsx
+++ app/migration/projects/features/filters/filters-tags/filters-tags.tsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
 
+import { ProjectConstants } from "src/api/Project/constants";
 import { ProjectTypes } from "src/api/Project/types";
-import { useIntl } from "src/hooks/useIntl";
 
 import { SelectableTag } from "components/ds/form/selectable-tag/selectable-tag";
 import { Flex } from "components/layout/flex/flex";
 import { Icon } from "components/layout/icon/icon";
+import { Translate } from "components/layout/translate/translate";
 
 import { ProjectsContext } from "../../../context/project.context";
 
 export function FiltersTags() {
-  const { T } = useIntl();
   const { filters } = useContext(ProjectsContext);
 
   const onFilterChange = (value: ProjectTypes.Tags[]) => {
@@ -25,24 +25,34 @@ export function FiltersTags() {
         onChange={onFilterChange}
         options={[
           {
-            value: ProjectTypes.Tags.BeginnersWelcome,
-            children: T(`v2.commons.enums.project.tags.${ProjectTypes.Tags.BeginnersWelcome}`),
-            icon: props => <Icon remixName="ri-seedling-line" {...props} />,
+            value: ProjectTypes.Tags.HotCommunity,
+            children: <Translate token={ProjectConstants.tagMapping.HOT_COMMUNITY.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.HOT_COMMUNITY.icon} {...props} />,
           },
           {
-            value: ProjectTypes.Tags.StrongExpertise,
-            children: T(`v2.commons.enums.project.tags.${ProjectTypes.Tags.StrongExpertise}`),
-            icon: props => <Icon remixName="ri-git-fork-line" {...props} />,
+            value: ProjectTypes.Tags.NewbiesWelcome,
+            children: <Translate token={ProjectConstants.tagMapping.NEWBIES_WELCOME.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.NEWBIES_WELCOME.icon} {...props} />,
           },
           {
-            value: ProjectTypes.Tags.LikelyToSendRewards,
-            children: T(`v2.commons.enums.project.tags.${ProjectTypes.Tags.LikelyToSendRewards}`),
-            icon: props => <Icon remixName="ri-hand-coin-line" {...props} />,
+            value: ProjectTypes.Tags.LikelyToReward,
+            children: <Translate token={ProjectConstants.tagMapping.LIKELY_TO_REWARD.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.LIKELY_TO_REWARD.icon} {...props} />,
           },
           {
-            value: ProjectTypes.Tags.FastPaced,
-            children: T(`v2.commons.enums.project.tags.${ProjectTypes.Tags.FastPaced}`),
-            icon: props => <Icon remixName="ri-rocket-2-line" {...props} />,
+            value: ProjectTypes.Tags.WorkInProgress,
+            children: <Translate token={ProjectConstants.tagMapping.WORK_IN_PROGRESS.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.WORK_IN_PROGRESS.icon} {...props} />,
+          },
+          {
+            value: ProjectTypes.Tags.FastAndFurious,
+            children: <Translate token={ProjectConstants.tagMapping.FAST_AND_FURIOUS.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.FAST_AND_FURIOUS.icon} {...props} />,
+          },
+          {
+            value: ProjectTypes.Tags.BigWhale,
+            children: <Translate token={ProjectConstants.tagMapping.BIG_WHALE.label} />,
+            icon: props => <Icon {...ProjectConstants.tagMapping.BIG_WHALE.icon} {...props} />,
           },
         ]}
       />
