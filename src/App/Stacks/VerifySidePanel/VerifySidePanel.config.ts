eb496015da81a276cd83995c45c138b02542fde9
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-06 16:41:18 +0100

    refactor: force locale

--- src/App/Stacks/VerifySidePanel/VerifySidePanel.config.ts
+++ src/App/Stacks/VerifySidePanel/VerifySidePanel.config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  lang: "en",
   uiConf: {
     customCssStr: `
       :root {
--- src/App/Stacks/VerifySidePanel/VerifySidePanel.tsx
+++ src/App/Stacks/VerifySidePanel/VerifySidePanel.tsx
@@ -13,7 +13,7 @@ const options = {};
 
 export function VerifySidePanel({ externalId, levelName }: TVerifySidePanel.Props) {
   const [token, setToken] = useState("");
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     handleTokenCreation();
