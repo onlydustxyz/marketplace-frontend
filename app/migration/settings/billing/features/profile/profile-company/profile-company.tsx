754c8b085983c43a84d51771b87589022d526d5a
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-06 16:48:52 +0100

    Clean text formating

--- app/migration/settings/billing/features/profile/profile-company/profile-company.tsx
+++ app/migration/settings/billing/features/profile/profile-company/profile-company.tsx
@@ -1,16 +1,27 @@
+import { format } from "date-fns";
+import { useMemo } from "react";
+
 import { ProfileItemGrid } from "app/migration/settings/billing/component/profile-item-grid/profile-item-grid";
 import { ProfileItem } from "app/migration/settings/billing/component/profile-item/profile-item";
 
 import { TProfileCompany } from "./profile-company.types";
 
 export function ProfileCompany({ profile }: TProfileCompany.Props) {
+  const registrationDate = useMemo(() => {
+    if (profile.registrationDate) {
+      return format(new Date(profile.registrationDate), "MMM dd, yyyy");
+    }
+
+    return profile.registrationDate;
+  }, [profile]);
+
   return (
     <ProfileItemGrid>
       <ProfileItem label="v2.pages.settings.billing.company.companyName">{profile.name}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.company.registrationNumber">
         {profile.registrationNumber}
       </ProfileItem>
-      <ProfileItem label="v2.pages.settings.billing.company.registrationDate">{profile.registrationDate}</ProfileItem>
+      <ProfileItem label="v2.pages.settings.billing.company.registrationDate">{registrationDate}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.company.address">{profile.address}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.company.country">{profile.country}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.company.usEntity">{`${profile.usEntity}`}</ProfileItem>
--- app/migration/settings/billing/features/profile/profile-individual/profile-individual.tsx
+++ app/migration/settings/billing/features/profile/profile-individual/profile-individual.tsx
@@ -1,19 +1,45 @@
+import { format } from "date-fns";
+import { useMemo } from "react";
+
 import { ProfileItemGrid } from "app/migration/settings/billing/component/profile-item-grid/profile-item-grid";
 import { ProfileItem } from "app/migration/settings/billing/component/profile-item/profile-item";
 
 import { TProfileIndividual } from "./profile-individual.types";
 
 export function ProfileIndividual({ profile }: TProfileIndividual.Props) {
+  const birthdate = useMemo(() => {
+    if (profile.birthdate) {
+      return format(new Date(profile.birthdate), "MMM dd, yyyy");
+    }
+    return profile.birthdate;
+  }, [profile]);
+
+  const validUntil = useMemo(() => {
+    if (profile.validUntil) {
+      return format(new Date(profile.validUntil), "yyyy-MM-dd");
+    }
+    return profile.validUntil;
+  }, [profile]);
+
   return (
     <ProfileItemGrid>
       <ProfileItem label="v2.pages.settings.billing.individual.firstName">{profile.firstName}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.individual.lastName">{profile.lastName}</ProfileItem>
-      <ProfileItem label="v2.pages.settings.billing.individual.birthdate">{profile.birthdate}</ProfileItem>
+      <ProfileItem label="v2.pages.settings.billing.individual.birthdate">{birthdate}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.individual.address">{profile.address}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.individual.country">{profile.country}</ProfileItem>
       <ProfileItem label="v2.pages.settings.billing.individual.usCitizen">{`${profile.usCitizen}`}</ProfileItem>
-      <ProfileItem label="v2.pages.settings.billing.individual.idDocumentType">{profile.idDocumentType}</ProfileItem>
-      <ProfileItem label="v2.pages.settings.billing.individual.validUntil">{profile.validUntil}</ProfileItem>
+      <ProfileItem label="v2.pages.settings.billing.individual.identityDocumentType">
+        {profile.idDocumentType ? (
+          <>
+            {profile.idDocumentType}
+            {profile.idDocumentCountryCode ? (
+              <span className="uppercase">&nbsp;({profile.idDocumentCountryCode})</span>
+            ) : null}
+          </>
+        ) : null}
+      </ProfileItem>
+      <ProfileItem label="v2.pages.settings.billing.individual.validUntil">{validUntil}</ProfileItem>
     </ProfileItemGrid>
   );
 }
