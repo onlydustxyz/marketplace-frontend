b8c73e244b4bfd02090435565337dd2f9af9b443
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-07 09:22:48 +0100

    feat: added technologies and ecosystems to project detail stack

--- src/App/Stacks/ProjectOverviewSidePanel/components/Informations.tsx
+++ src/App/Stacks/ProjectOverviewSidePanel/components/Informations.tsx
@@ -1,8 +1,10 @@
 import { UseGetProjectBySlugResponse } from "src/api/Project/queries";
 import { IMAGES } from "src/assets/img";
 import { ProjectOverviewContributor } from "src/components/Project/Overview/OverviewContributors";
+import { ProjectOverviewEcosystem } from "src/components/Project/Overview/OverviewEcosystem";
 import { ProjectOverviewLead } from "src/components/Project/Overview/OverviewLead";
 import { ProjectOverviewMoreInfo } from "src/components/Project/Overview/OverviewMoreInfo";
+import { ProjectOverviewTechnologies } from "src/components/Project/Overview/OverviewTechnologies";
 import isDefined from "src/utils/isDefined";
 
 import { Card } from "components/ds/card/card";
@@ -14,22 +16,11 @@ export interface ProjectOverviewInformationsProps {
 }
 
 export const ProjectOverviewInformations = ({ project }: ProjectOverviewInformationsProps) => {
-  const {
-    contributorCount,
-    topContributors,
-    leaders,
-    invitedLeaders,
-    // sponsors,
-    moreInfos,
-  } = project;
+  const { contributorCount, topContributors, leaders, invitedLeaders, moreInfos, technologies, ecosystems } = project;
   const projectLeads = leaders?.filter(lead => isDefined(lead?.login)) || [];
   const projectInvited = invitedLeaders?.filter(lead => isDefined(lead?.login)) || [];
 
-  if (
-    !projectLeads.length &&
-    !contributorCount
-    // && !sponsors?.length && !moreInfos?.length
-  ) {
+  if (!projectLeads.length && !contributorCount) {
     return (
       <Card>
         <EmptyState
@@ -43,7 +34,8 @@ export const ProjectOverviewInformations = ({ project }: ProjectOverviewInformat
     <div className="flex-col divide-y divide-greyscale-50/8 rounded-2xl border border-card-border-light bg-greyscale-900 shadow-medium">
       <ProjectOverviewLead projectId={project?.id} projectLeads={projectLeads} projectInvited={projectInvited} />
       <ProjectOverviewContributor contributorCount={contributorCount} topContributors={topContributors} />
-      {/*<ProjectOverviewSponsors sponsors={sponsors} />*/}
+      <ProjectOverviewEcosystem ecosystems={ecosystems} />
+      <ProjectOverviewTechnologies technologies={technologies} />
       <ProjectOverviewMoreInfo moreInfos={moreInfos} />
     </div>
   );
