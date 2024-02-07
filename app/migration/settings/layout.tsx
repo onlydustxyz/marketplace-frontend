6a9acecd8b247a98723a8d57b137c7a5d4a87688
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-07 10:27:32 +0100

    Add header

--- app/migration/settings/layout.tsx
+++ app/migration/settings/layout.tsx
@@ -2,16 +2,19 @@ import { PropsWithChildren } from "react";
 
 import { Sidebar } from "app/migration/settings/components/sidebar";
 
+import Header from "src/App/Layout/Header";
+
 import { ScrollView } from "components/layout/pages/scroll-view/scroll-view";
 
 export default function SettingsLayout({ children }: PropsWithChildren) {
   return (
     // This outer div should be in the root layout
     <div className="flex h-[calc(100dvh)] w-screen flex-col xl:fixed">
+      <Header />
       <div className="flex w-full flex-1 flex-col gap-4 overflow-hidden pt-4 xl:h-0 xl:flex-row xl:gap-2 xl:p-6 xl:pt-0">
         <Sidebar />
 
-        <ScrollView className="bg-space bg-no-repeat xl:rounded-r-2xl">
+        <ScrollView className="od-space-background xl:rounded-r-2xl">
           <div className="mx-auto flex h-full max-w-7xl flex-1 flex-col gap-4 px-4 py-6 xl:px-8">{children}</div>
         </ScrollView>
       </div>
