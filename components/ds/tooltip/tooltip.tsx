8afb1d7cb0bc63834a8c021030ab93a3ac1fab02
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-02-07 17:07:42 +0100

    styles: tooltip padding (#1784)

--- components/ds/tooltip/tooltip.tsx
+++ components/ds/tooltip/tooltip.tsx
@@ -9,7 +9,7 @@ export function Tooltip({ as: Component = "div", children, ...props }: TTooltip.
       showArrow={true}
       classNames={{
         base: "before:bg-greyscale-800",
-        content: "p-4 bg-greyscale-800 od-text-body-s",
+        content: "px-3 py-2 bg-greyscale-800 od-text-body-s text-greyscale-50 rounded-lg shadow-md font-walsheim",
       }}
     >
       <Component>{children}</Component>
