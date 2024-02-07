66a1cc42d6c50cfcff128500d24a4273c5a32efe
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-07 10:16:13 +0100

    Add stacks subscribe

--- src/App/Stacks/VerifySidePanel/VerifySidePanel.tsx
+++ src/App/Stacks/VerifySidePanel/VerifySidePanel.tsx
@@ -1,11 +1,15 @@
 import SumsubWebSdk from "@sumsub/websdk-react";
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 import { createSumsubToken } from "app/api/sumsub-token/handlers";
 
+import { StackRoute } from "src/App/Stacks/Stacks";
 import { config } from "src/App/Stacks/VerifySidePanel/VerifySidePanel.config";
 import { TVerifySidePanel } from "src/App/Stacks/VerifySidePanel/VerifySidePanel.types";
+import MeApi from "src/api/me";
 import { IMAGES } from "src/assets/img";
+import { useSubscribeStacks } from "src/libs/react-stack";
 
 import { EmptyState } from "components/layout/placeholders/empty-state";
 
@@ -14,6 +18,9 @@ const options = {};
 export function VerifySidePanel({ externalId, levelName }: TVerifySidePanel.Props) {
   const [token, setToken] = useState("");
   const [error, setError] = useState(false);
+  const { open } = useSubscribeStacks(StackRoute.Verify);
+  const [isPanelHasOpenedState, setIsPanelHasOpenedState] = useState(false);
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     handleTokenCreation();
@@ -28,6 +35,15 @@ export function VerifySidePanel({ externalId, levelName }: TVerifySidePanel.Prop
       console.error(error);
     }
   }
+
+  useEffect(() => {
+    if (open && !isPanelHasOpenedState) {
+      setIsPanelHasOpenedState(true);
+    } else if (!open && isPanelHasOpenedState) {
+      queryClient.invalidateQueries({ queryKey: MeApi.billing.tags.anyProfile, exact: false });
+      setIsPanelHasOpenedState(false);
+    }
+  }, [open, isPanelHasOpenedState]);
 
   function handleExpiration() {
     setToken("");
