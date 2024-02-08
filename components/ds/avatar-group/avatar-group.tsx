cfadfa4fbb5e5c2f0037282cc18bf54f17cf49ad
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-08 16:45:19 +0100

    wip: avatar group

---
+++ components/ds/avatar-group/avatar-group.tsx
@@ -0,0 +1,15 @@
+import { cn } from "src/utils/cn";
+
+import { TThumbnailGroup } from "components/ds/thumbnail-group/thumbnail-group.types";
+import { thumbnailGroupVariants } from "components/ds/thumbnail-group/thumbnail-group.variants";
+import { Thumbnail } from "components/ds/thumbnail/thumbnail";
+
+export function AvatarGroup({ thumbnails, className, defaultSrc = true, ...props }: TThumbnailGroup.Props) {
+  return (
+    <div className={cn(thumbnailGroupVariants({ ...props }), className)}>
+      {thumbnails.map((thumbnail, index) => (
+        <Thumbnail key={`thumbnail-${index}`} {...thumbnail} {...props} defaultSrc={defaultSrc} />
+      ))}
+    </div>
+  );
+}
