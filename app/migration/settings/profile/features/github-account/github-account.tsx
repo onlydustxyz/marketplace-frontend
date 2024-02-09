b0ab2596b9a33a6a267f72e4cd2f21a3b96329da
NeoxAzrot <sami.lafrance@akwai.co>
2024-02-09 14:10:53 +0100

    feat(githubAccount): WIP

--- app/migration/settings/profile/features/github-account/github-account.tsx
+++ app/migration/settings/profile/features/github-account/github-account.tsx
@@ -2,7 +2,7 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
 
-import { useIntl } from "src/hooks/useIntl";
+import { cn } from "src/utils/cn";
 
 import { Button } from "components/ds/button/button";
 import { Card } from "components/ds/card/card";
@@ -13,8 +13,14 @@ import { Typography } from "components/layout/typography/typography";
 
 // TODO: Change Button with link using the new library
 export function ProfileGithubAccount() {
-  const { T } = useIntl();
   const { user } = useAuth0();
+
+  const onTriggerResync = () => {
+    console.log("onTriggerResync");
+  };
+
+  const isRefetching = false;
+  const isLoading = false;
 
   return (
     <Card background="base">
@@ -35,28 +41,37 @@ export function ProfileGithubAccount() {
           />
         </Flex>
 
-        <Flex className="flex-col justify-between gap-3 lg:flex-row lg:items-center">
-          <Flex alignItems="center" className="gap-3">
-            {user?.picture && (
-              <img className="h-8 w-8 rounded-full" src={user?.picture} loading="lazy" alt={T("profile.avatar")} />
-            )}
-
-            <Flex direction="col">
-              <Typography variant="title-s" className="text-sm leading-4">
-                {user?.nickname}
-              </Typography>
-              <Typography variant="body-s" className="text-spaceBlue-200">
-                {user?.email}
-              </Typography>
-            </Flex>
+        <Flex justifyContent="between" className="flex-col gap-3 rounded-lg bg-white/5 p-3 lg:flex-row lg:items-center">
+          <Flex direction="col">
+            <Typography variant="body-s-bold">{user?.nickname}</Typography>
+            <Typography variant="body-s" className="text-spaceBlue-200">
+              {user?.email}
+            </Typography>
           </Flex>
 
-          <a href="https://github.com/settings/profile" target="_blank" rel="noopener noreferrer" className="w-fit">
-            <Button variant="secondary" size="s">
-              <Icon remixName="ri-github-fill" />
-              <Translate token="v2.pages.settings.profile.githubAccount.button" />
+          <Flex alignItems="center" className="gap-3">
+            <Button
+              variant="secondary"
+              size="s"
+              className={cn({
+                "border-spacePurple-500 text-spacePurple-500": isRefetching || isLoading,
+              })}
+              onClick={onTriggerResync}
+            >
+              <Icon
+                remixName="ri-refresh-line"
+                className={cn({ "animate-spin text-spacePurple-300": isRefetching || isLoading })}
+              />
+              <Translate token="v2.pages.settings.profile.githubAccount.buttons.resync" />
             </Button>
-          </a>
+
+            <a href="https://github.com/settings/profile" target="_blank" rel="noopener noreferrer" className="w-fit">
+              <Button variant="secondary" size="s">
+                <Icon remixName="ri-github-fill" />
+                <Translate token="v2.pages.settings.profile.githubAccount.buttons.edit" />
+              </Button>
+            </a>
+          </Flex>
         </Flex>
       </Flex>
     </Card>
