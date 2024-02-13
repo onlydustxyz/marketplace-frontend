420f6ffb5f70d1b367d933f80a97a98f0273c33a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-13 13:49:26 +0100

    feat: mandate content

--- components/features/stacks/payments-flow/request-payments-stacks/features/views/mandate/mandate.tsx
+++ components/features/stacks/payments-flow/request-payments-stacks/features/views/mandate/mandate.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
+import { useIntl } from "src/hooks/useIntl";
+
 import { Button } from "components/ds/button/button";
+import { Card } from "components/ds/card/card";
+import { Checkbox } from "components/ds/checkbox/checkbox";
 import { TSelectRewards } from "components/features/stacks/payments-flow/request-payments-stacks/features/views/select-rewards/select-rewards.types";
 import { TRequestPaymentsStacks } from "components/features/stacks/payments-flow/request-payments-stacks/request-payments-stacks.types";
 import { ScrollView } from "components/layout/pages/scroll-view/scroll-view";
@@ -6,9 +12,33 @@ import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
 export function Mandate({ includedRewards, goTo }: TSelectRewards.Props) {
+  const { T } = useIntl();
+  const [accepted, setAccepted] = useState(false);
+
   const onSubmit = () => {
     goTo({ to: TRequestPaymentsStacks.Views.Generate });
   };
+
+  const [termsStart, termsPanel, termsEnd] = T("v2.pages.stacks.request_payments.mandate.terms").split("_");
+
+  function renderAnswers() {
+    return (
+      <ul>
+        {Array.from({ length: 4 }, (_, index) => {
+          const token = `v2.pages.stacks.request_payments.mandate.summary.answer_${index + 1}`;
+          const [start, bold, end] = T(token).split("_");
+
+          return (
+            <li key={token}>
+              {start}
+              <span className="font-semibold">{bold}</span>
+              {end}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 
   return (
     <div className="flex h-full flex-col justify-between">
@@ -22,16 +52,35 @@ export function Mandate({ includedRewards, goTo }: TSelectRewards.Props) {
                 className="text-greyscale-50"
               />
             </div>
-            CONTENT
+            <Typography
+              variant={"title-s"}
+              translate={{ token: "v2.pages.stacks.request_payments.mandate.title" }}
+              className="mb-4"
+            />
+            <Card background={false}>
+              <p className="prose leading-normal text-greyscale-50">
+                <Translate token="v2.pages.stacks.request_payments.mandate.summary.question" />
+                <br />
+                {renderAnswers()}
+              </p>
+            </Card>
           </div>
           <div className="absolute bottom-0 left-0 w-full bg-greyscale-900">
             <div className="grid gap-5 border-t border-card-border-light bg-card-background-light px-8 py-6">
-              CHECKBOX
+              <div className="flex justify-center">
+                <Checkbox onValueChange={setAccepted}>
+                  {termsStart}
+                  <button className="underline" onClick={() => alert("open mandate stack")}>
+                    {termsPanel}
+                  </button>
+                  {termsEnd}
+                </Checkbox>
+              </div>
               <div className="grid w-full grid-cols-2 gap-5">
                 <Button variant="secondary" size="m" className="w-full" onClick={() => goTo({ to: "close" })}>
                   <Translate token="v2.pages.stacks.request_payments.mandate.skip" />
                 </Button>
-                <Button variant="primary" size="m" className="w-full" onClick={onSubmit}>
+                <Button variant="primary" size="m" className="w-full" onClick={onSubmit} disabled={!accepted}>
                   <Translate
                     token="v2.pages.stacks.request_payments.mandate.confirm"
                     params={{ count: includedRewards?.length }}
