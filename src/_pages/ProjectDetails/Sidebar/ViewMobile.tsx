797b72271ffb545bd7c1f631c969811b5c0f798a
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-02-13 11:53:12 +0100

    Optimize images (#1815)

--- src/_pages/ProjectDetails/Sidebar/ViewMobile.tsx
+++ src/_pages/ProjectDetails/Sidebar/ViewMobile.tsx
@@ -6,10 +6,11 @@ import { components } from "src/__generated/api";
 import { UseGetProjectBySlugResponse } from "src/api/Project/queries";
 import BurgerIcon from "src/assets/icons/BurgerIcon";
 import Button, { ButtonSize, ButtonType } from "src/components/Button";
-import RoundedImage, { ImageSize } from "src/components/RoundedImage";
 import SidePanel from "src/components/SidePanel";
 import { useIntl } from "src/hooks/useIntl";
 import CloseLine from "src/icons/CloseLine";
+
+import { Avatar } from "components/ds/avatar/avatar";
 
 import { ProjectDetailsTab } from ".";
 import BackLink from "./BackLink";
@@ -41,12 +42,7 @@ export default function ViewMobile(props: Props) {
             <div className="flex items-center gap-1">
               <BackLink to={RoutePaths.Projects} className="divide-none" />
               <div className="flex items-center gap-2 font-belwe text-2xl">
-                <RoundedImage
-                  src={currentProject?.logoUrl || ""}
-                  useLogoFallback
-                  alt="Project Logo"
-                  size={ImageSize.Sm}
-                />
+                <Avatar src={currentProject?.logoUrl || ""} alt="Project Logo" size="s" />
                 <div className="line-clamp-1">{currentProject.name}</div>
               </div>
             </div>
