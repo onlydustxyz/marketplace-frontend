548906a3a739bd8593797fb05d147d3cf975aa43
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-13 14:01:06 +0100

    delete old mock

--- app/api/invoice/route.tsx
+++ app/api/invoice/route.tsx
@@ -27,60 +27,6 @@ export async function GET() {
     invoiceDetails: invoiceMock,
   });
 
-  const rewards = [
-    {
-      requestedAt: "2023-12-18T15:36:49.04901Z",
-      processedAt: null,
-      projectId: "7d04163c-4187-4313-8066-61504d34fc56",
-      status: "PENDING_INVOICE",
-      unlockDate: null,
-      amount: {
-        total: 100,
-        currency: "LORDS",
-        dollarsEquivalent: 55.6196555954649,
-      },
-      numberOfRewardedContributions: 1,
-      rewardedOnProjectName: "Deluge",
-      rewardedOnProjectLogoUrl:
-        "https://staging-onlydust-app-images.s3.eu-west-1.amazonaws.com/4e53ae9457d9d0ae336ee7cbc183f8a3.png",
-      id: "49ee3934-29f5-43c7-a9e7-6c34947a6e87",
-    },
-    {
-      requestedAt: "2023-12-28T10:58:15.632901Z",
-      processedAt: null,
-      projectId: "7d04163c-4187-4313-8066-61504d34fc56",
-      status: "PENDING_INVOICE",
-      unlockDate: null,
-      amount: {
-        total: 10,
-        currency: "USDC",
-        dollarsEquivalent: 10.00079477650715,
-      },
-      numberOfRewardedContributions: 1,
-      rewardedOnProjectName: "Bretzel",
-      rewardedOnProjectLogoUrl:
-        "https://staging-onlydust-app-images.s3.eu-west-1.amazonaws.com/4e53ae9457d9d0ae336ee7cbc183f8a3.png",
-      id: "5a761880-6175-4fc4-a10d-60a73eba5675",
-    },
-    {
-      requestedAt: "2023-12-28T10:58:15.632901Z",
-      processedAt: null,
-      projectId: "7d04163c-4187-4313-8066-61504d34fc56",
-      status: "PENDING_INVOICE",
-      unlockDate: null,
-      amount: {
-        total: 20,
-        currency: "USDC",
-        dollarsEquivalent: 20.00079477650715,
-      },
-      numberOfRewardedContributions: 1,
-      rewardedOnProjectName: "Onlydust",
-      rewardedOnProjectLogoUrl:
-        "https://staging-onlydust-app-images.s3.eu-west-1.amazonaws.com/4e53ae9457d9d0ae336ee7cbc183f8a3.png",
-      id: "5a761880-6175-4fc4-a10d-60a73eba5678",
-    },
-  ];
-
   const rewardSummary: TInvoice.RewardsSummaryProps = getRewardsSummaryProps({
     invoiceDetails: invoiceMock,
   });
