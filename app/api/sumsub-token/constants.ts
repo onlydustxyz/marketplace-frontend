0c4701e7fc1558cc2a9c093abd7668ed4188cd54
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-13 11:22:50 +0100

    fix: sumsub vars

--- app/api/sumsub-token/constants.ts
+++ app/api/sumsub-token/constants.ts
@@ -3,7 +3,7 @@ export namespace SUMSUB_CONST {
   export const SECRET_KEY = process.env.SUMSUB_SDK_SECRET_KEY ?? "";
   export const BASE_URL = "https://api.sumsub.com";
 
-  export const KYC_LEVEL = process.env.SUMSUB_KYC_LEVEL ?? "";
-  export const KYB_LEVEL = process.env.SUMSUB_KYB_LEVEL ?? "";
+  export const KYC_LEVEL = process.env.NEXT_PUBLIC_SUMSUB_KYC_LEVEL ?? "";
+  export const KYB_LEVEL = process.env.NEXT_PUBLIC_SUMSUB_KYB_LEVEL ?? "";
   export const DEFAULT_LEVEL = KYC_LEVEL;
 }
--- app/migration/settings/billing/hooks/useBillingButton.tsx
+++ app/migration/settings/billing/hooks/useBillingButton.tsx
@@ -1,5 +1,7 @@
 import { ReactElement, ReactNode } from "react";
 
+import { SUMSUB_CONST } from "app/api/sumsub-token/constants";
+
 import { useStackFeedback, useStackVerify } from "src/App/Stacks/Stacks";
 import { UseBillingProfileResponse } from "src/api/me/billing/queries";
 import { MeTypes } from "src/api/me/types";
@@ -23,9 +25,9 @@ export const useBillingButton = ({ status, type, id }: UseBillingButtonProps): U
   function handleVerify() {
     if (type && id) {
       if (type === MeTypes.billingProfileType.Individual) {
-        openVerify({ levelName: "basic-kyc-level", externalId: id });
+        openVerify({ levelName: SUMSUB_CONST.KYC_LEVEL, externalId: id });
       } else {
-        openVerify({ levelName: "basic-kyb-level", externalId: id });
+        openVerify({ levelName: SUMSUB_CONST.KYB_LEVEL, externalId: id });
       }
     }
   }
