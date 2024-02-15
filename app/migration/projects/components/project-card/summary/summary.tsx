88c6df30510af5694f604786b780fd5213745bb5
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-15 18:19:32 +0100

    Pennylane - QA review

--- app/migration/projects/components/project-card/summary/summary.tsx
+++ app/migration/projects/components/project-card/summary/summary.tsx
@@ -4,7 +4,7 @@ import { TSummary } from "./summary.types";
 
 export function Summary({ shortDescription }: TSummary.Props) {
   return (
-    <Typography variant="body-m" className="ml-px line-clamp-2 text-greyscale-200">
+    <Typography variant="body-m" className="ml-px line-clamp-2 max-w-[80%] text-greyscale-200">
       {shortDescription}
     </Typography>
   );
