33d65422a41bb016ce7f849994b436e0351679f1
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-19 14:49:30 +0100

    Clean

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,3 @@ function middleware() {
   return NextResponse.next();
 }
 export default withMigration(middleware);
-
-// check if cookie exist before fecth /me
-// if cookie exist and project is not in list -> fetch /me to check if have auth
-// else fetch /me to check if have auth
