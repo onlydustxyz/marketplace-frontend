9d256f0b98277231010d567ae909c3664c830687
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-19 11:20:35 +0100

    add footer banner to generate invoice

--- components/features/stacks/payments-flow/request-payments-stacks/features/views/generate-invoice/generate-invoice.tsx
+++ components/features/stacks/payments-flow/request-payments-stacks/features/views/generate-invoice/generate-invoice.tsx
@@ -1,9 +1,9 @@
 import { Spinner } from "src/components/Spinner/Spinner";
 
+import { Banner } from "components/ds/banner/banner";
 import { Button } from "components/ds/button/button";
 import { InvoicePreviewLoading } from "components/features/invoice-viewer/invoice-preview.loading";
 import InvoiceViewer from "components/features/invoice-viewer/invoice-viewer";
-import { AmountCounter } from "components/features/stacks/payments-flow/request-payments-stacks/components/amount-counter/amount-counter";
 import { useInvoicePreview } from "components/features/stacks/payments-flow/request-payments-stacks/hooks/use-invoice-preview/use-invoice-preview";
 import { useInvoiceUpload } from "components/features/stacks/payments-flow/request-payments-stacks/hooks/use-invoice-upload/use-invoice-upload";
 import { TRequestPaymentsStacks } from "components/features/stacks/payments-flow/request-payments-stacks/request-payments-stacks.types";
@@ -41,11 +41,22 @@ export function GenerateInvoice({ rewardIds, billingProfileId, goTo }: TGenerate
           />
         </div>
         {/*TODO add guidelines info card*/}
-        <ScrollView className="m-4 w-auto rounded-2xl border border-card-border-light p-4">
+        <ScrollView className="m-4 mb-0 w-auto rounded-2xl border border-card-border-light p-4">
           <div className="relative z-0 flex justify-center">{renderInvoicePreview()}</div>
         </ScrollView>
         <div className="w-full bg-greyscale-900">
-          <AmountCounter total={1000} />
+          <div className="bg-greyscale-900 p-4">
+            <Banner
+              title={<Translate token={"v2.pages.stacks.request_payments.invoiceSubmission.banner.title"} />}
+              description={
+                <Translate token={"v2.pages.stacks.request_payments.invoiceSubmission.banner.description"} />
+              }
+              variant={"medium"}
+              hasBorder={false}
+              icon={{ remixName: "ri-information-line" }}
+              size={"s"}
+            />
+          </div>
           <div className="flex h-auto w-full items-center justify-between gap-5 border-t border-card-border-light bg-card-background-light px-8 py-6">
             {/* // empty div to keep the flex layout */}
             {isLoading || isPendingUploadInvoice ? <Spinner /> : <div />}
