2582fc677f6e631e7e7f6bb91353db7ae8efc999
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-20 12:49:06 +0100

    add setting invoices page

---
+++ app/migration/settings/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { SettingsHeader } from "app/migration/settings/components/settings-header/settings-header";
+
+export default function InvoicesPage() {
+  return (
+    <div className="flex flex-col gap-6">
+      <SettingsHeader title="v2.pages.settings.profile.title" subtitle="v2.pages.settings.profile.subtitle" />
+    </div>
+  );
+}
