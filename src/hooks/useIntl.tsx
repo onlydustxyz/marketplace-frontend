c310b335376f14a56ae368107398c97f4a4bb918
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-20 13:35:12 +0100

    Move translation folder at the root

--- src/hooks/useIntl.tsx
+++ src/hooks/useIntl.tsx
@@ -2,24 +2,23 @@
 
 import { PropsWithChildren } from "react";
 import { Autocomplete, TParams, Talkr, tr, useT } from "talkr";
-
-import en_base from "src/translations/en.json";
-import en_commons from "src/translations/v2/en/commons/commons.json";
-import en_commons_enum from "src/translations/v2/en/commons/enums.json";
-import en_features_banners from "src/translations/v2/en/features/banners.json";
-import en_features_contributors from "src/translations/v2/en/features/contributors.json";
-import en_features_ecosystems from "src/translations/v2/en/features/ecosystems.json";
-import en_features from "src/translations/v2/en/features/features.json";
-import en_features_filters from "src/translations/v2/en/features/filters.json";
-import en_features_leaders from "src/translations/v2/en/features/leaders.json";
-import en_features_menu from "src/translations/v2/en/features/menu.json";
-import en_features_payout_status from "src/translations/v2/en/features/payout-status.json";
-import en_features_sidebar from "src/translations/v2/en/features/sidebar.json";
-import en_features_verify from "src/translations/v2/en/features/verify.json";
-import en_projects from "src/translations/v2/en/pages/projects.json";
-import en_settings_billing from "src/translations/v2/en/pages/settings-billing.json";
-import en_settings_payout from "src/translations/v2/en/pages/settings-payout.json";
-import en_settings_profile from "src/translations/v2/en/pages/settings-profile.json";
+import en_base from "translations/en.json";
+import en_commons from "translations/v2/en/commons/commons.json";
+import en_commons_enum from "translations/v2/en/commons/enums.json";
+import en_features_banners from "translations/v2/en/features/banners.json";
+import en_features_contributors from "translations/v2/en/features/contributors.json";
+import en_features_ecosystems from "translations/v2/en/features/ecosystems.json";
+import en_features from "translations/v2/en/features/features.json";
+import en_features_filters from "translations/v2/en/features/filters.json";
+import en_features_leaders from "translations/v2/en/features/leaders.json";
+import en_features_menu from "translations/v2/en/features/menu.json";
+import en_features_payout_status from "translations/v2/en/features/payout-status.json";
+import en_features_sidebar from "translations/v2/en/features/sidebar.json";
+import en_features_verify from "translations/v2/en/features/verify.json";
+import en_projects from "translations/v2/en/pages/projects.json";
+import en_settings_billing from "translations/v2/en/pages/settings-billing.json";
+import en_settings_payout from "translations/v2/en/pages/settings-payout.json";
+import en_settings_profile from "translations/v2/en/pages/settings-profile.json";
 
 const en = {
   ...en_base,
