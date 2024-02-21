b362a792f955b18085c87b0f886a6a8bc50688ce
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-21 17:28:21 +0100

    wip: basic ui

--- app/migration/settings/payout-preferences/page.tsx
+++ app/migration/settings/payout-preferences/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { SettingsHeader } from "app/migration/settings/components/settings-header/settings-header";
 
-function PayoutPreferencesPage() {
-  return <div>PayoutPreferencesPage</div>;
+import { Banner } from "components/ds/banner/banner";
+import { Card } from "components/ds/card/card";
+import { Translate } from "components/layout/translate/translate";
+
+export default function PayoutPreferencesPage() {
+  return (
+    <>
+      <SettingsHeader
+        title="v2.pages.settings.payoutPreferences.title"
+        subtitle="v2.pages.settings.payoutPreferences.subtitle"
+      />
+
+      <Card background="base">Table</Card>
+
+      <Banner
+        variant="base"
+        title={<Translate token="v2.pages.settings.payoutPreferences.warning" />}
+        icon={{ remixName: "ri-error-warning-line" }}
+        size="s"
+        hasBorder
+      />
+    </>
+  );
 }
-
-export default PayoutPreferencesPage;
