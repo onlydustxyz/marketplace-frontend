65dbafc18b3955c0a7e962332b13f7d1fb980ba3
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-21 19:12:39 +0100

    add global to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     setupFiles: ["src/test/setup.ts"],
     environment: "jsdom",
     exclude: [...configDefaults.exclude],
