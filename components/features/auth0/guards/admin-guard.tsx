b69f0cddf365c3ce9144d8fa1add3961878f3fa1
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-02-21 15:33:02 +0100

    fix: impersonation redirect

--- components/features/auth0/guards/admin-guard.tsx
+++ components/features/auth0/guards/admin-guard.tsx
@@ -3,21 +3,24 @@ import { ComponentType, useEffect } from "react";
 
 import MeApi from "src/api/me";
 
+import { useImpersonation } from "components/features/impersonation/use-impersonation";
+
 import { NEXT_ROUTER } from "constants/router";
 
 export function withAdminGuard<P extends object>(Component: ComponentType<P>) {
   // eslint-disable-next-line react/display-name
   return (props: P) => {
     const router = useRouter();
+    const { isImpersonating } = useImpersonation();
 
     const { isLoading, isRefetching, data } = MeApi.queries.useGetMe({});
     const { isAdmin } = data ?? {};
 
     useEffect(() => {
-      if (isLoading || isRefetching || isAdmin) return;
+      if (isLoading || isRefetching || isAdmin || isImpersonating) return;
 
       router.push(NEXT_ROUTER.notFound);
-    }, [isLoading, isRefetching, isAdmin]);
+    }, [isLoading, isRefetching, isAdmin, isImpersonating]);
 
     if (isLoading || isRefetching || !isAdmin) {
       return <></>;
