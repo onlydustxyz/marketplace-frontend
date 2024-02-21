c834ac1d8ad2251daf9936db06ca3fa35ce4ac67
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-21 15:39:54 +0100

    fix upload file type and size limit

--- components/features/stacks/payments-flow/request-payments-stacks/components/upload-file/upload-file.tsx
+++ components/features/stacks/payments-flow/request-payments-stacks/components/upload-file/upload-file.tsx
@@ -1,16 +1,25 @@
 import React, { useRef } from "react";
 
+import { useIntl } from "src/hooks/useIntl";
+import { useShowToaster } from "src/hooks/useToaster";
+
 import { Card } from "components/ds/card/card";
 import { IconTag } from "components/ds/icon-tag/icon-tag";
 import { TUploadFile } from "components/features/stacks/payments-flow/request-payments-stacks/components/upload-file/upload-file.types";
 import { Translate } from "components/layout/translate/translate";
 
 export function UploadFile({ setSelectedFile }: TUploadFile.Props) {
+  const { T } = useIntl();
+  const showToaster = useShowToaster();
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   function handleOnChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    if (event.target.files && event.target.files.length > 0) {
-      setSelectedFile(event.target.files[0]);
+    if (event.target.files) {
+      if (event.target.files[0].size > 3000000) {
+        showToaster(T("v2.pages.stacks.request_payments.uploadInvoice.errorMaxSizeFile"), { isError: true });
+      } else {
+        setSelectedFile(event.target.files[0]);
+      }
     }
   }
 
@@ -34,7 +43,7 @@ export function UploadFile({ setSelectedFile }: TUploadFile.Props) {
         />{" "}
         <Translate token="v2.pages.stacks.request_payments.uploadInvoice.dragAndDrop" />
       </div>
-      <input type="file" ref={inputRef} onChange={handleOnChange} className="hidden" />
+      <input type="file" ref={inputRef} onChange={handleOnChange} className="hidden" accept="application/pdf" />
     </Card>
   );
 }
