dfb51f0324dc29e27888e0a47d45361d83e29ca4
Mehdi Hamri <pixelfact.company@gmail.com>
2024-02-22 14:40:58 +0100

    try config

--- app/api/invoice/route.tsx
+++ app/api/invoice/route.tsx
@@ -2,13 +2,9 @@ import { renderToStream } from "@react-pdf/renderer";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-import { getHeaderProps, getInvoiceInfoProps, getRewardsSummaryProps } from "app/api/invoice/builders/builders";
 import { fetchInvoicePreviewData } from "app/api/invoice/handlers/fetch-invoice-preview-data";
 
-import { MeTypes } from "src/api/me/types";
-
 import { InvoiceTemplate } from "components/features/invoice-template/invoice-template";
-import { TInvoice } from "components/features/invoice-template/invoice-template.types";
 
 export async function GET(request: NextRequest) {
   /* ------
@@ -24,7 +20,7 @@ export async function GET(request: NextRequest) {
   Retrieve the invoice preview data
   ------ */
   const searchParams = request.nextUrl.searchParams;
-  const isSample = searchParams.get("isSample") ?? "false";
+  // const isSample = searchParams.get("isSample") ?? "false";
   const billingProfileId = searchParams.get("billingProfileId") ?? "";
   const rewardIds = searchParams.get("rewardIds") ?? "";
   let invoicePreviewData;
@@ -37,28 +33,28 @@ export async function GET(request: NextRequest) {
   /* ------
   Determine the billing profile type
   ------ */
-  const isUserIndividual = invoicePreviewData?.billingProfileType === MeTypes.billingProfileType.Individual;
+  // const isUserIndividual = invoicePreviewData?.billingProfileType === MeTypes.billingProfileType.Individual;
 
   /* ------
   Build the invoice content
   ------ */
-  const header: TInvoice.HeaderProps = getHeaderProps({
-    isUserIndividual,
-    isSample,
-    invoiceNumber: invoicePreviewData.number,
-  });
-  const invoiceInfo: TInvoice.InvoiceInfoProps = getInvoiceInfoProps({
-    isUserIndividual,
-    invoiceDetails: invoicePreviewData,
-  });
-  const rewardSummary: TInvoice.RewardsSummaryProps = getRewardsSummaryProps({
-    invoiceDetails: invoicePreviewData,
-  });
-  const footer = {
-    invoiceName: isUserIndividual
-      ? `${invoicePreviewData.individualBillingProfile?.firstName} ${invoicePreviewData.individualBillingProfile?.lastName}`
-      : `${invoicePreviewData.companyBillingProfile?.name}`,
-  };
+  // const header: TInvoice.HeaderProps = getHeaderProps({
+  //   isUserIndividual,
+  //   isSample,
+  //   invoiceNumber: invoicePreviewData.number,
+  // });
+  // const invoiceInfo: TInvoice.InvoiceInfoProps = getInvoiceInfoProps({
+  //   isUserIndividual,
+  //   invoiceDetails: invoicePreviewData,
+  // });
+  // const rewardSummary: TInvoice.RewardsSummaryProps = getRewardsSummaryProps({
+  //   invoiceDetails: invoicePreviewData,
+  // });
+  // const footer = {
+  //   invoiceName: isUserIndividual
+  //     ? `${invoicePreviewData.individualBillingProfile?.firstName} ${invoicePreviewData.individualBillingProfile?.lastName}`
+  //     : `${invoicePreviewData.companyBillingProfile?.name}`,
+  // };
 
   /* ------
   Create a stream containing the pdf blob
@@ -66,9 +62,7 @@ export async function GET(request: NextRequest) {
   let stream;
 
   try {
-    stream = await renderToStream(
-      <InvoiceTemplate header={header} invoiceInfos={invoiceInfo} rewardSummary={rewardSummary} footer={footer} />
-    );
+    stream = await renderToStream(<InvoiceTemplate />);
     if (!stream) {
       return new NextResponse("Internal Server Error (!stream)", { status: 500 });
     }
--- components/features/invoice-template/invoice-template.tsx
+++ components/features/invoice-template/invoice-template.tsx
@@ -1,47 +1,17 @@
-import { Document, Font, Page } from "@react-pdf/renderer";
-import React from "react";
+// Font.register({
+//   family: "GT Walsheim",
+//   fonts: [
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Thin.ttf", fontWeight: 100 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Ultra-Light.ttf", fontWeight: 200 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Light.ttf", fontWeight: 300 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Regular.ttf", fontWeight: 400 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Medium.ttf", fontWeight: 500 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Bold.ttf", fontWeight: 700 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Ultra-Bold.ttf", fontWeight: 800 },
+//     { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Black.ttf", fontWeight: 900 },
+//   ],
+// });
 
-import { styles } from "components/features/invoice-template/invoice-template.styles";
-import { TInvoice } from "components/features/invoice-template/invoice-template.types";
-import { InvoiceFooter } from "components/features/invoice-template/sections/invoice-footer";
-import { InvoiceHeader } from "components/features/invoice-template/sections/invoice-header";
-import { InvoiceInfo } from "components/features/invoice-template/sections/invoice-infos";
-import { InvoiceSummary } from "components/features/invoice-template/sections/invoice-summary";
-
-Font.register({
-  family: "GT Walsheim",
-  fonts: [
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Thin.ttf", fontWeight: 100 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Ultra-Light.ttf", fontWeight: 200 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Light.ttf", fontWeight: 300 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Regular.ttf", fontWeight: 400 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Medium.ttf", fontWeight: 500 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Bold.ttf", fontWeight: 700 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Ultra-Bold.ttf", fontWeight: 800 },
-    { src: "src/assets/fonts/GTWalsheimPro/GT-Walsheim-Pro-Black.ttf", fontWeight: 900 },
-  ],
-});
-
-export function InvoiceTemplate({ header, invoiceInfos, rewardSummary, footer }: TInvoice.InvoiceTemplateProps) {
-  return (
-    <Document pageLayout="oneColumn">
-      <Page size="A4" style={styles.page}>
-        <InvoiceHeader title={header.title} />
-        <InvoiceInfo
-          senderInfos={invoiceInfos.senderInfos}
-          recipientInfos={invoiceInfos.recipientInfos}
-          legalInfos={invoiceInfos.legalInfos}
-          isUserIndividual={invoiceInfos.isUserIndividual}
-        />
-        <InvoiceSummary
-          rewards={rewardSummary.rewards}
-          vat={rewardSummary.vat}
-          totalBeforeTax={rewardSummary.totalBeforeTax}
-          totalTax={rewardSummary.totalTax}
-          totalAfterTax={rewardSummary.totalAfterTax}
-        />
-        <InvoiceFooter invoiceName={footer.invoiceName} />
-      </Page>
-    </Document>
-  );
+export function InvoiceTemplate() {
+  return <div>hello</div>;
 }
