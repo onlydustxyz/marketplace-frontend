3f93e2aab838492ada67610d66ac3d5febc34fee
Alexis Benoliel <alexbenoliel@gmail.com>
2024-02-26 10:36:30 +0100

    CI

--- e2e/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("has title", async ({ page }) => {
-  await page.goto("metadata/p/deluge/opengraph-image");
-  await expect(page).toHaveScreenshot();
-});
---
+++ e2e/opengraph-image.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "@playwright/test";
+
+test("should generate generic metadata image", async ({ page }) => {
+  await page.goto("/opengraph-image");
+  await expect(page).toHaveScreenshot();
+});
+
+test("should generate project opengraph image", async ({ page }) => {
+  await page.goto("metadata/p/deluge/opengraph-image");
+  await expect(page).toHaveScreenshot();
+});
+
+test("should generate user opengraph image", async ({ page }) => {
+  await page.goto("metadata/u/PierreOucif/opengraph-image");
+  await expect(page).toHaveScreenshot();
+});
