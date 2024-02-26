1a72b8868298c48ced834783115c0adcea0b1336
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-02-26 11:47:38 +0100

    request payment button to be primary

--- src/_pages/Rewards/request-payment/request-payment.tsx
+++ src/_pages/Rewards/request-payment/request-payment.tsx
@@ -15,7 +15,7 @@ export function RequestPayment() {
 
   return (
     <div className="item-center flex w-full flex-row justify-end">
-      <Button variant="multi-color" onClick={open} size="s">
+      <Button variant="primary" onClick={open} size="s">
         <Translate token="v2.pages.stacks.request_payments.openButton" />
         <div className="flex h-5 w-5 items-center justify-center rounded-full bg-card-background-heavy">
           <Typography variant="body-s-bold">{rewardsPendingInvoice.rewards.length}</Typography>
