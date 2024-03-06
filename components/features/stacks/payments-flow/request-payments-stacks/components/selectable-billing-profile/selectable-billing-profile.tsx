8f0bc3500b44023cfd17d66768dc7cc6252590f4
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-03-06 16:31:49 +0100

    handle disable state

--- components/features/stacks/payments-flow/request-payments-stacks/components/selectable-billing-profile/selectable-billing-profile.tsx
+++ components/features/stacks/payments-flow/request-payments-stacks/components/selectable-billing-profile/selectable-billing-profile.tsx
@@ -17,22 +17,34 @@ export function SelectableBillingProfile({
   selected,
   onChange,
   value,
+  disabled,
 }: TSelectableBillingProfile.Props) {
   function onClick() {
     onChange(value);
   }
   return (
-    <Card background={false} border={selected ? "heavy" : "light"} className="p-4" onClick={onClick}>
+    <Card
+      background={false}
+      border={selected ? "heavy" : "light"}
+      className={cn("p-4", { "cursor-not-allowed": disabled })}
+      onClick={!disabled ? onClick : undefined}
+    >
       <Flex justifyContent="between" alignItems="center" className="gap-1">
         <Flex justifyContent="start" alignItems="center" className="gap-4">
           <Chip className="h-8 w-8">
-            <Icon {...icon} className={cn("h-4 w-4", icon.className)} />
+            <Icon {...icon} className={cn("h-4 w-4", icon.className, { "text-greyscale-500": disabled })} />
           </Chip>
           <div>
-            <Typography variant={"title-s"} className="mb-0.5 text-greyscale-50">
+            <Typography
+              variant={"title-s"}
+              className={cn("mb-0.5 capitalize text-greyscale-50", { "text-greyscale-500": disabled })}
+            >
               {title}
             </Typography>
-            <Typography variant={"title-s"} className="mb-0.5 text-greyscale-50">
+            <Typography
+              variant={"body-s"}
+              className={cn("mb-0.5 text-spacePurple-500", { "text-greyscale-500": disabled })}
+            >
               <Translate
                 token="v2.pages.stacks.request_payments.selectBillingProfile.rewardsCount"
                 params={{ count }}
@@ -40,14 +52,17 @@ export function SelectableBillingProfile({
             </Typography>
           </div>
         </Flex>
-        <Checkbox
-          radius="full"
-          isSelected={selected}
-          className="pointer-events-none"
-          classNames={{
-            wrapper: "after:bg-spacePurple-500",
-          }}
-        />
+        {!disabled ? (
+          <Checkbox
+            radius="full"
+            isSelected={selected}
+            className="pointer-events-none"
+            classNames={{
+              wrapper: "after:bg-spacePurple-500",
+            }}
+            disabled={disabled}
+          />
+        ) : null}
       </Flex>
     </Card>
   );
