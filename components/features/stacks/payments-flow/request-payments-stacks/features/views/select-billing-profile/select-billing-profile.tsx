cbd579dbc3618e0b3748cce7b6668507872cdac5
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-03-06 17:58:52 +0100

    add empty state

--- components/features/stacks/payments-flow/request-payments-stacks/features/views/select-billing-profile/select-billing-profile.tsx
+++ components/features/stacks/payments-flow/request-payments-stacks/features/views/select-billing-profile/select-billing-profile.tsx
@@ -1,3 +1,4 @@
+import { IMAGES } from "src/assets/img";
 import { Spinner } from "src/components/Spinner/Spinner";
 
 import { Button } from "components/ds/button/button";
@@ -7,6 +8,7 @@ import { UseBillingProfileIcons } from "components/features/stacks/payments-flow
 import { TRequestPaymentsStacks } from "components/features/stacks/payments-flow/request-payments-stacks/request-payments-stacks.types";
 import { Flex } from "components/layout/flex/flex";
 import { ScrollView } from "components/layout/pages/scroll-view/scroll-view";
+import { EmptyState } from "components/layout/placeholders/empty-state/empty-state";
 import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
@@ -37,31 +39,44 @@ export function SelectBillingProfile({
                 className="text-greyscale-50"
               />
             </div>
-            <Typography
-              variant={"special-label"}
-              translate={{ token: "v2.pages.stacks.request_payments.selectBillingProfile.title" }}
-              className="mb-4 text-spaceBlue-200"
-            />
-            <Flex justifyContent="start" direction={"col"} className="gap-4">
-              {billingProfiles?.length ? (
-                <RadioGroupCustom onChange={onSelectBillingProfile} value={selectedBillingProfile?.id ?? ""}>
-                  {({ value, onChange }) =>
-                    billingProfiles?.map(profile => (
-                      <SelectableBillingProfile
-                        key={profile.id}
-                        title={profile.name}
-                        count={profile.rewardCount ?? 0}
-                        icon={{ remixName: billingProfilesIcons[profile.type] }}
-                        disabled={profile.rewardCount === 0}
-                        onChange={onChange}
-                        selected={value === selectedBillingProfile?.id}
-                        value={profile.id}
-                      />
-                    ))
-                  }
-                </RadioGroupCustom>
-              ) : null}
-            </Flex>
+
+            {billingProfiles?.length ? (
+              <>
+                <Typography
+                  variant={"special-label"}
+                  translate={{ token: "v2.pages.stacks.request_payments.selectBillingProfile.title" }}
+                  className="mb-4 text-spaceBlue-200"
+                />
+                <Flex justifyContent="start" direction={"col"} className="gap-4">
+                  <RadioGroupCustom onChange={onSelectBillingProfile} value={selectedBillingProfile?.id ?? ""}>
+                    {({ value, onChange }) =>
+                      billingProfiles?.map(profile => (
+                        <SelectableBillingProfile
+                          key={profile.id}
+                          title={profile.name}
+                          count={profile.rewardCount ?? 0}
+                          icon={{ remixName: billingProfilesIcons[profile.type] }}
+                          disabled={profile.rewardCount === 0}
+                          onChange={onChange}
+                          selected={value === selectedBillingProfile?.id}
+                          value={profile.id}
+                        />
+                      ))
+                    }
+                  </RadioGroupCustom>
+                </Flex>
+              </>
+            ) : (
+              <div className="flex w-full flex-col py-6">
+                <EmptyState
+                  illustrationSrc={IMAGES.global.categories}
+                  title={{ token: "v2.pages.stacks.request_payments.selectBillingProfile.emptyState.title" }}
+                  description={{
+                    token: "v2.pages.stacks.request_payments.selectBillingProfile.emptyState.description",
+                  }}
+                />
+              </div>
+            )}
           </div>
           <div className="absolute bottom-0 left-0 w-full bg-greyscale-900">
             <div className="flex h-auto w-full items-center justify-between gap-5 border-t border-card-border-light bg-card-background-light px-8 py-6">
