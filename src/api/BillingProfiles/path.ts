1b0037ac66e515a07cb634691ca16a44266047a3
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-03-20 15:04:43 +0100

    add update coworker role (#1957)

--- src/api/BillingProfiles/path.ts
+++ src/api/BillingProfiles/path.ts
@@ -17,4 +17,6 @@ export const BILLING_PROFILES_PATH = {
   INVITE_COWORKER_BY_ID: (billingProfileId: string) => BASE_API_V1(`billing-profiles/${billingProfileId}/coworkers`),
   DELETE_COWORKER_BY_ID: (billingProfileId: string, githubUserId: string) =>
     BASE_API_V1(`billing-profiles/${billingProfileId}/coworkers/${githubUserId}`),
+  UPDATE_COWORKER_ROLE: (billingProfileId: string, githubUserId: string) =>
+    BASE_API_V1(`billing-profiles/${billingProfileId}/coworkers/${githubUserId}/role`),
 };
