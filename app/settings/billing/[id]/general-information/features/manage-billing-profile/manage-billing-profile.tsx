7fac2266532676755226f94d18c402d9a2df7d55
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-03-20 16:44:09 +0100

    refactor iconName

--- app/settings/billing/[id]/general-information/features/manage-billing-profile/manage-billing-profile.tsx
+++ app/settings/billing/[id]/general-information/features/manage-billing-profile/manage-billing-profile.tsx
@@ -1,5 +1,5 @@
 import { useParams, useRouter } from "next/navigation";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 import { TManageBillingProfile } from "app/settings/billing/[id]/general-information/features/manage-billing-profile/manage-billing-profile.types";
 
@@ -10,10 +10,16 @@ import { useIntl } from "src/hooks/useIntl";
 import { Button } from "components/ds/button/button";
 import { ConfirmationModal } from "components/ds/modals/confirmation/confirmation";
 import { Icon } from "components/layout/icon/icon";
+import { RemixIconsName } from "components/layout/icon/remix-icon-names.types";
 import { Translate } from "components/layout/translate/translate";
 
 import { NEXT_ROUTER } from "constants/router";
 
+const iconName: Record<string, RemixIconsName> = {
+  delete: "ri-delete-bin-2-line",
+  disable: "ri-forbid-2-line",
+  enable: "ri-refresh-line",
+};
 export function ManageBillingProfile({ actionType }: TManageBillingProfile.Props) {
   const { T } = useIntl();
   const router = useRouter();
@@ -81,21 +87,10 @@ export function ManageBillingProfile({ actionType }: TManageBillingProfile.Props
     }
   }
 
-  const iconName = useMemo(() => {
-    switch (actionType) {
-      case "delete":
-        return "ri-delete-bin-2-line";
-      case "disable":
-        return "ri-forbid-2-line";
-      case "enable":
-        return "ri-refresh-line";
-    }
-  }, [actionType]);
-
   return (
     <div className="mt-6 w-max">
       <Button variant="secondary" size="s" onClick={onOpenConfirmation}>
-        <Icon remixName={iconName} />
+        <Icon remixName={iconName[actionType]} />
         <Translate
           token="v2.pages.settings.billing.information.manageBillingProfile.button"
           params={{
