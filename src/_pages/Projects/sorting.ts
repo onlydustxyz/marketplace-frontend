febd646ea6d628617461e9696a88cacb29d0179e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-03-20 16:12:49 +0100

    refactor(projects): remove legacy

--- src/_pages/Projects/sorting.ts
+++
@@ -1,8 +0,0 @@
-export enum Sorting {
-  Trending = "RANK",
-  ProjectName = "NAME",
-  ReposCount = "REPO_COUNT",
-  ContributorsCount = "CONTRIBUTOR_COUNT",
-}
-
-export const PROJECT_SORTINGS = [Sorting.Trending, Sorting.ProjectName, Sorting.ReposCount, Sorting.ContributorsCount];
