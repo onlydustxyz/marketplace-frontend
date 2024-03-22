b330acc54bb709077d80a733ffa418f615ddbeb5
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-03-22 09:32:32 +0100

    Button polymorphism (#1965)

--- components/ds/button/button.types.ts
+++ components/ds/button/button.types.ts
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ElementType, PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef, ElementType } from "react";
 import { VariantProps } from "tailwind-variants";
 
 import { buttonPrimaryVariants } from "./variants/button-primary.variants";
@@ -7,14 +7,15 @@ import { buttonTertiaryVariants } from "./variants/button-tertiary.variants";
 import { buttonVariants } from "./variants/button.variants";
 
 export namespace TButton {
-  type HtmlButton = ButtonHTMLAttributes<HTMLButtonElement>;
-
   export type Variants = VariantProps<typeof buttonVariants>;
   export type PrimaryVariants = VariantProps<typeof buttonPrimaryVariants>;
   export type SecondaryVariants = VariantProps<typeof buttonSecondaryVariants>;
   export type TertiaryVariants = VariantProps<typeof buttonTertiaryVariants>;
 
-  export interface Props extends PropsWithChildren, Variants, HtmlButton {
-    as?: ElementType;
-  }
+  export const DEFAULT_ELEMENT_TYPE = "button";
+
+  export type Props<T extends ElementType = typeof DEFAULT_ELEMENT_TYPE> = Variants &
+    ComponentPropsWithoutRef<T> & {
+      as?: T;
+    };
 }
