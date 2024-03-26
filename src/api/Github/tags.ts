95f6dc8830a03b63484fbc13d78d21ca8e51acad
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-03-26 16:55:48 +0100

    delete Github front API

--- src/api/Github/tags.ts
+++
@@ -1,7 +0,0 @@
-import { RESSOURCE_TAGS } from "src/api/ressource-tags";
-
-export const GITHUB_TAGS = {
-  all: [RESSOURCE_TAGS.GITHUB],
-  installations: () => [...GITHUB_TAGS.all, "installations"],
-  installation: (installations_id: string) => [...GITHUB_TAGS.installations(), { installations_id }],
-};
