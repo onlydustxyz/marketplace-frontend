1d0ac81c0dfb7ab7611b10da8948bca4bc04a917
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-03-28 18:24:37 +0100

    wip(sponsor): panel state

--- app/sponsor/components/sponsor-side-panels/sponsor-side-panels.types.ts
+++ app/sponsor/components/sponsor-side-panels/sponsor-side-panels.types.ts
@@ -1,8 +1,10 @@
 import { TButton } from "components/ds/button/button.types";
+import { TSponsorProjectStack } from "components/features/stacks/sponsor-project-stack/sponsor-project-stack.types";
 
 export namespace TSponsorSidePanels {
   export interface Props {
     panel: "fillout" | "project";
     buttonProps?: TButton.Props;
+    projectParams?: TSponsorProjectStack.Props;
   }
 }
