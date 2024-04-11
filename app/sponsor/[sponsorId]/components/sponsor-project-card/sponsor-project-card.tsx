6670ae958c5d35ffb19311e571eda51db3ef6451
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-04-11 14:59:57 +0200

    feat(sponsor): projects

--- app/sponsor/[sponsorId]/components/sponsor-project-card/sponsor-project-card.tsx
+++ app/sponsor/[sponsorId]/components/sponsor-project-card/sponsor-project-card.tsx
@@ -1,25 +1,30 @@
+import { useMediaQuery } from "usehooks-ts";
 import { Money } from "utils/Money/Money";
 
+import { TSponsorProjectCard } from "app/sponsor/[sponsorId]/components/sponsor-project-card/sponsor-project-card.types";
 import { SponsorSidePanels } from "app/sponsor/[sponsorId]/components/sponsor-side-panels/sponsor-side-panels";
+
+import { viewportConfig } from "src/config";
 
 import { Avatar } from "components/ds/avatar/avatar";
 import { Card } from "components/ds/card/card";
+import { SkeletonEl } from "components/ds/skeleton/skeleton";
 import { Icon } from "components/layout/icon/icon";
 import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
-export function SponsorProjectCard() {
+export function SponsorProjectCard({ name, logoUrl, remainingBudgets }: TSponsorProjectCard.Props) {
   return (
-    <Card background={"base"} className={"grid gap-6"}>
+    <Card background={"base"} className={"grid content-start gap-6"}>
       <header className={"flex items-center justify-between overflow-hidden"}>
         <Avatar.Labelled
-          avatarProps={{ src: "", alt: "", size: "m", shape: "square" }}
-          labelProps={{ title: "" }}
+          avatarProps={{ src: logoUrl, alt: name, size: "m", shape: "square" }}
+          labelProps={{ title: name }}
           className={"flex-1"}
           truncate
         >
           <Typography variant={"body-xl-bold"} className={"truncate"}>
-            Madara
+            {name}
           </Typography>
         </Avatar.Labelled>
 
@@ -48,9 +53,10 @@ export function SponsorProjectCard() {
           <Translate token={"v2.pages.sponsor.project.totalBudget"} />
         </Typography>
 
-        <Avatar.Labelled avatarProps={{ src: "", alt: "", size: "s" }}>
+        <Avatar.Labelled avatarProps={{ src: Money.USD.logoUrl, alt: Money.USD.name, size: "s" }}>
           <Typography variant={"body-l-bold"}>
             {
+              // TODO @hayden get total USD amount
               Money.format({
                 amount: 123123,
                 currency: Money.USD,
@@ -61,36 +67,40 @@ export function SponsorProjectCard() {
         </Avatar.Labelled>
       </Card>
 
-      <div className={"grid gap-2"}>
-        <Typography variant={"body-s-bold"} className={"uppercase text-spaceBlue-200"}>
-          <Translate token={"v2.pages.sponsor.project.perCurrency"} />
-        </Typography>
-        <ul className={"grid gap-2"}>
-          <li className={"flex items-center justify-between"}>
-            <Avatar.Labelled avatarProps={{ src: "", alt: "", size: "xs" }}>
-              <Typography variant={"body-m"}>
-                {
-                  Money.format({
-                    amount: 123123,
-                    currency: Money.USD,
-                    options: { currencyClassName: "od-text-body-s" },
-                  }).html
-                }
-              </Typography>
-            </Avatar.Labelled>
+      {remainingBudgets.length ? (
+        <div className={"grid gap-2"}>
+          <Typography variant={"body-s-bold"} className={"uppercase text-spaceBlue-200"}>
+            <Translate token={"v2.pages.sponsor.project.perCurrency"} />
+          </Typography>
+          <ul className={"grid gap-2"}>
+            {remainingBudgets.map(({ amount, currency, usdEquivalent }, i) => (
+              <li key={`${amount}_${i}`} className={"flex items-center justify-between"}>
+                <Avatar.Labelled avatarProps={{ src: currency.logoUrl, alt: currency.name, size: "xs" }}>
+                  <Typography variant={"body-m"}>
+                    {
+                      Money.format({
+                        amount,
+                        currency,
+                        options: { currencyClassName: "od-text-body-s" },
+                      }).html
+                    }
+                  </Typography>
+                </Avatar.Labelled>
 
-            <Typography variant={"body-s-bold"} className={"text-spaceBlue-200"}>
-              {
-                Money.format({
-                  amount: 1234,
-                  currency: Money.USD,
-                  options: { prefixAmountWithTilde: true },
-                }).string
-              }
-            </Typography>
-          </li>
-        </ul>
-      </div>
+                <Typography variant={"body-s-bold"} className={"text-spaceBlue-200"}>
+                  {
+                    Money.format({
+                      amount: usdEquivalent,
+                      currency: Money.USD,
+                      options: { prefixAmountWithTilde: true },
+                    }).string
+                  }
+                </Typography>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
 
       <SponsorSidePanels
         panel={"project"}
@@ -113,3 +123,9 @@ export function SponsorProjectCard() {
     </Card>
   );
 }
+
+SponsorProjectCard.Skeleton = function SponsorProjectCardSkeleton() {
+  const isSm = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.sm}px)`);
+  const height = isSm ? "256px" : "296px";
+  return <SkeletonEl width="100%" height={height} variant="rounded" />;
+};
--- app/sponsor/[sponsorId]/features/sponsor-section-project/sponsor-section-project.tsx
+++ app/sponsor/[sponsorId]/features/sponsor-section-project/sponsor-section-project.tsx
@@ -1,10 +1,40 @@
+import { isString } from "lodash";
+import { useParams } from "next/navigation";
+
 import { SponsorProjectCard } from "app/sponsor/[sponsorId]/components/sponsor-project-card/sponsor-project-card";
 import { SponsorSidePanels } from "app/sponsor/[sponsorId]/components/sponsor-side-panels/sponsor-side-panels";
+
+import SponsorApi from "src/api/Sponsors";
 
 import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
 export function SponsorSectionProject() {
+  const { sponsorId } = useParams();
+
+  const sponsorIdIsString = isString(sponsorId);
+
+  const { data, isLoading } = SponsorApi.queries.useGetSponsorById({
+    params: {
+      sponsorId: sponsorIdIsString ? sponsorId : "",
+    },
+    options: {
+      enabled: sponsorIdIsString,
+    },
+  });
+
+  function renderProjects() {
+    if (isLoading) {
+      return Array.from({ length: 3 }).map((_, i) => <SponsorProjectCard.Skeleton key={i} />);
+    }
+
+    if (data) {
+      return data.projects.map((project, i) => <SponsorProjectCard key={`${project.name}_${i}`} {...project} />);
+    }
+
+    return null;
+  }
+
   return (
     <section className={"grid gap-5"}>
       <header className={"grid gap-3 sm:flex sm:items-center sm:justify-between"}>
@@ -22,11 +52,7 @@ export function SponsorSectionProject() {
         />
       </header>
 
-      <div className={"grid gap-3 sm:grid-cols-2 sm:gap-6 xl:grid-cols-3"}>
-        <SponsorProjectCard />
-        <SponsorProjectCard />
-        <SponsorProjectCard />
-      </div>
+      <div className={"grid gap-3 sm:grid-cols-2 sm:gap-6 xl:grid-cols-3"}>{renderProjects()}</div>
     </section>
   );
 }
