809c403258e44564580239b59b7677472b97a9ba
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-04-15 14:56:38 +0200

    refactor project budget

--- components/features/project-budget/project-budget.tsx
+++ components/features/project-budget/project-budget.tsx
@@ -7,71 +7,43 @@ import { CurrencyIcons } from "src/components/Currency/CurrencyIcon";
 import { TProjectBudget } from "components/features/project-budget/project-budget.types";
 import { Typography } from "components/layout/typography/typography";
 
-export function ProjectBudget({ selectedBudget, rewardAmount }: TProjectBudget.Props) {
+function BudgetInfoRow({ label, amount, currency }: TProjectBudget.BudgetInfoRowProps) {
+  return (
+    <div className="flex flex-row justify-between">
+      <Typography variant="body-s" className="text-greyscale-50">
+        {label}
+      </Typography>
+      <div className="flex flex-row items-center gap-2">
+        <Typography variant="body-s" className="text-greyscale-50">
+          {Money.format({ amount, currency }).string}
+        </Typography>
+        <Chip solid className="h-5 w-5 flex-shrink-0">
+          <CurrencyIcons currency={Money.fromSchema({ code: currency.code })} className="h-4 w-4" />
+        </Chip>
+      </div>
+    </div>
+  );
+}
+
+export function ProjectBudget({ selectedBudget, rewardAmount }: TProjectBudget.ProjectBudgetProps) {
+  const rewardAmountParsed = useMemo(() => parseFloat(rewardAmount) || 0, [rewardAmount]);
   const budgetAfterReward = useMemo(() => {
-    if (!selectedBudget || isNaN(parseFloat(rewardAmount))) return 0;
-    return selectedBudget?.remaining - parseFloat(rewardAmount);
-  }, [selectedBudget, rewardAmount]);
+    if (!selectedBudget) return 0;
+    return selectedBudget.remaining - rewardAmountParsed;
+  }, [selectedBudget, rewardAmountParsed]);
 
   return (
     <div className="flex flex-col gap-3">
       <Typography variant="body-s" className="uppercase text-spaceBlue-200">
         Project Budget
       </Typography>
-      <div className="flex flex-row justify-between">
-        <Typography variant="body-s" className="text-greyscale-50">
-          Current budget
-        </Typography>
-        <div className="flex flex-row items-center gap-2">
-          <Typography variant="body-s" className="text-greyscale-50">
-            {
-              Money.format({
-                amount: selectedBudget?.remaining,
-                currency: selectedBudget?.currency,
-              }).string
-            }
-          </Typography>
-          <Chip solid className="h-5 w-5 flex-shrink-0">
-            <CurrencyIcons currency={Money.fromSchema({ code: selectedBudget?.currency.code })} className="h-4 w-4" />
-          </Chip>
-        </div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <Typography variant="body-s" className="text-greyscale-50">
-          Amount rewarded
-        </Typography>
-        <div className="flex flex-row items-center gap-2">
-          <Typography variant="body-s" className="text-greyscale-50">
-            {
-              Money.format({
-                amount: isNaN(parseFloat(rewardAmount)) ? 0 : parseFloat(rewardAmount),
-                currency: selectedBudget?.currency,
-              }).string
-            }
-          </Typography>
-          <Chip solid className="h-5 w-5 flex-shrink-0">
-            <CurrencyIcons currency={Money.fromSchema({ code: selectedBudget?.currency.code })} className="h-4 w-4" />
-          </Chip>
-        </div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <Typography variant="body-s" className="text-greyscale-50">
-          Budget after reward
-        </Typography>
-        <div className="flex flex-row items-center gap-2">
-          <Typography variant="body-s" className="text-greyscale-50">
-            {
-              Money.format({
-                amount: budgetAfterReward,
-                currency: selectedBudget?.currency,
-              }).string
-            }
-          </Typography>
-          <Chip solid className="h-5 w-5 flex-shrink-0">
-            <CurrencyIcons currency={Money.fromSchema({ code: selectedBudget?.currency.code })} className="h-4 w-4" />
-          </Chip>
-        </div>
-      </div>
+      {selectedBudget && (
+        <>
+          <BudgetInfoRow label="Current budget" amount={selectedBudget.remaining} currency={selectedBudget.currency} />
+          <BudgetInfoRow label="Amount rewarded" amount={rewardAmountParsed} currency={selectedBudget.currency} />
+          <BudgetInfoRow label="Budget after reward" amount={budgetAfterReward} currency={selectedBudget.currency} />
+        </>
+      )}
     </div>
   );
 }
--- components/features/project-budget/project-budget.types.ts
+++ components/features/project-budget/project-budget.types.ts
@@ -1,7 +1,14 @@
+import { Money } from "utils/Money/Money";
+
 import { components } from "src/__generated/api";
 
 export namespace TProjectBudget {
-  export interface Props {
+  export interface BudgetInfoRowProps {
+    label: string;
+    amount: number;
+    currency: Money.Currency;
+  }
+  export interface ProjectBudgetProps {
     selectedBudget?: components["schemas"]["BudgetResponse"];
     rewardAmount: string;
   }
