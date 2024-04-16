2cc123748775429402fc84626716e3c17ed26b14
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-04-16 18:14:07 +0200

    Revert "Add hackathons list && E-1263 (#2001)"
    This reverts commit 2944f7807581f570235a3149317ab0981836228f.

--- src/hooks/useIntl.tsx
+++ src/hooks/useIntl.tsx
@@ -21,7 +21,6 @@ import en_features_table from "translations/v2/en/features/table.json";
 import en_features_verify from "translations/v2/en/features/verify.json";
 import en_billing_create from "translations/v2/en/pages/billing-create-stacks.json";
 import en_billing_invite_team_member from "translations/v2/en/pages/billing-invite-team-member-stacks.json";
-import en_hackathons from "translations/v2/en/pages/hackathons.json";
 import en_projects from "translations/v2/en/pages/projects.json";
 import en_settings_billing from "translations/v2/en/pages/settings-billing.json";
 import en_settings_billing_coworkers from "translations/v2/en/pages/settings-billing/settings-billing-coworkers.json";
@@ -76,7 +75,6 @@ const en = {
         payout: en_settings_payout,
         invoices: en_settings_invoices,
         payoutPreferences: en_settings_payout_preferences,
-        hackathons: en_hackathons,
       },
       stacks: {
         request_payments: en_stack_request_payments,
--- utils/hackathons/is-future.ts
+++
@@ -1,9 +0,0 @@
-import { isFuture } from "date-fns";
-
-import { components } from "src/__generated/api";
-
-export function isHackathonFuture(hackathon: components["schemas"]["HackathonsListItemResponse"]) {
-  const start = new Date(hackathon.startDate);
-
-  return isFuture(start);
-}
