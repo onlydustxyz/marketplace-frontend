73b4b4e05f40bee1429423986f98d6d0a6894fb4
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-04-17 15:17:25 +0200

    refactor remo link

--- src/_pages/ProjectDetails/Insights/commons/LastContributionCard/LastContributionCard.tsx
+++ src/_pages/ProjectDetails/Insights/commons/LastContributionCard/LastContributionCard.tsx
@@ -4,7 +4,7 @@ import ArrowRightSLine from "src/icons/ArrowRightSLine";
 import StackLine from "src/icons/StackLine";
 import displayRelativeDate from "src/utils/displayRelativeDate";
 
-import { BaseLink } from "components/layout/base-link/base-link";
+import { Link } from "components/ds/link/link";
 
 import { LastContributionCardProps } from "./LastContributionCard.type";
 
@@ -15,29 +15,27 @@ export default function LastContributionCard({
 }: LastContributionCardProps) {
   const { T } = useIntl();
   return (
-    <BaseLink href={linkUrl}>
-      <Card className="bg-card-background-light px-2 py-3 shadow-medium lg:px-2 lg:py-3">
-        <div className="flex items-center gap-3">
-          <div className="rounded-lg border border-card-border-medium bg-card-background-medium p-2 leading-none text-greyscale-50">
-            <div className="h-4 w-4 text-base leading-none">
-              <StackLine />
-            </div>
+    <Card className="bg-card-background-light px-2 py-3 shadow-medium lg:px-2 lg:py-3">
+      <div className="flex items-center gap-3">
+        <div className="rounded-lg border border-card-border-medium bg-card-background-medium p-2 leading-none text-greyscale-50">
+          <div className="h-4 w-4 text-base leading-none">
+            <StackLine />
           </div>
-          <div className="flex-1">
-            <div className="text-sm font-normal text-greyscale-200">
-              {T("project.details.insights.churned.lastContribution.title")}
-            </div>
-            <div className=" line-clamp-1 text-xs font-medium">
-              <span>
-                {displayRelativeDate(new Date(lastContributionDate))}{" "}
-                {T("project.details.insights.churned.lastContribution.on")}{" "}
-              </span>
-              <span className="text-spacePurple-300">{repoName}</span>
-            </div>
-          </div>
-          <ArrowRightSLine />
         </div>
-      </Card>
-    </BaseLink>
+        <div className="flex-1">
+          <div className="text-sm font-normal text-greyscale-200">
+            {T("project.details.insights.churned.lastContribution.title")}
+          </div>
+          <div className=" line-clamp-1 text-xs font-medium">
+            <span>
+              {displayRelativeDate(new Date(lastContributionDate))}{" "}
+              {T("project.details.insights.churned.lastContribution.on")}{" "}
+            </span>
+            <Link href={linkUrl}>{repoName}</Link>
+          </div>
+        </div>
+        <ArrowRightSLine />
+      </div>
+    </Card>
   );
 }
