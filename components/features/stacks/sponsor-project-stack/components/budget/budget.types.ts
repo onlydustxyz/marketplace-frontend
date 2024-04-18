5fa81f451dc30f308f12ce03d8af38cde951921b
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-04-18 18:17:49 +0200

    refactor(sponsor): budget summaries

--- components/features/stacks/sponsor-project-stack/components/budget/budget.types.ts
+++ components/features/stacks/sponsor-project-stack/components/budget/budget.types.ts
@@ -1,8 +1,12 @@
+import { Money } from "utils/Money/Money";
+
 import { Key } from "src/hooks/useIntl";
 
 export namespace TBudget {
   export interface Props {
     label: Key;
+    amount: number;
+    currency: Money.Currency;
     isAllocation?: boolean;
   }
 }
