68ef3f26a6dbc451a6f9d0a187aa144abef85191
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-04-26 10:59:14 +0200

    Add user api client resource (#2049)

---
+++ api-client/resources/users/types.ts
@@ -0,0 +1,5 @@
+import { components } from "src/__generated/api";
+
+type PublicUserProfileResponse = components["schemas"]["PublicUserProfileResponse"];
+
+export interface UserPublicProfileResponse extends PublicUserProfileResponse {}
