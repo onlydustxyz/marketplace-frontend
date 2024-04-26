a0724f9ee585ea6fcd2aede3ba5d874e700980c5
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-04-26 10:22:33 +0200

    add user fetch to page

---
+++ app/migration/u/[githubLogin]/page.tsx
@@ -0,0 +1,37 @@
+import { usersApiClient } from "api-client/resources/users";
+import { notFound } from "next/navigation";
+
+import { ActivityGraph } from "app/migration/u/[githubLogin]/features/activity-graph/activity-graph";
+import { EcosystemsSection } from "app/migration/u/[githubLogin]/features/ecosystems-section/ecosystems-section";
+import { LanguagesSection } from "app/migration/u/[githubLogin]/features/languages-section/languages-section";
+import { ProfileCard } from "app/migration/u/[githubLogin]/features/profile-card/profile-card";
+import { TotalEarnedGraph } from "app/migration/u/[githubLogin]/features/total-earned-graph/total-earned-graph";
+import { WorkDistributionGraph } from "app/migration/u/[githubLogin]/features/work-distribution-graph/work-distribution-graph";
+
+async function getUserPublicProfile(githubLogin: string) {
+  try {
+    return await usersApiClient.fetch.getUserPublicProfileByGithubLogin(githubLogin).request();
+  } catch {
+    notFound();
+  }
+}
+export default async function PublicProfilePage({ params }: { params: { githubLogin: string } }) {
+  const data = await getUserPublicProfile(params.githubLogin);
+  console.log("data", data);
+  return (
+    <div className="flex w-full flex-col items-start justify-start gap-10">
+      <ProfileCard />
+      <div className="flex w-full flex-row items-start justify-start gap-6">
+        <div className="flex flex-1 flex-col items-start justify-start gap-10">
+          <LanguagesSection />
+          <EcosystemsSection />
+        </div>
+        <div className="flex w-1/3 flex-col items-start justify-start gap-6">
+          <ActivityGraph />
+          <TotalEarnedGraph />
+          <WorkDistributionGraph />
+        </div>
+      </div>
+    </div>
+  );
+}
