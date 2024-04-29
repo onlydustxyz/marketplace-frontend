b2afcc5a4f65a759664f0bbeb8e2e99d6ad81790
Alexis Benoliel <alexbenoliel@gmail.com>
2024-04-29 16:49:13 +0200

    Clean naming

--- app/sponsor/[sponsorId]/features/sponsor-section-project/sponsor-section-project.tsx
+++ app/sponsor/[sponsorId]/features/sponsor-section-project/sponsor-section-project.tsx
@@ -13,7 +13,7 @@ import { Typography } from "components/layout/typography/typography";
 
 export function SponsorSectionProject() {
   const { data, isLoading, isError } = useSponsorDetail();
-  const canSponsorProject = useMemo(() => data?.availableBudgets.filter(b => Boolean(b.amount)) ?? [], [data]);
+  const canSponsorProject = useMemo(() => data?.availableBudgets.filter(b => Boolean(b.amount))?.length, [data]);
 
   const renderProjects = useCallback(() => {
     if (isError) {
@@ -30,7 +30,7 @@ export function SponsorSectionProject() {
 
     if (data) {
       return data.projects.map(project => (
-        <SponsorProjectCard key={project.id} project={project} disableSponsorButton={!canSponsorProject.length} />
+        <SponsorProjectCard key={project.id} project={project} disableSponsorButton={!canSponsorProject} />
       ));
     }
 
@@ -50,11 +50,11 @@ export function SponsorSectionProject() {
             size: "s",
             className: "w-full sm:w-auto",
             children: <Translate token="v2.pages.sponsor.project.sponsorNewProject" />,
-            disabled: !canSponsorProject.length,
+            disabled: !canSponsorProject,
             backgroundColor: "blue",
           }}
           tooltipProps={{
-            enabled: !canSponsorProject.length,
+            enabled: !canSponsorProject,
             content: <Translate token="v2.pages.sponsor.project.disbaledSponsorNewProject" />,
           }}
         />
