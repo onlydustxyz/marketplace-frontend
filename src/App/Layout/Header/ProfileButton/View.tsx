e6ca8d788d9424bae6af1f1c1e1a492db293732a
NeoxAzrot <sami.lafrance@akwai.co>
2024-04-29 10:22:21 +0200

    fix(menu): translate

--- src/App/Layout/Header/ProfileButton/View.tsx
+++ src/App/Layout/Header/ProfileButton/View.tsx
@@ -11,6 +11,7 @@ import { cn } from "src/utils/cn";
 import { BaseLink } from "components/layout/base-link/base-link";
 import { Flex } from "components/layout/flex/flex";
 import { Icon } from "components/layout/icon/icon";
+import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
 import { NEXT_ROUTER } from "constants/router";
@@ -150,7 +151,9 @@ export function View({
                   <BaseLink href={NEXT_ROUTER.sponsor.details.root(sponsors[0].id)}>
                     <MenuItem>
                       <Icon remixName="ri-service-line" size={20} />
-                      <div className="grow">{T("v2.features.menu.sponsoring")}</div>
+                      <div className="grow">
+                        <Translate token="v2.features.menu.sponsoring" />
+                      </div>
                     </MenuItem>
                   </BaseLink>
                 ) : null}
@@ -164,24 +167,32 @@ export function View({
                 <BaseLink href={NEXT_ROUTER.publicProfile.root(githubUserId.toString())}>
                   <MenuItem>
                     <Icon remixName="ri-user-line" size={20} />
-                    <div className="grow">{T("v2.features.menu.publicProfile")}</div>
+                    <div className="grow">
+                      <Translate token="v2.features.menu.publicProfile" />
+                    </div>
                   </MenuItem>
                 </BaseLink>
               ) : null}
 
               <MenuItem onClick={openFeedback}>
                 <Icon remixName="ri-discuss-line" size={20} />
-                <div className="grow">{T("v2.features.menu.feedback")}</div>
+                <div className="grow">
+                  <Translate token="v2.features.menu.feedback" />
+                </div>
               </MenuItem>
 
               <MenuItem onClick={openFullTermsAndConditions}>
                 <Icon remixName="ri-bill-line" size={20} />
-                <div className="grow">{T("v2.features.menu.terms")}</div>
+                <div className="grow">
+                  <Translate token="v2.features.menu.terms" />
+                </div>
               </MenuItem>
 
               <MenuItem onClick={openPrivacyPolicy}>
                 <Icon remixName="ri-lock-line" size={20} />
-                <div className="grow">{T("v2.features.menu.privacy")}</div>
+                <div className="grow">
+                  <Translate token="v2.features.menu.privacy" />
+                </div>
               </MenuItem>
 
               <span className="my-1 block h-px bg-greyscale-50/8" />
@@ -190,7 +201,9 @@ export function View({
             <div>
               <MenuItem onClick={handleLogout}>
                 <Icon remixName="ri-logout-box-r-line" size={20} />
-                <div className="grow">{T("v2.features.menu.logout")}</div>
+                <div className="grow">
+                  <Translate token="v2.features.menu.logout" />
+                </div>
               </MenuItem>
             </div>
           </Menu.Items>
