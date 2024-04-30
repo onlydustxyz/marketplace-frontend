15ae7693a65b648ddc3e27b18dae3a8cb8d8a7b7
NeoxAzrot <sami.lafrance@akwai.co>
2024-04-30 16:28:27 +0200

    feat(*): hover animation

--- components/features/charts/pie-chart/pie-chart.tsx
+++ components/features/charts/pie-chart/pie-chart.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { animate, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import { Cell, Legend, Pie, PieChart as RechartsPieChart, ResponsiveContainer, Sector, Tooltip } from "recharts";
 import { PieSectorDataItem } from "recharts/types/polar/Pie";
 
@@ -7,23 +9,42 @@ import { CustomLegend } from "../custom-legend/custom-legend";
 import { CustomTooltip } from "../custom-tooltip/custom-tooltip";
 import { TPieChart } from "./pie-chart.types";
 
-// TODO: Active shape, regarder pour avoir une transition animée
-export function PieChart({ data, renderTooltip }: TPieChart.Props) {
-  const DEFAULT_COLORS = ["#FFBC66", "#CE66FF", "#666BD7", "#66FFEF", "#F69EF3"];
+function ActiveShape({ cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill }: PieSectorDataItem) {
+  const [animationInnerRadius, setAnimationInnerRadius] = useState(innerRadius);
+  const [animationOuterRadius, setAnimationOuterRadius] = useState(outerRadius);
 
-  function renderActiveShape({ cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill }: PieSectorDataItem) {
-    return (
+  const animation = useRef(
+    animate(0, 3, {
+      duration: 0.3,
+      autoplay: false,
+      onUpdate: latest => {
+        setAnimationInnerRadius(Number(innerRadius) - latest);
+        setAnimationOuterRadius(Number(outerRadius) + latest);
+      },
+    })
+  );
+
+  useEffect(() => {
+    animation.current.play();
+  }, []);
+
+  return (
+    <motion.g>
       <Sector
         cx={cx}
         cy={cy}
-        innerRadius={Number(innerRadius) - 3}
-        outerRadius={Number(outerRadius) + 3}
+        innerRadius={animationInnerRadius}
+        outerRadius={animationOuterRadius}
         startAngle={startAngle}
         endAngle={endAngle}
         fill={fill}
       />
-    );
-  }
+    </motion.g>
+  );
+}
+
+export function PieChart({ data, renderTooltip }: TPieChart.Props) {
+  const DEFAULT_COLORS = ["#FFBC66", "#CE66FF", "#666BD7", "#66FFEF", "#F69EF3"];
 
   return (
     <ResponsiveContainer width="100%" height={148} className="px-12">
@@ -35,7 +56,7 @@ export function PieChart({ data, renderTooltip }: TPieChart.Props) {
           outerRadius={70}
           stroke="none"
           cx={74}
-          activeShape={renderActiveShape}
+          activeShape={<ActiveShape />}
         >
           {data.map((entry, index) => {
             const color = entry.color || DEFAULT_COLORS[index % DEFAULT_COLORS.length];
