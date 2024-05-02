d577039df9de545ff564c30973859f6b64b39dfa
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-02 17:52:05 +0200

    Text align left hackathon

--- app/migration/h/[slug]/components/track-project/track-project.tsx
+++ app/migration/h/[slug]/components/track-project/track-project.tsx
@@ -11,8 +11,10 @@ export function TrackProject({ project }: TTrackProject.Props) {
       <div className="flex w-full flex-row items-start justify-start gap-3 rounded-2xl border border-card-border-light p-6">
         <Avatar size={"xl"} shape={"square"} src={project.logoUrl} />
         <div className="flex flex-col items-start justify-start gap-1">
-          <Typography variant="title-m">{project.name}</Typography>
-          <Typography variant="body-m" className="text-greyscale-200">
+          <Typography variant="title-m" className="text-left">
+            {project.name}
+          </Typography>
+          <Typography variant="body-m" className="text-left text-greyscale-200">
             {project.shortDescription}
           </Typography>
         </div>
