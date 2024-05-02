287d415a21b9548a2f260dab102279f4304828bb
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-02 18:15:46 +0200

    Add previous hackathons

--- app/migration/hackathons/page.tsx
+++ app/migration/hackathons/page.tsx
@@ -12,6 +12,9 @@ async function HackathonsPage() {
 
   const liveNow = data.hackathons.filter(hackathon => isHackathonLive(hackathon));
   const comingSoon = data.hackathons.filter(hackathon => isHackathonFuture(hackathon));
+  const pastHackathon = data.hackathons.filter(
+    hackathon => !isHackathonLive(hackathon) && !isHackathonFuture(hackathon)
+  );
 
   return (
     <div className="scrollbar-sm relative z-[1] h-full w-full overflow-y-auto bg-no-repeat lg:rounded-3xl">
@@ -27,6 +30,11 @@ async function HackathonsPage() {
             title={<Translate token={"v2.pages.hackathons.comingSoon"} />}
             icon={{ remixName: "ri-calendar-event-line" }}
             items={comingSoon}
+          />
+          <HackathonSection
+            title={<Translate token={"v2.pages.hackathons.previous"} />}
+            icon={{ remixName: "ri-calendar-event-line" }}
+            items={pastHackathon}
           />
         </div>
       </div>
