dc9b7f016961f3317787b4efc92545e3d0f38a2a
Sami Lafrance <sami.lepays@gmail.com>
2024-05-10 11:35:57 +0200

    Spacing header card (#2087)

--- app/migration/u/[githubLogin]/features/profile-overview/profile-overview.tsx
+++ app/migration/u/[githubLogin]/features/profile-overview/profile-overview.tsx
@@ -5,6 +5,7 @@ import { IMAGES } from "src/assets/img";
 
 import { Card } from "components/ds/card/card";
 import { ProfileCard } from "components/features/profile-card/profile-card";
+import { Flex } from "components/layout/flex/flex";
 import { RemixIconsName } from "components/layout/icon/remix-icon-names.types";
 
 import { TMostActiveCard } from "./components/most-active-section/most-active-card/most-active-card.types";
@@ -111,46 +112,54 @@ export function ProfileOverview(_: TProfileOverview.Props) {
   ];
 
   return (
-    <Card className="flex w-full flex-col items-start justify-start gap-10" background="base">
-      <div className="flex w-full flex-row flex-wrap items-start justify-between gap-2">
-        <div className="flex flex-1">
-          <ProfileCard {...profileCardMock} />
-        </div>
-        <div className="flex flex-1">
-          <ProfileSummary {...profileRestInfo} />
-        </div>
+    <Flex direction="col" className="gap-4 md:gap-0">
+      <div className="flex md:hidden">
+        <ProfileCard {...profileCardMock} />
       </div>
-      <div className="flex w-full flex-col items-start justify-between gap-6 md:flex-row md:gap-10">
-        <div className="flex w-full flex-1">
-          <MostActiveSection
-            icon={{
-              remixName: "ri-code-s-slash-line",
-            }}
-            title={{
-              translate: {
-                token: "v2.pages.publicProfile.header.languages.title",
-              },
-            }}
-            list={mostActiveLanguagesMock}
-            wrapperClassName="xl:grid-cols-4"
-          />
+
+      <Card className="flex w-full flex-col items-start justify-start gap-6 md:gap-10" background="base">
+        <div className="flex w-full flex-row flex-wrap items-start justify-between gap-10">
+          <div className="hidden flex-1 md:flex">
+            <ProfileCard {...profileCardMock} />
+          </div>
+
+          <div className="flex flex-1">
+            <ProfileSummary {...profileRestInfo} />
+          </div>
         </div>
 
-        <div className="flex w-full md:w-1/3">
-          <MostActiveSection
-            icon={{
-              remixName: "ri-global-line",
-            }}
-            title={{
-              translate: {
-                token: "v2.pages.publicProfile.header.ecosystems.title",
-              },
-            }}
-            list={mostActiveEcosystemsMock}
-            wrapperClassName="md:grid-cols-1 xl:grid-cols-2"
-          />
+        <div className="flex w-full flex-col items-start justify-between gap-6 md:flex-row md:gap-10">
+          <div className="flex w-full flex-1">
+            <MostActiveSection
+              icon={{
+                remixName: "ri-code-s-slash-line",
+              }}
+              title={{
+                translate: {
+                  token: "v2.pages.publicProfile.header.languages.title",
+                },
+              }}
+              list={mostActiveLanguagesMock}
+              wrapperClassName="xl:grid-cols-4"
+            />
+          </div>
+
+          <div className="flex w-full md:w-1/3">
+            <MostActiveSection
+              icon={{
+                remixName: "ri-global-line",
+              }}
+              title={{
+                translate: {
+                  token: "v2.pages.publicProfile.header.ecosystems.title",
+                },
+              }}
+              list={mostActiveEcosystemsMock}
+              wrapperClassName="md:grid-cols-1 xl:grid-cols-2"
+            />
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Flex>
   );
 }
