d0d7caa3eba299f7aa0a431d1e82d302bf180f49
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-05-10 11:42:14 +0200

    user query is public

--- api-client/resources/users/queries/use-get-user-public-profile-by-github-id.ts
+++ api-client/resources/users/queries/use-get-user-public-profile-by-github-id.ts
@@ -1,6 +1,5 @@
 "use client";
 
-import { useAuth0 } from "@auth0/auth0-react";
 import { useQuery } from "@tanstack/react-query";
 import { useReactQueryAdapter } from "api-client/adapter/react-query/react-query-adapter";
 import { getUserPublicProfileByGithubId } from "api-client/resources/users/fetch/get-user-public-profile-by-github-id";
@@ -14,10 +13,9 @@ export const useGetUserPublicProfileByGithubId = ({
   isEnabled?: boolean;
 }) => {
   const { query } = useReactQueryAdapter<UserPublicProfileResponseV2>(getUserPublicProfileByGithubId(githubId));
-  const { isAuthenticated } = useAuth0();
 
   return useQuery<UserPublicProfileResponseV2>({
     ...query,
-    enabled: isAuthenticated && isEnabled,
+    enabled: isEnabled,
   });
 };
