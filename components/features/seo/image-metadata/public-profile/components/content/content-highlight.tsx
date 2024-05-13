0d097d753c0991ff4d5987c19817ecadf9d3c016
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-13 15:16:45 +0200

    Clean code

--- components/features/seo/image-metadata/public-profile/components/content/content-highlight.tsx
+++ components/features/seo/image-metadata/public-profile/components/content/content-highlight.tsx
@@ -1,7 +1,9 @@
+import { ReactNode } from "react";
+
 interface Props {
   name: string;
   image: string;
-  icon: React.ReactNode;
+  icon: ReactNode;
   label: string;
 }
 export function ContentHighlight({ name, image, label, icon }: Props) {
--- components/features/seo/image-metadata/public-profile/image-metadata.tsx
+++ components/features/seo/image-metadata/public-profile/image-metadata.tsx
@@ -1,5 +1,6 @@
 import { OGActivityGraph } from "components/features/seo/image-metadata/public-profile/components/activity/activity-graph";
 import { OgContent } from "components/features/seo/image-metadata/public-profile/components/content/content";
+import { Wrapper } from "components/features/seo/image-metadata/public-profile/components/wrapper";
 
 import { THackathonImageMetadataProps } from "./image-metadata.types";
 
@@ -11,54 +12,10 @@ export function PublicProfileImageMetadata({
   topLanguages,
   topEcosystem,
 }: THackathonImageMetadataProps.Props) {
-  const backgroundUrl = `${process.env.NEXT_PUBLIC_METADATA_ASSETS_S3_BUCKET}/profile-opengraph-background.png`;
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        height: "100%",
-        position: "relative",
-        backgroundColor: "black",
-        zIndex: 1,
-      }}
-    >
-      <img
-        src={backgroundUrl}
-        alt="background"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: -1,
-        }}
-      />
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          zIndex: 2,
-          paddingLeft: 72,
-          paddingRight: 63,
-        }}
-      >
-        <OgContent image={image} login={login} title={title} topLanguages={topLanguages} topEcosystem={topEcosystem} />
-        <OGActivityGraph data={data} />
-      </div>
-    </div>
+    <Wrapper>
+      <OgContent image={image} login={login} title={title} topLanguages={topLanguages} topEcosystem={topEcosystem} />
+      <OGActivityGraph data={data} />
+    </Wrapper>
   );
 }
