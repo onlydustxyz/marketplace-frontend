c28dcc653d87bb5d9a027eb9269c4d115bb0791d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-15 18:04:00 +0200

    refactor(*): removed legacy load more

--- components/layout/api/load-more/load-more.type.ts
+++
@@ -1,7 +0,0 @@
-import { BasePaginatedParams } from "actions/type.actions";
-
-export interface LoadMoreProps<PARAMS extends BasePaginatedParams = BasePaginatedParams> {
-  params: PARAMS;
-  onFetchMore(params: PARAMS): Promise<JSX.Element>;
-  hasMore: boolean;
-}
