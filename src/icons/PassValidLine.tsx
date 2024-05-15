609bac39dcba9318df7dab685616668752d79b65
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-15 17:48:27 +0200

    refactor(*): removed unused icons

--- src/icons/PassValidLine.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "src/utils/cn";
-
-interface Props {
-  className?: string;
-}
-
-export default function PassValidLine({ className }: Props) {
-  return <i className={cn("ri-pass-valid-line", className)} />;
-}
--- src/icons/WhatsappFill.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "src/utils/cn";
-
-interface Props {
-  className?: string;
-}
-
-export default function WhatsappFill({ className }: Props) {
-  return <i className={cn("ri-whatsapp-fill", className)} />;
-}
