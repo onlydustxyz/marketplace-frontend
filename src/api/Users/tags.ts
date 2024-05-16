5672bcfdb2a7af2e47d8ac3207b957f49fb116e8
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-05-16 11:16:31 +0200

    b-1599-cleanreplace-old-users-queries-mostly-used-in-contributor (#2091)

--- src/api/Users/tags.ts
+++ src/api/Users/tags.ts
@@ -3,11 +3,5 @@ import { RESSOURCE_TAGS } from "src/api/ressource-tags";
 export const USERS_TAGS = {
   all: [RESSOURCE_TAGS.USERS],
   users: (login?: string) => [...USERS_TAGS.all, "users", { login }],
-  user_profile_by_github_id: (githubUserId: string) => [
-    RESSOURCE_TAGS.USERS,
-    "user-profile-by-github-id",
-    { githubUserId },
-  ],
-  user_profile_by_github_login: (login: string) => [RESSOURCE_TAGS.USERS, "user-profile-by-github-login", { login }],
   user_contributions: (login: string) => [RESSOURCE_TAGS.USERS, "user-contributions", { login }],
 };
