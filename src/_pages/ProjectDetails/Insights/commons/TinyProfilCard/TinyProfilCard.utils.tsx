09bbba830e9fe842914a2aa00186bf6b1a5d9055
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-16 14:40:30 +0200

    feat(insights): random profile cover

--- src/_pages/ProjectDetails/Insights/commons/TinyProfilCard/TinyProfilCard.utils.tsx
+++ src/_pages/ProjectDetails/Insights/commons/TinyProfilCard/TinyProfilCard.utils.tsx
@@ -2,14 +2,23 @@ import { ReactElement } from "react";
 
 import { ProfileCover } from "./TinyProfilCard.type";
 
-function getCoverClass(cover: `${ProfileCover}`): string {
-  const coverClasses: Record<ProfileCover, string> = {
-    [ProfileCover.Blue]: "bg-profile-blue",
-    [ProfileCover.Cyan]: "bg-profile-cyan",
-    [ProfileCover.Magenta]: "bg-profile-magenta",
-    [ProfileCover.Yellow]: "bg-profile-yellow",
-  };
-  return coverClasses[cover] || coverClasses[ProfileCover.Blue];
+const COVER_VALUES = Object.values(ProfileCover);
+const MAX_COVER_INDEX = COVER_VALUES.length - 1;
+
+const coverClasses: Record<ProfileCover, string> = {
+  [ProfileCover.Blue]: "bg-profile-blue",
+  [ProfileCover.Cyan]: "bg-profile-cyan",
+  [ProfileCover.Magenta]: "bg-profile-magenta",
+  [ProfileCover.Yellow]: "bg-profile-yellow",
+};
+
+function getCoverClass(cover: `${ProfileCover}`, username: string): string {
+  // Random float between 0 and 1 based on username length
+  const randomFloat = Math.abs(Math.sin(username.length));
+  const randomIndex = Math.round(randomFloat * MAX_COVER_INDEX);
+  const randomCover = COVER_VALUES[randomIndex];
+
+  return coverClasses[cover] || coverClasses[randomCover];
 }
 
 function OptionalSection({
