b625e3207cf12f6306b0442d6352a8d0de4e7e58
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-20 17:05:58 +0200

    refactor(user): user projects lead hook

--- hooks/users/use-projects-lead/use-projects-lead.ts
+++ hooks/users/use-projects-lead/use-projects-lead.ts
@@ -1,12 +1,12 @@
 import { useCurrentUser } from "../use-current-user/use-current-user";
 import { TUseProjectsLead } from "./use-projects-lead.types";
 
-export const UseProjectsLead = (): TUseProjectsLead.Return => {
-  const { user, isLoading } = useCurrentUser();
+export const useProjectsLead = (): TUseProjectsLead.Return => {
+  const { user, ...restQuery } = useCurrentUser();
 
   return {
     projectsLead: user?.projectsLed?.length ? user.projectsLed : [],
     pendingProjectsLead: user?.pendingProjectsLed?.length ? user.pendingProjectsLed : [],
-    isLoading,
+    ...restQuery,
   };
 };
