42fdd420b63f955b0b4d33e0fe3ba15f68bf00fe
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-22 17:03:21 +0200

    feat(*): dust score

--- app/c/[committeeId]/jury/page.tsx
+++ app/c/[committeeId]/jury/page.tsx
@@ -6,6 +6,8 @@ import { CommitteeLoadingPage } from "app/c/[committeeId]/features/loading-page/
 import { CommitteePublicPage } from "app/c/[committeeId]/features/public-page/public-page";
 import { ProjectsAccordion } from "app/c/[committeeId]/jury/features/projects-accordion/projects-accordion";
 
+import { DustScore } from "components/features/dust-score/dust-score";
+
 export default function CommitteeJuryPage() {
   const { isAuthenticated, isLoading } = useAuth0();
 
@@ -17,9 +19,12 @@ export default function CommitteeJuryPage() {
     <div className="scrollbar-sm my-auto flex items-start justify-center">
       <div className="px-6 py-12">
         {isAuthenticated ? (
-          <ProjectsAccordion
-            projects={[{ id: "123", name: "nice project", logoUrl: "", description: "awesome project", status: "" }]}
-          />
+          <>
+            <ProjectsAccordion
+              projects={[{ id: "123", name: "nice project", logoUrl: "", description: "awesome project", status: "" }]}
+            />
+            <DustScore initialScore={2} isSmall onScoreChange={score => console.log({ score })} />
+          </>
         ) : (
           <CommitteePublicPage />
         )}
