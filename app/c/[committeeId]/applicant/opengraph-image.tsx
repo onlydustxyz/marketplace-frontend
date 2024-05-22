f3f6425214c7775aab88f4438ea2701d849d8c49
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-22 15:32:15 +0200

    Revamp graph

--- app/c/[committeeId]/applicant/opengraph-image.tsx
+++ app/c/[committeeId]/applicant/opengraph-image.tsx
@@ -1,27 +1,5 @@
-import { committeeApiClient } from "api-client/resources/committees";
-import { format } from "date-fns";
-
-import { CommitteeImageMetadata } from "components/features/seo/image-metadata/committee/image-metadata";
-import { Generator } from "components/features/seo/image-metadata/commons/generator/generator";
-import { GenericImageMetadata } from "components/features/seo/image-metadata/generic/image-metadata";
+import OpenGraphImage from "../features/opengraph-image";
 
 export default async function Image(props: { params: { committeeId: string } }) {
-  try {
-    const committee = await committeeApiClient.fetch.getPublicCommittee(props.params.committeeId).request();
-    return Generator({
-      children: (
-        <CommitteeImageMetadata
-          name={committee?.name}
-          dates={`${format(new Date(committee?.applicationStartDate), "dd/MM/yyyy")} - ${format(
-            new Date(committee?.applicationEndDate),
-            "dd/MM/yyyy"
-          )}`}
-        />
-      ),
-    });
-  } catch {
-    return Generator({
-      children: <GenericImageMetadata />,
-    });
-  }
+  return OpenGraphImage(props);
 }
