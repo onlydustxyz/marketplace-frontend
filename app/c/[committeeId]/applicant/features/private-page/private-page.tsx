d8391abc1a88f132c58044fc6ff1626a4f5f04e5
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-05-23 13:54:33 +0200

    Retry + invalidate  (#2168)

--- app/c/[committeeId]/applicant/features/private-page/private-page.tsx
+++ app/c/[committeeId]/applicant/features/private-page/private-page.tsx
@@ -42,7 +42,12 @@ export function CommitteeApplicantPrivatePage() {
   const isInitialLoadingRef = useRef(true);
   const statusRef = useRef<GetCommitteeProjectApplicationResponse["status"]>();
 
-  const { data, isError, isFetching } = committeeApiClient.queries.useGetCommitteeProjectApplication({
+  const {
+    data,
+    isError,
+    isFetching,
+    refetch: refetchCommetteeProjectApplication,
+  } = committeeApiClient.queries.useGetCommitteeProjectApplication({
     committeeId: typeof committeeId === "string" ? committeeId : "",
     projectId,
   });
@@ -56,7 +61,7 @@ export function CommitteeApplicantPrivatePage() {
     }
   }, [isError, initialProjectId]);
 
-  const { mutate, isPending, ...restMutation } = committeeApiClient.mutations.useUpdateCommitteeProjectApplication({
+  const { isPending, ...restMutation } = committeeApiClient.mutations.useUpdateCommitteeProjectApplication({
     committeeId: typeof committeeId === "string" ? committeeId : "",
     projectId,
   });
@@ -120,12 +125,16 @@ export function CommitteeApplicantPrivatePage() {
   function handleFormSubmit(values: TPrivatePage.form) {
     if (!canSubmit) return;
 
-    mutate({
-      answers: values.answers.map(a => ({
-        questionId: a.questionId,
-        answer: a.answer,
-      })),
-    });
+    restMutation
+      .mutateAsync({
+        answers: values.answers.map(a => ({
+          questionId: a.questionId,
+          answer: a.answer,
+        })),
+      })
+      .then(() => {
+        refetchCommetteeProjectApplication();
+      });
   }
 
   const renderMainTitle = useMemo(() => {
