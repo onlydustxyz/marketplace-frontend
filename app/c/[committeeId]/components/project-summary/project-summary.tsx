2cc1bc2743964a74785f6536f059964ab54b9d1a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-24 16:47:45 +0200

    wip(committee): github link

--- app/c/[committeeId]/components/project-summary/project-summary.tsx
+++ app/c/[committeeId]/components/project-summary/project-summary.tsx
@@ -3,9 +3,12 @@ import { TProjectSummary } from "app/c/[committeeId]/components/project-summary/
 import MarkdownPreview from "src/components/MarkdownPreview";
 
 import { Avatar } from "components/ds/avatar/avatar";
+import { Button } from "components/ds/button/button";
 import { Card } from "components/ds/card/card";
 import { Tag } from "components/ds/tag/tag";
 import { Contributor } from "components/features/contributor/contributor";
+import { BaseLink } from "components/layout/base-link/base-link";
+import { Icon } from "components/layout/icon/icon";
 import { Translate } from "components/layout/translate/translate";
 import { Typography } from "components/layout/typography/typography";
 
@@ -18,9 +21,17 @@ export function ProjectSummary({ project }: TProjectSummary.Props) {
         <Avatar src={project.logoUrl} size={"2xl"} shape={"square"} isBordered={false} />
 
         <div className={"grid flex-1 gap-2"}>
-          <Typography variant={"title-m"} className={"truncate"}>
-            {project.name}
-          </Typography>
+          <div className={"flex items-center justify-between gap-2 overflow-hidden"}>
+            <Typography variant={"title-m"} className={"truncate"}>
+              {project.name}
+            </Typography>
+
+            <BaseLink href={""}>
+              <Button as={"div"} size="s" variant="secondary" iconOnly className="h-8 w-8 rounded-xl p-2">
+                <Icon remixName={"ri-github-fill"} size={16} />
+              </Button>
+            </BaseLink>
+          </div>
 
           {project.projectLeads?.length ? (
             <ul className={"flex flex-wrap gap-x-3 gap-y-1"}>
