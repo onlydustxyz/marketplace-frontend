8279d2596ffe42cc43a1f8f408b79e89e53517ed
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-30 14:03:37 +0200

    add cache

--- app/u/[githubLogin]/opengraph-image.tsx
+++ app/u/[githubLogin]/opengraph-image.tsx
@@ -7,16 +7,19 @@ import { getLevelRange } from "components/features/graphs/activity-graph/utils/g
 import { getWeekId } from "components/features/graphs/activity-graph/utils/getWeekId";
 import { Generator } from "components/features/seo/image-metadata/commons/generator/generator";
 import { GenericImageMetadata } from "components/features/seo/image-metadata/generic/image-metadata";
+import { PublicProfileImageMetadata } from "components/features/seo/image-metadata/public-profile/image-metadata";
+import { TPublicProfileImageMetadata } from "components/features/seo/image-metadata/public-profile/image-metadata.types";
 
-export const runtime = "edge";
 export default async function Image(props: { params: { githubLogin: string } }) {
   try {
-    const user = await usersApiClient.fetch.getUserPublicProfileByGithubLogin(props.params.githubLogin).request();
+    const user = await usersApiClient.fetch
+      .getUserPublicProfileByGithubLogin(props.params.githubLogin)
+      .request({ next: { revalidate: 86400 } });
     const githubUserId = user?.githubUserId || 0;
-    const [stats] = await Promise.all([
+    const [stats, languages, ecosystems] = await Promise.all([
       usersApiClient.fetch
         .getUserPublicStats(githubUserId)
-        .request()
+        .request({ next: { revalidate: 86400 } })
         .then(res => res)
         .catch(e => {
           console.error(e);
@@ -24,7 +27,7 @@ export default async function Image(props: { params: { githubLogin: string } }) 
         }),
       usersApiClient.fetch
         .getUserPublicLanguages(githubUserId, { pageSize: 2, pageIndex: 0 })
-        .request()
+        .request({ next: { revalidate: 86400 } })
         .then(res => res)
         .catch(e => {
           console.error(e);
@@ -32,13 +35,16 @@ export default async function Image(props: { params: { githubLogin: string } }) 
         }),
       usersApiClient.fetch
         .getUserPublicEcosystems(githubUserId, { pageSize: 2, pageIndex: 0 })
-        .request()
+        .request({ next: { revalidate: 86400 } })
         .then(res => res)
         .catch(e => {
           console.error(e);
           return null;
         }),
     ]);
+
+    const ecosystem = ecosystems?.ecosystems?.[0];
+    const language = languages?.languages?.[0];
 
     const createData = () => {
       const data: {
@@ -65,10 +71,41 @@ export default async function Image(props: { params: { githubLogin: string } }) 
       return data;
     };
 
-    createData();
+    const data = createData();
 
     return Generator({
-      children: <GenericImageMetadata />,
+      children: (
+        <PublicProfileImageMetadata
+          login={user.login}
+          image={user.avatarUrl}
+          contributionCount={user.statsSummary?.contributionCount || 0}
+          rewardsCount={user.statsSummary?.rewardCount || 0}
+          title={
+            user.statsSummary?.rankCategory
+              ? TPublicProfileImageMetadata.rankCategoryTranslationMapping[user.statsSummary?.rankCategory]
+              : ""
+          }
+          rank={user.statsSummary?.rank || 0}
+          rankPercentile={user.statsSummary?.rankPercentile || 0}
+          {...(ecosystem
+            ? {
+                topEcosystem: {
+                  name: ecosystem.ecosystem.name,
+                  image: ecosystem.ecosystem.logoUrl,
+                },
+              }
+            : {})}
+          {...(language
+            ? {
+                topLanguages: {
+                  name: language.language.name,
+                  image: language.language.logoUrl,
+                },
+              }
+            : {})}
+          data={data}
+        />
+      ),
     });
   } catch {
     return Generator({
