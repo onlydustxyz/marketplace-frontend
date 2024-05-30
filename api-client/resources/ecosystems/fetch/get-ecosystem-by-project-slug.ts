e65941e09dfb36b1f294a001103ceadedaa0ceb4
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-05-30 11:21:47 +0200

    refactor(gfi): query params for API

--- api-client/resources/ecosystems/fetch/get-ecosystem-by-project-slug.ts
+++ api-client/resources/ecosystems/fetch/get-ecosystem-by-project-slug.ts
@@ -7,10 +7,18 @@ import { GetEcosystemProjectPageResponse } from "../types";
 
 export function getEcosystemByProjectSlug({
   ecosystemSlug,
+  queryParams,
 }: {
   ecosystemSlug: string;
+  queryParams?: Record<string, string>;
 }): IFetchAdapater<GetEcosystemProjectPageResponse> {
-  return new FetchAdapter<GetEcosystemProjectPageResponse>(adapters.get_projects_by_slug)
+  const fetchAdapter = new FetchAdapter<GetEcosystemProjectPageResponse>(adapters.get_projects_by_slug)
     .setPathParams({ ecosystemSlug })
     .setTag(tags.projects_by_slug(ecosystemSlug));
+
+  if (queryParams) {
+    fetchAdapter.setParams(queryParams).setTag(tags.projects_by_slug(ecosystemSlug, queryParams));
+  }
+
+  return fetchAdapter;
 }
--- api-client/resources/ecosystems/tags.ts
+++ api-client/resources/ecosystems/tags.ts
@@ -2,5 +2,6 @@ import { apiResources } from "api-client/config/resources";
 
 export default {
   root: `${apiResources.ecosystems}-root`,
-  projects_by_slug: (slug: string) => `${apiResources.ecosystems}-projects-by-slug-${slug}`,
+  projects_by_slug: (slug: string, queryParams?: Record<string, string>) =>
+    `${apiResources.ecosystems}-projects-by-slug-${slug}-${queryParams ? JSON.stringify(queryParams) : ""}`,
 };
