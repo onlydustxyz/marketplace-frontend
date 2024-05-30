6682d7f973b7ed47aace5fbf42d1311614d0b803
Alexis Benoliel <alexbenoliel@gmail.com>
2024-05-30 11:51:06 +0200

    add  background

--- components/features/seo/image-metadata/public-profile/components/wrapper.tsx
+++ components/features/seo/image-metadata/public-profile/components/wrapper.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export function Wrapper({ children }: PropsWithChildren) {
+  const backgroundUrl = `${process.env.NEXT_PUBLIC_METADATA_ASSETS_S3_BUCKET}/profile-opengraph-background.png`;
   return (
     <div
       style={{
@@ -12,6 +13,21 @@ export function Wrapper({ children }: PropsWithChildren) {
         zIndex: 1,
       }}
     >
+      <img
+        src={backgroundUrl}
+        alt="background"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: -1,
+        }}
+      />
       <div
         style={{
           width: "100%",
