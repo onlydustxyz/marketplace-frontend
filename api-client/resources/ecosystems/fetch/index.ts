840110a4a2f317da2ebe506dc14be46345f759d1
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-05-31 15:19:20 +0200

    delete duplicate

--- api-client/resources/ecosystems/fetch/index.ts
+++ api-client/resources/ecosystems/fetch/index.ts
@@ -1,4 +1,3 @@
 export * from "./get-ecosystem-project-by-slug";
 export * from "./get-ecosystem-contributors-by-slug";
-export * from "./get-ecosystem-project-by-slug";
 export * from "./get-all-ecosystems";
