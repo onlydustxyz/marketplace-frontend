2a91e42202de27a88c699a67bf03f45295de59e5
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-03 18:32:24 +0200

    hide navigation mobile

--- app/ecosystems/components/slider-stepper/slider-stepper.types.ts
+++ app/ecosystems/components/slider-stepper/slider-stepper.types.ts
@@ -6,6 +6,7 @@ export namespace TCarouselStepper {
   type ButtonProps = Omit<ComponentProps<typeof Button>, "variant" | "size" | "iconOnly">;
   export interface Props {
     prevProps: ButtonProps;
+    className?: string;
     nextProps: ButtonProps;
   }
 }
