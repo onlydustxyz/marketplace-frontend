6464ba55dcde73090760d341d5602dc62d6573cc
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-03 14:11:23 +0200

    WIP : language section

---
+++ app/ecosystems/[ecosystemSlug]/features/languages/languages.tsx
@@ -0,0 +1,11 @@
+import { Card } from "components/ds/card/card";
+
+import { TLanguages } from "./languages.types";
+
+export function Languages({ children }: TLanguages.Props) {
+  return (
+    <div>
+      <Card>coucou</Card>
+    </div>
+  );
+}
