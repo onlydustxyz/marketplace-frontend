13c6ba078792685011cf7de11d8a9582470d85ce
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-06-03 16:16:18 +0200

    Learn more (#2216)

---
+++ app/ecosystems/[ecosystemSlug]/features/learn-more/learn-more.loading.tsx
@@ -0,0 +1,24 @@
+import { SectionLoading } from "app/ecosystems/components/section/section.loading";
+
+import { SkeletonEl } from "components/ds/skeleton/skeleton";
+
+export async function LearnMoreLoading() {
+  return (
+    <SectionLoading>
+      <div className="flex w-full flex-col items-start justify-between gap-6 lg:flex-row lg:items-center">
+        <SkeletonEl width="50%" height="100%" variant="rounded" className="aspect-[16/9]" />
+        <div className="flex w-1/2 flex-col items-start justify-between gap-4">
+          <SkeletonEl width="75%" height="24px" variant="rounded" />
+          <div className="flex w-full flex-col items-start gap-2">
+            <SkeletonEl width="100%" height="12px" variant="rounded" />
+            <SkeletonEl width="95%" height="12px" variant="rounded" />
+            <SkeletonEl width="90%" height="12px" variant="rounded" />
+          </div>
+          <div className="mt-2">
+            <SkeletonEl width="100px" height="36px" />
+          </div>
+        </div>
+      </div>
+    </SectionLoading>
+  );
+}
---
+++ app/ecosystems/[ecosystemSlug]/features/learn-more/learn-more.tsx
@@ -0,0 +1,55 @@
+import { ecosystemsApiClient } from "api-client/resources/ecosystems";
+import Image from "next/image";
+
+import { Section } from "app/ecosystems/components/section/section";
+
+import { Button } from "components/ds/button/button";
+import { Translate } from "components/layout/translate/translate";
+import { Typography } from "components/layout/typography/typography";
+
+import { TLearnMore } from "./learn-more.types";
+
+export async function LearnMore({ ecosystemSlug }: TLearnMore.Props) {
+  const ecosystem = await ecosystemsApiClient.fetch.getEcosystemBySlug({ slug: ecosystemSlug }).request({
+    next: { revalidate: 120 },
+  });
+
+  const article = ecosystem.relatedArticles?.[0];
+
+  if (!article) {
+    return null;
+  }
+
+  return (
+    <Section
+      iconProps={{ remixName: "ri-graduation-cap-line" }}
+      titleProps={{
+        translate: {
+          token: "v2.pages.ecosystems.detail.learnMore.title",
+          params: { name: ecosystem.name },
+        },
+      }}
+    >
+      <div className="flex flex-col items-start justify-between gap-6 lg:flex-row lg:items-center">
+        <Image
+          src={article.imageUrl}
+          alt={ecosystem.name}
+          width={600}
+          height={350}
+          className="h-full w-full rounded-xl object-cover object-center shadow-light aspect-[16/9]"
+        />
+        <div className="flex flex-col items-start justify-between gap-4">
+          <Typography variant={"title-m"}>{article.title}</Typography>
+          <Typography variant={"body-l"} className="text-spaceBlue-100">
+            {article.description}
+          </Typography>
+          <div className="mt-2">
+            <Button as={"a"} href={article.url} target={"_blank"}>
+              <Translate token={"v2.pages.ecosystems.detail.learnMore.button"} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+}
