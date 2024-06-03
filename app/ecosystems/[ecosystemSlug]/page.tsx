0a1c7357e077093c0daea063f5ccfd5a73231477
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-06-03 17:05:02 +0200

    b-1785-ecosystem-top-10-projects-ui (#2207)

--- app/ecosystems/[ecosystemSlug]/page.tsx
+++ app/ecosystems/[ecosystemSlug]/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { LeaderBoard } from "app/ecosystems/[ecosystemSlug]/features/leader-board/leader-board";
 import { LearnMore } from "app/ecosystems/[ecosystemSlug]/features/learn-more/learn-more";
@@ -6,6 +7,8 @@ import { LearnMoreLoading } from "app/ecosystems/[ecosystemSlug]/features/learn-
 import { Overview } from "app/ecosystems/[ecosystemSlug]/features/overview/overview";
 import { OverviewLoading } from "app/ecosystems/[ecosystemSlug]/features/overview/overview.loading";
 import { ProjectGoodFirstIssues } from "app/ecosystems/[ecosystemSlug]/features/project-good-first-issues/project-good-first-issues";
+import { TopProjects } from "app/ecosystems/[ecosystemSlug]/features/top-projects/top-projects";
+import { TopProjectsLoading } from "app/ecosystems/[ecosystemSlug]/features/top-projects/top-projects.loading";
 import { SectionLoading } from "app/ecosystems/components/section/section.loading";
 
 import { Button } from "components/ds/button/button";
@@ -72,6 +75,13 @@ export default async function EcosystemDetailPage({ params }: { params: { ecosys
             </div>
           </div>
         </Container>
+
+        <ErrorBoundary fallback={null}>
+          <Suspense fallback={<TopProjectsLoading />}>
+            <TopProjects ecosystemSlug={ecosystemSlug} />
+          </Suspense>
+        </ErrorBoundary>
+
         <Suspense fallback={<LearnMoreLoading />}>
           <LearnMore ecosystemSlug={ecosystemSlug} />
         </Suspense>
