b338bad1c4b8f1053c078e26b92a885904fad652
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-04 20:36:05 +0200

    refactor(project): side panel ecosystem link

--- src/components/Project/Overview/OverviewEcosystem.tsx
+++ src/components/Project/Overview/OverviewEcosystem.tsx
@@ -5,6 +5,8 @@ import RoundedImage, { ImageSize, Rounding } from "src/components/RoundedImage";
 
 import { Link } from "components/ds/link/link";
 
+import { NEXT_ROUTER } from "constants/router";
+
 import { useIntl } from "hooks/translate/use-translate";
 
 import Section, { SectionIcon } from "./OverviewSection";
@@ -23,8 +25,12 @@ export const ProjectOverviewEcosystem = ({ ecosystems }: Props) => {
   return sortedByName?.length ? (
     <Section icon={SectionIcon.Global} title={T("project.details.overview.ecosystems", { count: sortedByName.length })}>
       <div className="flex flex-row flex-wrap gap-3">
-        {sortedByName.map(({ id, url, name, logoUrl }) => (
-          <Link key={id} href={url} className="flex flex-row items-center gap-2 text-sm font-normal">
+        {sortedByName.map(({ id, slug, name, logoUrl }) => (
+          <Link
+            key={id}
+            href={NEXT_ROUTER.ecosystems.details.root(slug)}
+            className="flex flex-row items-center gap-2 text-sm font-normal"
+          >
             <RoundedImage alt={name} rounding={Rounding.Circle} size={ImageSize.Sm} src={logoUrl} />
             {name}
           </Link>
