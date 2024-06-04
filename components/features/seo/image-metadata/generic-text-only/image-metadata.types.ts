811d9a1d32d34388b3773565e1244c048b862000
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-04 09:02:15 +0200

    Add open graph image

---
+++ components/features/seo/image-metadata/generic-text-only/image-metadata.types.ts
@@ -0,0 +1,5 @@
+import { TContent } from "components/features/seo/image-metadata/commons/content/content.types";
+
+export namespace TGenericTextOnlyImageMetadataProps {
+  export interface Props extends TContent.Props {}
+}
