98844a47e5d186b27cde6d569ceb32cf4dfdb21c
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-05 11:10:51 +0200

    request logger info

--- app/logger/[...endpoint]/route.tsx
+++ app/logger/[...endpoint]/route.tsx
@@ -1,19 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-function paramsToObject(entries: IterableIterator<[string, string]>) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const result: any = {};
-  for (const [key, value] of entries) {
-    result[key] = value;
-  }
-  return result;
-}
-
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const status = parseInt(searchParams.get("status") ?? "201");
   searchParams.delete("status");
-  console.log(`PARAMS : ${JSON.stringify(paramsToObject(searchParams.entries()))}`);
   return new NextResponse(request.body, {
     status,
   });
