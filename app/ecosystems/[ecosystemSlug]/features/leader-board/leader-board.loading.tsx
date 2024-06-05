1a8dff23e4573967b78099eed5f711dd7a435d60
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-05 18:22:25 +0200

    Fix loading responsive (#2273)

--- app/ecosystems/[ecosystemSlug]/features/leader-board/leader-board.loading.tsx
+++ app/ecosystems/[ecosystemSlug]/features/leader-board/leader-board.loading.tsx
@@ -5,7 +5,7 @@ import { SkeletonEl } from "components/ds/skeleton/skeleton";
 export function LeaderBoardLoading() {
   return (
     <SectionLoading>
-      <div className="grid gap-4 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <SkeletonEl width="100%" height="466px" variant="rounded" />
         <SkeletonEl width="100%" height="466px" variant="rounded" />
       </div>
