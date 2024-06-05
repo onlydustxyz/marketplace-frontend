e422b7b5bfc74614e5a91924d910af5b7cea3865
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-05 17:38:23 +0200

    B-1853

--- app/ecosystems/[ecosystemSlug]/features/learn-more/learn-more.tsx
+++ app/ecosystems/[ecosystemSlug]/features/learn-more/learn-more.tsx
@@ -31,15 +31,19 @@ export async function LearnMore({ ecosystemSlug }: TLearnMore.Props) {
       }}
     >
       <div className="flex flex-col items-start justify-between gap-6 lg:flex-row lg:items-center">
-        <Image
-          src={article.imageUrl}
-          alt={ecosystem.name}
-          width={600}
-          height={350}
-          className="h-full w-1/2 rounded-xl object-cover object-center shadow-light aspect-[16/9]"
-        />
+        <a rel="noreferrer" href={article.url} target={"_blank"} className="block w-1/2">
+          <Image
+            src={article.imageUrl}
+            alt={ecosystem.name}
+            width={600}
+            height={350}
+            className="h-full w-full rounded-xl object-cover object-center shadow-light aspect-[16/9]"
+          />
+        </a>
         <div className="flex flex-col items-start justify-between gap-4">
-          <Typography variant={"title-m"}>{article.title}</Typography>
+          <a href={article.url} rel="noreferrer" target={"_blank"}>
+            <Typography variant={"title-m"}>{article.title}</Typography>
+          </a>
           <Typography variant={"body-l"} className="text-spaceBlue-100">
             {article.description}
           </Typography>
