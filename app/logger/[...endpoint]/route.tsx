a1f88022738e3fdb23ad866442dc164e5308011b
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-05 10:24:17 +0200

    request logger

--- app/logger/[...endpoint]/route.tsx
+++ app/logger/[...endpoint]/route.tsx
@@ -4,8 +4,8 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const url = searchParams.get("url") ?? "";
   const status = parseInt(searchParams.get("status") ?? "201");
+  console.info(url);
   return new NextResponse(request.body, {
     status,
-    statusText: url,
   });
 }
