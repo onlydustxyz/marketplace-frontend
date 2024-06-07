84442abbc26b060729fd66dccf07f3729859b3d9
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-06-07 16:57:34 +0200

    B 1866 add most trendy projects (#2278)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,6 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 import { PageGrid } from "app/home/components/page-grid/page-grid";
 import { Activity } from "app/home/features/activity/activity";
 import { Journey } from "app/home/features/journey/journey";
@@ -7,30 +10,39 @@ import { Profile } from "app/home/features/profile/profile";
 import { RecommendedProjects } from "app/home/features/recommended-projects/recommended-projects";
 import { Rewards } from "app/home/features/rewards/rewards";
 import { TrendyProjects } from "app/home/features/trendy-projects/trendy-projects";
+import { TrendyProjectsLoading } from "app/home/features/trendy-projects/trendy-projects.loading";
 
 import { RequiredAuthGuard, RequiredUnauthGuard } from "components/features/auth0/guards/auth-guard";
 import { Container } from "components/layout/container/container";
+import { ScrollView } from "components/layout/pages/scroll-view/scroll-view";
 
 export default function HomePage() {
   return (
-    <Container>
-      <PageGrid>
-        <JourneyGuard>
-          <Journey />
-        </JourneyGuard>
+    <ScrollView>
+      <Container>
+        <PageGrid>
+          <JourneyGuard>
+            <Journey />
+          </JourneyGuard>
 
-        <RequiredAuthGuard>
-          <Profile />
-          <Rewards />
-          <LeadProjects />
-          <RecommendedProjects />
-        </RequiredAuthGuard>
+          <RequiredAuthGuard>
+            <Profile />
+            <Rewards />
+            <LeadProjects />
+            <RecommendedProjects />
+          </RequiredAuthGuard>
 
-        <RequiredUnauthGuard>
-          <TrendyProjects />
-          <Activity />
-        </RequiredUnauthGuard>
-      </PageGrid>
-    </Container>
+          <RequiredUnauthGuard>
+            <ErrorBoundary fallback={null}>
+              <Suspense fallback={<TrendyProjectsLoading />}>
+                <TrendyProjects />
+              </Suspense>
+            </ErrorBoundary>
+
+            <Activity />
+          </RequiredUnauthGuard>
+        </PageGrid>
+      </Container>
+    </ScrollView>
   );
 }
---
+++ components/ds/project-list-item/project-list-item.tsx
@@ -0,0 +1,33 @@
+import { cn } from "src/utils/cn";
+
+import { AvatarLabelled } from "components/ds/avatar/avatar.labelled";
+import { TProjectListItem } from "components/ds/project-list-item/project-list-item.types";
+import { BaseLink } from "components/layout/base-link/base-link";
+import { Icon } from "components/layout/icon/icon";
+import { Typography } from "components/layout/typography/typography";
+
+import { NEXT_ROUTER } from "constants/router";
+
+export function ProjectListItem({ project, className }: TProjectListItem.Props) {
+  return (
+    <BaseLink
+      href={NEXT_ROUTER.projects.details.root(project.slug)}
+      className={cn(
+        "group flex w-full max-w-full items-center gap-3 px-3 py-4 transition-all hover:bg-card-background-medium sm:gap-5 sm:px-5 sm:py-6",
+        className
+      )}
+    >
+      <AvatarLabelled
+        avatarProps={{ src: project.logoUrl, alt: project.name, size: "l", shape: "square" }}
+        labelProps={{ title: project.name }}
+        className="col-span-3 flex-1"
+      >
+        <Typography variant="title-s">{project.name}</Typography>
+        <Typography variant="body-s" className="line-clamp-2 whitespace-normal text-spaceBlue-100">
+          {project.shortDescription}
+        </Typography>
+      </AvatarLabelled>
+      <Icon remixName="ri-arrow-right-s-line" className="transition-all group-hover:translate-x-1" size={24} />
+    </BaseLink>
+  );
+}
