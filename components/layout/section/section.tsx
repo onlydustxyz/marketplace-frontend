25ede85f865a413b82419f1dc0195bb9b231dd86
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-10 15:07:57 +0200

    oups

--- components/layout/section/section.tsx
+++ components/layout/section/section.tsx
@@ -7,7 +7,7 @@ export function Section({ children, classNames, ...restProps }: TSection.Props) 
   return (
     <section className={cn("flex flex-col gap-4", classNames?.section)}>
       <SectionHeader {...restProps} />
-      <div className={cn("relative h-full", classNames?.content)}>{children}</div>
+      <div className={cn("relative", classNames?.content)}>{children}</div>
     </section>
   );
 }
