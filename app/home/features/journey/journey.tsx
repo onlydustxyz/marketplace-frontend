3295e3df045231b089b8e3c0f24514dea3474ed2
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-10 16:15:41 +0200

    WIP journey progressbar

--- app/home/features/journey/journey.tsx
+++ app/home/features/journey/journey.tsx
@@ -3,6 +3,7 @@ import styles from "app/home/styles/styles.module.css";
 import { cn } from "src/utils/cn";
 
 import { Card } from "components/ds/card/card";
+import { ProgressBar } from "components/ds/progress-bar/progress-bar";
 import { Section } from "components/layout/section/section";
 
 import { TJourney } from "./journey.types";
@@ -11,12 +12,16 @@ export function Journey(_: TJourney.Props) {
   return (
     <div className={cn("w-full", styles.areaJourney)}>
       <Section
-        iconProps={{ remixName: "ri-code-s-slash-line" }}
+        iconProps={{ remixName: "ri-rocket-2-line" }}
         titleProps={{
-          children: "Journey",
+          translate: {
+            token: "v2.pages.home.journey.title",
+          },
         }}
       >
-        <Card background={"base"}>Journey</Card>
+        <Card background={"base"}>
+          <ProgressBar maxValue={100} value={33} />
+        </Card>
       </Section>
     </div>
   );
--- translations/v2/en/pages/home/index.ts
+++ translations/v2/en/pages/home/index.ts
@@ -1,8 +1,9 @@
 import activity from "./activity.json";
+import journey from "./journey.json";
 import leadProjects from "./lead-projects.json";
 import profile from "./profile.json";
 import recommendedProjects from "./recommended-projects.json";
 import rewards from "./rewards.json";
 import trendyProjects from "./trendy-projects.json";
 
-export default { activity, leadProjects, profile, recommendedProjects, rewards, trendyProjects };
+export default { activity, leadProjects, profile, recommendedProjects, rewards, trendyProjects, journey };
