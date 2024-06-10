6f3e8621ce7e4468958a75cde8203da6fbc72a86
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-10 18:30:43 +0200

    WIP journey steps

--- app/home/features/journey/journey.private.tsx
+++ app/home/features/journey/journey.private.tsx
@@ -7,13 +7,34 @@ import styles from "app/home/styles/styles.module.css";
 import { cn } from "src/utils/cn";
 
 import { Card } from "components/ds/card/card";
+import { IconTag } from "components/ds/icon-tag/icon-tag";
 import { ProgressBar } from "components/ds/progress-bar/progress-bar";
+import { Icon } from "components/layout/icon/icon";
 import { Section } from "components/layout/section/section";
 import { Typography } from "components/layout/typography/typography";
 
 import { TJourney } from "./journey.types";
 
-export function JourneyPrivate(_: TJourney.Props) {
+function JourneyItem({ stepName, completion }: TJourney.JourneyItemProps) {
+  return (
+    <div className="flex items-center gap-4 py-5">
+      <Card background={"light"} className="w-fit !p-4">
+        <Icon remixName={TJourney.stepMapping[stepName]} size={24} />
+      </Card>
+      <div className="flex-1">
+        <Typography translate={{ token: `v2.pages.home.journey.steps.${stepName}.title` }} variant="body-m" />
+        <Typography
+          translate={{ token: `v2.pages.home.journey.steps.${stepName}.description` }}
+          variant="body-s"
+          className="text-spaceBlue-200"
+        />
+      </div>
+      <IconTag icon={{ remixName: completion ? "ri-check-line" : "ri-arrow-right-s-line", size: 12 }} size={"s"} />
+    </div>
+  );
+}
+
+export function JourneyPrivate(_: TJourney.JourneyPrivateProps) {
   const { data, isLoading } = meApiClient.queries.useGetMyJourney({});
 
   if (!data && !isLoading) return null;
@@ -28,7 +49,7 @@ export function JourneyPrivate(_: TJourney.Props) {
           },
         }}
       >
-        <Card background={"base"}>
+        <Card background={"base"} className="flex flex-col gap-3">
           <div className="flex items-center gap-2">
             <ProgressBar
               maxValue={100}
@@ -43,6 +64,10 @@ export function JourneyPrivate(_: TJourney.Props) {
               translate={{ token: "v2.pages.home.journey.progressPercentage", params: { count: data.completion } }}
               className="text-spaceBlue-200"
             />
+          </div>
+          <div className="flex flex-col divide-y divide-card-border-light">
+            <JourneyItem stepName="step1" completion={data.individualBillingProfileSetup} />
+            <JourneyItem stepName="step1" completion={data.individualBillingProfileSetup} />
           </div>
         </Card>
       </Section>
--- app/home/features/journey/journey.types.ts
+++ app/home/features/journey/journey.types.ts
@@ -1,5 +1,19 @@
 import { PropsWithChildren } from "react";
 
+import { RemixIconsName } from "components/layout/icon/remix-icon-names.types";
+
 export namespace TJourney {
-  export interface Props extends PropsWithChildren {}
+  export interface JourneyItemProps {
+    stepName: string;
+    completion: boolean;
+  }
+  export interface JourneyPrivateProps extends PropsWithChildren {}
+
+  export const stepMapping: Record<string, RemixIconsName> = {
+    step1: "ri-building-line",
+    step2: "ri-stack-line",
+    step3: "ri-medal-2-fill",
+    step4: "ri-user-line",
+    step5: "ri-send-plane-2-line",
+  };
 }
