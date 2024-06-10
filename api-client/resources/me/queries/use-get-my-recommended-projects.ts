4a46da69acae7103aafa3650f6a9f51dc16113a5
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-10 18:14:36 +0200

    fix(api): recommended projects

--- api-client/resources/me/queries/use-get-my-recommended-projects.ts
+++ api-client/resources/me/queries/use-get-my-recommended-projects.ts
@@ -4,13 +4,12 @@ import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { useReactInfiniteQueryAdapter } from "api-client/adapter/react-infinite-query/react-infinite-query-adapter";
 import { getMyRecommendedProjects } from "api-client/resources/me/fetch";
 import { GetRecommendedProjectsPageResponse } from "api-client/resources/me/types";
+import { ParametersInterfaceWithReactQuery } from "api-client/types/parameters-interface";
 
-type Params = Parameters<typeof getMyRecommendedProjects>;
-
-export const useGetMyRecommendedProjects = ({ params }: { params: Params[0] }) => {
-  const query = useReactInfiniteQueryAdapter<GetRecommendedProjectsPageResponse>(getMyRecommendedProjects(params));
-
-  return useSuspenseInfiniteQuery<GetRecommendedProjectsPageResponse>({
-    ...query,
-  });
+export const useGetMyRecommendedProjects = (
+  fetch: ParametersInterfaceWithReactQuery<typeof getMyRecommendedProjects>
+) => {
+  return useSuspenseInfiniteQuery<GetRecommendedProjectsPageResponse>(
+    useReactInfiniteQueryAdapter<GetRecommendedProjectsPageResponse>(getMyRecommendedProjects(fetch))
+  );
 };
