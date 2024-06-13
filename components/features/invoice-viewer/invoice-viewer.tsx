ff6981e40454ab9649a8e392ccf5065c1bbb5166
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-06-13 17:19:21 +0200

    Term and condition (#2101)
    (cherry picked from commit 1e406aa595a26084d5013fdeae4c308bfc8bfbf8)

--- components/features/invoice-viewer/invoice-viewer.tsx
+++ components/features/invoice-viewer/invoice-viewer.tsx
@@ -3,19 +3,21 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
+import { cn } from "src/utils/cn";
+
 import "./external-styles/annotation-layer.css";
 import "./external-styles/text-layer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.js", import.meta.url).toString();
 
-export default function InvoiceViewer({ fileUrl }: { fileUrl: string }) {
+export default function InvoiceViewer({ fileUrl, className }: { fileUrl: string; className?: string }) {
   const [numPages, setNumPages] = useState(0);
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
   }
 
   return (
-    <Document className="w-fit" file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
+    <Document className={cn("w-fit", className)} file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
       {Array.from(new Array(numPages), (_el, index) => (
         <Page key={`page_${index + 1}`} pageNumber={index + 1} className="mb-2" />
       ))}
