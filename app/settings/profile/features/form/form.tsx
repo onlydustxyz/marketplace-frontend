41b2005e0609237d0e908e7c54ca8082a57fc92d
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-06-13 17:04:53 +0200

    refactor(settings): removed technologies from profile edition (#2322)

--- app/settings/profile/features/form/form.tsx
+++ app/settings/profile/features/form/form.tsx
@@ -1,6 +1,5 @@
 import { FormContact } from "./contact/contact";
 import { FormInformations } from "./informations/informations";
-import { FormTechnologies } from "./technologies/technologies";
 import { FormWeeklyAllocatedTime } from "./weekly-allocated-time/weekly-allocated-time";
 
 export function ProfileForm() {
@@ -8,7 +7,6 @@ export function ProfileForm() {
     <>
       <FormInformations />
       <FormContact />
-      <FormTechnologies />
       <FormWeeklyAllocatedTime />
     </>
   );
