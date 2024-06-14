d0763f283f60a9dfaf0e6883e73f3cc1bb772398
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-14 12:05:01 +0200

    add overview acccordion

--- app/p/[slug]/features/good-first-issues/components/issue-card/issue-card.tsx
+++ app/p/[slug]/features/good-first-issues/components/issue-card/issue-card.tsx
@@ -1,6 +1,9 @@
+import { Accordion, AccordionItem, Selection } from "@nextui-org/react";
+import { useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { viewportConfig } from "src/config";
+import { cn } from "src/utils/cn";
 import displayRelativeDate from "src/utils/displayRelativeDate";
 
 import { Card } from "components/ds/card/card";
@@ -10,10 +13,14 @@ import { Flex } from "components/layout/flex/flex";
 import { Icon } from "components/layout/icon/icon";
 import { Typography } from "components/layout/typography/typography";
 
+import { useIntl } from "hooks/translate/use-translate";
+
 import { ApplyButton } from "./components/apply-button/apply-button";
 import { TIssueCard } from "./issue-card.types";
 
 export function IssueCard({ issue }: TIssueCard.Props) {
+  const { T } = useIntl();
+  const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set("1"));
   const isMd = useMediaQuery(`(min-width: ${viewportConfig.breakpoints.md}px)`);
 
   return (
@@ -87,6 +94,46 @@ export function IssueCard({ issue }: TIssueCard.Props) {
         </Flex>
 
         {!isMd ? <ApplyButton url={issue.htmlUrl} /> : null}
+
+        <Accordion
+          className="!p-0"
+          selectedKeys={selectedKeys}
+          onSelectionChange={setSelectedKeys}
+          defaultSelectedKeys={["1"]}
+        >
+          <AccordionItem
+            key="1"
+            className="!rounded-2xl !border-1 !border-card-border-light !bg-card-background-medium !px-0 !shadow-none"
+            classNames={{
+              content: "!px-4 !py-4",
+              trigger: "!px-4 justify-start items-center",
+              startContent: "!flex-shrink gap-2",
+              indicator: "text-snow",
+            }}
+            indicator={({ isOpen }) => (
+              <Icon
+                remixName="ri-arrow-down-s-line"
+                className={cn("transition-transform", { "rotate-180": isOpen })}
+                size={24}
+              />
+            )}
+            startContent={
+              <div className="flex items-center gap-2">
+                <Icon remixName="ri-bill-line" size={16} />
+                <Typography
+                  variant="body-m"
+                  translate={{ token: "v2.pages.project.overview.goodFirstIssues.overview" }}
+                  className="uppercase"
+                />
+              </div>
+            }
+            disableIndicatorAnimation
+            aria-label={issue.title}
+            // title={T("v2.pages.project.overview.goodFirstIssues.overview")}
+          >
+            {issue.body}
+          </AccordionItem>
+        </Accordion>
       </Flex>
     </Card>
   );
