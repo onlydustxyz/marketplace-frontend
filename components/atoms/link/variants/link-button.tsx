55e9b5fd6284b1bc1778256af6356d52afaba3db
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-18 18:06:34 +0200

    Fix review

--- components/atoms/link/variants/link-button.tsx
+++ components/atoms/link/variants/link-button.tsx
@@ -5,7 +5,7 @@ import { LinkCoreVariants } from "components/atoms/link/link.variants";
 import { TLinkButtonProps } from "../link.types";
 
 export function LinkButton({ classNames, ...props }: TLinkButtonProps) {
-  const { color } = props;
+  const { color, ...htmlProps } = props;
   const slots = LinkCoreVariants({ color });
-  return <button className={cn(slots.base(), classNames?.base)} {...props} />;
+  return <button className={cn(slots.base(), classNames?.base)} {...htmlProps} />;
 }
