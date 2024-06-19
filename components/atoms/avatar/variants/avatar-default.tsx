a9017a5dcaff8cdc09ad367dbfd5c3e398638114
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-19 19:32:33 +0200

    Fix story

--- components/atoms/avatar/variants/avatar-default.tsx
+++ components/atoms/avatar/variants/avatar-default.tsx
@@ -1,7 +1,9 @@
 import { AvatarNextUiAdapter } from "components/atoms/avatar/adapters/next-ui/next-ui.adapter";
 
+// KEEP RELATIVE FOR STORYBOOK AUTODOCS
 import { AvatarCore } from "../avatar.core";
+import { AvatarPort } from "../avatar.types";
 
-export function Avatar({ ...props }) {
+export function Avatar({ ...props }: AvatarPort) {
   return <AvatarCore Adapter={AvatarNextUiAdapter} {...props} />;
 }
