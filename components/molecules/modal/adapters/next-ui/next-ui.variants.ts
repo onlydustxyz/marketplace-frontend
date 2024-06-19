0444ab656994bd18877411da4fe7490607dc8546
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-19 18:25:29 +0200

    styles(modal): backdrop

--- components/molecules/modal/adapters/next-ui/next-ui.variants.ts
+++ components/molecules/modal/adapters/next-ui/next-ui.variants.ts
@@ -4,7 +4,7 @@ export const ModalNextUiVariants = tv({
   slots: {
     modal: "group max-w-[500px] rounded-xl p-0",
     body: "p-4",
-    backdrop: "backdrop-blur-sm",
+    backdrop: "bg-container-backdrop",
     header: "flex items-center justify-between gap-4 p-4 pb-0",
     footer: "flex items-center justify-between gap-4 p-4",
   },
