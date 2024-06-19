5ce4974f3a3ed62f6a8426265b79a9898fef6b1a
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-19 17:40:34 +0200

    Drawer

--- components/molecules/drawer/drawer.types.ts
+++ components/molecules/drawer/drawer.types.ts
@@ -6,21 +6,17 @@ import { DrawerCoreVariants } from "./drawer.variants";
 type Variants = VariantProps<typeof DrawerCoreVariants>;
 type classNames = Partial<typeof DrawerCoreVariants["slots"]>;
 
-interface HeaderProps {
+interface BlockProps {
   startContent?: ReactNode;
   endContent?: ReactNode;
 }
 
-interface FooterProps {
-  startContent?: ReactNode;
-  endContent?: ReactNode;
-}
 export interface DrawerPort<C extends ElementType> extends Variants, PropsWithChildren {
   htmlProps?: ComponentPropsWithoutRef<C>;
   as?: C;
   classNames?: classNames;
-  header?: HeaderProps;
-  footer?: FooterProps;
+  header?: BlockProps;
+  footer?: BlockProps;
   onOpenChange?: (value: boolean) => void;
   isOpen?: boolean;
 }
