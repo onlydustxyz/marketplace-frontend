b3f7734af3874725cdfa122e3cfebb973fbb319b
Alexis Benoliel <alexbenoliel@gmail.com>
2024-06-20 10:02:12 +0200

    Fix review

--- components/atoms/link/variants/link-default.tsx
+++ components/atoms/link/variants/link-default.tsx
@@ -3,6 +3,6 @@ import { LinkDefaultAdapter } from "components/atoms/link/adapters/default/defau
 import { LinkCore } from "../link.core";
 import { LinkPort } from "../link.types";
 
-export function Link({ ...props }: LinkPort) {
+export function Link(props: LinkPort) {
   return <LinkCore Adapter={LinkDefaultAdapter} {...props} />;
 }
