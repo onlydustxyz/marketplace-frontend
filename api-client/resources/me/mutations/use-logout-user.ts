e62eee5b71fd8dff85231fe3ccc582b90525c46c
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-20 17:40:58 +0200

    spread options

--- api-client/resources/me/mutations/use-logout-user.ts
+++ api-client/resources/me/mutations/use-logout-user.ts
@@ -4,13 +4,11 @@ import { useMutation } from "@tanstack/react-query";
 import { useReactQueryAdapter } from "api-client/adapter/react-query/react-query-adapter";
 import { logoutUser } from "api-client/resources/me/fetch/logout-user";
 
-export const useLogoutUser = (options?: { onSuccess?: () => void }) => {
+export const useLogoutUser = (options: { onSuccess?: () => void } = {}) => {
   const { mutation } = useReactQueryAdapter(logoutUser());
 
   return useMutation({
     ...mutation,
-    onSuccess: async () => {
-      options?.onSuccess?.();
-    },
+    ...options,
   });
 };
