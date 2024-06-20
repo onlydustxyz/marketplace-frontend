0016103bf9ea9542e65996225fbca8eb52cc4aaf
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-20 22:37:41 +0200

    fix(typo): html props

--- components/atoms/typo/typo.core.tsx
+++ components/atoms/typo/typo.core.tsx
@@ -12,13 +12,15 @@ export function TypoCore<C extends ElementType = "span">({
   translate,
   children,
   as,
+  htmlProps,
   ...props
 }: TTypoProps<C>) {
-  const slots = TypoCoreVariants({ ...props });
+  const { weight, variant, size, color } = props;
+  const slots = TypoCoreVariants({ weight, variant, size, color });
   const Component = as || "span";
 
   return (
-    <Component {...props} className={cn(slots.base(), classNames?.base)}>
+    <Component {...htmlProps} className={cn(slots.base(), classNames?.base)}>
       {translate ? <Translate {...translate} /> : children}
     </Component>
   );
