9a4c34615105ba08870e0587806243ba6f22d377
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-20 13:16:09 +0200

    WIP handle dynamic scope

--- components/features/grant-permission/hooks/useDynamicScopes.ts
+++ components/features/grant-permission/hooks/useDynamicScopes.ts
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { meApiClient } from "api-client/resources/me";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocalStorage } from "react-use";
 
 import { handleLoginWithRedirect } from "components/features/auth0/handlers/handle-login";
@@ -9,7 +9,9 @@ import { useCurrentUser } from "hooks/users/use-current-user/use-current-user";
 
 export function useDynamicScopes() {
   const { loginWithRedirect } = useAuth0();
+  const hasLogout = useRef(false);
   const [scopeStorage, setScopeStorage] = useLocalStorage("dynamic-github-public-repo-scope");
+  const [hasAskedForPermission, setHasAskedForPermission] = useState(false);
 
   const { mutate: logoutUser } = meApiClient.mutations.useLogoutUser({
     onSuccess: () => {
@@ -19,16 +21,26 @@ export function useDynamicScopes() {
 
   const { user } = useCurrentUser();
   const canApply = user?.isAuthorizedToApplyOnGithubIssues;
+  // const canApply = false;
 
   useEffect(() => {
-    if (scopeStorage && !canApply) {
-      logoutUser({});
+    if (hasAskedForPermission) {
+      if (scopeStorage && !canApply) {
+        if (!hasLogout.current) {
+          hasLogout.current = true;
+          logoutUser({});
+        }
+      }
+      if (!scopeStorage && canApply) {
+        handleLoginWithRedirect(loginWithRedirect, { grantPermissionFLowTriggered: "true" });
+      }
     }
-  }, [scopeStorage, canApply]);
+  }, [scopeStorage, canApply, hasAskedForPermission]);
 
   function handleAddDynamicScopes() {
     if (process.env.NEXT_PUBLIC_GITHUB_PUBLIC_REPO_SCOPE) {
       setScopeStorage(process.env.NEXT_PUBLIC_GITHUB_PUBLIC_REPO_SCOPE);
+      setHasAskedForPermission(true);
     }
   }
 
