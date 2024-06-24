5fdcc2b76792208e5f3142a1c9420e6ee3b25c63
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-24 12:14:28 +0200

    Simplify grant permission flow (#2375)

--- components/features/grant-permission/hooks/use-public-repo-scope.ts
+++ components/features/grant-permission/hooks/use-public-repo-scope.ts
@@ -1,91 +1,44 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { PopupConfigOptions, PopupLoginOptions } from "@auth0/auth0-spa-js";
 import { meApiClient } from "api-client/resources/me";
-import { useEffect, useRef, useState } from "react";
-import { useLocalStorage } from "react-use";
+
+import { handleLoginWithRedirect } from "components/features/auth0/handlers/handle-login";
 
 import { useCurrentUser } from "hooks/users/use-current-user/use-current-user";
 
 async function handleLoginWithPopup(
   loginWithPopup: (options?: PopupLoginOptions, config?: PopupConfigOptions) => Promise<void>
 ) {
-  await loginWithPopup({
+  return loginWithPopup({
     authorizationParams: { connection_scope: process.env.NEXT_PUBLIC_GITHUB_PUBLIC_REPO_SCOPE },
   });
 }
 
-export function usePublicRepoScope({
-  onCreateSuccess,
-  onUpdateSuccess,
-}: {
-  onCreateSuccess?: () => void;
-  onUpdateSuccess?: () => void;
-}) {
-  const { loginWithPopup } = useAuth0();
-  const hasLogout = useRef(false);
-  const [scopeStorage, setScopeStorage] = useLocalStorage("dynamic-github-public-repo-scope");
-  const [hasAskedForPermission, setHasAskedForPermission] = useState<false | "update-permission" | "create-permission">(
-    false
-  );
-
-  const isAuthFlowTerminated = useRef<false | "update-permission-terminated" | "create-permission-terminated">(false);
-
-  const { mutate: logoutUser } = meApiClient.mutations.useLogoutUser({
-    onSuccess: async () => {
-      await handleLoginWithPopup(loginWithPopup).then(() => {
-        isAuthFlowTerminated.current = "create-permission-terminated";
-      });
-    },
-  });
-
+export function usePublicRepoScope({ onSuccessCallback }: { onSuccessCallback?: () => void }) {
+  const { loginWithPopup, isAuthenticated, loginWithRedirect } = useAuth0();
   const { user } = useCurrentUser();
   const canApply = user?.isAuthorizedToApplyOnGithubIssues;
 
-  useEffect(() => {
-    if (!hasLogout.current && hasAskedForPermission && scopeStorage) {
-      hasLogout.current = true;
-      if (hasAskedForPermission === "create-permission") {
-        logoutUser({});
-      }
-      if (hasAskedForPermission === "update-permission") {
-        handleLoginWithPopup(loginWithPopup).then(() => {
-          isAuthFlowTerminated.current = "update-permission-terminated";
-        });
-      }
-    }
-  }, [scopeStorage, canApply, hasAskedForPermission]);
+  const { mutateAsync: logoutUser } = meApiClient.mutations.useLogoutUser({});
 
-  useEffect(() => {
-    if (canApply) {
-      if (isAuthFlowTerminated.current === "create-permission-terminated") {
-        isAuthFlowTerminated.current = false;
-        onCreateSuccess?.();
-      }
-      if (isAuthFlowTerminated.current === "update-permission-terminated") {
-        isAuthFlowTerminated.current = false;
-        onUpdateSuccess?.();
-      }
-    }
-  }, [canApply]);
+  async function getPermissions() {
+    await logoutUser({});
+    await handleLoginWithPopup(loginWithPopup);
+  }
 
-  function createAskForPermission() {
+  async function handleVerifyPermissions() {
+    if (!isAuthenticated) {
+      await handleLoginWithRedirect(loginWithRedirect);
+      return;
+    }
+
     if (!canApply) {
-      return "create-permission";
+      await getPermissions();
+      onSuccessCallback?.();
+      return;
     }
-
-    if (canApply && !scopeStorage) {
-      return "update-permission";
-    }
-
-    return false;
+    onSuccessCallback?.();
   }
 
-  function handleAddPublicRepoScope() {
-    if (process.env.NEXT_PUBLIC_GITHUB_PUBLIC_REPO_SCOPE) {
-      setScopeStorage(process.env.NEXT_PUBLIC_GITHUB_PUBLIC_REPO_SCOPE);
-      setHasAskedForPermission(createAskForPermission());
-    }
-  }
-
-  return { handleAddPublicRepoScope, scopeStorage, canApply };
+  return { handleVerifyPermissions, getPermissions, canApply };
 }
