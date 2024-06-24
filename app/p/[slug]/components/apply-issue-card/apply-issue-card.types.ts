65ce03f5065cf44bdd2568e1be169c20de5c2511
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-24 16:28:59 +0200

    refactor(paper): decoupled variants

--- app/p/[slug]/components/apply-issue-card/apply-issue-card.types.ts
+++ app/p/[slug]/components/apply-issue-card/apply-issue-card.types.ts
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from "react";
 
-import { TPaperProps } from "components/atoms/paper";
+import { PaperPort } from "components/atoms/paper";
 import { TypoPort } from "components/atoms/typo/typo.types";
 import { TIcon } from "components/layout/icon/icon.types";
 
 export namespace TApplyIssueCard {
   export interface Props extends PropsWithChildren {
-    container?: TPaperProps<"article">["container"];
+    container?: PaperPort<"article">["container"];
     iconProps: TIcon.Props;
     titleProps: TypoPort<"span">;
     className?: string;
