ffcfc9e804b564eba327390e1e5e08420c676453
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-24 20:03:08 +0200

    refactor(checkbox): variants

--- components/atoms/checkbox/checkbox.types.ts
+++ components/atoms/checkbox/checkbox.types.ts
@@ -1,12 +1,18 @@
-import { VariantProps } from "tailwind-variants";
+interface Variants {
+  color: "white" | "black";
+  isDisabled: boolean;
+  isError: boolean;
+}
 
-import { CheckboxCoreVariants } from "./checkbox.variants";
+interface ClassNames {
+  base: string;
+  label: string;
+  wrapper: string;
+  icon: string;
+}
 
-type Variants = VariantProps<typeof CheckboxCoreVariants>;
-type classNames = Partial<typeof CheckboxCoreVariants["slots"]>;
-
-export interface CheckboxPort extends Variants {
-  classNames?: classNames;
+export interface CheckboxPort extends Partial<Variants> {
+  classNames?: Partial<ClassNames>;
   onChange?: (checked: boolean) => void;
   value?: boolean;
   isDisabled?: boolean;
