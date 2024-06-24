f1d74b3918ae0ac6094898ce413a27db78afc721
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-06-24 17:30:03 +0200

    Revert "clean form when closing drawer, and update form value when change"
    This reverts commit 81d4915585994bc304c7d453b1bcb126d3202633.

--- app/p/[slug]/features/apply-issue-drawer/apply-issue-drawer.tsx
+++ app/p/[slug]/features/apply-issue-drawer/apply-issue-drawer.tsx
@@ -25,7 +25,7 @@ export function ApplyIssueDrawer({ issue, hasApplied, state }: TApplyIssueDrawer
   const { capture } = usePosthog();
   const {
     project: { data: project },
-    form: { control, handleSubmit, reset, setValue },
+    form: { control, handleSubmit },
     create: { isPending: createIsPending },
     update: { isPending: updateIsPending },
     delete: { isPending: deleteIsPending },
@@ -42,17 +42,6 @@ export function ApplyIssueDrawer({ issue, hasApplied, state }: TApplyIssueDrawer
       });
     }
   }, [isOpen, project]);
-
-  useEffect(() => {
-    if (!isOpen) {
-      reset();
-    }
-  }, [isOpen]);
-
-  useEffect(() => {
-    setValue("motivations", issue.currentUserApplication?.motivations ?? "");
-    setValue("problemSolvingApproach", issue.currentUserApplication?.problemSolvingApproach ?? "");
-  }, [issue]);
 
   const header = useMemo(() => {
     const StartContent = (
