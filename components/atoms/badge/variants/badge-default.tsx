78dca7072c2dd6b1e4fbd401093c03993315c855
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 19:45:10 +0200

    refactor(badge): HOC

--- components/atoms/badge/variants/badge-default.tsx
+++ components/atoms/badge/variants/badge-default.tsx
@@ -1,10 +1,10 @@
 import { ElementType } from "react";
 
 import { BadgeDefaultAdapter } from "components/atoms/badge/adapters/default/default.adapter";
-import { BadgeCore } from "components/atoms/badge/badge.core";
+import { withComponentAdapter } from "components/hocs/with-component-adapter";
 
 import { BadgePort } from "../badge.types";
 
 export function Badge<C extends ElementType = "div">(props: BadgePort<C>) {
-  return <BadgeCore Adapter={BadgeDefaultAdapter} {...props} />;
+  return withComponentAdapter<BadgePort<C>>(BadgeDefaultAdapter)(props);
 }
--- components/atoms/badge/variants/badge-dot.tsx
+++ components/atoms/badge/variants/badge-dot.tsx
@@ -1,19 +1,16 @@
 import { ElementType } from "react";
 
 import { BadgeDefaultAdapter } from "components/atoms/badge/adapters/default/default.adapter";
+import { withComponentAdapter } from "components/hocs/with-component-adapter";
 
-import { BadgeCore } from "../badge.core";
 import { BadgeDotPortDot } from "../badge.types";
 
 export function BadgeDot<C extends ElementType = "div">(props: BadgeDotPortDot<C>) {
-  return (
-    <BadgeCore
-      Adapter={BadgeDefaultAdapter}
-      {...props}
-      hideContent={true}
-      classNames={{
-        base: "h-2 w-2 rounded-full p-0",
-      }}
-    />
-  );
+  return withComponentAdapter<BadgeDotPortDot<C>>(BadgeDefaultAdapter)({
+    ...props,
+    hideContent: true,
+    classNames: {
+      base: "h-2 w-2 rounded-full p-0",
+    },
+  });
 }
