4dbec7667fdfbcd2818079c04d067bba62a8f413
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 19:54:06 +0200

    refactor(link): HOC

--- components/atoms/link/index.ts
+++ components/atoms/link/index.ts
@@ -1,3 +1,2 @@
-export * from "./link.core";
 export * from "./variants/link-default";
 export * from "./link.types";
--- components/atoms/link/variants/link-default.tsx
+++ components/atoms/link/variants/link-default.tsx
@@ -1,8 +1,8 @@
 import { LinkDefaultAdapter } from "components/atoms/link/adapters/default/default.adapter";
+import { withComponentAdapter } from "components/hocs/with-component-adapter";
 
-import { LinkCore } from "../link.core";
 import { LinkPort } from "../link.types";
 
 export function Link(props: LinkPort) {
-  return <LinkCore Adapter={LinkDefaultAdapter} {...props} />;
+  return withComponentAdapter<LinkPort>(LinkDefaultAdapter)(props);
 }
