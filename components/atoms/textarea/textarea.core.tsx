0f358fa020b0bbc6adbb23c01deda5a7126d5ac6
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 19:03:58 +0200

    refactor(textarea): HOC

--- components/atoms/textarea/textarea.core.tsx
+++
@@ -1,8 +0,0 @@
-import { forwardRef } from "react";
-
-import { TextareaPort } from "components/atoms/textarea/textarea.types";
-import { PropsWithAdapter } from "components/types/props-with-adapter";
-
-export const TextareaCore = forwardRef(function TextareaCore({ Adapter, ...props }: PropsWithAdapter<TextareaPort>) {
-  return <Adapter {...props} />;
-});
--- components/atoms/textarea/variants/textarea-default.tsx
+++ components/atoms/textarea/variants/textarea-default.tsx
@@ -1,9 +1,10 @@
 import { ForwardedRef, forwardRef } from "react";
 
+import { withComponentAdapter } from "components/hocs/with-component-adapter";
+
 import { TextareaNextUiAdapter } from "../adapters/next-ui/next-ui.adapter";
-import { TextareaCore } from "../textarea.core";
 import { TextareaPort } from "../textarea.types";
 
 export const Textarea = forwardRef(function Textarea(props: TextareaPort, ref: ForwardedRef<HTMLTextAreaElement>) {
-  return <TextareaCore ref={ref} Adapter={TextareaNextUiAdapter} {...props} />;
+  return withComponentAdapter<TextareaPort, HTMLTextAreaElement>(TextareaNextUiAdapter)(props, ref);
 });
