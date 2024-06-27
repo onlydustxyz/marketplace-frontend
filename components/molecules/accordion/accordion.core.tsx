3e1aac89f3f497c3fe14d69002f7a7fc4543a631
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 21:19:24 +0200

    refactor(accordion): HOC

--- components/molecules/accordion/accordion.core.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithAdapter } from "components/types/props-with-adapter";
-
-import { AccordionPort } from "./accordion.types";
-
-export function AccordionCore({ Adapter, ...props }: PropsWithAdapter<AccordionPort>) {
-  return <Adapter {...props} />;
-}
--- components/molecules/accordion/index.ts
+++ components/molecules/accordion/index.ts
@@ -1,4 +1,3 @@
-export * from "./accordion.core";
 export * from "./accordion.loading";
 export * from "./accordion.types";
 export * from "./variants/accordion-default";
