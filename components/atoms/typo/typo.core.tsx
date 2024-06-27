4bdf4b01fdf2990b583f63af376a68b25cfa386d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 20:08:42 +0200

    refactor(typo): HOC

--- components/atoms/typo/typo.core.tsx
+++
@@ -1,9 +0,0 @@
-import { ElementType } from "react";
-
-import { PropsWithAdapter } from "components/types/props-with-adapter";
-
-import { TypoPort } from "./typo.types";
-
-export function TypoCore<C extends ElementType = "span">({ Adapter, ...props }: PropsWithAdapter<TypoPort<C>>) {
-  return <Adapter {...props} />;
-}
