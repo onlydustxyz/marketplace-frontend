14c308e22e5f0c7ac82ff12a0d929c4bbe0f0dbc
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 19:41:32 +0200

    refactor(avatar): HOC

--- components/atoms/avatar/avatar.core.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithAdapter } from "components/types/props-with-adapter";
-
-import { AvatarPort } from "./avatar.types";
-
-export function AvatarCore({ Adapter, ...props }: PropsWithAdapter<AvatarPort>) {
-  return <Adapter {...props} />;
-}
