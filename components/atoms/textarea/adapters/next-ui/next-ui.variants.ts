ec30a7d0fc8ba560342b274722532df03fc99b4d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-27 19:07:59 +0200

    styless(textarea): font size

--- components/atoms/textarea/adapters/next-ui/next-ui.variants.ts
+++ components/atoms/textarea/adapters/next-ui/next-ui.variants.ts
@@ -7,7 +7,7 @@ export const TextareaNextUiVariants = tv({
     inputWrapper:
       "rounded-md border border-container-stroke-separator text-text-1 !outline-none group-data-[focus=true]:border-interactions-white-hover group-data-[hover=true]:border-interactions-white-hover",
     innerWrapper: "",
-    input: "text-xs text-text-1",
+    input: "text-sm text-text-1",
     errorMessage: "",
     label: "text-xs text-text-1",
     helperWrapper: "",
