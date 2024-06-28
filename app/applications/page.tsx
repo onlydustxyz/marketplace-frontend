cae1fc2ace66bd5984691d4694cbe491b882819e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-06-28 15:44:22 +0200

    feat(applications): translations

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,3 +1,15 @@
+import { Typo } from "components/atoms/typo";
+import { PosthogOnMount } from "components/features/posthog/components/posthog-on-mount/posthog-on-mount";
+
 export default function ApplicationsPage() {
-  return <div>Applications</div>;
+  return (
+    <>
+      <PosthogOnMount eventName={"my_applications_list_viewed"} />
+      <div className="mx-auto w-full px-2 sm:px-8">
+        <div className="flex flex-col gap-10 py-8">
+          <Typo as={"h1"} variant={"brand"} size={"2xl"} translate={{ token: "v2.pages.applications.table.title" }} />
+        </div>
+      </div>
+    </>
+  );
 }
