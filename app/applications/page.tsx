f7412741af735b8f11ae2d3a40bed16b876cea7a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-01 11:11:11 +0200

    wip(applications): page

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { Typo } from "components/atoms/typo";
 import { PosthogOnMount } from "components/features/posthog/components/posthog-on-mount/posthog-on-mount";
 
+import { ApplicationsTable } from "./features/applications-table/applications-table";
+import { useApplicationsTable } from "./features/applications-table/applications-table.hooks";
+import { ApplicationsTableLoading } from "./features/applications-table/applications-table.loading";
+
 export default function ApplicationsPage() {
+  const { query } = useApplicationsTable();
+  const { isLoading } = query;
+
   return (
     <>
       <PosthogOnMount eventName={"my_applications_list_viewed"} />
       <div className="mx-auto w-full px-2 sm:px-8">
         <div className="flex flex-col gap-10 py-8">
           <Typo as={"h1"} variant={"brand"} size={"2xl"} translate={{ token: "v2.pages.applications.table.title" }} />
+
+          {isLoading ? <ApplicationsTableLoading /> : <ApplicationsTable />}
         </div>
       </div>
     </>
