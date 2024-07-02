b5b43098efbd770fac78cf1a4bc4b292e8f78a61
Alexis Benoliel <alexbenoliel@gmail.com>
2024-07-02 16:58:21 +0200

    Add project type

--- app/h/[slug]/components/header/header.types.ts
+++ app/h/[slug]/components/header/header.types.ts
@@ -1,17 +1,15 @@
+import { ProjectLinkResponse } from "api-client/resources/projects/types";
 import { ReactNode } from "react";
-
-import { components } from "src/__generated/api";
 
 import { HackathonStatus } from "components/organisms/hackathon-card";
 
 export namespace THeader {
-  type Project = components["schemas"]["ProjectLinkResponse"];
   export interface Props {
     title: string;
     slug?: string;
     location?: ReactNode;
     startDate: string;
     status: HackathonStatus;
-    projects?: Project[];
+    projects?: ProjectLinkResponse[];
   }
 }
