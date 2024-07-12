345884d061004e54662b7f8beb17d59316d4ac3f
Alexis Benoliel <alexbenoliel@gmail.com>
2024-07-12 14:17:25 +0200

    Review

--- components/molecules/cards/card-project/card-project.stories.tsx
+++ components/molecules/cards/card-project/card-project.stories.tsx
@@ -7,7 +7,7 @@ import { CardProject } from "./variants/card-project-default";
 
 type Story = StoryObj<typeof CardProject>;
 
-const defaultPort: CardProjectPort<"div"> = {
+const defaultProps: CardProjectPort<"div"> = {
   avatarProps: {},
   title: "Starknet dart",
   description: "The goal of this SDK is to be able to interact with StarkNet smart contracts in a type-safe way.",
@@ -38,7 +38,7 @@ export const Default: Story = {
   render: args => {
     return (
       <div className="flex w-[370px] items-center gap-2">
-        <CardProject {...defaultPort} {...args} />
+        <CardProject {...defaultProps} {...args} />
       </div>
     );
   },
