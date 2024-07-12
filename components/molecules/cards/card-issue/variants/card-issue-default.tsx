83876672335cf659b94542c341da193b61d7d965
Alexis Benoliel <alexbenoliel@gmail.com>
2024-07-12 14:26:45 +0200

    fix build

--- components/molecules/cards/card-issue/variants/card-issue-default.tsx
+++ components/molecules/cards/card-issue/variants/card-issue-default.tsx
@@ -1,10 +1,10 @@
-import { CardIssueDefaultAdapter } from "../adapters/default/default.adapter";
-import { CardIssuePort } from "../card-issue.types";
-import { withComponentAdapter } from "components/hocs/with-component-adapter";
 import { ElementType } from "react";
 
-export function CardIssue<C extends ElementType = "div">(
-  props: CardIssuePort<C>
-) {
+import { withComponentAdapter } from "components/hocs/with-component-adapter";
+
+import { CardIssueDefaultAdapter } from "../adapters/default/default.adapter";
+import { CardIssuePort } from "../card-issue.types";
+
+export function CardIssue<C extends ElementType = "div">(props: CardIssuePort<C>) {
   return withComponentAdapter<CardIssuePort<C>>(CardIssueDefaultAdapter)(props);
 }
