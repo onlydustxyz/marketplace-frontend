a5acfda259318d808f807012d0bb7ec1a4de08a0
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-12 11:03:03 +0200

    refactor(hackathon): prepared sidebar layout

--- app/hackathons/[hackathonSlug]/page.tsx
+++ app/hackathons/[hackathonSlug]/page.tsx
@@ -4,9 +4,12 @@ import { Paper } from "components/atoms/paper";
 
 export default function HackathonPage({ params }: { params: { hackathonSlug: string } }) {
   return (
-    <>
-      <Header hackathonSlug={params.hackathonSlug} />
-      <Paper size={"m"}>Content</Paper>
-    </>
+    <div className={"flex gap-4"}>
+      <div className={"flex-1"}>
+        <Header hackathonSlug={params.hackathonSlug} />
+        <Paper size={"m"}>Content</Paper>
+      </div>
+      {/*<aside>Sidebar</aside>*/}
+    </div>
   );
 }
