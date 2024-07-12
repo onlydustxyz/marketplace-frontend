51f2022d195f1951f01b49decb25d5375f2c308a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-12 14:43:28 +0200

    feat(hackathon): data fetching

--- app/hackathons/[hackathonSlug]/page.tsx
+++ app/hackathons/[hackathonSlug]/page.tsx
@@ -1,13 +1,29 @@
+import { bootstrap } from "core/bootstrap";
+import { Hackathon } from "core/domain/hackathon/models/hackathon-modal";
+import { notFound } from "next/navigation";
+
 import { Header } from "app/hackathons/[hackathonSlug]/components/header/header";
 
 import { Paper } from "components/atoms/paper";
 
-export default function HackathonPage({ params }: { params: { hackathonSlug: string } }) {
+async function getHackathon(hackathonSlug: string) {
+  try {
+    const hackathonStorage = bootstrap.getHackathonStoragePortForClient();
+    return await hackathonStorage.getHackathonBySlug({ pathParams: { hackathonSlug } }).request();
+  } catch {
+    notFound();
+  }
+}
+
+export default async function HackathonPage({ params }: { params: { hackathonSlug: string } }) {
+  const data = await getHackathon(params.hackathonSlug);
+  const hackathon = new Hackathon(data);
+
   return (
     <div className={"flex gap-4"}>
       <div className={"flex-1"}>
         <Header hackathonSlug={params.hackathonSlug} />
-        <Paper size={"m"}>Content</Paper>
+        <Paper size={"m"}>{hackathon.title}</Paper>
       </div>
       {/*<aside>Sidebar</aside>*/}
     </div>
