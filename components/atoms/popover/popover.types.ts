628c3968759b901bb98b3797f6850f4c396df0d4
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-07-15 15:02:08 +0200

    Design system: Popover (#2481)

---
+++ components/atoms/popover/popover.types.ts
@@ -0,0 +1,34 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+
+export interface PopoverContextPort {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface PopoverPort {
+  children: ReactNode[];
+  defaultOpen?: boolean;
+  placement?:
+    | "top-start"
+    | "top"
+    | "top-end"
+    | "bottom-start"
+    | "bottom"
+    | "bottom-end"
+    | "right-start"
+    | "right"
+    | "right-end"
+    | "left-start"
+    | "left"
+    | "left-end";
+  closeOnBlur?: boolean;
+}
+
+export interface PopoverTriggerPort {
+  children(args: PopoverContextPort): ReactNode;
+}
+
+export interface PopoverContentPort {
+  children(args: PopoverContextPort): ReactNode;
+  className?: string;
+}
