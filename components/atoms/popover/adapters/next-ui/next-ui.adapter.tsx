334a9b467b0a45a6d8af6ec83bcaa7a8fc9c142a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-15 14:43:01 +0200

    feat(ds): popover

---
+++ components/atoms/popover/adapters/next-ui/next-ui.adapter.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
+import { createContext, useContext, useState } from "react";
+
+import { cn } from "src/utils/cn";
+
+import {
+  PopoverContentPort,
+  PopoverContextPort,
+  PopoverPort,
+  PopoverTriggerPort,
+} from "components/atoms/popover/popover.types";
+
+const PopoverContext = createContext<PopoverContextPort>({
+  isOpen: false,
+  setIsOpen: () => {},
+});
+
+export function PopoverNextUiAdapter({
+  children,
+  defaultOpen = false,
+  placement = "bottom-start",
+  closeOnBlur = true,
+}: PopoverPort) {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  return (
+    <PopoverContext.Provider value={{ isOpen, setIsOpen }}>
+      <Popover isOpen={isOpen} onOpenChange={setIsOpen} placement={placement} shouldCloseOnBlur={closeOnBlur}>
+        {children}
+      </Popover>
+    </PopoverContext.Provider>
+  );
+}
+
+PopoverNextUiAdapter.Trigger = function PopoverNextUiAdapterTrigger({ children }: PopoverTriggerPort) {
+  const context = useContext(PopoverContext);
+
+  return <PopoverTrigger>{children(context)}</PopoverTrigger>;
+};
+
+PopoverNextUiAdapter.Content = function PopoverNextUiAdapterContent({ children, className }: PopoverContentPort) {
+  const context = useContext(PopoverContext);
+
+  return (
+    <PopoverContent
+      className={cn(
+        "rounded-xl border border-container-stroke-separator bg-container-action p-3 text-text-1 shadow-none",
+        className
+      )}
+    >
+      {children(context)}
+    </PopoverContent>
+  );
+};
