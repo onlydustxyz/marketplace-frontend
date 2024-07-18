b46dd44ce950ce58002277c5ecb409b8ab18020e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-18 09:58:34 +0200

    wip

--- core/infrastructure/marketplace-api-client-adapter/http/fetch-http-client/fetch-http-client.ts
+++ core/infrastructure/marketplace-api-client-adapter/http/fetch-http-client/fetch-http-client.ts
@@ -1,37 +1,36 @@
-import { FirstParameter } from "core/helpers/types";
-import { HttpClient } from "core/infrastructure/marketplace-api-client-adapter/http/http-client/http-client";
+// interface FetchHttpClientInterface extends HttpClient {}
+interface FetchHttpClientInterface {}
 
-interface FetchHttpClientInterface extends HttpClient {}
-
-export class FetchHttpClient extends HttpClient implements FetchHttpClientInterface {
+// export class FetchHttpClient extends HttpClient implements FetchHttpClientInterface {
+export class FetchHttpClient implements FetchHttpClientInterface {
   constructor() {
-    super();
+    // super();
   }
 
-  async request<R>({
-    path,
-    method,
-    tag,
-    pathParams,
-    queryParams,
-    version,
-    body,
-    next: nextParams = {},
-  }: FirstParameter<FetchHttpClientInterface["request"]>): Promise<R> {
-    const url = this.buildUrl({ path, pathParams, queryParams, version });
-    const headers = await this.getHeaders();
-    const tags = tag ? [tag] : undefined;
-    const next = { ...nextParams, tags };
-    const cache = !nextParams?.revalidate ? "no-cache" : undefined;
-
-    const response = await fetch(url, {
-      method,
-      headers,
-      body,
-      next,
-      cache,
-    });
-
-    return this.formatResponse<R>(response);
-  }
+  // async request<R>({
+  //   path,
+  //   method,
+  //   tag,
+  //   pathParams,
+  //   queryParams,
+  //   version,
+  //   body,
+  //   next: nextParams = {},
+  // }: FirstParameter<FetchHttpClientInterface["request"]>): Promise<R> {
+  //   const url = this.buildUrl({ path, pathParams, queryParams, version });
+  //   const headers = await this.getHeaders();
+  //   const tags = tag ? [tag] : undefined;
+  //   const next = { ...nextParams, tags };
+  //   const cache = !nextParams?.revalidate ? "no-cache" : undefined;
+  //
+  //   const response = await fetch(url, {
+  //     method,
+  //     headers,
+  //     body,
+  //     next,
+  //     cache,
+  //   });
+  //
+  //   return this.formatResponse<R>(response);
+  // }
 }
