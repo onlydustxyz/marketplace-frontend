666c1de203c2a1a36bd74ae97a2e912c60044fc4
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-07-18 17:27:55 +0200

    Drop old hackathon page and related components/utils/api-client... (#2507)

--- api-client/resources/hackathons/tags.ts
+++
@@ -1,7 +0,0 @@
-import { apiResources } from "api-client/config/resources";
-
-export default {
-  root: `${apiResources.hackathons}-root`,
-  details: `${apiResources.hackathons}-details`,
-  by_slug: (slug: string) => `${apiResources.hackathons}-by-slug-${slug}`,
-};
