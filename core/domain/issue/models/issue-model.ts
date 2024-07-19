daa3536d7f92fa23a5a14734371135f784e87480
Alexis Benoliel <alexbenoliel@gmail.com>
2024-07-19 11:03:06 +0200

    revamp extend classes

--- core/domain/issue/models/issue-model.ts
+++ core/domain/issue/models/issue-model.ts
@@ -1,5 +1,5 @@
-import { GithubIssueListItemResponse, ListIssue, ListIssueInterface } from "core/domain/issue/models/list-issue-model";
-import { extendMultipleClasses } from "core/infrastructure/marketplace-api-client-adapter/mappers/extend-multiple-classes";
+import { GithubIssueListItemResponse, ListIssue } from "core/domain/issue/models/list-issue-model";
+import { extendClasses } from "core/infrastructure/marketplace-api-client-adapter/mappers/extend-classes";
 import { mapApiToClass } from "core/infrastructure/marketplace-api-client-adapter/mappers/map-api-to-class";
 
 import { components } from "src/__generated/api";
@@ -11,12 +11,12 @@ interface GithubIssueResponse extends _GithubIssueResponse {
   assignees: GithubIssueListItemResponse["assignees"];
 }
 
-export interface IssueInterface extends ListIssueInterface, GithubIssueResponse {}
-
-const ExtendedWithListIssue = extendMultipleClasses(mapApiToClass<GithubIssueResponse>(), ListIssue);
-
-export class Issue extends ExtendedWithListIssue implements IssueInterface {
-  constructor(protected readonly props: GithubIssueResponse) {
+export interface IssueInterface extends GithubIssueResponse {}
+export class Issue extends mapApiToClass<GithubIssueResponse>() implements IssueInterface {
+  constructor(protected props: GithubIssueResponse) {
     super(props);
+    Object.assign(this, props);
   }
 }
+
+extendClasses(Issue, [ListIssue]);
