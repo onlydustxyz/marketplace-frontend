9ce0a772bce06e4c16705c87d4a0278b32a229d1
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-07-19 14:04:48 +0200

    revamp extend classes (#2516)

---
+++ core/infrastructure/marketplace-api-client-adapter/mappers/extend-classes.ts
@@ -0,0 +1,13 @@
+import { AnyType } from "core/helpers/types";
+
+export function extendClasses(derivedCtor: AnyType, constructors: AnyType[]) {
+  constructors.forEach(baseCtor => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null)
+      );
+    });
+  });
+}
