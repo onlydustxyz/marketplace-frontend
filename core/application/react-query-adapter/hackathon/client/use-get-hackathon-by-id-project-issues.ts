930504715a096eec9c73229dd7ccc290c5106c31
Sami Lafrance <sami.lepays@gmail.com>
2024-07-22 11:08:52 +0200

    fix(*): request type

--- core/application/react-query-adapter/hackathon/client/use-get-hackathon-by-id-project-issues.ts
+++ core/application/react-query-adapter/hackathon/client/use-get-hackathon-by-id-project-issues.ts
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { useQueryAdapter } from "core/application/react-query-adapter/helpers/use-query-adapter";
-import { ReactQueryParameters } from "core/application/react-query-adapter/react-query-adapter.types";
+import { UseQueryFacadeParams, useQueryAdapter } from "core/application/react-query-adapter/helpers/use-query-adapter";
 import { bootstrap } from "core/bootstrap";
+import { GetHackathonByIdProjectIssuesFormattedResponse } from "core/domain/hackathon/hackathon-contract.types";
 import { HackathonFacadePort } from "core/domain/hackathon/inputs/hackathon-facade-port";
 
 export function useGetHackathonByIdProjectIssues({
   pathParams,
   queryParams,
   options,
-}: ReactQueryParameters<HackathonFacadePort["getHackathonByIdProjectIssues"]>) {
+}: UseQueryFacadeParams<
+  HackathonFacadePort["getHackathonByIdProjectIssues"],
+  GetHackathonByIdProjectIssuesFormattedResponse
+>) {
   const hackathonStoragePort = bootstrap.getHackathonStoragePortForClient();
 
   return useQuery(
