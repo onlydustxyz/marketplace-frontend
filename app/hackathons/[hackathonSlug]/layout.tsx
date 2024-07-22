5fbb12bbb5ed62d9ad2434bc90ea6f2f99b59b97
Alexis Benoliel <alexbenoliel@gmail.com>
2024-07-22 18:31:25 +0200

    Remove hackathon layout

--- app/hackathons/[hackathonSlug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function HackathonLayout({ children }: PropsWithChildren) {
-  // return <Container>{children}</Container>;
-  return children;
-}
