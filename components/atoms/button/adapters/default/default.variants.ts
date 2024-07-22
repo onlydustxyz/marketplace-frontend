6f50b5e43f55bc8051f3a2ea814009f8c2277fb5
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-07-22 17:34:35 +0200

    fix button height (#2529)

--- components/atoms/button/adapters/default/default.variants.ts
+++ components/atoms/button/adapters/default/default.variants.ts
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 
 export const ButtonDefaultVariants = tv({
   slots: {
-    base: "group relative flex w-fit cursor-pointer overflow-hidden rounded-lg bg-container-1 transition-colors",
+    base: "group relative flex h-fit w-fit cursor-pointer overflow-hidden rounded-lg bg-container-1 transition-colors",
     content: "flex w-full flex-row items-center justify-center",
     startIcon: "transition-color text-inherit",
     endIcon: "transition-color text-inherit",
