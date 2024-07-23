9c68d8db36b6335c72c028a8cf3486ca8afab2aa
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-23 09:54:12 +0200

    refactor(hackathon): moved display date to core

--- core/domain/hackathon/models/list-hackathon-model.ts
+++ core/domain/hackathon/models/list-hackathon-model.ts
@@ -1,4 +1,5 @@
 import { bootstrap } from "core/bootstrap";
+import { DateFacadePort } from "core/helpers/date/date-facade-port";
 import { mapApiToClass } from "core/infrastructure/marketplace-api-client-adapter/mappers/map-api-to-class";
 
 import { components } from "src/__generated/api";
@@ -12,26 +13,27 @@ export interface ListHackathonInterface extends HackathonsListResponse {
   isLive(): boolean;
   isPast(): boolean;
   getStatus(): HackathonStatus;
+  formatOpenGraphDate(): string;
 }
 
 export class ListHackathon extends mapApiToClass<HackathonsListResponse>() implements ListHackathonInterface {
+  dateHelper: DateFacadePort;
+
   constructor(protected props: HackathonsListResponse) {
     super(props);
+    this.dateHelper = bootstrap.getDateHelperPort();
   }
 
   isComingSoon() {
-    return bootstrap.getDateHelperPort().isFuture(new Date(this.startDate));
+    return this.dateHelper.isFuture(new Date(this.startDate));
   }
 
   isLive() {
-    return (
-      bootstrap.getDateHelperPort().isPast(new Date(this.startDate)) &&
-      bootstrap.getDateHelperPort().isFuture(new Date(this.endDate))
-    );
+    return this.dateHelper.isPast(new Date(this.startDate)) && this.dateHelper.isFuture(new Date(this.endDate));
   }
 
   isPast() {
-    return bootstrap.getDateHelperPort().isPast(new Date(this.endDate));
+    return this.dateHelper.isPast(new Date(this.endDate));
   }
 
   getStatus() {
@@ -44,5 +46,27 @@ export class ListHackathon extends mapApiToClass<HackathonsListResponse>() imple
     }
 
     return "closed";
+  }
+
+  formatOpenGraphDate() {
+    const startDate = new Date(this.startDate);
+    const endDate = new Date(this.endDate);
+    const format = this.dateHelper.format;
+
+    const start = {
+      day: format(startDate, "dd"),
+      month: format(startDate, "MMMM"),
+      year: format(startDate, "yyyy"),
+    };
+
+    const end = {
+      day: format(endDate, "dd"),
+      month: format(endDate, "MMMM"),
+    };
+
+    // March 18 - 24 2024
+    // March 18 - April 24 2024
+    // March 18 2024 - March 24 2025
+    return `${start.month} ${start.day} - ${end.month} ${end.day} ${start.year}`;
   }
 }
