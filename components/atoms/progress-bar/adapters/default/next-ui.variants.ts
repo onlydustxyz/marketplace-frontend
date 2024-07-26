d8e6c15a8effec5d80d874700534ba1d7310dbce
Sami Lafrance <sami.lepays@gmail.com>
2024-07-26 11:07:53 +0200

    fix(progress): indicator variant

--- components/atoms/progress-bar/adapters/default/next-ui.variants.ts
+++ components/atoms/progress-bar/adapters/default/next-ui.variants.ts
@@ -4,7 +4,7 @@ export const ProgressBarNextUiVariants = tv({
   slots: {
     base: "h-2",
     track: "border border-container-stroke-separator bg-interactions-black-disabled",
-    indicator: "bg-brand-2",
+    indicator: "",
   },
   variants: {
     color: {
