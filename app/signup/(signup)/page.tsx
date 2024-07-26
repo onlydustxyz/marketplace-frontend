bed858da63327d0b5d71b4d253f2158c629bde36
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-07-26 11:26:10 +0200

    Create sign up section (#2578)

--- app/signup/(signup)/page.tsx
+++ app/signup/(signup)/page.tsx
@@ -1,3 +1,5 @@
+import { SignupCtas } from "app/signup/(signup)/features/signup-ctas/signup-ctas";
+
 import { Paper } from "components/atoms/paper";
 import { Typo } from "components/atoms/typo";
 import { SignupTemplate } from "components/templates/signup-template/signup-template";
@@ -7,7 +9,7 @@ import { SigninCta } from "./features/signin-cta/signin-cta";
 export default function SignupPage() {
   return (
     <SignupTemplate>
-      <div>
+      <div className="flex h-full flex-col gap-3">
         <Paper size={"l"} container={"3"} classNames={{ base: "grid gap-6" }}>
           <div className="grid gap-2">
             <Typo
@@ -20,6 +22,19 @@ export default function SignupPage() {
           </div>
 
           <SigninCta />
+        </Paper>
+        <Paper size={"l"} container={"3"} classNames={{ base: "flex-1 flex flex-col gap-6" }}>
+          <div className="grid gap-2">
+            <Typo
+              size={"2xl"}
+              variant={"brand"}
+              color={"text-1"}
+              translate={{ token: "v2.pages.signup.signupSection.title" }}
+            />
+            <Typo size={"s"} color={"text-2"} translate={{ token: "v2.pages.signup.signupSection.subtitle" }} />
+          </div>
+
+          <SignupCtas />
         </Paper>
       </div>
     </SignupTemplate>
