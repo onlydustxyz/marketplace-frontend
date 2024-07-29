e9e4e92e01bca95533993f210024589e213fe683
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-29 15:44:44 +0200

    fix(query): set user on success callback

--- app/signup/onboarding/complete-your-profile/complete-your-profile.hooks.ts
+++ app/signup/onboarding/complete-your-profile/complete-your-profile.hooks.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserReactQueryAdapter } from "core/application/react-query-adapter/user";
 import { useClientBootstrapContext } from "core/bootstrap/client-bootstrap-context";
