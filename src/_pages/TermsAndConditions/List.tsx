23c923ce6c66c5a8cf9abd6106d1224ef5e64891
Sami Lafrance <sami.lepays@gmail.com>
2024-07-30 14:14:01 +0200

    fix(*): WIP

--- src/_pages/TermsAndConditions/List.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function List({ children }: PropsWithChildren) {
-  return <ul className="mb-4 mt-4 list-disc pl-4">{children}</ul>;
-}
--- src/_pages/TermsAndConditions/Paragraph.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function Paragraph({ children }: PropsWithChildren) {
-  return <p className="pt-3">{children}</p>;
-}
