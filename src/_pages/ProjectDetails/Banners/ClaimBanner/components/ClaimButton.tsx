cf9bb3cca145bf503566cbb29708f61ff8920f8f
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-07-31 15:47:26 +0200

    refactor(signup): claim button

--- src/_pages/ProjectDetails/Banners/ClaimBanner/components/ClaimButton.tsx
+++ src/_pages/ProjectDetails/Banners/ClaimBanner/components/ClaimButton.tsx
@@ -1,9 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from "next/navigation";
 
 import Button, { ButtonSize, ButtonType } from "src/components/Button";
 import MagicLine from "src/icons/MagicLine";
 
-import { handleLoginWithRedirect } from "components/features/auth0/handlers/handle-login";
+import { NEXT_ROUTER } from "constants/router";
 
 import { useIntl } from "hooks/translate/use-translate";
 
@@ -11,13 +12,14 @@ type ClaimButton = { callback: () => void };
 
 export function ClaimButton({ callback }: ClaimButton) {
   const { T } = useIntl();
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { isAuthenticated } = useAuth0();
+  const router = useRouter();
 
   const startprojectClaim = async () => {
     if (isAuthenticated) {
       callback();
     } else {
-      await handleLoginWithRedirect(loginWithRedirect);
+      router.push(NEXT_ROUTER.signup.root);
     }
   };
 
