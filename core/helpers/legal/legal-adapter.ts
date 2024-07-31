6f3ccb9174c4467ce6aa60709d2062d3b5af220b
Sami Lafrance <sami.lepays@gmail.com>
2024-07-31 14:35:51 +0200

    feat(legal-notice): add env

--- core/helpers/legal/legal-adapter.ts
+++ core/helpers/legal/legal-adapter.ts
@@ -2,8 +2,8 @@ import { LegalFacadePort } from "core/helpers/legal/legal-facade-port";
 
 export class LegalAdapter implements LegalFacadePort {
   private urls = {
-    terms: "https://od-legals-production.s3.eu-west-1.amazonaws.com/terms-and-conditions.pdf",
-    privacy: "https://od-legals-production.s3.eu-west-1.amazonaws.com/privacy-policy.pdf",
+    terms: `${process.env.NEXT_PUBLIC_LEGALS_S3_BUCKET}/terms-and-conditions.pdf`,
+    privacy: `${process.env.NEXT_PUBLIC_LEGALS_S3_BUCKET}/privacy-policy.pdf`,
   };
 
   getTermsAndConditionsUrl() {
