212c9a31e246c645ff6057a0a9b2db1d22bf4e29
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-01 11:43:45 +0200

    Revert "Update issue card (#2609)"
    This reverts commit 432f69f043a457670fb51cd28a0df3911a52213b.

--- components/atoms/radio-group/adapters/next-ui/next-ui.adapter.tsx
+++ components/atoms/radio-group/adapters/next-ui/next-ui.adapter.tsx
@@ -22,10 +22,7 @@ function Radio<V extends string, C extends ElementType = "div">({
   const slots = RadioGroupNextUiVariants({ isDisabled, isActive: isSelected, mixed, color });
 
   return (
-    <Component
-      {...getBaseProps()}
-      className={cn(slots.item(), classNames?.item, { "pointer-events-none": isDisabled })}
-    >
+    <Component {...getBaseProps()} className={cn(slots.item(), { "pointer-events-none": isDisabled })}>
       <InnerComponent {...componentProps}>
         <VisuallyHidden>
           <input {...getInputProps()} />
@@ -71,7 +68,7 @@ export function RadioGroupNextUiAdapter<V extends string, C extends ElementType 
       value={value}
     >
       {items.map(item => (
-        <Radio key={item.value} as={Component} classNames={classNames} {...props} {...item} />
+        <Radio key={item.value} as={Component} {...props} {...item} />
       ))}
     </NextRadioGroup>
   );
--- components/molecules/cards/card-issue/card-issue.hooks.tsx
+++ components/molecules/cards/card-issue/card-issue.hooks.tsx
@@ -1,40 +1,99 @@
 import { formatDistanceToNowStrict } from "date-fns";
 import { ReactNode } from "react";
 
+import { Avatar } from "components/atoms/avatar";
+import { ButtonPort } from "components/atoms/button/button.types";
 import { Button } from "components/atoms/button/variants/button-default";
 import { CardIssuePort } from "components/molecules/cards/card-issue/card-issue.types";
 
 type GetActionsProps = Pick<
   CardIssuePort<"div">,
-  "applyActionProps" | "viewActionProps" | "assignedActionProps" | "status" | "assignee" | "githubUsername"
+  "applyActionProps" | "viewActionProps" | "status" | "githubLink" | "assignee" | "tokens"
 >;
-type GetActionsReturn = ReactNode;
+type GetActionsReturn = ReactNode[];
 
-const useAction = ({
-  applyActionProps,
-  viewActionProps,
-  assignedActionProps,
-  status = "open",
-  assignee,
-  githubUsername,
-}: GetActionsProps): GetActionsReturn => {
-  if (status === "open") {
-    return <Button {...applyActionProps} />;
-  }
-
-  if (status === "applied") {
-    return <Button {...viewActionProps} />;
-  }
-
-  if (status === "assigned") {
-    if (assignee?.name === githubUsername) {
-      return <Button {...viewActionProps} />;
-    }
-
-    return <Button isDisabled {...assignedActionProps} />;
+const useGithubLinkComponent = ({ githubLink, tokens }: Pick<CardIssuePort<"div">, "githubLink" | "tokens">) => {
+  if (githubLink) {
+    return (
+      <Button
+        size={"s"}
+        as="a"
+        variant="secondary-light"
+        htmlProps={{ href: githubLink, target: "_blank" }}
+        startIcon={{ remixName: "ri-github-line" }}
+      >
+        {tokens.githubLink}
+      </Button>
+    );
   }
 
   return null;
+};
+
+const useAssigneeComponent = ({ assignee }: Pick<CardIssuePort<"div">, "assignee">) => {
+  if (assignee) {
+    const defaultProps: Partial<ButtonPort<"button">> = {
+      startContent: <Avatar shape={"square"} size={"xs"} {...assignee.avatar} />,
+      children: assignee.name,
+    };
+
+    if (assignee.href) {
+      return (
+        <Button size={"s"} variant="secondary-light" {...defaultProps} as="a" htmlProps={{ href: assignee.href }} />
+      );
+    }
+
+    if (assignee.onClick) {
+      return <Button size={"s"} variant="secondary-light" {...defaultProps} onClick={assignee.onClick} />;
+    }
+
+    return <Button size={"s"} variant="secondary-light" {...defaultProps} />;
+  }
+
+  return null;
+};
+
+const usePrimaryActionComponent = ({
+  applyActionProps,
+  viewActionProps,
+  status,
+}: Pick<CardIssuePort<"div">, "applyActionProps" | "viewActionProps" | "status">) => {
+  if (status === "open") {
+    return <Button size={"s"} {...applyActionProps} />;
+  }
+
+  if (status === "applied") {
+    return <Button size={"s"} {...viewActionProps} />;
+  }
+
+  return null;
+};
+
+const useActions = ({
+  applyActionProps,
+  viewActionProps,
+  status = "open",
+  githubLink,
+  tokens,
+  assignee,
+}: GetActionsProps): GetActionsReturn => {
+  const githubLinkComponent = useGithubLinkComponent({ githubLink, tokens });
+  const assigneeComponent = useAssigneeComponent({ assignee });
+  const primaryActionComponent = usePrimaryActionComponent({ applyActionProps, viewActionProps, status });
+
+  if (status === "open") {
+    return [githubLinkComponent, primaryActionComponent];
+  }
+
+  if (status === "applied") {
+    return [githubLinkComponent, primaryActionComponent];
+  }
+
+  if (status === "assigned") {
+    return [assigneeComponent, githubLinkComponent];
+  }
+
+  return [];
 };
 
 const useCreatedAt = ({ createdAt }: Pick<CardIssuePort<"div">, "createdAt">): string | null => {
@@ -46,6 +105,9 @@ const useCreatedAt = ({ createdAt }: Pick<CardIssuePort<"div">, "createdAt">): s
 };
 
 export const useCardIssue = {
-  useAction,
+  useAssigneeComponent,
+  usePrimaryActionComponent,
+  useGithubLinkComponent,
+  useActions,
   useCreatedAt,
 };
