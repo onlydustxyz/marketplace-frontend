5f2560fa25b7a026abb36a3c9e1fa38a6c3ff471
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-01 18:21:41 +0200

    Delete account already exist (#2627)

--- app/signup/components/account-already-exist/account-already-exist.tsx
+++ app/signup/components/account-already-exist/account-already-exist.tsx
@@ -7,11 +7,6 @@ import { TAccountAlreadyExist } from "app/signup/components/account-already-exis
 import { useLogout } from "src/App/Layout/Header/ProfileButton/Logout.hooks";
 
 import { Button } from "components/atoms/button/variants/button-default";
-import { Typo } from "components/atoms/typo";
-import { BaseLink } from "components/layout/base-link/base-link";
-import { Translate } from "components/layout/translate/translate";
-
-import { NEXT_ROUTER } from "constants/router";
 
 export function AccountAlreadyExist({ showDisconnectButton = true }: TAccountAlreadyExist.Props) {
   const { handleLogout } = useLogout();
@@ -20,18 +15,7 @@ export function AccountAlreadyExist({ showDisconnectButton = true }: TAccountAlr
   } = useClientBootstrapContext();
   const { isAuthenticated = false } = authProvider ?? {};
   return (
-    <div className="flex justify-between">
-      <Typo as="div" size="s" classNames={{ base: "py-2.5" }}>
-        <Translate token="v2.pages.signup.accountAlreadyExist.label" as={"span"} />
-        &nbsp;
-        <BaseLink href={NEXT_ROUTER.signup.root}>
-          <Translate
-            token="v2.pages.signup.accountAlreadyExist.link"
-            className="cursor-pointer text-text-2"
-            as={"span"}
-          />
-        </BaseLink>
-      </Typo>
+    <div className="flex justify-end">
       {isAuthenticated && showDisconnectButton ? (
         <Button
           variant="secondary-light"
