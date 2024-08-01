19d5360483eaa6bbd6c9362affc52fcb3f64ca94
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-01 14:35:42 +0200

    feat(onboarding): step guard

--- app/signup/onboarding/payout-information/page.tsx
+++ app/signup/onboarding/payout-information/page.tsx
@@ -2,16 +2,15 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { BillingProfileReactQueryAdapter } from "core/application/react-query-adapter/billing-profile";
-import { UserReactQueryAdapter } from "core/application/react-query-adapter/user";
-import { useClientBootstrapContext } from "core/bootstrap/client-bootstrap-context";
 import { BillingProfileTypeUnion } from "core/domain/billing-profile/models/billing-profile.types";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 import { AccountAlreadyExist } from "app/signup/components/account-already-exist/account-already-exist";
 import { StepHeader } from "app/signup/components/step-header/step-header";
 import { Title } from "app/signup/components/title/title";
+import { StepGuard } from "app/signup/onboarding/components/step-guard/step-guard";
 import { BillingProfiles } from "app/signup/onboarding/payout-information/components/billing-profiles/billing-profiles";
 import { TBillingProfiles } from "app/signup/onboarding/payout-information/components/billing-profiles/billing-profiles.types";
 
@@ -27,22 +26,6 @@ import { NEXT_ROUTER } from "constants/router";
 
 export default function PayoutInformationPage() {
   const router = useRouter();
-  const {
-    clientBootstrap: { authProvider },
-  } = useClientBootstrapContext();
-  const { isAuthenticated = false } = authProvider ?? {};
-
-  const { data: userOnboarding } = UserReactQueryAdapter.client.useGetMyOnboarding({
-    options: {
-      enabled: isAuthenticated,
-    },
-  });
-
-  useEffect(() => {
-    if (userOnboarding?.payoutInformationProvided) {
-      router.push(NEXT_ROUTER.signup.onboarding.root);
-    }
-  }, [userOnboarding]);
 
   const formMethods = useForm<TBillingProfiles.form>({
     resolver: zodResolver(TBillingProfiles.validation),
@@ -101,37 +84,40 @@ export default function PayoutInformationPage() {
   }, [isPendingCreateBillingProfile]);
 
   return (
-    <FormProvider {...formMethods}>
-      <form onSubmit={handleSubmit(handleCreateBillingProfile)} className="h-full">
-        <SignupTemplate header={<AccountAlreadyExist />} footer={renderFooter}>
-          <Paper size={"l"} container={"2"} classNames={{ base: "flex flex-col gap-6 min-h-full" }}>
-            <StepHeader
-              step={2}
-              stepPath={"/signup/onboarding"}
-              subStep={{ token: "v2.pages.signup.payoutInformation.stepTitle" }}
-            />
-            <Title
-              title={{ token: "v2.pages.signup.payoutInformation.title" }}
-              content={{ token: "v2.pages.signup.payoutInformation.subtitle" }}
-            />
-            <BillingProfiles />
-            <Paper size={"s"} container={"3"} classNames={{ base: "flex flex-col gap-2" }}>
-              <Typo
-                size={"xs"}
-                weight={"medium"}
-                color={"text-1"}
-                translate={{ token: "v2.pages.signup.payoutInformation.specialMention.title" }}
+    <>
+      <StepGuard step={"payoutInformationProvided"} />
+      <FormProvider {...formMethods}>
+        <form onSubmit={handleSubmit(handleCreateBillingProfile)} className="h-full">
+          <SignupTemplate header={<AccountAlreadyExist />} footer={renderFooter}>
+            <Paper size={"l"} container={"2"} classNames={{ base: "flex flex-col gap-6 min-h-full" }}>
+              <StepHeader
+                step={2}
+                stepPath={"/signup/onboarding"}
+                subStep={{ token: "v2.pages.signup.payoutInformation.stepTitle" }}
               />
-              <Typo
-                size={"xs"}
-                weight={"medium"}
-                color={"text-2"}
-                translate={{ token: "v2.pages.signup.payoutInformation.specialMention.description" }}
+              <Title
+                title={{ token: "v2.pages.signup.payoutInformation.title" }}
+                content={{ token: "v2.pages.signup.payoutInformation.subtitle" }}
               />
+              <BillingProfiles />
+              <Paper size={"s"} container={"3"} classNames={{ base: "flex flex-col gap-2" }}>
+                <Typo
+                  size={"xs"}
+                  weight={"medium"}
+                  color={"text-1"}
+                  translate={{ token: "v2.pages.signup.payoutInformation.specialMention.title" }}
+                />
+                <Typo
+                  size={"xs"}
+                  weight={"medium"}
+                  color={"text-2"}
+                  translate={{ token: "v2.pages.signup.payoutInformation.specialMention.description" }}
+                />
+              </Paper>
             </Paper>
-          </Paper>
-        </SignupTemplate>
-      </form>
-    </FormProvider>
+          </SignupTemplate>
+        </form>
+      </FormProvider>
+    </>
   );
 }
--- core/application/react-query-adapter/user/client/use-get-my-onboarding.ts
+++ core/application/react-query-adapter/user/client/use-get-my-onboarding.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { UseQueryFacadeParams, useQueryAdapter } from "core/application/react-query-adapter/helpers/use-query-adapter";
 import { bootstrap } from "core/bootstrap";
+import { useClientBootstrapContext } from "core/bootstrap/client-bootstrap-context";
 import { UserFacadePort } from "core/domain/user/inputs/user-facade-port";
 import { UserOnboardingInterface } from "core/domain/user/models/user-onboarding-model";
 
@@ -9,10 +10,18 @@ export function useGetMyOnboarding({
 }: UseQueryFacadeParams<UserFacadePort["getMyOnboarding"], UserOnboardingInterface>) {
   const userStoragePort = bootstrap.getUserStoragePortForClient();
 
+  const {
+    clientBootstrap: { authProvider },
+  } = useClientBootstrapContext();
+  const { isAuthenticated = false } = authProvider ?? {};
+
   return useQuery(
     useQueryAdapter({
       ...userStoragePort.getMyOnboarding({}),
-      options,
+      options: {
+        ...options,
+        enabled: isAuthenticated && options?.enabled,
+      },
     })
   );
 }
