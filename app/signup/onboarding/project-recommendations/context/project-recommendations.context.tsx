5001855645d0999d0a419e4989f55e3d7477dbea
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-01 18:06:25 +0200

    fix edit profile

--- app/signup/onboarding/project-recommendations/context/project-recommendations.context.tsx
+++ app/signup/onboarding/project-recommendations/context/project-recommendations.context.tsx
@@ -71,7 +71,7 @@ export function ProjectRecommendationContextProvider({ children }: TProjectRecom
   async function onSubmit(data: TProjectRecommendationContext.form) {
     await setMyProfile({
       ...data,
-      joiningGoal: data.goal as TProjectRecommendationContext.Goal,
+      joiningGoal: data.joiningGoal as TProjectRecommendationContext.Goal,
     });
   }
 
