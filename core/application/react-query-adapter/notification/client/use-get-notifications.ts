d560a1eb7e9ca801cbaeb2abb059cd1b211f2a12
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-02 09:38:09 +0200

    Fix query

--- core/application/react-query-adapter/notification/client/use-get-notifications.ts
+++ core/application/react-query-adapter/notification/client/use-get-notifications.ts
@@ -2,11 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import { UseQueryFacadeParams, useQueryAdapter } from "core/application/react-query-adapter/helpers/use-query-adapter";
 import { bootstrap } from "core/bootstrap";
 import { NotificationFacadePort } from "core/domain/notification/input/notification-facade-port";
+import { NotificationInterface } from "core/domain/notification/models/notification.types";
 import { GetNotificationsResponse } from "core/domain/notification/notification-contract.types";
 
 export function useGetNotifications({
   options,
-}: UseQueryFacadeParams<NotificationFacadePort["getNotifications"], GetNotificationsResponse>) {
+}: UseQueryFacadeParams<
+  NotificationFacadePort["getNotifications"],
+  Omit<GetNotificationsResponse, "notifications"> & {
+    notifications: NotificationInterface[];
+  }
+>) {
   const notificationStoragePort = bootstrap.getNotificationStoragePortForClient();
 
   return useQuery(
