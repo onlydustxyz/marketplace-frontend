d1ce600f8d1ee0cdbc18c1f6f59340cae92336d3
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-02 13:45:10 +0200

    Update mock

--- core/infrastructure/marketplace-api-client-adapter/mock-adapters/notification-client-adapter-mock.ts
+++ core/infrastructure/marketplace-api-client-adapter/mock-adapters/notification-client-adapter-mock.ts
@@ -10,47 +10,49 @@ export class NotificationClientAdapterMock implements NotificationStoragePort {
 
   getNotifications = () => {
     const request = async () => {
-      return new NotificationList({
+      return {
         totalPageNumber: 0,
         totalItemNumber: 0,
         hasMore: true,
         nextPageIndex: 0,
-        notifications: [
-          {
-            id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            timestamp: "2024-08-02T08:47:03.016Z",
-            status: "UNREAD",
-            type: "MAINTAINER_APPLICATION_TO_REVIEW",
-            data: {
-              maintainerApplicationToReview: {
-                project: {
-                  id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                  slug: "my-awesome-project",
-                  name: "string",
-                  logoUrl: "string",
-                },
-                applicant: {
-                  githubUserId: 595505,
-                  login: "ofux",
-                  avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
-                  isRegistered: true,
+        ...new NotificationList({
+          notifications: [
+            {
+              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+              timestamp: "2024-08-02T08:47:03.016Z",
+              status: "UNREAD",
+              type: "MAINTAINER_APPLICATION_TO_REVIEW",
+              data: {
+                maintainerApplicationToReview: {
+                  project: {
+                    id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    slug: "my-awesome-project",
+                    name: "string",
+                    logoUrl: "string",
+                  },
+                  applicant: {
+                    githubUserId: 595505,
+                    login: "ofux",
+                    avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+                    isRegistered: true,
+                  },
                 },
               },
             },
-          },
-          {
-            id: "4fa85f64-5717-4562-b3fc-2c963f66afa6",
-            timestamp: "2024-08-02T08:47:03.016Z",
-            status: "UNREAD",
-            type: "MAINTAINER_COMMITTEE_APPLICATION_CREATED",
-            data: {
-              maintainerCommitteeApplicationCreated: {
-                committeeName: "string",
+            {
+              id: "4fa85f64-5717-4562-b3fc-2c963f66afa6",
+              timestamp: "2024-08-02T08:47:03.016Z",
+              status: "UNREAD",
+              type: "MAINTAINER_COMMITTEE_APPLICATION_CREATED",
+              data: {
+                maintainerCommitteeApplicationCreated: {
+                  committeeName: "string",
+                },
               },
             },
-          },
-        ],
-      });
+          ],
+        }),
+      };
     };
 
     return {
