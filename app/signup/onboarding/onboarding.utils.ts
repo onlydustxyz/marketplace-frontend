3d7c30061faf10b73ad04be6cad169238615408f
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-02 11:59:11 +0200

    fix sanitize

--- app/signup/onboarding/onboarding.utils.ts
+++ app/signup/onboarding/onboarding.utils.ts
@@ -1,5 +1,5 @@
 export function sanitizeData<T extends string | boolean | undefined | string[], K extends string>(key: K, data: T) {
-  if (!!data || data === false || !data?.length) {
+  if (!!data || data === false || data?.length) {
     return { [key]: data };
   }
 
