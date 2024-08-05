a7c0e508ff9a2ce615537c145b17213e5835c862
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-05 12:03:56 +0200

    refactor(*): split providers

--- app/(v1)/layout.tsx
+++ app/(v1)/layout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 
 import { PageBanner } from "app/(v1)/features/page-banner/page-banner";
+import V1Providers from "app/(v1)/providers";
 
 import Header from "src/App/Layout/Header";
 
@@ -11,13 +12,15 @@ import { NEXT_ROUTER } from "constants/router";
 
 export default function V1Layout({ children }: PropsWithChildren) {
   return (
-    <div className="z-[1] flex h-[calc(100dvh)] w-screen flex-col bg-space-gradient xl:fixed">
-      <RenderByPath path={NEXT_ROUTER.signup.root} exact={false} matches={false}>
-        <PageBanner />
-        <Header />
-      </RenderByPath>
-      <SpaceBackground />
-      {children}
-    </div>
+    <V1Providers>
+      <div className="z-[1] flex h-[calc(100dvh)] w-screen flex-col bg-space-gradient xl:fixed">
+        <RenderByPath path={NEXT_ROUTER.signup.root} exact={false} matches={false}>
+          <PageBanner />
+          <Header />
+        </RenderByPath>
+        <SpaceBackground />
+        {children}
+      </div>
+    </V1Providers>
   );
 }
