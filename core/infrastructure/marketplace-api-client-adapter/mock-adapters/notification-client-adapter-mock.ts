4ca34b7f17555614cf3eacf5bc58ff9d37989f7f
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-06 09:49:27 +0200

    fix build

--- core/infrastructure/marketplace-api-client-adapter/mock-adapters/notification-client-adapter-mock.ts
+++ core/infrastructure/marketplace-api-client-adapter/mock-adapters/notification-client-adapter-mock.ts
@@ -19,34 +19,13 @@ export class NotificationClientAdapterMock implements NotificationStoragePort {
         nextPageIndex: 0,
         notifications: [
           {
-            id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            timestamp: "2024-08-02T08:47:03.016Z",
-            status: "UNREAD" as const,
-            type: "MAINTAINER_APPLICATION_TO_REVIEW" as const,
-            data: {
-              maintainerApplicationToReview: {
-                project: {
-                  id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                  slug: "my-awesome-project",
-                  name: "string",
-                  logoUrl: "string",
-                },
-                applicant: {
-                  githubUserId: 595505,
-                  login: "ofux",
-                  avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
-                  isRegistered: true,
-                },
-              },
-            },
-          },
-          {
             id: "4fa85f64-5717-4562-b3fc-2c963f66afa6",
             timestamp: "2024-08-02T08:47:03.016Z",
             status: "UNREAD" as const,
             type: "MAINTAINER_COMMITTEE_APPLICATION_CREATED" as const,
             data: {
               maintainerCommitteeApplicationCreated: {
+                committeeId: "string",
                 committeeName: "string",
               },
             },
