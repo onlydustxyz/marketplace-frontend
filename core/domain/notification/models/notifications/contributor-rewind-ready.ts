cfe842232ca471865495391ed4f94ab1a4e346ab
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-23 15:01:36 +0100

    feat: add ContributorRewindReady notification model and update notification button to handle null notifications
    - Introduced a new notification model for ContributorRewindReady.
    - Updated the notification button component to safely render notifications, ensuring that null values are handled gracefully.

---
+++ core/domain/notification/models/notifications/contributor-rewind-ready.ts
@@ -0,0 +1,41 @@
+import { Notification } from "core/domain/notification/models/notification-model";
+import { NotificationInterface } from "core/domain/notification/models/notification.types";
+import { NotificationStatus } from "core/domain/notification/notification-constants";
+
+import { components } from "src/__generated/api";
+
+export class ContributorRewindReady implements NotificationInterface {
+  data: components["schemas"]["NotificationContributorRewindReady"] | undefined;
+  constructor(private notification: Notification) {
+    this.data = notification.data.contributorRewindReady;
+  }
+
+  getId() {
+    return this.notification.id;
+  }
+
+  getTimestamp() {
+    return this.notification.timestamp;
+  }
+
+  getStatus() {
+    return this.notification.status;
+  }
+
+  hasRead() {
+    return this.notification.status === NotificationStatus.READ;
+  }
+
+  getTitle() {
+    return "Your OSS Rewind is ready";
+  }
+
+  getDescription() {
+    const { year } = this.data || {};
+    return `Your OSS Rewind for ${year} is ready!`;
+  }
+
+  getUrl() {
+    return "https://rewind.onlydust.com";
+  }
+}
